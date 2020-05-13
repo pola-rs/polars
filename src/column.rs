784a3eaf1cbbc4d8116e5c279f204806450707d1
ritchie46 <ritchie46@gmail.com>
2020-05-13 21:43:00 +0200

    Choose for typed array.

--- src/column.rs
+++ src/column.rs
@@ -10,11 +10,12 @@ use arrow::{
 };
 use num::Zero;
 use std::fmt::{Debug, Formatter};
+use std::marker::PhantomData;
 use std::ops::{Add, Div, Mul, Sub};
+use std::rc::Rc;
 use std::sync::Arc;
 
-#[derive(Clone)]
-struct ChunkedArray {
+struct ChunkedArray<T> {
     field: Field,
     // For now settle with dynamic generics until we are more confident about the api
     chunks: Vec<ArrayRef>,
@@ -22,46 +23,54 @@ struct ChunkedArray {
     len: usize,
     /// sum of all chunk nulls
     null_counts: usize,
+    phantom: PhantomData<T>,
 }
 
-impl ChunkedArray {
-    fn new<T>(name: &str, v: &[T::Native]) -> Self
+impl<T> ChunkedArray<T>
+where
+    T: ArrowPrimitiveType,
+{
+    fn new<K>(name: &str, v: &[K::Native]) -> Self
     where
-        T: ArrowPrimitiveType,
+        K: ArrowPrimitiveType,
     {
-        let mut builder = PrimitiveBuilder::<T>::new(v.len());
+        let mut builder = PrimitiveBuilder::<K>::new(v.len());
         v.into_iter().for_each(|&val| {
             builder.append_value(val).expect("Could not append value");
         });
 
-        let field = Field::new(name, T::get_data_type(), true);
+        let field = Field::new(name, K::get_data_type(), true);
 
         ChunkedArray {
             field,
             chunks: vec![Arc::new(builder.finish())],
             len: v.len(),
             null_counts: 0,
+            phantom: PhantomData,
         }
     }
+}
 
+impl<T> ChunkedArray<T> {
     fn copy_with_array(&self, arr: Vec<ArrayRef>) -> Self {
         ChunkedArray {
             field: self.field.clone(),
             chunks: arr,
             len: self.len,
             null_counts: self.null_counts,
+            phantom: PhantomData,
         }
     }
-
-    /// Caller determines the data type, and only works on single chunked series.
-    fn get_iter<T: ArrowNumericType>(&self) -> impl Iterator<Item = &T::Native> + '_ {
-        let a0_any = self.chunks[0].as_any();
-        let arr = a0_any
-            .downcast_ref::<PrimitiveArray<T>>()
-            .expect("could not downcast");
-        let slice = arr.value_slice(0, arr.len());
-        slice.iter()
-    }
+    //
+    // /// Caller determines the data type, and only works on single chunked series.
+    // fn get_iter<T: ArrowNumericType>(&self) -> impl Iterator<Item = &T::Native> + '_ {
+    //     let a0_any = self.chunks[0].as_any();
+    //     let arr = a0_any
+    //         .downcast_ref::<PrimitiveArray<T>>()
+    //         .expect("could not downcast");
+    //     let slice = arr.value_slice(0, arr.len());
+    //     slice.iter()
+    // }
 }
 
 macro_rules! variant_operand {
@@ -88,59 +97,66 @@ macro_rules! variant_operand {
     }};
 }
 
-impl Add for &ChunkedArray {
-    type Output = ChunkedArray;
+impl<T> Add for &ChunkedArray<T>
+where
+    T: ArrowNumericType,
+    T::Native: Add<Output = T::Native>
+        + Sub<Output = T::Native>
+        + Mul<Output = T::Native>
+        + Div<Output = T::Native>
+        + num::Zero,
+{
+    type Output = ChunkedArray<T>;
 
     fn add(self, rhs: Self) -> Self::Output {
         let expect_str = "Could not add, check data types and length";
-        let result = match self.field.data_type() {
-            DataType::Int32 => variant_operand![self, rhs, datatypes::Int32Type, add, expect_str],
-            DataType::Float32 => {
-                variant_operand![self, rhs, datatypes::Float32Type, add, expect_str]
-            }
-            _ => return unimplemented!(),
-        };
-        result
+        variant_operand![self, rhs, T, add, expect_str]
     }
 }
 
-impl Mul for &ChunkedArray {
-    type Output = ChunkedArray;
+impl<T> Mul for &ChunkedArray<T>
+where
+    T: ArrowNumericType,
+    T::Native: Add<Output = T::Native>
+        + Sub<Output = T::Native>
+        + Mul<Output = T::Native>
+        + Div<Output = T::Native>
+        + num::Zero,
+{
+    type Output = ChunkedArray<T>;
 
     fn mul(self, rhs: Self) -> Self::Output {
         let expect_str = "Could not multiply, check data types and length";
-        let result = match self.field.data_type() {
-            DataType::Int32 => {
-                variant_operand![self, rhs, datatypes::Int32Type, multiply, expect_str]
-            }
-            DataType::Float32 => {
-                variant_operand![self, rhs, datatypes::Float32Type, multiply, expect_str]
-            }
-            _ => return unimplemented!(),
-        };
-        result
+        variant_operand!(self, rhs, T, multiply, expect_str)
     }
 }
 
-impl Sub for &ChunkedArray {
-    type Output = ChunkedArray;
+impl<T> Sub for &ChunkedArray<T>
+where
+    T: ArrowNumericType,
+    T::Native: Add<Output = T::Native>
+        + Sub<Output = T::Native>
+        + Mul<Output = T::Native>
+        + Div<Output = T::Native>
+        + num::Zero,
+{
+    type Output = ChunkedArray<T>;
 
     fn sub(self, rhs: Self) -> Self::Output {
         let expect_str = "Could not subtract, check data types and length";
-        let result = match self.field.data_type() {
-            DataType::Int32 => {
-                variant_operand![self, rhs, datatypes::Int32Type, subtract, expect_str]
-            }
-            DataType::Float32 => {
-                variant_operand![self, rhs, datatypes::Float32Type, subtract, expect_str]
-            }
-            _ => return unimplemented!(),
-        };
-        result
+        variant_operand![self, rhs, T, subtract, expect_str]
     }
 }
 
-impl Add for ChunkedArray {
+impl<T> Add for ChunkedArray<T>
+where
+    T: ArrowNumericType,
+    T::Native: Add<Output = T::Native>
+        + Sub<Output = T::Native>
+        + Mul<Output = T::Native>
+        + Div<Output = T::Native>
+        + num::Zero,
+{
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
@@ -148,7 +164,15 @@ impl Add for ChunkedArray {
     }
 }
 
-impl Mul for ChunkedArray {
+impl<T> Mul for ChunkedArray<T>
+where
+    T: ArrowNumericType,
+    T::Native: Add<Output = T::Native>
+        + Sub<Output = T::Native>
+        + Mul<Output = T::Native>
+        + Div<Output = T::Native>
+        + num::Zero,
+{
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
@@ -156,7 +180,15 @@ impl Mul for ChunkedArray {
     }
 }
 
-impl Sub for ChunkedArray {
+impl<T> Sub for ChunkedArray<T>
+where
+    T: ArrowNumericType,
+    T::Native: Add<Output = T::Native>
+        + Sub<Output = T::Native>
+        + Mul<Output = T::Native>
+        + Div<Output = T::Native>
+        + num::Zero,
+{
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
@@ -164,9 +196,21 @@ impl Sub for ChunkedArray {
     }
 }
 
-impl Debug for ChunkedArray {
+impl<T> Debug for ChunkedArray<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.write_str(&format!("{:?}", self.chunks))
+    }
+}
+
+impl<T> Clone for ChunkedArray<T> {
+    fn clone(&self) -> Self {
+        ChunkedArray {
+            field: self.field.clone(),
+            chunks: self.chunks.clone(),
+            len: self.len,
+            null_counts: self.null_counts,
+            phantom: PhantomData,
+        }
     }
 }
 
@@ -174,7 +218,7 @@ impl Debug for ChunkedArray {
 mod test {
     use super::*;
 
-    fn get_array() -> ChunkedArray {
+    fn get_array() -> ChunkedArray<datatypes::Int32Type> {
         ChunkedArray::new::<datatypes::Int32Type>("a", &[1, 2, 3])
     }
 
@@ -182,18 +226,19 @@ mod test {
     fn arithmetic() {
         let s1 = get_array();
         println!("{:?}", s1.chunks);
-        let s2 = s1.clone();
-        println!("{:?}", &s1 + &s2);
-        println!("{:?}", &s1 - &s2);
-        println!("{:?}", &s1 * &s2);
+        let s2 = &s1.clone();
+        let s1 = &s1;
+        println!("{:?}", s1 + s2);
+        println!("{:?}", s1 - s2);
+        println!("{:?}", s1 * s2);
     }
 
-    #[test]
-    fn iter() {
-        let s1 = get_array();
-        let mut a = s1.get_iter::<datatypes::Int32Type>();
-        let v = a.next().unwrap();
-
-        println!("{:?}", v)
-    }
+    // #[test]
+    // fn iter() {
+    //     let s1 = get_array();
+    //     let mut a = s1.get_iter::<datatypes::Int32Type>();
+    //     let v = a.next().unwrap();
+    //
+    //     println!("{:?}", v)
+    // }
 }
