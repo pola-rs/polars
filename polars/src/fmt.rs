295ff88336d4cf128a0d59561cdc104e6dd945ba
ritchie46 <ritchie46@gmail.com>
2020-06-28 21:21:30 +0200

    prettytable init

--- polars/src/fmt.rs
+++ polars/src/fmt.rs
@@ -1,6 +1,8 @@
 use crate::datatypes::{AnyType, ToStr};
 use crate::prelude::*;
 use num::{Num, NumCast};
+#[cfg(feature = "pretty")]
+use prettytable::Table;
 use std::{
     fmt,
     fmt::{Debug, Display, Formatter},
@@ -63,6 +65,35 @@ impl Debug for DataFrame {
     }
 }
 
+#[cfg(feature = "pretty")]
+impl Display for DataFrame {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut table = Table::new();
+        let names = self
+            .schema()
+            .fields()
+            .iter()
+            .map(|f| format!("{}\n---\n{}", f.name(), f.data_type().to_str()))
+            .collect();
+        table.set_titles(names);
+        for i in 0..10 {
+            let opt = self.get(i);
+            if let Some(row) = opt {
+                let mut row_str = Vec::with_capacity(row.len());
+                for v in &row {
+                    row_str.push(format!("{}", v));
+                }
+                table.add_row(row.iter().map(|v| format!("{}", v)).collect());
+            } else {
+                break;
+            }
+        }
+        write!(f, "{}", table)?;
+        Ok(())
+    }
+}
+
+#[cfg(not(feature = "pretty"))]
 impl Display for DataFrame {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         for field in self.schema().fields() {
@@ -110,6 +141,7 @@ fn fmt_float<T: Num + NumCast>(f: &mut Formatter<'_>, width: usize, v: T) -> fmt
     }
 }
 
+#[cfg(not(feature = "pretty"))]
 impl Display for AnyType<'_> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let width = 15;
@@ -126,6 +158,27 @@ impl Display for AnyType<'_> {
             AnyType::Date32(v) => write!(f, "{:>width$}", v, width = width),
             AnyType::Time64(v, _) => write!(f, "{:>width$}", v, width = width),
             AnyType::Duration(v, _) => write!(f, "{:>width$}", v, width = width),
+        }
+    }
+}
+
+#[cfg(feature = "pretty")]
+impl Display for AnyType<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let width = 0;
+        match self {
+            AnyType::Null => write!(f, "{}", "null"),
+            AnyType::U32(v) => write!(f, "{}", v),
+            AnyType::I32(v) => fmt_integer(f, width, *v),
+            AnyType::I64(v) => fmt_integer(f, width, *v),
+            AnyType::F32(v) => fmt_float(f, width, *v),
+            AnyType::F64(v) => fmt_float(f, width, *v),
+            AnyType::Bool(v) => write!(f, "{}", *v),
+            AnyType::Str(v) => write!(f, "{}", format!("\"{}\"", v)),
+            AnyType::Date64(v) => write!(f, "{}", v),
+            AnyType::Date32(v) => write!(f, "{}", v),
+            AnyType::Time64(v, _) => write!(f, "{}", v),
+            AnyType::Duration(v, _) => write!(f, "{}", v),
         }
     }
 }
