798d6b2af2b41a03c44f8423bf246d2ec73a8ad1
ritchie46 <ritchie46@gmail.com>
2020-07-01 15:55:22 +0200

    make sure that arithmetic succeeds on different chunked arrays

--- polars/src/series/chunked_array/arithmetic.rs
+++ polars/src/series/chunked_array/arithmetic.rs
@@ -3,6 +3,8 @@ use arrow::{array::ArrayRef, compute};
 use std::ops::{Add, Div, Mul, Sub};
 use std::sync::Arc;
 
+// TODO: Add Boolean arithmetic
+
 macro_rules! operand_on_primitive_arr {
     ($_self:expr, $rhs:tt, $operator:expr, $expect:expr) => {{
         let mut new_chunks = Vec::with_capacity($_self.chunks.len());
@@ -18,6 +20,38 @@ macro_rules! operand_on_primitive_arr {
     }};
 }
 
+macro_rules! apply_operand_iter {
+
+    ($self:ident, $rhs:ident, $operand:tt) => {
+            {
+
+                if let Ok(slice_lhs) = $self.cont_slice() {
+                    slice_lhs
+                        .iter()
+                        .zip($rhs.into_iter())
+                        .map(|(&left, opt_right)| opt_right.map(|right| left $operand right))
+                        .collect()
+                } else if let Ok(slice_rhs) = $rhs.cont_slice() {
+                    slice_rhs
+                        .iter()
+                        .zip($self.into_iter())
+                        .map(|(&right, opt_left)| opt_left.map(|left| left $operand right))
+                        .collect()
+                } else {
+                    $self.into_iter()
+                        .zip($rhs.into_iter())
+                        .map(|(opt_left, opt_right)| match (opt_left, opt_right) {
+                            (None, None) => None,
+                            (None, Some(_)) => None,
+                            (Some(_), None) => None,
+                            (Some(left), Some(right)) => Some(left $operand right),
+                        })
+                        .collect()
+                }
+            }
+    }
+}
+
 impl<T> Add for &ChunkedArray<T>
 where
     T: PolarNumericType,
@@ -30,8 +64,12 @@ where
     type Output = ChunkedArray<T>;
 
     fn add(self, rhs: Self) -> Self::Output {
-        let expect_str = "Could not add, check data types and length";
-        operand_on_primitive_arr![self, rhs, compute::add, expect_str]
+        if self.chunk_id == rhs.chunk_id {
+            let expect_str = "Could not add, check data types and length";
+            operand_on_primitive_arr![self, rhs, compute::add, expect_str]
+        } else {
+            apply_operand_iter!(self, rhs, +)
+        }
     }
 }
 
@@ -48,8 +86,12 @@ where
     type Output = ChunkedArray<T>;
 
     fn div(self, rhs: Self) -> Self::Output {
-        let expect_str = "Could not divide, check data types and length";
-        operand_on_primitive_arr!(self, rhs, compute::divide, expect_str)
+        if self.chunk_id == rhs.chunk_id {
+            let expect_str = "Could not divide, check data types and length";
+            operand_on_primitive_arr!(self, rhs, compute::divide, expect_str)
+        } else {
+            apply_operand_iter!(self, rhs, /)
+        }
     }
 }
 
@@ -65,8 +107,12 @@ where
     type Output = ChunkedArray<T>;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        let expect_str = "Could not multiply, check data types and length";
-        operand_on_primitive_arr!(self, rhs, compute::multiply, expect_str)
+        if self.chunk_id == rhs.chunk_id {
+            let expect_str = "Could not multiply, check data types and length";
+            operand_on_primitive_arr!(self, rhs, compute::multiply, expect_str)
+        } else {
+            apply_operand_iter!(self, rhs, *)
+        }
     }
 }
 
@@ -82,8 +128,12 @@ where
     type Output = ChunkedArray<T>;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        let expect_str = "Could not subtract, check data types and length";
-        operand_on_primitive_arr![self, rhs, compute::subtract, expect_str]
+        if self.chunk_id == rhs.chunk_id {
+            let expect_str = "Could not subtract, check data types and length";
+            operand_on_primitive_arr![self, rhs, compute::subtract, expect_str]
+        } else {
+            apply_operand_iter!(self, rhs, -)
+        }
     }
 }
 
@@ -99,7 +149,11 @@ where
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        (&self).add(&rhs)
+        if self.chunk_id == rhs.chunk_id {
+            (&self).add(&rhs)
+        } else {
+            apply_operand_iter!(self, rhs, +)
+        }
     }
 }
 
@@ -149,5 +203,34 @@ where
 
     fn sub(self, rhs: Self) -> Self::Output {
         (&self).sub(&rhs)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::prelude::*;
+
+    fn create_two_chunked() -> (Int32Chunked, Int32Chunked) {
+        let mut a1 = Int32Chunked::new_from_slice("a", &[1, 2, 3]);
+        let a2 = Int32Chunked::new_from_slice("a", &[4, 5, 6]);
+        let a3 = Int32Chunked::new_from_slice("a", &[1, 2, 3, 4, 5, 6]);
+        a1.append(&a2);
+        (a1, a3)
+    }
+
+    #[test]
+    fn test_chunk_mismatch() {
+        let (a1, a2) = create_two_chunked();
+        // with different chunks
+        &a1 + &a2;
+        &a1 - &a2;
+        &a1 / &a2;
+        &a1 * &a2;
+
+        // with same chunks
+        &a1 + &a1;
+        &a1 - &a1;
+        &a1 / &a1;
+        &a1 * &a1;
     }
 }
