0a97ddf3757afa32c2541f865e53b6ecf97e6480
ritchie46 <ritchie46@gmail.com>
2020-07-29 11:22:32 +0200

    use arrow 1.0.0

--- polars/src/frame/ser/csv.rs
+++ polars/src/frame/ser/csv.rs
@@ -220,12 +220,11 @@ mod test {
         let mut buf: Vec<u8> = Vec::new();
         let df = create_df();
 
-        // TODO: headers not working: https://github.com/apache/arrow/pull/7554
         CsvWriter::new(&mut buf)
             .has_headers(true)
             .finish(&df)
             .expect("csv written");
         let csv = std::str::from_utf8(&buf).unwrap();
-        assert_eq!("0,22.1\n1,19.9\n2,7\n3,2\n4,3\n", csv);
+        assert_eq!("days,temp\n0,22.1\n1,19.9\n2,7\n3,2\n4,3\n", csv);
     }
 }
