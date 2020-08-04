11ee399a23332b010b83eeda9e49abcc46e26281
ritchie46 <ritchie46@gmail.com>
2020-08-04 15:26:21 +0200

    DoubleEndedIterator for NumIterManyChunk

--- polars/src/chunked_array/iterator.rs
+++ polars/src/chunked_array/iterator.rs
@@ -217,26 +217,70 @@ where
 {
     ca: &'a ChunkedArray<T>,
     chunks: Vec<&'a PrimitiveArray<T>>,
-    current_iter: Copied<Iter<'a, T::Native>>,
-    array_i: usize,
-    chunk_i: usize,
-    current_len: usize,
+    // current iterator if we iterate from the left
+    current_iter_left: Copied<Iter<'a, T::Native>>,
+    // If iter_left and iter_right are the same, this is None and we need to use iter left
+    // This is done because we can only have one owner
+    current_iter_right: Option<Copied<Iter<'a, T::Native>>>,
+    chunk_idx_left: usize,
+    idx_left: usize,
+    idx_right: usize,
+    chunk_idx_right: usize,
 }
 
 impl<'a, T> NumIterManyChunk<'a, T>
 where
     T: PolarsNumericType,
 {
+    fn set_current_iter_left(&mut self) {
+        let current_chunk = unsafe { self.chunks.get_unchecked(self.chunk_idx_left) };
+        self.current_iter_left = current_chunk
+            .value_slice(0, current_chunk.len())
+            .iter()
+            .copied();
+    }
+
+    fn set_current_iter_right(&mut self) {
+        if self.chunk_idx_left == self.chunk_idx_left {
+            // from left and right we use the same iterator
+            self.current_iter_right = None
+        } else {
+            let current_chunk = unsafe { self.chunks.get_unchecked(self.chunk_idx_right) };
+            self.current_iter_right = Some(
+                current_chunk
+                    .value_slice(0, current_chunk.len())
+                    .iter()
+                    .copied(),
+            );
+        }
+    }
     fn new(ca: &'a ChunkedArray<T>) -> Self {
         let chunks = ca.downcast_chunks();
-        let current_len = chunks[0].len();
+        let current_len_left = chunks[0].len();
+        let current_iter_left = chunks[0].value_slice(0, current_len_left).iter().copied();
+
+        let idx_left = 0;
+        let chunk_idx_left = 0;
+        let idx_right = ca.len();
+
+        let chunk_idx_right = chunks.len() - 1;
+        let current_iter_right;
+        if chunk_idx_left == chunk_idx_right {
+            current_iter_right = None
+        } else {
+            let arr = chunks[chunk_idx_right];
+            current_iter_right = Some(arr.value_slice(0, arr.len()).iter().copied())
+        }
+
         NumIterManyChunk {
             ca,
-            current_len,
-            current_iter: chunks[0].value_slice(0, current_len).iter().copied(),
+            current_iter_left,
             chunks,
-            array_i: 0,
-            chunk_i: 0,
+            current_iter_right,
+            idx_left,
+            chunk_idx_left,
+            idx_right,
+            chunk_idx_right,
         }
     }
 }
@@ -248,26 +292,25 @@ where
     type Item = Option<T::Native>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.array_i == self.current_len {
-            // go to the next array in the chunks
-            self.chunk_i += 1;
-            self.array_i = 0;
+        let opt_val = self.current_iter_left.next();
 
-            if self.chunk_i < self.chunks.len() {
-                let current_chunk = unsafe { self.chunks.get_unchecked(self.chunk_i) };
-                // not passed last chunk
-                self.current_len = current_chunk.len();
-                self.current_iter = current_chunk
-                    .value_slice(0, self.current_len)
-                    .iter()
-                    .copied();
-            } else {
-                // end of iterator
+        let opt_val = if opt_val.is_none() {
+            // iterators have met in the middle or at the end
+            if self.idx_left == self.idx_right {
                 return None;
+            // one chunk is finished but there are still more chunks
+            } else {
+                self.chunk_idx_left += 1;
+                self.set_current_iter_left();
             }
-        }
-        self.array_i += 1;
-        self.current_iter.next().map(Some)
+            // so we return the first value of the next chunk
+            self.current_iter_left.next()
+        } else {
+            // we got a value
+            opt_val
+        };
+        self.idx_left += 1;
+        opt_val.map(Some)
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -281,7 +324,28 @@ where
     T: PolarsNumericType,
 {
     fn next_back(&mut self) -> Option<Self::Item> {
-        unimplemented!()
+        let opt_val = match &mut self.current_iter_right {
+            Some(it) => it.next_back(),
+            None => self.current_iter_left.next_back(),
+        };
+
+        let opt_val = if opt_val.is_none() {
+            // iterators have met in the middle or at the beginning
+            if self.idx_left == self.idx_right {
+                return None;
+            // one chunk is finished but there are still more chunks
+            } else {
+                self.chunk_idx_right -= 1;
+                self.set_current_iter_right();
+            }
+            // so we return the first value of the next chunk from the back
+            self.current_iter_left.next_back()
+        } else {
+            // we got a value
+            opt_val
+        };
+        self.idx_right -= 1;
+        opt_val.map(Some)
     }
 }
 
@@ -780,6 +844,42 @@ mod test {
     #[test]
     fn test_iter_numitersinglechunk() {
         let a = UInt32Chunked::new_from_slice("a", &[1, 2, 3]);
+
+        // normal iterator
+        let mut it = a.into_iter();
+        assert_eq!(it.next(), Some(Some(1)));
+        assert_eq!(it.next(), Some(Some(2)));
+        assert_eq!(it.next(), Some(Some(3)));
+        assert_eq!(it.next(), None);
+
+        // reverse iterator
+        let mut it = a.into_iter();
+        assert_eq!(it.next_back(), Some(Some(3)));
+        assert_eq!(it.next_back(), Some(Some(2)));
+        assert_eq!(it.next_back(), Some(Some(1)));
+        assert_eq!(it.next_back(), None);
+
+        // iterators should not cross
+        let mut it = a.into_iter();
+        assert_eq!(it.next_back(), Some(Some(3)));
+        assert_eq!(it.next(), Some(Some(1)));
+        assert_eq!(it.next(), Some(Some(2)));
+        // should stop here as we took this one from the back
+        assert_eq!(it.next(), None);
+
+        // do the same from the right side
+        let mut it = a.into_iter();
+        assert_eq!(it.next(), Some(Some(1)));
+        assert_eq!(it.next_back(), Some(Some(3)));
+        assert_eq!(it.next_back(), Some(Some(2)));
+        assert_eq!(it.next_back(), None);
+    }
+
+    #[test]
+    fn test_iter_numitermanychunk() {
+        let mut a = UInt32Chunked::new_from_slice("a", &[1, 2]);
+        let a_b = UInt32Chunked::new_from_slice("", &[3]);
+        a.append(&a_b);
 
         // normal iterator
         let mut it = a.into_iter();
