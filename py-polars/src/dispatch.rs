e330bd12c53988319902f35ddcdc1e45c4b720c6
ritchie46 <ritchie46@gmail.com>
2020-09-24 16:31:53 +0200

    [Python] apply lambda (only same types); 85

--- py-polars/src/dispatch.rs
+++ py-polars/src/dispatch.rs
@@ -3,47 +3,50 @@ use polars::prelude::*;
 use pyo3::prelude::*;
 use pyo3::types::PyTuple;
 
-pub trait PyPolarsNumericType: PolarsNumericType {}
+pub trait PyArrowPrimitiveType: ArrowPrimitiveType {}
 
-impl PyPolarsNumericType for UInt8Type {}
-impl PyPolarsNumericType for UInt16Type {}
-impl PyPolarsNumericType for UInt32Type {}
-impl PyPolarsNumericType for UInt64Type {}
-impl PyPolarsNumericType for Int8Type {}
-impl PyPolarsNumericType for Int16Type {}
-impl PyPolarsNumericType for Int32Type {}
-impl PyPolarsNumericType for Int64Type {}
-impl PyPolarsNumericType for Float32Type {}
-impl PyPolarsNumericType for Float64Type {}
-impl PyPolarsNumericType for Date32Type {}
-impl PyPolarsNumericType for Date64Type {}
-impl PyPolarsNumericType for Time64NanosecondType {}
-impl PyPolarsNumericType for Time64MicrosecondType {}
-impl PyPolarsNumericType for Time32MillisecondType {}
-impl PyPolarsNumericType for Time32SecondType {}
-impl PyPolarsNumericType for DurationNanosecondType {}
-impl PyPolarsNumericType for DurationMicrosecondType {}
-impl PyPolarsNumericType for DurationMillisecondType {}
-impl PyPolarsNumericType for DurationSecondType {}
-impl PyPolarsNumericType for IntervalYearMonthType {}
-impl PyPolarsNumericType for IntervalDayTimeType {}
-impl PyPolarsNumericType for TimestampNanosecondType {}
-impl PyPolarsNumericType for TimestampMicrosecondType {}
-impl PyPolarsNumericType for TimestampMillisecondType {}
-impl PyPolarsNumericType for TimestampSecondType {}
+impl PyArrowPrimitiveType for UInt8Type {}
+impl PyArrowPrimitiveType for UInt16Type {}
+impl PyArrowPrimitiveType for UInt32Type {}
+impl PyArrowPrimitiveType for UInt64Type {}
+impl PyArrowPrimitiveType for Int8Type {}
+impl PyArrowPrimitiveType for Int16Type {}
+impl PyArrowPrimitiveType for Int32Type {}
+impl PyArrowPrimitiveType for Int64Type {}
+impl PyArrowPrimitiveType for Float32Type {}
+impl PyArrowPrimitiveType for Float64Type {}
+impl PyArrowPrimitiveType for Date32Type {}
+impl PyArrowPrimitiveType for Date64Type {}
+impl PyArrowPrimitiveType for Time64NanosecondType {}
+impl PyArrowPrimitiveType for Time64MicrosecondType {}
+impl PyArrowPrimitiveType for Time32MillisecondType {}
+impl PyArrowPrimitiveType for Time32SecondType {}
+impl PyArrowPrimitiveType for DurationNanosecondType {}
+impl PyArrowPrimitiveType for DurationMicrosecondType {}
+impl PyArrowPrimitiveType for DurationMillisecondType {}
+impl PyArrowPrimitiveType for DurationSecondType {}
+impl PyArrowPrimitiveType for IntervalYearMonthType {}
+impl PyArrowPrimitiveType for IntervalDayTimeType {}
+impl PyArrowPrimitiveType for TimestampNanosecondType {}
+impl PyArrowPrimitiveType for TimestampMicrosecondType {}
+impl PyArrowPrimitiveType for TimestampMillisecondType {}
+impl PyArrowPrimitiveType for TimestampSecondType {}
+impl PyArrowPrimitiveType for BooleanType {}
 
-pub trait ApplyLambda<'a> {
-    fn apply_lambda(&self, _py: Python, _lambda: &'a PyAny) -> PyResult<PySeries> {
+pub trait ApplyLambda<'a, 'b> {
+    fn apply_lambda(&'b self, _py: Python, _lambda: &'a PyAny) -> PyResult<PySeries> {
         unimplemented!()
     }
 }
 
-impl<'a, T> ApplyLambda<'a> for ChunkedArray<T>
+impl<'a, 'b, T> ApplyLambda<'a, 'b> for ChunkedArray<T>
 where
-    T: PyPolarsNumericType,
+    T: PyArrowPrimitiveType,
     T::Native: ToPyObject + FromPyObject<'a>,
+    &'b ChunkedArray<T>:
+        IntoIterator<Item = Option<T::Native>> + IntoNoNullIterator<Item = T::Native>,
 {
-    fn apply_lambda(&self, py: Python, lambda: &'a PyAny) -> PyResult<PySeries> {
+    fn apply_lambda(&'b self, py: Python, lambda: &'a PyAny) -> PyResult<PySeries> {
         let ca = if self.null_count() == 0 {
             let mut it = self.into_no_null_iter();
             let mut builder = PrimitiveChunkedBuilder::<T>::new(self.name(), self.len());
@@ -72,6 +75,36 @@ where
     }
 }
 
-impl<'a> ApplyLambda<'a> for LargeListChunked {}
-impl<'a> ApplyLambda<'a> for BooleanChunked {}
-impl<'a> ApplyLambda<'a> for Utf8Chunked {}
+impl<'a, 'b> ApplyLambda<'a, 'b> for Utf8Chunked {
+    fn apply_lambda(&'b self, py: Python, lambda: &'a PyAny) -> PyResult<PySeries> {
+        let ca = if self.null_count() == 0 {
+            let mut it = self.into_no_null_iter();
+            let mut builder = Utf8ChunkedBuilder::new(self.name(), self.len());
+
+            while let Some(v) = it.next() {
+                let arg = PyTuple::new(py, &[v]);
+                let out = lambda.call1(arg)?;
+                let s: &str = out.extract()?;
+                builder.append_value(s)
+            }
+            builder.finish()
+        } else {
+            let mut it = self.into_iter();
+            let mut builder = Utf8ChunkedBuilder::new(self.name(), self.len());
+            while let Some(opt_v) = it.next() {
+                if let Some(v) = opt_v {
+                    let arg = PyTuple::new(py, &[v]);
+                    let out = lambda.call1(arg)?;
+                    let s: &str = out.extract()?;
+                    builder.append_value(s)
+                } else {
+                    builder.append_null()
+                }
+            }
+            builder.finish()
+        };
+        Ok(PySeries::new(ca.into_series()))
+    }
+}
+
+impl<'a, 'b> ApplyLambda<'a, 'b> for LargeListChunked {}
