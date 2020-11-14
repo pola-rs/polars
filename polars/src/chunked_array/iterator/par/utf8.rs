57b48aaacdd02690d856c1b56d40dfa28e63df3e
Adrián Gallego Castellanos <kugoad@openmailbox.org>
2020-11-14 12:30:47 +0100

    Implement `Utf8ParChunkIterDispatch` to provide static polymorphism to parallel iterators. Implement `IntoParallelIterator` for `&'a Utf8Chunked` which will choose the implementation of `ParallelIterator` that is more efficient for the current `&'a Utf8Chunked`: - If the chunked array has only one chunk and no null element then use `Utf8ParIterSingleChunk`. - If the chunked array has only one chunk and does have null element then use `Utf8ParIterSingleChunkNullCheck`. - If the chunked array has many chunks and no null element then use `Utf8ParIterManyChunk`. - If the chunked array has many chunks and does have null element then use `Utf8ParIterManyChunkNullCheck`.

--- polars/src/chunked_array/iterator/par/utf8.rs
+++ polars/src/chunked_array/iterator/par/utf8.rs
@@ -110,6 +110,12 @@ pub struct Utf8ParIterSingleChunk<'a> {
     ca: &'a Utf8Chunked,
 }
 
+impl<'a> Utf8ParIterSingleChunk<'a> {
+    fn new(ca: &'a Utf8Chunked) -> Self {
+        Utf8ParIterSingleChunk { ca }
+    }
+}
+
 impl<'a> From<Utf8ProducerSingleChunk<'a>> for Utf8IterSingleChunk<'a> {
     fn from(prod: Utf8ProducerSingleChunk<'a>) -> Self {
         let chunks = prod.ca.downcast_chunks();
@@ -140,6 +146,12 @@ impl_utf8_parallel_iterator!(
 #[derive(Debug, Clone)]
 pub struct Utf8ParIterSingleChunkNullCheck<'a> {
     ca: &'a Utf8Chunked,
+}
+
+impl<'a> Utf8ParIterSingleChunkNullCheck<'a> {
+    fn new(ca: &'a Utf8Chunked) -> Self {
+        Utf8ParIterSingleChunkNullCheck { ca }
+    }
 }
 
 impl<'a> From<Utf8ProducerSingleChunkNullCheck<'a>> for Utf8IterSingleChunkNullCheck<'a> {
@@ -174,6 +186,12 @@ impl_utf8_parallel_iterator!(
 #[derive(Debug, Clone)]
 pub struct Utf8ParIterManyChunk<'a> {
     ca: &'a Utf8Chunked,
+}
+
+impl<'a> Utf8ParIterManyChunk<'a> {
+    fn new(ca: &'a Utf8Chunked) -> Self {
+        Utf8ParIterManyChunk { ca }
+    }
 }
 
 impl<'a> From<Utf8ProducerManyChunk<'a>> for Utf8IterManyChunk<'a> {
@@ -221,6 +239,12 @@ pub struct Utf8ParIterManyChunkNullCheck<'a> {
     ca: &'a Utf8Chunked,
 }
 
+impl<'a> Utf8ParIterManyChunkNullCheck<'a> {
+    fn new(ca: &'a Utf8Chunked) -> Self {
+        Utf8ParIterManyChunkNullCheck { ca }
+    }
+}
+
 impl<'a> From<Utf8ProducerManyChunkNullCheck<'a>> for Utf8IterManyChunkNullCheck<'a> {
     fn from(prod: Utf8ProducerManyChunkNullCheck<'a>) -> Self {
         let ca = prod.ca;
@@ -260,20 +284,118 @@ impl_utf8_parallel_iterator!(
     Option<&'a str>
 );
 
+/// Static dispatching structure to allow static polymorphism of chunked
+/// parallel iterators.
+///
+/// All the iterators of the dispatcher returns `Option<&'a str>`.
+pub enum Utf8ParChunkIterDispatch<'a> {
+    SingleChunk(Utf8ParIterSingleChunk<'a>),
+    SingleChunkNullCheck(Utf8ParIterSingleChunkNullCheck<'a>),
+    ManyChunk(Utf8ParIterManyChunk<'a>),
+    ManyChunkNullCheck(Utf8ParIterManyChunkNullCheck<'a>),
+}
+
+/// Convert `&'a Utf8Chunked` into a `ParallelIterator` using the most
+/// efficient `ParallelIterator` for the given `&'a Utf8Chunked`.
+///
+/// - If `&'a Utf8Chunked` has only a chunk and has no null values, it uses `Utf8ParIterSingleChunk`.
+/// - If `&'a Utf8Chunked` has only a chunk and does have null values, it uses `Utf8ParIterSingleChunkNullCheck`.
+/// - If `&'a Utf8Chunked` has many chunks and has no null values, it uses `Utf8ParIterManyChunk`.
+/// - If `&'a Utf8Chunked` has many chunks and does have null values, it uses `Utf8ParIterManyChunkNullCheck`.
 impl<'a> IntoParallelIterator for &'a Utf8Chunked {
-    type Iter = Utf8ParIterSingleChunk<'a>;
+    type Iter = Utf8ParChunkIterDispatch<'a>;
     type Item = Option<&'a str>;
 
     fn into_par_iter(self) -> Self::Iter {
-        Utf8ParIterSingleChunk { ca: self }
+        let chunks = self.downcast_chunks();
+        match chunks.len() {
+            1 => {
+                if self.null_count() == 0 {
+                    Utf8ParChunkIterDispatch::SingleChunk(Utf8ParIterSingleChunk::new(self))
+                } else {
+                    Utf8ParChunkIterDispatch::SingleChunkNullCheck(
+                        Utf8ParIterSingleChunkNullCheck::new(self),
+                    )
+                }
+            }
+            _ => {
+                if self.null_count() == 0 {
+                    Utf8ParChunkIterDispatch::ManyChunk(Utf8ParIterManyChunk::new(self))
+                } else {
+                    Utf8ParChunkIterDispatch::ManyChunkNullCheck(
+                        Utf8ParIterManyChunkNullCheck::new(self),
+                    )
+                }
+            }
+        }
     }
 }
 
-/// Parallel Iterator for chunked arrays with more than one chunk.
-/// It DOES perform null check, then, it is appropriated
-/// for chunks whose contents can be null.
+impl<'a> ParallelIterator for Utf8ParChunkIterDispatch<'a> {
+    type Item = Option<&'a str>;
+
+    fn drive_unindexed<C>(self, consumer: C) -> C::Result
+    where
+        C: UnindexedConsumer<Self::Item>,
+    {
+        match self {
+            Utf8ParChunkIterDispatch::SingleChunk(a) => a.drive_unindexed(consumer),
+            Utf8ParChunkIterDispatch::SingleChunkNullCheck(a) => a.drive_unindexed(consumer),
+            Utf8ParChunkIterDispatch::ManyChunk(a) => a.drive_unindexed(consumer),
+            Utf8ParChunkIterDispatch::ManyChunkNullCheck(a) => a.drive_unindexed(consumer),
+        }
+    }
+
+    fn opt_len(&self) -> Option<usize> {
+        match self {
+            Utf8ParChunkIterDispatch::SingleChunk(a) => a.opt_len(),
+            Utf8ParChunkIterDispatch::SingleChunkNullCheck(a) => a.opt_len(),
+            Utf8ParChunkIterDispatch::ManyChunk(a) => a.opt_len(),
+            Utf8ParChunkIterDispatch::ManyChunkNullCheck(a) => a.opt_len(),
+        }
+    }
+}
+
+impl<'a> IndexedParallelIterator for Utf8ParChunkIterDispatch<'a> {
+    fn len(&self) -> usize {
+        match self {
+            Utf8ParChunkIterDispatch::SingleChunk(a) => a.len(),
+            Utf8ParChunkIterDispatch::SingleChunkNullCheck(a) => a.len(),
+            Utf8ParChunkIterDispatch::ManyChunk(a) => a.len(),
+            Utf8ParChunkIterDispatch::ManyChunkNullCheck(a) => a.len(),
+        }
+    }
+
+    fn drive<C>(self, consumer: C) -> C::Result
+    where
+        C: Consumer<Self::Item>,
+    {
+        match self {
+            Utf8ParChunkIterDispatch::SingleChunk(a) => a.drive(consumer),
+            Utf8ParChunkIterDispatch::SingleChunkNullCheck(a) => a.drive(consumer),
+            Utf8ParChunkIterDispatch::ManyChunk(a) => a.drive(consumer),
+            Utf8ParChunkIterDispatch::ManyChunkNullCheck(a) => a.drive(consumer),
+        }
+    }
+
+    fn with_producer<CB>(self, callback: CB) -> CB::Output
+    where
+        CB: ProducerCallback<Self::Item>,
+    {
+        match self {
+            Utf8ParChunkIterDispatch::SingleChunk(a) => a.with_producer(callback),
+            Utf8ParChunkIterDispatch::SingleChunkNullCheck(a) => a.with_producer(callback),
+            Utf8ParChunkIterDispatch::ManyChunk(a) => a.with_producer(callback),
+            Utf8ParChunkIterDispatch::ManyChunkNullCheck(a) => a.with_producer(callback),
+        }
+    }
+}
+
+/// Parallel Iterator for chunked arrays with just one chunk.
+/// The chunks cannot have null values so it does NOT perform null
+/// checks.
 ///
-/// It returns the result wrapped in an `Option`.
+/// The return tipe is `&'a str`.
 #[derive(Debug, Clone)]
 pub struct Utf8ParIterCont<'a> {
     ca: &'a Utf8Chunked,
