31b699785a8749ad4c77b963c2010668e786cdd5
ritchie46 <ritchie46@gmail.com>
2021-01-04 18:40:59 +0100

    covariance and correlation functions

---
+++ polars/src/lazy/functions.rs
@@ -0,0 +1,67 @@
+use crate::{lazy::prelude::*, prelude::*};
+
+pub fn cov(a: Expr, b: Expr) -> Expr {
+    let name = "cov";
+    let function = move |a: Series, b: Series| {
+        let s = match a.dtype() {
+            ArrowDataType::Float32 => {
+                let ca_a = a.f32().unwrap();
+                let ca_b = b.f32().unwrap();
+                Series::new(name, &[crate::functions::cov(ca_a, ca_b)])
+            }
+            ArrowDataType::Float64 => {
+                let ca_a = a.f64().unwrap();
+                let ca_b = b.f64().unwrap();
+                Series::new(name, &[crate::functions::cov(ca_a, ca_b)])
+            }
+            _ => {
+                let a = a.cast::<Float64Type>()?;
+                let b = b.cast::<Float64Type>()?;
+                let ca_a = a.f64().unwrap();
+                let ca_b = b.f64().unwrap();
+                Series::new(name, &[crate::functions::cov(ca_a, ca_b)])
+            }
+        };
+        Ok(s)
+    };
+    binary_function(
+        a,
+        b,
+        function,
+        Some(Field::new(name, ArrowDataType::Float32, true)),
+    )
+    .alias(name)
+}
+
+pub fn pearson_corr(a: Expr, b: Expr) -> Expr {
+    let name = "pearson_corr";
+    let function = move |a: Series, b: Series| {
+        let s = match a.dtype() {
+            ArrowDataType::Float32 => {
+                let ca_a = a.f32().unwrap();
+                let ca_b = b.f32().unwrap();
+                Series::new(name, &[crate::functions::pearson_corr(ca_a, ca_b)])
+            }
+            ArrowDataType::Float64 => {
+                let ca_a = a.f64().unwrap();
+                let ca_b = b.f64().unwrap();
+                Series::new(name, &[crate::functions::pearson_corr(ca_a, ca_b)])
+            }
+            _ => {
+                let a = a.cast::<Float64Type>()?;
+                let b = b.cast::<Float64Type>()?;
+                let ca_a = a.f64().unwrap();
+                let ca_b = b.f64().unwrap();
+                Series::new(name, &[crate::functions::pearson_corr(ca_a, ca_b)])
+            }
+        };
+        Ok(s)
+    };
+    binary_function(
+        a,
+        b,
+        function,
+        Some(Field::new(name, ArrowDataType::Float32, true)),
+    )
+    .alias(name)
+}
