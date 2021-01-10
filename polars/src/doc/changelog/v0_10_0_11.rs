cb40ee1f03b99eb1a3e6bf749e72aebaed4a968c
ritchie46 <ritchie46@gmail.com>
2021-01-10 13:56:35 +0100

    update changelog

--- polars/src/doc/changelog/v0_10_0_11.rs
+++ polars/src/doc/changelog/v0_10_0_11.rs
@@ -6,6 +6,10 @@
 //! * Performance increase in take kernel
 //! * Performance increase in ChunkedArray builders
 //! * Join operation on multiple columns.
+//! * ~3.5 x performance increase in groupby operations (measured on db-benchmark),
+//!   due to embarrassingly parallel grouping and better branch prediction (tight loops).
+//! * Performance increase on join operation due to better branch prediction.
+//! * Categorical datatype and global string cache (BETA).
 //!
 //! * Lazy
 //!     - Lot's of bug fixes in optimizer.
