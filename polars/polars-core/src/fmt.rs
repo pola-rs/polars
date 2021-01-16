87d18e814493ca949e833eb79d901a1078917c94
Lukasz Wiklendt <lwiklendt@gmail.com>
2021-01-17 09:15:48 +1030

    fix impl Display for DataFrame

--- polars/polars-core/src/fmt.rs
+++ polars/polars-core/src/fmt.rs
@@ -316,14 +316,13 @@ impl Debug for DataFrame {
 
 fn prepare_row(
     row: Vec<AnyValue>,
-    insert_idx: usize,
-    reduce_columns: bool,
-    max_n_cols: usize,
+    n_first: usize,
+    n_last: usize,
 ) -> Vec<String> {
-    let string_limit = 32;
-    let mut row_str = Vec::with_capacity(row.len());
-    for v in row.iter().take(max_n_cols) {
-        let str_val = if let AnyValue::Utf8(s) = v {
+
+    fn make_str_val(v: &AnyValue) -> String {
+        let string_limit = 32;
+        if let AnyValue::Utf8(s) = v {
             if s.len() > string_limit {
                 format!("{}...", &s[..string_limit])
             } else {
@@ -331,11 +330,19 @@ fn prepare_row(
             }
         } else {
             format!("{}", v)
-        };
-        row_str.push(str_val);
+        }
+    }
+
+    let reduce_columns = n_first + n_last < row.len();
+    let mut row_str = Vec::with_capacity(n_first + n_last + reduce_columns as usize);
+    for v in row[0..n_first].iter() {
+        row_str.push(make_str_val(v));
     }
     if reduce_columns {
-        row_str.insert(insert_idx, "...".to_string());
+        row_str.push("...".to_string());
+    }
+    for v in row[row.len() - n_last..].iter() {
+        row_str.push(make_str_val(v));
     }
     row_str
 }
@@ -350,18 +357,27 @@ impl Display for DataFrame {
             .unwrap_or_else(|_| "8".to_string())
             .parse()
             .unwrap_or(8);
-        let insert_idx = max_n_cols / 2;
-        let reduce_columns = self.width() > max_n_cols;
 
-        let mut names: Vec<_> = self
-            .schema()
-            .fields()
-            .iter()
-            .take(max_n_cols)
-            .map(|f| format!("{}\n---\n{}", f.name(), f.data_type()))
-            .collect();
+        let (n_first, n_last) = if self.width() > max_n_cols {
+            ((max_n_cols + 1) / 2, max_n_cols / 2)
+        } else {
+            (self.width(), 0)
+        };
+        let reduce_columns = n_first + n_last < self.width();
+
+        let field_to_str = |f: &Field| format!("{}\n---\n{}", f.name(), f.data_type());
+
+        let mut names = Vec::with_capacity(n_first + n_last + reduce_columns as usize);
+        let schema = self.schema();
+        let fields = schema.fields();
+        for field in fields[0..n_first].iter() {
+            names.push(field_to_str(field))
+        }
         if reduce_columns {
-            names.insert(insert_idx, "...".to_string())
+            names.push("...".to_string())
+        }
+        for field in fields[self.width() - n_last..].iter() {
+            names.push(field_to_str(field))
         }
         let mut table = Table::new();
         table
@@ -381,13 +397,13 @@ impl Display for DataFrame {
         if self.height() > max_n_rows {
             for i in 0..(max_n_rows / 2) {
                 let row = self.get(i).unwrap();
-                rows.push(prepare_row(row, insert_idx, reduce_columns, max_n_cols));
+                rows.push(prepare_row(row, n_first, n_last));
             }
             let dots = rows[0].iter().map(|_| "...".to_string()).collect();
             rows.push(dots);
             for i in (self.height() - max_n_cols / 2 - 1)..self.height() {
                 let row = self.get(i).unwrap();
-                rows.push(prepare_row(row, insert_idx, reduce_columns, max_n_cols));
+                rows.push(prepare_row(row, n_first, n_last));
             }
             for row in rows {
                 table.add_row(row);
@@ -396,7 +412,7 @@ impl Display for DataFrame {
             for i in 0..max_n_rows {
                 let opt = self.get(i);
                 if let Some(row) = opt {
-                    table.add_row(prepare_row(row, insert_idx, reduce_columns, max_n_cols));
+                    table.add_row(prepare_row(row, n_first, n_last));
                 } else {
                     break;
                 }
