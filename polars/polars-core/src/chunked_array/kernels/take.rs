2f36f435170636ee8ab079d2c2f7e4d198f1537a
Ritchie Vink <ritchie46@gmail.com>
2021-01-29 21:02:51 +0100

    remove boolean take kernel, use arrow's

--- polars/polars-core/src/chunked_array/kernels/take.rs
+++ polars/polars-core/src/chunked_array/kernels/take.rs
@@ -1,41 +1,11 @@
 use crate::chunked_array::builder::aligned_vec_to_primitive_array;
 use crate::prelude::*;
 use arrow::array::{
-    Array, ArrayData, BooleanArray, LargeStringArray, LargeStringBuilder, PrimitiveArray,
-    UInt32Array,
+    Array, ArrayData, LargeStringArray, LargeStringBuilder, PrimitiveArray, UInt32Array,
 };
 use arrow::buffer::MutableBuffer;
-use arrow::util::bit_util;
 use std::mem;
 use std::sync::Arc;
-
-/// Forked snippet from Arrow. Remove on Arrow 3.0 release
-pub(crate) fn take_no_null_boolean(arr: &BooleanArray, indices: &UInt32Array) -> Arc<BooleanArray> {
-    assert_eq!(arr.null_count(), 0);
-    let data_len = indices.len();
-    let num_bytes = bit_util::ceil(data_len, 8);
-
-    // fill with false bits
-    let mut val_buf = MutableBuffer::new(num_bytes).with_bitset(num_bytes, false);
-    let val_slice = val_buf.as_slice_mut();
-    (0..data_len).for_each(|i| {
-        let index = indices.value(i) as usize;
-        if arr.value(index) {
-            bit_util::set_bit(val_slice, i)
-        }
-    });
-    let nulls = indices.data_ref().null_buffer().cloned();
-    let data = ArrayData::new(
-        ArrowDataType::Boolean,
-        indices.len(),
-        None,
-        nulls,
-        0,
-        vec![val_buf.into()],
-        vec![],
-    );
-    Arc::new(BooleanArray::from(Arc::new(data)))
-}
 
 /// Forked snippet from Arrow. Only does not write to a Vec, but directly to aligned memory.
 pub(crate) fn take_no_null_primitive<T: PolarsNumericType>(
