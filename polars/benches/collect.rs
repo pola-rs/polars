785a2159c4fee89baa7f6796ead52a3cc4403652
Ritchie Vink <ritchie46@gmail.com>
2021-02-07 10:43:38 +0100

    improve performance of Utf8 ChunkedArray from_iter ~30%

--- polars/benches/collect.rs
+++ polars/benches/collect.rs
@@ -17,6 +17,11 @@ fn bench_collect_optional_num(v: &[Option<i32>]) {
     criterion::black_box(f());
 }
 
+fn bench_collect_optional_str(v: &[Option<String>]) {
+    let f = || v.iter().map(|opt| opt.as_ref()).collect::<Utf8Chunked>();
+    criterion::black_box(f());
+}
+
 fn create_array(size: i32, null_percentage: f32) -> Vec<Option<i32>> {
     let mut rng = StdRng::seed_from_u64(0);
     (0..size)
@@ -25,6 +30,26 @@ fn create_array(size: i32, null_percentage: f32) -> Vec<Option<i32>> {
                 None
             } else {
                 Some(i)
+            }
+        })
+        .collect()
+}
+
+fn create_string_array(
+    size: i32,
+    null_percentage: f32,
+    lower_len: usize,
+    upper_len: usize,
+) -> Vec<Option<String>> {
+    let mut rng = StdRng::seed_from_u64(0);
+    (0..size)
+        .map(|i| {
+            if rng.gen::<f32>() < null_percentage {
+                None
+            } else {
+                let length = rng.gen_range(lower_len..upper_len);
+                let s: String = (0..length).map(|_| rng.gen::<u8>() as char).collect();
+                Some(s)
             }
         })
         .collect()
@@ -48,6 +73,15 @@ fn add_benchmark(c: &mut Criterion) {
     let v = create_array(4096, 0.05);
     c.bench_function("collect optional_num 4096", |b| {
         b.iter(|| bench_collect_optional_num(&v))
+    });
+
+    let v = create_string_array(1024, 0.05, 5, 80);
+    c.bench_function("collect optional_str 1024", |b| {
+        b.iter(|| bench_collect_optional_str(&v))
+    });
+    let v = create_string_array(4096, 0.05, 5, 80);
+    c.bench_function("collect optional_str 4096", |b| {
+        b.iter(|| bench_collect_optional_str(&v))
     });
 }
 
