c4d554eabd900790d448dc203f752068c0b970e8
paq <89paku@gmail.com>
2021-02-14 14:22:45 +0900

    remove a unused function

--- polars/polars-core/src/prelude.rs
+++ polars/polars-core/src/prelude.rs
@@ -35,13 +35,6 @@ pub use std::sync::Arc;
 #[cfg(feature = "temporal")]
 pub use crate::chunked_array::temporal::conversion::*;
 
-#[cfg(test)]
-pub(crate) fn create_df() -> DataFrame {
-    let s0 = Series::new("days", [0, 1, 2, 3, 4].as_ref());
-    let s1 = Series::new("temp", [22.1, 19.9, 7., 2., 3.].as_ref());
-    DataFrame::new(vec![s0, s1]).unwrap()
-}
-
 #[macro_export]
 macro_rules! as_result {
     ($block:block) => {{
