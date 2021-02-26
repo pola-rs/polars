06473937ea62118c5df61aecf9bc3a99f4915ab6
Ritchie Vink <ritchie46@gmail.com>
2021-02-26 11:37:44 +0100

    remove redundant aligned_vec_to_primitive function

--- polars/polars-core/src/chunked_array/kernels/temporal.rs
+++ polars/polars-core/src/chunked_array/kernels/temporal.rs
@@ -1,7 +1,5 @@
 use crate::chunked_array::{
-    builder::{
-        aligned_vec_to_primitive_array, build_with_existing_null_bitmap_and_slice, get_bitmap,
-    },
+    builder::{build_with_existing_null_bitmap_and_slice, get_bitmap},
     temporal::conversions_utils::*,
 };
 use crate::prelude::*;
@@ -11,18 +9,14 @@ use std::sync::Arc;
 
 pub fn date32_as_duration(arr: &PrimitiveArray<Date32Type>) -> ArrayRef {
     let vals = arr.values();
-    let (null_count, null_bit_buffer) = get_bitmap(arr);
+    let (_null_count, null_bit_buffer) = get_bitmap(arr);
 
     let av = vals
         .iter()
         .map(|days| (days * 3600 * 24 * 1000) as i64)
-        .collect();
+        .collect::<AlignedVec<_>>();
 
-    Arc::new(aligned_vec_to_primitive_array::<DurationMillisecondType>(
-        av,
-        null_bit_buffer,
-        Some(null_count),
-    ))
+    Arc::new(av.into_primitive_array::<DurationMillisecondType>(null_bit_buffer))
 }
 
 pub fn date64_as_duration(arr: &PrimitiveArray<Date64Type>) -> ArrayRef {
@@ -37,19 +31,15 @@ macro_rules! to_temporal_unit {
     ($name: ident, $chrono_method:ident, $to_datetime_fn: expr, $dtype_in: ty, $dtype_out:ty) => {
         pub fn $name(arr: &PrimitiveArray<$dtype_in>) -> ArrayRef {
             let vals = arr.values();
-            let (null_count, null_bit_buffer) = get_bitmap(arr);
+            let (_null_count, null_bit_buffer) = get_bitmap(arr);
             let av = vals
                 .iter()
                 .map(|&v| {
                     let dt = $to_datetime_fn(v);
                     dt.$chrono_method()
                 })
-                .collect();
-            Arc::new(aligned_vec_to_primitive_array::<$dtype_out>(
-                av,
-                null_bit_buffer,
-                Some(null_count),
-            ))
+                .collect::<AlignedVec<_>>();
+            Arc::new(av.into_primitive_array::<$dtype_out>(null_bit_buffer))
         }
     };
 }
