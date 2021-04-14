3af47fe4fa4280aec399c53fc380bbc6ff848395
Ritchie Vink <ritchie46@gmail.com>
2021-04-14 16:01:57 +0200

    fix issue with unaligned pyarrow chunkedarrays; #545

--- py-polars/tests/test_io.py
+++ py-polars/tests/test_io.py
@@ -1,6 +1,8 @@
 import io
 from utils import get_complete_df
 import polars as pl
+import pandas as pd
+import numpy as np
 
 
 def test_to_from_buffer():
@@ -22,3 +24,31 @@ def test_read_web_file():
     url = "https://raw.githubusercontent.com/ritchie46/polars/master/examples/aggregate_multiple_files_in_chunks/datasets/foods1.csv"
     df = pl.read_csv(url)
     assert df.shape == (27, 4)
+
+
+def test_parquet_chunks():
+    """
+    This failed in https://github.com/ritchie46/polars/issues/545
+    """
+    cases = [
+        1048576,
+        1048577,
+    ]
+
+    for case in cases:
+        f = io.BytesIO()
+        # repeat until it has case instances
+        df = pd.DataFrame(
+            np.tile([1.0, pd.to_datetime("2010-10-10")], [case, 1]),
+            columns=["floats", "dates"],
+        )
+        print(df)
+
+        # write as parquet
+        df.to_parquet(f)
+
+        print(f"reading {case} dates with polars...", end="")
+        f.seek(0)
+
+        # read it with polars
+        polars_df = pl.read_parquet(f)
