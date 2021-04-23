947355824cd257555e7d6458fc8464f974babac5
Ritchie Vink <ritchie46@gmail.com>
2021-04-23 16:37:17 +0200

    update physical execution state

--- polars/polars-lazy/src/physical_plan/mod.rs
+++ polars/polars-lazy/src/physical_plan/mod.rs
@@ -1,19 +1,17 @@
 pub mod executors;
 pub mod expressions;
 pub mod planner;
+pub(crate) mod state;
 
+use crate::physical_plan::state::ExecutionState;
 use crate::prelude::*;
-use ahash::RandomState;
 use polars_core::prelude::*;
 use polars_io::PhysicalIoExpr;
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
 
-pub enum ExprVal {
-    Series(Series),
-    Column(Vec<String>),
-}
-
+/// A type that implements this transforms a LogicalPlan to a physical plan.
+///
+/// We could produce different physical plans with different goals in mind, e.g. memory optimized
+/// performance optimized, out of core, etc.
 pub trait PhysicalPlanner {
     fn create_physical_plan(
         &self,
@@ -27,8 +25,9 @@ pub trait PhysicalPlanner {
 // combine physical expressions, which produce Series.
 
 /// Executors will evaluate physical expressions and collect them in a DataFrame.
+///
+/// Executors have other executors as input. By having a tree of executors we can execute the
+/// physical plan until the last executor is evaluated.
 pub trait Executor: Send + Sync {
-    fn execute(&mut self, cache: &Cache) -> Result<DataFrame>;
+    fn execute(&mut self, cache: &ExecutionState) -> Result<DataFrame>;
 }
-
-pub(crate) type Cache = Arc<Mutex<HashMap<String, DataFrame, RandomState>>>;
