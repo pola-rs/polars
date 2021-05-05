b808c812419c02df7d2bfe4e79fdda742efb5560
Ritchie Vink <ritchie46@gmail.com>
2021-05-05 09:01:17 +0200

    don't support cum_agg for boolean types; users can cast

--- polars/polars-core/src/chunked_array/ops/cum_agg.rs
+++ polars/polars-core/src/chunked_array/ops/cum_agg.rs
@@ -63,7 +63,7 @@ where
             true => Box::new(self.into_iter().rev()),
         };
         let mut ca: Self = iter
-            .scan(Bounded::max_value(), |state, v| match v {
+            .scan(Bounded::min_value(), |state, v| match v {
                 Some(v) => {
                     *state += v;
                     Some(Some(*state))
@@ -83,55 +83,8 @@ where
 impl ChunkCumAgg<CategoricalType> for CategoricalChunked {}
 impl ChunkCumAgg<Utf8Type> for Utf8Chunked {}
 impl ChunkCumAgg<ListType> for ListChunked {}
-impl ChunkCumAgg<BooleanType> for BooleanChunked {
-    fn cum_max(&self, reverse: bool) -> ChunkedArray<BooleanType> {
-        let iter: Box<dyn Iterator<Item = Option<bool>>> = match reverse {
-            false => Box::new(self.into_iter()),
-            true => Box::new(self.into_iter().rev()),
-        };
-        let mut ca: Self = iter
-            .scan(false, |state, v| match v {
-                Some(v) => {
-                    if !v & *state {
-                        *state = v
-                    }
-                    Some(Some(*state))
-                }
-                None => Some(None),
-            })
-            .collect();
-        ca.rename(self.name());
-        if reverse {
-            ca.reverse()
-        } else {
-            ca
-        }
-    }
+impl ChunkCumAgg<BooleanType> for BooleanChunked {}
 
-    fn cum_min(&self, reverse: bool) -> ChunkedArray<BooleanType> {
-        let iter: Box<dyn Iterator<Item = Option<bool>>> = match reverse {
-            false => Box::new(self.into_iter()),
-            true => Box::new(self.into_iter().rev()),
-        };
-        let mut ca: Self = iter
-            .scan(true, |state, v| match v {
-                Some(v) => {
-                    if v & !(*state) {
-                        *state = v
-                    }
-                    Some(Some(*state))
-                }
-                None => Some(None),
-            })
-            .collect();
-        ca.rename(self.name());
-        if reverse {
-            ca.reverse()
-        } else {
-            ca
-        }
-    }
-}
 #[cfg(feature = "object")]
 impl<T> ChunkCumAgg<ObjectType<T>> for ObjectChunked<T> {}
 
@@ -146,5 +99,17 @@ mod test {
         assert_eq!(Vec::from(&out), &[None, Some(3), Some(3), None, Some(1)]);
         let out = ca.cum_max(false);
         assert_eq!(Vec::from(&out), &[None, Some(1), Some(3), None, Some(3)]);
+    }
+
+    #[test]
+    fn test_cum_sum() {
+        let ca = Int32Chunked::new_from_opt_slice("foo", &[None, Some(1), Some(3), None, Some(1)]);
+        let out = ca.cum_sum(true);
+        assert_eq!(
+            Vec::from(&out),
+            &[Some(5), Some(5), Some(4), Some(1), Some(1)]
+        );
+        let out = ca.cum_sum(false);
+        assert_eq!(Vec::from(&out), &[None, Some(1), Some(4), Some(4), Some(5)]);
     }
 }
