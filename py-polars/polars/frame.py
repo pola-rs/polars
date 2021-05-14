382b598f97939da861af3d8bfb135ff40a2c6fe9
Ritchie Vink <ritchie46@gmail.com>
2021-05-14 13:16:08 +0200

    python polars 0.7.16

--- py-polars/polars/frame.py
+++ py-polars/polars/frame.py
@@ -1397,6 +1397,10 @@ class DataFrame:
         """
         if _is_expr(strategy):
             return self.lazy().fill_none(strategy).collect()
+        if not isinstance(strategy, str):
+            from .lazy import lit
+
+            return self.fill_none(lit(strategy))
         return wrap_df(self._df.fill_none(strategy))
 
     def explode(self, columns: "Union[str, List[str]]") -> "DataFrame":
