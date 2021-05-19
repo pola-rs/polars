8f1cceb98dfed44e89e52bbe2b0435a4b24008a5
Ritchie Vink <ritchie46@gmail.com>
2021-05-19 21:51:49 +0200

    Lazy parallelize hstack

--- polars/polars-lazy/src/physical_plan/executors/stack.rs
+++ polars/polars-lazy/src/physical_plan/executors/stack.rs
@@ -1,6 +1,8 @@
 use crate::physical_plan::state::ExecutionState;
 use crate::prelude::*;
-use polars_core::prelude::*;
+use polars_core::{prelude::*, POOL};
+use rayon::prelude::*;
+
 pub struct StackExec {
     input: Box<dyn Executor>,
     expr: Vec<Arc<dyn PhysicalExpr>>,
@@ -17,21 +19,27 @@ impl Executor for StackExec {
         let mut df = self.input.execute(state)?;
         let height = df.height();
 
-        let res: Result<_> = self.expr.iter().try_for_each(|expr| {
-            let s = expr.evaluate(&df, state).map(|series| {
-                // literal series. Should be whole column size
-                if series.len() == 1 && height > 1 {
-                    series.expand_at_index(0, height)
-                } else {
-                    series
-                }
-            })?;
+        let res = POOL.install(|| {
+            self.expr
+                .par_iter()
+                .map(|expr| {
+                    expr.evaluate(&df, state).map(|series| {
+                        // literal series. Should be whole column size
+                        if series.len() == 1 && height > 1 {
+                            series.expand_at_index(0, height)
+                        } else {
+                            series
+                        }
+                    })
+                })
+                .collect::<Result<Vec<_>>>()
+        })?;
 
+        for s in res {
             let name = s.name().to_string();
             df.replace_or_add(&name, s)?;
-            Ok(())
-        });
-        let _ = res?;
+        }
+
         state.clear_expr_cache();
         Ok(df)
     }
