c5307a3540857517063cc80aa555acdf93e8f6ca
Ritchie Vink <ritchie46@gmail.com>
2021-05-21 20:05:18 +0200

    python 0.7.17

--- .github/deploy_manylinux.sh
+++ .github/deploy_manylinux.sh
@@ -8,5 +8,6 @@ rm py-polars/README.md
 cp README.md py-polars/README.md
 cd py-polars
 rustup override set nightly-2021-03-24
+export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3'
 maturin publish \
 --username ritchie46
