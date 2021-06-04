24f822e6f65bd6dc0b36f2f876950ae6ed2b4287
Ritchie Vink <ritchie46@gmail.com>
2021-06-04 12:13:45 +0200

    benchmark and assert checksums of db-benchmark

--- py-polars/tests/db-benchmark/main.py
+++ py-polars/tests/db-benchmark/main.py
@@ -27,8 +27,110 @@ t0 = time.time()
 print("q1")
 out = x.groupby("id1").agg(pl.sum("v1")).collect()
 print(time.time() - t0)
+print("out.shape", out.shape)
+print('out["v1_sum"].sum()', out["v1_sum"].sum())
+
+t0easy = time.time()
+t0 = time.time()
+print("q2")
+out = x.groupby(["id1", "id2"]).agg(pl.sum("v1")).collect()
+print(time.time() - t0)
+print("out.shape", out.shape)
+print('out["v1_sum"].sum()', out["v1_sum"].sum())
+
+t0 = time.time()
+print("q3")
+out = x.groupby("id3").agg([pl.sum("v1"), pl.mean("v3")]).collect()
+print(time.time() - t0)
+print("out.shape", out.shape)
+print('out["v1_sum"].sum()', out["v1_sum"].sum())
+print('out["v3_mean"].sum()', out["v3_mean"].sum())
+
+t0 = time.time()
+print("q4")
+out = x.groupby("id4").agg([pl.mean("v1"), pl.mean("v2"), pl.mean("v3")]).collect()
+print(time.time() - t0)
+print("out.shape", out.shape)
+print('out["v1_mean"].sum()', out["v1_mean"].sum())
+print('out["v2_mean"].sum()', out["v2_mean"].sum())
+print('out["v3_mean"].sum()', out["v3_mean"].sum())
+
+t0 = time.time()
+print("q5")
+out = x.groupby("id6").agg([pl.sum("v1"), pl.sum("v2"), pl.sum("v3")]).collect()
+print(time.time() - t0)
+print("out.shape", out.shape)
+print('out["v1_sum"].sum()', out["v1_sum"].sum())
+print('out["v2_sum"].sum()', out["v2_sum"].sum())
+easy_time = time.time() - t0easy
+t0advanced = time.time()
+
+t0 = time.time()
+print("q6")
+out = (
+    x.groupby(["id4", "id5"])
+    .agg([pl.median("v3").alias("v3_median"), pl.std("v3").alias("v3_std")])
+    .collect()
+)
+print(time.time() - t0)
+print("out.shape", out.shape)
+print('out["v3_median"].sum()', out["v3_median"].sum())
+print('out["v3_std"].sum()', out["v3_std"].sum())
+
+t0 = time.time()
+print("q7")
+out = (
+    x.groupby("id3").agg([(pl.max("v1") - pl.min("v2")).alias("range_v1_v2")]).collect()
+)
+print(time.time() - t0)
+print("out.shape", out.shape)
+print('out["range_v1_v2"].sum()', out["range_v1_v2"].sum())
+
+t0 = time.time()
+print("q8")
+out = (
+    x.drop_nulls("v3")
+    .sort("v3", reverse=True)
+    .groupby("id6")
+    .agg(col("v3").head(2).alias("largest2_v3"))
+    .explode("largest2_v3")
+    .collect()
+)
+print(time.time() - t0)
+print("out.shape", out.shape)
+print('out["largest2_v3"].sum()', out["largest2_v3"].sum())
+
+t0 = time.time()
+print("q9")
+out = (
+    x.groupby(["id2", "id4"])
+    .agg((pl.pearson_corr("v1", "v2") ** 2).alias("r2"))
+    .collect()
+)
+print(time.time() - t0)
+print("out.shape", out.shape)
+print('out["r2"].sum()', out["r2"].sum())
+
+t0 = time.time()
+print("q10")
+out = (
+    x.groupby(["id1", "id2", "id3", "id4", "id5", "id6"])
+    .agg([pl.sum("v3").alias("v3"), pl.count("v1").alias("count")])
+    .collect()
+)
+print(time.time() - t0)
+print("out.shape", out.shape)
+print("easy took:", easy_time, "s")
+print("advanced took:", time.time() - t0advanced, "s")
+print("total took:", time.time() - t00, "s")
+
+t00 = time.time()
+t0 = time.time()
+print("q1")
+out = x.groupby("id1").agg(pl.sum("v1")).collect()
+print(time.time() - t0)
 assert out.shape == (96, 2)
-assert out["v1_sum"].sum() == 28498857
+assert out["v1_sum"].sum() == 28501451
 
 t0easy = time.time()
 t0 = time.time()
@@ -36,33 +138,32 @@ print("q2")
 out = x.groupby(["id1", "id2"]).agg(pl.sum("v1")).collect()
 print(time.time() - t0)
 assert out.shape == (9216, 3)
-assert out["v1_sum"].sum() == 28498857
+assert out["v1_sum"].sum() == 28501451
 
 t0 = time.time()
 print("q3")
 out = x.groupby("id3").agg([pl.sum("v1"), pl.mean("v3")]).collect()
 print(time.time() - t0)
 assert out.shape == (95001, 3)
-assert out["v1_sum"].sum() == 28498857
-assert np.isclose(out["v3_mean"].sum(), 4749467.631946711)
+assert out["v1_sum"].sum() == 28501451
+assert np.isclose(out["v3_mean"].sum(), 4751358.825104358)
 
 t0 = time.time()
 print("q4")
 out = x.groupby("id4").agg([pl.mean("v1"), pl.mean("v2"), pl.mean("v3")]).collect()
 print(time.time() - t0)
 assert out.shape == (96, 4)
-assert np.isclose(out["v1_mean"].sum(), 287.9894309270617)
-assert np.isclose(out["v2_mean"].sum(), 767.8529216923457)
-assert np.isclose(out["v3_mean"].sum(), 4799.873270453374)
+assert np.isclose(out["v1_mean"].sum(), 288.0192364601018)
+assert np.isclose(out["v2_mean"].sum(), 767.9422306545811)
+assert np.isclose(out["v3_mean"].sum(), 4801.784316931509)
 
 t0 = time.time()
 print("q5")
 out = x.groupby("id6").agg([pl.sum("v1"), pl.sum("v2"), pl.sum("v3")]).collect()
 print(time.time() - t0)
 assert out.shape == (95001, 4)
-assert out["v1_sum"].sum() == 28498857
-assert out["v2_sum"].sum() == 75988394
-assert np.isclose(out["v3_sum"].sum(), 474969574.0477772)
+assert out["v1_sum"].sum() == 28501451
+assert out["v2_sum"].sum() == 75998165
 easy_time = time.time() - t0easy
 t0advanced = time.time()
 
@@ -75,8 +176,8 @@ out = (
 )
 print(time.time() - t0)
 assert out.shape == (9216, 4)
-assert np.isclose(out["v3_median"].sum(), 460771.21644399985)
-assert np.isclose(out["v3_std"].sum(), 266006.9046221105)
+assert np.isclose(out["v3_median"].sum(), 460892.5487690001)
+assert np.isclose(out["v3_std"].sum(), 266052.20492321637)
 
 t0 = time.time()
 print("q7")
@@ -85,7 +186,7 @@ out = (
 )
 print(time.time() - t0)
 assert out.shape == (95001, 2)
-assert out["range_v1_v2"].sum() == 379850
+assert out["range_v1_v2"].sum() == 379846
 
 t0 = time.time()
 print("q8")
@@ -99,7 +200,7 @@ out = (
 )
 print(time.time() - t0)
 assert out.shape == (190002, 2)
-assert np.isclose(out["largest2_v3"].sum(), 18700554.77963197)
+assert np.isclose(out["largest2_v3"].sum(), 18700642.66837202)
 
 t0 = time.time()
 print("q9")
@@ -110,7 +211,7 @@ out = (
 )
 print(time.time() - t0)
 assert out.shape == (9216, 3)
-assert np.isclose(out["r2"].sum(), 9.941512305644292)
+assert np.isclose(out["r2"].sum(), 9.896846028461322)
 
 t0 = time.time()
 print("q10")
@@ -123,4 +224,4 @@ print(time.time() - t0)
 print("easy took:", easy_time, "s")
 print("advanced took:", time.time() - t0advanced, "s")
 print("total took:", time.time() - t00, "s")
-assert out.shape == (9999993, 8)
+assert out.shape == (9999995, 8)
