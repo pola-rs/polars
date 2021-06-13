095c911209648fa6325b64f4a5f47815605f8f7c
Ritchie Vink <ritchie46@gmail.com>
2021-06-13 12:02:20 +0200

    also fix for list-type

--- polars/polars-lazy/src/physical_plan/expressions/cast.rs
+++ polars/polars-lazy/src/physical_plan/expressions/cast.rs
@@ -17,6 +17,16 @@ impl CastExpr {
 impl PhysicalExpr for CastExpr {
     fn evaluate(&self, df: &DataFrame, state: &ExecutionState) -> Result<Series> {
         let series = self.input.evaluate(df, state)?;
+        // this is quite dirty
+        // We use the booleanarray as null series, because we have no null array.
+        // in a ternary or binary operation, we then do type coercion to matching supertype.
+        // here we create a null array for the types we cannot cast to from a booleanarray
+        if matches!(self.data_type, DataType::List(_)) {
+            // the booleanarray is hacked as null type
+            if series.bool().is_ok() && series.null_count() == series.len() {
+                return Ok(ListChunked::full_null(series.name(), series.len()).into_series());
+            }
+        }
         series.cast_with_dtype(&self.data_type)
     }
     fn to_field(&self, input_schema: &Schema) -> Result<Field> {
