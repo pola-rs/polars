1e74574beb991c2c9bea2b35c3411d471cb97555
Ritchie Vink <ritchie46@gmail.com>
2021-06-13 20:45:12 +0200

    arrow2: polars-arrow

---
+++ polars/polars-arrow/src/error.rs
@@ -0,0 +1,16 @@
+use std::borrow::Cow;
+use thiserror::Error as ThisError;
+
+type ErrString = Cow<'static, str>;
+
+#[derive(Debug, ThisError)]
+pub enum PolarsError {
+    #[error(transparent)]
+    ArrowError(#[from] arrow::error::ArrowError),
+    #[error("{0}")]
+    ComputeError(ErrString),
+    #[error("Out of bounds: {0}")]
+    OutOfBounds(ErrString),
+}
+
+pub type Result<T> = std::result::Result<T, PolarsError>;
---
+++ polars/polars-arrow/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod array;
+pub mod bit_util;
+pub mod buffer;
+pub mod error;
+pub mod kernels;
+pub mod prelude;
+pub mod utils;
