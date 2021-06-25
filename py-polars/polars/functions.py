af8cc27468377419e87c79ec54265527fd47075c
Ritchie Vink <ritchie46@gmail.com>
2021-06-25 13:39:37 +0200

    python from_pandas accept series and datetimeindex

--- py-polars/polars/functions.py
+++ py-polars/polars/functions.py
@@ -2,6 +2,9 @@ from typing import Union, TextIO, Optional, List, BinaryIO, Sequence, Any
 from io import StringIO, BytesIO
 import numpy as np
 from pathlib import Path
+
+import pandas as pd
+
 from .frame import DataFrame
 from .series import Series
 from .lazy import LazyFrame
@@ -445,8 +448,23 @@ def from_arrow(a: "Union[pa.Table, pa.Array]", rechunk: bool = True) -> "DataFra
     raise ValueError(f"expected arrow table / array, got {a}")
 
 
+def _from_pandas_helper(a: "pandas.Series") -> "pa.Array":  # noqa: F821
+    dtype = a.dtype
+    if dtype == "datetime64[ns]":
+        # We first cast to ms because that's the unit of Date64,
+        # Then we cast to via int64 to date64. Casting directly to Date64 lead to
+        # loss of time information https://github.com/ritchie46/polars/issues/476
+        arr = pa.array(np.array(a.values, dtype="datetime64[ms]"))
+        arr = pa.compute.cast(arr, pa.int64())
+        return pa.compute.cast(arr, pa.date64())
+    elif dtype == "object" and isinstance(a.iloc[0], str):
+        return pa.array(a, pa.large_utf8())
+    else:
+        return pa.array(a)
+
+
 def from_pandas(
-    df: "pandas.DataFrame", rechunk: bool = True  # noqa: F821
+    df: "[pandas.DataFrame, pandas.DateTimeIndex]", rechunk: bool = True  # noqa: F821
 ) -> "DataFrame":
     """
     Convert from a pandas DataFrame to a polars DataFrame.
@@ -462,6 +480,8 @@ def from_pandas(
     -------
     A Polars DataFrame
     """
+    if type(df) == pd.Series or type(df) == pd.DatetimeIndex:
+        return from_arrow(_from_pandas_helper(df))
 
     # Note: we first tried to infer the schema via pyarrow and then modify the schema if needed.
     # However arrow 3.0 determines the type of a string like this:
@@ -469,18 +489,9 @@ def from_pandas(
     # needlessly allocating and failing when the string is too large for the string dtype.
     data = {}
 
-    for (name, dtype) in zip(df.columns, df.dtypes):
-        if dtype == "object" and isinstance(df[name].iloc[0], str):
-            data[name] = pa.array(df[name], pa.large_utf8())
-        elif dtype == "datetime64[ns]":
-            # We first cast to ms because that's the unit of Date64,
-            # Then we cast to via int64 to date64. Casting directly to Date64 lead to
-            # loss of time information https://github.com/ritchie46/polars/issues/476
-            arr = pa.array(np.array(df[name].values, dtype="datetime64[ms]"))
-            arr = pa.compute.cast(arr, pa.int64())
-            data[name] = pa.compute.cast(arr, pa.date64())
-        else:
-            data[name] = pa.array(df[name])
+    for name in df.columns:
+        s = df[name]
+        data[name] = _from_pandas_helper(s)
 
     table = pa.table(data)
     return from_arrow(table, rechunk)
