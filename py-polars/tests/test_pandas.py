e673f222bd3f1a19851056706feca70cf59f7226
Ritchie Vink <ritchie46@gmail.com>
2021-06-25 13:39:37 +0200

    python from_pandas accept series and datetimeindex

--- py-polars/tests/test_pandas.py
+++ py-polars/tests/test_pandas.py
@@ -10,3 +10,10 @@ def test_from_pandas_datetime():
     assert s.dt.hour()[0] == 20
     assert s.dt.minute()[0] == 20
     assert s.dt.second()[0] == 20
+
+    date_times = pd.date_range(
+        "2021-06-24 00:00:00", "2021-06-24 10:00:00", freq="1H", closed="left"
+    )
+    s = pl.from_pandas(date_times)
+    assert s[0] == 1624492800000
+    assert s[-1] == 1624525200000
