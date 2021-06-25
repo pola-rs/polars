0eca4d6aa5945789f0ba8ad45c90806f03b6ff20
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-06-25 06:21:23 +0000

    Minor fixes.

--- polars/polars-io/src/csv.rs
+++ polars/polars-io/src/csv.rs
@@ -457,11 +457,11 @@ mod test {
     #[test]
     fn write_csv() {
         let mut buf: Vec<u8> = Vec::new();
-        let mut df = create_df();
+        let df = create_df();
 
         CsvWriter::new(&mut buf)
             .has_headers(true)
-            .finish(&mut df)
+            .finish(&df)
             .expect("csv written");
         let csv = std::str::from_utf8(&buf).unwrap();
         assert_eq!("days,temp\n0,22.1\n1,19.9\n2,7.0\n3,2.0\n4,3.0\n", csv);
