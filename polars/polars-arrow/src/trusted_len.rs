c3e5571ec2c320cad0f9963f7b080ac9b80adf2e
Ritchie Vink <ritchie46@gmail.com>
2021-07-02 10:33:26 +0200

    improve argsort performance

--- polars/polars-arrow/src/trusted_len.rs
+++ polars/polars-arrow/src/trusted_len.rs
@@ -1,7 +1,7 @@
 use crate::utils::TrustMyLength;
 use crate::vec::AlignedVec;
 use arrow::buffer::MutableBuffer;
-use arrow::datatypes::ArrowNativeType;
+use arrow::datatypes::{ArrowNativeType, ArrowPrimitiveType};
 use arrow::util::bit_util;
 use std::slice::Iter;
 
@@ -53,6 +53,9 @@ unsafe impl<I: TrustedLen + DoubleEndedIterator> TrustedLen for std::iter::Rev<I
 
 unsafe impl<I: Iterator<Item = J>, J> TrustedLen for TrustMyLength<I, J> {}
 unsafe impl<T> TrustedLen for std::ops::Range<T> where std::ops::Range<T>: Iterator {}
+unsafe impl<T: ArrowPrimitiveType> TrustedLen for arrow::array::PrimitiveIter<'_, T> {}
+unsafe impl TrustedLen for arrow::array::GenericStringIter<'_, i64> {}
+unsafe impl TrustedLen for arrow::array::BooleanIter<'_> {}
 
 ///
 /// unzips an iterator over an Option<T> into a given validity buffer and value buffer
@@ -95,4 +98,52 @@ pub unsafe fn trusted_len_unzip_extend<I, P, T>(
         "Trusted iterator length was not accurately reported"
     );
     values.set_len(values.len() + upper)
+}
+
+pub trait PushUnchecked<T> {
+    /// Will push an item and not check if there is enough capacity
+    ///
+    /// # Safety
+    /// Caller must ensure the array has enough capacity to hold `T`.
+    unsafe fn push_unchecked(&mut self, value: T);
+
+    /// Will push an item and not check if there is enough capacity nor update the array's lenght
+    /// # Safety
+    /// Caller must ensure the array has enough capacity to hold `T`.
+    /// Caller must update the length when its done updating the vector.
+    unsafe fn push_unchecked_no_len_set(&mut self, value: T);
+
+    /// Extend the array with an iterator who's length can be trusted
+    fn extend_trusted_len<I: IntoIterator<Item = T> + TrustedLen>(&mut self, iter: I);
+}
+
+impl<T> PushUnchecked<T> for Vec<T> {
+    #[inline]
+    unsafe fn push_unchecked(&mut self, value: T) {
+        let end = self.as_mut_ptr().add(self.len());
+        std::ptr::write(end, value);
+        self.set_len(self.len() + 1);
+    }
+
+    #[inline]
+    unsafe fn push_unchecked_no_len_set(&mut self, value: T) {
+        let end = self.as_mut_ptr().add(self.len());
+        std::ptr::write(end, value);
+    }
+
+    #[inline]
+    fn extend_trusted_len<I: IntoIterator<Item = T> + TrustedLen>(&mut self, iter: I) {
+        let iter = iter.into_iter();
+        let upper = iter.size_hint().1.expect("must have an upper bound");
+        self.reserve(upper);
+
+        unsafe {
+            let mut dst = self.as_mut_ptr().add(self.len());
+            for value in iter {
+                std::ptr::write(dst, value);
+                dst = dst.add(1)
+            }
+            self.set_len(self.len() + upper)
+        }
+    }
 }
