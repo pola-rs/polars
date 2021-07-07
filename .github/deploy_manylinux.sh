5271c3e826f689748abe33d1b9477566f4c30323
Ritchie Vink <ritchie46@gmail.com>
2021-07-07 10:30:01 +0200

    update rustc in CI to 2021-07-04

--- .github/deploy_manylinux.sh
+++ .github/deploy_manylinux.sh
@@ -7,7 +7,7 @@ ls -la
 rm py-polars/README.md
 cp README.md py-polars/README.md
 cd py-polars
-rustup override set nightly-2021-03-24
+rustup override set nightly-2021-07-04
 export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+popcnt'
 maturin publish \
 --username ritchie46
