b045b39b1041fd8578845c05a99ab1bbeded76c1
Marc van Heerden <11750833+marcvanheerden@users.noreply.github.com>
2021-07-07 08:53:11 +0200

    resolve "~" to homedir in CsvReader (#931)

--- polars/polars-io/src/utils.rs
+++ polars/polars-io/src/utils.rs
@@ -1,4 +1,6 @@
+use dirs::home_dir;
 use polars_core::prelude::*;
+use std::path::{Path, PathBuf};
 
 pub(crate) fn to_arrow_compatible_df(df: &DataFrame) -> DataFrame {
     // Our categorical type is not known to arrow/ parquet, so we coerce to large-utf8.
@@ -11,4 +13,59 @@ pub(crate) fn to_arrow_compatible_df(df: &DataFrame) -> DataFrame {
         })
         .collect::<Vec<_>>();
     DataFrame::new_no_checks(cols)
+}
+
+pub(crate) fn resolve_homedir(path: &Path) -> PathBuf {
+    // replace "~" with home directory
+    if path.starts_with("~") {
+        if let Some(homedir) = home_dir() {
+            return homedir.join(path.strip_prefix("~").unwrap());
+        }
+    }
+
+    path.into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::resolve_homedir;
+    use std::path::PathBuf;
+
+    #[cfg(not(target_os = "windows"))]
+    #[test]
+    fn test_resolve_homedir() {
+        let paths: Vec<PathBuf> = vec![
+            "~/dir1/dir2/test.csv".into(),
+            "/abs/path/test.csv".into(),
+            "rel/path/test.csv".into(),
+            "/".into(),
+            "~".into(),
+        ];
+
+        let resolved: Vec<PathBuf> = paths.iter().map(|x| resolve_homedir(x)).collect();
+
+        assert_eq!(resolved[0].file_name(), paths[0].file_name());
+        assert!(resolved[0].is_absolute());
+        assert_eq!(resolved[1], paths[1]);
+        assert_eq!(resolved[2], paths[2]);
+        assert_eq!(resolved[3], paths[3]);
+        assert!(resolved[4].is_absolute());
+    }
+
+    #[cfg(target_os = "windows")]
+    #[test]
+    fn test_resolve_homedir_windows() {
+        let paths: Vec<PathBuf> = vec![
+            r#"c:\Users\user1\test.csv"#.into(),
+            r#"~\user1\test.csv"#.into(),
+            "~".into(),
+        ];
+
+        let resolved: Vec<PathBuf> = paths.iter().map(|x| resolve_homedir(x)).collect();
+
+        assert_eq!(resolved[0], paths[0]);
+        assert_eq!(resolved[1].file_name(), paths[1].file_name());
+        assert!(resolved[1].is_absolute());
+        assert!(resolved[2].is_absolute());
+    }
 }
