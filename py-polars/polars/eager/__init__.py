f04728a5999ee4abbd25e393be200e3d9caf6e17
Stijn de Gooijer <stijn@degooijer.io>
2021-07-13 22:09:49 +0200

    Split off functions and whenthen

--- py-polars/polars/eager/__init__.py
+++ py-polars/polars/eager/__init__.py
@@ -1,3 +1,5 @@
 # flake8: noqa
 from .frame import *
 from .series import *
+
+__all__ = frame.__all__ + series.__all__
--- py-polars/polars/lazy/__init__.py
+++ py-polars/polars/lazy/__init__.py
@@ -1,3 +1,7 @@
 # flake8: noqa
 from .expr import *
 from .frame import *
+from .functions import *
+from .whenthen import *
+
+__all__ = expr.__all__ + frame.__all__ + functions.__all__ + whenthen.__all__
