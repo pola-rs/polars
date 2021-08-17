a9f920dfe79e3b53c1396ad1ce555c210aa0ab1b
Ritchie Vink <ritchie46@gmail.com>
2021-08-17 15:32:50 +0200

    chunkedarray interpolate

--- polars/polars-core/src/chunked_array/ops/mod.rs
+++ polars/polars-core/src/chunked_array/ops/mod.rs
@@ -24,6 +24,7 @@ pub(crate) mod downcast;
 pub(crate) mod explode;
 pub(crate) mod fill_none;
 pub(crate) mod filter;
+pub(crate) mod interpolate;
 #[cfg(feature = "is_in")]
 pub(crate) mod is_in;
 pub(crate) mod peaks;
@@ -38,6 +39,10 @@ pub(crate) mod unique;
 pub(crate) mod window;
 #[cfg(feature = "zip_with")]
 pub(crate) mod zip;
+
+pub trait Interpolate {
+    fn interpolate(&self) -> Self;
+}
 
 #[cfg(feature = "reinterpret")]
 pub trait Reinterpret {
