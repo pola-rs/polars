a5569ddc849efa7b2018eb53f968917b719423bb
Ritchie Vink <ritchie46@gmail.com>
2021-08-26 08:21:40 +0200

    python expose rank

--- py-polars/polars/eager/series.py
+++ py-polars/polars/eager/series.py
@@ -2365,6 +2365,31 @@ class Series:
         """
         return np.abs(self)  # type: ignore
 
+    def rank(self, method: str = "average") -> "Series":  # type: ignore
+        """
+        Assign ranks to data, dealing with ties appropriately.
+
+        Parameters
+        ----------
+        method
+            {'average', 'min', 'max', 'dense', 'ordinal'}, optional
+            The method used to assign ranks to tied elements.
+            The following methods are available (default is 'average'):
+              * 'average': The average of the ranks that would have been assigned to
+                all the tied values is assigned to each value.
+              * 'min': The minimum of the ranks that would have been assigned to all
+                the tied values is assigned to each value.  (This is also
+                referred to as "competition" ranking.)
+              * 'max': The maximum of the ranks that would have been assigned to all
+                the tied values is assigned to each value.
+              * 'dense': Like 'min', but the rank of the next highest element is
+                assigned the rank immediately after those assigned to the tied
+                elements.
+              * 'ordinal': All values are given a distinct rank, corresponding to
+                the order that the values occur in `a`.
+        """
+        return wrap_s(self._s.rank(method))
+
 
 class StringNameSpace:
     """
--- py-polars/src/series.rs
+++ py-polars/src/series.rs
@@ -1160,6 +1160,11 @@ impl PySeries {
             Err(e) => Err(e),
         }
     }
+
+    pub fn rank(&self, method: &str) -> PyResult<Self> {
+        let method = str_to_rankmethod(method)?;
+        Ok(self.series.rank(method).into())
+    }
 }
 
 macro_rules! impl_ufuncs {
