e938ff4b77dc7c8a5080e7656040331f45312325
Ritchie Vink <ritchie46@gmail.com>
2021-08-26 11:10:05 +0200

    fix bug in explode on sliced lists

--- polars/polars-core/src/chunked_array/ops/explode.rs
+++ polars/polars-core/src/chunked_array/ops/explode.rs
@@ -28,7 +28,7 @@ pub(crate) fn offsets_to_indexes(offsets: &[i64], capacity: usize) -> AlignedVec
 }
 
 impl ChunkExplode for ListChunked {
-    fn explode_and_offsets(&self) -> Result<(Series, &[i64])> {
+    unsafe fn explode_and_offsets(&self) -> Result<(Series, &[i64], Series)> {
         // A list array's memory layout is actually already 'exploded', so we can just take the values array
         // of the list. And we also return a slice of the offsets. This slice can be used to find the old
         // list layout or indexes to expand the DataFrame in the same manner as the 'explode' operation
@@ -37,19 +37,22 @@ impl ChunkExplode for ListChunked {
             .downcast_iter()
             .next()
             .ok_or_else(|| PolarsError::NoData("cannot explode empty list".into()))?;
-        let list_data = listarr.data();
-        let values = listarr.values();
-        let offset_ptr = list_data.buffers()[0].as_ptr() as *const i64;
-        // offsets in the list array. These indicate where a new list starts
-        let offsets = unsafe { std::slice::from_raw_parts(offset_ptr, self.len()) };
+        let offsets = listarr.value_offsets();
+
+        // This is unsafe in case of a rechunk, that's why we return ListChunked so that lifetime
+        // stay bounded to that ownership
+        let offsets = std::mem::transmute::<&[i64], &[i64]>(offsets);
+        let values = listarr
+            .values()
+            .slice(listarr.offset(), (offsets[offsets.len() - 1]) as usize);
 
         let s = Series::try_from((self.name(), values)).unwrap();
-        Ok((s, offsets))
+        Ok((s, offsets, ca.into_series()))
     }
 }
 
 impl ChunkExplode for Utf8Chunked {
-    fn explode_and_offsets(&self) -> Result<(Series, &[i64])> {
+    unsafe fn explode_and_offsets(&self) -> Result<(Series, &[i64], Series)> {
         // A list array's memory layout is actually already 'exploded', so we can just take the values array
         // of the list. And we also return a slice of the offsets. This slice can be used to find the old
         // list layout or indexes to expand the DataFrame in the same manner as the 'explode' operation
@@ -64,11 +67,12 @@ impl ChunkExplode for Utf8Chunked {
         // We get the offsets of the strings in the original array
         let offset_ptr = list_data.buffers()[0].as_ptr() as *const i64;
         // offsets in the list array. These indicate where a new list starts
-        let offsets = unsafe { std::slice::from_raw_parts(offset_ptr, self.len()) };
+        // This is unsafe in case of a rechunk
+        let offsets = std::slice::from_raw_parts(offset_ptr, self.len());
 
         // Because the strings are u8 stored but really are utf8 data we need to traverse the utf8 to
         // get the chars indexes
-        let str_data = unsafe { std::str::from_utf8_unchecked(str_values_buf.as_slice()) };
+        let str_data = std::str::from_utf8_unchecked(str_values_buf.as_slice());
         // iterator over index and chars, we take only the index
         // todo! directly create a buffer from an aligned vec or a mutable buffer
         let mut new_offsets = str_data.char_indices().map(|t| t.0 as i64).collect_vec();
@@ -108,6 +112,35 @@ impl ChunkExplode for Utf8Chunked {
         let new_arr = Arc::new(LargeStringArray::from(arr_data)) as ArrayRef;
 
         let s = Series::try_from((self.name(), new_arr)).unwrap();
-        Ok((s, offsets))
+        Ok((s, offsets, ca.into_series()))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::chunked_array::builder::get_list_builder;
+
+    #[test]
+    fn test_explode_list() -> Result<()> {
+        let mut builder = get_list_builder(&DataType::Int32, 5, 5, "a");
+
+        builder.append_series(&Series::new("", &[1, 2, 3, 3]));
+        builder.append_series(&Series::new("", &[1]));
+        builder.append_series(&Series::new("", &[2]));
+
+        let ca = builder.finish();
+
+        // normal explode
+        let exploded = ca.explode()?;
+        let out: Vec<_> = exploded.i32()?.into_no_null_iter().collect();
+        assert_eq!(out, &[1, 2, 3, 3, 1, 2]);
+
+        // sliced explode
+        let exploded = ca.slice(0, 1).explode()?;
+        let out: Vec<_> = exploded.i32()?.into_no_null_iter().collect();
+        assert_eq!(out, &[1, 2, 3, 3]);
+
+        Ok(())
     }
 }
