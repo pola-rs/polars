2fb3100ef9d2afe451703d577e3f20257c36e5f7
Ritchie Vink <ritchie46@gmail.com>
2021-08-30 14:00:08 +0200

    numpy ufunc works on expressions

--- py-polars/src/conversion.rs
+++ py-polars/src/conversion.rs
@@ -7,6 +7,7 @@ use polars::frame::row::Row;
 use polars::frame::NullStrategy;
 use polars::prelude::AnyValue;
 use polars_core::utils::arrow::types::NativeType;
+use polars::series::ops::diff::NullBehavior;
 use pyo3::basic::CompareOp;
 use pyo3::conversion::{FromPyObject, IntoPy};
 use pyo3::exceptions::PyValueError;
@@ -15,7 +16,6 @@ use pyo3::types::PySequence;
 use pyo3::{PyAny, PyResult};
 use std::fmt::{Display, Formatter};
 use std::hash::{Hash, Hasher};
-use polars::series::ops::diff::NullBehavior;
 
 #[repr(transparent)]
 pub struct Wrap<T>(pub T);
