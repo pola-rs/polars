f8432ac7b485a0eeb4fabee0fc50b08fd3a60300
Ritchie Vink <ritchie46@gmail.com>
2021-09-15 12:20:41 +0200

    CI update nightly

--- .github/deploy_manylinux.sh
+++ .github/deploy_manylinux.sh
@@ -7,7 +7,7 @@ ls -la
 rm py-polars/README.md
 cp README.md py-polars/README.md
 cd py-polars
-rustup override set nightly-2021-08-30
+rustup override set nightly-2021-09-14
 export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+popcnt'
 maturin publish \
   --skip-existing \
