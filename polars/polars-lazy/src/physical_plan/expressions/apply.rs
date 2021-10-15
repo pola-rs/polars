c4fb980112758fbe2b9629cfcd2ccca821f82e3d
Ritchie Vink <ritchie46@gmail.com>
2021-10-15 10:37:31 +0200

    lazy apply custom function on multiple columns

--- polars/polars-lazy/src/physical_plan/expressions/apply.rs
+++ polars/polars-lazy/src/physical_plan/expressions/apply.rs
@@ -13,6 +13,21 @@ pub struct ApplyExpr {
     pub collect_groups: ApplyOptions,
 }
 
+impl ApplyExpr {
+    #[allow(clippy::ptr_arg)]
+    fn prepare_multiple_inputs<'a>(
+        &self,
+        df: &DataFrame,
+        groups: &'a GroupTuples,
+        state: &ExecutionState,
+    ) -> Result<Vec<AggregationContext<'a>>> {
+        self.inputs
+            .par_iter()
+            .map(|e| e.evaluate_on_groups(df, groups, state))
+            .collect()
+    }
+}
+
 impl PhysicalExpr for ApplyExpr {
     fn as_expression(&self) -> &Expr {
         &self.expr
@@ -38,46 +53,106 @@ impl PhysicalExpr for ApplyExpr {
         groups: &'a GroupTuples,
         state: &ExecutionState,
     ) -> Result<AggregationContext<'a>> {
-        if self.inputs.len() > 1 {
-            return Err(PolarsError::InvalidOperation(
-                "function with multiple inputs not yet supported in aggregation context".into(),
-            ));
-        }
-        let mut ac = self.inputs[0].evaluate_on_groups(df, groups, state)?;
+        if self.inputs.len() == 1 {
+            let mut ac = self.inputs[0].evaluate_on_groups(df, groups, state)?;
 
-        match self.collect_groups {
-            ApplyOptions::ApplyGroups => {
-                let mut container = vec![Default::default()];
-                let name = ac.series().name().to_string();
+            match self.collect_groups {
+                ApplyOptions::ApplyGroups => {
+                    let mut container = [Default::default()];
+                    let name = ac.series().name().to_string();
 
-                let mut ca: ListChunked = ac
-                    .aggregated()
-                    .list()
-                    .unwrap()
-                    .into_iter()
-                    .map(|opt_s| {
-                        opt_s.and_then(|s| {
-                            container[0] = s;
+                    let mut ca: ListChunked = ac
+                        .aggregated()
+                        .list()
+                        .unwrap()
+                        .into_iter()
+                        .map(|opt_s| {
+                            opt_s.and_then(|s| {
+                                container[0] = s;
+                                self.function.call_udf(&mut container).ok()
+                            })
+                        })
+                        .collect();
+                    ca.rename(&name);
+                    ac.with_series(ca.into_series());
+                    Ok(ac)
+                }
+                ApplyOptions::ApplyFlat => {
+                    let s = self.function.call_udf(&mut [ac.flat().into_owned()])?;
+                    ac.with_update_groups(UpdateGroups::WithGroupsLen);
+                    ac.with_series(s);
+                    Ok(ac)
+                }
+                ApplyOptions::ApplyList => {
+                    let s = self
+                        .function
+                        .call_udf(&mut [ac.aggregated().into_owned()])?;
+                    ac.with_series(s);
+                    Ok(ac)
+                }
+            }
+        } else {
+            let mut acs = self.prepare_multiple_inputs(df, groups, state)?;
+
+            match self.collect_groups {
+                ApplyOptions::ApplyGroups => {
+                    let mut container = vec![Default::default(); acs.len()];
+                    let name = acs[0].series().name().to_string();
+
+                    // aggregate representation of the aggregation contexts
+                    // then unpack the lists and finaly create iterators from this list chunked arrays.
+                    let lists = acs
+                        .iter_mut()
+                        .map(|ac| {
+                            let s = ac.aggregated();
+                            s.list().unwrap().clone()
+                        })
+                        .collect::<Vec<_>>();
+                    let mut iters = lists.iter().map(|ca| ca.into_iter()).collect::<Vec<_>>();
+
+                    // length of the items to iterate over
+                    let len = lists[0].len();
+
+                    let mut ca: ListChunked = (0..len)
+                        .map(|_| {
+                            container.clear();
+                            for iter in &mut iters {
+                                match iter.next().unwrap() {
+                                    None => return None,
+                                    Some(s) => container.push(s),
+                                }
+                            }
                             self.function.call_udf(&mut container).ok()
                         })
-                    })
-                    .collect();
-                ca.rename(&name);
-                ac.with_series(ca.into_series());
-                Ok(ac)
-            }
-            ApplyOptions::ApplyFlat => {
-                let s = self.function.call_udf(&mut [ac.flat().into_owned()])?;
-                ac.with_update_groups(UpdateGroups::WithGroupsLen);
-                ac.with_series(s);
-                Ok(ac)
-            }
-            ApplyOptions::ApplyList => {
-                let s = self
-                    .function
-                    .call_udf(&mut [ac.aggregated().into_owned()])?;
-                ac.with_series(s);
-                Ok(ac)
+                        .collect();
+                    ca.rename(&name);
+                    let mut ac = acs.pop().unwrap();
+                    ac.with_series(ca.into_series());
+                    Ok(ac)
+                }
+                ApplyOptions::ApplyFlat => {
+                    let mut s = acs
+                        .iter()
+                        .map(|ac| ac.flat().into_owned())
+                        .collect::<Vec<_>>();
+
+                    let s = self.function.call_udf(&mut s)?;
+                    let mut ac = acs.pop().unwrap();
+                    ac.with_update_groups(UpdateGroups::WithGroupsLen);
+                    ac.with_series(s);
+                    Ok(ac)
+                }
+                ApplyOptions::ApplyList => {
+                    let mut s = acs
+                        .iter_mut()
+                        .map(|ac| ac.aggregated().into_owned())
+                        .collect::<Vec<_>>();
+                    let s = self.function.call_udf(&mut s)?;
+                    let mut ac = acs.pop().unwrap();
+                    ac.with_update_groups(UpdateGroups::WithGroupsLen);
+                    ac.with_series(s);
+                    Ok(ac)
+                }
             }
         }
     }
@@ -97,28 +172,93 @@ impl PhysicalAggregation for ApplyExpr {
         groups: &GroupTuples,
         state: &ExecutionState,
     ) -> Result<Option<Series>> {
-        if self.inputs.len() > 1 {
-            return Err(PolarsError::InvalidOperation(
-                "function with multiple inputs not yet supported in aggregation context".into(),
-            ));
-        }
-        let mut ac = self.inputs[0].evaluate_on_groups(df, groups, state)?;
+        if self.inputs.len() == 1 {
+            let mut ac = self.inputs[0].evaluate_on_groups(df, groups, state)?;
 
-        match self.collect_groups {
-            ApplyOptions::ApplyGroups => {
-                let mut container = vec![Default::default()];
-                let name = ac.series().name().to_string();
+            match self.collect_groups {
+                ApplyOptions::ApplyGroups => {
+                    let mut container = [Default::default()];
+                    let name = ac.series().name().to_string();
 
-                let mut all_unit_length = true;
+                    let mut all_unit_length = true;
 
-                let mut ca: ListChunked = ac
-                    .aggregated()
-                    .list()
-                    .unwrap()
-                    .into_iter()
-                    .map(|opt_s| {
-                        opt_s.and_then(|s| {
-                            container[0] = s;
+                    let mut ca: ListChunked = ac
+                        .aggregated()
+                        .list()
+                        .unwrap()
+                        .into_iter()
+                        .map(|opt_s| {
+                            opt_s.and_then(|s| {
+                                container[0] = s;
+                                let out = self.function.call_udf(&mut container).ok();
+
+                                if let Some(s) = &out {
+                                    if s.len() != 1 {
+                                        all_unit_length = false;
+                                    }
+                                }
+                                out
+                            })
+                        })
+                        .collect();
+                    ca.rename(&name);
+                    if all_unit_length {
+                        return Ok(Some(ca.explode()?));
+                    }
+                    Ok(Some(ca.into_series()))
+                }
+                ApplyOptions::ApplyFlat => {
+                    // the function needs to be called on a flat series
+                    // but the series may be flat or aggregated
+                    // if its flat, we just apply and return
+                    // if not flat, the flattening sorts by group, so we must create new group tuples
+                    // and again aggregate.
+                    let out = self.function.call_udf(&mut [ac.flat().into_owned()]);
+                    if ac.is_flat() {
+                        out.map(Some)
+                    } else {
+                        // TODO! maybe just apply over list?
+                        ac.with_update_groups(UpdateGroups::WithGroupsLen);
+                        Ok(out?.agg_list(ac.groups()))
+                    }
+                }
+                ApplyOptions::ApplyList => self
+                    .function
+                    .call_udf(&mut [ac.aggregated().into_owned()])
+                    .map(Some),
+            }
+        } else {
+            let mut acs = self.prepare_multiple_inputs(df, groups, state)?;
+
+            match self.collect_groups {
+                ApplyOptions::ApplyGroups => {
+                    let mut all_unit_length = true;
+                    let mut container = vec![Default::default(); acs.len()];
+                    let name = acs[0].series().name().to_string();
+
+                    // aggregate representation of the aggregation contexts
+                    // then unpack the lists and finaly create iterators from this list chunked arrays.
+                    let lists = acs
+                        .iter_mut()
+                        .map(|ac| {
+                            let s = ac.aggregated();
+                            s.list().unwrap().clone()
+                        })
+                        .collect::<Vec<_>>();
+                    let mut iters = lists.iter().map(|ca| ca.into_iter()).collect::<Vec<_>>();
+
+                    // length of the items to iterate over
+                    let len = lists[0].len();
+
+                    let mut ca: ListChunked = (0..len)
+                        .map(|_| {
+                            container.clear();
+                            for iter in &mut iters {
+                                match iter.next().unwrap() {
+                                    None => return None,
+                                    Some(s) => container.push(s),
+                                }
+                            }
                             let out = self.function.call_udf(&mut container).ok();
 
                             if let Some(s) = &out {
@@ -128,33 +268,29 @@ impl PhysicalAggregation for ApplyExpr {
                             }
                             out
                         })
-                    })
-                    .collect();
-                ca.rename(&name);
-                if all_unit_length {
-                    return Ok(Some(ca.explode()?));
+                        .collect();
+                    ca.rename(&name);
+                    if all_unit_length {
+                        return Ok(Some(ca.explode()?));
+                    }
+                    Ok(Some(ca.into_series()))
                 }
-                Ok(Some(ca.into_series()))
-            }
-            ApplyOptions::ApplyFlat => {
-                // the function needs to be called on a flat series
-                // but the series may be flat or aggregated
-                // if its flat, we just apply and return
-                // if not flat, the flattening sorts by group, so we must create new group tuples
-                // and again aggregate.
-                let out = self.function.call_udf(&mut [ac.flat().into_owned()]);
-                if ac.is_flat() {
-                    out.map(Some)
-                } else {
-                    // TODO! maybe just apply over list?
-                    ac.with_update_groups(UpdateGroups::WithGroupsLen);
-                    Ok(out?.agg_list(ac.groups()))
+                ApplyOptions::ApplyFlat => {
+                    // the function needs to be called on a flat series
+                    // but the series may be flat or aggregated
+                    // if its flat, we just apply and return
+                    // if not flat, the flattening sorts by group, so we must create new group tuples
+                    // and again aggregate.
+                    panic!("flat apply function with multiple inputs no yet implemented, but likely apply is a better fit anyway ;)")
+                }
+                ApplyOptions::ApplyList => {
+                    let mut s = acs
+                        .iter_mut()
+                        .map(|ac| ac.aggregated().into_owned())
+                        .collect::<Vec<_>>();
+                    self.function.call_udf(&mut s).map(Some)
                 }
             }
-            ApplyOptions::ApplyList => self
-                .function
-                .call_udf(&mut [ac.aggregated().into_owned()])
-                .map(Some),
         }
     }
 }
