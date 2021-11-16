509ca74e60d897c797e900481564ad00c30c1e6e
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2021-11-16 14:54:12 -0700

    Update filter docs

--- py-polars/polars/eager/frame.py
+++ py-polars/polars/eager/frame.py
@@ -1252,6 +1252,39 @@ class DataFrame:
         ----------
         predicate
             Expression that evaluates to a boolean Series.
+
+        Examples
+        --------
+
+        >>> df = pl.DataFrame({
+        >>>     "foo": [1, 2, 3],
+        >>>     "bar": [6, 7, 8],
+        >>>     "ham": ['a', 'b', 'c']
+        >>> })
+        >>> # Filter on one condition
+        >>> df.filter(col("foo") < 3)
+        shape: (2, 3)
+        ┌─────┬─────┬─────┐
+        │ foo ┆ bar ┆ ham │
+        │ --- ┆ --- ┆ --- │
+        │ i64 ┆ i64 ┆ str │
+        ╞═════╪═════╪═════╡
+        │ 1   ┆ 6   ┆ a   │
+        ├╌╌╌╌╌┼╌╌╌╌╌┼╌╌╌╌╌┤
+        │ 2   ┆ 7   ┆ b   │
+        └─────┴─────┴─────┘
+
+        >>>  # Filter on multiple conditions
+        >>> df.filter((col("foo") < 3) & (col("ham") == "a"))
+        shape: (1, 3)
+        ┌─────┬─────┬─────┐
+        │ foo ┆ bar ┆ ham │
+        │ --- ┆ --- ┆ --- │
+        │ i64 ┆ i64 ┆ str │
+        ╞═════╪═════╪═════╡
+        │ 1   ┆ 6   ┆ a   │
+        └─────┴─────┴─────┘
+
         """
         return (
             self.lazy()
