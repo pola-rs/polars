c149cda1933e4eb989cdff493be7d9a9a325a631
Jeroen van Zundert <mail@jeroenvanzundert.nl>
2021-11-17 19:09:25 +0100

    Add missing assert statements test_horizontal_agg

--- py-polars/tests/test_exprs.py
+++ py-polars/tests/test_exprs.py
@@ -4,7 +4,7 @@ import polars as pl
 def test_horizontal_agg(fruits_cars: pl.DataFrame) -> None:
     df = fruits_cars
     out = df.select(pl.max([pl.col("A"), pl.col("B")]))  # type: ignore
-    out[:, 0].to_list() == [5, 4, 3, 4, 5]
+    assert out[:, 0].to_list() == [5, 4, 3, 4, 5]
 
     out = df.select(pl.min([pl.col("A"), pl.col("B")]))  # type: ignore
-    out[:, 0].to_list() == [1, 2, 3, 2, 1]
+    assert out[:, 0].to_list() == [1, 2, 3, 2, 1]
