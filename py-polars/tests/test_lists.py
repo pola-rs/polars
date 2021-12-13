827110412c40e0703f0908de5692118377fb8ae8
Ritchie Vink <ritchie46@gmail.com>
2021-12-13 19:38:27 +0100

    fix filter list with logical types

--- py-polars/tests/test_lists.py
+++ py-polars/tests/test_lists.py
@@ -40,3 +40,29 @@ def test_dtype() -> None:
     a = pl.Series("a", [[1, 2, 3], [2, 5], [6, 7, 8, 9]])
     assert a.dtype == pl.List
     assert a.inner_dtype == pl.Int64
+
+
+def test_categorical() -> None:
+    # https://github.com/pola-rs/polars/issues/2038
+    df = pl.DataFrame(
+        [
+            pl.Series("a", [1, 1, 1, 1, 1, 1, 1, 1]),
+            pl.Series("b", [8, 2, 3, 6, 3, 6, 2, 2]),
+            pl.Series("c", ["a", "b", "c", "a", "b", "c", "a", "b"]).cast(
+                pl.Categorical
+            ),
+        ]
+    )
+    out = (
+        df.groupby(["a", "b"])
+        .agg(
+            [
+                pl.col("c").count().alias("num_different_c"),
+                pl.col("c").alias("c_values"),
+            ]
+        )
+        .filter(pl.col("num_different_c") >= 2)
+        .to_series(3)
+    )
+
+    assert out.inner_dtype == pl.Categorical
