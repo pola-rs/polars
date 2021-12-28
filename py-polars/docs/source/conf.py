59bba167294d95f791bb3b29a5119e0a340f5eb2
Jeroen van Zundert <mail@jeroenvanzundert.nl>
2021-12-28 13:14:27 +0100

    Fix formatting

--- py-polars/docs/source/conf.py
+++ py-polars/docs/source/conf.py
@@ -63,7 +63,7 @@ html_theme = "pydata_sphinx_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ['css/custom.css']  # relative to html_static_path
+html_css_files = ["css/custom.css"]  # relative to html_static_path
 
 html_logo = "../img/polars_logo.png"
 autosummary_generate = True
