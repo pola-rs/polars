b4aab5da1353292bb78d70f6b077d3390e2b46c9
J van Zundert <mail@jeroenvanzundert.nl>
2021-12-29 09:39:18 +0100

    Print each argument on new line in Python docs (#2207)

--- py-polars/docs/source/conf.py
+++ py-polars/docs/source/conf.py
@@ -62,7 +62,8 @@ html_theme = "pydata_sphinx_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]  # relative to html_static_path
 
 html_logo = "../img/polars_logo.png"
 autosummary_generate = True
