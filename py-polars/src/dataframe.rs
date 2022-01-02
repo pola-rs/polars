0af4e76dd5539a9c007658a5d5499cda99cbc04f
Ritchie Vink <ritchie46@gmail.com>
2022-01-02 11:07:02 +0100

    python polars: clippy

--- py-polars/src/dataframe.rs
+++ py-polars/src/dataframe.rs
@@ -747,7 +747,7 @@ impl PyDataFrame {
             // call the lambda and get a python side DataFrame wrapper
             let result_df_wrapper = match lambda.call1(py, (python_df_wrapper,)) {
                 Ok(pyobj) => pyobj,
-                Err(e) => panic!("UDF failed: {}", e.pvalue(py).to_string()),
+                Err(e) => panic!("UDF failed: {}", e.pvalue(py)),
             };
             // unpack the wrapper in a PyDataFrame
             let py_pydf = result_df_wrapper.getattr(py, "_df").expect(
