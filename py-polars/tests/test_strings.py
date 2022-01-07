92cf2d285694f00355bc42b6ef5d3a10409ffdd0
Ritchie Vink <ritchie46@gmail.com>
2022-01-07 21:08:32 +0100

    first check ascii before we check utf8 in lossy encoding

--- py-polars/tests/test_strings.py
+++ py-polars/tests/test_strings.py
@@ -1,6 +1,7 @@
 import polars as pl
 
-def test_extract_binary():
-    df = pl.DataFrame({'foo': ['aron', 'butler', 'charly', 'david']})
-    out = df.filter(pl.col('foo').str.extract('^(a)', 1) == 'a').to_series()
+
+def test_extract_binary() -> None:
+    df = pl.DataFrame({"foo": ["aron", "butler", "charly", "david"]})
+    out = df.filter(pl.col("foo").str.extract("^(a)", 1) == "a").to_series()
     assert out[0] == "aron"
