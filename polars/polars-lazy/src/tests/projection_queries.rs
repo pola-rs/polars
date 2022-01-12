dfe86efd2b220a6683eaee2ab863b3a01302aa49
Ritchie Vink <ritchie46@gmail.com>
2022-01-12 19:55:09 +0100

    extend test to all file formats

--- polars/polars-lazy/src/tests/projection_queries.rs
+++ polars/polars-lazy/src/tests/projection_queries.rs
@@ -35,19 +35,27 @@ fn test_join_suffix_and_drop() -> Result<()> {
 }
 
 #[test]
-fn test_union_and_aggscan() -> Result<()> {
+fn test_union_and_agg_projections() -> Result<()> {
+    init_files();
     // a union vstacks columns and aggscan optimization determines columns to aggregate in a
     // hashmap, if that doesn't set them sorted the vstack will panic.
     let glob = "../../examples/aggregate_multiple_files_in_chunks/datasets/*.parquet";
-    let lf = LazyFrame::scan_parquet(glob.into(), Default::default())?;
+    let lf1 = LazyFrame::scan_parquet(glob.into(), Default::default())?;
+    let glob = "../../examples/aggregate_multiple_files_in_chunks/datasets/*.ipc";
+    let lf2 = LazyFrame::scan_ipc(glob.into(), Default::default())?;
+    let glob = "../../examples/aggregate_multiple_files_in_chunks/datasets/*.csv";
+    let lf3 = LazyCsvReader::new(glob.into()).finish()?;
 
-    let lf = lf.filter(col("category").eq(lit("vegetables"))).select([
-        col("fats_g").sum().alias("sum"),
-        col("fats_g").cast(DataType::Float64).mean().alias("mean"),
-    ]);
+    for lf in [lf1, lf2, lf3] {
+        let lf = lf.filter(col("category").eq(lit("vegetables"))).select([
+            col("fats_g").sum().alias("sum"),
+            col("fats_g").cast(DataType::Float64).mean().alias("mean"),
+            col("fats_g").min().alias("min"),
+        ]);
 
-    let out = lf.collect()?;
-    assert_eq!(out.shape(), (1, 2));
+        let out = lf.collect()?;
+        assert_eq!(out.shape(), (1, 3));
+    }
 
     Ok(())
 }
