345bf0fd13dbb1542854becaac5b30437f936a26
Ritchie Vink <ritchie46@gmail.com>
2022-01-15 10:49:42 +0100

    python polars 0.12.12

--- py-polars/tests/test_queries.py
+++ py-polars/tests/test_queries.py
@@ -10,7 +10,7 @@ def test_sort_by_bools() -> None:
             "ham": ["a", "b", "c"],
         }
     )
-    out = df.with_column((pl.col("foo") % 2 == 1).alias("foo_uneven")).sort(
-        by=["foo", "foo_uneven"]
+    out = df.with_column((pl.col("foo") % 2 == 1).alias("foo_odd")).sort(
+        by=["foo", "foo_odd"]
     )
     assert out.shape == (3, 4)
