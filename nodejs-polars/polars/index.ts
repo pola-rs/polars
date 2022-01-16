8f0f0291957e2121587b93cd930e94226968cebe
universalmind303 <universalmind.candy@gmail.com>
2022-01-16 14:07:51 -0600

    refactor: split implementations from declarations in  io.ts to make code easier to navigate

--- nodejs-polars/polars/index.ts
+++ nodejs-polars/polars/index.ts
@@ -2,7 +2,7 @@ import * as series from "./series";
 import * as df from "./dataframe";
 import { DataType } from "./datatypes";
 import * as func from "./functions";
-import * as io from "./io";
+import io from "./io";
 import * as cfg from "./cfg";
 import {version as _version} from "../package.json";
 
@@ -13,6 +13,7 @@ import  {
   GroupBy as lazyGroupBy,
   when as _when
 } from "./lazy";
+
 
 namespace pl {
   export import Expr = lazyExpr.Expr
