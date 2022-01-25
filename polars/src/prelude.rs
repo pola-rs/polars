a3311a6065f834c6510de0e24b470ded2768f9a8
Ritchie Vink <ritchie46@gmail.com>
2022-01-25 08:19:29 +0100

    expose libs to polars (#2460)

--- polars/src/prelude.rs
+++ polars/src/prelude.rs
@@ -1,7 +1,10 @@
-pub use polars_core::prelude::QuantileInterpolOptions;
+pub use polars_core::frame::groupby::*;
 pub use polars_core::{prelude::*, utils::NoNull};
 #[cfg(feature = "polars-io")]
 pub use polars_io::prelude::*;
 
 #[cfg(feature = "lazy")]
 pub use polars_lazy::prelude::*;
+
+#[cfg(feature = "temporal")]
+pub use polars_core::time::*;
