f3aee748b5f9089e7122e4026e12d6f677e66b02
Ritchie Vink <ritchie46@gmail.com>
2022-02-15 20:38:38 +0100

    csv tests to integration tests

---
+++ polars/tests/it/io/mod.rs
@@ -0,0 +1,8 @@
+mod csv;
+use polars::prelude::*;
+
+pub(crate) fn create_df() -> DataFrame {
+    let s0 = Series::new("days", [0, 1, 2, 3, 4].as_ref());
+    let s1 = Series::new("temp", [22.1, 19.9, 7., 2., 3.].as_ref());
+    DataFrame::new(vec![s0, s1]).unwrap()
+}
