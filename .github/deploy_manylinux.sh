b0e173ac2c5785175a7415ccebd81edab1b12c4b
Ritchie Vink <ritchie46@gmail.com>
2022-02-22 10:46:55 +0100

    python polars 0.13.5

--- .github/deploy_manylinux.sh
+++ .github/deploy_manylinux.sh
@@ -8,7 +8,7 @@ rm py-polars/README.md
 cp README.md py-polars/README.md
 cd py-polars
 rustup override set nightly-2022-01-01
-export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+popcnt'
+export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1+sse4.2,+popcnt,+aes,+avx,+avx2,+bmi1,+bmi2+fma'
 maturin publish \
   --skip-existing \
   --username ritchie46
