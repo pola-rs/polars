dd3b66cbbcd0a67748842d40d6e411a5e494dd2b
Yu Ri Tan <yuri_tan@outlook.com>
2022-03-11 16:54:45 +0100

    rename deprecated feature and formatting

--- polars/polarSQL/src/main.rs
+++ polars/polarSQL/src/main.rs
@@ -4,51 +4,51 @@ use sqlparser::ast;
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
 
-use polars_lazy::dsl;
 use polars_core::prelude::*;
+use polars_lazy::dsl;
 
-
-fn main(){
+fn main() {
     dbg!("Hello World!");
 }
 
 fn extract_projections(items: &[ast::SelectItem]) -> Vec<dsl::Expr> {
     use ast::SelectItem::*;
-    items.iter().map(|si| {
-        match si {
-            Wildcard => {
-                dsl::col("*")
-            }
+    items
+        .iter()
+        .map(|si| match si {
+            Wildcard => dsl::col("*"),
             _ => {
                 todo!()
             }
-        }
-    }) .collect()
+        })
+        .collect()
 }
-
 
 fn extract_files(relation: &ast::TableFactor) -> Vec<&str> {
     use ast::TableFactor::*;
     match relation {
-        Table { name, ..} => {
+        Table { name, .. } => {
             let idents = &name.0;
             assert_eq!(idents.len(), 1, "only 1 table allowed");
             idents.iter().map(|ident| &*ident.value).collect()
-        },
-        _ => todo!()
+        }
+        _ => todo!(),
     }
 }
 
-
 fn run_expressions(projections: &[dsl::Expr], table: &str) -> Result<()> {
-    
     let splitted = table.split(".").collect::<Vec<_>>();
     if splitted.len() == 1 {
-        Err(PolarsError::ComputeError("expected a file with an extension".into()))
+        Err(PolarsError::ComputeError(
+            "expected a file with an extension".into(),
+        ))
     } else {
         match splitted[splitted.len() - 1] {
             "csv" => {
-                let df = LazyCsvReader::new(table.into()).finish()?.select(projections).collect()?;
+                let df = LazyCsvReader::new(table.into())
+                    .finish()?
+                    .select(projections)
+                    .collect()?;
                 println!("ran query in {} ms\n {:?}", 1, df);
             }
             "parquet" => {
@@ -57,16 +57,14 @@ fn run_expressions(projections: &[dsl::Expr], table: &str) -> Result<()> {
             "ipc" | "arrow" => {
                 todo!()
             }
-            ext => panic!("extension {} not yet supported", ext)
+            ext => panic!("extension {} not yet supported", ext),
         }
 
         Ok(())
     }
 }
 
-
 fn parse_query(query: &str) {
-
     let dialect = GenericDialect {}; // or AnsiDialect
     let mut parsed = Parser::parse_sql(&dialect, query).unwrap();
     // still have to decide what to do with with multiple statements.
@@ -75,33 +73,28 @@ fn parse_query(query: &str) {
     let stmt = parsed.pop().unwrap();
 
     match stmt {
-        ast::Statement::Query(query) => {
-            match query.body {
-                ast::SetExpr::Select(select) => {
-                    let projections = extract_projections(&select.projection);
-                
-                    let from_tables = select.from;
-                    assert_eq!(from_tables.len(), 1, "multiple sources not yet supported");
+        ast::Statement::Query(query) => match query.body {
+            ast::SetExpr::Select(select) => {
+                let projections = extract_projections(&select.projection);
 
-                    let relation = &from_tables[0].relation;
-                    let tables = extract_files(relation);
-                    assert_eq!(tables.len(), 1, "multiple sources not yet supported");
+                let from_tables = select.from;
+                assert_eq!(from_tables.len(), 1, "multiple sources not yet supported");
 
-                    run_expressions(&projections, &tables[0]).unwrap();
+                let relation = &from_tables[0].relation;
+                let tables = extract_files(relation);
+                assert_eq!(tables.len(), 1, "multiple sources not yet supported");
 
-                }
-                _ => {
-                    panic!("not yet supported")
-                }
+                run_expressions(&projections, &tables[0]).unwrap();
             }
-        }
+            _ => {
+                panic!("not yet supported")
+            }
+        },
         _ => {
             panic!("not yet supported")
         }
     }
-
 }
-
 
 #[cfg(test)]
 mod test {
@@ -112,5 +105,4 @@ mod test {
         let query = "select * from \"../../examples/aggregate_multiple_files_in_chunks/datasets/foods1.csv\"";
         parse_query(query);
     }
-
 }
