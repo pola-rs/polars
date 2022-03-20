8b4fc752f11a1f6e5a50fefb2d23f73ad2b74a92
Sergey N <sergey.nikonov@aioneers.com>
2022-03-20 12:24:30 +0300

    changed to 5 cols and added output

--- py-polars/polars/cfg.py
+++ py-polars/polars/cfg.py
@@ -68,10 +68,17 @@ class Config:
         Examples
         --------
 
-        >>> import polars as pl
-        >>> pl.cfg.Config.set_tbl_cols(1000)
+        >>> pl.cfg.Config.set_tbl_cols(5)
         >>> df = pl.DataFrame({str(i):[i] for i in range(100)})
         >>> df
+        shape: (1, 100)
+        ┌─────┬─────┬─────┬─────┬─────┬─────┐
+        │ 0   ┆ 1   ┆ 2   ┆ ... ┆ 98  ┆ 99  │
+        │ --- ┆ --- ┆ --- ┆     ┆ --- ┆ --- │
+        │ i64 ┆ i64 ┆ i64 ┆     ┆ i64 ┆ i64 │
+        ╞═════╪═════╪═════╪═════╪═════╪═════╡
+        │ 0   ┆ 1   ┆ 2   ┆ ... ┆ 98  ┆ 99  │
+        └─────┴─────┴─────┴─────┴─────┴─────┘        
         """
 
         os.environ["POLARS_FMT_MAX_COLS"] = str(n)
