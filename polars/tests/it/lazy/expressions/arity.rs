6682ac262e3f6750a9f0264996a4b549719f5e18
Ritchie Vink <ritchie46@gmail.com>
2022-04-02 16:06:17 +0200

    fix when_then_chain containing nulls

--- polars/tests/it/lazy/expressions/arity.rs
+++ polars/tests/it/lazy/expressions/arity.rs
@@ -43,3 +43,37 @@ fn ternary_expand_sizes() -> Result<()> {
     assert_eq!(vals, &["a1", "b2", "otherwise"]);
     Ok(())
 }
+
+#[test]
+#[cfg(feature = "strings")]
+fn includes_null_predicate_3038() -> Result<()> {
+    let df = df! {
+        "a" => [Some("a1"), None, None],
+    }?;
+    let res = df
+        .lazy()
+        .with_column(
+            when(col("a").map(
+                move |s| {
+                    s.utf8()?
+                        .to_lowercase()
+                        .contains("not_exist")
+                        .map(Into::into)
+                },
+                GetOutput::from_type(DataType::Boolean),
+            ))
+            .then(lit("unexpected"))
+            .when(col("a").eq(lit("a1".to_string())))
+            .then(lit("good hit"))
+            .otherwise(Expr::Literal(LiteralValue::Null))
+            .alias("b"),
+        )
+        .collect()?;
+
+    let exp_df = df! {
+        "a" => [Some("a1"), None, None],
+        "b" => [Some("good hit"), None, None],
+    }?;
+    assert!(res.frame_equal_missing(&exp_df));
+    Ok(())
+}
