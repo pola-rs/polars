ee52e4a4e85ad7b6e9419c3f5c8169f0dd4d39b4
Ritchie Vink <ritchie46@gmail.com>
2022-04-04 16:57:26 +0200

    python polars 64 release

--- .github/deploy_manylinux.sh
+++ .github/deploy_manylinux.sh
@@ -9,6 +9,17 @@ cp README.md py-polars/README.md
 cd py-polars
 rustup override set nightly-2022-03-23
 export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1+sse4.2,+popcnt,+avx,+fma'
+
+# first the default release
+maturin publish \
+  --skip-existing \
+  --username ritchie46
+
+# now compile polars with bigidx feature
+sed -i 's/name = "polars"/name = "polars-64"/' pyproject.toml
+# a brittle hack to insert the 'bigidx' feature
+sed '/s/"dynamic_groupby",/"dynamic_groupby",\n"bigidx",/' Cargo.toml
+
 maturin publish \
   --skip-existing \
   --username ritchie46
