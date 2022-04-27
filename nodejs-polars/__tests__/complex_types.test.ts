95c0aaf3b29b30923aca94bd1d74305f233ed4a5
universalmind303 <universalmind.candy@gmail.com>
2022-04-27 12:13:51 -0500

    more updates for 0.5.0

--- nodejs-polars/__tests__/complex_types.test.ts
+++ nodejs-polars/__tests__/complex_types.test.ts
@@ -3,13 +3,13 @@ import pl from "@polars";
 
 describe("complex types", () => {
 
-  test.todo("nested arrays round trip", () => {
+  test.skip("nested arrays round trip", () => {
     const arr = [[["foo"]], [], null];
     const s = pl.Series("", arr);
     const actual = s.toArray();
     expect(actual).toEqual(arr);
   });
-  test.todo("struct arrays round trip", () => {
+  test.skip("struct arrays round trip", () => {
     const arr = [{foo: "a", bar: 1}, null, null];
     const s = pl.Series("", arr);
     const actual = s.toArray();
