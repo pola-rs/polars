0bd1b576da4b0816c1cc73afa4a845abe00c3c3c
Ritchie Vink <ritchie46@gmail.com>
2022-05-04 08:07:52 +0200

    sed inplace (#3296)

--- .github/deploy_manylinux.sh
+++ .github/deploy_manylinux.sh
@@ -9,7 +9,7 @@ rm py-polars/README.md
 cp README.md py-polars/README.md
 cd py-polars
 rustup override set nightly-2022-04-01
-export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1+sse4.2,+popcnt,+avx,+fma'
+export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
 
 # first the default release
 maturin publish \
@@ -19,7 +19,7 @@ maturin publish \
 # now compile polars with bigidx feature
 sed -i 's/name = "polars"/name = "polars-u64-idx"/' pyproject.toml
 # a brittle hack to insert the 'bigidx' feature
-sed 's/"dynamic_groupby",/"dynamic_groupby",\n"bigidx",/' Cargo.toml
+sed -i 's/"dynamic_groupby",/"dynamic_groupby",\n"bigidx",/' Cargo.toml
 
 maturin publish \
   --skip-existing \
