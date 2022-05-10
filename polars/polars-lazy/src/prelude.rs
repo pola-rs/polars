69934c6f1c47620fe8315287fefb2a0c35baaa3b
Fernando Herrera <fernando.j.herrera@gmail.com>
2022-05-10 07:55:33 +0100

    order of attributes

--- polars/polars-lazy/src/prelude.rs
+++ polars/polars-lazy/src/prelude.rs
@@ -2,20 +2,21 @@ pub(crate) use polars_utils::arena::{Arena, Node};
 
 #[cfg(feature = "temporal")]
 pub(crate) use polars_time::in_nanoseconds_window;
-#[cfg(feature = "dynamic_groupby")]
+#[cfg(feature = "dynamic_groupby")] 
 pub(crate) use polars_time::{DynamicGroupOptions, PolarsTemporalGroupby, RollingGroupOptions};
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg(not(feature = "dynamic_groupby"))]
 #[derive(Clone, Debug)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct DynamicGroupOptions {
     pub index_column: String,
 }
+
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[cfg(not(feature = "dynamic_groupby"))]
 #[derive(Clone, Debug)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct RollingGroupOptions {
     pub index_column: String,
 }
