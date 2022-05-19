a73c04c0ffeb84a779608a003810bae908a0d435
Ritchie Vink <ritchie46@gmail.com>
2022-05-19 11:48:37 +0200

    infer dtype of empty list in recursive list construction & fix struct.arr take (#3433)
    * infer dtype of empty list in recursive list construction
    
    * fix struct.arr take

--- py-polars/tests/test_struct.py
+++ py-polars/tests/test_struct.py
@@ -326,3 +326,38 @@ def test_struct_agg_list() -> None:
             [{"x": 3, "y": 300}, {"x": 4, "y": 400}, {"x": 5, "y": 500}],
         ],
     }
+
+
+def test_struct_empty_list_creation() -> None:
+    payload = [[], [{"a": 1, "b": 2}, {"a": 3, "b": 4}, {"a": 5, "b": 6}], []]
+
+    assert pl.DataFrame({"list_struct": payload}).to_dict(False) == {
+        "list_struct": [[], [{"a": 1, "b": 2}, {"a": 3, "b": 4}, {"a": 5, "b": 6}], []]
+    }
+
+    # pop first
+    payload = payload[1:]
+    assert pl.DataFrame({"list_struct": payload}).to_dict(False) == {
+        "list_struct": [[{"a": 1, "b": 2}, {"a": 3, "b": 4}, {"a": 5, "b": 6}], []]
+    }
+
+
+def test_struct_arr_methods() -> None:
+    df = pl.DataFrame(
+        {
+            "list_struct": [
+                [{"a": 1, "b": 2}, {"a": 3, "b": 4}, {"a": 5, "b": 6}],
+                [{"a": 1, "b": 2}, {"a": 3, "b": 4}],
+                [{"a": 1, "b": 2}],
+            ],
+        }
+    )
+    assert df.select([pl.col("list_struct").arr.first()]).to_dict(False) == {
+        "list_struct": [{"a": 1, "b": 2}, {"a": 1, "b": 2}, {"a": 1, "b": 2}]
+    }
+    assert df.select([pl.col("list_struct").arr.last()]).to_dict(False) == {
+        "list_struct": [{"a": 5, "b": 6}, {"a": 3, "b": 4}, {"a": 1, "b": 2}]
+    }
+    assert df.select([pl.col("list_struct").arr.get(0)]).to_dict(False) == {
+        "list_struct": [{"a": 1, "b": 2}, {"a": 1, "b": 2}, {"a": 1, "b": 2}]
+    }
