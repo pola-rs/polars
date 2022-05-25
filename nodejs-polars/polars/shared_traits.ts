764141534c4793eafab97d2dc6c5c87074342f84
r <ryanrussell@users.noreply.github.com>
2022-05-24 22:37:38 -0500

    Fix typos

--- nodejs-polars/polars/shared_traits.ts
+++ nodejs-polars/polars/shared_traits.ts
@@ -269,13 +269,13 @@ export interface Round<T> {
    */
   floor(): T;
   /**
-   * Ceil underlying floating point array to the heighest integers smaller or equal to the float value.
+   * Ceil underlying floating point array to the highest integers smaller or equal to the float value.
    * Only works on floating point Series
    */
   ceil(): T;
 
   /**
-   * Clip (limit) the values in an array to any value that fits in 64 floating poitns range.
+   * Clip (limit) the values in an array to any value that fits in 64 floating point range.
    * Only works for the following dtypes: {Int32, Int64, Float32, Float64, UInt32}.
    * If you want to clip other dtypes, consider writing a when -> then -> otherwise expression
    * @param min Minimum value
