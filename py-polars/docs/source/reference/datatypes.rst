564a3272739665d7a309bff8c84cacbb698042ad
adamgreg <adam@aguilaengineering.com>
2022-05-26 05:21:35 +0100

    Add datatypes and exceptions to Python docs (#3499)

---
+++ py-polars/docs/source/reference/datatypes.rst
@@ -0,0 +1,58 @@
+==========
+Data Types
+==========
+.. currentmodule:: polars.datatypes
+
+.. autosummary::
+    :toctree: api/
+    :nosignatures:
+    
+    DataType
+
+Numeric
+~~~~~~~
+.. autosummary::
+    :toctree: api/
+    :nosignatures:
+
+    Float32
+    Float64
+    Int16
+    Int32
+    Int64
+    Int8
+    UInt16
+    UInt32
+    UInt64
+    UInt8
+
+Date / Time
+~~~~~~~~~~~
+.. autosummary::
+    :toctree: api/
+    :nosignatures:
+
+    Date
+    Datetime
+    Duration
+    Time
+
+Nested
+~~~~~~
+.. autosummary::
+    :toctree: api/
+
+    List
+    Struct
+
+Other
+~~~~~
+.. autosummary::
+    :toctree: api/
+    :nosignatures:
+
+    Boolean
+    Categorical
+    Null
+    Object
+    Utf8
--- py-polars/polars/exceptions.py
+++ py-polars/polars/exceptions.py
@@ -14,27 +14,43 @@ except ImportError:  # pragma: no cover
     # when there is no binary yet
 
     class ArrowError(Exception):  # type: ignore
+        """Exception raised the underlying Arrow library encounters an error"""
+
         pass
 
     class ComputeError(Exception):  # type: ignore
+        """Exception raised when we couldn't finish the computation"""
+
         pass
 
     class NoDataError(Exception):  # type: ignore
+        """Exception raised when an operation can not be performed on an empty data structure"""
+
         pass
 
     class NotFoundError(Exception):  # type: ignore
+        """Exception raised when a specified column is not found"""
+
         pass
 
     class SchemaError(Exception):  # type: ignore
+        """Exception raised when trying to combine data structures with mismatched schemas"""
+
         pass
 
     class ShapeError(Exception):  # type: ignore
+        """Exception raised when trying to combine data structures with incompatible shapes"""
+
         pass
 
     class DuplicateError(Exception):  # type: ignore
+        """Exception raised when a column name is duplicated"""
+
         pass
 
     class PanicException(Exception):  # type: ignore
+        """Exception raised when an unexpected state causes a panic in the underlying Rust library"""
+
         pass
 
 
