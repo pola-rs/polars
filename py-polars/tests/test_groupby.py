2427ad4fe4fe11cd6c264f17ffb10d683b2a77f1
Ritchie Vink <ritchie46@gmail.com>
2022-06-13 20:10:10 +0200

    fix groupby aggregation on empty df (#3688)

--- py-polars/tests/test_groupby.py
+++ py-polars/tests/test_groupby.py
@@ -15,3 +15,23 @@ def test_groupby_sorted_empty_dataframe_3680() -> None:
         .tail(1)
         .collect()
     ).shape == (0, 2)
+
+
+def test_groupby_custom_agg_empty_list() -> None:
+    assert (
+        pl.DataFrame(
+            [
+                pl.Series("key", [], dtype=pl.Categorical),
+                pl.Series("val", [], dtype=pl.Float64),
+            ]
+        )
+        .groupby("key")
+        .agg(
+            [
+                pl.col("val").mean().alias("mean"),
+                pl.col("val").std().alias("std"),
+                pl.col("val").skew().alias("skew"),
+                pl.col("val").kurtosis().alias("kurt"),
+            ]
+        )
+    ).dtypes == [pl.Categorical, pl.Float64, pl.Float64, pl.Float64, pl.Float64]
