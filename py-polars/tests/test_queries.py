pola-rs/polars

commit 74081ac85bbe04f2adbd238c4edd6c3b6562880e

inplace arithmetic (#3709)

polars/polars-lazy/src/physical_plan/expressions/binary.rs
@@ -29,7 +29,36 @@ impl BinaryExpr {
     }
 }
 
-pub(crate) fn apply_operator(left: &Series, right: &Series, op: Operator) -> Result<Series> {
+/// Can partially do operations in place.
+fn apply_operator_owned(left: Series, right: Series, op: Operator) -> Result<Series> {
+    match op {
+        Operator::Gt => ChunkCompare::<&Series>::gt(&left, &right).map(|ca| ca.into_series()),
+        Operator::GtEq => ChunkCompare::<&Series>::gt_eq(&left, &right).map(|ca| ca.into_series()),
+        Operator::Lt => ChunkCompare::<&Series>::lt(&left, &right).map(|ca| ca.into_series()),
+        Operator::LtEq => ChunkCompare::<&Series>::lt_eq(&left, &right).map(|ca| ca.into_series()),
+        Operator::Eq => ChunkCompare::<&Series>::equal(&left, &right).map(|ca| ca.into_series()),
+        Operator::NotEq => {
+            ChunkCompare::<&Series>::not_equal(&left, &right).map(|ca| ca.into_series())
+        }
+        Operator::Plus => Ok(left + right),
+        Operator::Minus => Ok(left - right),
+        Operator::Multiply => Ok(left * right),
+        Operator::Divide => Ok(&left / &right),
+        Operator::TrueDivide => {
+            use DataType::*;
+            match left.dtype() {
+                Date | Datetime(_, _) | Float32 | Float64 => Ok(&left / &right),
+                _ => Ok(&left.cast(&Float64)? / &right.cast(&Float64)?),
+            }
+        }
+        Operator::And => left.bitand(&right),
+        Operator::Or => left.bitor(&right),
+        Operator::Xor => left.bitxor(&right),
+        Operator::Modulus => Ok(&left % &right),
+    }
+}
+
+pub fn apply_operator(left: &Series, right: &Series, op: Operator) -> Result<Series> {
     match op {
         Operator::Gt => ChunkCompare::<&Series>::gt(left, right).map(|ca| ca.into_series()),
         Operator::GtEq => ChunkCompare::<&Series>::gt_eq(left, right).map(|ca| ca.into_series()),
@@ -69,7 +98,7 @@ impl PhysicalExpr for BinaryExpr {
                 || self.right.evaluate(df, state),
             )
         });
-        apply_operator(&lhs?, &rhs?, self.op)
+        apply_operator_owned(lhs?, rhs?, self.op)
     }
 
     #[allow(clippy::ptr_arg)]
@@ -119,7 +148,17 @@ impl PhysicalExpr for BinaryExpr {
                 | Operator::Modulus
                 | Operator::TrueDivide,
             ) => {
-                let out = apply_operator(lhs, rhs, self.op)?;
+                // we want to be able to mutate in place
+                // so we take the lhs to make sure that we drop
+                let lhs = lhs.clone();
+                let rhs = rhs.clone();
+
+                // drop lhs so that we might operate in place
+                {
+                    let _ = ac_l.take();
+                }
+
+                let out = apply_operator_owned(lhs, rhs, self.op)?;
 
                 ac_l.with_series(out, true);
                 Ok(ac_l)
@@ -130,12 +169,16 @@ impl PhysicalExpr for BinaryExpr {
             //
             (AggState::AggregatedFlat(_), AggState::Literal(_), _)
             | (AggState::Literal(_), AggState::AggregatedFlat(_), _) => {
-                let l = ac_l.series();
-                let r = ac_r.series();
-                let mut s = apply_operator(l, r, self.op)?;
-                s.rename(l.name());
+                let l = ac_l.series().clone();
+                let r = ac_r.series().clone();
 
-                ac_l.with_series(s, true);
+                // drop lhs so that we might operate in place
+                {
+                    let _ = ac_l.take();
+                }
+                let out = apply_operator_owned(l, r, self.op)?;
+
+                ac_l.with_series(out, true);
                 Ok(ac_l)
             }
             // One of the two exprs is aggregated with flat aggregation, e.g. `e.min(), e.max(), e.first()`
@@ -252,11 +295,15 @@ impl PhysicalExpr for BinaryExpr {
                 ac_l.sort_by_groups();
                 ac_r.sort_by_groups();
 
-                let out = apply_operator(
-                    ac_l.flat_naive().as_ref(),
-                    ac_r.flat_naive().as_ref(),
-                    self.op,
-                )?;
+                let lhs = ac_l.flat_naive().as_ref().clone();
+                let rhs = ac_r.flat_naive().as_ref().clone();
+
+                // drop lhs so that we might operate in place
+                {
+                    let _ = ac_l.take();
+                }
+
+                let out = apply_operator_owned(lhs, rhs, self.op)?;
 
                 // we flattened the series, so that sorts by group
                 ac_l.with_update_groups(UpdateGroups::WithGroupsLen);
@@ -265,11 +312,15 @@ impl PhysicalExpr for BinaryExpr {
             }
             // flatten the Series and apply the operators
             (AggState::AggregatedList(_), AggState::AggregatedList(_), _) => {
-                let out = apply_operator(
-                    ac_l.flat_naive().as_ref(),
-                    ac_r.flat_naive().as_ref(),
-                    self.op,
-                )?;
+                let lhs = ac_l.flat_naive().as_ref().clone();
+                let rhs = ac_l.flat_naive().as_ref().clone();
+
+                // drop lhs so that we might operate in place
+                {
+                    let _ = ac_l.take();
+                }
+
+                let out = apply_operator_owned(lhs, rhs, self.op)?;
 
                 ac_l.combine_groups(ac_r).with_series(out, false);
                 ac_l.with_update_groups(UpdateGroups::WithGroupsLen);
@@ -297,11 +348,15 @@ impl PhysicalExpr for BinaryExpr {
                     ac_l.with_series(out, false);
                     Ok(ac_l)
                 } else {
-                    let out = apply_operator(
-                        ac_l.flat_naive().as_ref(),
-                        ac_r.flat_naive().as_ref(),
-                        self.op,
-                    )?;
+                    let lhs = ac_l.flat_naive().as_ref().clone();
+                    let rhs = ac_r.flat_naive().as_ref().clone();
+
+                    // drop lhs so that we might operate in place
+                    {
+                        let _ = ac_l.take();
+                    }
+
+                    let out = apply_operator_owned(lhs, rhs, self.op)?;
 
                     ac_l.combine_groups(ac_r).with_series(out, false);
 

polars/polars-arrow/Cargo.toml
@@ -9,9 +9,9 @@ description = "Arrow interfaces for Polars DataFrame library"
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 
 [dependencies]
-arrow = { package = "arrow2", git = "https://github.com/jorgecarleitao/arrow2", rev = "39db6fb7514364bfea08d594793b23e1ed5a7def", features = ["compute_concatenate"], default-features = false }
+# arrow = { package = "arrow2", git = "https://github.com/jorgecarleitao/arrow2", rev = "39db6fb7514364bfea08d594793b23e1ed5a7def", features = ["compute_concatenate"], default-features = false }
 # arrow = { package = "arrow2", path = "../../../arrow2", features = ["compute_concatenate"], default-features = false }
-# arrow = { package = "arrow2", git = "https://github.com/ritchie46/arrow2", branch = "count_shared", features = ["compute_concatenate"], default-features = false }
+arrow = { package = "arrow2", git = "https://github.com/ritchie46/arrow2", branch = "arity_assign", features = ["compute_concatenate"], default-features = false }
 # arrow = { package = "arrow2", version = "0.12", default-features = false, features = ["compute_concatenate"] }
 hashbrown = "0.12"
 num = "^0.4"

polars/polars-core/Cargo.toml
@@ -19,7 +19,7 @@ default = ["docs", "temporal", "private"]
 lazy = ["sort_multiple"]
 
 # ~40% faster collect, needed until trustedlength iter stabilizes
-# more fast paths
+# more fast paths, slower compilation
 performant = []
 
 # extra utilities for Utf8Chunked
@@ -171,11 +171,11 @@ thiserror = "^1.0"
 
 [dependencies.arrow]
 package = "arrow2"
-git = "https://github.com/jorgecarleitao/arrow2"
-# git = "https://github.com/ritchie46/arrow2"
-rev = "39db6fb7514364bfea08d594793b23e1ed5a7def"
+# git = "https://github.com/jorgecarleitao/arrow2"
+git = "https://github.com/ritchie46/arrow2"
+# rev = "39db6fb7514364bfea08d594793b23e1ed5a7def"
 # path = "../../../arrow2"
-# branch = "count_shared"
+branch = "arity_assign"
 # version = "0.12"
 default-features = false
 features = [

polars/polars-core/src/chunked_array/arithmetic.rs
@@ -1,8 +1,11 @@
 //! Implementations of arithmetic operations on ChunkedArray's.
 use crate::prelude::*;
-use crate::utils::align_chunks_binary;
+use crate::utils::{align_chunks_binary, align_chunks_binary_owned};
 use arrow::array::PrimitiveArray;
-use arrow::{compute, compute::arithmetics::basic};
+use arrow::{
+    compute,
+    compute::{arithmetics::basic, arity_assign},
+};
 use num::{Num, NumCast, ToPrimitive};
 use std::borrow::Cow;
 use std::ops::{Add, Div, Mul, Rem, Sub};
@@ -93,6 +96,52 @@ where
     ca
 }
 
+/// This assigns to the owned buffer if the ref count is 1
+fn arithmetic_helper_owned<T, Kernel, F>(
+    mut lhs: ChunkedArray<T>,
+    mut rhs: ChunkedArray<T>,
+    kernel: Kernel,
+    operation: F,
+) -> ChunkedArray<T>
+where
+    T: PolarsNumericType,
+    Kernel: Fn(&mut PrimitiveArray<T::Native>, &mut PrimitiveArray<T::Native>),
+    F: Fn(T::Native, T::Native) -> T::Native,
+{
+    let ca = match (lhs.len(), rhs.len()) {
+        (a, b) if a == b => {
+            let (mut lhs, mut rhs) = align_chunks_binary_owned(lhs, rhs);
+            lhs.downcast_iter_mut()
+                .zip(rhs.downcast_iter_mut())
+                .for_each(|(lhs, rhs)| kernel(lhs, rhs));
+            lhs
+        }
+        // broadcast right path
+        (_, 1) => {
+            let opt_rhs = rhs.get(0);
+            match opt_rhs {
+                None => ChunkedArray::full_null(lhs.name(), lhs.len()),
+                Some(rhs) => {
+                    lhs.apply_mut(|lhs| operation(lhs, rhs));
+                    lhs
+                }
+            }
+        }
+        (1, _) => {
+            let opt_lhs = lhs.get(0);
+            match opt_lhs {
+                None => ChunkedArray::full_null(lhs.name(), rhs.len()),
+                Some(lhs) => {
+                    rhs.apply_mut(|rhs| operation(lhs, rhs));
+                    rhs
+                }
+            }
+        }
+        _ => panic!("Cannot apply operation on arrays of different lengths"),
+    };
+    ca
+}
+
 // Operands on ChunkedArray & ChunkedArray
 
 impl<T> Add for &ChunkedArray<T>
@@ -157,7 +206,12 @@ where
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        (&self).add(&rhs)
+        arithmetic_helper_owned(
+            self,
+            rhs,
+            |a, b| arity_assign::binary(a, b, |a, b| a + b),
+            |lhs, rhs| lhs + rhs,
+        )
     }
 }
 
@@ -168,7 +222,12 @@ where
     type Output = Self;
 
     fn div(self, rhs: Self) -> Self::Output {
-        (&self).div(&rhs)
+        arithmetic_helper_owned(
+            self,
+            rhs,
+            |a, b| arity_assign::binary(a, b, |a, b| a / b),
+            |lhs, rhs| lhs / rhs,
+        )
     }
 }
 
@@ -179,7 +238,12 @@ where
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        (&self).mul(&rhs)
+        arithmetic_helper_owned(
+            self,
+            rhs,
+            |a, b| arity_assign::binary(a, b, |a, b| a * b),
+            |lhs, rhs| lhs * rhs,
+        )
     }
 }
 
@@ -190,7 +254,12 @@ where
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        (&self).sub(&rhs)
+        arithmetic_helper_owned(
+            self,
+            rhs,
+            |a, b| arity_assign::binary(a, b, |a, b| a - b),
+            |lhs, rhs| lhs - rhs,
+        )
     }
 }
 
@@ -279,8 +348,14 @@ where
 {
     type Output = ChunkedArray<T>;
 
-    fn add(self, rhs: N) -> Self::Output {
-        (&self).add(rhs)
+    fn add(mut self, rhs: N) -> Self::Output {
+        if std::env::var("ASSIGN").is_ok() {
+            let adder: T::Native = NumCast::from(rhs).unwrap();
+            self.apply_mut(|val| val + adder);
+            self
+        } else {
+            (&self).add(rhs)
+        }
     }
 }
 
@@ -291,8 +366,14 @@ where
 {
     type Output = ChunkedArray<T>;
 
-    fn sub(self, rhs: N) -> Self::Output {
-        (&self).sub(rhs)
+    fn sub(mut self, rhs: N) -> Self::Output {
+        if std::env::var("ASSIGN").is_ok() {
+            let subber: T::Native = NumCast::from(rhs).unwrap();
+            self.apply_mut(|val| val - subber);
+            self
+        } else {
+            (&self).sub(rhs)
+        }
     }
 }
 
@@ -315,8 +396,14 @@ where
 {
     type Output = ChunkedArray<T>;
 
-    fn mul(self, rhs: N) -> Self::Output {
-        (&self).mul(rhs)
+    fn mul(mut self, rhs: N) -> Self::Output {
+        if std::env::var("ASSIGN").is_ok() {
+            let multiplier: T::Native = NumCast::from(rhs).unwrap();
+            self.apply_mut(|val| val * multiplier);
+            self
+        } else {
+            (&self).mul(rhs)
+        }
     }
 }
 

polars/polars-core/src/chunked_array/ops/apply.rs
@@ -106,6 +106,16 @@ impl<T: PolarsNumericType> ChunkedArray<T> {
     }
 }
 
+impl<T: PolarsNumericType> ChunkedArray<T> {
+    pub(crate) fn apply_mut<F>(&mut self, f: F)
+    where
+        F: Fn(T::Native) -> T::Native + Copy,
+    {
+        self.downcast_iter_mut()
+            .for_each(|arr| arrow::compute::arity_assign::unary(arr, f));
+    }
+}
+
 impl<'a, T> ChunkApply<'a, T::Native, T::Native> for ChunkedArray<T>
 where
     T: PolarsNumericType,

polars/polars-core/src/chunked_array/ops/downcast.rs
@@ -51,6 +51,18 @@ where
             unsafe { &*(arr as *const dyn Array as *const PrimitiveArray<T::Native>) }
         })
     }
+
+    pub(crate) fn downcast_iter_mut(
+        &mut self,
+    ) -> impl Iterator<Item = &mut PrimitiveArray<T::Native>> + DoubleEndedIterator {
+        self.chunks.iter_mut().map(|arr| {
+            // Safety:
+            // This should be the array type in PolarsNumericType
+            let arr = &mut **arr;
+            unsafe { &mut *(arr as *mut dyn Array as *mut PrimitiveArray<T::Native>) }
+        })
+    }
+
     pub fn downcast_chunks(&self) -> Chunks<'_, PrimitiveArray<T::Native>> {
         Chunks::new(&self.chunks)
     }

polars/polars-core/src/series/arithmetic/borrowed.rs
@@ -1,9 +1,4 @@
-use crate::prelude::*;
-use crate::utils::{get_supertype, get_time_units};
-use num::{Num, NumCast};
-use std::borrow::Cow;
-use std::fmt::Debug;
-use std::ops;
+use super::*;
 
 pub trait NumOpsDispatch: Debug {
     fn subtract(&self, rhs: &Series) -> Result<Series> {

polars/polars-core/src/series/arithmetic/mod.rs
@@ -1,0 +1,11 @@
+mod borrowed;
+mod owned;
+
+use crate::prelude::*;
+use crate::utils::{get_supertype, get_time_units};
+use num::{Num, NumCast};
+use std::borrow::Cow;
+use std::fmt::Debug;
+use std::ops::{self, Add, Div, Mul, Sub};
+
+pub use borrowed::*;

commit 85f8a900087200a1e97cd1590f1890940b15c695

fix aritmetic bug introduced in #3709

polars/polars-lazy/src/physical_plan/expressions/binary.rs
@@ -313,7 +313,7 @@ impl PhysicalExpr for BinaryExpr {
             // flatten the Series and apply the operators
             (AggState::AggregatedList(_), AggState::AggregatedList(_), _) => {
                 let lhs = ac_l.flat_naive().as_ref().clone();
-                let rhs = ac_l.flat_naive().as_ref().clone();
+                let rhs = ac_r.flat_naive().as_ref().clone();
 
                 // drop lhs so that we might operate in place
                 {

py-polars/tests/test_queries.py
@@ -183,3 +183,28 @@ def test_groupby_agg_equals_zero_3535() -> None:
         "val1": [10, 0, -99],
         "val2": [None, 0.0, 10.5],
     }
+
+
+def test_arithmetic_in_aggregation_3739() -> None:
+    def demean_dot() -> pl.Expr:
+        x = pl.col("x")
+        y = pl.col("y")
+        x1 = x - x.mean()
+        y1 = y - y.mean()
+        return (x1 * y1).sum().alias("demean_dot")
+
+    assert (
+        pl.DataFrame(
+            {
+                "key": ["a", "a", "a", "a"],
+                "x": [4, 2, 2, 4],
+                "y": [2, 0, 2, 0],
+            }
+        )
+        .groupby("key")
+        .agg(
+            [
+                demean_dot(),
+            ]
+        )
+    ).to_dict(False) == {"key": ["a"], "demean_dot": [0.0]}