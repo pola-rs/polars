442fbd2a331179745facefd43d22e04dca2cf5a6
Cory Grinstead <cory.grinstead@trydatabook.com>
2022-06-21 11:38:19 -0500

    reexport polars for extension libraries

--- nodejs-polars/src/lib.rs
+++ nodejs-polars/src/lib.rs
@@ -1,8 +1,6 @@
 #![allow(clippy::nonstandard_macro_braces)] // needed because clippy does not understand proc macro of pyo3
 #![allow(clippy::transmute_undefined_repr)]
 
-extern crate core;
-extern crate polars;
 
 #[macro_use]
 extern crate napi_derive;
@@ -30,3 +28,11 @@ pub mod list_construction;
 pub mod prelude;
 pub mod series;
 pub mod utils;
+pub use polars_core;
+pub mod export {
+    pub use crate::dataframe::JsDataFrame;
+    pub use crate::lazy::dataframe::JsLazyFrame;
+    pub use polars::prelude::LazyFrame;
+    pub use polars;
+    pub use polars_core;
+}
