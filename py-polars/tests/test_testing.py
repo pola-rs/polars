cad1cc841e33c95ffdaa0b0e1060ec22fd35ef76
Alexander Beedie <alexander.m.beedie@icloud.com>
2022-07-01 00:39:20 +0400

    fix test expressions

--- py-polars/tests/test_testing.py
+++ py-polars/tests/test_testing.py
@@ -246,9 +246,9 @@ def test_strategy_null_probability(
     for obj in (s, df1, df2, df3):
         assert len(obj) == 50  # type: ignore[arg-type]
 
-    assert s.null_count() < df1.null_count().fold(sum)
-    assert df1.null_count().fold(sum) < df2.null_count().fold(sum)
-    assert df2.null_count().fold(sum) < df3.null_count().fold(sum)
+    assert s.null_count() < df1.null_count().fold(sum).sum()
+    assert df1.null_count().fold(sum).sum() < df2.null_count().fold(sum).sum()
+    assert df2.null_count().fold(sum).sum() < df3.null_count().fold(sum).sum()
 
     nulls_col0, nulls_col1 = df2.null_count().rows()[0]
     assert nulls_col0 > nulls_col1
