2af51a7428f60913f13205b7ff1677ca06659d01
Stijn de Gooijer <stijn@degooijer.io>
2022-07-01 15:40:52 +0200

    Add error ignore type to classes

--- py-polars/polars/exceptions.py
+++ py-polars/polars/exceptions.py
@@ -13,28 +13,28 @@ except ImportError:  # pragma: no cover
     # They are only redefined for documentation purposes
     # when there is no binary yet
 
-    class ArrowError(Exception):  # type: ignore
+    class ArrowError(Exception):  # type: ignore[no-redef]
         """Exception raised the underlying Arrow library encounters an error"""
 
-    class ComputeError(Exception):  # type: ignore
+    class ComputeError(Exception):  # type: ignore[no-redef]
         """Exception raised when we couldn't finish the computation"""
 
-    class NoDataError(Exception):  # type: ignore
+    class NoDataError(Exception):  # type: ignore[no-redef]
         """Exception raised when an operation can not be performed on an empty data structure"""
 
-    class NotFoundError(Exception):  # type: ignore
+    class NotFoundError(Exception):  # type: ignore[no-redef]
         """Exception raised when a specified column is not found"""
 
-    class SchemaError(Exception):  # type: ignore
+    class SchemaError(Exception):  # type: ignore[no-redef]
         """Exception raised when trying to combine data structures with mismatched schemas"""
 
-    class ShapeError(Exception):  # type: ignore
+    class ShapeError(Exception):  # type: ignore[no-redef]
         """Exception raised when trying to combine data structures with incompatible shapes"""
 
-    class DuplicateError(Exception):  # type: ignore
+    class DuplicateError(Exception):  # type: ignore[no-redef]
         """Exception raised when a column name is duplicated"""
 
-    class PanicException(Exception):  # type: ignore
+    class PanicException(Exception):  # type: ignore[no-redef]
         """Exception raised when an unexpected state causes a panic in the underlying Rust library"""
 
 
