8b8630b98551e0e23cf88d11bf387cecf5af41b2
Ritchie Vink <ritchie46@gmail.com>
2022-07-06 11:05:57 +0200

    fix dataframe explode with empty lists (#3916)

--- py-polars/tests/test_explode.py
+++ py-polars/tests/test_explode.py
@@ -11,3 +11,19 @@ def test_explode_empty_df_3402() -> None:
 def test_explode_empty_df_3460() -> None:
     df = pl.DataFrame({"a": pa.array([[]], type=pa.large_list(pa.int32()))})
     assert df.explode("a").dtypes == [pl.Int32]
+
+
+def test_explode_empty_df_3902() -> None:
+    df = pl.DataFrame(
+        {
+            "first": [1, 2, 3, 4, 5],
+            "second": [["a"], [], ["b", "c"], [], ["d", "f", "g"]],
+        }
+    )
+    expected = pl.DataFrame(
+        {
+            "first": [1, 2, 3, 3, 4, 5, 5, 5],
+            "second": ["a", None, "b", "c", None, "d", "f", "g"],
+        }
+    )
+    assert df.explode("second").frame_equal(expected)
