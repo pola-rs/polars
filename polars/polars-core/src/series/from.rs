e5193308a6babf6469b3d99816218ec41f5be166
Ritchie Vink <ritchie46@gmail.com>
2022-07-18 09:08:25 +0200

    recursively convert arrow logical types in to_arrow

--- polars/polars-core/src/series/from.rs
+++ polars/polars-core/src/series/from.rs
@@ -93,7 +93,7 @@ impl Series {
                 let chunks = cast_chunks(&chunks, &DataType::Utf8, false).unwrap();
                 Ok(Utf8Chunked::from_chunks(name, chunks).into_series())
             }
-            ArrowDataType::List(_) => {
+            ArrowDataType::List(_) | ArrowDataType::LargeList(_) => {
                 let chunks = chunks.iter().map(convert_inner_types).collect();
                 Ok(ListChunked::from_chunks(name, chunks).into_series())
             }
@@ -176,10 +176,6 @@ impl Series {
                     ArrowTimeUnit::Microsecond => &s * 1_000,
                     ArrowTimeUnit::Nanosecond => s,
                 })
-            }
-            ArrowDataType::LargeList(_) => {
-                let chunks = chunks.iter().map(convert_inner_types).collect();
-                Ok(ListChunked::from_chunks(name, chunks).into_series())
             }
             ArrowDataType::Null => {
                 // we don't support null types yet so we use a small digit type filled with nulls
