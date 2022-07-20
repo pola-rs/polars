c4836c5bc4491d521b50fe73620e6e0512e95d21
Ritchie Vink <ritchie46@gmail.com>
2022-07-20 09:41:55 +0200

    dataframe display: use POLARS_FMT_STR_LEN

---
+++ polars/polars-core/src/config.rs
@@ -0,0 +1,3 @@
+pub(crate) const FMT_STR_LEN: &str = "POLARS_FMT_STR_LEN";
+pub(crate) const FMT_MAX_COLS: &str = "POLARS_FMT_MAX_COLS";
+pub(crate) const FMT_MAX_ROWS: &str = "POLARS_FMT_MAX_ROWS";
