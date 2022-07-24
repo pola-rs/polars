1e2fbd92c9becbe8a76c6c99c287d3d2bd2e934e
Ritchie Vink <ritchie46@gmail.com>
2022-07-24 17:38:44 +0200

    Mypy disallow untyped calls (#4140)

--- py-polars/tests/test_df.py
+++ py-polars/tests/test_df.py
@@ -1096,13 +1096,13 @@ def test_hashing_on_python_objects() -> None:
     df = pl.DataFrame({"a": [1, 1, 3, 4], "b": [1, 1, 2, 2]})
 
     class Foo:
-        def __init__(self):  # type: ignore[no-untyped-def]
+        def __init__(self) -> None:
             pass
 
-        def __hash__(self):  # type: ignore[no-untyped-def]
+        def __hash__(self) -> int:
             return 0
 
-        def __eq__(self, other):  # type: ignore[no-untyped-def]
+        def __eq__(self, other: Any) -> bool:
             return True
 
     df = df.with_column(pl.col("a").apply(lambda x: Foo()).alias("foo"))
@@ -1304,10 +1304,10 @@ def test_transpose() -> None:
 
 def test_extension() -> None:
     class Foo:
-        def __init__(self, value):  # type: ignore[no-untyped-def]
+        def __init__(self, value: Any) -> None:
             self.value = value
 
-        def __repr__(self):  # type: ignore[no-untyped-def]
+        def __repr__(self) -> str:
             return f"foo({self.value})"
 
     foos = [Foo(1), Foo(2), Foo(3)]
