ba400f4f4946da57a4d07626f508d5828b02e5d2
Ritchie Vink <ritchie46@gmail.com>
2022-07-26 09:18:02 +0200

    pivot: fix logical type of multiple indexes (#4159)

--- py-polars/tests/test_pivot.py
+++ py-polars/tests/test_pivot.py
@@ -75,6 +75,19 @@ def test_pivot_categorical_index() -> None:
         columns=[("A", pl.Categorical), ("B", pl.Categorical)],
     )
 
-    assert df.pivot(values="B", index=["A"], columns="B", aggregate_fn="count").to_dict(
-        False
-    ) == {"A": ["Fire", "Water"], "Car": [1, 2], "Ship": [1, None]}
+    df = pl.DataFrame(
+        {
+            "A": ["Fire", "Water", "Water", "Fire"],
+            "B": ["Car", "Car", "Car", "Ship"],
+            "C": ["Paper", "Paper", "Paper", "Paper"],
+        },
+        columns=[("A", pl.Categorical), ("B", pl.Categorical), ("C", pl.Categorical)],
+    )
+    assert df.pivot(
+        values="B", index=["A", "C"], columns="B", aggregate_fn="count"
+    ).to_dict(False) == {
+        "A": ["Fire", "Water"],
+        "C": ["Paper", "Paper"],
+        "Car": [1, 2],
+        "Ship": [1, None],
+    }
