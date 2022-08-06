ba38b2810a18eddccb3529d94f9570f6bfe2f9db
Stijn de Gooijer <stijn@degooijer.io>
2022-08-06 21:18:12 +0200

    Fix errors

--- py-polars/src/conversion.rs
+++ py-polars/src/conversion.rs
@@ -304,11 +304,16 @@ impl FromPyObject<'_> for Wrap<ClosedWindow> {
     fn extract(ob: &'_ PyAny) -> PyResult<Self> {
         let s = ob.extract::<&str>()?;
         Ok(Wrap(match s {
-            "none" => ClosedWindow::None,
-            "both" => ClosedWindow::Both,
             "left" => ClosedWindow::Left,
             "right" => ClosedWindow::Right,
-            _ => panic!("{}", "closed should be any of {'none', 'left', 'right'}"),
+            "both" => ClosedWindow::Both,
+            "none" => ClosedWindow::None,
+            e => {
+                return Err(PyValueError::new_err(format!(
+                    "closed must be one of {{'left', 'right', 'both', 'none'}}, got {}",
+                    e,
+                )))
+            }
         }))
     }
 }
