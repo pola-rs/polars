d63c8844db94b9027becf2c0136362a392bc2b0a
Ritchie Vink <ritchie46@gmail.com>
2022-08-14 08:37:33 +0200

    fix[rust]: melt default value name was wrong in predicate pushdown (#4404)

--- py-polars/tests/test_filter.py
+++ py-polars/tests/test_filter.py
@@ -9,3 +9,20 @@ def test_simplify_expression_lit_true_4376() -> None:
     assert df.lazy().filter((pl.col("column_0") == 1) | pl.lit(True)).collect(
         simplify_expression=True
     ).shape == (3, 3)
+
+
+def test_melt_values_predicate_pushdown() -> None:
+    lf = pl.DataFrame(
+        {
+            "id": [1],
+            "asset_key_1": ["123"],
+            "asset_key_2": ["456"],
+            "asset_key_3": ["abc"],
+        }
+    ).lazy()
+
+    assert (
+        lf.melt("id", ["asset_key_1", "asset_key_2", "asset_key_3"])
+        .filter(pl.col("value") == pl.lit("123"))
+        .collect()
+    ).to_dict(False) == {"id": [1], "variable": ["asset_key_1"], "value": ["123"]}
