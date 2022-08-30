748b79b9314e11f299b23d3d95405264960b4cd4
Jeroen van Zundert <mail@jeroenvanzundert.nl>
2022-08-30 22:45:20 +0200

    fix[python]: Add pytz to show_versions

--- py-polars/polars/show_versions.py
+++ py-polars/polars/show_versions.py
@@ -48,7 +48,15 @@ def show_versions() -> None:
 
 def _get_dependency_info() -> dict[str, str]:
     # see the list of dependencies in pyproject.toml
-    opt_deps = ["pyarrow", "pandas", "numpy", "fsspec", "connectorx", "xlsx2csv"]
+    opt_deps = [
+        "pyarrow",
+        "pandas",
+        "numpy",
+        "fsspec",
+        "connectorx",
+        "xlsx2csv",
+        "pytz",
+    ]
     return {name: _get_dep_version(name) for name in opt_deps}
 
 
