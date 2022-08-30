f685e6adc6da92732e55ed192f1d2951ab12a910
Stijn de Gooijer <stijn@degooijer.io>
2022-08-30 04:49:42 +0200

    Fix doctest

--- py-polars/polars/internals/expr/expr.py
+++ py-polars/polars/internals/expr/expr.py
@@ -5400,7 +5400,7 @@ class Expr:
         Examples
         --------
         >>> df = pl.DataFrame({"a": [1, 2, 3]})
-        >>> df.select(pl.col("a").sample(seed=1))
+        >>> df.select(pl.col("a").sample(frac=1.0, with_replacement=True, seed=1))
         shape: (3, 1)
         ┌─────┐
         │ a   │
