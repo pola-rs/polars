f94d2c0f42d9f33ae16cd6ee3cacfdcb4d403088
Ritchie Vink <ritchie46@gmail.com>
2022-08-31 13:33:55 +0200

    fix[rust]: fix is_in empty list

--- polars/polars-lazy/src/dsl/function_expr/is_in.rs
+++ polars/polars-lazy/src/dsl/function_expr/is_in.rs
@@ -3,6 +3,7 @@ use super::*;
 pub(super) fn is_in(s: &mut [Series]) -> Result<Series> {
     let left = &s[0];
     let other = &s[1];
+    dbg!(left, other);
 
     left.is_in(other).map(|ca| ca.into_series())
 }
