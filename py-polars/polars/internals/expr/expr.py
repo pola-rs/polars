a61436da972d9d6daa8a03b295bbb518e116fa1c
Stijn de Gooijer <stijn@degooijer.io>
2022-08-31 11:55:32 +0200

    Update Expr.sample

--- py-polars/polars/internals/expr/expr.py
+++ py-polars/polars/internals/expr/expr.py
@@ -7,7 +7,6 @@ from datetime import date, datetime, time, timedelta
 from typing import TYPE_CHECKING, Any, Callable, NoReturn, Sequence, cast
 from warnings import warn
 
-from polars import internals as pli
 from polars.datatypes import (
     DataType,
     PolarsDataType,
@@ -24,6 +23,8 @@ from polars.internals.expr.meta import ExprMetaNameSpace
 from polars.internals.expr.string import ExprStringNameSpace
 from polars.internals.expr.struct import ExprStructNameSpace
 from polars.utils import _timedelta_to_pl_duration, accessor, deprecated_alias
+
+from polars import internals as pli
 
 try:
     from polars.polars import PyExpr
@@ -5540,20 +5541,22 @@ class Expr:
             seed = random.randint(0, 10000)
         return wrap_expr(self._pyexpr.shuffle(seed))
 
-    @deprecated_alias(fraction="frac")
     def sample(
         self,
+        n: int | None = None,
         frac: float | None = None,
-        with_replacement: bool = True,
+        with_replacement: bool = False,
         shuffle: bool = False,
         seed: int | None = None,
-        n: int | None = None,
     ) -> Expr:
         """
         Sample from this expression.
 
         Parameters
         ----------
+        n
+            Number of items to return. Cannot be used with `frac`. Defaults to 1 if
+            `frac` is None.
         frac
             Fraction of items to return. Cannot be used with `n`.
         with_replacement
@@ -5563,8 +5566,6 @@ class Expr:
         seed
             Seed for the random number generator. If set to None (default), a random
             seed is used.
-        n
-            Number of items to return. Cannot be used with `frac`.
 
         Examples
         --------
@@ -5584,25 +5585,17 @@ class Expr:
         └─────┘
 
         """
-        warn(
-            "The function signature for Expr.sample will change in a future"
-            " version. Explicitly set `frac` and `with_replacement` using keyword"
-            " arguments to retain the same behaviour.",
-            FutureWarning,
-            stacklevel=2,
-        )
-
         if n is not None and frac is not None:
             raise ValueError("cannot specify both `n` and `frac`")
 
-        if n is not None and frac is None:
-            return wrap_expr(self._pyexpr.sample_n(n, with_replacement, shuffle, seed))
+        if frac is not None:
+            return wrap_expr(
+                self._pyexpr.sample_frac(frac, with_replacement, shuffle, seed)
+            )
 
-        if frac is None:
-            frac = 1.0
-        return wrap_expr(
-            self._pyexpr.sample_frac(frac, with_replacement, shuffle, seed)
-        )
+        if n is None:
+            n = 1
+        return wrap_expr(self._pyexpr.sample_n(n, with_replacement, shuffle, seed))
 
     def ewm_mean(
         self,
