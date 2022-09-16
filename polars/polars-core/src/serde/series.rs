eac935ad0ec6223b73759d3c0882e1f69398e017
Ray Zhang <rzhang@hudson-trading.com>
2022-09-16 02:23:08 -0400

    feat[rust]: serde for `Duration` type (#4867)

--- polars/polars-core/src/serde/series.rs
+++ polars/polars-core/src/serde/series.rs
@@ -50,6 +50,11 @@ impl Serialize for Series {
                     let ca = self.categorical().unwrap();
                     ca.serialize(serializer)
                 }
+                #[cfg(feature = "dtype-duration")]
+                DataType::Duration(_) => {
+                    let ca = self.duration().unwrap();
+                    ca.serialize(serializer)
+                }
                 _ => {
                     // cast small integers to i32
                     self.cast(&DataType::Int32).unwrap().serialize(serializer)
@@ -152,6 +157,13 @@ impl<'de> Deserialize<'de> for Series {
                         let values: Vec<Option<i64>> = map.next_value()?;
                         Ok(Series::new(&name, values)
                             .cast(&DataType::Datetime(tu, tz))
+                            .unwrap())
+                    }
+                    #[cfg(feature = "dtype-duration")]
+                    DeDataType::Duration(tu) => {
+                        let values: Vec<Option<i64>> = map.next_value()?;
+                        Ok(Series::new(&name, values)
+                            .cast(&DataType::Duration(tu))
                             .unwrap())
                     }
                     DeDataType::Boolean => {
