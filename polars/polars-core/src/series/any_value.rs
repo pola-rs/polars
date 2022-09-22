fa87cd6f35fcc85480b7e9543b40e2f50de6706a
Özgür Akkurt <oezgurmakkurt@gmail.com>
2022-09-22 18:05:32 +0300

    fix DataType from AnyValue

--- polars/polars-core/src/series/any_value.rs
+++ polars/polars-core/src/series/any_value.rs
@@ -153,7 +153,7 @@ impl<'a> From<&AnyValue<'a>> for DataType {
             Null => DataType::Null,
             Boolean(_) => DataType::Boolean,
             Utf8(_) | Utf8Owned(_) => DataType::Utf8,
-            Binary(_) | BinaryOwned(_) => DataType::Utf8,
+            Binary(_) | BinaryOwned(_) => DataType::Binary,
             UInt32(_) => DataType::UInt32,
             UInt64(_) => DataType::UInt64,
             Int32(_) => DataType::Int32,
