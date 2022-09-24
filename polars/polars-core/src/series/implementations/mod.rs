d12be1cd2732bf284beeb251ef65ef0dd4ae75bc
Ritchie Vink <ritchie46@gmail.com>
2022-09-24 12:18:45 +0200

    refactor[rust]: remove agg_median from series trait (#4963)

--- polars/polars-core/src/series/implementations/mod.rs
+++ polars/polars-core/src/series/implementations/mod.rs
@@ -179,9 +179,6 @@ macro_rules! impl_dyn_series {
                 self.0.agg_quantile(groups, quantile, interpol)
             }
 
-            unsafe fn agg_median(&self, groups: &GroupsProxy) -> Series {
-                self.0.agg_median(groups)
-            }
             fn zip_outer_join_column(
                 &self,
                 right_column: &Series,
