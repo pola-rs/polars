dfcddfd07a716d3ee6ad1c238e812daf61761798
Oscar Riordan <o.w.riordan@gmail.com>
2022-10-10 13:41:38 +0100

    Correct logic.

--- polars/polars-time/src/windows/window.rs
+++ polars/polars-time/src/windows/window.rs
@@ -56,23 +56,21 @@ impl Window {
 
     /// Round the given ns timestamp by the window boundary.
     pub fn round_ns(&self, t: i64) -> i64 {
-        let t = t + self.every.nanoseconds() / 2 as i64;
+        let t = t + self.every.nanoseconds() / 2_i64;
         self.truncate_ns(t)
     }
 
     /// Round the given us timestamp by the window boundary.
     pub fn round_us(&self, t: i64) -> i64 {
-        let t = t + timeunit_scale(ArrowTimeUnit::Microsecond, ArrowTimeUnit::Nanosecond) as i64
-            * self.every.nanoseconds()
-            / 2 as i64;
+        let t = t + self.every.nanoseconds()
+            / (2 * timeunit_scale(ArrowTimeUnit::Nanosecond, ArrowTimeUnit::Microsecond) as i64);
         self.truncate_us(t)
     }
 
     /// Round the given ms timestamp by the window boundary.
     pub fn round_ms(&self, t: i64) -> i64 {
-        let t = t + timeunit_scale(ArrowTimeUnit::Millisecond, ArrowTimeUnit::Nanosecond) as i64
-            * self.every.nanoseconds()
-            / 2 as i64;
+        let t = t + self.every.nanoseconds()
+            / (2 * timeunit_scale(ArrowTimeUnit::Nanosecond, ArrowTimeUnit::Millisecond) as i64);
         self.truncate_ms(t)
     }
 
