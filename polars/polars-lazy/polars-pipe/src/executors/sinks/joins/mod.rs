d80885a5525b883b9279e0662d272bd652dfc695
Ritchie Vink <ritchie46@gmail.com>
2022-11-07 15:41:39 +0100

    refactor(rust): make generic join builder more dry (#5439)

--- polars/polars-lazy/polars-pipe/src/executors/sinks/joins/mod.rs
+++ polars/polars-lazy/polars-pipe/src/executors/sinks/joins/mod.rs
@@ -1,7 +1,8 @@
 #[cfg(feature = "cross_join")]
 mod cross;
+mod generic_build;
 mod inner;
 
 #[cfg(feature = "cross_join")]
 pub(crate) use cross::*;
-pub(crate) use inner::GenericBuild;
+pub(crate) use generic_build::GenericBuild;
