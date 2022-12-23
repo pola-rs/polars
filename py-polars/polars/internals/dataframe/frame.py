7ddc531ea27960d2f63db15fcacbda15a4f64c21
Jeroen van Zundert <mail@jeroenvanzundert.nl>
2022-12-23 17:38:30 +0100

    Improve docstring DataFrame.item

--- py-polars/polars/internals/dataframe/frame.py
+++ py-polars/polars/internals/dataframe/frame.py
@@ -1506,9 +1506,19 @@ class DataFrame:
 
         Examples
         --------
-        >>> df = pl.DataFrame({"foo": [1]})
-        >>> df.item()
-        1
+        >>> df = pl.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+        >>> result = df.select((pl.col("a") * pl.col("b")).sum())
+        >>> result
+        shape: (1, 1)
+        ┌─────┐
+        │ a   │
+        │ --- │
+        │ i64 │
+        ╞═════╡
+        │ 32  │
+        └─────┘
+        >>> result.item()
+        32
 
         """
         if self.shape != (1, 1):
