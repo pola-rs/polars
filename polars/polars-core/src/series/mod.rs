46192fe02e1ae601236b1f59c438c0e2e0ef6319
Ritchie Vink <ritchie46@gmail.com>
2022-12-29 09:51:33 +0100

    feat(rust, python): allow all null cast

--- polars/polars-core/src/series/mod.rs
+++ polars/polars-core/src/series/mod.rs
@@ -228,7 +228,12 @@ impl Series {
 
     /// Cast `[Series]` to another `[DataType]`
     pub fn cast(&self, dtype: &DataType) -> PolarsResult<Self> {
-        self.0.cast(dtype)
+        let len = self.len();
+        if self.null_count() == len {
+            Ok(Series::full_null(self.name(), len, dtype))
+        } else {
+            self.0.cast(dtype)
+        }
     }
 
     /// Compute the sum of all values in this Series.
