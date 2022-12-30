c19cde5c578d6c0c872721427087ef8c2957d846
Ritchie Vink <ritchie46@gmail.com>
2022-12-30 09:06:50 +0100

    fix(rust, python): fix arrow nested null conversion

--- py-polars/tests/unit/test_interop.py
+++ py-polars/tests/unit/test_interop.py
@@ -490,3 +490,14 @@ def test_numpy_preserve_uint64_4112() -> None:
 def test_view_ub() -> None:
     # this would be UB if the series was dropped and not passed to the view
     assert np.sum(pl.Series([3, 1, 5]).sort().view()) == 9
+
+
+def test_arrow_list_null_5697() -> None:
+    # Create a pyarrow table with a list[null] column.
+    pa_table = pa.table([[[None]]], names=["mycol"])
+    df = pl.from_arrow(pa_table)
+    pa_table = df.to_arrow()
+    # again to polars to test the schema
+    assert pl.from_arrow(pa_table,).schema == {  # type: ignore[union-attr]
+        "mycol": pl.List(pl.Null)
+    }
