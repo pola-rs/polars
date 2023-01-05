77454ac33f850d7c75fa9e69e7636869cf9ca394
Kival Mahadew <kivalm@protonmail.com>
2023-01-05 16:32:49 +0200

    minimum wasm changes

--- polars/polars-utils/src/sort.rs
+++ polars/polars-utils/src/sort.rs
@@ -16,7 +16,39 @@ use crate::IdxSize;
 ///
 /// # Safety
 /// The caller must ensure that the right indexes fo `&[(_, IdxSize)]` are integers ranging from `0..idx.len`
+#[cfg(not(target_family = "wasm"))]
 pub unsafe fn perfect_sort(pool: &ThreadPool, idx: &[(IdxSize, IdxSize)], out: &mut Vec<IdxSize>) {
+    let chunk_size = std::cmp::max(
+        idx.len() / pool.current_num_threads(),
+        pool.current_num_threads(),
+    );
+
+    out.reserve(idx.len());
+    let ptr = out.as_mut_ptr() as *const IdxSize as usize;
+
+    pool.install(|| {
+        idx.par_chunks(chunk_size).for_each(|indices| {
+            let ptr = ptr as *mut IdxSize;
+            for (idx_val, idx_location) in indices {
+                // Safety:
+                // idx_location is in bounds by invariant of this function
+                // and we ensured we have at least `idx.len()` capacity
+                *ptr.add(*idx_location as usize) = *idx_val;
+            }
+        });
+    });
+    // Safety:
+    // all elements are written
+    out.set_len(idx.len());
+}
+
+// wasm alternative with different signature
+#[cfg(target_family = "wasm")]
+pub unsafe fn perfect_sort(
+    pool: &crate::wasm::Pool,
+    idx: &[(IdxSize, IdxSize)],
+    out: &mut Vec<IdxSize>,
+) {
     let chunk_size = std::cmp::max(
         idx.len() / pool.current_num_threads(),
         pool.current_num_threads(),
