c45274342dc00b022a4c03e35e5a330aaac98202
Ritchie Vink <ritchie46@gmail.com>
2023-01-05 09:51:59 +0100

    fix(rust, python): block streaming on literal series/range

--- polars/polars-lazy/src/physical_plan/streaming/convert.rs
+++ polars/polars-lazy/src/physical_plan/streaming/convert.rs
@@ -41,17 +41,37 @@ fn to_physical_piped_expr(
 }
 
 fn is_streamable(node: Node, expr_arena: &Arena<AExpr>) -> bool {
-    expr_arena.iter(node).all(|(_, ae)| match ae {
+    // check weather leaf colum is Col or Lit
+    let mut seen_column = false;
+    let mut seen_lit_range = false;
+    let all = expr_arena.iter(node).all(|(_, ae)| match ae {
         AExpr::Function { options, .. } | AExpr::AnonymousFunction { options, .. } => {
             matches!(options.collect_groups, ApplyOptions::ApplyFlat)
         }
-        AExpr::Column(_)
-        | AExpr::Literal(_)
-        | AExpr::BinaryExpr { .. }
-        | AExpr::Alias(_, _)
-        | AExpr::Cast { .. } => true,
+        AExpr::Column(_) => {
+            seen_column = true;
+            true
+        }
+        AExpr::BinaryExpr { .. } | AExpr::Alias(_, _) | AExpr::Cast { .. } => true,
+        AExpr::Literal(lv) => match lv {
+            LiteralValue::Series(_) | LiteralValue::Range { .. } => {
+                seen_lit_range = true;
+                true
+            }
+            _ => true,
+        },
         _ => false,
-    })
+    });
+
+    if all {
+        // adding a range or literal series to chunks will fail because sizes don't match
+        // if column is a leaf column then it is ok
+        // - so we want to block `with_column(lit(Series))`
+        // - but we want to allow `with_column(col("foo").is_in(Series))`
+        // that means that IFF we seen a lit_range, we only allow if we also seen a `column`.
+        return if seen_lit_range { seen_column } else { true };
+    }
+    false
 }
 
 fn all_streamable(exprs: &[Node], expr_arena: &Arena<AExpr>) -> bool {
