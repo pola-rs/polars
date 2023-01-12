759411b93bf2a97ea9415c636923f8ae3b7af0c5
Ritchie Vink <ritchie46@gmail.com>
2023-01-12 13:09:46 +0100

    fix(rust, python): explode of empty nullable list

--- polars/polars-core/src/series/ops/extend.rs
+++ polars/polars-core/src/series/ops/extend.rs
@@ -3,20 +3,7 @@ use crate::prelude::*;
 impl Series {
     /// Extend with a constant value.
     pub fn extend_constant(&self, value: AnyValue, n: usize) -> PolarsResult<Self> {
-        use AnyValue::*;
-        let s = match value {
-            Float32(v) => Series::new("", vec![v]),
-            Float64(v) => Series::new("", vec![v]),
-            UInt32(v) => Series::new("", vec![v]),
-            UInt64(v) => Series::new("", vec![v]),
-            Int32(v) => Series::new("", vec![v]),
-            Int64(v) => Series::new("", vec![v]),
-            Utf8(v) => Series::new("", vec![v]),
-            Boolean(v) => Series::new("", vec![v]),
-            Date(v) => Series::new("", vec![v]),
-            Null => BooleanChunked::full_null("", 1).into_series(),
-            dt => panic!("{dt:?} not supported"),
-        };
+        let s = Series::from_any_values("", &[value]).unwrap();
         let s = s.cast(self.dtype())?;
         let to_append = s.new_from_index(0, n);
 
