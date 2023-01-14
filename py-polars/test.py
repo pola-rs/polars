43def24a3ae30ebd4a8efa0e51cc9e9a4f55c291
Mehrbod Akhlaghpour <m9.akhlaghpoor@gmail.com>
2023-01-14 00:34:25 -0500

    Add: added some tests

--- py-polars/test.py
+++
@@ -1,3 +0,0 @@
-import polars as pl
-a = pl.List(pl.Utf8).string_repr()
-print(a)
--- py-polars/tests/unit/test_df.py
+++ py-polars/tests/unit/test_df.py
@@ -2627,3 +2627,14 @@ def test_item() -> None:
     df = pl.DataFrame({})
     with pytest.raises(ValueError):
         df.item()
+
+
+def test_string_repr() -> None:
+    reprs = pl.List(pl.Utf8).string_repr()
+    assert reprs == "List[Utf8]"
+
+    reprs = pl.List(pl.Date).string_repr()
+    assert reprs == "List[Date]"
+
+    reprs = pl.List(pl.Int32).string_repr()
+    assert reprs == "List[Int32]"
