2327a6801ad838d0ade3a0a85636fcd9a2804327
Stijn de Gooijer <stijn@degooijer.io>
2023-01-21 14:22:19 +0100

    feat(python): Rename `Series.struct.to_frame` to `.struct.unnest` (#6352)

--- py-polars/polars/internals/series/struct.py
+++ py-polars/polars/internals/series/struct.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+import warnings
 from typing import TYPE_CHECKING
 
 import polars.internals as pli
@@ -33,10 +34,6 @@ class StructNameSpace:
     def _ipython_key_completions_(self) -> list[str]:
         return self.fields
 
-    def to_frame(self) -> pli.DataFrame:
-        """Convert this Struct Series to a DataFrame."""
-        return pli.wrap_df(self._s.struct_to_frame())
-
     @property
     def fields(self) -> list[str]:
         """Get the names of the fields."""
@@ -65,3 +62,34 @@ class StructNameSpace:
             New names in the order of the struct's fields
 
         """
+
+    def to_frame(self) -> pli.DataFrame:
+        """Convert this Struct Series to a DataFrame."""
+        warnings.warn(
+            "`Series.struct.to_frame` has been renamed to `Series.struct.unnest`."
+            " Use the new method name to silence this warning.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return pli.wrap_df(self._s.struct_unnest())
+
+    def unnest(self) -> pli.DataFrame:
+        """
+        Convert this struct Series to a DataFrame with a separate column for each field.
+
+        Examples
+        --------
+        >>> s = pl.Series([{"a": 1, "b": 2}, {"a": 3, "b": 4}])
+        >>> s.struct.unnest()
+        shape: (2, 2)
+        ┌─────┬─────┐
+        │ a   ┆ b   │
+        │ --- ┆ --- │
+        │ i64 ┆ i64 │
+        ╞═════╪═════╡
+        │ 1   ┆ 2   │
+        │ 3   ┆ 4   │
+        └─────┴─────┘
+
+        """
+        return pli.wrap_df(self._s.struct_unnest())
