dfc400c95444e2095dbe99241b789dc00eaf2a16
Philippe Laflamme <philippe@hopper.com>
2023-02-04 11:56:10 -0500

    feat: SeriesTrait for DecimalChunked

--- polars/polars-core/src/series/implementations/decimal.rs
+++ polars/polars-core/src/series/implementations/decimal.rs
@@ -13,52 +13,77 @@ impl private::PrivateSeries for SeriesWrap<DecimalChunked> {}
 
 impl SeriesTrait for SeriesWrap<DecimalChunked> {
     fn rename(&mut self, name: &str) {
-        todo!()
+        self.0.rename(name)
     }
 
     fn chunks(&self) -> &Vec<ArrayRef> {
-        todo!()
+        self.0.chunks()
     }
 
-    fn take_iter(&self, _iter: &mut dyn TakeIterator) -> PolarsResult<Series> {
-        todo!()
+    unsafe fn take_iter_unchecked(&self, iter: &mut dyn TakeIterator) -> Series {
+        ChunkTake::take_unchecked(self.0.deref(), iter.into())
+            .into_decimal(self.0.precision(), self.0.scale())
+            .into_series()
     }
 
-    unsafe fn take_iter_unchecked(&self, _iter: &mut dyn TakeIterator) -> Series {
-        todo!()
+    unsafe fn take_unchecked(&self, idx: &IdxCa) -> PolarsResult<Series> {
+        let mut out = ChunkTake::take_unchecked(self.0.deref(), idx.into());
+
+        if self.0.is_sorted_flag() && (idx.is_sorted_flag() || idx.is_sorted_reverse_flag()) {
+            out.set_sorted_flag(idx.is_sorted_flag2())
+        }
+
+        Ok(out
+            .into_decimal(self.0.precision(), self.0.scale())
+            .into_series())
     }
 
-    unsafe fn take_unchecked(&self, _idx: &IdxCa) -> PolarsResult<Series> {
-        todo!()
+    unsafe fn take_opt_iter_unchecked(&self, iter: &mut dyn TakeIteratorNulls) -> Series {
+        ChunkTake::take_unchecked(self.0.deref(), iter.into())
+            .into_decimal(self.0.precision(), self.0.scale())
+            .into_series()
     }
 
-    unsafe fn take_opt_iter_unchecked(&self, _iter: &mut dyn TakeIteratorNulls) -> Series {
-        todo!()
+    fn take(&self, indices: &IdxCa) -> PolarsResult<Series> {
+        ChunkTake::take(self.0.deref(), indices.into()).map(|ca| {
+            ca.into_decimal(self.0.precision(), self.0.scale())
+                .into_series()
+        })
     }
 
-    fn take(&self, _indices: &IdxCa) -> PolarsResult<Series> {
-        todo!()
+    fn take_iter(&self, iter: &mut dyn TakeIterator) -> PolarsResult<Series> {
+        ChunkTake::take(self.0.deref(), iter.into()).map(|ca| {
+            ca.into_decimal(self.0.precision(), self.0.scale())
+                .into_series()
+        })
     }
 
     fn len(&self) -> usize {
-        todo!()
+        self.0.len()
     }
 
     fn take_every(&self, n: usize) -> Series {
-        todo!()
+        self.0
+            .take_every(n)
+            .into_decimal(self.0.precision(), self.0.scale())
+            .into_series()
     }
 
     fn has_validity(&self) -> bool {
-        todo!()
+        self.0.has_validity()
     }
 
     #[cfg(feature = "chunked_ids")]
     unsafe fn _take_chunked_unchecked(&self, by: &[ChunkId], sorted: IsSorted) -> Series {
-        todo!()
+        let ca = self.0.deref().take_chunked_unchecked(by, sorted);
+        ca.into_decimal(self.0.precision(), self.0.scale())
+            .into_series()
     }
 
     #[cfg(feature = "chunked_ids")]
     unsafe fn _take_opt_chunked_unchecked(&self, by: &[Option<ChunkId>]) -> Series {
-        todo!()
+        let ca = self.0.deref().take_opt_chunked_unchecked(by);
+        ca.into_decimal(self.0.precision(), self.0.scale())
+            .into_series()
     }
 }
