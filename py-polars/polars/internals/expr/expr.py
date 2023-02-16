aaceac90065c3ca700e209a14cf86e381db939c5
Stijn de Gooijer <stijn@degooijer.io>
2023-02-16 17:28:51 +0100

    chore(python): Bump ruff version (#6936)

--- py-polars/polars/internals/expr/expr.py
+++ py-polars/polars/internals/expr/expr.py
@@ -66,9 +66,9 @@ def selection_to_pyexpr_list(
 ) -> list[PyExpr]:
     if exprs is None:
         exprs = []
-    elif isinstance(exprs, (str, Expr, pli.Series, pli.WhenThen, pli.WhenThenThen)):
-        exprs = [exprs]
-    elif not isinstance(exprs, Iterable):
+    elif isinstance(
+        exprs, (str, Expr, pli.Series, pli.WhenThen, pli.WhenThenThen)
+    ) or not isinstance(exprs, Iterable):
         exprs = [exprs]
     return [
         expr_to_lit_or_expr(e, str_to_lit=False, structify=structify)._pyexpr
