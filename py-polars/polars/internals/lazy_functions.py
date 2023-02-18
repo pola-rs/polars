65c92433ae9a819d8776678f87d493076f192a6e
Stijn de Gooijer <stijn@degooijer.io>
2023-02-18 13:59:00 +0100

    Add *args

--- py-polars/polars/internals/lazy_functions.py
+++ py-polars/polars/internals/lazy_functions.py
@@ -80,24 +80,24 @@ if TYPE_CHECKING:
 
 def col(
     name: str | PolarsDataType | Iterable[str] | Iterable[PolarsDataType],
+    *more_names: str | PolarsDataType,
 ) -> pli.Expr:
     """
-    Return an expression representing a column in a DataFrame.
-
-    Can be used to select:
-
-    - a single column by name
-    - all columns by using a wildcard `"*"`
-    - column by regular expression if the regex starts with `^` and ends with `$`
-    - all columns with the same dtype by using a Polars type
+    Return an expression representing column(s) in a dataframe.
 
     Parameters
     ----------
     name
-        A string that holds the name of the column
+        The name or datatype of the column(s) to represent. Accepts regular expression
+        input. Regular expressions should start with ``^`` and end with ``$``.
+    *more_names
+        Additional names or datatypes of columns to represent, specified as positional
+        arguments.
 
     Examples
     --------
+    Pass a single column name to represent that column.
+
     >>> df = pl.DataFrame(
     ...     {
     ...         "ham": [1, 2, 3],
@@ -117,6 +117,9 @@ def col(
     │ 2   │
     │ 1   │
     └─────┘
+
+    Use the wildcard ``*`` to represent all columns.
+
     >>> df.select(pl.col("*"))
     shape: (3, 4)
     ┌─────┬───────────┬─────┬─────┐
@@ -128,17 +131,6 @@ def col(
     │ 2   ┆ 22        ┆ 2   ┆ b   │
     │ 3   ┆ 33        ┆ 1   ┆ c   │
     └─────┴───────────┴─────┴─────┘
-    >>> df.select(pl.col("^ham.*$"))
-    shape: (3, 2)
-    ┌─────┬───────────┐
-    │ ham ┆ hamburger │
-    │ --- ┆ ---       │
-    │ i64 ┆ i64       │
-    ╞═════╪═══════════╡
-    │ 1   ┆ 11        │
-    │ 2   ┆ 22        │
-    │ 3   ┆ 33        │
-    └─────┴───────────┘
     >>> df.select(pl.col("*").exclude("ham"))
     shape: (3, 3)
     ┌───────────┬─────┬─────┐
@@ -150,6 +142,23 @@ def col(
     │ 22        ┆ 2   ┆ b   │
     │ 33        ┆ 1   ┆ c   │
     └───────────┴─────┴─────┘
+
+    Regular expression input is supported.
+
+    >>> df.select(pl.col("^ham.*$"))
+    shape: (3, 2)
+    ┌─────┬───────────┐
+    │ ham ┆ hamburger │
+    │ --- ┆ ---       │
+    │ i64 ┆ i64       │
+    ╞═════╪═══════════╡
+    │ 1   ┆ 11        │
+    │ 2   ┆ 22        │
+    │ 3   ┆ 33        │
+    └─────┴───────────┘
+
+    Multiple columns can be represented by passing a list of names.
+
     >>> df.select(pl.col(["hamburger", "foo"]))
     shape: (3, 2)
     ┌───────────┬─────┐
@@ -161,7 +170,23 @@ def col(
     │ 22        ┆ 2   │
     │ 33        ┆ 1   │
     └───────────┴─────┘
-    >>> # Select columns with a dtype
+
+    Or use positional arguments to represent multiple columns in the same way.
+
+    >>> df.select(pl.col("hamburger", "foo"))
+    shape: (3, 2)
+    ┌───────────┬─────┐
+    │ hamburger ┆ foo │
+    │ ---       ┆ --- │
+    │ i64       ┆ i64 │
+    ╞═══════════╪═════╡
+    │ 11        ┆ 3   │
+    │ 22        ┆ 2   │
+    │ 33        ┆ 1   │
+    └───────────┴─────┘
+
+    Easily select all columns that match a certain data type by passing that datatype.
+
     >>> df.select(pl.col(pl.Utf8))
     shape: (3, 1)
     ┌─────┐
@@ -173,8 +198,7 @@ def col(
     │ b   │
     │ c   │
     └─────┘
-    >>> # Select columns from a list of dtypes
-    >>> df.select(pl.col([pl.Int64, pl.Float64]))
+    >>> df.select(pl.col(pl.Int64, pl.Float64))
     shape: (3, 3)
     ┌─────┬───────────┬─────┐
     │ ham ┆ hamburger ┆ foo │
@@ -187,6 +211,19 @@ def col(
     └─────┴───────────┴─────┘
 
     """
+    if more_names:
+        if isinstance(name, str):
+            names_str = [name]
+            names_str.extend(more_names)  # type: ignore[arg-type]
+            return pli.wrap_expr(pycols(names_str))
+        elif is_polars_dtype(name):
+            dtypes = [name]
+            dtypes.extend(more_names)
+            return pli.wrap_expr(_dtype_cols(dtypes))
+        raise TypeError(
+            f"Invalid input for `col`. Expected `str` or `DataType`, got {type(name)!r}"
+        )
+
     if isinstance(name, str):
         return pli.wrap_expr(pycol(name))
     elif is_polars_dtype(name):
@@ -202,12 +239,12 @@ def col(
         elif is_polars_dtype(item):
             return pli.wrap_expr(_dtype_cols(names))
         else:
-            raise ValueError(
-                "Invalid input for `col`. Expected iterable of type `str` or DataType,"
+            raise TypeError(
+                "Invalid input for `col`. Expected iterable of type `str` or `DataType`,"
                 f" got iterable of type {type(item)!r}"
             )
     else:
-        raise ValueError(
+        raise TypeError(
             f"Invalid input for `col`. Expected `str` or `DataType`, got {type(name)!r}"
         )
 
