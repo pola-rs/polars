fix(python): subtracting Series from date had wrong sign

py-polars/polars/internals/series/series.py
@@ -530,10 +530,12 @@ class Series:
             return wrap_s(getattr(self._s, op_s)(other._s))
         if _check_for_numpy(other) and isinstance(other, np.ndarray):
             return wrap_s(getattr(self._s, op_s)(Series(other)._s))
-        # recurse; the 'if' statement above will ensure we return early
         if isinstance(other, (date, datetime, timedelta, str)):
             other = Series("", [other])
-            return self._arithmetic(other, op_s, op_ffi)
+            if "rhs" in op_ffi:
+                return wrap_s(getattr(other._s, op_s)(self._s))
+            else:
+                return wrap_s(getattr(self._s, op_s)(other._s))
         if isinstance(other, float) and not self.is_float():
             _s = sequence_to_pyseries("", [other])
             if "rhs" in op_ffi: