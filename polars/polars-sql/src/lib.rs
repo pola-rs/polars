aa8bc50e28bc1e5f9f739e20aa9e57aba3458546
universalmind303 <universalmind.candy@gmail.com>
2023-03-09 11:01:40 -0600

    additional test cases

--- polars/polars-sql/src/lib.rs
+++ polars/polars-sql/src/lib.rs
@@ -343,6 +343,30 @@ mod test {
             .unwrap();
         assert!(df_sql.frame_equal_missing(&df_pl));
     }
+
+    #[test]
+    fn test_iss_7440() {
+        let df = df! {
+            "a" => [2.0, -2.5]
+        }
+        .unwrap()
+        .lazy();
+        let sql = r#"SELECT a, FLOOR(a) AS floor, CEIL(a) AS ceil FROM df"#;
+        let mut context = SQLContext::try_new().unwrap();
+        context.register("df", df.clone());
+
+        let df_sql = context.execute(sql).unwrap().collect().unwrap();
+
+        let df_pl = df
+            .select(&[
+                col("a"),
+                col("a").floor().alias("floor"),
+                col("a").ceil().alias("ceil"),
+            ])
+            .collect()
+            .unwrap();
+        assert!(df_sql.frame_equal_missing(&df_pl));
+    }
     #[test]
     fn test_string_functions() {
         let df = df! {
