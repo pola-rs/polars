dc1d5fd1273d41567764bf87c59b8f8a9b6c33bf
Ed Cashin <ed.cashin@acm.org>
2023-03-12 20:50:02 -0400

    Use PolarsResult to avoid unwrap

--- examples/string_filter/src/main.rs
+++ examples/string_filter/src/main.rs
@@ -1,14 +1,13 @@
 use polars::lazy::prelude::*;
 use polars::prelude::*;
 
-fn main() {
+fn main() -> PolarsResult<()> {
     let a = Series::new("a", [1, 2, 3, 4]);
     let b = Series::new("b", ["one", "two", "three", "four"]);
-    let df = DataFrame::new(vec![a, b])
-        .unwrap()
+    let df = DataFrame::new(vec![a, b])?
         .lazy()
         .filter(col("b").str().starts_with(lit("t")))
-        .collect()
-        .unwrap();
+        .collect()?;
     println!("{df:?}");
+    Ok(())
 }
