a36533f33ff06a3f968f9c27c5a39b509977bce0
alexander-beedie <alexander.m.beedie@icloud.com>
2023-03-16 18:51:53 +0400

    enhance import test

--- py-polars/tests/unit/test_polars_import.py
+++ py-polars/tests/unit/test_polars_import.py
@@ -13,7 +13,6 @@ def _import_timings() -> bytes:
     return (
         subprocess.run(cmd, shell=True, capture_output=True)
         .stderr.replace(b"import time:", b"")
-        .replace(b" ", b"")
         .strip()
     )
 
@@ -26,6 +25,8 @@ def _import_timings_as_frame(best_of: int) -> pl.DataFrame:
             separator="|",
             has_header=True,
             new_columns=["own_time", "cumulative_time", "import"],
+        ).with_columns(
+            pl.col(["own_time", "cumulative_time"]).str.strip().cast(pl.Int32)
         )
         for _ in range(best_of)
     ]
@@ -34,8 +35,8 @@ def _import_timings_as_frame(best_of: int) -> pl.DataFrame:
             import_timings,
             key=lambda tm: int(tm["cumulative_time"].max()),  # type: ignore[arg-type]
         )[0]
+        .reverse()
         .select("import", "own_time", "cumulative_time")
-        .sort(by=["cumulative_time"], descending=True)
     )
 
 
@@ -44,17 +45,22 @@ def test_polars_import() -> None:
     # note: take the fastest of three runs to reduce noise.
     df_import = _import_timings_as_frame(best_of=3)
 
-    # ensure that we have not broken lazy-loading (numpy, pandas, pyarrow, etc).
-    lazy_modules = [dep for dep in pl.dependencies.__all__ if not dep.startswith("_")]
-    for mod in lazy_modules:
-        not_imported = not df_import["import"].str.starts_with(mod).any()
-        assert not_imported, f"lazy-loading regression: found {mod!r} at import time"
-
-    # ensure that we do not have an import speed regression.
     with pl.Config() as cfg:
-        cfg.set_tbl_rows(25)
+        # get a complete view of what's going on in case of failure
+        cfg.set_tbl_rows(250)
+        cfg.set_fmt_str_lengths(100)
         cfg.set_tbl_hide_dataframe_shape()
 
+        # ensure that we have not broken lazy-loading (numpy, pandas, pyarrow, etc).
+        lazy_modules = [
+            dep for dep in pl.dependencies.__all__ if not dep.startswith("_")
+        ]
+        for mod in lazy_modules:
+            not_imported = not df_import["import"].str.starts_with(mod).any()
+            if_err = f"lazy-loading regression: found {mod!r} at import time"
+            assert not_imported, f"{if_err}\n{df_import}"
+
+        # ensure that we do not have an import speed regression.
         total_import_time = df_import["cumulative_time"].max()
         assert isinstance(total_import_time, int)
 
