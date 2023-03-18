3fc9cb55ab24ebd64e62165809ec2213e130efec
Ritchie Vink <ritchie46@gmail.com>
2023-03-18 10:39:39 +0100

    refactor(rust): don't branch via error in read_csv::parse_dates

--- polars/polars-io/src/csv/read.rs
+++ polars/polars-io/src/csv/read.rs
@@ -603,19 +603,23 @@ fn parse_dates(mut df: DataFrame, fixed_schema: &Schema) -> DataFrame {
     let cols = unsafe { std::mem::take(df.get_columns_mut()) }
         .into_par_iter()
         .map(|s| {
-            if let Ok(ca) = s.utf8() {
-                // don't change columns that are in the fixed schema.
-                if fixed_schema.index_of(s.name()).is_some() {
-                    return s;
-                }
+            match s.dtype() {
+                DataType::Utf8 => {
+                    let ca = s.utf8().unwrap();
+                    // don't change columns that are in the fixed schema.
+                    if fixed_schema.index_of(s.name()).is_some() {
+                        return s;
+                    }
 
-                #[cfg(feature = "dtype-time")]
-                if let Ok(ca) = ca.as_time(None, false) {
-                    return ca.into_series();
+                    #[cfg(feature = "dtype-time")]
+                    if let Ok(ca) = ca.as_time(None, false) {
+                        return ca.into_series();
+                    }
+                    s
+                },
+                _ => {
+                    s
                 }
-                s
-            } else {
-                s
             }
         })
         .collect::<Vec<_>>();
