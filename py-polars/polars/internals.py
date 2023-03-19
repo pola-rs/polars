57961c3fb94736d5505642933cb0d2d3be1a42aa
Stijn de Gooijer <stijn@degooijer.io>
2023-03-19 19:49:17 +0100

    Change internals from folder to a file

--- py-polars/polars/internals.py
+++ py-polars/polars/internals.py
@@ -1,10 +1,8 @@
 """
-Core Polars functionality.
+Re-export Polars functionality to avoid cyclical imports.
 
-The modules within `polars.internals` are interdependent. To prevent cyclical imports,
-they all import from each other via this __init__ file using
-`from polars import internals as pli`. The imports below are being shared across this
-module.
+If you run into cyclical imports, export functionality in this module, then import this
+module using `from polars import internals as pli`.
 """
 from polars.dataframe import DataFrame, wrap_df
 from polars.expr import (
