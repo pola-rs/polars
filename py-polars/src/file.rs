7d1200c08da5c626eeaef7717acfc3271f66c925
Didrik <didrik@galteland.com>
2023-03-21 12:31:40 +0100

    fix(python): fix non-existant error msg (#7657)

--- py-polars/src/file.rs
+++ py-polars/src/file.rs
@@ -191,7 +191,7 @@ pub enum EitherRustPythonFile {
 
 fn no_such_file_err(file_path: &str) -> PyResult<()> {
     let msg = if file_path.len() > 88 {
-        let file_path: String = file_path.chars().rev().take(88).collect();
+        let file_path: &str = &file_path[file_path.len() - 88..];
         format!("No such file or directory: ...{file_path}",)
     } else {
         format!("No such file or directory: {file_path}",)
