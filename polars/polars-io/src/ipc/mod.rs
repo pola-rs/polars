2c09aee4f691641696811a90cb08971c6979c7c2
Ritchie Vink <ritchie46@gmail.com>
2023-03-21 08:47:30 +0100

    split ipc write/read module

--- polars/polars-io/src/ipc/mod.rs
+++ polars/polars-io/src/ipc/mod.rs
@@ -6,8 +6,11 @@ mod ipc_file;
 #[cfg(feature = "ipc_streaming")]
 mod ipc_stream;
 mod mmap;
+#[cfg(feature = "ipc")]
+mod write;
 
 #[cfg(feature = "ipc")]
-pub use ipc_file::{BatchedWriter, IpcCompression, IpcReader, IpcWriter, IpcWriterOption};
+pub use ipc_file::IpcReader;
 #[cfg(feature = "ipc_streaming")]
 pub use ipc_stream::*;
+pub use write::{BatchedWriter, IpcCompression, IpcWriter, IpcWriterOption};
