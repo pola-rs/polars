fb72ca484b602ba040346a7294766712f491a16e
ritchie <ritchie46@gmail.com>
2023-03-25 07:54:41 +0100

    fix test

--- py-polars/tests/unit/test_errors.py
+++ py-polars/tests/unit/test_errors.py
@@ -483,7 +483,7 @@ def test_err_on_time_datetime_cast() -> None:
 
 
 def test_invalid_inner_type_cast_list() -> None:
-    s = pl.Series([[-1, 1]]).cast(pl.List(pl.Categorical))
+    s = pl.Series([[-1, 1]])
     with pytest.raises(
         pl.ComputeError, match=r"cannot cast list inner type: 'Int64' to Categorical"
     ):
