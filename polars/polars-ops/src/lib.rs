e7c527173805cc8686ad46bf1d9fdfb6479389a7
ritchie <ritchie46@gmail.com>
2023-03-28 10:50:53 +0200

    activate unstable feature

--- polars/polars-ops/src/lib.rs
+++ polars/polars-ops/src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
-#![cfg_attr(nightly, feature(unicode_internals))]
+#![cfg_attr(feature = "nightly", feature(unicode_internals))]
 pub mod chunked_array;
 #[cfg(feature = "pivot")]
 pub use frame::pivot;
