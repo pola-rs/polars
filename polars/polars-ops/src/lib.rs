460fd743b7c07fbee4f3784640bb864ac6ad6dc0
ritchie <ritchie46@gmail.com>
2023-03-28 10:38:32 +0200

    fix feature

--- polars/polars-ops/src/lib.rs
+++ polars/polars-ops/src/lib.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![cfg_attr(nightly, feature(unicode_internals))]
-#![feature(unicode_internals)]
 pub mod chunked_array;
 #[cfg(feature = "pivot")]
 pub use frame::pivot;
