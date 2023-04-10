95aaa686fb1b46c558104d360b35f209e247ac13
Jonas Haag <jonas@lophus.org>
2023-04-10 19:37:28 +0300

    Add a bunch of missing feature flag guards

--- py-polars/src/lazy/dsl.rs
+++ py-polars/src/lazy/dsl.rs
@@ -724,6 +724,7 @@ impl PyExpr {
     }
 
     #[pyo3(signature = (pat, literal, strict))]
+    #[cfg(feature = "regex")]
     pub fn str_contains(&self, pat: PyExpr, literal: Option<bool>, strict: bool) -> PyExpr {
         match literal {
             Some(true) => self.inner.clone().str().contains_literal(pat.inner).into(),
@@ -863,6 +864,7 @@ impl PyExpr {
             .into()
     }
 
+    #[cfg(all(feature = "json", feature = "strings"))]
     pub fn str_json_extract(&self, dtype: Option<Wrap<DataType>>) -> PyExpr {
         let dtype = dtype.map(|wrap| wrap.0);
 
