9ab2c56ba801e6c2b9b97fa694f3ec951aa21eeb
Robert Bennett <rltbennett@icloud.com>
2023-04-10 17:16:22 -0400

    Made all fields on `DatetimeArgs` `Expr` (no more `Option<Expr>`) Added `DatetimeArgs::new(y, m, d)` and `.with_hms(h, m, s)` Removed Default from `DatetimeArgs` (since `DatetimeArgs::default()` really shouldn't exist) Made all fields of `DurationArgs` `Expr` instead of `Option<Expr>` Manually implemented `Default` for `DurationArgs` to make all fields `lit(0)` Updated/improved docs

--- polars/polars-lazy/polars-plan/src/dsl/functions.rs
+++ polars/polars-lazy/polars-plan/src/dsl/functions.rs
@@ -450,49 +450,80 @@ pub fn arange(low: Expr, high: Expr, step: usize) -> Expr {
 }
 
 macro_rules! impl_unit_setter {
-    ($fn_name:ident($field:ident -> $into_ty:ty $(,)?)) => {
+    ($fn_name:ident($field:ident)) => {
         #[doc = concat!("Set the ", stringify!($field))]
-        pub fn $fn_name(mut self, n: impl Into<$into_ty>) -> Self {
+        pub fn $fn_name(mut self, n: Expr) -> Self {
             self.$field = n.into();
             self
         }
     };
 }
 
-#[derive(Default)]
+/// Arguments used by [`datetime`] in order to produce an `Expr` of `Datetime`
+///
+/// Construct a `DatetimeArgs` with `DatetimeArgs::new(y, m, d)`. This will set the other time units to `lit(0)`. You
+/// can then set the other fields with the `with_*` methods, or use `with_hms` to set `hour`, `minute`, and `second` all
+/// at once.
+///
+/// # Examples
+/// ```
+/// // construct a DatetimeArgs set to July 20, 1969 at 20:17
+/// let args = DatetimeArgs::new(lit(1969), lit(7), lit(20)).with_hms(lit(20), lit(17), lit(0));
+/// // or
+/// let args = DatetimeArgs::new(lit(1969), lit(7), lit(20)).with_hour(lit(20)).with_minute(lit(17));
+///
+/// // construct a DatetimeArgs using existing columns
+/// let args = DatetimeArgs::new(lit(2023), col("month"), col("day"));
+/// ```
+#[derive(Debug, Clone)]
 pub struct DatetimeArgs {
     pub year: Expr,
     pub month: Expr,
     pub day: Expr,
-    pub hour: Option<Expr>,
-    pub minute: Option<Expr>,
-    pub second: Option<Expr>,
-    pub microsecond: Option<Expr>,
+    pub hour: Expr,
+    pub minute: Expr,
+    pub second: Expr,
+    pub microsecond: Expr,
 }
 
 impl DatetimeArgs {
-    /// Construct a new `DatetimeArgs` set to `year`, `month`, `day`.
+    /// Construct a new `DatetimeArgs` set to `year`, `month`, `day`
     ///
-    /// Other fields default to 0. Use the `with_*` methods to set them.
+    /// Other fields default to `lit(0)`. Use the `with_*` methods to set them.
     pub fn new(year: Expr, month: Expr, day: Expr) -> Self {
         Self {
             year,
             month,
             day,
-            ..Default::default()
+            hour: lit(0),
+            minute: lit(0),
+            second: lit(0),
+            microsecond: lit(0),
         }
     }
 
-    impl_unit_setter!(with_year(year -> Expr));
-    impl_unit_setter!(with_month(month -> Expr));
-    impl_unit_setter!(with_day(day -> Expr));
-    impl_unit_setter!(with_hour(hour -> Option<Expr>));
-    impl_unit_setter!(with_minute(minute -> Option<Expr>));
-    impl_unit_setter!(with_second(second -> Option<Expr>));
-    impl_unit_setter!(with_microsecond(microsecond -> Option<Expr>));
+    /// Set `hour`, `minute`, and `second`
+    ///
+    /// Equivalent to `self.with_hour(hour).with_minute(minute).with_second(second)`.
+    pub fn with_hms(self, hour: Expr, minute: Expr, second: Expr) -> Self {
+        Self {
+            hour,
+            minute,
+            second,
+            ..self
+        }
+    }
+
+    impl_unit_setter!(with_year(year));
+    impl_unit_setter!(with_month(month));
+    impl_unit_setter!(with_day(day));
+    impl_unit_setter!(with_hour(hour));
+    impl_unit_setter!(with_minute(minute));
+    impl_unit_setter!(with_second(second));
+    impl_unit_setter!(with_microsecond(microsecond));
 }
 
-/// Construct a column of `Datetime` from the provided args.
+/// Construct a column of `Datetime` from the provided [`DatetimeArgs`].
 #[cfg(feature = "temporal")]
 pub fn datetime(args: DatetimeArgs) -> Expr {
     use polars_core::export::chrono::NaiveDate;
@@ -575,15 +606,7 @@ pub fn datetime(args: DatetimeArgs) -> Expr {
     }) as Arc<dyn SeriesUdf>);
 
     Expr::AnonymousFunction {
-        input: vec![
-            year,
-            month,
-            day,
-            hour.unwrap_or_else(|| lit(0)),
-            minute.unwrap_or_else(|| lit(0)),
-            second.unwrap_or_else(|| lit(0)),
-            microsecond.unwrap_or_else(|| lit(0)),
-        ],
+        input: vec![year, month, day, hour, minute, second, microsecond],
         function,
         output_type: GetOutput::from_type(DataType::Datetime(TimeUnit::Microseconds, None)),
         options: FunctionOptions {
@@ -596,36 +619,67 @@ pub fn datetime(args: DatetimeArgs) -> Expr {
     .alias("datetime")
 }
 
-#[derive(Default)]
+/// Arguments used by [`duration`] in order to produce an `Expr` of `Duration`
+///
+/// To construct a `DurationArgs`, use struct literal syntax with `..Default::default()` to leave unspecified fields at
+/// their default value of `lit(0)`, as demonstrated below.
+///
+/// ```
+/// let args = DurationArgs {
+///     days: lit(5),
+///     hours: col("num_hours"),
+///     minutes: col("num_minutes"),
+///     ..Default::default()  // other fields are lit(0)
+/// };
+/// ```
+/// If you prefer builder syntax, `with_*` methods are also available.
+/// ```
+/// let args = DurationArgs::new().with_weeks(lit(42)).with_hours(lit(84));
+/// ```
+#[derive(Debug, Clone)]
 pub struct DurationArgs {
-    pub weeks: Option<Expr>,
-    pub days: Option<Expr>,
-    pub hours: Option<Expr>,
-    pub minutes: Option<Expr>,
-    pub seconds: Option<Expr>,
-    pub milliseconds: Option<Expr>,
-    pub microseconds: Option<Expr>,
-    pub nanoseconds: Option<Expr>,
+    pub weeks: Expr,
+    pub days: Expr,
+    pub hours: Expr,
+    pub minutes: Expr,
+    pub seconds: Expr,
+    pub milliseconds: Expr,
+    pub microseconds: Expr,
+    pub nanoseconds: Expr,
+}
+
+impl Default for DurationArgs {
+    fn default() -> Self {
+        Self {
+            weeks: lit(0),
+            days: lit(0),
+            hours: lit(0),
+            minutes: lit(0),
+            seconds: lit(0),
+            milliseconds: lit(0),
+            microseconds: lit(0),
+            nanoseconds: lit(0),
+        }
+    }
 }
 
 impl DurationArgs {
-    /// Create a new `DurationArgs` with all fields set to `None`, which is converted to 0 at runtime. Use the `with_*`
-    /// methods to set the fields.
+    /// Create a new `DurationArgs` with all fields set to `lit(0)`. Use the `with_*` methods to set the fields.
     pub fn new() -> Self {
         Self::default()
     }
 
-    impl_unit_setter!(with_weeks(weeks -> Option<Expr>));
-    impl_unit_setter!(with_days(days -> Option<Expr>));
-    impl_unit_setter!(with_hours(hours -> Option<Expr>));
-    impl_unit_setter!(with_minutes(minutes -> Option<Expr>));
-    impl_unit_setter!(with_seconds(seconds -> Option<Expr>));
-    impl_unit_setter!(with_milliseconds(milliseconds -> Option<Expr>));
-    impl_unit_setter!(with_microseconds(microseconds -> Option<Expr>));
-    impl_unit_setter!(with_nanoseconds(nanoseconds -> Option<Expr>));
+    impl_unit_setter!(with_weeks(weeks));
+    impl_unit_setter!(with_days(days));
+    impl_unit_setter!(with_hours(hours));
+    impl_unit_setter!(with_minutes(minutes));
+    impl_unit_setter!(with_seconds(seconds));
+    impl_unit_setter!(with_milliseconds(milliseconds));
+    impl_unit_setter!(with_microseconds(microseconds));
+    impl_unit_setter!(with_nanoseconds(nanoseconds));
 }
 
-/// Construct a column of `Duration` from the provided args.
+/// Construct a column of `Duration` from the provided [`DurationArgs`].
 #[cfg(feature = "temporal")]
 pub fn duration(args: DurationArgs) -> Expr {
     let function = SpecialEq::new(Arc::new(move |s: &mut [Series]| {
@@ -678,14 +732,14 @@ pub fn duration(args: DurationArgs) -> Expr {
 
     Expr::AnonymousFunction {
         input: vec![
-            args.days.unwrap_or_else(|| lit(0i64)),
-            args.seconds.unwrap_or_else(|| lit(0i64)),
-            args.nanoseconds.unwrap_or_else(|| lit(0i64)),
-            args.microseconds.unwrap_or_else(|| lit(0i64)),
-            args.milliseconds.unwrap_or_else(|| lit(0i64)),
-            args.minutes.unwrap_or_else(|| lit(0i64)),
-            args.hours.unwrap_or_else(|| lit(0i64)),
-            args.weeks.unwrap_or_else(|| lit(0i64)),
+            args.days,
+            args.seconds,
+            args.nanoseconds,
+            args.microseconds,
+            args.milliseconds,
+            args.minutes,
+            args.hours,
+            args.weeks,
         ],
         function,
         output_type: GetOutput::from_type(DataType::Duration(TimeUnit::Nanoseconds)),
