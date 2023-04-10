798f265e549d91c6233c3a4c87fc600bb3d93cdf
MarcoGorelli <>
2023-04-10 20:15:14 +0100

    pre-commit

--- polars/polars-lazy/polars-plan/src/dsl/function_expr/mod.rs
+++ polars/polars-lazy/polars-plan/src/dsl/function_expr/mod.rs
@@ -482,7 +482,9 @@ impl From<TemporalFunction> for SpecialEq<Arc<dyn SeriesUdf>> {
             Truncate(every, offset) => map!(datetime::truncate, &every, &offset),
             Round(every, offset) => map!(datetime::round, &every, &offset),
             #[cfg(feature = "timezones")]
-            CastTimezone(tz, use_earliest) => map!(datetime::replace_timezone, tz.as_deref(), use_earliest),
+            CastTimezone(tz, use_earliest) => {
+                map!(datetime::replace_timezone, tz.as_deref(), use_earliest)
+            }
             #[cfg(feature = "timezones")]
             TzLocalize(tz) => map!(datetime::tz_localize, &tz),
             Combine(tu) => map_as_slice!(temporal::combine, tu),
