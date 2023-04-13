e17144f60efe438bde648030b30b2440ca5ead8e
ritchie <ritchie46@gmail.com>
2023-04-13 13:06:44 +0200

    perf(rust, python): `arr.eval` run on expression engine when possible

--- polars/polars-lazy/src/dsl/mod.rs
+++ polars/polars-lazy/src/dsl/mod.rs
@@ -34,6 +34,7 @@
 mod eval;
 pub mod functions;
 mod into;
+#[cfg(feature = "list_eval")]
 mod list;
 
 #[cfg(any(feature = "cumulative_eval", feature = "list_eval"))]
@@ -41,6 +42,7 @@ pub use eval::*;
 pub use functions::*;
 #[cfg(any(feature = "cumulative_eval", feature = "list_eval"))]
 use into::IntoExpr;
+#[cfg(feature = "list_eval")]
 pub use list::*;
 pub use polars_plan::dsl::*;
 pub use polars_plan::logical_plan::UdfSchema;
