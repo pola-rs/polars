3e144fb9490656135273a723f952a982a9a6ef17
universalmind303 <universalmind.candy@gmail.com>
2023-04-14 09:57:08 -0500

    wip

--- examples/read_csv/src/main.rs
+++ examples/read_csv/src/main.rs
@@ -1,6 +1,18 @@
 use polars::prelude::*;
 
 fn main() -> PolarsResult<()> {
+    let mut buf = br#"foo,bar\n1,2"#;
+    let mut cursor = std::io::Cursor::new(buf);
+    let _ = polars::read_parquet!(&mut cursor);
+    let path = std::path::Path::new("foo.parquet");
+    let mut cursor = std::io::Cursor::new(buf);
+    let _ = polars::read_parquet!(&mut cursor);
+    
+    let _ = polars::read_parquet!(&mut buf);
+    let _ = polars::read_parquet!(&path);
+    let _ = polars::read_parquet!(path);
+    let _ = polars::read_parquet!("foo.parquet");
+
     let _ = polars::read_csv!("foo.csv");
     let _ = polars::read_csv!("foo.csv", delimiter = b',');
     let _ = polars::scan_csv!("foo.csv");
