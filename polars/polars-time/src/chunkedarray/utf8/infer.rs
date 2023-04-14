8b0c362bbe12d0ddad1083b91b20a43e161cff9d
MarcoGorelli <>
2023-04-14 20:19:36 +0100

    pre-commit

--- polars/polars-time/src/chunkedarray/utf8/infer.rs
+++ polars/polars-time/src/chunkedarray/utf8/infer.rs
@@ -135,15 +135,6 @@ impl Pattern {
             },
         }
     }
-    // pub fn is_inferable_bytes(&self, val: &[u8]) -> bool {
-    //     match self {
-    //         Pattern::DateDMY => true, // there are very few Date patterns, so it's
-    //         Pattern::DateYMD => true, // cheaper to just try them
-    //         Pattern::DatetimeDMY => DATETIME_DMY_BYTES_RE.is_match(val),
-    //         Pattern::DatetimeYMD => DATETIME_YMD_BYTES_RE.is_match(val),
-    //         Pattern::DatetimeYMDZ => DATETIME_YMDZ_BYTES_RE.is_match(val),
-    //     }
-    // }
 }
 
 pub trait StrpTimeParser<T> {
@@ -163,9 +154,6 @@ impl StrpTimeParser<i64> for DatetimeInfer<i64> {
                 .or_else(|| {
                     // TODO! this will try all patterns.
                     // somehow we must early escape if value is invalid
-                    // if !self.pattern_with_offset.pattern.is_inferable_bytes(val) {
-                    //     return None;
-                    // }
                     for fmt in self.patterns {
                         if self.fmt_len == 0 {
                             self.fmt_len = strptime::fmt_len(fmt.as_bytes())?;
@@ -196,9 +184,8 @@ impl StrpTimeParser<i32> for DatetimeInfer<i32> {
                 .parse(val, self.latest_fmt.as_bytes(), self.fmt_len)
                 .map(|ndt| naive_date_to_date(ndt.date()))
                 .or_else(|| {
-                    // if !self.pattern_with_offset.pattern.is_inferable_bytes(val) {
-                    //     return None;
-                    // }
+                    // TODO! this will try all patterns.
+                    // somehow we must early escape if value is invalid
                     for fmt in self.patterns {
                         if self.fmt_len == 0 {
                             self.fmt_len = strptime::fmt_len(fmt.as_bytes())?;
