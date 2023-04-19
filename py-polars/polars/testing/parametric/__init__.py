2f508baaea5e9f266e6340d3392e23b33bda9f17
StefanBRas <28559054+StefanBRas@users.noreply.github.com>
2023-04-19 20:44:22 +0200

    fix(python): fix unintentional loading of hypothesis profile (#8362)
    Co-authored-by: sras <sras@baselifescience.com>

--- py-polars/polars/testing/parametric/__init__.py
+++ py-polars/polars/testing/parametric/__init__.py
@@ -1,31 +1,8 @@
-import os
 from typing import Any
-
-from hypothesis import settings
 
 from polars.dependencies import _HYPOTHESIS_AVAILABLE
 
 if _HYPOTHESIS_AVAILABLE:
-    # Default profile (eg: running locally)
-    common_settings = {"print_blob": True, "deadline": None}
-    settings.register_profile(
-        name="polars.default",
-        max_examples=100,
-        **common_settings,  # type: ignore[arg-type]
-    )
-    # CI 'max' profile (10x the number of iterations).
-    # this is more expensive (about 4x slower), and not actually
-    # enabled in our usual CI pipeline; requires explicit opt-in.
-    settings.register_profile(
-        name="polars.ci",
-        max_examples=1000,
-        **common_settings,  # type: ignore[arg-type]
-    )
-    if os.getenv("CI_MAX"):
-        settings.load_profile("polars.ci")
-    else:
-        settings.load_profile("polars.default")
-
     from polars.testing.parametric.primitives import (
         column,
         columns,
