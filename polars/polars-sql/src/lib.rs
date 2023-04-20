ccd71de540873dde0856ff57440410419d656ba2
Cory Grinstead <universalmind.candy@gmail.com>
2023-04-20 14:14:12 -0500

    feat(python,rust,cli): add support for SQL "IN" expr (#8396)

--- polars/polars-sql/src/lib.rs
+++ polars/polars-sql/src/lib.rs
@@ -835,4 +835,24 @@ mod test {
 
         Ok(())
     }
+
+    #[test]
+    #[cfg(feature = "csv")]
+    fn iss_8395() -> PolarsResult<()> {
+        let mut context = SQLContext::new();
+        let sql = r#"
+        with foods as (
+            SELECT *
+            FROM read_csv('../../examples/datasets/foods1.csv')
+        )
+        select * from foods where category IN ('vegetables', 'seafood')"#;
+        let res = context.execute(sql)?;
+        let df = res.collect()?;
+
+        // assert that the df only contains [vegetables, seafood]
+        let s = df.column("category")?.unique()?.sort(false);
+        let expected = Series::new("category", &["seafood", "vegetables"]);
+        assert!(s.series_equal(&expected));
+        Ok(())
+    }
 }
