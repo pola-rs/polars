73e060b43b44ab29a704d88fd8667c68884daff0
MarcoGorelli <>
2023-04-23 13:52:17 +0100

    satisfy features check

--- polars/polars-time/src/month_start.rs
+++ polars/polars-time/src/month_start.rs
@@ -110,6 +110,6 @@ impl PolarsMonthStart for DateChunked {
         _tz: Option<&impl PolarsTimeZone>,
     ) -> PolarsResult<Self> {
         let no_offset = Duration::parse("0ns");
-        self.truncate(Duration::parse("1mo"), no_offset, NO_TIMEZONE)
+        PolarsTruncate::truncate(self, Duration::parse("1mo"), no_offset, NO_TIMEZONE)
     }
 }
