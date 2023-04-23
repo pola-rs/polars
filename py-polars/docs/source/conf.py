70e2a496f5b6d2a6082e96cb25f3a593f0e0b0b4
alexander-beedie <alexander.m.beedie@icloud.com>
2023-04-23 18:11:16 +0400

    docs(python): integrate live refresh/reload facility while writing polars docs

--- py-polars/docs/source/conf.py
+++ py-polars/docs/source/conf.py
@@ -64,7 +64,7 @@ templates_path = ["_templates", sphinx_autosummary_accessors.templates_path]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
