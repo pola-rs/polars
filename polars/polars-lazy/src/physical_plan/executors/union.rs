b862c9c4df0cd9b51f9a89b0a8de645ab1570ba9
Joshua Peek <josh@users.noreply.github.com>
2023-04-24 01:56:35 +0000

    perf(rust): use par_iter over entire concat input

--- polars/polars-lazy/src/physical_plan/executors/union.rs
+++ polars/polars-lazy/src/physical_plan/executors/union.rs
@@ -20,7 +20,7 @@ impl Executor for UnionExec {
         if _is_fetch_query() {
             self.options.parallel = false;
         }
-        let mut inputs = std::mem::take(&mut self.inputs);
+        let inputs = std::mem::take(&mut self.inputs);
 
         let sliced_path = if let Some((offset, _)) = self.options.slice {
             offset >= 0
@@ -82,28 +82,19 @@ impl Executor for UnionExec {
                 println!("UNION: union is run in parallel")
             }
 
-            // we don't use par_iter directly because the LP may also start threads for every LP (for instance scan_csv)
-            // this might then lead to a rayon SO. So we take a multitude of the threads to keep work stealing
-            // within bounds
-            let out = POOL.install(|| {
+            let dfs = POOL.install(|| {
                 inputs
-                    .chunks_mut(POOL.current_num_threads() * 3)
-                    .map(|chunk| {
-                        chunk
-                            .into_par_iter()
-                            .enumerate()
-                            .map(|(idx, input)| {
-                                let mut input = std::mem::take(input);
-                                let mut state = state.split();
-                                state.branch_idx += idx;
-                                input.execute(&mut state)
-                            })
-                            .collect::<PolarsResult<Vec<_>>>()
+                    .into_par_iter()
+                    .enumerate()
+                    .map(|(idx, mut input)| {
+                        let mut state = state.split();
+                        state.branch_idx += idx;
+                        input.execute(&mut state)
                     })
                     .collect::<PolarsResult<Vec<_>>>()
             });
 
-            concat_df(out?.iter().flat_map(|dfs| dfs.iter()))
+            concat_df(dfs.iter().flatten())
         }
     }
 }
