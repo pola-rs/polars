4df54954a213a4d242db28c6154fd4d7160d145d
MarcoGorelli <>
2023-04-24 11:19:39 +0100

    fixup

--- py-polars/polars/expr/datetime.py
+++ py-polars/polars/expr/datetime.py
@@ -1670,8 +1670,8 @@ class ExprDateTimeNameSpace:
             - 1d    (1 day)
             - 1w    (1 week)
             - 1mo   (1 calendar month)
-            - 1mo_saturating (same as above, but saturates to the last day of the month if the target
-              date does not exist)
+            - 1mo_saturating (same as above, but saturates to the last day of the month
+              if the target date does not exist)
             - 1y    (1 calendar year)
             - 1i    (1 index count)
 
