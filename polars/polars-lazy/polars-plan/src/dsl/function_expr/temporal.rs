274567ce715192ecd9ac277b6cbdb8f6c84d66b8
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-04-25 10:38:54 +0100

    perf(rust): remove some unnecessary calls and matches (#8490)
    Co-authored-by: MarcoGorelli <>

--- polars/polars-lazy/polars-plan/src/dsl/function_expr/temporal.rs
+++ polars/polars-lazy/polars-plan/src/dsl/function_expr/temporal.rs
@@ -21,7 +21,7 @@ pub(super) fn date_offset(s: Series, offset: Duration) -> PolarsResult<Series> {
         DataType::Datetime(tu, tz) => {
             let ca = s.datetime().unwrap();
 
-            fn adder<T: PolarsTimeZone>(
+            fn offset_fn<T: PolarsTimeZone>(
                 tu: TimeUnit,
             ) -> fn(&Duration, i64, Option<&T>) -> PolarsResult<i64> {
                 match tu {
@@ -34,13 +34,22 @@ pub(super) fn date_offset(s: Series, offset: Duration) -> PolarsResult<Series> {
             let out = match tz {
                 #[cfg(feature = "timezones")]
                 Some(ref tz) => match tz.parse::<Tz>() {
-                    Ok(tz) => ca.0.try_apply(|v| adder(tu)(&offset, v, Some(&tz))),
+                    Ok(tz) => {
+                        let offset_fn = offset_fn(tu);
+                        ca.0.try_apply(|v| offset_fn(&offset, v, Some(&tz)))
+                    }
                     Err(_) => match parse_offset(tz) {
-                        Ok(tz) => ca.0.try_apply(|v| adder(tu)(&offset, v, Some(&tz))),
+                        Ok(tz) => {
+                            let offset_fn = offset_fn(tu);
+                            ca.0.try_apply(|v| offset_fn(&offset, v, Some(&tz)))
+                        }
                         Err(_) => unreachable!(),
                     },
                 },
-                _ => ca.0.try_apply(|v| adder(tu)(&offset, v, NO_TIMEZONE)),
+                _ => {
+                    let offset_fn = offset_fn(tu);
+                    ca.0.try_apply(|v| offset_fn(&offset, v, NO_TIMEZONE))
+                }
             }?;
             out.cast(&DataType::Datetime(tu, tz))
         }
