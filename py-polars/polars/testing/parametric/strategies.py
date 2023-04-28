0932e34d3c948da342885a4904c07a7fa419bbef
alexander-beedie <alexander.m.beedie@icloud.com>
2023-04-29 01:07:44 +0400

    docs(python): add more detail to internal strategies docstrings and comments

--- py-polars/polars/testing/parametric/strategies.py
+++ py-polars/polars/testing/parametric/strategies.py
@@ -5,7 +5,15 @@ from datetime import datetime, timedelta
 from itertools import chain
 from random import choice, shuffle
 from string import ascii_letters, ascii_uppercase, digits, punctuation
-from typing import TYPE_CHECKING, Any, Callable, Iterator, MutableMapping, Sequence
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Iterator,
+    Mapping,
+    MutableMapping,
+    Sequence,
+)
 
 from hypothesis.strategies import (
     SearchStrategy,
@@ -61,6 +69,12 @@ if TYPE_CHECKING:
         from typing_extensions import Self
 
 
+def between(draw: DrawFn, type_: type, min_: Any, max_: Any) -> Any:
+    """Draw a value in a given range from a type-inferred strategy."""
+    strategy_init = from_type(type_).function  # type: ignore[attr-defined]
+    return draw(strategy_init(min_, max_))
+
+
 # scalar dtype strategies are largely straightforward, mapping directly
 # onto the associated hypothesis strategy, with dtype-defined limits
 strategy_bool = booleans()
@@ -97,17 +111,13 @@ strategy_duration = timedeltas(
 )
 
 
-def between(draw: DrawFn, type_: type, min_: Any, max_: Any) -> Any:
-    """Draw a value in a given range from a type-inferred strategy."""
-    strategy_init = from_type(type_).function  # type: ignore[attr-defined]
-    return draw(strategy_init(min_, max_))
-
-
 @composite
 def strategy_decimal(draw: DrawFn) -> PyDecimal:
+    """Draw a decimal value, varying the number of decimal places."""
     places = draw(integers(min_value=0, max_value=18))
     return draw(
-        # TODO: once fixed, re-enable decimal nan/inf values.
+        # TODO: once fixed, re-enable decimal nan/inf values...
+        #  (see https://github.com/pola-rs/polars/issues/8421)
         decimals(
             allow_nan=False,
             allow_infinity=False,
@@ -135,12 +145,21 @@ class StrategyLookup(MutableMapping[PolarsDataType, SearchStrategy[Any]]):
     def __init__(
         self,
         items: (
-            dict[
+            Mapping[
                 PolarsDataType, SearchStrategy[Any] | Callable[..., SearchStrategy[Any]]
             ]
             | None
         ) = None,
     ):
+        """
+        Initialise lookup with the given dtype/strategy items.
+
+        Parameters
+        ----------
+        items
+            A dtype to strategy dict/mapping.
+
+        """
         self._items = {}
         if items is not None:
             self._items.update(items)
@@ -150,27 +169,39 @@ class StrategyLookup(MutableMapping[PolarsDataType, SearchStrategy[Any]]):
         item: PolarsDataType,
         value: SearchStrategy[Any] | Callable[..., SearchStrategy[Any]],
     ) -> None:
+        """Add a dtype and its associated strategy to the lookup."""
         self._items[item] = value
 
     def __delitem__(self, item: PolarsDataType) -> None:
+        """Remove the given dtype from the lookup."""
         del self._items[item]
 
     def __getitem__(self, item: PolarsDataType) -> SearchStrategy[Any]:
+        """Retrieve a hypothesis strategy for the given dtype."""
         strat = self._items[item]
+
+        # if the item is a scalar strategy, return it directly
         if isinstance(strat, SearchStrategy):
             return strat
+
+        # instantiate nested strategies on demand, using the inner dtype.
+        # if no inner dtype, a randomly selected dtype is assigned.
         return strat(inner_dtype=getattr(item, "inner", None))
 
     def __len__(self) -> int:
+        """Return the number of items in the lookup."""
         return len(self._items)
 
     def __iter__(self) -> Iterator[PolarsDataType]:
+        """Iterate over the lookup's dtype keys."""
         yield from self._items
 
     def __or__(self, other: StrategyLookup) -> StrategyLookup:
+        """Create a new StrategyLookup from the union of this lookup and another."""
         return StrategyLookup().update(self).update(other)
 
     def update(self, items: StrategyLookup) -> Self:  # type: ignore[override]
+        """Add new strategy items to the lookup."""
         self._items.update(items)
         return self
 
@@ -213,6 +244,17 @@ def _get_strategy_dtypes(
     base_type: bool = False,
     excluding: tuple[PolarsDataType] | PolarsDataType | None = None,
 ) -> list[PolarsDataType]:
+    """
+    Get a list of all the dtypes for which we have a strategy.
+
+    Parameters
+    ----------
+    base_type
+        If True, return the base types for each dtype (eg:``List(Utf8)`` → ``List``).
+    excluding
+        A dtype or sequence of dtypes to omit from the results.
+
+    """
     excluding = (excluding,) if is_polars_dtype(excluding) else (excluding or ())  # type: ignore[assignment]
     strategy_dtypes = list(chain(scalar_strategies.keys(), nested_strategies.keys()))
     return [
