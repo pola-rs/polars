2ac652bcb86534a923b0d31fa2773c965bc6da46
Kyle Barron <kylebarron2@gmail.com>
2023-04-28 19:35:52 -0400

    Comment out NamedFrom

--- polars/polars-core/src/named_from.rs
+++ polars/polars-core/src/named_from.rs
@@ -9,9 +9,7 @@ use chrono::NaiveDateTime;
 #[cfg(feature = "dtype-time")]
 use chrono::NaiveTime;
 
-use crate::chunked_array::builder::{
-    get_fixed_size_list_builder, get_list_builder, AnonymousListBuilder,
-};
+use crate::chunked_array::builder::{get_list_builder, AnonymousListBuilder};
 use crate::prelude::*;
 
 pub trait NamedFrom<T, Phantom: ?Sized> {
@@ -146,32 +144,34 @@ impl<T: AsRef<[Series]>> NamedFrom<T, ListType> for Series {
     }
 }
 
-#[cfg(feature = "dtype-fixed-size-list")]
-impl<T: AsRef<[Series]>> NamedFrom<T, FixedSizeListType> for Series {
-    fn new(name: &str, s: T) -> Self {
-        let series_slice = s.as_ref();
-        let list_cap = series_slice.len();
+// #[cfg(feature = "dtype-fixed-size-list")]
+// impl<T: AsRef<([Series], usize)>> NamedFrom<T, FixedSizeListType> for Series {
+//     fn new(name: &str, s: T) -> Self {
+//         use crate::chunked_array::builder::{get_fixed_size_list_builder, AnonymousFixedSizeListBuilder};
 
-        let dt = series_slice[0].dtype();
+//         let (series_slice, inner_size) = s.as_ref();
+//         let list_cap = series_slice.len();
 
-        // inner type is also list so we need the anonymous builder
-        if let DataType::FixedSizeList(_, _) = dt {
-            let mut builder = AnonymousListBuilder::new(name, list_cap, Some(dt.clone()));
-            for s in series_slice {
-                builder.append_series(s)
-            }
-            builder.finish().into_series()
-        } else {
-            let values_cap = series_slice.iter().fold(0, |acc, s| acc + s.len());
+//         let dt = series_slice[0].dtype();
 
-            let mut builder = get_fixed_size_list_builder(dt, values_cap, list_cap, name).unwrap();
-            for series in series_slice {
-                builder.append_series(series)
-            }
-            builder.finish().into_series()
-        }
-    }
-}
+//         // inner type is also list so we need the anonymous builder
+//         if let DataType::FixedSizeList(_, _) = dt {
+//             let mut builder = AnonymousFixedSizeListBuilder::new(name, list_cap, Some(dt.clone()), inner_size);
+//             for s in series_slice {
+//                 builder.append_series(s)
+//             }
+//             builder.finish().into_series()
+//         } else {
+//             let values_cap = series_slice.iter().fold(0, |acc, s| acc + s.len());
+
+//             let mut builder = get_fixed_size_list_builder(dt, values_cap, list_cap, name).unwrap();
+//             for series in series_slice {
+//                 builder.append_series(series)
+//             }
+//             builder.finish().into_series()
+//         }
+//     }
+// }
 
 impl<T: AsRef<[Option<Series>]>> NamedFrom<T, [Option<Series>]> for Series {
     fn new(name: &str, s: T) -> Self {
