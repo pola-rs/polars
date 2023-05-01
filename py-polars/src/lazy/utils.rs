483ec7a259687ebb9a738511369d672714680901
Stijn de Gooijer <stijn@degooijer.io>
2023-05-01 17:39:39 +0200

    refactor(python): Remove duplicate util in Rust bindings

--- py-polars/src/lazy/utils.rs
+++
@@ -1,9 +0,0 @@
-use polars::lazy::dsl::Expr;
-
-use crate::lazy::dsl::PyExpr;
-
-pub fn py_exprs_to_exprs(py_exprs: Vec<PyExpr>) -> Vec<Expr> {
-    // Safety:
-    // transparent struct
-    unsafe { std::mem::transmute(py_exprs) }
-}
