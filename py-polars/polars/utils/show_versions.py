dd5d322ccd799b451f22fae63cc5d2ecc90078b4
Marshall <mcrumiller@users.noreply.github.com>
2023-05-01 15:43:45 -0400

    Remove brackets for clarity and performance
    Co-authored-by: Stijn de Gooijer <stijn@degooijer.io>

--- py-polars/polars/utils/show_versions.py
+++ py-polars/polars/utils/show_versions.py
@@ -39,10 +39,8 @@ def show_versions() -> None:
     # determine key length for alignment
     keylen = (
         max(
-            [
-                len(x)
-                for x in [*deps.keys(), "Polars", "Index type", "Platform", "Python"]
-            ]
+            len(x)
+            for x in [*deps.keys(), "Polars", "Index type", "Platform", "Python"]
         )
         + 1
     )
