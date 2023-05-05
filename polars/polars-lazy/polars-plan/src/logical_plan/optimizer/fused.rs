a04dffa95abab17d788b10e0a0f61b48f1164d94
ritchie <ritchie46@gmail.com>
2023-05-05 17:05:23 +0200

    perf(rust, python): add fused multiply add optimization for expressions

--- polars/polars-lazy/polars-plan/src/logical_plan/optimizer/fused.rs
+++ polars/polars-lazy/polars-plan/src/logical_plan/optimizer/fused.rs
@@ -27,75 +27,74 @@ impl OptimizationRule for FusedArithmetic {
 
         use AExpr::*;
         match expr {
-            BinaryExpr { left, op, right } => {
-                match op {
-                    // For fma the plus is always the out as the multiply takes prevalence
-                    Operator::Plus => {
-                        match expr_arena.get(*left) {
-                            // Argument order is a + b * c
-                            // so we must swap operands
-                            //
-                            // input
-                            // (a * b) + c
-                            // swapped as
-                            // c + (a * b)
-                            BinaryExpr {
-                                left: a,
-                                op: Operator::Multiply,
-                                right: b,
-                            } => {
-                                let Some(input_node) = lp_arena.get(lp_node).get_input() else {return Ok(None)};
-                                let schema = lp_arena.get(input_node).schema(lp_arena);
-                                let type_left = expr_arena.get(*left).get_type(
-                                    &schema,
-                                    Context::Default,
-                                    expr_arena,
-                                )?;
-                                let type_right = expr_arena.get(*right).get_type(
-                                    &schema,
-                                    Context::Default,
-                                    expr_arena,
-                                )?;
-                                if type_left.is_numeric() && type_right.is_numeric() {
-                                    let input = vec![*right, *a, *b];
-                                    Ok(Some(get_expr(input)))
-                                } else {
-                                    Ok(None)
-                                }
-                            }
-                            _ => match expr_arena.get(*right) {
-                                // input
-                                // (a + (b * c)
-                                // kept as input
-                                BinaryExpr {
-                                    left: a,
-                                    op: Operator::Multiply,
-                                    right: b,
-                                } => {
-                                    let Some(input_node) = lp_arena.get(lp_node).get_input() else {return Ok(None)};
-                                    let schema = lp_arena.get(input_node).schema(lp_arena);
-                                    let type_left = expr_arena.get(*left).get_type(
-                                        &schema,
-                                        Context::Default,
-                                        expr_arena,
-                                    )?;
-                                    let type_right = expr_arena.get(*right).get_type(
-                                        &schema,
-                                        Context::Default,
-                                        expr_arena,
-                                    )?;
-                                    if type_left.is_numeric() && type_right.is_numeric() {
-                                        let input = vec![*left, *a, *b];
-                                        Ok(Some(get_expr(input)))
-                                    } else {
-                                        Ok(None)
-                                    }
-                                }
-                                _ => Ok(None),
-                            },
+            BinaryExpr {
+                left,
+                op: Operator::Plus,
+                right,
+            } => {
+                // For fma the plus is always the out as the multiply takes prevalence
+                match expr_arena.get(*left) {
+                    // Argument order is a + b * c
+                    // so we must swap operands
+                    //
+                    // input
+                    // (a * b) + c
+                    // swapped as
+                    // c + (a * b)
+                    BinaryExpr {
+                        left: a,
+                        op: Operator::Multiply,
+                        right: b,
+                    } => {
+                        let Some(input_node) = lp_arena.get(lp_node).get_input() else {return Ok(None)};
+                        let schema = lp_arena.get(input_node).schema(lp_arena);
+                        let type_left = expr_arena.get(*left).get_type(
+                            &schema,
+                            Context::Default,
+                            expr_arena,
+                        )?;
+                        let type_right = expr_arena.get(*right).get_type(
+                            &schema,
+                            Context::Default,
+                            expr_arena,
+                        )?;
+                        if type_left.is_numeric() && type_right.is_numeric() {
+                            let input = vec![*right, *a, *b];
+                            Ok(Some(get_expr(input)))
+                        } else {
+                            Ok(None)
                         }
                     }
-                    _ => Ok(None),
+                    _ => match expr_arena.get(*right) {
+                        // input
+                        // (a + (b * c)
+                        // kept as input
+                        BinaryExpr {
+                            left: a,
+                            op: Operator::Multiply,
+                            right: b,
+                        } => {
+                            let Some(input_node) = lp_arena.get(lp_node).get_input() else {return Ok(None)};
+                            let schema = lp_arena.get(input_node).schema(lp_arena);
+                            let type_left = expr_arena.get(*left).get_type(
+                                &schema,
+                                Context::Default,
+                                expr_arena,
+                            )?;
+                            let type_right = expr_arena.get(*right).get_type(
+                                &schema,
+                                Context::Default,
+                                expr_arena,
+                            )?;
+                            if type_left.is_numeric() && type_right.is_numeric() {
+                                let input = vec![*left, *a, *b];
+                                Ok(Some(get_expr(input)))
+                            } else {
+                                Ok(None)
+                            }
+                        }
+                        _ => Ok(None),
+                    },
                 }
             }
             _ => Ok(None),
