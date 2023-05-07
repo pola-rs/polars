d482b0786cd55ddc1baa44564bb06faffaeca3fc
Marshall Crumiller <mcrumiller@gmail.com>
2023-05-07 17:35:34 -0400

    Fix format and duplicate test

--- py-polars/tests/unit/test_exprs.py
+++ py-polars/tests/unit/test_exprs.py
@@ -1084,52 +1084,14 @@ def test_cache_expr(monkeypatch: Any, capfd: Any) -> None:
     ],
 )
 def test_extend_constant(const: Any, dtype: pl.PolarsDataType) -> None:
-    df = pl.DataFrame({
-        'a': pl.Series("s", [None], dtype=dtype)
-    })
-    
-    expected = pl.DataFrame({
-        'a': pl.Series("s", [None, const, const, const], dtype=dtype)
-    }) 
-    
-    assert_series_equal(
-        df.select(
-            pl.col('a').extend_constant(const, 3)
-        ),
-        expected
+    df = pl.DataFrame({"a": pl.Series("s", [None], dtype=dtype)})
+
+    expected = pl.DataFrame(
+        {"a": pl.Series("s", [None, const, const, const], dtype=dtype)}
     )
 
+    assert_frame_equal(df.select(pl.col("a").extend_constant(const, 3)), expected)
 
-@pytest.mark.parametrize(
-    ("const", "dtype"),
-    [
-        (1, pl.Int8),
-        (4, pl.UInt32),
-        (4.5, pl.Float32),
-        (None, pl.Float64),
-        ("白鵬翔", pl.Utf8),
-        (date.today(), pl.Date),
-        (datetime.now(), pl.Datetime("ns")),
-        (time(23, 59, 59), pl.Time),
-        (timedelta(hours=7, seconds=123), pl.Duration("ms")),
-    ],
-)
-def test_extend_constant(const: Any, dtype: pl.PolarsDataType) -> None:
-    df = pl.DataFrame({
-        'a': pl.Series([None], dtype=dtype)
-    })
-    
-    expected = pl.DataFrame({
-        'a': pl.Series([None, const, const, const], dtype=dtype)
-    }) 
-    
-    assert_frame_equal(
-        df.select(
-            pl.col('a').extend_constant(const, 3)
-        ),
-        expected
-    )
-    
 
 @pytest.mark.parametrize(
     ("const", "dtype"),
@@ -1147,18 +1109,13 @@ def test_extend_constant(const: Any, dtype: pl.PolarsDataType) -> None:
 )
 def test_extend_constant_arr(const: Any, dtype: pl.PolarsDataType) -> None:
     """
-    Test extend_constant in pl.List array
-    
-    NOTE: This function currently fails when the Series is a list with a single [None] value.
-          Hence, this function does not begin with [[None]], but [[const]].
+    Test extend_constant in pl.List array.
+
+    NOTE: This function currently fails when the Series is a list with a single [None]
+          value. Hence, this function does not begin with [[None]], but [[const]].
     """
-    s = pl.Series('s', [[const]], dtype=pl.List(dtype))
-    
-    expected = pl.Series('s', [[const, const, const, const]], dtype=pl.List(dtype))
-    
-    assert_series_equal(
-        s.arr.eval(
-            pl.element().extend_constant(const, 3)
-        ),
-        expected
-    )
+    s = pl.Series("s", [[const]], dtype=pl.List(dtype))
+
+    expected = pl.Series("s", [[const, const, const, const]], dtype=pl.List(dtype))
+
+    assert_series_equal(s.arr.eval(pl.element().extend_constant(const, 3)), expected)
