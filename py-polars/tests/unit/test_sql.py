0a0dfcb9d3ec4f7d9b66b2e4a14551e64c96499f
Alexander Beedie <alexander-beedie@users.noreply.github.com>
2023-05-08 12:11:55 +0400

    feat(rust,python): add support for `USING` clause in SQL join operations (#8731)

--- py-polars/tests/unit/test_sql.py
+++ py-polars/tests/unit/test_sql.py
@@ -13,9 +13,9 @@ def foods_ipc_path() -> str:
 
 
 def test_sql_groupby(foods_ipc_path: Path) -> None:
-    c = pl.SQLContext()
-
     lf = pl.scan_ipc(foods_ipc_path)
+
+    c = pl.SQLContext()
     c.register("foods", lf)
 
     out = c.query(
@@ -40,59 +40,94 @@ def test_sql_groupby(foods_ipc_path: Path) -> None:
 
     lf = pl.LazyFrame(
         {
-            "group": ["a", "b", "c", "c", "b"],
-            "attr": ["x", "y", "x", "y", "y"],
+            "grp": ["a", "b", "c", "c", "b"],
+            "att": ["x", "y", "x", "y", "y"],
         }
     )
+    with pytest.raises(TypeError, match="Cannot register.*DataFrame.*use LazyFrame"):
+        c.register("test", lf.collect())  # type: ignore[arg-type]
+
     c.register("test", lf)
     out = c.query(
         """
         SELECT
-            group,
-            COUNT(DISTINCT attr) AS n_dist_attr
+            grp,
+            COUNT(DISTINCT att) AS n_dist_attr
         FROM test
-        GROUP BY group
+        GROUP BY grp
         HAVING n_dist_attr > 1
         """
     )
-    assert out.to_dict(False) == {"group": ["c"], "n_dist_attr": [2]}
+    assert out.to_dict(False) == {"grp": ["c"], "n_dist_attr": [2]}
 
 
-def test_sql_join(foods_ipc_path: Path) -> None:
+def test_sql_join_inner(foods_ipc_path: Path) -> None:
     lf = pl.scan_ipc(foods_ipc_path)
 
     c = pl.SQLContext()
     c.register_many(foods1=lf, foods2=lf)
-    out = c.query(
+
+    for join_clause in (
+        "ON foods1.category = foods2.category",
+        "USING (category)",
+    ):
+        out = c.query(
+            f"""
+            SELECT *
+            FROM foods1
+            INNER JOIN foods2 {join_clause}
+            LIMIT 2
+            """
+        )
+        assert out.to_dict(False) == {
+            "category": ["vegetables", "vegetables"],
+            "calories": [45, 20],
+            "fats_g": [0.5, 0.0],
+            "sugars_g": [2, 2],
+            "calories_right": [45, 45],
+            "fats_g_right": [0.5, 0.5],
+            "sugars_g_right": [2, 2],
+        }
+
+
+def test_sql_join_left() -> None:
+    frames = {
+        "tbl_a": pl.LazyFrame({"a": [1, 2, 3], "b": [4, None, 6]}),
+        "tbl_b": pl.LazyFrame({"a": [3, 2, 1], "b": [6, 5, 4], "c": ["x", "y", "z"]}),
+        "tbl_c": pl.LazyFrame({"c": ["w", "y", "z"], "d": [10.5, -50.0, 25.5]}),
+    }
+    c = pl.SQLContext()
+    c.register_many(frames)
+
+    out = c.execute(
         """
-        SELECT * FROM
-        foods1 INNER JOIN foods2 ON foods1.category = foods2.category
-        LIMIT 2
+        SELECT a, b, c, d
+        FROM tbl_a
+        LEFT JOIN tbl_b USING (a,b)
+        LEFT JOIN tbl_c USING (c)
+        ORDER BY c DESC
         """
     )
-    assert out.to_dict(False) == {
-        "category": ["vegetables", "vegetables"],
-        "calories": [45, 20],
-        "fats_g": [0.5, 0.0],
-        "sugars_g": [2, 2],
-        "calories_right": [45, 45],
-        "fats_g_right": [0.5, 0.5],
-        "sugars_g_right": [2, 2],
-    }
+    assert out.collect().rows() == [
+        (1, 4, "z", 25.5),
+        (2, None, "y", -50.0),
+        (3, 6, "x", None),
+    ]
 
 
 def test_sql_is_between(foods_ipc_path: Path) -> None:
-    c = pl.SQLContext()
-
     lf = pl.scan_ipc(foods_ipc_path)
+
+    c = pl.SQLContext()
     c.register("foods1", lf)
 
     out = c.query(
         """
-        SELECT * FROM foods1
+        SELECT *
+        FROM foods1
         WHERE foods1.calories BETWEEN 20 AND 31
         LIMIT 4
-        """
+    """
     )
 
     assert out.to_dict(False) == {
@@ -104,7 +139,8 @@ def test_sql_is_between(foods_ipc_path: Path) -> None:
 
     out = c.query(
         """
-        SELECT * FROM foods1
+        SELECT *
+        FROM foods1
         WHERE calories NOT BETWEEN 20 AND 31
         LIMIT 4
         """
@@ -119,14 +155,15 @@ def test_sql_is_between(foods_ipc_path: Path) -> None:
 
 
 def test_sql_trim(foods_ipc_path: Path) -> None:
-    c = pl.SQLContext()
-
     lf = pl.scan_ipc(foods_ipc_path)
+
+    c = pl.SQLContext()
     c.register("foods1", lf)
 
     out = c.query(
         """
-        SELECT TRIM(LEADING 'v' FROM category) FROM foods1
+        SELECT TRIM(LEADING 'v' FROM category)
+        FROM foods1
         LIMIT 2
         """
     )
