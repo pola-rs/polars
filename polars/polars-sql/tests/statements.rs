56652e6fa737c60d3fe77560a154794b2c75e05d
Cory Grinstead <universalmind.candy@gmail.com>
2023-05-19 01:39:41 -0500

    fix(cli,rust,python): sql qualified wildcards (#8916)

--- polars/polars-sql/tests/statements.rs
+++ polars/polars-sql/tests/statements.rs
@@ -43,3 +43,35 @@ fn select_exclude_multi() {
     let actual = ctx.execute(sql);
     assert!(actual.is_ok());
 }
+
+#[test]
+fn select_from_join() {
+    let df1 = df![
+        "a" => [1,2,3],
+        "b" => ["l", "m", "n"]
+    ]
+    .unwrap();
+    let df2 = df![
+        "a" => [4,2,3],
+        "c" => ["x", "y", "z"]
+    ]
+    .unwrap();
+
+    let expected = df![
+        "a" => [2,3],
+        "b" => ["m", "n"]
+    ]
+    .unwrap();
+    let mut ctx = SQLContext::new();
+    ctx.register("test", df1.lazy());
+    ctx.register("test2", df2.lazy());
+
+    let sql = r#"
+    SELECT test.* 
+    FROM test 
+    INNER JOIN test2 
+    USING(a)
+    "#;
+    let actual = ctx.execute(sql).unwrap().collect().unwrap();
+    assert!(actual.frame_equal(&expected));
+}
