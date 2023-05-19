4b6d0eb1589a79b500219a3d7c49eab0b062578e
universalmind303 <universalmind.candy@gmail.com>
2023-05-19 11:56:36 -0500

    feat(cli,rust,python): add sql compound identifiers

--- polars/polars-sql/tests/statements.rs
+++ polars/polars-sql/tests/statements.rs
@@ -45,7 +45,7 @@ fn select_exclude_multi() {
 }
 
 #[test]
-fn select_from_join() {
+fn select_qualified_wildcard() {
     let df1 = df![
         "a" => [1,2,3],
         "b" => ["l", "m", "n"]
@@ -68,6 +68,39 @@ fn select_from_join() {
 
     let sql = r#"
     SELECT test.* 
+    FROM test 
+    INNER JOIN test2 
+    USING(a)
+    "#;
+    let actual = ctx.execute(sql).unwrap().collect().unwrap();
+    assert!(actual.frame_equal(&expected));
+}
+
+#[test]
+fn select_qualified_column() {
+    let df1 = df![
+        "a" => [1,2,3],
+        "b" => ["l", "m", "n"]
+    ]
+    .unwrap();
+    let df2 = df![
+        "a" => [4,2,3],
+        "c" => ["x", "y", "z"]
+    ]
+    .unwrap();
+
+    let expected = df![
+        "b" => ["m", "n"],
+        "a" => [2,3],
+        "c" => ["y", "z"]
+    ]
+    .unwrap();
+    let mut ctx = SQLContext::new();
+    ctx.register("test", df1.lazy());
+    ctx.register("test2", df2.lazy());
+
+    let sql = r#"
+    SELECT test.b, test2.*
     FROM test 
     INNER JOIN test2 
     USING(a)
