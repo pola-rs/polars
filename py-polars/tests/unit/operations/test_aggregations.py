5199126396f9a94666f13133705478414c8ac3f8
Ritchie Vink <ritchie46@gmail.com>
2023-05-24 12:34:10 +0200

    fix(rust, python): raise error on invalid aggregation (#9013)

--- py-polars/tests/unit/operations/test_aggregations.py
+++ py-polars/tests/unit/operations/test_aggregations.py
@@ -232,3 +232,29 @@ def test_online_variance() -> None:
         .select(["no_nulls", "nulls"]),
         df.select(pl.all().exclude("id").std()),
     )
+
+
+def test_err_on_implode_and_agg() -> None:
+    df = pl.DataFrame({"type": ["water", "fire", "water", "earth"]})
+
+    # this would OOB
+    with pytest.raises(
+        pl.InvalidOperationError,
+        match=r"'implode' followed by an aggregation is not allowed",
+    ):
+        df.groupby("type").agg(pl.col("type").implode().first().alias("foo"))
+
+    # implode + function should be allowed in groupby
+    assert df.groupby("type", maintain_order=True).agg(
+        pl.col("type").implode().list.head().alias("foo")
+    ).to_dict(False) == {
+        "type": ["water", "fire", "earth"],
+        "foo": [["water", "water"], ["fire"], ["earth"]],
+    }
+
+    # but not during a window function as the groups cannot be mapped back
+    with pytest.raises(
+        pl.InvalidOperationError,
+        match=r"'implode' followed by an aggregation is not allowed",
+    ):
+        df.lazy().select(pl.col("type").implode().list.head(1).over("type")).collect()
