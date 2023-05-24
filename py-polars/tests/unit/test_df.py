d06c807ca1094ff10592184711445f6d30afadd2
Tobias Pitters <tobias.pitters@gmail.com>
2023-05-24 20:23:10 +0200

    update test_df

--- py-polars/tests/unit/test_df.py
+++ py-polars/tests/unit/test_df.py
@@ -1756,16 +1756,6 @@ def test_from_rows_of_dicts() -> None:
         assert df3.rows() == [(1, 100), (2, 101)]
         assert df3.schema == {"id": pl.Int16, "value": pl.Int32}
 
-def test_repeat_by_empty_series() -> None:
-    df = pl.DataFrame(
-        {
-            "a": ["x", "y", "z"],
-        }
-    )
-    expected = pl.DataFrame({"a": []})
-    result = df.select(pl.col("a").repeat_by(pl.Series([])))
-    assert_frame_equal(result, expected)
-
 def test_repeat_by_literal() -> None:
     df = pl.DataFrame(
         {
@@ -1775,6 +1765,7 @@ def test_repeat_by_literal() -> None:
     )
     expected = pl.DataFrame({"a": [["x", "x", "x"], ["y", "y", "y"], ["z", "z", "z"]]})
     result = df.select(pl.col("a").repeat_by("n"))
+    import pdb; pdb.set_trace()
     assert_frame_equal(result, expected)
 
 def test_repeat_by() -> None:
