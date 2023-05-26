bcb33c8985f05b82cedd9f8db8384f10a1c20249
Ritchie Vink <ritchie46@gmail.com>
2023-05-26 09:42:58 +0200

    feat(rust, python): implement apply for rolling/dynamic_groupby (#9049)

--- polars/polars-lazy/src/physical_plan/executors/groupby_rolling.rs
+++ polars/polars-lazy/src/physical_plan/executors/groupby_rolling.rs
@@ -1,4 +1,6 @@
 #[cfg(feature = "dynamic_groupby")]
+use polars_core::frame::groupby::GroupBy;
+#[cfg(feature = "dynamic_groupby")]
 use polars_time::RollingGroupOptions;
 
 use super::*;
@@ -12,6 +14,7 @@ pub(crate) struct GroupByRollingExec {
     pub(crate) options: RollingGroupOptions,
     pub(crate) input_schema: SchemaRef,
     pub(crate) slice: Option<(i64, usize)>,
+    pub(crate) apply: Option<Arc<dyn DataFrameUdf>>,
 }
 
 impl GroupByRollingExec {
@@ -30,6 +33,16 @@ impl GroupByRollingExec {
             .collect::<PolarsResult<Vec<_>>>()?;
 
         let (mut time_key, mut keys, groups) = df.groupby_rolling(keys, &self.options)?;
+
+        if let Some(f) = &self.apply {
+            let gb = GroupBy::new(&df, vec![], groups, None);
+            let out = gb.apply(move |df| f.call_udf(df))?;
+            return Ok(if let Some((offset, len)) = self.slice {
+                out.slice(offset, len)
+            } else {
+                out
+            });
+        }
 
         let mut groups = &groups;
         #[allow(unused_assignments)]
