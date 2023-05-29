23203f1454ac3bb0c819d243289b617e8767ade4
Marco Edward Gorelli <marcogorelli@protonmail.com>
2023-05-29 16:47:20 +0100

    fix(rust, python): rolling_groupy was returning incorrect results when offset was positive (#9082)

--- py-polars/tests/unit/operations/test_groupby_rolling.py
+++ py-polars/tests/unit/operations/test_groupby_rolling.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
 from datetime import datetime
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 import pytest
 
 import polars as pl
 from polars.testing import assert_frame_equal, assert_series_equal
+
+if TYPE_CHECKING:
+    from polars.type_aliases import ClosedInterval
 
 
 def bad_agg_parameters() -> list[Any]:
@@ -171,6 +174,56 @@ def test_groupby_rolling_negative_offset_crossing_dst(time_zone: str | None) -> 
                 eager=True,
             ),
             "value": [[1, 4], [4, 9], [9, 155], [155]],
+        }
+    )
+    assert_frame_equal(result, expected)
+
+
+@pytest.mark.parametrize("time_zone", [None, "US/Central"])
+@pytest.mark.parametrize(
+    ("offset", "closed", "expected_values"),
+    [
+        ("0d", "left", [[1, 4], [4, 9], [9, 155], [155]]),
+        ("0d", "right", [[4, 9], [9, 155], [155], []]),
+        ("0d", "both", [[1, 4, 9], [4, 9, 155], [9, 155], [155]]),
+        ("0d", "none", [[4], [9], [155], []]),
+        ("1d", "left", [[4, 9], [9, 155], [155], []]),
+        ("1d", "right", [[9, 155], [155], [], []]),
+        ("1d", "both", [[4, 9, 155], [9, 155], [155], []]),
+        ("1d", "none", [[9], [155], [], []]),
+    ],
+)
+def test_groupby_rolling_non_negative_offset_9077(
+    time_zone: str | None,
+    offset: str,
+    closed: ClosedInterval,
+    expected_values: list[list[int]],
+) -> None:
+    df = pl.DataFrame(
+        {
+            "datetime": pl.date_range(
+                datetime(2021, 11, 6),
+                datetime(2021, 11, 9),
+                "1d",
+                time_zone=time_zone,
+                eager=True,
+            ),
+            "value": [1, 4, 9, 155],
+        }
+    )
+    result = df.groupby_rolling(
+        index_column="datetime", period="2d", offset=offset, closed=closed
+    ).agg(pl.col("value"))
+    expected = pl.DataFrame(
+        {
+            "datetime": pl.date_range(
+                datetime(2021, 11, 6),
+                datetime(2021, 11, 9),
+                "1d",
+                time_zone=time_zone,
+                eager=True,
+            ),
+            "value": expected_values,
         }
     )
     assert_frame_equal(result, expected)
