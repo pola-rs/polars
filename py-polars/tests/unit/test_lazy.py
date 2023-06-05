9ec4a835562b7e199178b5d3c19919ec07f9bb72
Stijn de Gooijer <stijn@degooijer.io>
2023-06-05 10:05:02 +0200

    Restore test

--- py-polars/tests/unit/test_lazy.py
+++ py-polars/tests/unit/test_lazy.py
@@ -1098,9 +1098,11 @@ def test_self_join() -> None:
     out = (
         ldf.join(other=ldf, left_on="manager_id", right_on="employee_id", how="left")
         .select(
-            pl.col("employee_id"),
-            pl.col("employee_name"),
-            pl.col("employee_name_right").alias("manager_name"),
+            [
+                pl.col("employee_id"),
+                pl.col("employee_name"),
+                pl.col("employee_name_right").alias("manager_name"),
+            ]
         )
         .fetch()
     )
