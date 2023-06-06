2b27b78a2e8c47721568fba0534f3417dedcd550
Ritchie Vink <ritchie46@gmail.com>
2023-06-06 13:31:37 +0200

    fix(rust, python): fix cached reproject offsets (#9254)

--- polars/polars-lazy/polars-pipe/src/executors/operators/reproject.rs
+++ polars/polars-lazy/polars-pipe/src/executors/operators/reproject.rs
@@ -28,11 +28,18 @@ pub(crate) fn reproject_chunk(
     schema: &Schema,
 ) -> PolarsResult<()> {
     let out = if positions.is_empty() {
-        let out = chunk.data.select(schema.iter_names())?;
+        // use the chunk schema to cache
+        // the positions for subsequent calls
+        let chunk_schema = chunk.data.schema();
+
+        let out = chunk
+            .data
+            .select_with_schema_unchecked(schema.iter_names(), &chunk_schema)?;
+
         *positions = out
             .get_columns()
             .iter()
-            .map(|s| schema.get_full(s.name()).unwrap().0)
+            .map(|s| chunk_schema.get_full(s.name()).unwrap().0)
             .collect();
         out
     } else {
@@ -74,5 +81,40 @@ impl Operator for ReProjectOperator {
 
     fn fmt(&self) -> &str {
         "re-project-operator"
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use polars_core::prelude::*;
+
+    use super::*;
+
+    #[test]
+    fn test_reproject_chunk() {
+        let df = df![
+            "a" => [1, 2],
+            "b" => [1, 2],
+            "c" => [1, 2],
+            "d" => [1, 2],
+        ]
+        .unwrap();
+
+        let mut chunk1 = DataChunk::new(0, df.clone());
+        let mut chunk2 = DataChunk::new(1, df);
+
+        let mut positions = vec![];
+
+        let mut out_schema = Schema::new();
+        out_schema.with_column("c".into(), DataType::Int32);
+        out_schema.with_column("b".into(), DataType::Int32);
+        out_schema.with_column("d".into(), DataType::Int32);
+        out_schema.with_column("a".into(), DataType::Int32);
+
+        reproject_chunk(&mut chunk1, &mut positions, &out_schema).unwrap();
+        // second call cached the positions
+        reproject_chunk(&mut chunk2, &mut positions, &out_schema).unwrap();
+        assert_eq!(&chunk1.data.schema(), &out_schema);
+        assert_eq!(&chunk2.data.schema(), &out_schema);
     }
 }
