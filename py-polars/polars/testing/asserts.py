2e078041b70cfeab0178d5d57ee6b339f1ff5e6f
Daniel G, Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2023-06-08 15:25:34 +0200

    simplify and comment nan handling

--- py-polars/polars/testing/asserts.py
+++ py-polars/polars/testing/asserts.py
@@ -383,10 +383,8 @@ def _assert_series_inner(
                 mismatch = True
             elif comparing_float_dtypes:
                 # note: take special care with NaN values.
-                if (
-                    not nans_compare_equal
-                    and (left.is_nan() & (left.is_nan() == right.is_nan())).any()
-                ):
+                # if NaNs don't compare as equal, a single NaN is sufficient for a mismatch
+                if not nans_compare_equal and (left.is_nan() | right.is_nan()).any():
                     nan_info = " (nans_compare_equal=False)"
                     mismatch = True
                 elif (left.is_nan() != right.is_nan()).any():
