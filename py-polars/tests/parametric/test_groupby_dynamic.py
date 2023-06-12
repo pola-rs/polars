a38504c299d1c7add4f2cac8a6ae9d4c62827f01
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-06-12 09:47:24 +0100

    refactor a bit

--- py-polars/tests/parametric/test_groupby_dynamic.py
+++ py-polars/tests/parametric/test_groupby_dynamic.py
@@ -18,90 +18,48 @@ else:
 
 import pandas as pd
 import pytz
-from hypothesis import assume, given, reject
+from hypothesis import given, reject
 from hypothesis import strategies as st
 
 import polars as pl
 
 
-@given(
-    datetimes=st.lists(
-        st.datetimes(
-            min_value=dt.datetime(1980, 1, 1),
-            # Can't currently go beyond 2038, see
-            # https://github.com/pola-rs/polars/issues/9315
-            max_value=dt.datetime(2038, 1, 1),
-        ),
-        min_size=1,
-    ),
-    timezone=st.timezone_keys(),
-    closed=st.sampled_from(("left", "right")),
-    every_alias=st.sampled_from(
-        (
-            ("d", "D"),
-            ("mo", "MS"),
-            ("y", "YS"),
-        )
-    ),
-    number=st.integers(
-        min_value=1,
-        # Can't currently go above 1:
-        # https://github.com/pola-rs/polars/issues/9333
-        max_value=1,
-    ),
-    data=st.data(),
-)
-def test_against_pandas(
-    datetimes: list[dt.datetime],
-    closed: Literal["left", "right"],
-    timezone: str,
-    every_alias: tuple[str, str],
-    number: int,
+def _compare_polars_and_pandas(
+    *,
     data: st.DataObject,
-) -> None:
-    pl_every, pd_alias = every_alias
-    assume(timezone in zoneinfo.available_timezones())
+    datetimes: list[dt.datetime],
+    timezone: str,
+    pl_every: str,
+    pd_alias: str,
+    pl_startby: StartBy,
+    closed: Literal["left", "right"],
+    number: int,
+):
     nrows = len(datetimes)
     values = data.draw(
         st.lists(st.floats(10, 20), min_size=nrows, max_size=nrows), label="values"
     )
-    try:
-        df = (
-            pl.DataFrame({"ts": datetimes, "values": values})
-            .sort("ts")
-            .with_columns(
-                pl.col("ts").dt.replace_time_zone("UTC").dt.convert_time_zone(timezone)
-            )
+    df = (
+        pl.DataFrame({"ts": datetimes, "values": values})
+        .sort("ts")
+        .with_columns(
+            pl.col("ts").dt.replace_time_zone("UTC").dt.convert_time_zone(timezone)
         )
-    except pl.exceptions.ComputeError as exp:
-        assert "unable to parse time zone" in str(exp)  # noqa: PT017
-        reject()
+    )
 
-    try:
-        result = df.groupby_dynamic(
-            "ts",
-            every=f"{number}{pl_every}",
-            closed=closed,
-        ).agg(pl.col("values").sum())
-    except pl.exceptions.PolarsPanicError as exp:
-        # This computation may fail in the rare case that the beginning of a month
-        # lands on a DST transition.
-        assert "is non-existent" in str(exp) or "is ambiguous" in str(  # noqa: PT017
-            exp
-        )
-        reject()
+    result = df.groupby_dynamic(
+        "ts",
+        every=f"{number}{pl_every}",
+        start_by=pl_startby,
+        closed=closed,
+    ).agg(pl.col("values").sum())
 
-    try:
-        result_pd = (
-            df.to_pandas()
-            .resample(f"{number}{pd_alias}", closed=closed, label="left", on="ts")[
-                "values"
-            ]
-            .sum()
-            .reset_index()
-        )
-    except (pytz.exceptions.NonExistentTimeError, pytz.exceptions.AmbiguousTimeError):
-        reject()
+    result_pd = (
+        df.to_pandas()
+        .resample(f"{number}{pd_alias}", closed=closed, label="left", on="ts")["values"]
+        .sum()
+        .reset_index()
+    )
 
     # pandas fills in "holes", but polars doesn't
     # https://github.com/pola-rs/polars/issues/8831
@@ -121,7 +79,68 @@ def test_against_pandas(
         ),
         min_size=1,
     ),
-    timezone=st.timezone_keys(),
+    timezone=st.sampled_from(list(zoneinfo.available_timezones())),
+    closed=st.sampled_from(("left", "right")),
+    every_alias=st.sampled_from(
+        (
+            ("d", "D"),
+            ("mo", "MS"),
+            ("y", "YS"),
+        )
+    ),
+    number=st.integers(
+        min_value=1,
+        # Can't currently go above 1:
+        # https://github.com/pola-rs/polars/issues/9333
+        max_value=1,
+    ),
+    data=st.data(),
+)
+def test_non_weekly(
+    datetimes: list[dt.datetime],
+    closed: Literal["left", "right"],
+    timezone: str,
+    every_alias: tuple[str, str],
+    number: int,
+    data: st.DataObject,
+) -> None:
+    pl_every, pd_alias = every_alias
+    try:
+        _compare_polars_and_pandas(
+            data=data,
+            datetimes=datetimes,
+            pl_every=pl_every,
+            pd_alias=pd_alias,
+            pl_startby="window",
+            closed=closed,
+            number=number,
+            timezone=timezone,
+        )
+    except pl.exceptions.ComputeError as exp:
+        assert "unable to parse time zone" in str(exp)  # noqa: PT017
+        reject()
+    except pl.exceptions.PolarsPanicError as exp:
+        # This computation may fail in the rare case that the beginning of a month
+        # lands on a DST transition.
+        assert "is non-existent" in str(exp) or "is ambiguous" in str(  # noqa: PT017
+            exp
+        )
+        reject()
+    except (pytz.exceptions.NonExistentTimeError, pytz.exceptions.AmbiguousTimeError):
+        reject()
+
+
+@given(
+    datetimes=st.lists(
+        st.datetimes(
+            min_value=dt.datetime(1980, 1, 1),
+            # Can't currently go beyond 2038, see
+            # https://github.com/pola-rs/polars/issues/9315
+            max_value=dt.datetime(2038, 1, 1),
+        ),
+        min_size=1,
+    ),
+    timezone=st.sampled_from(list(zoneinfo.available_timezones())),
     closed=st.sampled_from(
         (
             "left",
@@ -148,7 +167,7 @@ def test_against_pandas(
     ),
     data=st.data(),
 )
-def test_against_pandas_weekly(
+def test_weekly(
     datetimes: list[dt.datetime],
     closed: Literal["left", "right"],
     timezone: str,
@@ -162,27 +181,20 @@ def test_against_pandas_weekly(
     # Trying to do so at the moment results in hypothesis complaining
     # about too many skipped tests.
     pl_every, pd_alias, pl_startby = every_alias_startby
-    assume(timezone in zoneinfo.available_timezones())
-    nrows = len(datetimes)
-    values = data.draw(
-        st.lists(st.floats(10, 20), min_size=nrows, max_size=nrows), label="values"
-    )
     try:
-        df = (
-            pl.DataFrame({"ts": datetimes, "values": values})
-            .sort("ts")
-            .with_columns(
-                pl.col("ts").dt.replace_time_zone("UTC").dt.convert_time_zone(timezone)
-            )
+        _compare_polars_and_pandas(
+            data=data,
+            datetimes=datetimes,
+            pl_every=pl_every,
+            pd_alias=pd_alias,
+            pl_startby=pl_startby,
+            closed=closed,
+            number=number,
+            timezone=timezone,
         )
     except pl.exceptions.ComputeError as exp:
         assert "unable to parse time zone" in str(exp)  # noqa: PT017
         reject()
-
-    try:
-        result = df.groupby_dynamic(
-            "ts", every=f"{number}{pl_every}", closed=closed, start_by=pl_startby
-        ).agg(pl.col("values").sum())
     except pl.exceptions.PolarsPanicError as exp:
         # This computation may fail in the rare case that the beginning of a month
         # lands on a DST transition.
@@ -190,22 +202,5 @@ def test_against_pandas_weekly(
             exp
         )
         reject()
-
-    try:
-        result_pd = (
-            df.to_pandas()
-            .resample(f"{number}{pd_alias}", on="ts", closed=closed, label="left")[
-                "values"
-            ]
-            .sum()
-            .reset_index()
-        )
     except (pytz.exceptions.NonExistentTimeError, pytz.exceptions.AmbiguousTimeError):
         reject()
-
-    # pandas fills in "holes", but polars doesn't
-    # https://github.com/pola-rs/polars/issues/8831
-    result_pd = result_pd[result_pd["values"] != 0.0].reset_index(drop=True)
-
-    result_pl = result.to_pandas()
-    pd.testing.assert_frame_equal(result_pd, result_pl)
