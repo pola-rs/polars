bc9e71eeb401a48b4c2e630c3a472b14849cc9bd
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-06-24 15:12:28 +0100

    fix(rust, python): fix lazy schema for date_range and time_range with alias

--- polars/polars-lazy/polars-plan/src/dsl/function_expr/schema.rs
+++ polars/polars-lazy/polars-plan/src/dsl/function_expr/schema.rs
@@ -59,8 +59,8 @@ impl FunctionExpr {
                     }
                     #[cfg(feature = "timezones")]
                     TzLocalize(tz) => return mapper.map_datetime_dtype_timezone(Some(tz)),
-                    DateRange { .. } => return mapper.map_to_supertype(),
-                    TimeRange { .. } => DataType::Time,
+                    DateRange { .. } => return mapper.map_to_list_supertype(),
+                    TimeRange { .. } => DataType::List(Box::new(DataType::Time)),
                     Combine(tu) => match mapper.with_same_dtype().unwrap().dtype {
                         DataType::Datetime(_, tz) => DataType::Datetime(*tu, tz),
                         DataType::Date => DataType::Datetime(*tu, None),
