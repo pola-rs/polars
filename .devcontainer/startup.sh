e4b7761c53906db5fe6fcb492dc23be0f644b95d
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-07-03 20:22:55 -0700

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -3,4 +3,4 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 # install maturin
 pip install maturin
 cd py-polars && maturin develop --release -- -C codegen-units=16 -C lto=thin -C target-cpu=native
-
+make test
