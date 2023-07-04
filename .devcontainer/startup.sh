4da1487e13b99bfe7f53cf47e95e6e7a17a5d5a2
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-07-03 23:13:03 -0700

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,19 +1,19 @@
-# install rust
-#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-#source "$HOME/.cargo/env"
-
 cd py-polars
 
 # construct a virtual environment as required by maturin
 python -m venv .venv
 source .venv/bin/activate
 
+# upgrade pip and install dev-dependencies
 python -m pip install --upgrade pip
 python -m pip install -r requirements-dev.txt
-python -m pip install -r requirements-lint.txt
-python -m pip install -r docs/requirements-docs.txt
 
-pip install maturin
+# Not required at this early stage...
+# python -m pip install -r requirements-lint.txt
+# python -m pip install -r docs/requirements-docs.txt
+
+# maturin is a dependency declared in requirements-dev.txt
+# pip install maturin
 
 maturin develop --release
 
