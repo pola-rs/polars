bc8582c547fa9c87ba8d0112298428679739ddcf
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-07-03 22:18:02 -0700

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,13 +1,13 @@
 # install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env"
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+#source "$HOME/.cargo/env"
 
 cd py-polars
 
 # construct the virtual environment
-python3 -m venv .venv
+# python3 -m venv .venv
 
-source .venv/bin/activate
+# source .venv/bin/activate
 python -m pip install --upgrade pip
 python -m pip install -r requirements-dev.txt
 python -m pip install -r requirements-lint.txt
@@ -15,6 +15,6 @@ python -m pip install -r docs/requirements-docs.txt
 
 pip install maturin
 
-maturin develop
-
-pytest -n auto --dist worksteal
+#maturin develop
+#
+#pytest -n auto --dist worksteal
