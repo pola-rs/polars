c301eec20b08a3b2408cc1a5a7a4f6a1266a41f7
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-07-04 10:46:29 -0700

    devcontainer.json with startup

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -2,5 +2,5 @@ rustc --version
 cargo --version
 
 cd py-polars
-# curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-# make build-release
+
+make build-release
