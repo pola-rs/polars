1b7a04bfadd6b28ff16db3ee82052b86d76f4450
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-07-03 23:41:43 -0700

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -8,14 +8,5 @@ source .venv/bin/activate
 python -m pip install --upgrade pip
 python -m pip install -r requirements-dev.txt
 
-# Not required at this early stage...
-# python -m pip install -r requirements-lint.txt
-# python -m pip install -r docs/requirements-docs.txt
-
-# maturin is a dependency declared in requirements-dev.txt
-# pip install maturin
-
-# from the README
-maturin develop --release -- -C codegen-units=16 -C lto=thin -C target-cpu=native
-
+@unset CONDA_PREFIX && maturin develop --release -- -C target-cpu=native
 pytest -n auto --dist worksteal
