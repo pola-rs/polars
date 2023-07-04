6608d9e4a6d81171df7eff1d1eeddba12330ea64
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-07-03 20:10:48 -0700

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,5 +1,6 @@
-# build the virtual environment and install all dependencies
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# install maturin
 pip install maturin
 cd py-polars && maturin develop --release -- -C codegen-units=16 -C lto=thin -C target-cpu=native
 
