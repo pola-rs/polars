47d8ff61871199d85ce185e4e6b0e345b0ce89e4
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-07-04 06:45:16 -0700

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,21 +1,6 @@
-#rustc --version
-#cargo --version
-#rustup update stable
-
 rustc --version
 cargo --version
 
 cd py-polars
 
-# construct a virtual environment as required by maturin
-python -m venv .venv
-# source .venv/bin/activate
-
-# upgrade pip and install dev-dependencies
-.venv/bin/pip install --upgrade pip
-.venv/bin/pip install -r requirements-dev.txt
-
-.venv/bin/maturin develop --release 
-#-- -C target-cpu=native
-
-#.venv/bin/pytest -n auto --dist worksteal
+make build
