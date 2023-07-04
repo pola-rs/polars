3134c1b55b3eefe5315f8e6e5e9a5acb15277930
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-07-04 00:02:41 -0700

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -2,11 +2,11 @@ cd py-polars
 
 # construct a virtual environment as required by maturin
 python -m venv .venv
-source .venv/bin/activate
+# source .venv/bin/activate
 
 # upgrade pip and install dev-dependencies
-.venv/bin/python -m pip install --upgrade pip
-.venv/bin/python -m pip install -r requirements-dev.txt
+.venv/bin/pip install --upgrade pip
+.venv/bin/pip install -r requirements-dev.txt
 
 .venv/bin/maturin develop --release -- -C target-cpu=native
 .venv/bin/pytest -n auto --dist worksteal
