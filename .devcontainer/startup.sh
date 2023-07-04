97e15b19ef1d5b1aa48addf7d01265e30a1a8757
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-07-04 10:36:04 -0700

    Update startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -4,9 +4,6 @@ cargo --version
 # install rust without any interaction
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-# install a linker
-sudo apt update && sudo apt install build-essential
-
 cd py-polars
 # curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 # make build-release
