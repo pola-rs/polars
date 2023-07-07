0609b026b08b181886e2979e919809f7e62e1924
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-07-06 21:40:18 -0700

    execute startup

---
+++ .devcontainer/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cargo --version
+rustc --version
+
+# follow https://github.com/pola-rs/polars/blob/main/CONTRIBUTING.md#setting-up-your-environment
+rustup toolchain install nightly --component miri
+
+# Install dprint, see https://dprint.dev/install/
+# this will be slower since it builds from the source
+cargo install --locked dprint
