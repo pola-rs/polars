4b0218cf22b9d8cdaefa71662baf26888a132be6
Alexander Beedie <alexander-beedie@users.noreply.github.com>
2023-07-07 17:48:08 +0100

    feat(rust,python): reworked comfy-table layout constraints, improving table wrapping/repr (#9744)

--- py-polars/tests/unit/test_cfg.py
+++ py-polars/tests/unit/test_cfg.py
@@ -67,13 +67,13 @@ def test_hide_header_elements() -> None:
     pl.Config.set_tbl_hide_column_data_types(True)
     assert (
         str(df) == "shape: (3, 3)\n"
-        "┌─────┬─────┬─────┐\n"
-        "│ a   ┆ b   ┆ c   │\n"
-        "╞═════╪═════╪═════╡\n"
-        "│ 1   ┆ 4   ┆ 7   │\n"
-        "│ 2   ┆ 5   ┆ 8   │\n"
-        "│ 3   ┆ 6   ┆ 9   │\n"
-        "└─────┴─────┴─────┘"
+        "┌───┬───┬───┐\n"
+        "│ a ┆ b ┆ c │\n"
+        "╞═══╪═══╪═══╡\n"
+        "│ 1 ┆ 4 ┆ 7 │\n"
+        "│ 2 ┆ 5 ┆ 8 │\n"
+        "│ 3 ┆ 6 ┆ 9 │\n"
+        "└───┴───┴───┘"
     )
 
     pl.Config.set_tbl_hide_column_data_types(False).set_tbl_hide_column_names(True)
@@ -342,12 +342,24 @@ def test_set_tbl_width_chars() -> None:
     pl.Config.set_tbl_width_chars(60)
     assert max(len(line) for line in str(df).split("\n")) == 60
 
-    # formula for determining min width is
-    # sum(max(min(header.len, 12), 5)) + header.len + 1
-    # so we end up with 12+5+10+4 = 31
-
+    # force minimal table size (will hard-wrap everything; "don't try this at home" :p)
     pl.Config.set_tbl_width_chars(0)
-    assert max(len(line) for line in str(df).split("\n")) == 31
+    assert max(len(line) for line in str(df).split("\n")) == 19
+
+    # this check helps to check that column width bucketing
+    # is exact; no extraneous character allocation
+    df = pl.DataFrame(
+        {
+            "A": [1, 2, 3, 4, 5],
+            "fruits": ["banana", "banana", "apple", "apple", "banana"],
+            "B": [5, 4, 3, 2, 1],
+            "cars": ["beetle", "audi", "beetle", "beetle", "beetle"],
+        },
+        schema_overrides={"A": pl.Int64, "B": pl.Int64},
+    ).select(pl.all(), pl.all().suffix("_suffix!"))
+
+    with pl.Config(tbl_width_chars=87):
+        assert max(len(line) for line in str(df).split("\n")) == 87
 
 
 def test_shape_below_table_and_inlined_dtype() -> None:
