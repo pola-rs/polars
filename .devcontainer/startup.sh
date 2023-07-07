c411a94b2cf1b31a7cbf71045d4d89d348c050f7
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-07-06 21:53:49 -0700

    rustfmt

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -5,6 +5,9 @@ rustc --version
 # follow https://github.com/pola-rs/polars/blob/main/CONTRIBUTING.md#setting-up-your-environment
 rustup toolchain install nightly --component miri
 
+# add https://github.com/rust-lang/rustfmt
+rustup component add rustfmt --toolchain nightly
+
 # Install dprint, see https://dprint.dev/install/
 # this will be slower since it builds from the source
 cargo install --locked dprint
