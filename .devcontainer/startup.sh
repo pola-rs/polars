9df03d040de188cf37649dd1a62a3f38f8f53910
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-07-06 22:14:59 -0700

    rustfmt

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -2,11 +2,14 @@
 cargo --version
 rustc --version
 
+toolchain="nightly-2023-06-23"
+
 # follow https://github.com/pola-rs/polars/blob/main/CONTRIBUTING.md#setting-up-your-environment
-rustup toolchain install nightly --component miri
+rustup toolchain install ${toolchain}-x86_64-unknown-linux-gnu --component miri
 
 # add https://github.com/rust-lang/rustfmt
-rustup component add rustfmt --toolchain nightly
+rustup component add rustfmt --toolchain ${toolchain}-x86_64-unknown-linux-gnu
+rustup component add clippy --toolchain ${toolchain}-x86_64-unknown-linux-gnu
 
 # Install dprint, see https://dprint.dev/install/
 # this will be slower since it builds from the source
