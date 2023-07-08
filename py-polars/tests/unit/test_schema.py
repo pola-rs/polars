65ffd24c4ed16d907da5a1184f0577a8d37b61a2
Alexander Beedie <alexander-beedie@users.noreply.github.com>
2023-07-08 22:46:24 +0100

    fix(rust,python): fix supertype detection

--- py-polars/tests/unit/test_schema.py
+++ py-polars/tests/unit/test_schema.py
@@ -468,24 +468,38 @@ def test_schema_ne_missing_9256() -> None:
 
 def test_concat_vertically_relaxed() -> None:
     a = pl.DataFrame(
-        {
-            "a": [1, 2, 3],
-            "b": [True, False, None],
-        },
+        data={"a": [1, 2, 3], "b": [True, False, None]},
         schema={"a": pl.Int8, "b": pl.Boolean},
     )
-
     b = pl.DataFrame(
-        {
-            "a": [43, 2, 3],
-            "b": [32, 1, None],
-        },
+        data={"a": [43, 2, 3], "b": [32, 1, None]},
         schema={"a": pl.Int16, "b": pl.Int64},
     )
-
     out = pl.concat([a, b], how="vertical_relaxed")
     assert out.schema == {"a": pl.Int16, "b": pl.Int64}
     assert out.to_dict(False) == {
         "a": [1, 2, 3, 43, 2, 3],
         "b": [1, 0, None, 32, 1, None],
+    }
+    out = pl.concat([b, a], how="vertical_relaxed")
+    assert out.schema == {"a": pl.Int16, "b": pl.Int64}
+    assert out.to_dict(False) == {
+        "a": [43, 2, 3, 1, 2, 3],
+        "b": [32, 1, None, 1, 0, None],
+    }
+
+    c = pl.DataFrame({"a": [1, 2], "b": [2, 1]})
+    d = pl.DataFrame({"a": [1.0, 0.2], "b": [None, 0.1]})
+
+    out = pl.concat([c, d], how="vertical_relaxed")
+    assert out.schema == {"a": pl.Float64, "b": pl.Float64}
+    assert out.to_dict(False) == {
+        "a": [1.0, 2.0, 1.0, 0.2],
+        "b": [2.0, 1.0, None, 0.1],
+    }
+    out = pl.concat([d, c], how="vertical_relaxed")
+    assert out.schema == {"a": pl.Float64, "b": pl.Float64}
+    assert out.to_dict(False) == {
+        "a": [1.0, 0.2, 1.0, 2.0],
+        "b": [None, 0.1, 2.0, 1.0],
     }
