8a25dac52e9232629f951636157880ff447ef994
Marshall <mcrumiller@users.noreply.github.com>
2023-07-12 13:52:32 -0400

    fix(rust, python): shrink_type on all-null columns (#9811)

--- py-polars/tests/unit/test_schema.py
+++ py-polars/tests/unit/test_schema.py
@@ -215,6 +215,9 @@ def test_shrink_dtype() -> None:
             "f": ["a", "b", "c"],
             "g": [0.1, 1.32, 0.12],
             "h": [True, None, False],
+            "i": pl.Series([None, None, None], dtype=pl.UInt64),
+            "j": pl.Series([None, None, None], dtype=pl.Int64),
+            "k": pl.Series([None, None, None], dtype=pl.Float64),
         }
     ).select(pl.all().shrink_dtype())
     assert out.dtypes == [
@@ -226,6 +229,9 @@ def test_shrink_dtype() -> None:
         pl.Utf8,
         pl.Float32,
         pl.Boolean,
+        pl.UInt8,
+        pl.Int8,
+        pl.Float32,
     ]
 
     assert out.to_dict(False) == {
@@ -237,6 +243,9 @@ def test_shrink_dtype() -> None:
         "f": ["a", "b", "c"],
         "g": [0.10000000149011612, 1.3200000524520874, 0.11999999731779099],
         "h": [True, None, False],
+        "i": [None, None, None],
+        "j": [None, None, None],
+        "k": [None, None, None],
     }
 
 
