9ddd8a12924b7afbeb173dbe01ba34ad71c06af3
Alexander Beedie <alexander-beedie@users.noreply.github.com>
2023-07-13 17:55:33 +0200

    feat(rust,python,cli): add `LENGTH` and `OCTET_LENGTH` string functions for SQL

--- polars/polars-lazy/polars-plan/src/dsl/function_expr/mod.rs
+++ polars/polars-lazy/polars-plan/src/dsl/function_expr/mod.rs
@@ -618,6 +618,9 @@ impl From<StringFunction> for SpecialEq<Arc<dyn SeriesUdf>> {
         match func {
             #[cfg(feature = "regex")]
             Contains { literal, strict } => map_as_slice!(strings::contains, literal, strict),
+            CountMatch(pat) => {
+                map!(strings::count_match, &pat)
+            }
             EndsWith { .. } => map_as_slice!(strings::ends_with),
             StartsWith { .. } => map_as_slice!(strings::starts_with),
             Extract { pat, group_index } => {
@@ -626,9 +629,8 @@ impl From<StringFunction> for SpecialEq<Arc<dyn SeriesUdf>> {
             ExtractAll => {
                 map_as_slice!(strings::extract_all)
             }
-            CountMatch(pat) => {
-                map!(strings::count_match, &pat)
-            }
+            NChars => map!(strings::n_chars),
+            Length => map!(strings::lengths),
             #[cfg(feature = "string_justify")]
             Zfill(alignment) => {
                 map!(strings::zfill, alignment)
