e915d2541d77295679a03b0f90321c022b032804
qqwy <qqwy@gmx.com>
2023-07-24 18:17:03 +0200

    Improve example program

--- examples/write_parquet_cloud/src/main.rs
+++ examples/write_parquet_cloud/src/main.rs
@@ -4,50 +4,56 @@ use polars::prelude::*;
 
 // Login to your aws account and then copy the ../datasets/foods1.parquet file to your own bucket.
 // Adjust the link below.
-const TEST_S3: &str = "s3://lov2test/polars/datasets/*.parquet";
+const TEST_S3_LOCATION: &str = "s3://lov2test/polars_write_example_cloud.parquet";
 
 fn main() -> PolarsResult<()> {
-    // let cred = Credentials::default().unwrap();
+    sink_file();
+    sink_cloud_local();
+    sink_aws();
 
-    // Propagate the credentials and other cloud options.
-    // let mut args = ScanArgsParquet::default();
-    // let cloud_options = cloud::CloudOptions::default().with_aws([
-    //     (Key::AccessKeyId, &cred.access_key.unwrap()),
-    //     (Key::SecretAccessKey, &cred.secret_key.unwrap()),
-    //     (Key::Region, &"us-west-2".into()),
-    // ]);
-    // args.cloud_options = Some(cloud_options);
 
-    let df = df!(
-        "foo" => &[1, 2, 3],
-        "bar" => &[None, Some("bak"), Some("baz")],
-    )
-    .unwrap();
 
+    Ok(())
+}
+
+fn sink_file() {
+    let df = example_dataframe();
+
+    // Writing to a local file:
     let path = "/tmp/polars_write_example.parquet".into();
-    let df = df.lazy().sink_parquet(path, Default::default()).unwrap();
+    df.lazy().sink_parquet(path, Default::default()).unwrap();
+}
 
-    dbg!(df);
+fn sink_cloud_local() {
+    let df = example_dataframe();
 
-    let df = df!(
-        "foo" => &[1, 2, 3],
-        "bar" => &[None, Some("bak"), Some("baz")],
-    )
-    .unwrap();
+    // Writing to a location that might be in the cloud:
     let uri = "file:///tmp/polars_write_example_cloud.parquet".to_string();
-    let df = df
-        .lazy()
+    df.lazy()
         .sink_parquet_cloud(uri, None, Default::default())
         .unwrap();
+}
 
-    // let df = LazyFrame::scan_parquet(TEST_S3, args)?
-    //     .with_streaming(true)
-    //     .select([
-    //         // select all columns
-    //         all(),
-    //     ])
-    //     .collect()?;
+fn sink_aws() {
+    let cred = Credentials::default().unwrap();
 
-    dbg!(df);
-    Ok(())
+    // Propagate the credentials and other cloud options.
+    let cloud_options = cloud::CloudOptions::default().with_aws([
+        (Key::AccessKeyId, &cred.access_key.unwrap()),
+        (Key::SecretAccessKey, &cred.secret_key.unwrap()),
+        (Key::Region, &"us-west-2".into()),
+    ]);
+    let cloud_options = Some(cloud_options);
+
+    let df = example_dataframe();
+
+    df.lazy().sink_parquet_cloud(TEST_S3_LOCATION.to_string(), cloud_options, Default::default()).unwrap();
+}
+
+fn example_dataframe() -> DataFrame {
+    df!(
+        "foo" => &[1, 2, 3],
+        "bar" => &[None, Some("bak"), Some("baz")],
+    )
+    .unwrap()
 }
