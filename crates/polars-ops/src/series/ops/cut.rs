9e5ab1fd355098e93b5a492cf86ae7f7c04e0c5d
Josh Magarick <jmagarick@gmail.com>
2023-07-25 21:37:57 -0700

    start

--- crates/polars-ops/src/series/ops/cut.rs
+++ crates/polars-ops/src/series/ops/cut.rs
@@ -1,6 +1,7 @@
 use std::cmp::PartialOrd;
 use std::iter::once;
 
+use polars_core::export::regex::Regex;
 use polars_core::prelude::*;
 
 fn map_cats(
@@ -57,39 +58,87 @@ fn map_cats(
     }
 }
 
+const MAX_PRECISION: usize = 32;
+
+fn find_label_precision(sorted_breaks: &[f64], precision: usize, scientific: bool) -> usize {
+    // Find a level of precision such that the numbers printed in labels are distinct.
+    if !sorted_breaks.is_empty() {
+        return precision;
+    }
+    // The following finds a level of precision such that every break has a unique string
+    // representation. While a little half-assed, it's also what everyone else does. We could
+    // possibly speed this up with "clever" floating point operations, but that's more complex and
+    // error prone. And unless you have millions of categories it isn't remotely an issue.
+    let format_fn = match scientific {
+        false => |b, p| format!("{0:.1$}", b, p),
+        true => |b, p| format!("{0:.1$e}", b, p),
+    };
+    let mut precision = precision;
+    'outer: while precision < MAX_PRECISION {
+        let mut last_str = format_fn(sorted_breaks[0], precision);
+        for b in sorted_breaks.iter().skip(1) {
+            let b_str: String = format_fn(*b, precision);
+            if b_str == last_str {
+                precision += 1;
+                continue 'outer;
+            }
+            last_str = b_str;
+        }
+        break;
+    }
+    precision
+}
+
+fn make_labels(sorted_breaks: &[f64], left_closed: bool, precision: usize, scientific: bool) -> Vec<String> {
+    let precision = find_label_precision(sorted_breaks, precision, scientific);
+    // This seems to be the most straightforward way to get rid of trailing zeros.
+    let (re, replacement) = match scientific {
+        true => (Regex::new(r"(\.?0+)e").unwrap(), "e"),
+        false => (Regex::new(r"(\.?0+)$").unwrap(), ""),
+    };
+    // Rust doesn't like returning these closures as part of the tuple above.
+    let format_fn = match scientific {
+        true => |v, p| format!("{0:.1$e}", v, p),
+        false => |v, p| format!("{0:.1$}", v, p),
+    };
+
+    let mut out = Vec::with_capacity(sorted_breaks.len() + 2);
+    let mut last_break_str = format!("{}", f64::NEG_INFINITY);
+    for v in sorted_breaks.iter().chain(once(&f64::INFINITY)) {
+        let raw_break_str = format_fn(v, precision);
+        let break_str = re.replace(&raw_break_str, replacement).to_string();
+        if left_closed {
+            out.push(format!("[{}, {})", last_break_str, break_str));
+        } else {
+            out.push(format!("({}, {}]", last_break_str, break_str))
+        }
+        last_break_str = break_str;
+    }
+    out
+}
+
 pub fn cut(
     s: &Series,
     breaks: Vec<f64>,
     labels: Option<Vec<String>>,
     left_closed: bool,
     include_breaks: bool,
+    precision: usize,
+    scientific: bool,
 ) -> PolarsResult<Series> {
-    polars_ensure!(!breaks.iter().any(|x| x.is_nan()), ComputeError: "Breaks cannot be NaN");
+    polars_ensure!(breaks.iter().all(|x| x.is_finite()), ComputeError: "Don't include NaN, Inf, or -Inf in breaks");
     // Breaks must be sorted to cut inputs properly.
     let mut breaks = breaks;
     let sorted_breaks = breaks.as_mut_slice();
     sorted_breaks.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
     polars_ensure!(sorted_breaks.windows(2).all(|x| x[0] != x[1]), Duplicate: "Breaks are not unique");
-    if !sorted_breaks.is_empty() {
-        polars_ensure!(sorted_breaks[0] > f64::NEG_INFINITY, ComputeError: "Don't include -inf in breaks");
-        polars_ensure!(sorted_breaks[sorted_breaks.len() - 1] < f64::INFINITY, ComputeError: "Don't include inf in breaks");
-    }
 
     let cutlabs = match labels {
         Some(ll) => {
             polars_ensure!(ll.len() == sorted_breaks.len() + 1, ShapeMismatch: "Provide nbreaks + 1 labels");
             ll
         }
-        None => (once(&f64::NEG_INFINITY).chain(sorted_breaks.iter()))
-            .zip(sorted_breaks.iter().chain(once(&f64::INFINITY)))
-            .map(|v| {
-                if left_closed {
-                    format!("[{}, {})", v.0, v.1)
-                } else {
-                    format!("({}, {}]", v.0, v.1)
-                }
-            })
-            .collect::<Vec<String>>(),
+        None => make_labels(&sorted_breaks, left_closed, precision, scientific),
     };
 
     map_cats(s, &cutlabs, sorted_breaks, left_closed, include_breaks)
@@ -102,6 +151,8 @@ pub fn qcut(
     left_closed: bool,
     allow_duplicates: bool,
     include_breaks: bool,
+    precision: usize,
+    scientific: bool,
 ) -> PolarsResult<Series> {
     let s = s.cast(&DataType::Float64)?;
     let s2 = s.sort(false);
@@ -134,7 +185,7 @@ pub fn qcut(
             }
         };
         qbreaks.dedup();
-        return cut(&s, qbreaks, lfilt, left_closed, include_breaks);
+        return cut(&s, qbreaks, lfilt, left_closed, include_breaks, precision, scientific);
     }
-    cut(&s, qbreaks, labels, left_closed, include_breaks)
+    cut(&s, qbreaks, labels, left_closed, include_breaks, precision, scientific)
 }
