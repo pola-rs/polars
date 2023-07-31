9c51e1829ea476ba19f0b86db75248f8bdd98f51
Horacio Hidalgo <horacio.m.hidalgo.herrera@intel.com>
2023-07-30 18:29:42 -0600

    Fix parquet example
    * Current example data is in csv format
    * Create a convertion to parquet first to be able to run the example

--- examples/read_parquet/src/main.rs
+++ examples/read_parquet/src/main.rs
@@ -1,6 +1,8 @@
 use polars::prelude::*;
 
 fn main() -> PolarsResult<()> {
+    csv_to_parquet();
+
     let df = LazyFrame::scan_parquet("../datasets/foods1.parquet", ScanArgsParquet::default())?
         .select([
             // select all columns
@@ -12,4 +14,17 @@ fn main() -> PolarsResult<()> {
 
     dbg!(df);
     Ok(())
+}
+
+fn csv_to_df() -> DataFrame {
+    CsvReader::from_path("../datasets/foods1.csv")
+        .unwrap()
+        .finish()
+        .unwrap()
+}
+
+fn csv_to_parquet() {
+    let mut df = csv_to_df();
+    let mut file = std::fs::File::create("../datasets/foods1.parquet").unwrap();
+    ParquetWriter::new(&mut file).finish(&mut df).unwrap();
 }
