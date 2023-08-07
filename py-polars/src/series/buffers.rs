598df75d6bfaf49f2683652279446c53d042998e
Ritchie Vink <ritchie46@gmail.com>
2023-08-07 12:27:19 +0200

    feat(python): utility to get buffers and pointers (#10331)

--- py-polars/src/series/buffers.rs
+++ py-polars/src/series/buffers.rs
@@ -5,26 +5,6 @@ use super::*;
 
 #[pymethods]
 impl PySeries {
-    fn offset_buffers(&self) -> PyResult<Self> {
-        let buffers: Box<dyn Iterator<Item = &OffsetsBuffer<i64>>> = match self.series.dtype() {
-            DataType::List(_) => {
-                let ca = self.series.list().unwrap();
-                Box::new(ca.downcast_iter().map(|arr| arr.offsets()))
-            }
-            DataType::Utf8 => {
-                let ca = self.series.utf8().unwrap();
-                Box::new(ca.downcast_iter().map(|arr| arr.offsets()))
-            }
-            _ => return Err(PyValueError::new_err("expected list/utf8")),
-        };
-        let buffers = buffers
-            .map(|arr| PrimitiveArray::from_data_default(arr.buffer().clone(), None).boxed())
-            .collect::<Vec<_>>();
-        Ok(Series::try_from((self.name(), buffers))
-            .map_err(PyPolarsErr::from)?
-            .into())
-    }
-
     fn get_ptr(&self) -> PyResult<usize> {
         let s = self.series.to_physical_repr();
         let arrays = s.chunks();
@@ -50,11 +30,135 @@ impl PySeries {
                 let ca: &ChunkedArray<$T> = s.as_ref().as_ref().as_ref();
                 get_ptr(ca)
             })),
+            DataType::Utf8 => {
+                let ca = s.utf8().unwrap();
+                let arr = ca.downcast_iter().next().unwrap();
+                Ok(arr.values().as_ptr() as usize)
+            }
+            DataType::Binary => {
+                let ca = s.binary().unwrap();
+                let arr = ca.downcast_iter().next().unwrap();
+                Ok(arr.values().as_ptr() as usize)
+            }
             _ => {
-                let msg = "Cannot take pointer of nested type";
+                let msg = "Cannot take pointer of nested type, try to first select a buffer";
                 raise_err!(msg, ComputeError);
             }
         }
+    }
+
+    fn get_buffer(&self, index: usize) -> PyResult<Option<Self>> {
+        match self.series.dtype().to_physical() {
+            dt if dt.is_numeric() => get_buffer_from_primitive(&self.series, index),
+            DataType::Boolean => get_buffer_from_primitive(&self.series, index),
+            DataType::Utf8 | DataType::List(_) | DataType::Binary => {
+                get_buffer_from_nested(&self.series, index)
+            }
+            DataType::Array(_, _) => {
+                let ca = self.series.array().unwrap();
+                match index {
+                    0 => {
+                        let buffers = ca
+                            .downcast_iter()
+                            .map(|arr| arr.values().clone())
+                            .collect::<Vec<_>>();
+                        Ok(Some(
+                            Series::try_from((self.series.name(), buffers))
+                                .map_err(PyPolarsErr::from)?
+                                .into(),
+                        ))
+                    }
+                    1 => Ok(get_bitmap(&self.series)),
+                    _ => raise_err!("expected an index <= 1", ComputeError),
+                }
+            }
+            _ => todo!(),
+        }
+    }
+}
+
+fn get_bitmap(s: &Series) -> Option<PySeries> {
+    if s.null_count() > 0 {
+        Some(s.is_not_null().into_series().into())
+    } else {
+        None
+    }
+}
+
+fn get_buffer_from_nested(s: &Series, index: usize) -> PyResult<Option<PySeries>> {
+    match index {
+        0 => {
+            let buffers: Box<dyn Iterator<Item = ArrayRef>> = match s.dtype() {
+                DataType::List(_) => {
+                    let ca = s.list().unwrap();
+                    Box::new(ca.downcast_iter().map(|arr| arr.values().clone()))
+                }
+                DataType::Utf8 => {
+                    let ca = s.utf8().unwrap();
+                    Box::new(ca.downcast_iter().map(|arr| {
+                        PrimitiveArray::from_data_default(arr.values().clone(), None).boxed()
+                    }))
+                }
+                DataType::Binary => {
+                    let ca = s.binary().unwrap();
+                    Box::new(ca.downcast_iter().map(|arr| {
+                        PrimitiveArray::from_data_default(arr.values().clone(), None).boxed()
+                    }))
+                }
+                dt => {
+                    let msg = format!("{dt} not yet supported as nested buffer access");
+                    raise_err!(msg, ComputeError);
+                }
+            };
+            let buffers = buffers.collect::<Vec<_>>();
+            Ok(Some(
+                Series::try_from((s.name(), buffers))
+                    .map_err(PyPolarsErr::from)?
+                    .into(),
+            ))
+        }
+        1 => Ok(get_bitmap(s)),
+        2 => get_offsets(s).map(Some),
+        _ => raise_err!("expected an index <= 2", ComputeError),
+    }
+}
+
+fn get_offsets(s: &Series) -> PyResult<PySeries> {
+    let buffers: Box<dyn Iterator<Item = &OffsetsBuffer<i64>>> = match s.dtype() {
+        DataType::List(_) => {
+            let ca = s.list().unwrap();
+            Box::new(ca.downcast_iter().map(|arr| arr.offsets()))
+        }
+        DataType::Utf8 => {
+            let ca = s.utf8().unwrap();
+            Box::new(ca.downcast_iter().map(|arr| arr.offsets()))
+        }
+        _ => return Err(PyValueError::new_err("expected list/utf8")),
+    };
+    let buffers = buffers
+        .map(|arr| PrimitiveArray::from_data_default(arr.buffer().clone(), None).boxed())
+        .collect::<Vec<_>>();
+    Ok(Series::try_from((s.name(), buffers))
+        .map_err(PyPolarsErr::from)?
+        .into())
+}
+
+fn get_buffer_from_primitive(s: &Series, index: usize) -> PyResult<Option<PySeries>> {
+    match index {
+        0 => {
+            let chunks = s
+                .chunks()
+                .iter()
+                .map(|arr| arr.with_validity(None))
+                .collect::<Vec<_>>();
+            Ok(Some(
+                Series::try_from((s.name(), chunks))
+                    .map_err(PyPolarsErr::from)?
+                    .into(),
+            ))
+        }
+        1 => Ok(get_bitmap(s)),
+        _ => raise_err!("expected an index <= 1", ComputeError),
     }
 }
 
