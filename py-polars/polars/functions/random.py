48f2f3be259aee093907fa27fc9d0cc99646006a
Orson Peters <orsonpeters@gmail.com>
2023-08-09 14:43:29 +0200

    fix Python lint

--- py-polars/polars/functions/random.py
+++ py-polars/polars/functions/random.py
@@ -7,7 +7,7 @@ with contextlib.suppress(ImportError):  # Module not available when building doc
 
 
 def set_random_seed(seed: int) -> None:
-    """
+    r"""
     Set the global random seed for Polars.
 
     This random seed is used to determine things such as shuffle ordering.
@@ -16,7 +16,7 @@ def set_random_seed(seed: int) -> None:
     Parameters
     ----------
     seed
-        A non-negative integer < 2\ :sup:`64` used to seed the internal global random number
-        generator.
+        A non-negative integer < 2\ :sup:`64` used to seed the internal global
+        random number generator.
     """
     plr.set_random_seed(seed)
