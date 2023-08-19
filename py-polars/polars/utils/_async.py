406f0015ac17c71618c5375fd916e3883dd5ed8c
Object905 <Object905@gmail.com>
2023-08-19 20:06:57 +0500

    fix: toml linter and mypy erors in _async

--- py-polars/polars/utils/_async.py
+++ py-polars/polars/utils/_async.py
@@ -8,17 +8,18 @@ if TYPE_CHECKING:
     from queue import Queue
 
     from polars import DataFrame
+    from polars.polars import PyDataFrame
 
 
 class _AsyncDataFrameResult:
-    queue: Queue
-    _result: DataFrame | Exception
+    queue: Queue[DataFrame | Exception]
+    _result: DataFrame | Exception | None
 
     def __init__(self, queue: Queue[DataFrame | Exception]) -> None:
         self.queue = queue
         self._result = None
 
-    def get(self, block=True, timeout=None) -> DataFrame:
+    def get(self, block: bool = True, timeout: float | None = None) -> DataFrame:
         if self._result is not None:
             if isinstance(self._result, Exception):
                 raise self._result
@@ -29,7 +30,7 @@ class _AsyncDataFrameResult:
             raise self._result
         return self._result
 
-    def _callback(self, obj):
+    def _callback(self, obj: PyDataFrame | Exception) -> None:
         if not isinstance(obj, Exception):
             obj = wrap_df(obj)
         self.queue.put_nowait(obj)
