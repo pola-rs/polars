6ac4e10c135a85b3e47fc38e5c893c9e0eece2b2
Weijie Guo <reswqa@163.com>
2023-08-19 16:47:17 +0800

    fix(rust, python): Fix serialization for categorical chunked.

--- py-polars/tests/unit/test_serde.py
+++ py-polars/tests/unit/test_serde.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 import pytest
 
 import polars as pl
+from polars import StringCache
 from polars.testing import assert_frame_equal, assert_series_equal
 
 
@@ -182,3 +183,10 @@ def test_pickle_lazyframe_nested_function_udf() -> None:
 
     q = pickle.loads(b)
     assert q.collect()["a"].to_list() == [2, 4, 6]
+
+
+@StringCache()
+def test_serde_categorical_series_10586() -> None:
+    s = pl.Series(["a", "b", "b", "a", "c"], dtype=pl.Categorical)
+    loaded_s = pickle.loads(pickle.dumps(s))
+    assert_series_equal(loaded_s, s)
