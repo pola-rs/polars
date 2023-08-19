1789cd6a4ee5b316a85925319a62a2f8df878143
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-08-19 07:22:54 +0100

    skip deprecated doctest

--- py-polars/polars/dataframe/frame.py
+++ py-polars/polars/dataframe/frame.py
@@ -4408,7 +4408,7 @@ class DataFrame:
         --------
         >>> df = pl.DataFrame({"foo": [1, 2, 3], "bar": [4, 5, 6]})
         >>> s = pl.Series([10, 20, 30])
-        >>> df.replace("foo", s)  # works in-place!
+        >>> df.replace("foo", s)  # works in-place!  # doctest: +SKIP
         shape: (3, 2)
         ┌─────┬─────┐
         │ foo ┆ bar │
