74a3a9f213ac2927121ff3c58ef4af18c2847abc
Weijie Guo <reswqa@163.com>
2023-08-27 13:03:17 +0800

    feat(rust, python): Supports series unique & arg_unique & n_unique for list (#10743)

--- crates/polars-core/src/series/implementations/list.rs
+++ crates/polars-core/src/series/implementations/list.rs
@@ -4,7 +4,7 @@ use std::borrow::Cow;
 #[cfg(feature = "group_by_list")]
 use ahash::RandomState;
 
-use super::{private, IntoSeries, SeriesTrait};
+use super::*;
 use crate::chunked_array::comparison::*;
 use crate::chunked_array::ops::compare_inner::{IntoPartialEqInner, PartialEqInner};
 use crate::chunked_array::ops::explode::ExplodeByOffsets;
@@ -187,6 +187,55 @@ impl SeriesTrait for SeriesWrap<ListChunked> {
 
     fn has_validity(&self) -> bool {
         self.0.has_validity()
+    }
+
+    #[cfg(feature = "group_by_list")]
+    fn unique(&self) -> PolarsResult<Series> {
+        if !self.inner_dtype().is_numeric() {
+            polars_bail!(opq = unique, self.inner_dtype());
+        }
+        // this can be called in aggregation, so this fast path can be worth a lot
+        if self.len() < 2 {
+            return Ok(self.0.clone().into_series());
+        }
+        let main_thread = POOL.current_thread_index().is_none();
+        let groups = self.group_tuples(main_thread, false);
+        // safety:
+        // groups are in bounds
+        Ok(unsafe { self.0.clone().into_series().agg_first(&groups?) })
+    }
+
+    #[cfg(feature = "group_by_list")]
+    fn n_unique(&self) -> PolarsResult<usize> {
+        if !self.inner_dtype().is_numeric() {
+            polars_bail!(opq = n_unique, self.inner_dtype());
+        }
+        // this can be called in aggregation, so this fast path can be worth a lot
+        match self.len() {
+            0 => Ok(0),
+            1 => Ok(1),
+            _ => {
+                let main_thread = POOL.current_thread_index().is_none();
+                let groups = self.group_tuples(main_thread, false)?;
+                Ok(groups.len())
+            },
+        }
+    }
+
+    #[cfg(feature = "group_by_list")]
+    fn arg_unique(&self) -> PolarsResult<IdxCa> {
+        if !self.inner_dtype().is_numeric() {
+            polars_bail!(opq = arg_unique, self.inner_dtype());
+        }
+        // this can be called in aggregation, so this fast path can be worth a lot
+        if self.len() == 1 {
+            return Ok(IdxCa::new_vec(self.name(), vec![0 as IdxSize]));
+        }
+        let main_thread = POOL.current_thread_index().is_none();
+        // arg_unique requires a stable order
+        let groups = self.group_tuples(main_thread, true)?;
+        let first = groups.take_group_firsts();
+        Ok(IdxCa::from_vec(self.name(), first))
     }
 
     fn is_null(&self) -> BooleanChunked {
