a4cb7a78005fc4f6714b76354229351d85b7dd7c
Fokko Driesprong <fokko@tabular.io>
2023-09-06 12:19:36 +0200

    Fix ruff

--- py-polars/tests/unit/io/test_iceberg.py
+++ py-polars/tests/unit/io/test_iceberg.py
@@ -40,66 +40,49 @@ def test_scan_iceberg_filter_on_column(iceberg_path: str) -> None:
 
 
 def test_true_expression() -> None:
-    from pyiceberg.expressions import (
-        AlwaysTrue,
-    )
+    from pyiceberg.expressions import AlwaysTrue
 
     expr = _to_ast("pa.compute.scalar(True)")
     assert _convert_predicate(expr) == AlwaysTrue()
 
 
 def test_false_expression() -> None:
-    from pyiceberg.expressions import (
-        AlwaysFalse,
-    )
+    from pyiceberg.expressions import AlwaysFalse
 
     expr = _to_ast("pa.compute.scalar(False)")
     assert _convert_predicate(expr) == AlwaysFalse()
 
 
 def test_is_null_expression() -> None:
-    from pyiceberg.expressions import (
-        IsNull,
-    )
+    from pyiceberg.expressions import IsNull
 
     expr = _to_ast("(pa.compute.field('borough')).is_null()")
     assert _convert_predicate(expr) == IsNull("borough")
 
 
 def test_is_not_null_expression() -> None:
-    from pyiceberg.expressions import (
-        IsNull,
-        Not,
-    )
+    from pyiceberg.expressions import IsNull, Not
 
     expr = _to_ast("~(pa.compute.field('location_id')).is_null()")
     assert _convert_predicate(expr) == Not(IsNull("location_id"))
 
 
 def test_is_nan_expression() -> None:
-    from pyiceberg.expressions import (
-        IsNaN,
-    )
+    from pyiceberg.expressions import IsNaN
 
     expr = _to_ast("(pa.compute.field('borough')).is_nan()")
     assert _convert_predicate(expr) == IsNaN("borough")
 
 
 def test_is_not_nan_expression() -> None:
-    from pyiceberg.expressions import (
-        IsNaN,
-        Not,
-    )
+    from pyiceberg.expressions import IsNaN, Not
 
     expr = _to_ast("~(pa.compute.field('location_id')).is_nan()")
     assert _convert_predicate(expr) == Not(IsNaN("location_id"))
 
 
 def test_isin_expression() -> None:
-    from pyiceberg.expressions import (
-        In,
-        literal,
-    )
+    from pyiceberg.expressions import In, literal
 
     expr = _to_ast("(pa.compute.field('location_id')).isin([1,2,3])")
     assert _convert_predicate(expr) == In(
@@ -131,45 +114,35 @@ def test_parse_combined_expression() -> None:
 
 
 def test_parse_gt() -> None:
-    from pyiceberg.expressions import (
-        GreaterThan,
-    )
+    from pyiceberg.expressions import GreaterThan
 
     expr = _to_ast("(pa.compute.field('dt') > '2023-08-08')")
     assert _convert_predicate(expr) == GreaterThan("dt", "2023-08-08")
 
 
 def test_parse_gteq() -> None:
-    from pyiceberg.expressions import (
-        GreaterThanOrEqual,
-    )
+    from pyiceberg.expressions import GreaterThanOrEqual
 
     expr = _to_ast("(pa.compute.field('dt') >= '2023-08-08')")
     assert _convert_predicate(expr) == GreaterThanOrEqual("dt", "2023-08-08")
 
 
 def test_parse_eq() -> None:
-    from pyiceberg.expressions import (
-        EqualTo,
-    )
+    from pyiceberg.expressions import EqualTo
 
     expr = _to_ast("(pa.compute.field('dt') == '2023-08-08')")
     assert _convert_predicate(expr) == EqualTo("dt", "2023-08-08")
 
 
 def test_parse_lt() -> None:
-    from pyiceberg.expressions import (
-        LessThan,
-    )
+    from pyiceberg.expressions import LessThan
 
     expr = _to_ast("(pa.compute.field('dt') < '2023-08-08')")
     assert _convert_predicate(expr) == LessThan("dt", "2023-08-08")
 
 
 def test_parse_lteq() -> None:
-    from pyiceberg.expressions import (
-        LessThanOrEqual,
-    )
+    from pyiceberg.expressions import LessThanOrEqual
 
     expr = _to_ast("(pa.compute.field('dt') <= '2023-08-08')")
     assert _convert_predicate(expr) == LessThanOrEqual("dt", "2023-08-08")
