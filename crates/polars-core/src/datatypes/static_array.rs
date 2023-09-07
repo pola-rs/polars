97db4babc6a67c007115b30f159085a18eaf9e6a
Orson Peters <orsonpeters@gmail.com>
2023-09-07 15:34:10 +0200

    forgot inline

--- crates/polars-core/src/datatypes/static_array.rs
+++ crates/polars-core/src/datatypes/static_array.rs
@@ -15,6 +15,7 @@ pub trait StaticArray: Array {
     where
         Self: 'a;
 
+    #[inline]
     fn get(&self, idx: usize) -> Option<Self::ValueT<'_>> {
         if idx >= self.len() {
             None
@@ -25,6 +26,7 @@ pub trait StaticArray: Array {
 
     /// # Safety
     /// It is the callers responsibility that the `idx < self.len()`.
+    #[inline]
     unsafe fn get_unchecked(&self, idx: usize) -> Option<Self::ValueT<'_>> {
         if self.is_null_unchecked(idx) {
             None
@@ -44,6 +46,7 @@ impl<T: NumericNative> StaticArray for PrimitiveArray<T> {
     type ValueT<'a> = T;
     type ValueIterT<'a> = std::iter::Copied<std::slice::Iter<'a, T>>;
 
+    #[inline]
     unsafe fn value_unchecked(&self, idx: usize) -> Self::ValueT<'_> {
         self.value_unchecked(idx)
     }
@@ -65,6 +68,7 @@ impl StaticArray for BooleanArray {
     type ValueT<'a> = bool;
     type ValueIterT<'a> = BitmapIter<'a>;
 
+    #[inline]
     unsafe fn value_unchecked(&self, idx: usize) -> Self::ValueT<'_> {
         self.value_unchecked(idx)
     }
@@ -86,6 +90,7 @@ impl StaticArray for Utf8Array<i64> {
     type ValueT<'a> = &'a str;
     type ValueIterT<'a> = Utf8ValuesIter<'a, i64>;
 
+    #[inline]
     unsafe fn value_unchecked(&self, idx: usize) -> Self::ValueT<'_> {
         self.value_unchecked(idx)
     }
@@ -107,6 +112,7 @@ impl StaticArray for BinaryArray<i64> {
     type ValueT<'a> = &'a [u8];
     type ValueIterT<'a> = BinaryValueIter<'a, i64>;
 
+    #[inline]
     unsafe fn value_unchecked(&self, idx: usize) -> Self::ValueT<'_> {
         self.value_unchecked(idx)
     }
@@ -128,6 +134,7 @@ impl StaticArray for ListArray<i64> {
     type ValueT<'a> = Box<dyn Array>;
     type ValueIterT<'a> = ListValuesIter<'a, i64>;
 
+    #[inline]
     unsafe fn value_unchecked(&self, idx: usize) -> Self::ValueT<'_> {
         self.value_unchecked(idx)
     }
@@ -150,6 +157,7 @@ impl StaticArray for FixedSizeListArray {
     type ValueT<'a> = Box<dyn Array>;
     type ValueIterT<'a> = ArrayValuesIter<'a, FixedSizeListArray>;
 
+    #[inline]
     unsafe fn value_unchecked(&self, idx: usize) -> Self::ValueT<'_> {
         self.value_unchecked(idx)
     }
@@ -172,6 +180,7 @@ impl<T: PolarsObject> StaticArray for ObjectArray<T> {
     type ValueT<'a> = &'a T;
     type ValueIterT<'a> = ObjectValueIter<'a, T>;
 
+    #[inline]
     unsafe fn value_unchecked(&self, idx: usize) -> Self::ValueT<'_> {
         self.value_unchecked(idx)
     }
