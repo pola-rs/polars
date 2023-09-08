c410597c4eed71a8648dab35c1382b0d31556535
Stijn de Gooijer <stijn@degooijer.io>
2023-09-08 11:25:59 +0200

    Add future_annotations lint

--- py-polars/docs/source/conf.py
+++ py-polars/docs/source/conf.py
@@ -1,6 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+from __future__ import annotations
+
 import inspect
 import os
 import re
