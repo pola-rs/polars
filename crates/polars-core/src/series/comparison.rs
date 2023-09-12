2eb7478ed77a1f6965b8a05da103a3c2314b4ffc
Joe <joseribeiro1017@gmail.com>
2023-09-12 09:17:10 +0100

    feat: add support for decimal comparison in the series comparison module

--- crates/polars-core/src/series/comparison.rs
+++ crates/polars-core/src/series/comparison.rs
@@ -28,6 +28,8 @@ macro_rules! impl_compare {
             DataType::Float32 => lhs.f32().unwrap().$method(rhs.f32().unwrap()),
             DataType::Float64 => lhs.f64().unwrap().$method(rhs.f64().unwrap()),
             DataType::List(_) => lhs.list().unwrap().$method(rhs.list().unwrap()),
+            #[cfg(feature = "dtype-decimal")]
+            DataType::Decimal(_, _) => lhs.decimal().unwrap().$method(rhs.decimal().unwrap()),
             #[cfg(feature = "dtype-struct")]
             DataType::Struct(_) => lhs
                 .struct_()
