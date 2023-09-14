afff93e0c87ab96fab8972a5a7c4909f93124072
ritchie <ritchie46@gmail.com>
2023-09-14 20:44:06 +0200

    more

--- crates/polars-plan/src/dsl/function_expr/plugin.rs
+++ crates/polars-plan/src/dsl/function_expr/plugin.rs
@@ -1,15 +1,40 @@
+use std::sync::RwLock;
 use polars_ffi::*;
 
 use super::*;
 use arrow::ffi::{
     ArrowSchema, import_field_from_c
 };
+use libloading::Library;
+use once_cell::sync::Lazy;
+
+static LOADED: Lazy<RwLock<PlHashMap<String, Library>>> = Lazy::new(|| {
+    Default::default()
+});
+
+fn get_lib(lib: &str) -> PolarsResult<&'static Library> {
+    let lib_map = LOADED.read().unwrap();
+    if let Some(library) = lib_map.get(lib) {
+        // lifetime is static as we never remove libraries.
+        return Ok(unsafe { std::mem::transmute::<&Library, &'static Library>(library) })
+    } else {
+            drop(lib_map);
+            let libary = unsafe { Library::new(lib).map_err(|e| {
+                PolarsError::ComputeError(format!("error loading dynamic library: {e}").into())
+            })?} ;
+
+            let mut lib_map = LOADED.write().unwrap();
+            lib_map.insert(lib.to_string(), libary);
+            drop(lib_map);
+
+        return get_lib(lib)
+    }
+}
 
 pub(super) unsafe fn call_plugin(s: &[Series], lib: &str, symbol: &str) -> PolarsResult<Series> {
-    let out = {
-        let lib = libloading::Library::new(lib).map_err(|e| {
-            PolarsError::ComputeError(format!("error loading dynamic library: {e}").into())
-        })?;
+    let lib = get_lib(lib)?;
+
+
         let symbol: libloading::Symbol<
             unsafe extern "C" fn(*const SeriesExport, usize) -> SeriesExport,
         > = lib.get(symbol.as_bytes()).unwrap();
@@ -20,20 +45,12 @@ pub(super) unsafe fn call_plugin(s: &[Series], lib: &str, symbol: &str) -> Polar
         let slice_ptr = input.as_ptr();
         std::mem::forget(input);
 
-        // let out = symbol(slice_ptr, n_args);
-        // let out = import_series(out);
-        // out
-        slice_ptr
-    };
-        // dbg!(out);
-        drop(out);
-        Ok(Series::full_null("", s[0].len(), &DataType::Null))
+        let out = symbol(slice_ptr, n_args);
+        import_series(out)
 }
 
 pub(super) unsafe fn plugin_field(fields: &[Field], lib: &str, symbol: &str) -> PolarsResult<Field> {
-    let lib = libloading::Library::new(lib).map_err(|e| {
-        PolarsError::ComputeError(format!("error loading dynamic library: {e}").into())
-    })?;
+    let lib = get_lib(lib)?;
 
     let symbol: libloading::Symbol<
         unsafe extern "C" fn(*const ArrowSchema, usize) -> ArrowSchema,
@@ -42,7 +59,6 @@ pub(super) unsafe fn plugin_field(fields: &[Field], lib: &str, symbol: &str) -> 
     let fields = fields.iter().map(|field| arrow::ffi::export_field_to_c(&field.to_arrow())).collect::<Vec<_>>().into_boxed_slice();
     let n_args = fields.len();
     let slice_ptr = fields.as_ptr();
-    std::mem::forget(fields);
     let out = symbol(slice_ptr, n_args);
 
     let arrow_field = import_field_from_c(&out)?;
