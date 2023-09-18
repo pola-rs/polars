716007ca327ebd1f2708f6c93d4945a23524373f
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-09-18 10:34:18 +0200

    try removing try-except

--- py-polars/tests/parametric/test_groupby_rolling.py
+++ py-polars/tests/parametric/test_groupby_rolling.py
@@ -166,17 +166,7 @@ def test_rolling_aggs(
         if window.is_empty():
             expected_dict["value"].append(None)
         else:
-            try:
-                value = getattr(window["value"], aggregation)()
-            except pl.exceptions.PolarsPanicError as exc:
-                assert any(  # noqa: PT017
-                    msg in str(exc)
-                    for msg in (
-                        "attempt to multiply with overflow",
-                        "attempt to add with overflow",
-                    )
-                )
-                reject()
+            value = getattr(window["value"], aggregation)()
             expected_dict["value"].append(value)
     expected = pl.DataFrame(expected_dict).select(
         pl.col("ts").cast(pl.Datetime(time_unit)),
