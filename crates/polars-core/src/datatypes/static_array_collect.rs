e4d7a4ad9c9c2a7c212ce77e039df5a28f9659c2
Orson Peters <orsonpeters@gmail.com>
2023-09-18 16:04:45 +0200

    list collect physical type fix

--- crates/polars-core/src/datatypes/static_array_collect.rs
+++ crates/polars-core/src/datatypes/static_array_collect.rs
@@ -703,7 +703,7 @@ impl<T: AsArray> ArrayFromIterDtype<T> for ListArray<i64> {
         for arr in &iter_values {
             builder.push(arr.as_array());
         }
-        builder.finish(Some(&dtype.to_arrow())).unwrap()
+        builder.finish(Some(&dtype.to_physical().to_arrow())).unwrap()
     }
 
     fn try_arr_from_iter_with_dtype<E, I: IntoIterator<Item = Result<T, E>>>(
@@ -725,7 +725,7 @@ impl<T: AsArray> ArrayFromIterDtype<Option<T>> for ListArray<i64> {
         for arr in &iter_values {
             builder.push_opt(arr.as_ref().map(|a| a.as_array()));
         }
-        builder.finish(Some(&dtype.to_arrow())).unwrap()
+        builder.finish(Some(&dtype.to_physical().to_arrow())).unwrap()
     }
 
     fn try_arr_from_iter_with_dtype<E, I: IntoIterator<Item = Result<Option<T>, E>>>(
@@ -751,7 +751,7 @@ impl ArrayFromIterDtype<Box<dyn Array>> for FixedSizeListArray {
         for arr in iter_values {
             builder.push(arr.into_boxed_array());
         }
-        builder.finish(Some(&dtype.to_arrow())).unwrap()
+        builder.finish(Some(&dtype.to_physical().to_arrow())).unwrap()
     }
 
     fn try_arr_from_iter_with_dtype<E, I: IntoIterator<Item = Result<Box<dyn Array>, E>>>(
@@ -780,7 +780,7 @@ impl ArrayFromIterDtype<Option<Box<dyn Array>>> for FixedSizeListArray {
                 None => builder.push_null(),
             }
         }
-        builder.finish(Some(&dtype.to_arrow())).unwrap()
+        builder.finish(Some(&dtype.to_physical().to_arrow())).unwrap()
     }
 
     fn try_arr_from_iter_with_dtype<
