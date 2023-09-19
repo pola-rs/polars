e3d622b919658f0d4056b0ed22420c3b5841b5be
ritchie <ritchie46@gmail.com>
2023-09-19 11:31:16 +0200

    perf: use ryu and itoa for primitive serialization

--- crates/polars-io/src/csv/write_impl.rs
+++ crates/polars-io/src/csv/write_impl.rs
@@ -8,7 +8,6 @@ use std::io::Write;
 use arrow::temporal_conversions;
 #[cfg(feature = "timezones")]
 use chrono::TimeZone;
-use lexical_core::{FormattedSize, ToLexical};
 use memchr::{memchr, memchr2};
 use polars_arrow::time_zone::Tz;
 use polars_core::prelude::*;
@@ -53,15 +52,16 @@ fn fmt_and_escape_str(f: &mut Vec<u8>, v: &str, options: &SerializeOptions) -> s
     }
 }
 
-fn fast_float_write<N: ToLexical>(f: &mut Vec<u8>, n: N, write_size: usize) -> std::io::Result<()> {
-    let len = f.len();
-    f.reserve(write_size);
-    unsafe {
-        let buffer = std::slice::from_raw_parts_mut(f.as_mut_ptr().add(len), write_size);
-        let written_n = n.to_lexical(buffer).len();
-        f.set_len(len + written_n);
-    }
-    Ok(())
+fn fast_float_write<I: ryu::Float>(f: &mut Vec<u8>, val: I) {
+    let mut buffer = ryu::Buffer::new();
+    let value = buffer.format(val);
+    f.extend_from_slice(value.as_bytes())
+}
+
+fn write_integer<I: itoa::Integer>(f: &mut Vec<u8>, val: I) {
+    let mut buffer = itoa::Buffer::new();
+    let value = buffer.format(val);
+    f.extend_from_slice(value.as_bytes())
 }
 
 unsafe fn write_anyvalue(
@@ -96,20 +96,50 @@ unsafe fn write_anyvalue(
 
             match value {
                 AnyValue::Null => write!(f, "{}", &options.null),
-                AnyValue::Int8(v) => write!(f, "{v}"),
-                AnyValue::Int16(v) => write!(f, "{v}"),
-                AnyValue::Int32(v) => write!(f, "{v}"),
-                AnyValue::Int64(v) => write!(f, "{v}"),
-                AnyValue::UInt8(v) => write!(f, "{v}"),
-                AnyValue::UInt16(v) => write!(f, "{v}"),
-                AnyValue::UInt32(v) => write!(f, "{v}"),
-                AnyValue::UInt64(v) => write!(f, "{v}"),
+                AnyValue::Int8(v) => {
+                    write_integer(f, v);
+                    Ok(())
+                },
+                AnyValue::Int16(v) => {
+                    write_integer(f, v);
+                    Ok(())
+                },
+                AnyValue::Int32(v) => {
+                    write_integer(f, v);
+                    Ok(())
+                },
+                AnyValue::Int64(v) => {
+                    write_integer(f, v);
+                    Ok(())
+                },
+                AnyValue::UInt8(v) => {
+                    write_integer(f, v);
+                    Ok(())
+                },
+                AnyValue::UInt16(v) => {
+                    write_integer(f, v);
+                    Ok(())
+                },
+                AnyValue::UInt32(v) => {
+                    write_integer(f, v);
+                    Ok(())
+                },
+                AnyValue::UInt64(v) => {
+                    write_integer(f, v);
+                    Ok(())
+                },
                 AnyValue::Float32(v) => match &options.float_precision {
-                    None => fast_float_write(f, v, f32::FORMATTED_SIZE_DECIMAL),
+                    None => {
+                        fast_float_write(f, v);
+                        Ok(())
+                    },
                     Some(precision) => write!(f, "{v:.precision$}"),
                 },
                 AnyValue::Float64(v) => match &options.float_precision {
-                    None => fast_float_write(f, v, f64::FORMATTED_SIZE_DECIMAL),
+                    None => {
+                        fast_float_write(f, v);
+                        Ok(())
+                    },
                     Some(precision) => write!(f, "{v:.precision$}"),
                 },
                 _ => {
