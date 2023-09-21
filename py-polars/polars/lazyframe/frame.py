9842e324d47a6603efa52ece277516b2badeb377
Steven Van Ingelgem <steven.vaningelgem@kbc.be>
2023-09-21 11:23:06 +0200

    WIP

--- py-polars/polars/lazyframe/frame.py
+++ py-polars/polars/lazyframe/frame.py
@@ -1861,21 +1861,21 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
         return result  # type: ignore[return-value]
 
     def sink_parquet(
-        self,
-        path: str | Path,
-        *,
-        compression: str = "zstd",
-        compression_level: int | None = None,
-        statistics: bool = False,
-        row_group_size: int | None = None,
-        data_pagesize_limit: int | None = None,
-        maintain_order: bool = True,
-        type_coercion: bool = True,
-        predicate_pushdown: bool = True,
-        projection_pushdown: bool = True,
-        simplify_expression: bool = True,
-        no_optimization: bool = False,
-        slice_pushdown: bool = True,
+            self,
+            path: str | Path,
+            *,
+            compression: str = "zstd",
+            compression_level: int | None = None,
+            statistics: bool = False,
+            row_group_size: int | None = None,
+            data_pagesize_limit: int | None = None,
+            maintain_order: bool = True,
+            type_coercion: bool = True,
+            predicate_pushdown: bool = True,
+            projection_pushdown: bool = True,
+            simplify_expression: bool = True,
+            no_optimization: bool = False,
+            slice_pushdown: bool = True,
     ) -> DataFrame:
         """
         Persists a LazyFrame at the provided path.
@@ -1944,6 +1944,99 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
         )
 
         return lf.sink_parquet(
+            path=path,
+            compression=compression,
+            compression_level=compression_level,
+            statistics=statistics,
+            row_group_size=row_group_size,
+            data_pagesize_limit=data_pagesize_limit,
+            maintain_order=maintain_order,
+        )
+
+    def sink_parquet_cloud(
+            self,
+            path: str | Path,
+            *,
+            compression: str = "zstd",
+            compression_level: int | None = None,
+            statistics: bool = False,
+            row_group_size: int | None = None,
+            data_pagesize_limit: int | None = None,
+            maintain_order: bool = True,
+            type_coercion: bool = True,
+            predicate_pushdown: bool = True,
+            projection_pushdown: bool = True,
+            simplify_expression: bool = True,
+            no_optimization: bool = False,
+            slice_pushdown: bool = True,
+    ) -> DataFrame:
+        """
+        Persists a LazyFrame at the provided path.
+
+        This allows streaming results that are larger than RAM to be written to disk.
+
+        Parameters
+        ----------
+        path
+            File path to which the file should be written.
+        compression : {'lz4', 'uncompressed', 'snappy', 'gzip', 'lzo', 'brotli', 'zstd'}
+            Choose "zstd" for good compression performance.
+            Choose "lz4" for fast compression/decompression.
+            Choose "snappy" for more backwards compatibility guarantees
+            when you deal with older parquet readers.
+        compression_level
+            The level of compression to use. Higher compression means smaller files on
+            disk.
+
+            - "gzip" : min-level: 0, max-level: 10.
+            - "brotli" : min-level: 0, max-level: 11.
+            - "zstd" : min-level: 1, max-level: 22.
+        statistics
+            Write statistics to the parquet headers. This requires extra compute.
+        row_group_size
+            Size of the row groups in number of rows.
+            If None (default), the chunks of the `DataFrame` are
+            used. Writing in smaller chunks may reduce memory pressure and improve
+            writing speeds.
+        data_pagesize_limit
+            Size limit of individual data pages.
+            If not set defaults to 1024 * 1024 bytes
+        maintain_order
+            Maintain the order in which data is processed.
+            Setting this to `False` will  be slightly faster.
+        type_coercion
+            Do type coercion optimization.
+        predicate_pushdown
+            Do predicate pushdown optimization.
+        projection_pushdown
+            Do projection pushdown optimization.
+        simplify_expression
+            Run simplify expressions optimization.
+        no_optimization
+            Turn off (certain) optimizations.
+        slice_pushdown
+            Slice pushdown optimization.
+
+        Returns
+        -------
+        DataFrame
+
+        Examples
+        --------
+        >>> lf = pl.scan_csv("/path/to/my_larger_than_ram_file.csv")  # doctest: +SKIP
+        >>> lf.sink_parquet_cloud("s3://bucket/path/out.parquet")  # doctest: +SKIP
+
+        """
+        lf = self._set_sink_optimizations(
+            type_coercion=type_coercion,
+            predicate_pushdown=predicate_pushdown,
+            projection_pushdown=projection_pushdown,
+            simplify_expression=simplify_expression,
+            no_optimization=no_optimization,
+            slice_pushdown=slice_pushdown,
+        )
+
+        return lf.sink_parquet_cloud(
             path=path,
             compression=compression,
             compression_level=compression_level,
--- py-polars/src/lazyframe.rs
+++ py-polars/src/lazyframe.rs
@@ -478,9 +478,46 @@ impl PyLazyFrame {
     }
 
     #[allow(clippy::too_many_arguments)]
-    #[cfg(all(feature = "streaming", feature = "parquet"))]
+    #[cfg(all(feature = "parquet", any(feature = "streaming", feature = "cloud_write"))]
     #[pyo3(signature = (path, compression, compression_level, statistics, row_group_size, data_pagesize_limit, maintain_order))]
     fn sink_parquet(
+        &self,
+        py: Python,
+        path: PathBuf,
+        compression: &str,
+        compression_level: Option<i32>,
+        statistics: bool,
+        row_group_size: Option<usize>,
+        data_pagesize_limit: Option<usize>,
+        maintain_order: bool,
+        cloud_options: &PyDict,
+    ) -> PyResult<()> {
+        let compression = parse_parquet_compression(compression, compression_level)?;
+
+        let options = ParquetWriteOptions {
+            compression,
+            statistics,
+            row_group_size,
+            data_pagesize_limit,
+            maintain_order,
+        };
+
+        // TODO: convert 'cloud_options' into CloudOptions
+        // TODO: switch between sink_parquet & sink_parquet_cloud based on `is_cloud_url`
+
+        // if we don't allow threads and we have udfs trying to acquire the gil from different
+        // threads we deadlock.
+        py.allow_threads(|| {
+            let ldf = self.ldf.clone();
+            ldf.sink_parquet(path, options).map_err(PyPolarsErr::from)
+        })?;
+        Ok(())
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    #[cfg(all(feature = "streaming", feature = "cloud_write", feature = "parquet"))]
+    #[pyo3(signature = (path, compression, compression_level, statistics, row_group_size, data_pagesize_limit, maintain_order))]
+    fn sink_parquet_cloud(
         &self,
         py: Python,
         path: PathBuf,
@@ -505,7 +542,7 @@ impl PyLazyFrame {
         // threads we deadlock.
         py.allow_threads(|| {
             let ldf = self.ldf.clone();
-            ldf.sink_parquet(path, options).map_err(PyPolarsErr::from)
+            ldf.sink_parquet_cloud(path, options).map_err(PyPolarsErr::from)
         })?;
         Ok(())
     }
