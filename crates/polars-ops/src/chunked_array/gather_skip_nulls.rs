7b62b48c59a31ed0375af9cf529f6d57cc2a142a
Orson Peters <orsonpeters@gmail.com>
2023-09-22 14:44:50 +0200

    add (failing) test

--- crates/polars-ops/src/chunked_array/gather_skip_nulls.rs
+++ crates/polars-ops/src/chunked_array/gather_skip_nulls.rs
@@ -6,7 +6,6 @@ use polars_core::prelude::gather::check_bounds_ca;
 use polars_core::prelude::*;
 use polars_utils::index::check_bounds;
 
-
 /// # Safety
 /// For each index pair, pair.0 < len && pair.1 < ca.null_count() must hold.
 unsafe fn gather_skip_null_idx_pairs_unchecked<'a, T: PolarsDataType>(
@@ -125,5 +124,75 @@ impl<T: PolarsDataType> ChunkGatherSkipNulls<IdxCa> for ChunkedArray<T> {
             arr = arr.with_validity_typed(concatenate_validities(&array_refs));
         }
         Ok(ChunkedArray::from_chunk_iter_like(self, [arr]))
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use std::ops::Range;
+
+    use rand::distributions::uniform::SampleUniform;
+    use rand::prelude::*;
+    use rand::rngs::SmallRng;
+
+    use super::*;
+
+    fn random_vec<T: SampleUniform + PartialOrd + Clone, R: Rng>(
+        rng: &mut R,
+        val: Range<T>,
+        len_range: Range<usize>,
+    ) -> Vec<T> {
+        let n = rng.gen_range(len_range);
+        (0..n).map(|_| rng.gen_range(val.clone())).collect()
+    }
+
+    fn random_filter<T: Clone, R: Rng>(rng: &mut R, v: &[T], pr: Range<f64>) -> Vec<Option<T>> {
+        let p = rng.gen_range(pr);
+        let rand_filter = |x| Some(x).filter(|_| rng.gen::<f64>() < p);
+        v.iter().cloned().map(rand_filter).collect()
+    }
+
+    fn ref_gather_nulls(v: Vec<Option<u32>>, idx: Vec<Option<usize>>) -> Option<Vec<Option<u32>>> {
+        if idx.iter().any(|oi| oi.map(|i| i >= v.len()) == Some(true)) {
+            return None;
+        }
+        Some(idx.into_iter().map(|i| v[i?]).collect())
+    }
+
+    fn test_equal_ref(ca: &UInt32Chunked, idx_ca: &IdxCa) {
+        let ref_ca: Vec<Option<u32>> = ca.into_iter().collect();
+        let ref_idx_ca: Vec<Option<usize>> =
+            (&idx_ca).into_iter().map(|i| Some(i? as usize)).collect();
+        let gather = ca.gather_skip_nulls(idx_ca).ok();
+        let ref_gather = ref_gather_nulls(ref_ca, ref_idx_ca);
+        assert!(gather.map(|ca| ca.into_iter().collect()) == ref_gather);
+    }
+
+    #[rustfmt::skip]
+    #[test]
+    fn gather_skip_nulls() {
+        let mut rng = SmallRng::seed_from_u64(0xdeadbeef);
+
+        for _test in 0..20 {
+            let num_elem_chunks = rng.gen_range(1..10);
+            let elem_chunks: Vec<_> = (0..num_elem_chunks).map(|_| random_vec(&mut rng, 0..u32::MAX, 0..10)).collect();
+            let null_elem_chunks: Vec<_> = elem_chunks.iter().map(|c| random_filter(&mut rng, c, 0.7..1.0)).collect();
+            let num_elems: usize = elem_chunks.iter().map(|c| c.len()).sum();
+
+            let num_idx_chunks = rng.gen_range(1..10);
+            let idx_chunks: Vec<_> = (0..num_idx_chunks).map(|_| random_vec(&mut rng, 0..num_elems as IdxSize, 0..10)).collect();
+            let null_idx_chunks: Vec<_> = idx_chunks.iter().map(|c| random_filter(&mut rng, c, 0.7..1.0)).collect();
+            
+            let nonnull_ca = UInt32Chunked::from_chunk_iter("", elem_chunks.iter().cloned().map(|v| v.into_iter().collect_arr()));
+            let ca = UInt32Chunked::from_chunk_iter("", null_elem_chunks.iter().cloned().map(|v| v.into_iter().collect_arr()));
+            let nonnull_idx_ca = IdxCa::from_chunk_iter("", idx_chunks.iter().cloned().map(|v| v.into_iter().collect_arr()));
+            let idx_ca = IdxCa::from_chunk_iter("", null_idx_chunks.iter().cloned().map(|v| v.into_iter().collect_arr()));
+            
+            test_equal_ref(&ca, &idx_ca);
+            test_equal_ref(&ca, &nonnull_idx_ca);
+            test_equal_ref(&nonnull_ca, &idx_ca);
+            test_equal_ref(&nonnull_ca, &nonnull_idx_ca);
+        }
     }
 }
