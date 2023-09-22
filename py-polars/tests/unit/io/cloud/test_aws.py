8813e08075d7bbf6894adb6fa519406e1f8829ea
Steven Van Ingelgem <steven@vaningelgem.be>
2023-09-22 13:31:53 +0200

    Also test for s3 for scan* methods.

--- py-polars/tests/unit/io/cloud/test_aws.py
+++ py-polars/tests/unit/io/cloud/test_aws.py
@@ -73,7 +73,9 @@ def test_read_s3(s3: str, function: Callable[..., Any], extension: str) -> None:
 @pytest.mark.parametrize(
     ("function", "extension"),
     [
+        (pl.scan_csv, "csv"),
         (pl.scan_ipc, "ipc"),
+        (pl.scan_parquet, "parquet"),
     ],
 )
 def test_scan_s3(s3: str, function: Callable[..., Any], extension: str) -> None:
