9d1695e699c60bf3b64674df09a5e31da40c13c1
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-09-28 14:00:02 +0100

    fix(python): handle ambiguous datetimes in pl.lit

--- py-polars/tests/unit/functions/test_lit.py
+++ py-polars/tests/unit/functions/test_lit.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+from datetime import datetime
 from typing import Any, Sequence
 
 import pytest
 
 import polars as pl
+from polars.testing import assert_frame_equal
 
 
 @pytest.mark.parametrize(
@@ -17,3 +19,21 @@ import polars as pl
 def test_lit_deprecated_sequence_input(sequence: Sequence[Any]) -> None:
     with pytest.deprecated_call():
         pl.lit(sequence)
+
+
+def test_lit_ambiguous_datetimes_11379() -> None:
+    df = pl.DataFrame(
+        {
+            "ts": pl.datetime_range(
+                datetime(2020, 10, 25),
+                datetime(2020, 10, 25, 2),
+                "1h",
+                time_zone="Europe/London",
+                eager=True,
+            )
+        }
+    )
+    for i in range(len(df)):
+        result = df.filter(pl.col("ts") >= df["ts"][i])
+        expected = df[i:]
+        assert_frame_equal(result, expected)
