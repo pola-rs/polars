65d8c2a054c71a661210b4694c2b1e1a51494def
ritchie <ritchie46@gmail.com>
2023-09-29 16:28:56 +0200

    feat: multi-threaded async runtime

--- crates/polars-io/src/pl_async.rs
+++ crates/polars-io/src/pl_async.rs
@@ -1,9 +1,16 @@
+use std::ops::Deref;
+
+use once_cell::sync::Lazy;
 use tokio::runtime::{Builder, Runtime};
 
-pub fn get_runtime() -> Runtime {
-    Builder::new_current_thread()
+static RUNTIME: Lazy<Runtime> = Lazy::new(|| {
+    Builder::new_multi_thread()
         .enable_io()
         .enable_time()
         .build()
         .unwrap()
+});
+
+pub fn get_runtime() -> &'static Runtime {
+    RUNTIME.deref()
 }
