b2db9b9e6404848c634925181c3c9c1cec6d9e63
alexander-beedie <alexander.m.beedie@icloud.com>
2023-10-01 10:56:31 +0400

    tweak to fix docs build

--- py-polars/polars/functions/col.py
+++ py-polars/polars/functions/col.py
@@ -1,19 +1,67 @@
 from __future__ import annotations
 
 import contextlib
-from typing import TYPE_CHECKING, Iterable, Protocol, cast
+from typing import TYPE_CHECKING, Any, Iterable, Protocol, cast
 
 from polars.datatypes import is_polars_dtype
 from polars.utils._wrap import wrap_expr
 
+plr: Any = None
 with contextlib.suppress(ImportError):  # Module not available when building docs
-    import polars.polars as plr
+    import polars.polars as plr  # type: ignore[no-redef]
 
 if TYPE_CHECKING:
     from polars.expr.expr import Expr
     from polars.type_aliases import PolarsDataType
 
 __all__ = ["col"]
+
+
+def _create_col(
+    name: str | PolarsDataType | Iterable[str] | Iterable[PolarsDataType],
+    *more_names: str | PolarsDataType,
+) -> Expr:
+    """Create one or more column expressions representing column(s) in a DataFrame."""
+    if more_names:
+        if isinstance(name, str):
+            names_str = [name]
+            names_str.extend(more_names)  # type: ignore[arg-type]
+            return wrap_expr(plr.cols(names_str))
+        elif is_polars_dtype(name):
+            dtypes = [name]
+            dtypes.extend(more_names)
+            return wrap_expr(plr.dtype_cols(dtypes))
+        else:
+            raise TypeError(
+                "invalid input for `col`"
+                f"\n\nExpected `str` or `DataType`, got {type(name).__name__!r}."
+            )
+
+    if isinstance(name, str):
+        return wrap_expr(plr.col(name))
+    elif is_polars_dtype(name):
+        return wrap_expr(plr.dtype_cols([name]))
+    elif isinstance(name, Iterable):
+        names = list(name)
+        if not names:
+            return wrap_expr(plr.cols(names))
+
+        item = names[0]
+        if isinstance(item, str):
+            return wrap_expr(plr.cols(names))
+        elif is_polars_dtype(item):
+            return wrap_expr(plr.dtype_cols(names))
+        else:
+            raise TypeError(
+                "invalid input for `col`"
+                "\n\nExpected iterable of type `str` or `DataType`,"
+                f" got iterable of type {type(item).__name__!r}"
+            )
+    else:
+        raise TypeError(
+            "invalid input for `col`"
+            f"\n\nExpected `str` or `DataType`, got {type(name).__name__!r}"
+        )
 
 
 # appease lint by casting `col` with a protocol that conforms to the factory interface
@@ -32,7 +80,7 @@ class Column(Protocol):
 # handle attribute lookup on the metaclass (we use the factory uninstantiated)
 class ColumnFactoryMeta(type):
     def __getattr__(self, name: str) -> Expr:
-        return wrap_expr(plr.col(name))
+        return _create_col(name)
 
 
 # factory that creates columns using `col("name")` or `col.name` syntax
@@ -100,7 +148,7 @@ class ColumnFactory(metaclass=ColumnFactoryMeta):
         *more_names: str | PolarsDataType,
     ) -> Expr:
         """
-        Create a column expression representing column(s) in a dataframe.
+        Create one or more column expressions representing column(s) in a DataFrame.
 
         Parameters
         ----------
@@ -235,55 +283,23 @@ class ColumnFactory(metaclass=ColumnFactoryMeta):
         │ 2   ┆ 22        ┆ 1   │
         └─────┴───────────┴─────┘
 
-
         """
-        if more_names:
-            if isinstance(name, str):
-                names_str = [name]
-                names_str.extend(more_names)  # type: ignore[arg-type]
-                return wrap_expr(plr.cols(names_str))
-            elif is_polars_dtype(name):
-                dtypes = [name]
-                dtypes.extend(more_names)
-                return wrap_expr(plr.dtype_cols(dtypes))
-            else:
-                raise TypeError(
-                    "invalid input for `col`"
-                    f"\n\nExpected `str` or `DataType`, got {type(name).__name__!r}."
-                )
+        return _create_col(name, *more_names)
 
-        if isinstance(name, str):
-            return wrap_expr(plr.col(name))
-        elif is_polars_dtype(name):
-            return wrap_expr(plr.dtype_cols([name]))
-        elif isinstance(name, Iterable):
-            names = list(name)
-            if not names:
-                return wrap_expr(plr.cols(names))
-
-            item = names[0]
-            if isinstance(item, str):
-                return wrap_expr(plr.cols(names))
-            elif is_polars_dtype(item):
-                return wrap_expr(plr.dtype_cols(names))
-            else:
-                raise TypeError(
-                    "invalid input for `col`"
-                    "\n\nExpected iterable of type `str` or `DataType`,"
-                    f" got iterable of type {type(item).__name__!r}"
-                )
-        else:
-            raise TypeError(
-                "invalid input for `col`"
-                f"\n\nExpected `str` or `DataType`, got {type(name).__name__!r}"
-            )
+    # appease sphinx; we actually use '__new__'
+    def __call__(
+        self,
+        name: str | PolarsDataType | Iterable[str] | Iterable[PolarsDataType],
+        *more_names: str | PolarsDataType,
+    ) -> Expr:
+        return _create_col(name, *more_names)
 
     def __getattr__(self, name: str) -> Expr:
         """
         Create a column expression using attribute syntax.
 
-        Note that this syntax does not support passing data types or multiple column
-        names.
+        Note that this syntax does not support passing data
+        types or multiple column names.
 
         Parameters
         ----------
