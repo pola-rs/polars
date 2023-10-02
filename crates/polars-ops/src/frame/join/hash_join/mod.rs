f9f68eaa68a7334cb5f415a51c7a95279c19b464
Ritchie Vink <ritchie46@gmail.com>
2023-10-02 10:28:12 +0200

    chore: upgrade to nightly-10-02 (#11460)

--- crates/polars-ops/src/frame/join/hash_join/mod.rs
+++ crates/polars-ops/src/frame/join/hash_join/mod.rs
@@ -12,7 +12,6 @@ mod zip_outer;
 pub use args::*;
 pub use multiple_keys::private_left_join_multiple_keys;
 pub(super) use multiple_keys::*;
-use polars_core::prelude::*;
 use polars_core::utils::{_set_partition_size, slice_slice, split_ca};
 use polars_core::POOL;
 pub(super) use single_keys::*;
