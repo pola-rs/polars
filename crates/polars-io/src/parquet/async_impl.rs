1ba0058616389141376976aff9fd93025d9d3751
ritchie <ritchie46@gmail.com>
2023-10-03 08:59:49 +0200

    spawn downloads on worker threads

--- crates/polars-io/src/parquet/async_impl.rs
+++ crates/polars-io/src/parquet/async_impl.rs
@@ -93,19 +93,11 @@ impl ParquetObjectStore {
     }
 }
 
-/// A vector of downloaded RowGroups.
-/// A RowGroup will have 1 or more columns, for each column we store:
-///   - a reference to its metadata
-///   - the actual content as downloaded from object storage (generally cloud).
-type RowGroupChunks = Vec<Vec<(u64, Bytes)>>;
-
 async fn read_single_column_async(
     async_reader: &ParquetObjectStore,
-    meta: &ColumnChunkMetaData,
+    start: usize,
+    length: usize,
 ) -> PolarsResult<(u64, Bytes)> {
-    let (start, length) = meta.byte_range();
-    let start = start as usize;
-    let length = length as usize;
     let chunk = async_reader
         .store
         .get_range(&async_reader.path, start..start + length)
@@ -114,128 +106,23 @@ async fn read_single_column_async(
     Ok((start as u64, chunk))
 }
 
-// async fn download_row_group<'a>(
-//     async_reader: &ParquetObjectStore,
-//     columns: &'a [ColumnChunkMetaData],
-//     field_name: &str,
-// ) {
-//
-//     {
-//         let ranges = get_field_columns(columns, field_name).iter().map(|meta| {
-//             let (start, len) = meta.byte_range();
-//             (start, start + len)
-//         }).collect::<Vec<_>>();
-//
-//         let mut clustered = Vec::with_capacity(ranges.len());
-//         let first_range = ranges[0];
-//         let start = first_range.0;
-//         let mut previous_end = first_range.1;
-//         clustered.push((start, previous_end, vec![first_range]));
-//         for (start, end) in ranges.iter().copied() {
-//             let this_range = (start, end);
-//
-//             if start == previous_end {
-//                 let (_start, total_end, members) = clustered.last_mut().unwrap();
-//                 *total_end += end;
-//                 members.push(this_range);
-//             } else {
-//                 clustered.push((start, end, vec![this_range]))
-//             }
-//             previous_end = end;
-//         }
-//         dbg!(clustered);
-//     }
-//
-// }
-//
-// fn cluster_ranges(ranges: &[(u64, u64)]) -> Vec<(u64, u64, Vec<(u64, u64)>)> {
-//
-//     let mut clustered = Vec::with_capacity(ranges.len());
-//     let first_range = ranges[0];
-//     let start = first_range.0;
-//     let mut previous_end = first_range.1;
-//     clustered.push((start, previous_end, vec![first_range]));
-//     for (start, end) in ranges.iter().copied().skip(1) {
-//         let this_range = (start, end);
-//
-//         if start == previous_end {
-//             let (_start, total_end, members) = clustered.last_mut().unwrap();
-//             *total_end += end;
-//             members.push(this_range);
-//         } else {
-//             clustered.push((start, end, vec![this_range]))
-//         }
-//         previous_end = end;
-//     }
-//     dbg!(clustered)
-// }
-
-async fn read_columns_async<'a>(
+async fn read_columns_async2(
     async_reader: &ParquetObjectStore,
-    columns: &'a [ColumnChunkMetaData],
-    field_name: &str,
+    ranges: &[(u64, u64)],
 ) -> PolarsResult<Vec<(u64, Bytes)>> {
-    let futures = get_field_columns(columns, field_name)
-        .into_iter()
-        .map(|meta| async { read_single_column_async(async_reader, meta).await });
+    let futures = ranges
+        .iter()
+        .map(|(start, length)| async { read_single_column_async(async_reader, *start as usize, *length as usize).await });
 
     try_join_all(futures).await
 }
-
-// async fn download_projection_rg<'a: 'b, 'b>(
-//     fields: &[SmartString],
-//     projection: &[usize],
-//     row_group: &'a RowGroupMetaData,
-//     async_reader: &'b ParquetObjectStore,
-// ) -> PolarsResult<RowGroupChunks<'a>> {
-//
-//     let mut ranges = vec![];
-//
-//     // - index of the buffer
-//     // - start
-//     // - len
-//     let mut mapping = vec![];
-//
-//     let mut current_range = (0, 0);
-//
-//     let mut buf_idx = 0usize;
-//     let mut previous_col_idx = projection[0].overflowing_sub(1).0;
-//     // let mut first_iter = true;
-//     for (field_name, &col_idx) in fields.iter().zip(projection) {
-//         let columns = row_group.columns();
-//         let col_chunk_meta = get_field_columns(columns, field_name);
-//         let start = col_chunk_meta.iter().map(|meta| meta.byte_range().0).next().unwrap();
-//         let end = col_chunk_meta.iter().map(|meta| {
-//             let (start, len) = meta.byte_range();
-//             let end = start + len;
-//             end
-//         } ).last().unwrap();
-//
-//         mapping.push((buf_idx, start - current_range.0, end));
-//
-//         // Same cluster;
-//         if col_idx == previous_col_idx.overflowing_add(1).0 {
-//             current_range.1 = end;
-//         // New cluster
-//         } else {
-//             buf_idx += 1;
-//             current_range = (start, end);
-//         }
-//
-//         previous_col_idx = col_idx;
-//     }
-//     ranges.push(current_range);
-//     dbg!(ranges, mapping);
-//
-//     todo!()
-// }
 
 /// Download rowgroups for the column whose indexes are given in `projection`.
 /// We concurrently download the columns for each field.
 async fn download_projection(
     fields: &[SmartString],
     row_groups: &[RowGroupMetaData],
-    async_reader: &ParquetObjectStore,
+    async_reader: &Arc<ParquetObjectStore>,
 ) -> PolarsResult<Vec<Vec<(u64, Bytes)>>> {
 
     // Build the cartesian product of the fields and the row groups.
@@ -244,41 +131,26 @@ async fn download_projection(
         .flat_map(|name| row_groups.iter().map(move |r| (name.clone(), r)))
         .map(|(name, row_group)| async move {
             let columns = row_group.columns();
-            read_columns_async(async_reader, columns, name.as_ref())
-                .map_err(to_compute_err)
-                .await
+            let ranges = columns.iter().filter_map(|meta| {
+                if meta.descriptor().path_in_schema[0] == name.as_str() {
+                   Some(meta.byte_range())
+                } else {
+                    None
+                }
+            }).collect::<Vec<_>>();
+            let async_reader = async_reader.clone();
+            let handle = tokio::spawn(async move {
+                read_columns_async2(&async_reader, &ranges).await
+            });
+            handle.await.unwrap()
         });
 
     // Download concurrently
     futures::future::try_join_all(product_futures).await
 }
 
-// async fn download_projection2<'a: 'b, 'b>(
-//     fields: &[smartstring],
-//     row_groups: &'a [RowGroupMetaData],
-//     async_reader: &'b ParquetObjectStore,
-// ) -> PolarsResult<RowGroupChunks<'a>> {
-//
-//     // Build the cartesian product of the fields and the row groups.
-//     let product_futures = fields
-//         .iter()
-//         .flat_map(|name| row_groups.iter().map(move |r| (name.clone(), r)))
-//         .map(|(name, row_group)| async move {
-//             let columns = row_group.columns();
-//
-//             let handle = tokio::spawn(async move {
-//                 read_columns_async(async_reader, columns, name.as_ref())
-//                     .map_err(to_compute_err).await
-//             });
-//             handle.await.unwrap()
-//         });
-//
-//     // Download concurrently
-//     futures::future::try_join_all(product_futures).await
-// }
-
 pub struct FetchRowGroupsFromObjectStore {
-    reader: ParquetObjectStore,
+    reader: Arc<ParquetObjectStore>,
     row_groups_metadata: Vec<RowGroupMetaData>,
     projected_fields: Vec<SmartString>,
     projection: Vec<usize>,
@@ -306,7 +178,7 @@ impl FetchRowGroupsFromObjectStore {
 
 
         Ok(FetchRowGroupsFromObjectStore {
-            reader,
+            reader: Arc::new(reader),
             row_groups_metadata: metadata.row_groups.to_owned(),
             projected_fields,
             projection,
@@ -333,9 +205,6 @@ impl FetchRowGroupsFromObjectStore {
         // Package in the format required by ColumnStore.
         let downloaded =
             download_projection(&self.projected_fields, row_groups, &self.reader).await?;
-
-        // let downloaded =
-        //     download_projection_rg(&self.projected_fields, &self.projection, &row_groups[0], &self.reader).await?;
 
         if self.logging {
             eprintln!(
