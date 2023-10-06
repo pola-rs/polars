9174371f3c98c9853d8a186d4140555ec46b45a5
ritchie <ritchie46@gmail.com>
2023-10-06 11:36:24 +0200

    remove dbg

--- crates/polars-lazy/src/physical_plan/executors/union.rs
+++ crates/polars-lazy/src/physical_plan/executors/union.rs
@@ -103,7 +103,6 @@ impl Executor for UnionExec {
                     .collect::<PolarsResult<Vec<_>>>()
             });
 
-            dbg!(&out);
             concat_df(out?.iter().flat_map(|dfs| dfs.iter()))
         }
         .map(|mut df| {
