3bda3830490d7702321fd3d12726bd15ac703a13
Orson Peters <orsonpeters@gmail.com>
2023-10-09 13:10:13 +0200

    fix(python): parse time unit properly in pl.lit (#11573)

--- py-polars/tests/unit/functions/test_lit.py
+++ py-polars/tests/unit/functions/test_lit.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Any, Sequence
 
+import numpy as np
 import pytest
 
 import polars as pl
@@ -37,3 +38,10 @@ def test_lit_ambiguous_datetimes_11379() -> None:
         result = df.filter(pl.col("ts") >= df["ts"][i])
         expected = df[i:]
         assert_frame_equal(result, expected)
+
+
+def test_list_datetime_11571() -> None:
+    sec_np_ns = np.timedelta64(1_000_000_000, "ns")
+    sec_np_us = np.timedelta64(1_000_000, "us")
+    assert pl.select(pl.lit(sec_np_ns))[0, 0] == timedelta(seconds=1)
+    assert pl.select(pl.lit(sec_np_us))[0, 0] == timedelta(seconds=1)
