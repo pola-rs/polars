ff69f18842297892f4d0b1a8dac0ff7a18bc676d
Steven Van Ingelgem <steven.vaningelgem@kbc.be>
2023-10-10 07:41:52 +0200

    Combine both methods into 1.

--- py-polars/polars/lazyframe/frame.py
+++ py-polars/polars/lazyframe/frame.py
@@ -1878,99 +1878,6 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
         row_group_size: int | None = None,
         data_pagesize_limit: int | None = None,
         maintain_order: bool = True,
-        type_coercion: bool = True,
-        predicate_pushdown: bool = True,
-        projection_pushdown: bool = True,
-        simplify_expression: bool = True,
-        no_optimization: bool = False,
-        slice_pushdown: bool = True,
-    ) -> DataFrame:
-        """
-        Persists a LazyFrame at the provided path.
-
-        This allows streaming results that are larger than RAM to be written to disk.
-
-        Parameters
-        ----------
-        path
-            File path to which the file should be written.
-        compression : {'lz4', 'uncompressed', 'snappy', 'gzip', 'lzo', 'brotli', 'zstd'}
-            Choose "zstd" for good compression performance.
-            Choose "lz4" for fast compression/decompression.
-            Choose "snappy" for more backwards compatibility guarantees
-            when you deal with older parquet readers.
-        compression_level
-            The level of compression to use. Higher compression means smaller files on
-            disk.
-
-            - "gzip" : min-level: 0, max-level: 10.
-            - "brotli" : min-level: 0, max-level: 11.
-            - "zstd" : min-level: 1, max-level: 22.
-        statistics
-            Write statistics to the parquet headers. This requires extra compute.
-        row_group_size
-            Size of the row groups in number of rows.
-            If None (default), the chunks of the `DataFrame` are
-            used. Writing in smaller chunks may reduce memory pressure and improve
-            writing speeds.
-        data_pagesize_limit
-            Size limit of individual data pages.
-            If not set defaults to 1024 * 1024 bytes
-        maintain_order
-            Maintain the order in which data is processed.
-            Setting this to `False` will  be slightly faster.
-        type_coercion
-            Do type coercion optimization.
-        predicate_pushdown
-            Do predicate pushdown optimization.
-        projection_pushdown
-            Do projection pushdown optimization.
-        simplify_expression
-            Run simplify expressions optimization.
-        no_optimization
-            Turn off (certain) optimizations.
-        slice_pushdown
-            Slice pushdown optimization.
-
-        Returns
-        -------
-        DataFrame
-
-        Examples
-        --------
-        >>> lf = pl.scan_csv("/path/to/my_larger_than_ram_file.csv")  # doctest: +SKIP
-        >>> lf.sink_parquet("out.parquet")  # doctest: +SKIP
-
-        """
-        lf = self._set_sink_optimizations(
-            type_coercion=type_coercion,
-            predicate_pushdown=predicate_pushdown,
-            projection_pushdown=projection_pushdown,
-            simplify_expression=simplify_expression,
-            no_optimization=no_optimization,
-            slice_pushdown=slice_pushdown,
-        )
-
-        return lf.sink_parquet(
-            path=normalize_filepath(path),
-            compression=compression,
-            compression_level=compression_level,
-            statistics=statistics,
-            row_group_size=row_group_size,
-            data_pagesize_limit=data_pagesize_limit,
-            maintain_order=maintain_order,
-        )
-
-    def sink_parquet_cloud(
-        self,
-        cloud_url: str,
-        *,
-        compression: str = "zstd",
-        compression_level: int | None = None,
-        statistics: bool = False,
-        row_group_size: int | None = None,
-        data_pagesize_limit: int | None = None,
-        maintain_order: bool = True,
         storage_options: dict[str, str] | None = None,
         retries: int = 0,
         type_coercion: bool = True,
@@ -1987,9 +1894,8 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
 
         Parameters
         ----------
-        cloud_url
-            String pointing to the location of the cloud object where the data should be
-            written.
+        path
+            File path to which the file should be written (can be a cloud url).
         compression : {'lz4', 'uncompressed', 'snappy', 'gzip', 'lzo', 'brotli', 'zstd'}
             Choose "zstd" for good compression performance.
             Choose "lz4" for fast compression/decompression.
@@ -2050,7 +1956,11 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
         Examples
         --------
         >>> lf = pl.scan_csv("/path/to/my_larger_than_ram_file.csv")  # doctest: +SKIP
-        >>> lf.sink_parquet_cloud("s3://bucket/out.parquet")  # doctest: +SKIP
+        >>> lf.sink_parquet("out.parquet")  # doctest: +SKIP
+
+        >>> lf = pl.scan_csv("/path/to/my_larger_than_ram_file.csv")  # doctest: +SKIP
+        >>> lf.sink_parquet("s3://bucket/out.parquet")  # doctest: +SKIP
+
         """
         lf = self._set_sink_optimizations(
             type_coercion=type_coercion,
@@ -2061,17 +1971,25 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
             slice_pushdown=slice_pushdown,
         )
 
-        return lf.sink_parquet_cloud(
-            cloud_url=cloud_url,
+        arguments = dict(
             compression=compression,
             compression_level=compression_level,
             statistics=statistics,
             row_group_size=row_group_size,
             data_pagesize_limit=data_pagesize_limit,
             maintain_order=maintain_order,
-            cloud_options=list((storage_options or {}).items()),
-            retries=retries,
         )
+
+        if isinstance(path, str) and _is_supported_cloud(path):
+            method_to_call = lf.sink_parquet_cloud
+            arguments["path"] = path
+            arguments["cloud_options"] = list((storage_options or {}).items())
+            arguments["retries"] = retries
+        else:
+            method_to_call = lf.sink_parquet
+            arguments["path"] = normalize_filepath(path)
+
+        return method_to_call(lf, **arguments)
 
     def sink_ipc(
         self,
