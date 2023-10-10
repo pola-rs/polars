0ffa3058647e9732e12e7b3dd6bf0e5f30ae608c
ritchie <ritchie46@gmail.com>
2023-10-10 11:44:39 +0200

    dbg

--- crates/polars-io/src/parquet/read_impl.rs
+++ crates/polars-io/src/parquet/read_impl.rs
@@ -511,7 +511,6 @@ impl BatchedParquetReader {
     }
 
     pub async fn next_batches(&mut self, n: usize) -> PolarsResult<Option<Vec<DataFrame>>> {
-        dbg!(n);
         // fill up fifo stack
         if self.row_group_offset <= self.n_row_groups && self.chunks_fifo.len() < n {
             let row_group_start = self.row_group_offset;
