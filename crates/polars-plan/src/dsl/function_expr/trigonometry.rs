a39bdfa1ddd03a782f80b1616ac0aa63e990b4d0
Steven Van Ingelgem <steven@vaningelgem.be>
2023-10-13 16:53:19 +0200

    Improve performance a little bit.

--- crates/polars-plan/src/dsl/function_expr/trigonometry.rs
+++ crates/polars-plan/src/dsl/function_expr/trigonometry.rs
@@ -177,7 +177,7 @@ where
     T::Native: Float,
     ChunkedArray<T>: IntoSeries,
 {
-    Ok(ca.apply_values(|v| v.cos() / v.sin()).into_series())
+    Ok(ca.apply_values(|v| v.tan().powi(-1)).into_series())
 }
 
 fn sin<T>(ca: &ChunkedArray<T>) -> PolarsResult<Series>
