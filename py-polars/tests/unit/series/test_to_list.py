6b1a93dde051261c8a6a4a58baa9b6e2596a9145
Stijn de Gooijer <stijn@degooijer.io>
2023-10-16 07:39:10 +0200

    Add parametric test for `s.to_list`

---
+++ py-polars/tests/unit/series/test_to_list.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from hypothesis import given
+
+import polars as pl
+from polars.testing import assert_series_equal
+from polars.testing.parametric import series
+
+
+@given(
+    s=series(
+        # TODO: Remove exclusion when bug is fixed.
+        # https://github.com/pola-rs/polars/issues/11744
+        excluded_dtypes=[pl.Duration]
+    )
+)
+def test_to_list(s: pl.Series) -> None:
+    values = s.to_list()
+    result = pl.Series(values, dtype=s.dtype)
+    assert_series_equal(s, result, categorical_as_str=True)
