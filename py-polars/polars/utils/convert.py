d320c0157a8e81b8c791bb3e5d811898b69ad02e
Stijn de Gooijer <stijn@degooijer.io>
2023-10-16 08:07:27 +0200

    minor refactor

--- py-polars/polars/utils/convert.py
+++ py-polars/polars/utils/convert.py
@@ -71,24 +71,24 @@ def _timedelta_to_pl_duration(td: timedelta | str | None) -> str | None:
     """Convert python timedelta to a polars duration string."""
     if td is None or isinstance(td, str):
         return td
-    else:
-        if td.days >= 0:
-            d = td.days and f"{td.days}d" or ""
-            s = td.seconds and f"{td.seconds}s" or ""
-            us = td.microseconds and f"{td.microseconds}us" or ""
-        else:
-            if not td.seconds and not td.microseconds:
-                d = td.days and f"{td.days}d" or ""
-                s = ""
-                us = ""
-            else:
-                corrected_d = td.days + 1
-                d = corrected_d and f"{corrected_d}d" or "-"
-                corrected_seconds = 24 * 3600 - (td.seconds + (td.microseconds > 0))
-                s = corrected_seconds and f"{corrected_seconds}s" or ""
-                us = td.microseconds and f"{10**6 - td.microseconds}us" or ""
 
-        return f"{d}{s}{us}"
+    if td.days >= 0:
+        d = td.days and f"{td.days}d" or ""
+        s = td.seconds and f"{td.seconds}s" or ""
+        us = td.microseconds and f"{td.microseconds}us" or ""
+    else:
+        if not td.seconds and not td.microseconds:
+            d = td.days and f"{td.days}d" or ""
+            s = ""
+            us = ""
+        else:
+            corrected_d = td.days + 1
+            d = corrected_d and f"{corrected_d}d" or "-"
+            corrected_seconds = 24 * 3600 - (td.seconds + (td.microseconds > 0))
+            s = corrected_seconds and f"{corrected_seconds}s" or ""
+            us = td.microseconds and f"{10**6 - td.microseconds}us" or ""
+
+    return f"{d}{s}{us}"
 
 
 def _negate_duration(duration: str) -> str:
