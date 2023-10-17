d1868c401fca32262355198449631728073f05ec
Weijie Guo <reswqa@163.com>
2023-10-17 10:27:13 +0800

    remove marco for cum_sum

--- crates/polars-ops/src/series/ops/cum_agg.rs
+++ crates/polars-ops/src/series/ops/cum_agg.rs
@@ -158,12 +158,12 @@ pub fn cumsum(s: &Series, reverse: bool) -> PolarsResult<Series> {
             let s = s.cast(&Int64)?;
             cumsum_numeric(s.i64()?, reverse).into_series()
         },
-        dt if dt.is_numeric() => {
-            with_match_physical_numeric_polars_type!(s.dtype(), |$T| {
-                let ca: &ChunkedArray<$T> = s.as_ref().as_ref().as_ref();
-                cumsum_numeric(ca, reverse).into_series()
-            })
-        },
+        Int32 => cumsum_numeric(s.i32()?, reverse).into_series(),
+        UInt32 => cumsum_numeric(s.u32()?, reverse).into_series(),
+        Int64 => cumsum_numeric(s.i64()?, reverse).into_series(),
+        UInt64 => cumsum_numeric(s.u64()?, reverse).into_series(),
+        Float32 => cumsum_numeric(s.f32()?, reverse).into_series(),
+        Float64 => cumsum_numeric(s.f64()?, reverse).into_series(),
         #[cfg(feature = "dtype-duration")]
         Duration(tu) => {
             let s = s.to_physical_repr();
@@ -177,46 +177,42 @@ pub fn cumsum(s: &Series, reverse: bool) -> PolarsResult<Series> {
 
 /// Get an array with the cumulative min computed at every element.
 pub fn cummin(s: &Series, reverse: bool) -> PolarsResult<Series> {
-    let out = match s.dtype() {
+    let original_type = s.dtype();
+    let s = s.to_physical_repr();
+    match s.dtype() {
         dt if dt.is_numeric() => {
             with_match_physical_numeric_polars_type!(s.dtype(), |$T| {
                 let ca: &ChunkedArray<$T> = s.as_ref().as_ref().as_ref();
-                cummin_numeric(ca, reverse).into_series()
-            })
-        },
-        dt if dt.to_physical().is_integer() => {
-            let original_type = s.dtype();
-            let s = s.to_physical_repr();
-            with_match_physical_numeric_polars_type!(s.dtype(), |$T| {
-                let ca: &ChunkedArray<$T> = s.as_ref().as_ref().as_ref();
-                cummin_numeric(ca, reverse).cast(original_type)?
+                let out = cummin_numeric(ca, reverse).into_series();
+                if original_type.is_logical(){
+                    out.cast(original_type)
+                }else{
+                    Ok(out)
+                }
             })
         },
         dt => polars_bail!(opq = cummin, dt),
-    };
-    Ok(out)
+    }
 }
 
 /// Get an array with the cumulative max computed at every element.
 pub fn cummax(s: &Series, reverse: bool) -> PolarsResult<Series> {
-    let out = match s.dtype() {
+    let original_type = s.dtype();
+    let s = s.to_physical_repr();
+    match s.dtype() {
         dt if dt.is_numeric() => {
             with_match_physical_numeric_polars_type!(s.dtype(), |$T| {
                 let ca: &ChunkedArray<$T> = s.as_ref().as_ref().as_ref();
-                cummax_numeric(ca, reverse).into_series()
+                let out = cummax_numeric(ca, reverse).into_series();
+                if original_type.is_logical(){
+                    out.cast(original_type)
+                }else{
+                    Ok(out)
+                }
             })
         },
-        dt if dt.to_physical().is_integer() => {
-            let original_type = s.dtype();
-            let s = s.to_physical_repr();
-            with_match_physical_numeric_polars_type!(s.dtype(), |$T| {
-                let ca: &ChunkedArray<$T> = s.as_ref().as_ref().as_ref();
-                cummax_numeric(ca, reverse).cast(original_type)?
-            })
-        },
-        dt => polars_bail!(opq = cummax, dt),
-    };
-    Ok(out)
+        dt => polars_bail!(opq = cummin, dt),
+    }
 }
 
 pub fn cumcount(s: &Series, reverse: bool) -> PolarsResult<Series> {
