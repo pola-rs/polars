a1898a04a14efe1402faae5b8cf7e3ed110cdb96
Stijn de Gooijer <stijndegooijer@gmail.com>
2023-10-23 14:30:11 +0200

    Update Rust bindings

--- py-polars/src/expr/general.rs
+++ py-polars/src/expr/general.rs
@@ -339,14 +339,14 @@ impl PyExpr {
         self.inner.clone().forward_fill(limit).into()
     }
 
-    fn shift(&self, n: i64) -> Self {
-        self.inner.clone().shift(n).into()
-    }
-    fn shift_and_fill(&self, n: i64, fill_value: Self) -> Self {
-        self.inner
-            .clone()
-            .shift_and_fill(n, fill_value.inner)
-            .into()
+    fn shift(&self, n: i64, fill_value: Option<Self>) -> Self {
+        let expr = self.inner.clone();
+
+        if let Some(v) = fill_value {
+            expr.shift_and_fill(n, v.inner).into()
+        } else {
+            expr.shift(n).into()
+        }
     }
 
     fn fill_null(&self, expr: Self) -> Self {
--- py-polars/src/lazyframe.rs
+++ py-polars/src/lazyframe.rs
@@ -834,14 +834,14 @@ impl PyLazyFrame {
         ldf.reverse().into()
     }
 
-    fn shift(&self, n: i64) -> Self {
-        let ldf = self.ldf.clone();
-        ldf.shift(n).into()
-    }
+    fn shift(&self, n: i64, fill_value: Option<PyExpr>) -> Self {
+        let lf = self.ldf.clone();
 
-    fn shift_and_fill(&self, n: i64, fill_value: PyExpr) -> Self {
-        let ldf = self.ldf.clone();
-        ldf.shift_and_fill(n, fill_value.inner).into()
+        if let Some(v) = fill_value {
+            lf.shift_and_fill(n, v.inner).into()
+        } else {
+            lf.shift(n).into()
+        }
     }
 
     fn fill_nan(&self, fill_value: PyExpr) -> Self {
