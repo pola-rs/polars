6ffbc82a854d6fcc3045e85401206009f115a0e1
Stijn de Gooijer <stijndegooijer@gmail.com>
2023-10-30 19:55:05 +0100

    Update docstrings

--- py-polars/polars/dataframe/frame.py
+++ py-polars/polars/dataframe/frame.py
@@ -6489,7 +6489,7 @@ class DataFrame:
 
         Cast all frame columns to the specified dtype:
 
-        >>> df.cast(pl.Utf8).to_dict(False)
+        >>> df.cast(pl.Utf8).to_dict(as_series=False)
         {'foo': ['1', '2', '3'],
          'bar': ['6.0', '7.0', '8.0'],
          'ham': ['2020-01-02', '2021-03-04', '2022-05-06']}
