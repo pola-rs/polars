396b7efaaea5c489e8f9f099448282a8ee16867a
cmdlineluser <99486669+cmdlineluser@users.noreply.github.com>
2023-10-30 23:41:03 +0000

    add lifetimes infer helpers

--- crates/polars-ops/src/chunked_array/strings/substring.rs
+++ crates/polars-ops/src/chunked_array/strings/substring.rs
@@ -2,8 +2,6 @@ use polars_core::prelude::arity::{binary_elementwise, ternary_elementwise};
 
 use crate::chunked_array::{Int64Chunked, UInt64Chunked, Utf8Chunked};
 
-/// Returns a Utf8Array<O> with a substring starting from `start` and with optional length `length` of each of the elements in `array`.
-/// `offset` can be negative, in which case the offset counts from the end of the string.
 fn utf8_substring_ternary<'a>(
     opt_str_val: Option<&'a str>,
     opt_offset: Option<i64>,
@@ -11,7 +9,7 @@ fn utf8_substring_ternary<'a>(
 ) -> Option<&'a str> {
     match (opt_str_val, opt_offset) {
         (Some(str_val), Some(offset)) => {
-            // compute where we should offset slicing this entry.
+            // if `offset` is negative, it counts from the end of the string
             let offset = if offset >= 0 {
                 offset as usize
             } else {
@@ -26,7 +24,6 @@ fn utf8_substring_ternary<'a>(
 
             let mut iter_chars = str_val.char_indices();
             if let Some((offset_idx, _)) = iter_chars.nth(offset) {
-                // length of the str
                 let len_end = str_val.len() - offset_idx;
 
                 // slice to end of str if no length given
@@ -38,7 +35,7 @@ fn utf8_substring_ternary<'a>(
                 if length == 0 {
                     return Some("");
                 }
-                // compute
+
                 let end_idx = iter_chars
                     .nth(length.saturating_sub(1))
                     .map(|(idx, _)| idx)
@@ -62,8 +59,32 @@ pub(super) fn utf8_substring(
         (1, 1) => ca.apply_generic(|opt_str_val| {
             utf8_substring_ternary(opt_str_val, offset.get(0), length.get(0))
         }),
-        //(1, _) => binary_elementwise(ca, length, |opt_str_val, opt_length| utf8_substring_ternary(opt_str_val, offset.get(0), opt_length)),
-        //(_, 1) => binary_elementwise(ca, offset, |opt_str_val, opt_offset| utf8_substring_ternary(opt_str_val, opt_offset, length.get(0))),
+        (1, _) => {
+            fn infer<F: for<'a> FnMut(Option<&'a str>, Option<u64>) -> Option<&'a str>>(f: F) -> F where
+            {
+                f
+            }
+
+            let off = offset.get(0);
+            binary_elementwise(
+                ca,
+                length,
+                infer(|val, len| utf8_substring_ternary(val, off, len)),
+            )
+        },
+        (_, 1) => {
+            fn infer<F: for<'a> FnMut(Option<&'a str>, Option<i64>) -> Option<&'a str>>(f: F) -> F where
+            {
+                f
+            }
+
+            let len = length.get(0);
+            binary_elementwise(
+                ca,
+                offset,
+                infer(|val, off| utf8_substring_ternary(val, off, len)),
+            )
+        },
         _ => ternary_elementwise(ca, offset, length, utf8_substring_ternary),
     }
 }
