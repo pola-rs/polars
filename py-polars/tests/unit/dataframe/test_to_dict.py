16252450de811beae028b7c92b67fc091f2bae8b
Stijn de Gooijer <stijndegooijer@gmail.com>
2023-10-31 08:51:48 +0100

    Add test

--- py-polars/tests/unit/dataframe/test_to_dict.py
+++ py-polars/tests/unit/dataframe/test_to_dict.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import pytest
 from hypothesis import given
 
 import polars as pl
@@ -12,3 +13,14 @@ def test_to_dict(df: pl.DataFrame) -> None:
     d = df.to_dict(as_series=False)
     result = pl.from_dict(d, schema=df.schema)
     assert_frame_equal(df, result, categorical_as_str=True)
+
+
+def test_to_dict_deprecated_positional() -> None:
+    with pytest.deprecated_call():
+        pl.DataFrame({"a": [1, 2]}).to_dict(False)
+
+
+def test_to_dict_deprecated_default() -> None:
+    with pytest.deprecated_call():
+        result = pl.DataFrame({"a": [1, 2]}).to_dict()
+    assert isinstance(result["a"], pl.Series)
