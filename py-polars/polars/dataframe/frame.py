24a74e0456cb6d289524fc83fdfe0fbc6889623a
Stijn de Gooijer <stijndegooijer@gmail.com>
2023-10-31 07:58:04 +0100

    Deprecate to_dict positional arg

--- py-polars/polars/dataframe/frame.py
+++ py-polars/polars/dataframe/frame.py
@@ -1919,7 +1919,7 @@ class DataFrame:
     ) -> dict[str, Series] | dict[str, list[Any]]:
         ...
 
-    # TODO: Make `as_series` keyword-only
+    @deprecate_nonkeyword_arguments(version="0.19.13")
     def to_dict(
         self,
         as_series: bool = True,  # noqa: FBT001
