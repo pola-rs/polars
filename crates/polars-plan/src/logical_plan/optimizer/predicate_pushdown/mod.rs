21778ab221170c88e5a2282b0384ba5dab51d6d6
Weijie Guo <reswqa@163.com>
2023-11-02 14:41:00 +0800

    fix: with_row_count should block predicate push down for lazy csv

--- crates/polars-plan/src/logical_plan/optimizer/predicate_pushdown/mod.rs
+++ crates/polars-plan/src/logical_plan/optimizer/predicate_pushdown/mod.rs
@@ -260,7 +260,13 @@ impl<'a> PredicatePushDown<'a> {
                 file_options: options,
                 output_schema
             } => {
-                let local_predicates = partition_by_full_context(&mut acc_predicates, expr_arena);
+                let mut local_predicates = partition_by_full_context(&mut acc_predicates, expr_arena);
+                if let Some(ref row_count) = options.row_count{
+                    let mut row_count_predicates = transfer_to_local_by_name(expr_arena, &mut acc_predicates, |name| {
+                        name.as_ref() == &row_count.name
+                    });
+                    local_predicates.append(&mut row_count_predicates);
+                }
                 let predicate = predicate_at_scan(acc_predicates, predicate, expr_arena);
 
                 if let (true, Some(predicate)) = (file_info.hive_parts.is_some(), predicate) {
