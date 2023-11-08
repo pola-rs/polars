f7b12ff07a2d6468dc4d983c98facd19d2e223fa
Stijn de Gooijer <stijndegooijer@gmail.com>
2023-11-08 12:26:10 +0100

    fix: Fix Decimal dtype table repr

--- py-polars/polars/series/string.py
+++ py-polars/polars/series/string.py
@@ -308,7 +308,7 @@ class StringNameSpace:
         ... )
         >>> s.str.to_decimal()
         shape: (7,)
-        Series: '' [decimal[2]]
+        Series: '' [decimal[*,2]]
         [
             40.12
             3420.13
