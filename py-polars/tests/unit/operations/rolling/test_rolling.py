19e09eec3ac73c90d43539717d7e4bd20ca7cdd8
uchiiii <uchikun17@gmail.com>
2023-11-08 11:07:51 +0900

    fmt

--- py-polars/tests/unit/operations/rolling/test_rolling.py
+++ py-polars/tests/unit/operations/rolling/test_rolling.py
@@ -835,7 +835,7 @@ def test_rolling_weighted_quantile_10031() -> None:
 
 
 def test_rolling_meta_eq_10101() -> None:
-    assert pl.col("A").rolling_sum(10).meta.eq(pl.col("A").rolling_sum(10)) == True
+    assert pl.col("A").rolling_sum(10).meta.eq(pl.col("A").rolling_sum(10)) is True
 
 
 def test_rolling() -> None:
