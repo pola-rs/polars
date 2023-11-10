220a49336479c6edb8176cf0f10c0bedd9c06758
Stijn de Gooijer <stijndegooijer@gmail.com>
2023-11-10 01:57:44 +0100

    Remove duplicated code

--- crates/polars-plan/src/logical_plan/builder.rs
+++ crates/polars-plan/src/logical_plan/builder.rs
@@ -299,15 +299,7 @@ impl LogicalPlanBuilder {
         truncate_ragged_lines: bool,
     ) -> PolarsResult<Self> {
         let path = path.into();
-        let mut file = polars_utils::open_file(&path).map_err(|e| {
-            let path = path.to_string_lossy();
-            if path.len() > 88 {
-                let path: String = path.chars().skip(path.len() - 88).collect();
-                polars_err!(ComputeError: "error open file: ...{}, {}", path, e)
-            } else {
-                polars_err!(ComputeError: "error open file: {}, {}", path, e)
-            }
-        })?;
+        let mut file = polars_utils::open_file(&path)?;
 
         let paths = Arc::new([path]);
 
