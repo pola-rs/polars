c9e4e7dcb6507ddc7b96c1f7c089b6d0dbbc4e00
Ritchie Vink <ritchie46@gmail.com>
2023-11-10 08:41:13 -0500

    perf: indexvec in group-by (#12371)

--- crates/polars-lazy/src/physical_plan/expressions/filter.rs
+++ crates/polars-lazy/src/physical_plan/expressions/filter.rs
@@ -4,6 +4,7 @@ use arrow::legacy::is_valid::IsValid;
 use polars_core::frame::group_by::GroupsProxy;
 use polars_core::prelude::*;
 use polars_core::POOL;
+use polars_utils::idx_vec::IdxVec;
 use rayon::prelude::*;
 
 use crate::physical_plan::state::ExecutionState;
@@ -94,7 +95,7 @@ impl PhysicalExpr for FilterExpr {
                             let groups = groups
                                 .par_iter()
                                 .map(|(first, idx)| unsafe {
-                                    let idx: Vec<IdxSize> = idx
+                                    let idx: IdxVec = idx
                                         .iter()
                                         .copied()
                                         .filter(|i| {
@@ -114,7 +115,7 @@ impl PhysicalExpr for FilterExpr {
                             let groups = groups
                                 .par_iter()
                                 .map(|&[first, len]| unsafe {
-                                    let idx: Vec<IdxSize> = (first..first + len)
+                                    let idx: IdxVec = (first..first + len)
                                         .filter(|&i| {
                                             // SAFETY: just checked bounds in short circuited lhs
                                             predicate.value(i as usize)
