61a388c98e65922e7c83f8324453f7547d3fa877
Dawei Hu <hoodavy@gmail.com>
2023-11-16 23:02:49 +0800

    fix merge conflict

--- crates/polars-io/src/odbc/read.rs
+++ crates/polars-io/src/odbc/read.rs
@@ -4,11 +4,13 @@ use arrow::error::Result;
 use arrow::io::odbc::api::Cursor;
 use arrow::io::odbc::{api, read, write};
 
-use super::*;
+use super::{DataFrame, MmapBytesReader, PolarsResult, SerReader};
 
 pub struct OdbcReader<R> {
     reader: R,
-    connection_string: AsRef<str>,
+    connector: AsRef<str>,
+    max_batch_size: i32,
+    query: AsRef<str>,
 }
 
 impl<'a, R> SerReader<R> for OdbcReader<'a, R>
@@ -19,12 +21,42 @@ where
         OdbcReader { reader }
     }
 
-    fn set_connection_string(mut self, connection_string: AsRef<str>) -> Self {
+    fn set_connector(mut self, connector: AsRef<str>) -> Self {
         self.connection_string = connection_string.as_ref();
         self
     }
 
+    fn set_query(mut self, query: AsRef<str>) -> Self {
+        self.query = query.as_ref();
+        self
+    }
+
+    fn set_max_batch_size(mut self, max_batch_size: i32) -> Self {
+        self.max_batch_size = max_batch_size
+        self
+    }
+
     fn finish(mut self) -> PolarsResult<DataFrame> {
+        let env = api::Environment::new()?;
+        let connection = env.connect_with_connection_string(connector)?;
+
+        let mut a = connection.prepare(query)?;
+        let fields = read::infer_schema(&a)?;
+
+        let buffer = read::buffer_from_metadata(&a, self.max_batch_size)?;
+
+        let mut chunks = vec![];
+        while let Some(batch) = cursor.fetch()? {
+            let arrays = (0..batch.num_cols())
+                .zip(fields.iter())
+                .map(|(index, field)| {
+                    let column_view = batch.column(index);
+                    read::deserialize(column_view, field.data_type.clone())
+                })
+                .collect::<Vec<_>>();
+            chunks.push(Chunk::new(arrays));
+        }
+    
         todo!()
     }
 }
