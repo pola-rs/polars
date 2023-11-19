d7dc785889fc1c2bb499267e868022910bc6ac12
ritchie <ritchie46@gmail.com>
2023-11-19 09:22:19 +0100

    fix: ensure stats-evalutor exists

--- crates/polars-lazy/src/physical_plan/expressions/mod.rs
+++ crates/polars-lazy/src/physical_plan/expressions/mod.rs
@@ -608,7 +608,6 @@ pub trait PhysicalExpr: Send + Sync {
     /// Can take &dyn Statistics and determine of a file should be
     /// read -> `true`
     /// or not -> `false`
-    #[cfg(feature = "parquet")]
     fn as_stats_evaluator(&self) -> Option<&dyn polars_io::predicates::StatsEvaluator> {
         None
     }
