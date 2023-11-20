07d93908e401af83daa8c4a9b2f73179b2953182
dmitrybugakov <bugakov.dima@gmail.com>
2023-11-20 20:47:28 +0100

    feat(rust): Add env variable for disk spill path configuration (#9832)

--- crates/polars-pipe/src/executors/sinks/mod.rs
+++ crates/polars-pipe/src/executors/sinks/mod.rs
@@ -31,3 +31,18 @@ pub(crate) use sort::*;
 // We must strike a balance between cache coherence and resizing costs.
 // Overallocation seems a lot more expensive than resizing so we start reasonable small.
 const HASHMAP_INIT_SIZE: usize = 64;
+
+pub(crate) static POLARS_SPILL_DIR: &str = "POLARS_SPILL_DIR";
+
+pub(crate) fn get_base_spill_dir() -> std::path::PathBuf {
+    let base_dir = std::env::var(POLARS_SPILL_DIR)
+        .unwrap_or_else(|_| std::env::temp_dir().to_string_lossy().into_owned());
+
+    if polars_core::config::verbose() {
+        eprintln!("Using base directory for spilling: {}", base_dir);
+    }
+
+    let mut base_path = std::path::PathBuf::from(base_dir);
+    base_path.push("polars");
+    base_path
+}
