9c53549b2ff498af666fb286f45c1ab20ffe9b99
Ritchie Vink <ritchie46@gmail.com>
2023-11-27 09:14:35 +0100

    fix: corr return nan if denominator is invalid (#12708)
    Co-authored-by: Ran <rccalman@gmail.com>

--- py-polars/tests/unit/operations/test_statistics.py
+++ py-polars/tests/unit/operations/test_statistics.py
@@ -24,6 +24,11 @@ def test_corr() -> None:
     assert_frame_equal(result, expected)
 
 
+def test_corr_nan() -> None:
+    df = pl.DataFrame({"a": [1.0, 1.0], "b": [1.0, 2.0]})
+    assert str(df.select(pl.corr("a", "b", ddof=1))[0, 0]) == "nan"
+
+
 def test_hist() -> None:
     a = pl.Series("a", [1, 3, 8, 8, 2, 1, 3])
     assert (
