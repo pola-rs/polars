dd5dd4f739f876a070d4fdc8d631d75009df2eb3
ritchie <ritchie46@gmail.com>
2023-11-27 20:30:01 +0100

    add test for undelete

--- crates/polars-arrow/src/legacy/kernels/rolling/median_filter.rs
+++ crates/polars-arrow/src/legacy/kernels/rolling/median_filter.rs
@@ -10,10 +10,6 @@ use polars_utils::IdxSize;
 use polars_utils::sort::arg_sort_ascending;
 use crate::legacy::kernels::rolling::Idx;
 
-fn get_h(k: usize) -> usize {
-    (k - 1) / 2
-}
-
 struct Block<'a, T: Copy + IsFloat> {
     k: usize,
     tail: usize,
@@ -31,12 +27,17 @@ struct Block<'a, T: Copy + IsFloat> {
 
 impl<T: Copy + Debug + IsFloat> Debug for Block<'_, T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        if self.n_element == 0 {
+            return writeln!(f, "empty block")
+        }
         writeln!(f, "elements in list: {}", self.n_element)?;
         writeln!(f, "m: {}", self.m)?;
         writeln!(f, "m_index: {}", self.current_index)?;
         writeln!(f, "α[m]: {:?}", self.alpha[self.m])?;
 
         let mut p = self.m as u32;
+
+        // Find start.
         loop {
             p = self.prev[p as usize];
             if p as usize == self.tail {
@@ -44,15 +45,13 @@ impl<T: Copy + Debug + IsFloat> Debug for Block<'_, T> {
                 break
             }
         }
-        let mut current = Vec::with_capacity(self.n_element);
-        current.push(self.alpha[p as usize]);
 
-        loop {
-            p = self.next[p as usize];
-            if p as usize == self.tail {
-                break
-            }
+        // Find all elements from start.
+        let mut current = Vec::with_capacity(self.n_element);
+        for _ in 0..self.n_element {
             current.push(self.alpha[p as usize]);
+            p = self.next[p as usize];
+
         }
 
         write!(f, "current buffer sorted: [")?;
@@ -116,10 +115,20 @@ impl<'a, T: Copy + IsFloat + PartialOrd + Debug> Block<'a, T> {
         self.prev[self.tail] = p as u32;
     }
 
+    fn delete_link(&mut self, i: usize) {
+        self.next[self.prev[i] as usize] = self.next[i];
+        self.prev[self.next[i] as usize] = self.prev[i];
+    }
+
+
+    fn undelete_link(&mut self, i: usize) {
+        self.next[self.prev[i] as usize] = i as u32;
+        self.prev[self.next[i] as usize] = i as u32;
+    }
+
     fn unwind(&mut self) {
         for i in (0..self.k - 1).rev() {
-            self.next[self.prev[i] as usize] = self.next[i];
-            self.prev[self.next[i] as usize] = self.prev[i];
+            self.delete_link(i)
         }
         self.m = self.tail;
         self.n_element = 0;
@@ -128,6 +137,7 @@ impl<'a, T: Copy + IsFloat + PartialOrd + Debug> Block<'a, T> {
     fn set_median(&mut self) {
         // median index position
         let new_index = self.n_element / 2;
+        dbg!(new_index);
         self.traverse_to_index(new_index)
     }
 
@@ -164,8 +174,7 @@ impl<'a, T: Copy + IsFloat + PartialOrd + Debug> Block<'a, T> {
         let current = self.get_pair(self.m);
 
         // delete from links
-        self.next[self.prev[i] as usize] = self.next[i];
-        self.prev[self.next[i] as usize] = self.prev[i];
+        self.delete_link(i);
 
         self.n_element -= 1;
 
@@ -191,12 +200,17 @@ impl<'a, T: Copy + IsFloat + PartialOrd + Debug> Block<'a, T> {
     }
 
     fn undelete(&mut self, i: usize) {
+        // undelete from links
+        self.undelete_link(i);
+
+        if self.at_end() {
+            self.m = i;
+            self.n_element = 1;
+            self.current_index = 0;
+            return
+        }
         let added = self.get_pair(i);
         let current = self.get_pair(self.m);
-
-        // undelete from links
-        self.next[self.prev[i] as usize] = i as IdxSize;
-        self.prev[self.next[i] as usize] = i as IdxSize;
 
         self.n_element += 1;
 
@@ -219,6 +233,11 @@ impl<'a, T: Copy + IsFloat + PartialOrd + Debug> Block<'a, T> {
                 self.m = self.prev[self.m as usize] as usize;
             }
         };
+    }
+
+    fn undelete_set_median(&mut self, i: usize) {
+        self.undelete(i);
+        self.set_median()
     }
 
     fn advance(&mut self) {
@@ -248,6 +267,69 @@ impl<'a, T: Copy + IsFloat + PartialOrd + Debug> Block<'a, T> {
 
 }
 
+fn rolling_median<T>(k: usize, slice: &[T])
+where T: Copy + IsFloat + PartialOrd + Debug
+{
+    let mut scratch_a = vec![];
+    let mut prev_a = vec![];
+    let mut next_a = vec![];
+
+    let mut scratch_b = vec![];
+    let mut prev_b = vec![];
+    let mut next_b = vec![];
+
+    let k = std::cmp::min(k, slice.len());
+    let alpha = &slice[..k];
+
+    // let mut out = Vec::with_capacity(slice.len());
+    let mut block_a = Block::new(alpha, &mut scratch_a, &mut prev_a, &mut next_a);
+    let mut block_b = Block::new(&alpha[..1], &mut scratch_b, &mut prev_b, &mut next_b);
+
+    let n_blocks = slice.len() / k;
+
+    block_a.unwind();
+
+
+    // let mut block_b = Block::new(h, alpha, &mut scratch_b, &mut prev_b, &mut next_b);
+    // out.push(block_b.peek());
+    //
+    // for j in 1..b {
+    //     block_a = block_b;
+    //
+    //     let alpha = &slice[j * k..(j + 1) *k];
+    //     block_b = if j % 2 == 0 {
+    //         Block::new(h, alpha, &mut scratch_b, &mut prev_b, &mut next_b)
+    //     } else {
+    //         Block::new(h, alpha, &mut scratch_a, &mut prev_a, &mut next_a)
+    //     };
+    //
+    //     block_b.unwind();
+    //     debug_assert_eq!(block_a.counter, h);
+    //     debug_assert_eq!(block_b.counter, h);
+    //
+    //     for i in 0..k {
+    //         block_a.delete(i);
+    //         block_b.undelete(i);
+    //         debug_assert!(block_a.counter + block_b.counter <= h);
+    //
+    //         if block_a.counter + block_b.counter <= h {
+    //             if block_a.peek() <= block_b.peek() {
+    //                 block_a.advance()
+    //             } else {
+    //                 block_b.advance()
+    //             }
+    //         }
+    //         debug_assert_eq!(block_a.counter + block_b.counter, h)
+    //         out.push(std::cmp::min(block_a.peek(), block_b.peek()));
+    //     }
+    //     debug_assert_eq!(block_a.counter, 0);
+    //     debug_assert_eq!(block_b.counter, h);
+    // }
+    // dbg!(out);
+
+}
+
+
 mod test {
     use super::*;
 
@@ -259,16 +341,49 @@ mod test {
         let mut prev = vec![];
         let mut next = vec![];
         let mut b = Block::new(&values, &mut scratch, &mut prev, &mut next);
-        b.delete(1);
-        b.delete(0);
-        b.delete(5);
-        b.delete(4);
-        b.undelete(4);
-        b.undelete(5);
-        b.undelete(0);
 
-        b.set_median();
+        // Unwind to get temporal window
+        b.unwind();
+
+        // Insert window in the right order
+        b.undelete_set_median(0);
+        // [[2]]
+        assert_eq!(b.peek(), Some(2));
+        b.undelete_set_median(1);
+        // [2, [8]]
+        assert_eq!(b.peek(), Some(8));
+        b.undelete_set_median(2);
+        // [2, [5], 8]
+        assert_eq!(b.peek(), Some(5));
+        b.undelete_set_median(3);
+        // [2, 5, [8], 9]
+        assert_eq!(b.peek(), Some(8));
+        b.undelete_set_median(4);
+        // [1, 2, [5], 8, 9]
+        assert_eq!(b.peek(), Some(5));
+        b.undelete_set_median(5);
+        // [1, 2, 3, [5], 8, 9]
+        assert_eq!(b.peek(), Some(5));
+        b.undelete_set_median(6);
+        // [1, 2, 3, [4], 5, 8, 9]
+        assert_eq!(b.peek(), Some(4));
+        b.undelete_set_median(7);
+        // [1, 2, 3, 4, [5], 8, 9, 10]
+        assert_eq!(b.peek(), Some(5));
+
+        // Now we will delete as the block` will leave the window.
+        // TODO! write
         dbg!(b);
+
+    }
+
+    #[test]
+    fn test_median() {
+        let values = [2, 8, 5, 9, 1, 3, 4, 10];
+        let k = 3;
+
+        rolling_median(k, &values);
+
 
     }
 }
