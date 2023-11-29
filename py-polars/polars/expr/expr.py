efe78023c696bf3a5307fa644ac763178db34904
Paddy Mullen <paddy@paddymullen.com>
2023-11-29 14:17:31 -0500

    chore: fixed typing

--- py-polars/polars/expr/expr.py
+++ py-polars/polars/expr/expr.py
@@ -132,7 +132,7 @@ class Expr:
     def _repr_html_(self) -> str:
         return self._pyexpr.to_str()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         if len(expr_str := self._pyexpr.to_str()) > 30:
             expr_str = f"{expr_str[:30]}…"
         return f"<{self.__class__.__name__} [{expr_str!r}] at {hex(id(self))}>"
