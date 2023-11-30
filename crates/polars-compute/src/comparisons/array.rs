733c634f679c1110638a9a01fbfaf545b9ccf93e
Orson Peters <orsonpeters@gmail.com>
2023-11-30 12:21:26 +0100

    add missing comment

--- crates/polars-compute/src/comparisons/array.rs
+++ crates/polars-compute/src/comparisons/array.rs
@@ -5,6 +5,10 @@ use arrow::datatypes::ArrowDataType;
 
 use crate::comparisons::TotalOrdKernel;
 
+/// Condenses a bitmap of n * width elements into one with n elements.
+/// 
+/// For each block of width bits a zero count is done. The block of bits is then
+/// replaced with a single bit: the result of true_zero_count(zero_count).
 fn agg_array_bitmap<F>(bm: Bitmap, width: usize, true_zero_count: F) -> Bitmap
 where
     F: Fn(usize) -> bool,
