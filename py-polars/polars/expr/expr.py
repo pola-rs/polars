89a63904d3b72ec17c23a47f8b56857af4fc048e
Paddy Mullen <paddy@paddymullen.com>
2023-11-30 01:49:40 -0500

    feat(python): add `__repr__` implementation for `Expr` (#12770)
    Co-authored-by: Alexander Beedie <alexander-beedie@users.noreply.github.com>

--- py-polars/polars/expr/expr.py
+++ py-polars/polars/expr/expr.py
@@ -132,6 +132,11 @@ class Expr:
     def _repr_html_(self) -> str:
         return self._pyexpr.to_str()
 
+    def __repr__(self) -> str:
+        if len(expr_str := self._pyexpr.to_str()) > 30:
+            expr_str = f"{expr_str[:30]}…"
+        return f"<{self.__class__.__name__} [{expr_str!r}] at 0x{id(self):X}>"
+
     def __str__(self) -> str:
         return self._pyexpr.to_str()
 
