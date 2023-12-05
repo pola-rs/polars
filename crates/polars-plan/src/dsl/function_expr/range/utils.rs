9054b5ef7e0f65562a8aad02e43206fb88cf03b0
Stijn de Gooijer <stijndegooijer@gmail.com>
2023-12-05 12:10:49 +0100

    WIP

--- crates/polars-plan/src/dsl/function_expr/range/utils.rs
+++ crates/polars-plan/src/dsl/function_expr/range/utils.rs
@@ -1,10 +1,15 @@
+// use std::iter::zip;
+
+// use polars_core::chunked_array::builder::ListPrimitiveChunkedBuilder;
+// use polars_core::prelude::{
+//     polars_bail, polars_ensure, ChunkedArray, PolarsIterator, PolarsResult, *,
+// };
 use polars_core::prelude::{polars_bail, polars_ensure, PolarsResult};
 use polars_core::series::Series;
 
 pub(super) fn temporal_series_to_i64_scalar(s: &Series) -> Option<i64> {
     s.to_physical_repr().get(0).unwrap().extract::<i64>()
 }
-
 pub(super) fn ensure_range_bounds_contain_exactly_one_value(
     start: &Series,
     end: &Series,
@@ -43,3 +48,80 @@ pub(super) fn broadcast_scalar_inputs(
         },
     }
 }
+
+// pub(super) fn broadcast_scalar_inputs_iter<T>(
+//     start: &ChunkedArray<T>,
+//     end: &ChunkedArray<T>,
+//     builder: ListPrimitiveChunkedBuilder<T>,
+//     range_impl: dyn Fn(T::Native, T::Native) -> PolarsResult<TimeChunked>,
+// ) -> PolarsResult<Series>
+// where
+//     T: PolarsNumericType,
+// {
+//     match (start.len(), end.len()) {
+//         (len_start, len_end) if len_start == len_end => {
+//             for (start, end) in zip(start, end) {
+//                 match (start, end) {
+//                     (Some(start), Some(end)) => {
+//                         let rng = range_impl(start, end)?;
+//                         builder.append_slice(rng.cont_slice().unwrap())
+//                     },
+//                     _ => builder.append_null(),
+//                 }
+//             }
+//         },
+//         (1, len_end) => {
+//             let start_scalar = unsafe { start.get_unchecked(0) };
+//             match start_scalar {
+//                 Some(start) => {
+//                     let range_impl = |end| range_impl(start, end);
+//                     for end_scalar in end {
+//                         match end_scalar {
+//                             Some(end) => {
+//                                 let rng = range_impl(end)?;
+//                                 builder.append_slice(rng.cont_slice().unwrap())
+//                             },
+//                             None => builder.append_null(),
+//                         }
+//                     }
+//                 },
+//                 None => {
+//                     for _ in 0..len_end {
+//                         builder.append_null()
+//                     }
+//                 },
+//             }
+//         },
+//         (len_start, 1) => {
+//             let end_scalar = unsafe { end.get_unchecked(0) };
+//             match end_scalar {
+//                 Some(end) => {
+//                     let range_impl = |start| range_impl(start, end);
+//                     for start_scalar in start {
+//                         match start_scalar {
+//                             Some(start) => {
+//                                 let rng = range_impl(start)?;
+//                                 builder.append_slice(rng.cont_slice().unwrap())
+//                             },
+//                             None => builder.append_null(),
+//                         }
+//                     }
+//                 },
+//                 None => {
+//                     for _ in 0..len_start {
+//                         builder.append_null()
+//                     }
+//                 },
+//             }
+//         },
+//         (len_start, len_end) => {
+//             polars_bail!(
+//                 ComputeError:
+//                 "lengths of `start` ({}) and `end` ({}) do not match",
+//                 len_start, len_end
+//             )
+//         },
+//     };
+//     let out = builder.finish().into_series();
+//     Ok(out)
+// }
