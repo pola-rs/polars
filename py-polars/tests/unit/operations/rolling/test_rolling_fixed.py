67d7cba293bf0504fac9de5403c0ce66e402d7c8
ritchie <ritchie46@gmail.com>
2023-12-06 09:24:45 +0100

    fix: patch rolling_var/rolling_std numerical stability

---
+++ py-polars/tests/unit/operations/rolling/test_rolling_fixed.py
@@ -0,0 +1,6 @@
+import polars as pl
+
+
+def test_rolling_var_stability_12905() -> None:
+    s1 = pl.Series("a", [36743.6 for _ in range(10)])
+    assert s1.rolling_var(window_size=12, min_periods=2).sum() == 0.0
