3722fd03d43adb81593505ba4578fc8e514ca405
alexander-beedie <alexander.m.beedie@icloud.com>
2023-12-10 23:20:31 +0400

    update "align_frames" for new outer join behaviour

--- py-polars/polars/functions/eager.py
+++ py-polars/polars/functions/eager.py
@@ -3,7 +3,14 @@ from __future__ import annotations
 import contextlib
 from functools import reduce
 from itertools import chain
-from typing import TYPE_CHECKING, Iterable, List, Sequence, cast, get_args
+from typing import (
+    TYPE_CHECKING,
+    Iterable,
+    List,
+    Sequence,
+    cast,
+    get_args,
+)
 
 import polars._reexport as pl
 from polars import functions as F
@@ -250,6 +257,45 @@ def concat(
     return out
 
 
+def _alignment_join(
+    *frames: FrameType,
+    align_on: list[str],
+    how: JoinStrategy = "outer",
+    descending: bool | Sequence[bool] = False,
+) -> LazyFrame:
+    """Creates a single master frame with all rows aligned on the common key values."""
+    # note: can stackoverflow if the join becomes too large, so
+    # we branch when hitting a large enough number of frames
+    join_lazy = len(frames) < 250
+
+    def join_func(x: FrameType, y: FrameType) -> FrameType:
+        if how != "outer":
+            # join key cols are merged/coalesced
+            return (x.lazy() if join_lazy else x).join(  # type: ignore[attr-defined]
+                y.lazy(), how=how, on=align_on, suffix=str(id(y))
+            )
+        else:
+            return (
+                (x.lazy() if join_lazy else x)
+                # type: ignore[attr-defined]
+                .join(y.lazy(), how=how, on=align_on, suffix=str(id(y)))
+                .with_columns(
+                    F.when(F.col(c).is_null())
+                    .then(F.col(f"{c}{id(y)!s}"))
+                    .otherwise(F.col(c))
+                    .alias(c)
+                    for c in align_on
+                )
+                .select(F.exclude(*(f"{c}{id(y)!s}" for c in align_on)))
+            )
+
+    df_joined = reduce(join_func, frames).sort(by=align_on, descending=descending)
+    if join_lazy:
+        return df_joined.collect(no_optimization=True).lazy()  # type: ignore[attr-defined]
+    else:
+        return df_joined.lazy()
+
+
 def align_frames(
     *frames: FrameType,
     on: str | Expr | Sequence[str] | Sequence[Expr] | Sequence[str | Expr],
@@ -403,35 +449,12 @@ def align_frames(
 
     # create aligned master frame (this is the most expensive part; afterwards
     # we just subselect out the columns representing the component frames)
-    eager = isinstance(frames[0], pl.DataFrame)
-
-    # we stackoverflow on many frames
-    # so we branch on an arbitrary chosen large number of frames
-    if len(frames) < 250:
-        # lazy variant
-        # this can SO
-        alignment_frame: LazyFrame = (
-            reduce(  # type: ignore[attr-defined]
-                lambda x, y: x.lazy().join(  # type: ignore[arg-type, return-value]
-                    y.lazy(), how=how, on=align_on, suffix=str(id(y))
-                ),
-                frames,
-            )
-            .sort(by=align_on, descending=descending)
-            .collect(no_optimization=True)
-            .lazy()
-        )
-    else:
-        # eager variant
-        # this doesn't SO
-        alignment_frame = (
-            reduce(
-                lambda x, y: x.join(y, how=how, on=align_on, suffix=str(id(y))),
-                frames,
-            )
-            .sort(by=align_on, descending=descending)
-            .lazy()
-        )
+    alignment_frame = _alignment_join(
+        *frames,
+        align_on=align_on,
+        how=how,
+        descending=descending,
+    )
 
     # select-out aligned components from the master frame
     aligned_cols = set(alignment_frame.columns)
@@ -447,6 +470,7 @@ def align_frames(
             f = f.select(select)
         aligned_frames.append(f)
 
+    eager = isinstance(frames[0], pl.DataFrame)
     return cast(
         List[FrameType], F.collect_all(aligned_frames) if eager else aligned_frames
     )
