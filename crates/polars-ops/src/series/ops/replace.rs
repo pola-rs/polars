a9951192ed12258ede27375062804484036d6bba
Stijn de Gooijer <stijndegooijer@gmail.com>
2023-12-12 14:00:16 +0100

    Fix replacing with null values

--- crates/polars-ops/src/series/ops/replace.rs
+++ crates/polars-ops/src/series/ops/replace.rs
@@ -1,22 +1,22 @@
+use polars_core::prelude::arity::binary_elementwise;
 use polars_core::prelude::*;
 use polars_core::utils::try_get_supertype;
 use polars_error::{polars_bail, polars_ensure, PolarsResult};
 
 use crate::frame::join::*;
 use crate::prelude::*;
-use crate::series::ops::coalesce_series;
+use crate::series::is_in;
 
 pub fn replace(s: &Series, old: &Series, new: &Series) -> PolarsResult<Series> {
-    let output_dtype = try_get_supertype(s.dtype(), new.dtype())?;
-
     if old.len() == 0 {
+        let output_dtype = try_get_supertype(s.dtype(), new.dtype())?;
         return s.cast(&output_dtype);
     }
 
     // TODO: Add fast path for replacing a single value (ZIP WITH?)
-    let replaced = join_replacer(s, old, new)?;
+    let replaced = get_replaced(s, old, new)?;
 
-    coalesce_series(&[replaced, s.cast(&output_dtype)?])
+    coalesce_replaced(&replaced, s, s, old, new)
 }
 
 pub fn replace_with_default(
@@ -26,6 +26,11 @@ pub fn replace_with_default(
     default: &Series,
 ) -> PolarsResult<Series> {
     let output_dtype = try_get_supertype(new.dtype(), default.dtype())?;
+
+    println!("{:?}", s.dtype());
+    println!("{:?}", old.dtype());
+    println!("{:?}", new.dtype());
+    println!("{:?}", output_dtype);
 
     let default = match default.len() {
         len if len == s.len() => default.cast(&output_dtype)?,
@@ -43,14 +48,13 @@ pub fn replace_with_default(
     }
 
     // TODO: Add fast path for replacing a single value
-    let replaced = join_replacer(s, old, new)?;
+    let replaced = get_replaced(s, old, new)?;
 
-    coalesce_series(&[replaced, default])
+    coalesce_replaced(&replaced, &default, s, old, new)
 }
 
-/// Create a Series containing only the replaced values at the right indices
-/// and nulls everywhere else
-fn join_replacer(s: &Series, old: &Series, new: &Series) -> PolarsResult<Series> {
+/// Create a Series containing only the replaced values and nulls everywhere else.
+fn get_replaced(s: &Series, old: &Series, new: &Series) -> PolarsResult<Series> {
     // length 1 is many-to-one replace, otherwise it's one-to-one
     polars_ensure!(
         (new.len() == old.len()) || new.len() == 1,
@@ -61,6 +65,8 @@ fn join_replacer(s: &Series, old: &Series, new: &Series) -> PolarsResult<Series>
 
     let df = DataFrame::new_no_checks(vec![s.cast(&join_dtype)?]);
 
+    println!("{:?}", df);
+
     let mut old = old.cast(&join_dtype)?;
     old.rename("__POLARS_REPLACE_OLD");
     let mut new = match new.len() {
@@ -69,6 +75,8 @@ fn join_replacer(s: &Series, old: &Series, new: &Series) -> PolarsResult<Series>
     };
     new.rename("__POLARS_REPLACE_NEW");
     let replacer = DataFrame::new_no_checks(vec![old, new]);
+
+    println!("{:?}", replacer);
 
     let df_joined = df.join(
         &replacer,
@@ -81,6 +89,53 @@ fn join_replacer(s: &Series, old: &Series, new: &Series) -> PolarsResult<Series>
         },
     )?;
 
+    println!("{:?}", df_joined);
+
     let s_joined = df_joined.column("__POLARS_REPLACE_NEW").unwrap();
     Ok(s_joined.clone())
+}
+
+/// Coalesce the replaced values with another column to get the final result.
+fn coalesce_replaced(
+    replaced: &Series,
+    other: &Series,
+    s: &Series,
+    old: &Series,
+    new: &Series,
+) -> PolarsResult<Series> {
+    let mask = match new.null_count() {
+        0 => replaced.is_not_null(),
+        _ => {
+            //  If we replace some values by null, we cannot do a regular coalesce
+            let is_not_null = replaced.is_not_null();
+
+            println!("{:?}", is_not_null);
+
+            let null_keys = determine_null_keys(old, new).unwrap();
+            let mapped_to_null = is_in(s, &null_keys)?;
+
+            println!("{:?}", mapped_to_null);
+
+            let opt_or = |l: Option<bool>, r: Option<bool>| {
+                let l = l.unwrap();
+                match r {
+                    Some(r) => l | r,
+                    None => false,
+                }
+            };
+            binary_elementwise(&is_not_null, &mapped_to_null, opt_or)
+        },
+    };
+
+    println!("{:?}", replaced);
+    println!("{:?}", other);
+    println!("{:?}", mask);
+
+    let out = replaced.zip_with(&mask, other)?;
+    Ok(out)
+}
+
+/// Determine the keys that map to null values
+fn determine_null_keys(old: &Series, new: &Series) -> PolarsResult<Series> {
+    old.filter(&new.is_null())
 }
