39bd63f26ea1566b0d9cf13cf9e8262ef9dd7d8f
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-12-14 09:02:59 +0000

    reorder tests

--- py-polars/tests/unit/operations/map/test_inefficient_map_warning.py
+++ py-polars/tests/unit/operations/map/test_inefficient_map_warning.py
@@ -62,17 +62,20 @@ TEST_CASES = [
         'pl.col("a").is_in((1, 2, 3, 4, 3)) & ((pl.col("a") % 2) == 0) & (pl.col("a") > 0)',
     ),
     ("a", "lambda x: MY_CONSTANT + x", 'MY_CONSTANT + pl.col("a")'),
+    (
+        "a",
+        "lambda x: (float(x) * int(x)) // 2",
+        '(pl.col("a").cast(pl.Float64) * pl.col("a").cast(pl.Int64)) // 2',
+    ),
+    # ---------------------------------------------
+    # numpy
+    # ---------------------------------------------
     ("a", "lambda x: 0 + numpy.cbrt(x)", '0 + pl.col("a").cbrt()'),
     ("a", "lambda x: np.sin(x) + 1", 'pl.col("a").sin() + 1'),
     (
         "a",  # note: functions operate on consts
         "lambda x: np.sin(3.14159265358979) + (x - 1) + abs(-3)",
         '(np.sin(3.14159265358979) + (pl.col("a") - 1)) + abs(-3)',
-    ),
-    (
-        "a",
-        "lambda x: (float(x) * int(x)) // 2",
-        '(pl.col("a").cast(pl.Float64) * pl.col("a").cast(pl.Int64)) // 2',
     ),
     # ---------------------------------------------
     # logical 'and/or' (validate nesting levels)
