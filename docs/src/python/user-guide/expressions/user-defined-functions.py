356bd52e206ceb29f5cde585037d1ee3211d31a6
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-12-16 11:28:40 +0000

    docs: update user-defined-functions for 0.19.x

--- docs/src/python/user-guide/expressions/user-defined-functions.py
+++ docs/src/python/user-guide/expressions/user-defined-functions.py
@@ -11,22 +11,25 @@ df = pl.DataFrame(
         "values": [10, 7, 1],
     }
 )
-
-out = df.group_by("keys", maintain_order=True).agg(
-    pl.col("values").map_batches(lambda s: s.shift()).alias("shift_map"),
-    pl.col("values").shift().alias("shift_expression"),
-)
 print(df)
 # --8<-- [end:dataframe]
 
-
-# --8<-- [start:apply]
+# --8<-- [start:shift_map_batches]
 out = df.group_by("keys", maintain_order=True).agg(
-    pl.col("values").map_elements(lambda s: s.shift()).alias("shift_map"),
+    pl.col("values").map_batches(lambda s: s.shift()).alias("shift_map_batches"),
     pl.col("values").shift().alias("shift_expression"),
 )
 print(out)
-# --8<-- [end:apply]
+# --8<-- [end:shift_map_batches]
+
+
+# --8<-- [start:map_elements]
+out = df.group_by("keys", maintain_order=True).agg(
+    pl.col("values").map_elements(lambda s: s.shift()).alias("shift_map_elements"),
+    pl.col("values").shift().alias("shift_expression"),
+)
+print(out)
+# --8<-- [end:map_elements]
 
 # --8<-- [start:counter]
 counter = 0
@@ -39,7 +42,7 @@ def add_counter(val: int) -> int:
 
 
 out = df.select(
-    pl.col("values").map_elements(add_counter).alias("solution_apply"),
+    pl.col("values").map_elements(add_counter).alias("solution_map_elements"),
     (pl.col("values") + pl.int_range(1, pl.count() + 1)).alias("solution_expr"),
 )
 print(out)
@@ -49,7 +52,7 @@ print(out)
 out = df.select(
     pl.struct(["keys", "values"])
     .map_elements(lambda x: len(x["keys"]) + x["values"])
-    .alias("solution_apply"),
+    .alias("solution_map_elements"),
     (pl.col("keys").str.len_bytes() + pl.col("values")).alias("solution_expr"),
 )
 print(out)
