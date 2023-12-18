fcc80b223236109a1d8e673f15735e4af86f526c
Ritchie Vink <ritchie46@gmail.com>
2023-12-18 10:42:47 +0100

    chore(rust): don't compile polars-ops by default (#13100)

--- crates/polars/src/prelude.rs
+++ crates/polars/src/prelude.rs
@@ -1,12 +1,12 @@
 #[cfg(feature = "polars-algo")]
 pub use polars_algo::prelude::*;
-pub use polars_core::frame::group_by::*;
 pub use polars_core::prelude::*;
 pub use polars_core::utils::NoNull;
 #[cfg(feature = "polars-io")]
 pub use polars_io::prelude::*;
 #[cfg(feature = "lazy")]
 pub use polars_lazy::prelude::*;
+#[cfg(feature = "polars-ops")]
 pub use polars_ops::prelude::*;
 #[cfg(feature = "temporal")]
 pub use polars_time::prelude::*;
