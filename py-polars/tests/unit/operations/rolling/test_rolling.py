126a56d0bd2db77e0c554464f22949785e5e6188
ritchie <ritchie46@gmail.com>
2023-12-18 07:19:17 +0100

    fix test

--- py-polars/tests/unit/operations/rolling/test_rolling.py
+++ py-polars/tests/unit/operations/rolling/test_rolling.py
@@ -838,11 +838,10 @@ def test_rolling_median_2() -> None:
     np.random.seed(12)
     n = 1000
     df = pl.DataFrame({"x": np.random.normal(0, 1, n)})
-    assert (
-        df.select(pl.col("x").rolling_median(window_size=10).sum()).item()
-        == 5.139429061527812
-    )
-    assert (
-        df.select(pl.col("x").rolling_median(window_size=100).sum()).item()
-        == 26.60506093611384
-    )
+    # this can differ because simd sizes and non-associativity of floats.
+    assert df.select(
+        pl.col("x").rolling_median(window_size=10).sum()
+    ).item() == pytest.approx(5.139429061527812)
+    assert df.select(
+        pl.col("x").rolling_median(window_size=100).sum()
+    ).item() == pytest.approx(26.60506093611384)
