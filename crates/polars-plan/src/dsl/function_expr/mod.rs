03d51a98a07d3fc60a06cd13c0be0c5d7f443c50
Weijie Guo <reswqa@163.com>
2023-12-20 17:44:11 +0800

    fix: Use the name of the leftmost expression in horizontal operations (#13143)

--- crates/polars-plan/src/dsl/function_expr/mod.rs
+++ crates/polars-plan/src/dsl/function_expr/mod.rs
@@ -1027,7 +1027,7 @@ impl From<FunctionExpr> for SpecialEq<Arc<dyn SeriesUdf>> {
             },
             BackwardFill { limit } => map!(dispatch::backward_fill, limit),
             ForwardFill { limit } => map!(dispatch::forward_fill, limit),
-            SumHorizontal => map_as_slice!(dispatch::sum_horizontal),
+            SumHorizontal => wrap!(dispatch::sum_horizontal),
             MaxHorizontal => wrap!(dispatch::max_horizontal),
             MinHorizontal => wrap!(dispatch::min_horizontal),
             #[cfg(feature = "ewma")]
