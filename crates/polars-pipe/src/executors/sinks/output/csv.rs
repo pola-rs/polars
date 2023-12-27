36042368fee4d1a955a55757b8bb0865f5190a3a
Ritchie Vink <ritchie46@gmail.com>
2023-12-27 09:50:33 +0100

    refactor(rust): split-up file_sink module (#13256)

---
+++ crates/polars-pipe/src/executors/sinks/output/csv.rs
@@ -0,0 +1,61 @@
+use std::path::Path;
+
+use crossbeam_channel::bounded;
+use polars_core::prelude::*;
+use polars_io::csv::CsvWriter;
+use polars_io::SerWriter;
+use polars_plan::prelude::CsvWriterOptions;
+
+use crate::executors::sinks::output::file_sink::{init_writer_thread, FilesSink, SinkWriter};
+use crate::pipeline::morsels_per_sink;
+
+pub struct CsvSink {}
+impl CsvSink {
+    #[allow(clippy::new_ret_no_self)]
+    pub fn new(path: &Path, options: CsvWriterOptions, schema: &Schema) -> PolarsResult<FilesSink> {
+        let file = std::fs::File::create(path)?;
+        let writer = CsvWriter::new(file)
+            .include_bom(options.include_bom)
+            .include_header(options.include_header)
+            .with_separator(options.serialize_options.separator)
+            .with_line_terminator(options.serialize_options.line_terminator)
+            .with_quote_char(options.serialize_options.quote_char)
+            .with_batch_size(options.batch_size)
+            .with_datetime_format(options.serialize_options.datetime_format)
+            .with_date_format(options.serialize_options.date_format)
+            .with_time_format(options.serialize_options.time_format)
+            .with_float_precision(options.serialize_options.float_precision)
+            .with_null_value(options.serialize_options.null)
+            .with_quote_style(options.serialize_options.quote_style)
+            .n_threads(1)
+            .batched(schema)?;
+
+        let writer = Box::new(writer) as Box<dyn SinkWriter + Send + Sync>;
+
+        let morsels_per_sink = morsels_per_sink();
+        let backpressure = morsels_per_sink * 2;
+        let (sender, receiver) = bounded(backpressure);
+
+        let io_thread_handle = Arc::new(Some(init_writer_thread(
+            receiver,
+            writer,
+            options.maintain_order,
+            morsels_per_sink,
+        )));
+
+        Ok(FilesSink {
+            sender,
+            io_thread_handle,
+        })
+    }
+}
+
+impl SinkWriter for polars_io::csv::BatchedWriter<std::fs::File> {
+    fn _write_batch(&mut self, df: &DataFrame) -> PolarsResult<()> {
+        self.write_batch(df)
+    }
+
+    fn _finish(&mut self) -> PolarsResult<()> {
+        Ok(())
+    }
+}
---
+++ crates/polars-pipe/src/executors/sinks/output/ipc.rs
@@ -0,0 +1,87 @@
+use std::path::Path;
+use std::sync::Arc;
+
+use crossbeam_channel::bounded;
+use polars_core::prelude::*;
+use polars_io::prelude::*;
+use polars_plan::prelude::IpcWriterOptions;
+
+use crate::executors::sinks::output::file_sink::{init_writer_thread, FilesSink, SinkWriter};
+use crate::pipeline::morsels_per_sink;
+
+pub struct IpcSink {}
+impl IpcSink {
+    #[allow(clippy::new_ret_no_self)]
+    pub fn new(path: &Path, options: IpcWriterOptions, schema: &Schema) -> PolarsResult<FilesSink> {
+        let file = std::fs::File::create(path)?;
+        let writer = IpcWriter::new(file)
+            .with_compression(options.compression)
+            .batched(schema)?;
+
+        let writer = Box::new(writer) as Box<dyn SinkWriter + Send>;
+
+        let morsels_per_sink = morsels_per_sink();
+        let backpressure = morsels_per_sink * 2;
+        let (sender, receiver) = bounded(backpressure);
+
+        let io_thread_handle = Arc::new(Some(init_writer_thread(
+            receiver,
+            writer,
+            options.maintain_order,
+            morsels_per_sink,
+        )));
+
+        Ok(FilesSink {
+            sender,
+            io_thread_handle,
+        })
+    }
+}
+
+#[cfg(feature = "cloud")]
+pub struct IpcCloudSink {}
+#[cfg(feature = "cloud")]
+impl IpcCloudSink {
+    #[allow(clippy::new_ret_no_self)]
+    #[tokio::main(flavor = "current_thread")]
+    pub async fn new(
+        uri: &str,
+        cloud_options: Option<&polars_io::cloud::CloudOptions>,
+        ipc_options: IpcWriterOptions,
+        schema: &Schema,
+    ) -> PolarsResult<FilesSink> {
+        let cloud_writer = polars_io::cloud::CloudWriter::new(uri, cloud_options).await?;
+        let writer = IpcWriter::new(cloud_writer)
+            .with_compression(ipc_options.compression)
+            .batched(schema)?;
+
+        let writer = Box::new(writer) as Box<dyn SinkWriter + Send>;
+
+        let morsels_per_sink = morsels_per_sink();
+        let backpressure = morsels_per_sink * 2;
+        let (sender, receiver) = bounded(backpressure);
+
+        let io_thread_handle = Arc::new(Some(init_writer_thread(
+            receiver,
+            writer,
+            ipc_options.maintain_order,
+            morsels_per_sink,
+        )));
+
+        Ok(FilesSink {
+            sender,
+            io_thread_handle,
+        })
+    }
+}
+
+impl<W: std::io::Write> SinkWriter for polars_io::ipc::BatchedWriter<W> {
+    fn _write_batch(&mut self, df: &DataFrame) -> PolarsResult<()> {
+        self.write_batch(df)
+    }
+
+    fn _finish(&mut self) -> PolarsResult<()> {
+        self.finish()?;
+        Ok(())
+    }
+}
