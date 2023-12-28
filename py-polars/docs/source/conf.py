224e5cffcde2c4668e1812fd0f7e04f6faf3f88e
chielP <chielpeters@live.nl>
2023-12-28 08:48:20 +0100

    change base url

--- py-polars/docs/source/conf.py
+++ py-polars/docs/source/conf.py
@@ -104,7 +104,7 @@ html_show_sourcelink = False
 # key site root paths
 static_assets_root = "https://raw.githubusercontent.com/pola-rs/polars-static/master"
 github_root = "https://github.com/pola-rs/polars"
-web_root = "https://pola-rs.github.io"
+web_root = "https://docs.pola.rs"
 
 # Specify version for version switcher dropdown menu
 git_ref = os.environ.get("POLARS_VERSION", "main")
