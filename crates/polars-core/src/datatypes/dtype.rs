1a520183f91eb5213aede45d38b096bbdb8cc6bd
Alexander Beedie <alexander-beedie@users.noreply.github.com>
2023-12-31 11:52:47 +0400

    Update crates/polars-core/src/datatypes/dtype.rs
    Co-authored-by: Stijn de Gooijer <stijn@degooijer.io>

--- crates/polars-core/src/datatypes/dtype.rs
+++ crates/polars-core/src/datatypes/dtype.rs
@@ -387,16 +387,9 @@ impl Display for DataType {
             #[cfg(feature = "object")]
             DataType::Object(s, _) => s,
             #[cfg(feature = "dtype-categorical")]
-            DataType::Categorical(rev_map, _) => {
-                if let Some(rev_map) = rev_map {
-                    if rev_map.is_enum() {
-                        "enum"
-                    } else {
-                        "cat"
-                    }
-                } else {
-                    "cat"
-                }
+            DataType::Categorical(rev_map, _) => match rev_map {
+                Some(r) if r.is_enum() => "enum",
+                _ => "cat",
             },
             #[cfg(feature = "dtype-struct")]
             DataType::Struct(fields) => return write!(f, "struct[{}]", fields.len()),
