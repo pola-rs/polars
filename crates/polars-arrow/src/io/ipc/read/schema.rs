15556d4350baa075bd0d8a8bbc02871c74119f2c
ritchie <ritchie46@gmail.com>
2024-01-06 10:20:46 +0100

    round trip

--- crates/polars-arrow/src/io/ipc/read/schema.rs
+++ crates/polars-arrow/src/io/ipc/read/schema.rs
@@ -277,6 +277,8 @@ fn get_data_type(
         LargeBinary(_) => (ArrowDataType::LargeBinary, IpcField::default()),
         Utf8(_) => (ArrowDataType::Utf8, IpcField::default()),
         LargeUtf8(_) => (ArrowDataType::LargeUtf8, IpcField::default()),
+        BinaryView(_) => (ArrowDataType::BinaryView, IpcField::default()),
+        Utf8View(_) => (ArrowDataType::Utf8View, IpcField::default()),
         FixedSizeBinary(fixed) => (
             ArrowDataType::FixedSizeBinary(
                 fixed
@@ -350,8 +352,6 @@ fn get_data_type(
         Union(union_) => deserialize_union(union_, field)?,
         Map(map) => deserialize_map(map, field)?,
         RunEndEncoded(_) => todo!(),
-        BinaryView(_) => todo!(),
-        Utf8View(_) => todo!(),
         LargeListView(_) | ListView(_) => todo!(),
     })
 }
