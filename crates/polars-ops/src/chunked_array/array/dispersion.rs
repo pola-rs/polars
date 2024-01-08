610d6b79c0df9d85b3d3a25f4c0942fb7c10828f
Ion Koutsouris <15728914+ion-elgreco@users.noreply.github.com>
2024-01-08 20:54:58 +0100

    use apply_amort

--- crates/polars-ops/src/chunked_array/array/dispersion.rs
+++ crates/polars-ops/src/chunked_array/array/dispersion.rs
@@ -4,26 +4,24 @@ use super::*;
 
 pub(super) fn median_with_nulls(ca: &ArrayChunked) -> PolarsResult<Series> {
     let mut out = match ca.inner_dtype() {
-        DataType::UInt32
-        | DataType::UInt64
-        | DataType::Int32
-        | DataType::Int64
-        | DataType::Float64 => {
-            let out: Float64Chunked = ca
-                .amortized_iter()
-                .map(|s| s.and_then(|s| s.as_ref().median()))
-                .collect();
-            out.into_series()
-        },
         DataType::Float32 => {
             let out: Float32Chunked = ca
-                .amortized_iter()
-                .map(|s| s.and_then(|s| s.as_ref().median().map(|v| v as f32)))
-                .collect();
+                .apply_amortized_generic(|s| s.and_then(|s| s.as_ref().median().map(|v| v as f32)))
+                .with_name(ca.name());
             out.into_series()
         },
+        #[cfg(feature = "dtype-duration")]
+        DataType::Duration(tu) => {
+            let out: Int64Chunked = ca
+                .apply_amortized_generic(|s| s.and_then(|s| s.as_ref().median().map(|v| v as i64)))
+                .with_name(ca.name());
+            out.into_duration(tu).into_series()
+        },
         _ => {
-            polars_bail!(ComputeError: "median on array with dtype: {} not yet supported", ca.dtype())
+            let out: Float64Chunked = ca
+                .apply_amortized_generic(|s| s.and_then(|s| s.as_ref().median()))
+                .with_name(ca.name());
+            out.into_series()
         },
     };
     out.rename(ca.name());
@@ -32,26 +30,25 @@ pub(super) fn median_with_nulls(ca: &ArrayChunked) -> PolarsResult<Series> {
 
 pub(super) fn std_with_nulls(ca: &ArrayChunked, ddof: u8) -> PolarsResult<Series> {
     let mut out = match ca.inner_dtype() {
-        DataType::UInt32
-        | DataType::UInt64
-        | DataType::Int32
-        | DataType::Int64
-        | DataType::Float64 => {
+        DataType::Float32 => {
+            let out: Float32Chunked = ca
+                .apply_amortized_generic(|s| s.and_then(|s| s.as_ref().std(ddof).map(|v| v as f32)))
+                .with_name(ca.name());
+            out.into_series()
+        },
+        #[cfg(feature = "dtype-duration")]
+        DataType::Duration(tu) => {
+            let out: Int64Chunked = ca
+                .apply_amortized_generic(|s| s.and_then(|s| s.as_ref().std(ddof).map(|v| v as i64)))
+                .with_name(ca.name());
+            out.into_duration(tu).into_series()
+        },
+        _ => {
             let out: Float64Chunked = ca
                 .amortized_iter()
                 .map(|s| s.and_then(|s| s.as_ref().std(ddof)))
                 .collect();
             out.into_series()
-        },
-        DataType::Float32 => {
-            let out: Float32Chunked = ca
-                .amortized_iter()
-                .map(|s| s.and_then(|s| s.as_ref().std(ddof).map(|v| v as f32)))
-                .collect();
-            out.into_series()
-        },
-        _ => {
-            polars_bail!(ComputeError: "median on array with dtype: {} not yet supported", ca.dtype())
         },
     };
     out.rename(ca.name());
@@ -60,26 +57,38 @@ pub(super) fn std_with_nulls(ca: &ArrayChunked, ddof: u8) -> PolarsResult<Series
 
 pub(super) fn var_with_nulls(ca: &ArrayChunked, ddof: u8) -> PolarsResult<Series> {
     let mut out = match ca.inner_dtype() {
-        DataType::UInt32
-        | DataType::UInt64
-        | DataType::Int32
-        | DataType::Int64
-        | DataType::Float64 => {
-            let out: Float64Chunked = ca
-                .amortized_iter()
-                .map(|s| s.and_then(|s| s.as_ref().var(ddof)))
-                .collect();
-            out.into_series()
-        },
         DataType::Float32 => {
             let out: Float32Chunked = ca
-                .amortized_iter()
-                .map(|s| s.and_then(|s| s.as_ref().var(ddof).map(|v| v as f32)))
-                .collect();
+                .apply_amortized_generic(|s| s.and_then(|s| s.as_ref().var(ddof).map(|v| v as f32)))
+                .with_name(ca.name());
             out.into_series()
         },
+        #[cfg(feature = "dtype-duration")]
+        DataType::Duration(TimeUnit::Milliseconds) => {
+            let out: Int64Chunked = ca
+                .apply_amortized_generic(|s| s.and_then(|s| s.as_ref().var(ddof).map(|v| v as i64)))
+                .with_name(ca.name());
+            out.into_duration(TimeUnit::Milliseconds).into_series()
+        },
+        #[cfg(feature = "dtype-duration")]
+        DataType::Duration(TimeUnit::Microseconds | TimeUnit::Nanoseconds) => {
+            let out: Int64Chunked = ca
+                .cast(&DataType::Array(
+                    Box::new(DataType::Duration(TimeUnit::Milliseconds)),
+                    ca.width(),
+                ))
+                .unwrap()
+                .array()
+                .unwrap()
+                .apply_amortized_generic(|s| s.and_then(|s| s.as_ref().var(ddof).map(|v| v as i64)))
+                .with_name(ca.name());
+            out.into_duration(TimeUnit::Milliseconds).into_series()
+        },
         _ => {
-            polars_bail!(ComputeError: "median on array with dtype: {} not yet supported", ca.dtype())
+            let out: Float64Chunked = ca
+                .apply_amortized_generic(|s| s.and_then(|s| s.as_ref().var(ddof)))
+                .with_name(ca.name());
+            out.into_series()
         },
     };
     out.rename(ca.name());
