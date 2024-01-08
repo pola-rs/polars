27383ac586f4e9d4f31ced730c0919f75793217a
Constantine Evans <constantine@costi.net>
2024-01-08 23:25:00 +0000

    simplify deserialize_decimal and improve checks

--- crates/polars-arrow/src/legacy/compute/decimal.rs
+++ crates/polars-arrow/src/legacy/compute/decimal.rs
@@ -26,7 +26,6 @@ pub fn infer_scale(bytes: &[u8]) -> Option<u8> {
 }
 
 /// Deserializes bytes to a single i128 representing a decimal
-/// The decimal precision and scale are not checked.
 #[inline]
 pub(super) fn deserialize_decimal(
     mut bytes: &[u8],
@@ -38,62 +37,34 @@ pub(super) fn deserialize_decimal(
         bytes = &bytes[1..];
     };
     let (lhs, rhs) = split_decimal_bytes(bytes);
-    let precision = precision.unwrap_or(u8::MAX);
+    let precision = precision.unwrap_or(38);
 
     let lhs_b = lhs?;
-    let abs = parse_integer_checked(lhs_b).and_then(|x| {
-        match rhs {
-            Some(rhs) => {
-                parse_integer_checked(rhs)
-                    .map(|y| (x, lhs_b, y, rhs))
-                    .and_then(|(lhs, lhs_b, rhs, rhs_b)| {
-                        let lhs_s = significant_digits(lhs_b);
-                        let leading_zeros_rhs = leading_zeros(rhs_b);
-                        let rhs_s = rhs_b.len() as u8 - leading_zeros_rhs;
+    let abs = parse_integer_checked(lhs_b).and_then(|x| match rhs {
+        Some(rhs) => parse_integer_checked(rhs)
+            .map(|y| (x, lhs_b, y, rhs))
+            .and_then(|(lhs, lhs_b, rhs, rhs_b)| {
+                let lhs_s = significant_digits(lhs_b);
 
-                        // parameters don't match bytes
-                        if lhs_s + rhs_s > precision || rhs_s > scale {
-                            None
-                        }
-                        // significant digits don't fit scale
-                        else if rhs_s < scale {
-                            // scale: 2
-                            // number: x.09
-                            // significant digits: 1
-                            // leading_zeros: 1
-                            // parsed: 9
-                            // so this is correct
-                            if leading_zeros_rhs + rhs_s == scale {
-                                Some((lhs, rhs))
-                            }
-                            // scale: 2
-                            // number: x.9
-                            // significant digits: 1
-                            // parsed: 9
-                            // so we must multiply by 10 to get 90
-                            else {
-                                let diff = scale as u32 - (rhs_s + leading_zeros_rhs) as u32;
-                                Some((lhs, rhs * 10i128.pow(diff)))
-                            }
-                        }
-                        // scale: 2
-                        // number: x.90
-                        // significant digits: 2
-                        // parsed: 90
-                        // so this is correct
-                        else {
-                            Some((lhs, rhs))
-                        }
-                    })
-                    .map(|(lhs, rhs)| lhs * 10i128.pow(scale as u32) + rhs)
-            },
-            None => {
-                if lhs_b.len() > precision as usize {
-                    return None;
+                if ((lhs_s as usize) + rhs_b.len() > (precision as usize))
+                    || (rhs_b.len() > scale as usize)
+                    || (rhs_b.first() == Some(&b'-'))
+                {
+                    None
+                } else if (rhs_b.len() as u8) == scale {
+                    Some((lhs, rhs))
+                } else {
+                    let diff = scale as u32 - rhs_b.len() as u32;
+                    Some((lhs, rhs * 10i128.pow(diff)))
                 }
-                parse_integer_checked(lhs_b).map(|lhs| lhs * 10i128.pow(scale as u32))
-            },
-        }
+            })
+            .map(|(lhs, rhs)| lhs * 10i128.pow(scale as u32) + rhs),
+        None => {
+            if (lhs_b.len() > precision as usize) || lhs_b.is_empty() {
+                return None;
+            }
+            Some(x * 10i128.pow(scale as u32))
+        },
     });
     if negative {
         Some(-abs?)
@@ -157,6 +128,9 @@ mod test {
         assert_eq!(deserialize_decimal(val.as_bytes(), precision, scale), None);
 
         let val = "12.3.ABC4";
+        assert_eq!(deserialize_decimal(val.as_bytes(), precision, scale), None);
+
+        let val = "12.-3";
         assert_eq!(deserialize_decimal(val.as_bytes(), precision, scale), None);
 
         let val = "";
