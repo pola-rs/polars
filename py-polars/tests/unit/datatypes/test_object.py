5cf8d51cbba9ca0031ed3d5e34e4a3b296ed8092
Weijie Guo <reswqa@163.com>
2024-01-09 22:55:46 +0800

    test fix 13512

--- py-polars/tests/unit/datatypes/test_object.py
+++ py-polars/tests/unit/datatypes/test_object.py
@@ -131,3 +131,25 @@ def test_object_apply_to_struct() -> None:
     s = pl.Series([0, 1, 2], dtype=pl.Object)
     out = s.map_elements(lambda x: {"a": str(x), "b": x})
     assert out.dtype == pl.Struct([pl.Field("a", pl.String), pl.Field("b", pl.Int64)])
+
+
+def test_null_obj_str_13512() -> None:
+    df1 = pl.DataFrame(
+        {
+            "key": [1],
+        }
+    )
+    df2 = pl.DataFrame({"key": [2], "a": pl.Series([1], dtype=pl.Object)})
+
+    out = df1.join(df2, on="key", how="left")
+    s = str(out)
+    assert s == (
+        "shape: (1, 2)\n"
+        "┌─────┬────────┐\n"
+        "│ key ┆ a      │\n"
+        "│ --- ┆ ---    │\n"
+        "│ i64 ┆ object │\n"
+        "╞═════╪════════╡\n"
+        "│ 1   ┆ null   │\n"
+        "└─────┴────────┘"
+    )
