55be56a0a02f24128d4940eee85168b5c05234db
Weijie Guo <reswqa@163.com>
2024-01-11 12:07:59 +0800

    fix: Empty series of AggregatedList should also have list dtype

--- crates/polars-lazy/src/physical_plan/expressions/filter.rs
+++ crates/polars-lazy/src/physical_plan/expressions/filter.rs
@@ -54,16 +54,23 @@ impl PhysicalExpr for FilterExpr {
             let preds = unsafe { ac_predicate.iter_groups(false) };
             let s = ac_s.aggregated();
             let ca = s.list()?;
-            // SAFETY: unstable series never lives longer than the iterator.
-            let out = unsafe {
-                ca.amortized_iter()
-                    .zip(preds)
-                    .map(|(opt_s, opt_pred)| match (opt_s, opt_pred) {
-                        (Some(s), Some(pred)) => s.as_ref().filter(pred.as_ref().bool()?).map(Some),
-                        _ => Ok(None),
-                    })
-                    .collect::<PolarsResult<ListChunked>>()?
-                    .with_name(s.name())
+            let out = if ca.len() == 0 {
+                // return an empty list if ca is empty.
+                ListChunked::full_null_with_dtype(ca.name(), 0, &ca.inner_dtype())
+            } else {
+                // SAFETY: unstable series never lives longer than the iterator.
+                unsafe {
+                    ca.amortized_iter()
+                        .zip(preds)
+                        .map(|(opt_s, opt_pred)| match (opt_s, opt_pred) {
+                            (Some(s), Some(pred)) => {
+                                s.as_ref().filter(pred.as_ref().bool()?).map(Some)
+                            },
+                            _ => Ok(None),
+                        })
+                        .collect::<PolarsResult<ListChunked>>()?
+                        .with_name(s.name())
+                }
             };
             ac_s.with_series(out.into_series(), true, Some(&self.expr))?;
             ac_s.update_groups = WithSeriesLen;
