808561f4fb952efb4a03218bed91792927f94b1e
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-01-11 07:29:15 +0100

    Fix mypy

--- py-polars/polars/exceptions.py
+++ py-polars/polars/exceptions.py
@@ -22,54 +22,54 @@ except ImportError:
     class PolarsError(Exception):  # type: ignore[no-redef]
         """Base class for all Polars errors."""
 
-    class ColumnNotFoundError(PolarsError):  # type: ignore[no-redef]
+    class ColumnNotFoundError(PolarsError):  # type: ignore[no-redef, misc]
         """Exception raised when a specified column is not found."""
 
-    class ComputeError(PolarsError):  # type: ignore[no-redef]
+    class ComputeError(PolarsError):  # type: ignore[no-redef, misc]
         """Exception raised when polars could not finish the computation."""
 
-    class DuplicateError(PolarsError):  # type: ignore[no-redef]
+    class DuplicateError(PolarsError):  # type: ignore[no-redef, misc]
         """Exception raised when a column name is duplicated."""
 
-    class InvalidOperationError(PolarsError):  # type: ignore[no-redef]
+    class InvalidOperationError(PolarsError):  # type: ignore[no-redef, misc]
         """Exception raised when an operation is not allowed on a certain data type."""
 
-    class NoDataError(PolarsError):  # type: ignore[no-redef]
+    class NoDataError(PolarsError):  # type: ignore[no-redef, misc]
         """Exception raised when an operation can not be performed on an empty data structure."""  # noqa: W505
 
-    class OutOfBoundsError(PolarsError):  # type: ignore[no-redef]
+    class OutOfBoundsError(PolarsError):  # type: ignore[no-redef, misc]
         """Exception raised when the given index is out of bounds."""
 
-    class PolarsPanicError(PolarsError):  # type: ignore[no-redef]
+    class PolarsPanicError(PolarsError):  # type: ignore[no-redef, misc]
         """Exception raised when an unexpected state causes a panic in the underlying Rust library."""  # noqa: W505
 
-    class SchemaError(PolarsError):  # type: ignore[no-redef]
+    class SchemaError(PolarsError):  # type: ignore[no-redef, misc]
         """Exception raised when trying to combine data structures with mismatched schemas."""  # noqa: W505
 
-    class SchemaFieldNotFoundError(PolarsError):  # type: ignore[no-redef]
+    class SchemaFieldNotFoundError(PolarsError):  # type: ignore[no-redef, misc]
         """Exception raised when a specified schema field is not found."""
 
-    class ShapeError(PolarsError):  # type: ignore[no-redef]
+    class ShapeError(PolarsError):  # type: ignore[no-redef, misc]
         """Exception raised when trying to combine data structures with incompatible shapes."""  # noqa: W505
 
-    class StringCacheMismatchError(PolarsError):  # type: ignore[no-redef]
+    class StringCacheMismatchError(PolarsError):  # type: ignore[no-redef, misc]
         """Exception raised when string caches come from different sources."""
 
-    class StructFieldNotFoundError(PolarsError):  # type: ignore[no-redef]
+    class StructFieldNotFoundError(PolarsError):  # type: ignore[no-redef, misc]
         """Exception raised when a specified schema field is not found."""
 
     class PolarsWarning(Exception):  # type: ignore[no-redef]
         """Base class for all Polars warnings."""
 
-    class CategoricalRemappingWarning(PolarsWarning):  # type: ignore[no-redef]
+    class CategoricalRemappingWarning(PolarsWarning):  # type: ignore[no-redef, misc]
         """Warning raised when a categorical needs to be remapped to be compatible with another categorical."""  # noqa: W505
 
 
-class InvalidAssert(PolarsError):
+class InvalidAssert(PolarsError):  # type: ignore[misc]
     """Exception raised when an unsupported testing assert is made."""
 
 
-class RowsError(PolarsError):
+class RowsError(PolarsError):  # type: ignore[misc]
     """Exception raised when the number of returned rows does not match expectation."""
 
 
@@ -81,19 +81,19 @@ class TooManyRowsReturnedError(RowsError):
     """Exception raised when more rows than expected are returned."""
 
 
-class ModuleUpgradeRequired(PolarsError):
+class ModuleUpgradeRequired(PolarsError):  # type: ignore[misc]
     """Exception raised when the module is installed but needs to be upgraded."""
 
 
-class ParameterCollisionError(PolarsError):
+class ParameterCollisionError(PolarsError):  # type: ignore[misc]
     """Exception raised when the same parameter occurs multiple times."""
 
 
-class UnsuitableSQLError(PolarsError):
+class UnsuitableSQLError(PolarsError):  # type: ignore[misc]
     """Exception raised when unsuitable SQL is given to a database method."""
 
 
-class ChronoFormatWarning(PolarsWarning):
+class ChronoFormatWarning(PolarsWarning):  # type: ignore[misc]
     """
     Warning raised when a chrono format string contains dubious patterns.
 
@@ -105,11 +105,11 @@ class ChronoFormatWarning(PolarsWarning):
     """
 
 
-class PolarsInefficientMapWarning(PolarsWarning):
+class PolarsInefficientMapWarning(PolarsWarning):  # type: ignore[misc]
     """Warning raised when a potentially slow `apply` operation is performed."""
 
 
-class TimeZoneAwareConstructorWarning(PolarsWarning):
+class TimeZoneAwareConstructorWarning(PolarsWarning):  # type: ignore[misc]
     """Warning raised when constructing Series from non-UTC time-zone-aware inputs."""
 
 
