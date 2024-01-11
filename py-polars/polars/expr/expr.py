ce3a939fad733fbe6c66c80ecee6d6abd2e69f0d
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-01-11 11:44:19 +0100

    fix(python): `Series.eq_missing` should return an Expr when the input is an Expr (#13628)

--- py-polars/polars/expr/expr.py
+++ py-polars/polars/expr/expr.py
@@ -57,9 +57,9 @@ from polars.utils.deprecation import (
 )
 from polars.utils.meta import threadpool_size
 from polars.utils.various import (
-    _warn_null_comparison,
     no_default,
     sphinx_accessor,
+    warn_null_comparison,
 )
 
 with contextlib.suppress(ImportError):  # Module not available when building docs
@@ -170,7 +170,7 @@ class Expr:
         return self._from_pyexpr(self._to_pyexpr(other)._and(self._pyexpr))
 
     def __eq__(self, other: Any) -> Self:  # type: ignore[override]
-        _warn_null_comparison(other)
+        warn_null_comparison(other)
         return self._from_pyexpr(self._pyexpr.eq(self._to_pyexpr(other)))
 
     def __floordiv__(self, other: Any) -> Self:
@@ -180,22 +180,22 @@ class Expr:
         return self._from_pyexpr(self._to_pyexpr(other) // self._pyexpr)
 
     def __ge__(self, other: Any) -> Self:
-        _warn_null_comparison(other)
+        warn_null_comparison(other)
         return self._from_pyexpr(self._pyexpr.gt_eq(self._to_pyexpr(other)))
 
     def __gt__(self, other: Any) -> Self:
-        _warn_null_comparison(other)
+        warn_null_comparison(other)
         return self._from_pyexpr(self._pyexpr.gt(self._to_pyexpr(other)))
 
     def __invert__(self) -> Self:
         return self.not_()
 
     def __le__(self, other: Any) -> Self:
-        _warn_null_comparison(other)
+        warn_null_comparison(other)
         return self._from_pyexpr(self._pyexpr.lt_eq(self._to_pyexpr(other)))
 
     def __lt__(self, other: Any) -> Self:
-        _warn_null_comparison(other)
+        warn_null_comparison(other)
         return self._from_pyexpr(self._pyexpr.lt(self._to_pyexpr(other)))
 
     def __mod__(self, other: Any) -> Self:
@@ -211,7 +211,7 @@ class Expr:
         return self._from_pyexpr(self._to_pyexpr(other) * self._pyexpr)
 
     def __ne__(self, other: Any) -> Self:  # type: ignore[override]
-        _warn_null_comparison(other)
+        warn_null_comparison(other)
         return self._from_pyexpr(self._pyexpr.neq(self._to_pyexpr(other)))
 
     def __neg__(self) -> Expr:
