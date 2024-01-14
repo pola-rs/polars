de18c8c7fbdf662b1cf7958ba812b8bb7a533ba9
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-01-14 17:07:43 +0000

    feat: convert fixed-offset timezones to respective Etc timezone from time zone database

--- crates/polars-core/src/chunked_array/temporal/mod.rs
+++ crates/polars-core/src/chunked_array/temporal/mod.rs
@@ -15,6 +15,10 @@ use chrono::NaiveDateTime;
 use chrono::NaiveTime;
 #[cfg(feature = "timezones")]
 use chrono_tz::Tz;
+#[cfg(feature = "timezones")]
+use once_cell::sync::Lazy;
+#[cfg(all(feature = "regex", feature = "timezones"))]
+use regex::Regex;
 #[cfg(feature = "dtype-time")]
 pub use time::time_to_time64ns;
 
@@ -25,6 +29,18 @@ use crate::prelude::{polars_bail, PolarsResult};
 pub fn unix_time() -> NaiveDateTime {
     NaiveDateTime::from_timestamp_opt(0, 0).unwrap()
 }
+
+#[cfg(feature = "timezones")]
+const FIXED_OFFSET_PATTERN: &str = r#"(?x)
+    ^
+    (?P<sign>[-+])?            # optional sign
+    (?P<hour>0[0-9]|1[0-4])    # hour (between 0 and 14)
+    :?                         # optional separator
+    00                         # minute
+    $
+    "#;
+#[cfg(feature = "timezones")]
+static FIXED_OFFSET_RE: Lazy<Regex> = Lazy::new(|| Regex::new(FIXED_OFFSET_PATTERN).unwrap());
 
 #[cfg(feature = "timezones")]
 pub(crate) fn validate_time_zone(tz: &str) -> PolarsResult<()> {
@@ -44,4 +60,29 @@ pub fn parse_time_zone(tz: &str) -> PolarsResult<Tz> {
             polars_bail!(ComputeError: "unable to parse time zone: '{}'. Please check the Time Zone Database for a list of available time zones", tz)
         },
     }
+}
+
+/// Convert fixed offset to Etc/GMT one from time zone database
+///
+/// E.g. +01:00 -> Etc/GMT-1
+///
+/// Note: the sign appears reversed, but is correct, see https://en.wikipedia.org/wiki/Tz_database#Area:
+/// > In order to conform with the POSIX style, those zone names beginning with
+/// > "Etc/GMT" have their sign reversed from the standard ISO 8601 convention.
+/// > In the "Etc" area, zones west of GMT have a positive sign and those east
+/// > have a negative sign in their name (e.g "Etc/GMT-14" is 14 hours ahead of GMT).
+#[cfg(feature = "timezones")]
+pub fn parse_fixed_offset(tz: &str) -> PolarsResult<String> {
+    if let Some(caps) = FIXED_OFFSET_RE.captures(tz) {
+        let sign = match caps.name("sign").map(|s| s.as_str()) {
+            Some("-") => "+",
+            _ => "-",
+        };
+        let hour = caps.name("hour").unwrap().as_str().parse::<i32>().unwrap();
+        let etc_tz = format!("Etc/GMT{}{}", sign, hour);
+        if etc_tz.parse::<Tz>().is_ok() {
+            return Ok(etc_tz);
+        }
+    }
+    polars_bail!(ComputeError: "unable to parse time zone: '{}'. Please check the Time Zone Database for a list of available time zones", tz)
 }
