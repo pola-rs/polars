3fe5128eb9b34f26c02f7e5b86bc0602c7401874
Weijie Guo <reswqa@163.com>
2024-01-15 23:52:05 +0800

    rename start to offset

--- crates/polars-ops/src/chunked_array/strings/substring.rs
+++ crates/polars-ops/src/chunked_array/strings/substring.rs
@@ -3,16 +3,16 @@ use polars_core::prelude::{Int64Chunked, StringChunked, UInt64Chunked};
 
 fn substring_ternary(
     opt_str_val: Option<&str>,
-    opt_start: Option<i64>,
+    opt_offset: Option<i64>,
     opt_length: Option<u64>,
 ) -> Option<&str> {
-    match (opt_str_val, opt_start) {
-        (Some(str_val), Some(start)) => {
+    match (opt_str_val, opt_offset) {
+        (Some(str_val), Some(offset)) => {
             // If `offset` is negative, it counts from the end of the string.
-            let offset = if start >= 0 {
-                start as usize
+            let offset = if offset >= 0 {
+                offset as usize
             } else {
-                let offset = (0i64 - start) as usize;
+                let offset = (0i64 - offset) as usize;
                 str_val
                     .char_indices()
                     .rev()
@@ -52,38 +52,38 @@ fn substring_ternary(
 
 pub(super) fn substring(
     ca: &StringChunked,
-    start: &Int64Chunked,
+    offset: &Int64Chunked,
     length: &UInt64Chunked,
 ) -> StringChunked {
-    match (ca.len(), start.len(), length.len()) {
+    match (ca.len(), offset.len(), length.len()) {
         (1, 1, _) => {
             // SAFETY: index `0` is in bound.
             let str_val = unsafe { ca.get_unchecked(0) };
             // SAFETY: index `0` is in bound.
-            let start = unsafe { start.get_unchecked(0) };
-            unary_elementwise(length, |length| substring_ternary(str_val, start, length))
+            let offset = unsafe { offset.get_unchecked(0) };
+            unary_elementwise(length, |length| substring_ternary(str_val, offset, length))
                 .with_name(ca.name())
         },
         (_, 1, 1) => {
             // SAFETY: index `0` is in bound.
-            let start = unsafe { start.get_unchecked(0) };
+            let offset = unsafe { offset.get_unchecked(0) };
             // SAFETY: index `0` is in bound.
             let length = unsafe { length.get_unchecked(0) };
-            unary_elementwise(ca, |str_val| substring_ternary(str_val, start, length))
+            unary_elementwise(ca, |str_val| substring_ternary(str_val, offset, length))
         },
         (1, _, 1) => {
             // SAFETY: index `0` is in bound.
             let str_val = unsafe { ca.get_unchecked(0) };
             // SAFETY: index `0` is in bound.
             let length = unsafe { length.get_unchecked(0) };
-            unary_elementwise(start, |start| substring_ternary(str_val, start, length))
+            unary_elementwise(offset, |offset| substring_ternary(str_val, offset, length))
                 .with_name(ca.name())
         },
         (1, len_b, len_c) if len_b == len_c => {
             // SAFETY: index `0` is in bound.
             let str_val = unsafe { ca.get_unchecked(0) };
-            binary_elementwise(start, length, |start, length| {
-                substring_ternary(str_val, start, length)
+            binary_elementwise(offset, length, |offset, length| {
+                substring_ternary(str_val, offset, length)
             })
         },
         (len_a, 1, len_c) if len_a == len_c => {
@@ -92,11 +92,11 @@ pub(super) fn substring(
                 f
             }
             // SAFETY: index `0` is in bound.
-            let start = unsafe { start.get_unchecked(0) };
+            let offset = unsafe { offset.get_unchecked(0) };
             binary_elementwise(
                 ca,
                 length,
-                infer(|str_val, length| substring_ternary(str_val, start, length)),
+                infer(|str_val, length| substring_ternary(str_val, offset, length)),
             )
         },
         (len_a, len_b, 1) if len_a == len_b => {
@@ -108,10 +108,10 @@ pub(super) fn substring(
             let length = unsafe { length.get_unchecked(0) };
             binary_elementwise(
                 ca,
-                start,
-                infer(|str_val, start| substring_ternary(str_val, start, length)),
+                offset,
+                infer(|str_val, offset| substring_ternary(str_val, offset, length)),
             )
         },
-        _ => ternary_elementwise(ca, start, length, substring_ternary),
+        _ => ternary_elementwise(ca, offset, length, substring_ternary),
     }
 }
