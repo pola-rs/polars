f6b15998167e2f9a2623314d69a1128182ed3f1b
Antonio Álvarez Caballero <3939991+29antonioac@users.noreply.github.com>
2024-01-15 13:07:20 +0000

    fix(python): selectors by_name and by_dtype should allow empty list as input (#11024)
    Co-authored-by: Antonio Caballero <antonio.caballero@concirrus.com>

--- py-polars/polars/selectors.py
+++ py-polars/polars/selectors.py
@@ -592,7 +592,7 @@ def by_dtype(
             raise TypeError(msg)
 
     return _selector_proxy_(
-        F.col(*all_dtypes), name="by_dtype", parameters={"dtypes": all_dtypes}
+        F.col(all_dtypes), name="by_dtype", parameters={"dtypes": all_dtypes}
     )
 
 
@@ -661,7 +661,7 @@ def by_name(*names: str | Collection[str]) -> SelectorType:
             TypeError(f"Invalid name: {nm!r}")
 
     return _selector_proxy_(
-        F.col(*all_names), name="by_name", parameters={"*names": all_names}
+        F.col(all_names), name="by_name", parameters={"*names": all_names}
     )
 
 
