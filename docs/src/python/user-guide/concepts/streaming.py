b3173fec8caae5d438b3baf0cde1113380dcdde3
r-brink <auc2yz7ab@mozmail.com>
2024-01-15 16:48:10 +0100

    improve streaming api section user guide

--- docs/src/python/user-guide/concepts/streaming.py
+++ docs/src/python/user-guide/concepts/streaming.py
@@ -1,12 +1,29 @@
+# --8<-- [start:import]
 import polars as pl
+# --8<-- [end:import]
 
 # --8<-- [start:streaming]
-q = (
+q1 = (
     pl.scan_csv("docs/data/iris.csv")
     .filter(pl.col("sepal_length") > 5)
     .group_by("species")
     .agg(pl.col("sepal_width").mean())
 )
-
-df = q.collect(streaming=True)
+df = q1.collect(streaming=True)
 # --8<-- [end:streaming]
+
+# --8<-- [start:example]
+print(q1.explain(streaming=True))
+
+# --8<-- [end:example]
+
+# --8<-- [start:example2]
+q2 = (
+    pl.scan_csv("docs/data/iris.csv")
+    .with_columns(
+        pl.col("sepal_length").mean().over("species")
+    )
+)
+
+print(q2.explain(streaming=True))
+# --8<-- [end:example2]
