b97bf5b1eebadb90667f63110e5ecf61e23b0641
r-brink <auc2yz7ab@mozmail.com>
2024-01-16 07:39:12 +0100

    fixed formatting

--- docs/src/rust/user-guide/concepts/streaming.rs
+++ docs/src/rust/user-guide/concepts/streaming.rs
@@ -9,7 +9,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .group_by(vec![col("species")])
         .agg([col("sepal_width").mean()]);
 
-    let df = q1.with_streaming(true).collect()?;
+    let df = q1.clone().with_streaming(true).collect()?;
     println!("{}", df);
     // --8<-- [end:streaming]
 
@@ -19,11 +19,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // --8<-- [end:example]
 
     // --8<-- [start:example2]
-    let q2 = LazyCsvReader::new("../data/iris.csv")
-    .finish()?
-    .with_columns(vec![
-        col("sepal_length").mean().over(vec![col("species")]).alias("sepal_length_mean")
-    ]);
+    let q2 = LazyCsvReader::new("docs/data/iris.csv")
+        .finish()?
+        .with_columns(vec![col("sepal_length")
+            .mean()
+            .over(vec![col("species")])
+            .alias("sepal_length_mean")]);
 
     let query_plan = q2.with_streaming(true).explain(true)?;
     println!("{}", query_plan);
