cb77a8a355636dce172f995b18149d9846674332
Weijie Guo <reswqa@163.com>
2024-01-18 17:31:06 +0800

    to serde-lazy

--- crates/polars-core/src/chunked_array/ops/mod.rs
+++ crates/polars-core/src/chunked_array/ops/mod.rs
@@ -408,7 +408,7 @@ pub trait ChunkSort<T: PolarsDataType> {
 pub type FillNullLimit = Option<IdxSize>;
 
 #[derive(Copy, Clone, Debug, PartialEq, Hash)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde-lazy", derive(Serialize, Deserialize))]
 pub enum FillNullStrategy {
     /// previous value in array
     Backward(FillNullLimit),
