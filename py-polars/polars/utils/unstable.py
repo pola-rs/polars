354d5a4384c58dcb49c1f82510e24584144c40e3
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-01-24 11:50:21 +0100

    Add unstable decorator

---
+++ py-polars/polars/utils/unstable.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import inspect
+import os
+import warnings
+from functools import wraps
+from typing import TYPE_CHECKING, Callable, TypeVar
+
+from polars.exceptions import UnstableWarning
+from polars.utils.various import find_stacklevel
+
+if TYPE_CHECKING:
+    import sys
+
+    if sys.version_info >= (3, 10):
+        from typing import ParamSpec
+    else:
+        from typing_extensions import ParamSpec
+
+    P = ParamSpec("P")
+    T = TypeVar("T")
+
+
+def issue_unstable_warning(message: str) -> None:
+    """
+    Issue a warning for use of unstable functionality.
+
+    The `warn_unstable` setting must be enabled, otherwise no warning is issued.
+
+    Parameters
+    ----------
+    message
+        The message associated with the warning.
+    """
+    enabled = bool(int(os.environ.get("POLARS_WARN_UNSTABLE", 0)))
+    if enabled:
+        warnings.warn(message, UnstableWarning, stacklevel=find_stacklevel())
+
+
+def unstable() -> Callable[[Callable[P, T]], Callable[P, T]]:
+    """Decorator to mark a function as experimental."""
+
+    def decorate(function: Callable[P, T]) -> Callable[P, T]:
+        @wraps(function)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+            issue_unstable_warning(
+                f"`{function.__name__}` is considered unstable."
+                " Its API and implementation are subject to change.",
+            )
+            return function(*args, **kwargs)
+
+        wrapper.__signature__ = inspect.signature(function)  # type: ignore[attr-defined]
+        return wrapper
+
+    return decorate
