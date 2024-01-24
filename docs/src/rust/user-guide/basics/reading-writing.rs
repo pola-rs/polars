92aca2779d74d68c4394981fd06b7e2840baef60
chielP <chielpeters@live.nl>
2024-01-24 11:15:48 +0100

    Revert "docs: Improve structure of user guide" (#13945)

--- docs/src/rust/user-guide/basics/reading-writing.rs
+++ docs/src/rust/user-guide/basics/reading-writing.rs
@@ -9,9 +9,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut df: DataFrame = df!(
         "integer" => &[1, 2, 3],
         "date" => &[
-                NaiveDate::from_ymd_opt(2025, 1, 1).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-                NaiveDate::from_ymd_opt(2025, 1, 2).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-                NaiveDate::from_ymd_opt(2025, 1, 3).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+                NaiveDate::from_ymd_opt(2022, 1, 1).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+                NaiveDate::from_ymd_opt(2022, 1, 2).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+                NaiveDate::from_ymd_opt(2022, 1, 3).unwrap().and_hms_opt(0, 0, 0).unwrap(),
         ],
         "float" => &[4.0, 5.0, 6.0]
     )
--- docs/user-guide/transformations/index.md
+++
@@ -1,8 +0,0 @@
-# Transformations
-
-The focus of this section is to describe different types of data transformations and provide some examples on how to use them.
-
-- [Joins](joins.md)
-- [Concatenation](concatenation.md)
-- [Pivot](pivot.md)
-- [Melt](melt.md)
