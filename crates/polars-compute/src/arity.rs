c484a63ab219a687b58e98d796e054d6a7168ab2
Orson Peters <orsonpeters@gmail.com>
2024-01-29 13:56:05 +0100

    reduce codesize kernels

--- crates/polars-compute/src/arity.rs
+++ crates/polars-compute/src/arity.rs
@@ -2,6 +2,44 @@ use arrow::array::PrimitiveArray;
 use arrow::compute::utils::combine_validities_and;
 use arrow::types::NativeType;
 
+/// To reduce codegen we use these helpers where the input and output arrays
+/// may overlap. These are marked to never be inlined, this way only a single
+/// unrolled kernel gets generated, even if we call it in multiple ways.
+///
+/// # Safety
+///  - arr must point to a readable slice of length len.
+///  - out must point to a writeable slice of length len.
+#[inline(never)]
+unsafe fn ptr_apply_unary_kernel<I: Copy, O, F: Fn(I) -> O>(
+    arr: *const I,
+    out: *mut O,
+    len: usize,
+    op: F,
+) {
+    for i in 0..len {
+        let ret = op(arr.add(i).read());
+        out.add(i).write(ret);
+    }
+}
+
+/// # Safety
+///  - left must point to a readable slice of length len.
+///  - right must point to a readable slice of length len.
+///  - out must point to a writeable slice of length len.
+#[inline(never)]
+unsafe fn ptr_apply_binary_kernel<L: Copy, R: Copy, O, F: Fn(L, R) -> O>(
+    left: *const L,
+    right: *const R,
+    out: *mut O,
+    len: usize,
+    op: F,
+) {
+    for i in 0..len {
+        let ret = op(left.add(i).read(), right.add(i).read());
+        out.add(i).write(ret);
+    }
+}
+
 /// Applies a function to all the values (regardless of nullability).
 ///
 /// May re-use the memory of the array if possible.
@@ -11,21 +49,27 @@ where
     O: NativeType,
     F: Fn(I) -> O,
 {
+    let len = arr.len();
+
     // Reuse memory if possible.
     if std::mem::size_of::<I>() == std::mem::size_of::<O>()
         && std::mem::align_of::<I>() == std::mem::align_of::<O>()
     {
         if let Some(values) = arr.get_mut_values() {
-            for v in values {
-                // SAFETY: checked same size & alignment, NativeType is always Pod.
-                *v = unsafe { std::mem::transmute_copy::<O, I>(&(op(*v))) };
-            }
+            let ptr = values.as_mut_ptr();
+            // SAFETY: checked same size & alignment I/O, NativeType is always Pod.
+            unsafe { ptr_apply_unary_kernel(ptr, ptr as *mut O, len, op) }
             return arr.transmute::<O>();
         }
     }
 
-    let ret = arr.values_iter().map(|x| op(*x)).collect();
-    PrimitiveArray::from_vec(ret).with_validity(arr.take_validity())
+    let mut out = Vec::with_capacity(len);
+    unsafe {
+        // SAFETY: checked pointers point to slices of length len.
+        ptr_apply_unary_kernel(arr.values().as_ptr(), out.as_mut_ptr(), len, op);
+        out.set_len(len);
+    }
+    PrimitiveArray::from_vec(out).with_validity(arr.take_validity())
 }
 
 /// Apply a binary function to all the values (regardless of nullability)
@@ -44,6 +88,7 @@ where
     F: Fn(L, R) -> O,
 {
     assert_eq!(lhs.len(), rhs.len());
+    let len = lhs.len();
 
     let validity = combine_validities_and(lhs.validity(), rhs.validity());
 
@@ -52,10 +97,11 @@ where
         && std::mem::align_of::<L>() == std::mem::align_of::<O>()
     {
         if let Some(lv) = lhs.get_mut_values() {
-            let pairs = lv.iter_mut().zip(rhs.values_iter());
-            for (l, r) in pairs {
-                // SAFETY: checked same size & alignment, NativeType is always Pod.
-                *l = unsafe { std::mem::transmute_copy::<O, L>(&(op(*l, *r))) };
+            let lp = lv.as_mut_ptr();
+            let rp = rhs.values().as_ptr();
+            unsafe {
+                // SAFETY: checked same size & alignment L/O, NativeType is always Pod.
+                ptr_apply_binary_kernel(lp, rp, lp as *mut O, len, op);
             }
             return lhs.transmute::<O>().with_validity(validity);
         }
@@ -64,16 +110,23 @@ where
         && std::mem::align_of::<R>() == std::mem::align_of::<O>()
     {
         if let Some(rv) = rhs.get_mut_values() {
-            let pairs = lhs.values_iter().zip(rv.iter_mut());
-            for (l, r) in pairs {
-                // SAFETY: checked same size & alignment, NativeType is always Pod.
-                *r = unsafe { std::mem::transmute_copy::<O, R>(&(op(*l, *r))) };
+            let lp = lhs.values().as_ptr();
+            let rp = rv.as_mut_ptr();
+            unsafe {
+                // SAFETY: checked same size & alignment R/O, NativeType is always Pod.
+                ptr_apply_binary_kernel(lp, rp, rp as *mut O, len, op);
             }
             return rhs.transmute::<O>().with_validity(validity);
         }
     }
 
-    let pairs = lhs.values_iter().zip(rhs.values_iter());
-    let ret = pairs.map(|(l, r)| op(*l, *r)).collect();
-    PrimitiveArray::from_vec(ret).with_validity(validity)
+    let mut out = Vec::with_capacity(len);
+    unsafe {
+        // SAFETY: checked pointers point to slices of length len.
+        let lp = lhs.values().as_ptr();
+        let rp = rhs.values().as_ptr();
+        ptr_apply_binary_kernel(lp, rp, out.as_mut_ptr(), len, op);
+        out.set_len(len);
+    }
+    PrimitiveArray::from_vec(out).with_validity(validity)
 }
