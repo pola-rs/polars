c98c0f56ddbe99a4816c49226d637c69646a40dc
chielP <chielpeters@live.nl>
2024-01-31 10:52:04 +0100

    feature flag

--- crates/polars-plan/src/logical_plan/optimizer/type_coercion/mod.rs
+++ crates/polars-plan/src/logical_plan/optimizer/type_coercion/mod.rs
@@ -231,13 +231,11 @@ fn modify_supertype(
             | (List(other), List(inner), AExpr::Literal(_), _)
                 if inner != other =>
             {
-                #[cfg(feature = "dtype-categorical")]
                 st = match &**inner {
+                    #[cfg(feature = "dtype-categorical")]
                     Categorical(_, ordering) => List(Box::new(Categorical(None, *ordering))),
                     _ => List(inner.clone()),
                 };
-                #[cfg(not(feature = "dtype-categorical"))]
-                st = List(inner.clone())
             },
             // do nothing
             _ => {},
