fb51ea7964cd519dac473ac7f9b9b2939ae0439e
Weijie Guo <reswqa@163.com>
2024-02-01 11:12:22 +0800

    docs(python): Document that Kleene logic is followed in `any_horizontal` and `all_horizontal`

--- py-polars/polars/functions/aggregation/horizontal.py
+++ py-polars/polars/functions/aggregation/horizontal.py
@@ -27,26 +27,33 @@ def all_horizontal(*exprs: IntoExpr | Iterable[IntoExpr]) -> Expr:
         Column(s) to use in the aggregation. Accepts expression input. Strings are
         parsed as column names, other non-expression inputs are parsed as literals.
 
+    Notes
+    -----
+    For `None` value, this follows Kleene logic
+    (https://en.wikipedia.org/wiki/Three-valued_logic#Kleene_and_Priest_logics).
+
     Examples
     --------
     >>> df = pl.DataFrame(
     ...     {
-    ...         "a": [False, False, True, True],
-    ...         "b": [False, True, None, True],
-    ...         "c": ["w", "x", "y", "z"],
+    ...         "a": [False, False, True, True, False, None],
+    ...         "b": [False, True, True, None, None, None],
+    ...         "c": ["u", "v", "w", "x", "y", "z"],
     ...     }
     ... )
     >>> df.with_columns(all=pl.all_horizontal("a", "b"))
-    shape: (4, 4)
+    shape: (6, 4)
     ┌───────┬───────┬─────┬───────┐
     │ a     ┆ b     ┆ c   ┆ all   │
     │ ---   ┆ ---   ┆ --- ┆ ---   │
     │ bool  ┆ bool  ┆ str ┆ bool  │
     ╞═══════╪═══════╪═════╪═══════╡
-    │ false ┆ false ┆ w   ┆ false │
-    │ false ┆ true  ┆ x   ┆ false │
-    │ true  ┆ null  ┆ y   ┆ null  │
-    │ true  ┆ true  ┆ z   ┆ true  │
+    │ false ┆ false ┆ u   ┆ false │
+    │ false ┆ true  ┆ v   ┆ false │
+    │ true  ┆ true  ┆ w   ┆ true  │
+    │ true  ┆ null  ┆ x   ┆ null  │
+    │ false ┆ null  ┆ y   ┆ false │
+    │ null  ┆ null  ┆ z   ┆ null  │
     └───────┴───────┴─────┴───────┘
     """
     pyexprs = parse_as_list_of_expressions(*exprs)
@@ -63,25 +70,32 @@ def any_horizontal(*exprs: IntoExpr | Iterable[IntoExpr]) -> Expr:
         Column(s) to use in the aggregation. Accepts expression input. Strings are
         parsed as column names, other non-expression inputs are parsed as literals.
 
+    Notes
+    -----
+    For `None` value, this follows Kleene logic
+    (https://en.wikipedia.org/wiki/Three-valued_logic#Kleene_and_Priest_logics).
+
     Examples
     --------
     >>> df = pl.DataFrame(
     ...     {
-    ...         "a": [False, False, True, None],
-    ...         "b": [False, True, None, None],
-    ...         "c": ["w", "x", "y", "z"],
+    ...         "a": [False, False, True, True, False, None],
+    ...         "b": [False, True, True, None, None, None],
+    ...         "c": ["u", "v", "w", "x", "y", "z"],
     ...     }
     ... )
     >>> df.with_columns(any=pl.any_horizontal("a", "b"))
-    shape: (4, 4)
+    shape: (6, 4)
     ┌───────┬───────┬─────┬───────┐
     │ a     ┆ b     ┆ c   ┆ any   │
     │ ---   ┆ ---   ┆ --- ┆ ---   │
     │ bool  ┆ bool  ┆ str ┆ bool  │
     ╞═══════╪═══════╪═════╪═══════╡
-    │ false ┆ false ┆ w   ┆ false │
-    │ false ┆ true  ┆ x   ┆ true  │
-    │ true  ┆ null  ┆ y   ┆ true  │
+    │ false ┆ false ┆ u   ┆ false │
+    │ false ┆ true  ┆ v   ┆ true  │
+    │ true  ┆ true  ┆ w   ┆ true  │
+    │ true  ┆ null  ┆ x   ┆ true  │
+    │ false ┆ null  ┆ y   ┆ null  │
     │ null  ┆ null  ┆ z   ┆ null  │
     └───────┴───────┴─────┴───────┘
     """
