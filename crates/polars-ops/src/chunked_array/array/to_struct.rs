0c3efaacb8b957fe4af369d5ec7f9efb57a8cb01
Weijie Guo <reswqa@163.com>
2024-02-02 21:17:23 +0800

    wrap by pool.install

--- crates/polars-ops/src/chunked_array/array/to_struct.rs
+++ crates/polars-ops/src/chunked_array/array/to_struct.rs
@@ -1,4 +1,5 @@
 use polars_core::export::rayon::prelude::*;
+use polars_core::POOL;
 use polars_utils::format_smartstring;
 use smartstring::alias::String as SmartString;
 
@@ -23,16 +24,18 @@ pub trait ToStruct: AsArray {
             .unwrap_or(&arr_default_struct_name_gen);
 
         polars_ensure!(n_fields != 0, ComputeError: "cannot create a struct with 0 fields");
-        let fields = (0..n_fields)
-            .into_par_iter()
-            .map(|i| {
-                ca.array_get(&Int64Chunked::from_slice("", &[i as i64]))
-                    .map(|mut s| {
-                        s.rename(&name_generator(i));
-                        s
-                    })
-            })
-            .collect::<PolarsResult<Vec<_>>>()?;
+        let fields = POOL.install(|| {
+            (0..n_fields)
+                .into_par_iter()
+                .map(|i| {
+                    ca.array_get(&Int64Chunked::from_slice("", &[i as i64]))
+                        .map(|mut s| {
+                            s.rename(&name_generator(i));
+                            s
+                        })
+                })
+                .collect::<PolarsResult<Vec<_>>>()
+        })?;
 
         StructChunked::new(ca.name(), &fields)
     }
