470cfafe264608b220e5c4bf0cee455e125e8064
Yin Jifeng <jifeng.yin@gmail.com>
2024-02-02 18:34:09 +0800

    feat(json): serialize decimal

--- crates/polars-json/src/json/write/serialize.rs
+++ crates/polars-json/src/json/write/serialize.rs
@@ -4,6 +4,8 @@ use arrow::array::*;
 use arrow::bitmap::utils::ZipValidity;
 use arrow::datatypes::{ArrowDataType, IntegerType, TimeUnit};
 use arrow::io::iterator::BufStreamingIterator;
+#[cfg(feature = "dtype-decimal")]
+use arrow::legacy::compute::decimal::format_decimal;
 use arrow::offset::Offset;
 #[cfg(feature = "chrono-tz")]
 use arrow::temporal_conversions::parse_offset_tz;
@@ -104,6 +106,25 @@ where
             } else {
                 write_float(buf, *x)
             }
+        } else {
+            buf.extend(b"null")
+        }
+    };
+
+    materialize_serializer(f, array.iter(), offset, take)
+}
+
+#[cfg(feature = "dtype-decimal")]
+fn decimal_serializer<'a>(
+    array: &'a PrimitiveArray<i128>,
+    scale: usize,
+    offset: usize,
+    take: usize,
+) -> Box<dyn StreamingIterator<Item = [u8]> + 'a + Send + Sync> {
+    let trim_zeros = polars_core::fmt::get_trim_decimal_zeros();
+    let f = move |x: Option<&i128>, buf: &mut Vec<u8>| {
+        if let Some(x) = x {
+            utf8::write_str(buf, format_decimal(*x, scale, trim_zeros).as_str()).unwrap()
         } else {
             buf.extend(b"null")
         }
@@ -420,6 +441,10 @@ pub(crate) fn new_serializer<'a>(
         },
         ArrowDataType::Float64 => {
             float_serializer::<f64>(array.as_any().downcast_ref().unwrap(), offset, take)
+        },
+        #[cfg(feature = "dtype-decimal")]
+        ArrowDataType::Decimal(_, scale) => {
+            decimal_serializer(array.as_any().downcast_ref().unwrap(), *scale, offset, take)
         },
         ArrowDataType::LargeUtf8 => {
             utf8_serializer::<i64>(array.as_any().downcast_ref().unwrap(), offset, take)
