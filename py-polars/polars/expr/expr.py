975af10594e4ac6d7f5dc70c60cd8c50723d08f9
deanm0000 <37878412+deanm0000@users.noreply.github.com>
2024-02-03 08:52:34 -0500

    fix(python): fix ufunc in agg (change __ufunc_array__ so it uses `is_elementwise=True` parameter) (#14135)

--- py-polars/polars/expr/expr.py
+++ py-polars/polars/expr/expr.py
@@ -31,7 +31,7 @@ from polars.datatypes import (
 )
 from polars.dependencies import _check_for_numpy
 from polars.dependencies import numpy as np
-from polars.exceptions import PolarsInefficientMapWarning
+from polars.exceptions import CustomUFuncWarning, PolarsInefficientMapWarning
 from polars.expr.array import ExprArrayNameSpace
 from polars.expr.binary import ExprBinaryNameSpace
 from polars.expr.categorical import ExprCatNameSpace
@@ -58,6 +58,7 @@ from polars.utils.deprecation import (
 )
 from polars.utils.unstable import issue_unstable_warning, unstable
 from polars.utils.various import (
+    find_stacklevel,
     no_default,
     sphinx_accessor,
     warn_null_comparison,
@@ -286,6 +287,7 @@ class Expr:
         self, ufunc: Callable[..., Any], method: str, *inputs: Any, **kwargs: Any
     ) -> Self:
         """Numpy universal functions."""
+        is_custom_ufunc = ufunc.__class__ != np.ufunc
         num_expr = sum(isinstance(inp, Expr) for inp in inputs)
         if num_expr > 1:
             if num_expr < len(inputs):
@@ -302,7 +304,20 @@ class Expr:
             args = [inp if not isinstance(inp, Expr) else s for inp in inputs]
             return ufunc(*args, **kwargs)
 
-        return self.map_batches(function)
+        if is_custom_ufunc is True:
+            msg = (
+                "Native numpy ufuncs are dispatched using `map_batches(ufunc, is_elementwise=True)` which "
+                "is safe for native Numpy and Scipy ufuncs but custom ufuncs in a group_by "
+                "context won't be properly grouped. Custom ufuncs are dispatched with is_elementwise=False. "
+                f"If {ufunc.__name__} needs elementwise then please use map_batches directly."
+            )
+            warnings.warn(
+                msg,
+                CustomUFuncWarning,
+                stacklevel=find_stacklevel(),
+            )
+            return self.map_batches(function, is_elementwise=False)
+        return self.map_batches(function, is_elementwise=True)
 
     @classmethod
     def from_json(cls, value: str) -> Self:
