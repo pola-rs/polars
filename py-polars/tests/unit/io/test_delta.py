7070368668164fc90379f49913972a96f3176c66
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-02-06 11:45:19 +0100

    docs(python): Small clarifications in the contributing guide (#14310)

--- py-polars/tests/unit/io/test_delta.py
+++ py-polars/tests/unit/io/test_delta.py
@@ -388,7 +388,7 @@ def test_write_delta_with_merge_and_no_table(tmp_path: Path) -> None:
 def test_write_delta_with_merge(tmp_path: Path) -> None:
     df = pl.DataFrame({"a": [1, 2, 3]})
 
-    df.write_delta(tmp_path, mode="append")
+    df.write_delta(tmp_path)
 
     merger = df.write_delta(
         tmp_path,
@@ -407,8 +407,7 @@ def test_write_delta_with_merge(tmp_path: Path) -> None:
 
     merger.when_matched_delete(predicate="t.a > 2").execute()
 
-    table = pl.read_delta(str(tmp_path))
+    result = pl.read_delta(str(tmp_path))
 
-    # TODO: Re-enable row order check after issue is resolved:
-    # https://github.com/delta-io/delta-rs/issues/2165
-    assert_frame_equal(df.filter(pl.col("a") <= 2), table, check_row_order=False)
+    expected = df.filter(pl.col("a") <= 2)
+    assert_frame_equal(result, expected, check_row_order=False)
