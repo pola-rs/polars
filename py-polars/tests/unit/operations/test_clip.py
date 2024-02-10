c40d13cf33876130aa89f9bb52b5ae7ff0140a7a
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-02-11 00:22:08 +0100

    test(python): Refactor `clip` tests

--- py-polars/tests/unit/operations/test_clip.py
+++ py-polars/tests/unit/operations/test_clip.py
@@ -5,45 +5,58 @@ from datetime import datetime
 import pytest
 
 import polars as pl
-from polars.testing.asserts.series import assert_series_equal
+from polars.testing import assert_frame_equal, assert_series_equal
 
 
-def test_clip() -> None:
-    clip_exprs = [
+@pytest.fixture()
+def clip_exprs() -> list[pl.Expr]:
+    return [
         pl.col("a").clip(pl.col("min"), pl.col("max")).alias("clip"),
         pl.col("a").clip(lower_bound=pl.col("min")).alias("clip_min"),
         pl.col("a").clip(upper_bound=pl.col("max")).alias("clip_max"),
     ]
 
-    df = pl.DataFrame(
+
+def test_clip_int(clip_exprs: list[pl.Expr]) -> None:
+    lf = pl.LazyFrame(
         {
             "a": [1, 2, 3, 4, 5],
             "min": [0, -1, 4, None, 4],
             "max": [2, 1, 8, 5, None],
         }
     )
+    result = lf.select(clip_exprs)
+    expected = pl.LazyFrame(
+        {
+            "clip": [1, 1, 4, None, None],
+            "clip_min": [1, 2, 4, None, 5],
+            "clip_max": [1, 1, 3, 4, None],
+        }
+    )
+    assert_frame_equal(result, expected)
 
-    assert df.select(clip_exprs).to_dict(as_series=False) == {
-        "clip": [1, 1, 4, None, None],
-        "clip_min": [1, 2, 4, None, 5],
-        "clip_max": [1, 1, 3, 4, None],
-    }
 
-    df = pl.DataFrame(
+def test_clip_float(clip_exprs: list[pl.Expr]) -> None:
+    lf = pl.LazyFrame(
         {
             "a": [1.0, 2.0, 3.0, 4.0, 5.0],
             "min": [0, -1.0, 4.0, None, 4.0],
             "max": [2.0, 1.0, 8.0, 5.0, None],
         }
     )
+    result = lf.select(clip_exprs)
+    expected = pl.LazyFrame(
+        {
+            "clip": [1.0, 1.0, 4.0, None, None],
+            "clip_min": [1.0, 2.0, 4.0, None, 5.0],
+            "clip_max": [1.0, 1.0, 3.0, 4.0, None],
+        }
+    )
+    assert_frame_equal(result, expected)
 
-    assert df.select(clip_exprs).to_dict(as_series=False) == {
-        "clip": [1.0, 1.0, 4.0, None, None],
-        "clip_min": [1.0, 2.0, 4.0, None, 5.0],
-        "clip_max": [1.0, 1.0, 3.0, 4.0, None],
-    }
 
-    df = pl.DataFrame(
+def test_clip_datetime(clip_exprs: list[pl.Expr]) -> None:
+    lf = pl.LazyFrame(
         {
             "a": [
                 datetime(1995, 6, 5, 10, 30),
@@ -71,33 +84,36 @@ def test_clip() -> None:
             ],
         }
     )
-
-    assert df.select(clip_exprs).to_dict(as_series=False) == {
-        "clip": [
-            datetime(1995, 6, 5, 10, 30),
-            datetime(1996, 6, 5),
-            datetime(2023, 9, 20, 18, 30, 6),
-            None,
-            None,
-            None,
-        ],
-        "clip_min": [
-            datetime(1995, 6, 5, 10, 30),
-            datetime(1996, 6, 5),
-            datetime(2023, 10, 20, 18, 30, 6),
-            None,
-            None,
-            datetime(2000, 1, 10),
-        ],
-        "clip_max": [
-            datetime(1995, 6, 5, 10, 30),
-            datetime(1995, 6, 5),
-            datetime(2023, 9, 20, 18, 30, 6),
-            None,
-            datetime(1993, 3, 13),
-            None,
-        ],
-    }
+    result = lf.select(clip_exprs)
+    expected = pl.LazyFrame(
+        {
+            "clip": [
+                datetime(1995, 6, 5, 10, 30),
+                datetime(1996, 6, 5),
+                datetime(2023, 9, 20, 18, 30, 6),
+                None,
+                None,
+                None,
+            ],
+            "clip_min": [
+                datetime(1995, 6, 5, 10, 30),
+                datetime(1996, 6, 5),
+                datetime(2023, 10, 20, 18, 30, 6),
+                None,
+                None,
+                datetime(2000, 1, 10),
+            ],
+            "clip_max": [
+                datetime(1995, 6, 5, 10, 30),
+                datetime(1995, 6, 5),
+                datetime(2023, 9, 20, 18, 30, 6),
+                None,
+                datetime(1993, 3, 13),
+                None,
+            ],
+        }
+    )
+    assert_frame_equal(result, expected)
 
 
 def test_clip_min_max_deprecated() -> None:
