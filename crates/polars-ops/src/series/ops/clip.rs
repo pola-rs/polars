83c6021360e99ee28b75176000948350ca650db8
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-02-11 01:11:24 +0100

    feat(python): Allow column name input in `clip` (#14410)

--- crates/polars-ops/src/series/ops/clip.rs
+++ crates/polars-ops/src/series/ops/clip.rs
@@ -3,6 +3,101 @@ use polars_core::prelude::arity::{binary_elementwise, ternary_elementwise};
 use polars_core::prelude::*;
 use polars_core::with_match_physical_numeric_polars_type;
 
+/// Set values outside the given boundaries to the boundary value.
+pub fn clip(s: &Series, min: &Series, max: &Series) -> PolarsResult<Series> {
+    polars_ensure!(
+        s.dtype().to_physical().is_numeric(),
+        InvalidOperation: "`clip` only supports physical numeric types"
+    );
+
+    let original_type = s.dtype();
+    // cast min & max to the dtype of s first.
+    let (min, max) = (min.cast(s.dtype())?, max.cast(s.dtype())?);
+
+    let (s, min, max) = (
+        s.to_physical_repr(),
+        min.to_physical_repr(),
+        max.to_physical_repr(),
+    );
+
+    match s.dtype() {
+        dt if dt.is_numeric() => {
+            with_match_physical_numeric_polars_type!(s.dtype(), |$T| {
+                let ca: &ChunkedArray<$T> = s.as_ref().as_ref().as_ref();
+                let min: &ChunkedArray<$T> = min.as_ref().as_ref().as_ref();
+                let max: &ChunkedArray<$T> = max.as_ref().as_ref().as_ref();
+                let out = clip_helper(ca, min, max).into_series();
+                if original_type.is_logical(){
+                    out.cast(original_type)
+                }else{
+                    Ok(out)
+                }
+            })
+        },
+        dt => polars_bail!(opq = clippy, dt),
+    }
+}
+
+/// Set values above the given maximum to the maximum value.
+pub fn clip_max(s: &Series, max: &Series) -> PolarsResult<Series> {
+    polars_ensure!(
+        s.dtype().to_physical().is_numeric(),
+        InvalidOperation: "`clip` only supports physical numeric types"
+    );
+
+    let original_type = s.dtype();
+    // cast max to the dtype of s first.
+    let max = max.cast(s.dtype())?;
+
+    let (s, max) = (s.to_physical_repr(), max.to_physical_repr());
+
+    match s.dtype() {
+        dt if dt.is_numeric() => {
+            with_match_physical_numeric_polars_type!(s.dtype(), |$T| {
+                let ca: &ChunkedArray<$T> = s.as_ref().as_ref().as_ref();
+                let max: &ChunkedArray<$T> = max.as_ref().as_ref().as_ref();
+                let out = clip_min_max_helper(ca, max, clamp_max).into_series();
+                if original_type.is_logical(){
+                    out.cast(original_type)
+                }else{
+                    Ok(out)
+                }
+            })
+        },
+        dt => polars_bail!(opq = clippy_max, dt),
+    }
+}
+
+/// Set values below the given minimum to the minimum value.
+pub fn clip_min(s: &Series, min: &Series) -> PolarsResult<Series> {
+    polars_ensure!(
+        s.dtype().to_physical().is_numeric(),
+        InvalidOperation: "`clip` only supports physical numeric types"
+    );
+
+    let original_type = s.dtype();
+    // cast min to the dtype of s first.
+    let min = min.cast(s.dtype())?;
+
+    let (s, min) = (s.to_physical_repr(), min.to_physical_repr());
+
+    match s.dtype() {
+        dt if dt.is_numeric() => {
+            with_match_physical_numeric_polars_type!(s.dtype(), |$T| {
+                let ca: &ChunkedArray<$T> = s.as_ref().as_ref().as_ref();
+                let min: &ChunkedArray<$T> = min.as_ref().as_ref().as_ref();
+                let out = clip_min_max_helper(ca, min, clamp_min).into_series();
+                if original_type.is_logical(){
+                    out.cast(original_type)
+                }else{
+                    Ok(out)
+                }
+            })
+        },
+        dt => polars_bail!(opq = clippy_min, dt),
+    }
+}
+
 fn clip_helper<T>(
     ca: &ChunkedArray<T>,
     min: &ChunkedArray<T>,
@@ -61,91 +156,5 @@ where
             (Some(s), Some(bound)) => Some(op(s, bound)),
             _ => None,
         }),
-    }
-}
-
-/// Clamp underlying values to the `min` and `max` values.
-pub fn clip(s: &Series, min: &Series, max: &Series) -> PolarsResult<Series> {
-    polars_ensure!(s.dtype().to_physical().is_numeric(), InvalidOperation: "Only physical numeric types are supported.");
-
-    let original_type = s.dtype();
-    // cast min & max to the dtype of s first.
-    let (min, max) = (min.cast(s.dtype())?, max.cast(s.dtype())?);
-
-    let (s, min, max) = (
-        s.to_physical_repr(),
-        min.to_physical_repr(),
-        max.to_physical_repr(),
-    );
-
-    match s.dtype() {
-        dt if dt.is_numeric() => {
-            with_match_physical_numeric_polars_type!(s.dtype(), |$T| {
-                let ca: &ChunkedArray<$T> = s.as_ref().as_ref().as_ref();
-                let min: &ChunkedArray<$T> = min.as_ref().as_ref().as_ref();
-                let max: &ChunkedArray<$T> = max.as_ref().as_ref().as_ref();
-                let out = clip_helper(ca, min, max).into_series();
-                if original_type.is_logical(){
-                    out.cast(original_type)
-                }else{
-                    Ok(out)
-                }
-            })
-        },
-        dt => polars_bail!(opq = clippy, dt),
-    }
-}
-
-/// Clamp underlying values to the `max` value.
-pub fn clip_max(s: &Series, max: &Series) -> PolarsResult<Series> {
-    polars_ensure!(s.dtype().to_physical().is_numeric(), InvalidOperation: "Only physical numeric types are supported.");
-
-    let original_type = s.dtype();
-    // cast max to the dtype of s first.
-    let max = max.cast(s.dtype())?;
-
-    let (s, max) = (s.to_physical_repr(), max.to_physical_repr());
-
-    match s.dtype() {
-        dt if dt.is_numeric() => {
-            with_match_physical_numeric_polars_type!(s.dtype(), |$T| {
-                let ca: &ChunkedArray<$T> = s.as_ref().as_ref().as_ref();
-                let max: &ChunkedArray<$T> = max.as_ref().as_ref().as_ref();
-                let out = clip_min_max_helper(ca, max, clamp_max).into_series();
-                if original_type.is_logical(){
-                    out.cast(original_type)
-                }else{
-                    Ok(out)
-                }
-            })
-        },
-        dt => polars_bail!(opq = clippy_max, dt),
-    }
-}
-
-/// Clamp underlying values to the `min` value.
-pub fn clip_min(s: &Series, min: &Series) -> PolarsResult<Series> {
-    polars_ensure!(s.dtype().to_physical().is_numeric(), InvalidOperation: "Only physical numeric types are supported.");
-
-    let original_type = s.dtype();
-    // cast min to the dtype of s first.
-    let min = min.cast(s.dtype())?;
-
-    let (s, min) = (s.to_physical_repr(), min.to_physical_repr());
-
-    match s.dtype() {
-        dt if dt.is_numeric() => {
-            with_match_physical_numeric_polars_type!(s.dtype(), |$T| {
-                let ca: &ChunkedArray<$T> = s.as_ref().as_ref().as_ref();
-                let min: &ChunkedArray<$T> = min.as_ref().as_ref().as_ref();
-                let out = clip_min_max_helper(ca, min, clamp_min).into_series();
-                if original_type.is_logical(){
-                    out.cast(original_type)
-                }else{
-                    Ok(out)
-                }
-            })
-        },
-        dt => polars_bail!(opq = clippy_min, dt),
     }
 }
--- crates/polars-plan/src/dsl/function_expr/clip.rs
+++ crates/polars-plan/src/dsl/function_expr/clip.rs
@@ -2,9 +2,9 @@ use super::*;
 
 pub(super) fn clip(s: &[Series], has_min: bool, has_max: bool) -> PolarsResult<Series> {
     match (has_min, has_max) {
-        (true, true) => polars_ops::prelude::clip(&s[0], &s[1], &s[2]),
-        (true, false) => polars_ops::prelude::clip_min(&s[0], &s[1]),
-        (false, true) => polars_ops::prelude::clip_max(&s[0], &s[1]),
+        (true, true) => polars_ops::series::clip(&s[0], &s[1], &s[2]),
+        (true, false) => polars_ops::series::clip_min(&s[0], &s[1]),
+        (false, true) => polars_ops::series::clip_max(&s[0], &s[1]),
         _ => unreachable!(),
     }
 }
