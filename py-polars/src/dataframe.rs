c6535a18c97b21fdd6511aa4437181650323998e
Marshall Crumiller <mcrumiller@gmail.com>
2024-02-15 20:37:30 -0500

    Ensure series are contiguous

--- py-polars/src/dataframe.rs
+++ py-polars/src/dataframe.rs
@@ -1371,7 +1371,11 @@ impl PyDataFrame {
     }
 
     #[pyo3(signature = (keep_names_as, column_names))]
-    pub fn transpose(&self, keep_names_as: Option<&str>, column_names: &PyAny) -> PyResult<Self> {
+    pub fn transpose(
+        &mut self,
+        keep_names_as: Option<&str>,
+        column_names: &PyAny,
+    ) -> PyResult<Self> {
         let new_col_names = if let Ok(name) = column_names.extract::<Vec<String>>() {
             Some(Either::Right(name))
         } else if let Ok(name) = column_names.extract::<String>() {
