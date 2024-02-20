bf87d2898b9e305fe3109701aeb62d1e4e68c0c8
Basheer Subei <6508762+bsubei@users.noreply.github.com>
2024-02-20 05:09:37 -0500

    docs(rust): Add missing 'string' column in reading-writing Rust example to match Python example (#14597)

--- docs/src/rust/user-guide/getting-started/reading-writing.rs
+++ docs/src/rust/user-guide/getting-started/reading-writing.rs
@@ -13,7 +13,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 NaiveDate::from_ymd_opt(2025, 1, 2).unwrap().and_hms_opt(0, 0, 0).unwrap(),
                 NaiveDate::from_ymd_opt(2025, 1, 3).unwrap().and_hms_opt(0, 0, 0).unwrap(),
         ],
-        "float" => &[4.0, 5.0, 6.0]
+        "float" => &[4.0, 5.0, 6.0],
+        "string" => &["a", "b", "c"],
     )
     .unwrap();
     println!("{}", df);
