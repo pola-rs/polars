f87a5e2c1c0e7b983e8c27247822302ce75ec2e8
Mikhail Sozin <mikhail.sozin@chainstack.com>
2024-02-29 18:57:12 +0100

    Fixed bug with conversation Fixed docstring for end methods Added tests

--- py-polars/tests/unit/namespaces/test_datetime.py
+++ py-polars/tests/unit/namespaces/test_datetime.py
@@ -243,6 +243,61 @@ def test_month_start_datetime(
     result = ser.dt.month_start().item()
     assert result == expected.replace(tzinfo=tzinfo)
 
+@pytest.mark.parametrize(
+    ("dt", "expected"),
+    [
+        (datetime(2022, 3, 15, 3), datetime(2022, 1, 1, 3)),
+        (datetime(2022, 3, 15, 3, 2, 1, 123000), datetime(2022, 1, 1, 3, 2, 1, 123000)),
+        (datetime(2022, 3, 15), datetime(2022, 1, 1)),
+        (datetime(2022, 1, 1), datetime(2022, 1, 1)),
+    ],
+)
+@pytest.mark.parametrize(
+    ("tzinfo", "time_zone"),
+    [
+        (None, None),
+        (ZoneInfo("Asia/Kathmandu"), "Asia/Kathmandu"),
+    ],
+)
+@pytest.mark.parametrize("time_unit", ["ms", "us", "ns"])
+def test_quarter_start_datetime(
+    dt: datetime,
+    expected: datetime,
+    time_unit: TimeUnit,
+    tzinfo: ZoneInfo | None,
+    time_zone: str | None,
+) -> None:
+    ser = pl.Series([dt]).dt.replace_time_zone(time_zone).dt.cast_time_unit(time_unit)
+    result = ser.dt.quarter_start().item()
+    assert result == expected.replace(tzinfo=tzinfo)
+
+@pytest.mark.parametrize(
+    ("dt", "expected"),
+    [
+        (datetime(2022, 3, 15, 3), datetime(2022, 1, 1, 3)),
+        (datetime(2022, 3, 15, 3, 2, 1, 123000), datetime(2022, 1, 1, 3, 2, 1, 123000)),
+        (datetime(2022, 3, 15), datetime(2022, 1, 1)),
+        (datetime(2022, 1, 1), datetime(2022, 1, 1)),
+    ],
+)
+@pytest.mark.parametrize(
+    ("tzinfo", "time_zone"),
+    [
+        (None, None),
+        (ZoneInfo("Asia/Kathmandu"), "Asia/Kathmandu"),
+    ],
+)
+@pytest.mark.parametrize("time_unit", ["ms", "us", "ns"])
+def test_year_start_datetime(
+    dt: datetime,
+    expected: datetime,
+    time_unit: TimeUnit,
+    tzinfo: ZoneInfo | None,
+    time_zone: str | None,
+) -> None:
+    ser = pl.Series([dt]).dt.replace_time_zone(time_zone).dt.cast_time_unit(time_unit)
+    result = ser.dt.year_start().item()
+    assert result == expected.replace(tzinfo=tzinfo)
 
 @pytest.mark.parametrize(
     ("dt", "expected"),
@@ -254,6 +309,33 @@ def test_month_start_datetime(
 def test_month_start_date(dt: date, expected: date) -> None:
     ser = pl.Series([dt])
     result = ser.dt.month_start().item()
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    ("dt", "expected"),
+    [
+        (date(2022, 3, 15), date(2022, 1, 1)),
+        (date(2022, 4, 1), date(2022, 4, 1)),
+        (date(2022, 12, 31), date(2022, 10, 1)),
+    ],
+)
+def test_quarter_start_date(dt: date, expected: date) -> None:
+    ser = pl.Series([dt])
+    result = ser.dt.quarter_start().item()
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    ("dt", "expected"),
+    [
+        (date(2022, 3, 15), date(2022, 1, 1)),
+        (date(2023, 12, 31), date(2023, 1, 1)),
+    ],
+)
+def test_year_start_date(dt: date, expected: date) -> None:
+    ser = pl.Series([dt])
+    result = ser.dt.year_start().item()
     assert result == expected
 
 
@@ -288,6 +370,68 @@ def test_month_end_datetime(
     result = ser.dt.month_end().item()
     assert result == expected.replace(tzinfo=tzinfo)
 
+@pytest.mark.parametrize(
+    ("dt", "expected"),
+    [
+        (datetime(2022, 1, 15, 3), datetime(2022, 3, 31, 3)),
+        (
+            datetime(2022, 3, 15, 3, 2, 1, 123000),
+            datetime(2022, 3, 31, 3, 2, 1, 123000),
+        ),
+        (datetime(2022, 3, 15), datetime(2022, 3, 31)),
+        (datetime(2022, 3, 31), datetime(2022, 3, 31)),
+        (datetime(2022, 12, 31), datetime(2022, 12, 31)),
+    ],
+)
+@pytest.mark.parametrize(
+    ("tzinfo", "time_zone"),
+    [
+        (None, None),
+        (ZoneInfo("Asia/Vladivostok"), "Asia/Vladivostok"),
+    ],
+)
+@pytest.mark.parametrize("time_unit", ["ms", "us", "ns"])
+def test_quarter_end_datetime(
+    dt: datetime,
+    expected: datetime,
+    time_unit: TimeUnit,
+    tzinfo: ZoneInfo | None,
+    time_zone: str | None,
+) -> None:
+    ser = pl.Series([dt]).dt.replace_time_zone(time_zone).dt.cast_time_unit(time_unit)
+    result = ser.dt.quarter_end().item()
+    assert result == expected.replace(tzinfo=tzinfo)
+
+@pytest.mark.parametrize(
+    ("dt", "expected"),
+    [
+        (datetime(2022, 3, 15, 3), datetime(2022, 12, 31, 3)),
+        (
+            datetime(2022, 3, 15, 3, 2, 1, 123000),
+            datetime(2022, 12, 31, 3, 2, 1, 123000),
+        ),
+        (datetime(2022, 3, 15), datetime(2022, 12, 31)),
+        (datetime(2022, 12, 31), datetime(2022, 12, 31)),
+    ],
+)
+@pytest.mark.parametrize(
+    ("tzinfo", "time_zone"),
+    [
+        (None, None),
+        (ZoneInfo("Asia/Kathmandu"), "Asia/Kathmandu"),
+    ],
+)
+@pytest.mark.parametrize("time_unit", ["ms", "us", "ns"])
+def test_year_end_datetime(
+    dt: datetime,
+    expected: datetime,
+    time_unit: TimeUnit,
+    tzinfo: ZoneInfo | None,
+    time_zone: str | None,
+) -> None:
+    ser = pl.Series([dt]).dt.replace_time_zone(time_zone).dt.cast_time_unit(time_unit)
+    result = ser.dt.year_end().item()
+    assert result == expected.replace(tzinfo=tzinfo)
 
 @pytest.mark.parametrize(
     ("dt", "expected"),
@@ -302,7 +446,35 @@ def test_month_end_date(dt: date, expected: date) -> None:
     assert result == expected
 
 
-def test_month_start_end_invalid() -> None:
+@pytest.mark.parametrize(
+    ("dt", "expected"),
+    [
+        (date(2022, 3, 15), date(2022, 3, 31)),
+        (date(2022, 4, 30), date(2022, 6, 30)),
+        (date(2022, 12, 31), date(2022, 12, 31)),
+    ],
+)
+def test_quarter_end_date(dt: date, expected: date) -> None:
+    ser = pl.Series([dt])
+    result = ser.dt.quarter_end().item()
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    ("dt", "expected"),
+    [
+        (date(2022, 3, 15), date(2022, 12, 31)),
+        (date(2022, 3, 31), date(2022, 12, 31)),
+        (date(2023, 1, 1), date(2023, 12, 31)),
+    ],
+)
+def test_year_end_date(dt: date, expected: date) -> None:
+    ser = pl.Series([dt])
+    result = ser.dt.year_end().item()
+    assert result == expected
+
+
+def test_date_start_end_invalid() -> None:
     ser = pl.Series([time(1, 2, 3)])
     with pytest.raises(
         InvalidOperationError,
@@ -314,6 +486,26 @@ def test_month_start_end_invalid() -> None:
         match=r"`month_end` operation not supported for dtype `time` \(expected: date/datetime\)",
     ):
         ser.dt.month_end()
+    with pytest.raises(
+        InvalidOperationError,
+        match=r"`quarter_start` operation not supported for dtype `time` \(expected: date/datetime\)",
+    ):
+        ser.dt.quarter_start()
+    with pytest.raises(
+        InvalidOperationError,
+        match=r"`quarter_end` operation not supported for dtype `time` \(expected: date/datetime\)",
+    ):
+        ser.dt.quarter_end()
+    with pytest.raises(
+        InvalidOperationError,
+        match=r"`year_start` operation not supported for dtype `time` \(expected: date/datetime\)",
+    ):
+        ser.dt.year_start()
+    with pytest.raises(
+        InvalidOperationError,
+        match=r"`year_end` operation not supported for dtype `time` \(expected: date/datetime\)",
+    ):
+        ser.dt.year_end()
 
 
 @pytest.mark.parametrize("time_unit", ["ms", "us", "ns"])
