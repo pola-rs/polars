c25d366a90393f9a2017d85ee7040be6f87585a5
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-01 11:51:37 +0000

    docstring, note as unstable, rename param

--- py-polars/polars/plugins.py
+++ py-polars/polars/plugins.py
@@ -1,18 +1,34 @@
 from __future__ import annotations
 
-import os
-from typing import Any
+from pathlib import Path
+
+from polars._utils.unstable import unstable
 
 __all__ = ["get_shared_lib_location"]
 
 
-def get_shared_lib_location(main_file: Any) -> str:
-    """Get location of Shared Object file."""
-    directory = os.path.dirname(main_file)  # noqa: PTH120
-    return os.path.join(  # noqa: PTH118
-        directory, next(filter(_is_shared_lib, os.listdir(directory)))
-    )
+@unstable()
+def get_shared_lib_location(package_init_path: str) -> str:
+    """
+    Get location of Shared Object file.
+
+    .. warning::
+        This functionality is considered **unstable**. It may be changed
+        at any point without it being considered a breaking change.
+
+    Parameters
+    ----------
+    package_init_path
+        The ``__init__.py`` file of the plugin package.
+
+    Returns
+    -------
+    str
+        The location of the Shared Object file.
+    """
+    directory = Path(package_init_path).parent
+    return str(directory / next(filter(_is_shared_lib, directory.iterdir())))
 
 
-def _is_shared_lib(file: str) -> bool:
-    return file.endswith((".so", ".dll", ".pyd"))
+def _is_shared_lib(file: Path) -> bool:
+    return file.name.endswith((".so", ".dll", ".pyd"))
