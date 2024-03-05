93dc8dd723d075cbe24d1451cf747ebe38f8f0c9
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-05 11:49:09 +0000

    make register_plugin public instead of _get_shared_lib_location

--- py-polars/polars/plugins.py
+++ py-polars/polars/plugins.py
@@ -3,35 +3,115 @@ from __future__ import annotations
 from pathlib import Path
 
 from polars._utils.unstable import unstable
+from polars._utils.parse_expr_input import parse_as_list_of_expressions
+from polars._utils.wrap import wrap_expr
 
-__all__ = ["get_shared_lib_location"]
+__all__ = ["register_plugin"]
 
 
-@unstable()
-def get_shared_lib_location(package_init_path: str) -> str:
-    """
-    Get location of Shared Object file.
-
-    See the `user guide <https://docs.pola.rs/user-guide/expressions/plugins/>`_
-    for more information about plugins.
-
-    .. warning::
-        This functionality is considered **unstable**. It may be changed
-        at any point without it being considered a breaking change.
-
-    Parameters
-    ----------
-    package_init_path
-        The ``__init__.py`` file of the plugin package.
-
-    Returns
-    -------
-    str
-        The location of the Shared Object file.
-    """
-    directory = Path(package_init_path).parent
-    return str(next(path for path in directory.iterdir() if _is_shared_lib(path)))
+def _get_shared_lib_location(package_init_path: str) -> str:
+    """Get location of dynamic library file."""
+    if Path(package_init_path).is_file():
+        package_dir = Path(package_init_path).parent
+    else:
+        package_dir = Path(package_init_path)
+    for path in package_dir.iterdir():
+        if _is_shared_lib(path) and path.name.startswith(package_dir.name):
+            return str(path)
+    raise ValueError(f"No shared library found in {package_dir}")
 
 
 def _is_shared_lib(file: Path) -> bool:
     return file.name.endswith((".so", ".dll", ".pyd"))
+
+@unstable()
+def register_plugin(
+    args: IntoExpr | list[IntoExpr],
+    *,
+    plugin_location: str | Path,
+    symbol: str,
+    kwargs: dict[Any, Any] | None = None,
+    is_elementwise: bool = False,
+    input_wildcard_expansion: bool = False,
+    returns_scalar: bool = False,
+    cast_to_supertypes: bool = False,
+    pass_name_to_apply: bool = False,
+    changes_length: bool = False,
+) -> pl.Expr:
+    """
+    Register a shared library as a plugin.
+
+    .. warning::
+        This is highly unsafe as this will call the C function
+        loaded by `lib::symbol`.
+
+        The parameters you give dictate how polars will deal
+        with the function. Make sure they are correct!
+
+    .. note::
+        This functionality is unstable and may change without it
+        being considered breaking.
+
+    See the `user guide <https://docs.pola.rs/user-guide/expressions/plugins/>`_
+    for more information about plugins.
+
+    Parameters
+    ----------
+    args
+        Arguments passed to this function. These get passed to the ``inputs``
+        argument on the Rust side, and have to be of type Expression (or be
+        convertible to expressions).
+    plugin_location
+        Path to package where plugin is located. This can either be the
+        ``__init__.py`` file or the directory containing the ``__init__.py`` file.
+    symbol
+        Function to load.
+    kwargs
+        Non-expression arguments. They must be JSON serializable.
+    is_elementwise
+        If the function only operates on scalars
+        this will trigger fast paths.
+    input_wildcard_expansion
+        Expand expressions as input of this function.
+    returns_scalar
+        Automatically explode on unit length if it ran as final aggregation.
+        this is the case for aggregations like `sum`, `min`, `covariance` etc.
+    cast_to_supertypes
+        Cast the input datatypes to their supertype.
+    pass_name_to_apply
+        if set, then the `Series` passed to the function in the group_by operation
+        will ensure the name is set. This is an extra heap allocation per group.
+    changes_length
+        For example a `unique` or a `slice`
+    
+    Returns
+    -------
+    pl.Expr
+    """
+    if not args:
+        raise ValueError("`args` must be non-empty")
+    args = parse_as_list_of_expressions(args)
+    if kwargs is None:
+        serialized_kwargs = b""
+    else:
+        import pickle
+
+        # Choose the highest protocol supported by https://docs.rs/serde-pickle/latest/serde_pickle/
+        serialized_kwargs = pickle.dumps(kwargs, protocol=5)
+    
+    lib_location = _get_shared_lib_location(plugin_location)
+    
+    return wrap_expr(
+        args[0].register_plugin(
+            lib_location,
+            symbol,
+            args[1:],
+            serialized_kwargs,
+            is_elementwise,
+            input_wildcard_expansion,
+            returns_scalar,
+            cast_to_supertypes,
+            pass_name_to_apply,
+            changes_length,
+        )
+    )
