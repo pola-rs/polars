477651d2756e8e97ef8a7c5d3566cc1a1b680069
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-03-11 08:31:02 +0000

    remove some unsafes not in hot loops

--- crates/polars-ops/src/chunked_array/datetime/replace_time_zone.rs
+++ crates/polars-ops/src/chunked_array/datetime/replace_time_zone.rs
@@ -19,8 +19,7 @@ pub fn replace_time_zone(
     let from_tz = parse_time_zone(from_time_zone)?;
     let to_tz = parse_time_zone(time_zone.unwrap_or("UTC"))?;
     if (from_tz == to_tz)
-        & ((from_tz == UTC)
-            | ((ambiguous.len() == 1) & (unsafe { ambiguous.get_unchecked(0) } == Some("raise"))))
+        & ((from_tz == UTC) | ((ambiguous.len() == 1) & (ambiguous.get(0) == Some("raise"))))
     {
         let mut out = datetime
             .0
@@ -40,7 +39,7 @@ pub fn replace_time_zone(
         TimeUnit::Nanoseconds => datetime_to_timestamp_ns,
     };
     let out = match ambiguous.len() {
-        1 => match unsafe { ambiguous.get_unchecked(0) } {
+        1 => match ambiguous.get(0) {
             Some(ambiguous) => datetime.0.try_apply(|timestamp| {
                 let ndt = timestamp_to_datetime(timestamp);
                 Ok(datetime_to_timestamp(convert_to_naive_local(
@@ -68,10 +67,7 @@ pub fn replace_time_zone(
         }),
     };
     let mut out = out?.into_datetime(datetime.time_unit(), time_zone.map(|x| x.to_string()));
-    if from_time_zone == "UTC"
-        && ambiguous.len() == 1
-        && unsafe { ambiguous.get_unchecked(0) } == Some("raise")
-    {
+    if from_time_zone == "UTC" && ambiguous.len() == 1 && ambiguous.get(0) == Some("raise") {
         // In general, the sortedness flag can't be preserved.
         // To be safe, we only do so in the simplest case when we know for sure that there is no "daylight savings weirdness" going on, i.e.:
         // - `from_tz` is guaranteed to not observe daylight savings time;
