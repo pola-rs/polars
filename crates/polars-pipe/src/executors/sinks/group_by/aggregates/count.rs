a3447a11975b73e58122867c099b3788f030324c
ritchie <ritchie46@gmail.com>
2024-03-14 11:20:56 +0100

    distuingish between len and count

--- crates/polars-pipe/src/executors/sinks/group_by/aggregates/count.rs
+++ crates/polars-pipe/src/executors/sinks/group_by/aggregates/count.rs
@@ -7,24 +7,28 @@ use polars_utils::unwrap::UnwrapUncheckedRelease;
 use super::*;
 use crate::operators::IdxSize;
 
-pub(crate) struct CountAgg {
+pub(crate) struct CountAgg<const INCLUDE_NULL: bool> {
     count: IdxSize,
 }
 
-impl CountAgg {
+impl<const INCLUDE_NULL: bool> CountAgg<INCLUDE_NULL> {
     pub(crate) fn new() -> Self {
         CountAgg { count: 0 }
     }
 }
 
-impl AggregateFn for CountAgg {
+impl<const INCLUDE_NULL: bool> AggregateFn for CountAgg<INCLUDE_NULL> {
     fn has_physical_agg(&self) -> bool {
         false
     }
 
     fn pre_agg(&mut self, _chunk_idx: IdxSize, item: &mut dyn ExactSizeIterator<Item = AnyValue>) {
         let item = unsafe { item.next().unwrap_unchecked_release() };
-        self.count += !matches!(item, AnyValue::Null) as IdxSize;
+        if INCLUDE_NULL {
+            self.count += 1;
+        } else {
+            self.count += !matches!(item, AnyValue::Null) as IdxSize;
+        }
     }
     fn pre_agg_ordered(
         &mut self,
