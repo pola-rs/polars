ddbb24cdaf3523c8e38e360d74e73ebfe61ba0df
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-03-15 22:30:28 +0100

    Implement date/time

--- crates/polars-core/src/series/any_value.rs
+++ crates/polars-core/src/series/any_value.rs
@@ -104,13 +104,11 @@ impl Series {
             DataType::Binary => any_values_to_binary(values, strict)?.into_series(),
             #[cfg(feature = "dtype-date")]
             DataType::Date => any_values_to_date(values, strict)?.into_series(),
+            #[cfg(feature = "dtype-time")]
+            DataType::Time => any_values_to_time(values, strict)?.into_series(),
             #[cfg(feature = "dtype-datetime")]
             DataType::Datetime(tu, tz) => any_values_to_primitive_nonstrict::<Int64Type>(values)
                 .into_datetime(*tu, (*tz).clone())
-                .into_series(),
-            #[cfg(feature = "dtype-time")]
-            DataType::Time => any_values_to_primitive_nonstrict::<Int64Type>(values)
-                .into_time()
                 .into_series(),
             #[cfg(feature = "dtype-duration")]
             DataType::Duration(tu) => any_values_to_primitive_nonstrict::<Int64Type>(values)
@@ -324,34 +322,44 @@ fn any_values_to_binary(values: &[AnyValue], strict: bool) -> PolarsResult<Binar
 
 #[cfg(feature = "dtype-date")]
 fn any_values_to_date(values: &[AnyValue], strict: bool) -> PolarsResult<DateChunked> {
-    fn any_values_to_date_strict(values: &[AnyValue]) -> PolarsResult<DateChunked> {
-        let mut builder = PrimitiveChunkedBuilder::<Int32Type>::new("", values.len());
-        for av in values {
-            match av {
-                AnyValue::Date(i) => builder.append_value(*i),
-                AnyValue::Null => builder.append_null(),
-                av => return Err(invalid_value_error(&DataType::Date, av)),
-            }
-        }
-        Ok(builder.finish().into_date())
-    }
-    fn any_values_to_date_nonstrict(values: &[AnyValue]) -> DateChunked {
-        let mapper = |av: &AnyValue| match av {
-            AnyValue::Date(i) => Some(*i),
-            AnyValue::Null => None,
-            av => match av.cast(&DataType::Date) {
-                AnyValue::Date(i) => Some(i),
-                _ => None,
+    let mut builder = PrimitiveChunkedBuilder::<Int32Type>::new("", values.len());
+    for av in values {
+        match av {
+            AnyValue::Date(i) => builder.append_value(*i),
+            AnyValue::Null => builder.append_null(),
+            av => {
+                if strict {
+                    return Err(invalid_value_error(&DataType::Date, av));
+                }
+                match av.cast(&DataType::Date) {
+                    AnyValue::Date(i) => builder.append_value(i),
+                    _ => builder.append_null(),
+                }
             },
-        };
-        let ca: Int32Chunked = values.iter().map(mapper).collect_trusted();
-        ca.into_date()
+        }
     }
-    if strict {
-        any_values_to_date_strict(values)
-    } else {
-        Ok(any_values_to_date_nonstrict(values))
+    Ok(builder.finish().into())
+}
+
+#[cfg(feature = "dtype-time")]
+fn any_values_to_time(values: &[AnyValue], strict: bool) -> PolarsResult<TimeChunked> {
+    let mut builder = PrimitiveChunkedBuilder::<Int64Type>::new("", values.len());
+    for av in values {
+        match av {
+            AnyValue::Time(i) => builder.append_value(*i),
+            AnyValue::Null => builder.append_null(),
+            av => {
+                if strict {
+                    return Err(invalid_value_error(&DataType::Time, av));
+                }
+                match av.cast(&DataType::Time) {
+                    AnyValue::Time(i) => builder.append_value(i),
+                    _ => builder.append_null(),
+                }
+            },
+        }
     }
+    Ok(builder.finish().into())
 }
 
 #[cfg(feature = "dtype-categorical")]
