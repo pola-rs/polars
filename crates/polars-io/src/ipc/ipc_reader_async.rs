49330400b84d5fff0cc436b1f599c756af00aed5
Mick van Gelderen <mickvangelderen@gmail.com>
2024-03-15 13:38:27 +0100

    refactor(rust): Use ObjectStore instead of AsyncRead in parquet get metadata (#15069)

--- crates/polars-io/src/ipc/ipc_reader_async.rs
+++ crates/polars-io/src/ipc/ipc_reader_async.rs
@@ -1,72 +1,17 @@
-use std::ops::Range;
 use std::sync::Arc;
 
 use arrow::io::ipc::read::{get_row_count, FileMetadata, OutOfSpecKind};
-use bytes::Bytes;
 use object_store::path::Path;
-use object_store::{ObjectMeta, ObjectStore};
+use object_store::ObjectMeta;
 use polars_core::datatypes::IDX_DTYPE;
 use polars_core::frame::DataFrame;
 use polars_core::schema::Schema;
 use polars_error::{polars_bail, polars_err, to_compute_err, PolarsResult};
 
-use crate::cloud::{build_object_store, CloudLocation, CloudOptions};
-use crate::pl_async::{
-    tune_with_concurrency_budget, with_concurrency_budget, MAX_BUDGET_PER_REQUEST,
-};
+use crate::cloud::{build_object_store, CloudLocation, CloudOptions, PolarsObjectStore};
 use crate::predicates::PhysicalIoExpr;
 use crate::prelude::{materialize_projection, IpcReader};
 use crate::RowIndex;
-
-/// Polars specific wrapper for `Arc<dyn ObjectStore>` that limits the number of
-/// concurrent requests for the entire application.
-#[derive(Debug, Clone)]
-pub struct PolarsObjectStore(Arc<dyn ObjectStore>);
-
-impl PolarsObjectStore {
-    pub fn new(store: Arc<dyn ObjectStore>) -> Self {
-        Self(store)
-    }
-
-    pub async fn get(&self, path: &Path) -> PolarsResult<Bytes> {
-        tune_with_concurrency_budget(1, || async {
-            self.0
-                .get(path)
-                .await
-                .map_err(to_compute_err)?
-                .bytes()
-                .await
-                .map_err(to_compute_err)
-        })
-        .await
-    }
-
-    pub async fn get_range(&self, path: &Path, range: Range<usize>) -> PolarsResult<Bytes> {
-        tune_with_concurrency_budget(1, || self.0.get_range(path, range))
-            .await
-            .map_err(to_compute_err)
-    }
-
-    pub async fn get_ranges(
-        &self,
-        path: &Path,
-        ranges: &[Range<usize>],
-    ) -> PolarsResult<Vec<Bytes>> {
-        tune_with_concurrency_budget(
-            (ranges.len() as u32).clamp(0, MAX_BUDGET_PER_REQUEST as u32),
-            || self.0.get_ranges(path, ranges),
-        )
-        .await
-        .map_err(to_compute_err)
-    }
-
-    /// Fetch the metadata of the parquet file, do not memoize it.
-    pub async fn head(&self, path: &Path) -> PolarsResult<ObjectMeta> {
-        with_concurrency_budget(1, || self.0.head(path))
-            .await
-            .map_err(to_compute_err)
-    }
-}
 
 /// An Arrow IPC reader implemented on top of PolarsObjectStore.
 pub struct IpcReaderAsync {
