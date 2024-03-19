f4a901b590bac0dd96ab20e9b607405f23089da7
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-03-19 11:20:34 +0100

    fixup!

--- py-polars/polars/convert.py
+++ py-polars/polars/convert.py
@@ -529,50 +529,6 @@ def from_pandas(
         raise TypeError(msg)
 
 
-def from_dataframe(df: SupportsInterchange, *, allow_copy: bool = True) -> DataFrame:
-    """
-    Build a Polars DataFrame from any dataframe supporting the interchange protocol.
-
-    Parameters
-    ----------
-    df
-        Object supporting the dataframe interchange protocol, i.e. must have implemented
-        the `__dataframe__` method.
-    allow_copy
-        Allow memory to be copied to perform the conversion. If set to False, causes
-        conversions that are not zero-copy to fail.
-
-    Notes
-    -----
-    Details on the Python dataframe interchange protocol:
-    https://data-apis.org/dataframe-protocol/latest/index.html
-
-    Using a dedicated function like :func:`from_pandas` or :func:`from_arrow` is a more
-    efficient method of conversion.
-
-    Examples
-    --------
-    Convert a pandas dataframe to Polars through the interchange protocol.
-
-    >>> import pandas as pd
-    >>> df_pd = pd.DataFrame({"a": [1, 2], "b": [3.0, 4.0], "c": ["x", "y"]})
-    >>> dfi = df_pd.__dataframe__()
-    >>> pl.from_dataframe(dfi)
-    shape: (2, 3)
-    ┌─────┬─────┬─────┐
-    │ a   ┆ b   ┆ c   │
-    │ --- ┆ --- ┆ --- │
-    │ i64 ┆ f64 ┆ str │
-    ╞═════╪═════╪═════╡
-    │ 1   ┆ 3.0 ┆ x   │
-    │ 2   ┆ 4.0 ┆ y   │
-    └─────┴─────┴─────┘
-    """
-    from polars.interchange.from_dataframe import from_dataframe
-
-    return from_dataframe(df, allow_copy=allow_copy)
-
-
 @deprecate_renamed_parameter("tbl", "data", version="0.20.17")
 def from_repr(data: str) -> DataFrame | Series:
     """
@@ -776,3 +732,47 @@ def _from_series_repr(m: re.Match[str]) -> Series:
         return _cast_repr_strings_with_schema(
             srs.to_frame(), schema={srs.name: dtype}
         ).to_series()
+
+
+def from_dataframe(df: SupportsInterchange, *, allow_copy: bool = True) -> DataFrame:
+    """
+    Build a Polars DataFrame from any dataframe supporting the interchange protocol.
+
+    Parameters
+    ----------
+    df
+        Object supporting the dataframe interchange protocol, i.e. must have implemented
+        the `__dataframe__` method.
+    allow_copy
+        Allow memory to be copied to perform the conversion. If set to False, causes
+        conversions that are not zero-copy to fail.
+
+    Notes
+    -----
+    Details on the Python dataframe interchange protocol:
+    https://data-apis.org/dataframe-protocol/latest/index.html
+
+    Using a dedicated function like :func:`from_pandas` or :func:`from_arrow` is a more
+    efficient method of conversion.
+
+    Examples
+    --------
+    Convert a pandas dataframe to Polars through the interchange protocol.
+
+    >>> import pandas as pd
+    >>> df_pd = pd.DataFrame({"a": [1, 2], "b": [3.0, 4.0], "c": ["x", "y"]})
+    >>> dfi = df_pd.__dataframe__()
+    >>> pl.from_dataframe(dfi)
+    shape: (2, 3)
+    ┌─────┬─────┬─────┐
+    │ a   ┆ b   ┆ c   │
+    │ --- ┆ --- ┆ --- │
+    │ i64 ┆ f64 ┆ str │
+    ╞═════╪═════╪═════╡
+    │ 1   ┆ 3.0 ┆ x   │
+    │ 2   ┆ 4.0 ┆ y   │
+    └─────┴─────┴─────┘
+    """
+    from polars.interchange.from_dataframe import from_dataframe
+
+    return from_dataframe(df, allow_copy=allow_copy)
