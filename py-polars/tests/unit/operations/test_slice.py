3f7515456d61054bff02bc74b88fe85c29efaa98
petrosbar <pbarb@math.uoa.gr>
2024-03-26 08:23:22 +0200

    fix(python): Handle special case correctly when slicing a `LazyFrame`

--- py-polars/tests/unit/operations/test_slice.py
+++ py-polars/tests/unit/operations/test_slice.py
@@ -92,6 +92,8 @@ def test_python_slicing_lazy_frame() -> None:
         # confirm frame slice matches python slice
         assert ldf[py_slice].collect().rows() == ldf.collect().rows()[py_slice]
 
+    assert_frame_equal(ldf[0::-1], ldf.head(1))
+    assert_frame_equal(ldf[2::-1], ldf.head(3).reverse())
     assert_frame_equal(ldf[::-1], ldf.reverse())
     assert_frame_equal(ldf[::-2], ldf.reverse().gather_every(2))
 
