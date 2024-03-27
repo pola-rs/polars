af81de014070e52d5a555cc25aa46380d05c349f
Ritchie Vink <ritchie46@gmail.com>
2024-03-27 21:33:07 +0100

    feat: CSE don't scan share if predicate pushdown predicates don't match (#15328)

--- crates/polars-plan/src/logical_plan/optimizer/mod.rs
+++ crates/polars-plan/src/logical_plan/optimizer/mod.rs
@@ -90,7 +90,7 @@ pub fn optimize(
     scratch: &mut Vec<Node>,
     hive_partition_eval: HiveEval<'_>,
 ) -> PolarsResult<Node> {
-    #[cfg(feature = "cse")]
+    #[allow(dead_code)]
     let verbose = verbose();
     // get toggle values
     let predicate_pushdown = opt_state.predicate_pushdown;
@@ -137,7 +137,7 @@ pub fn optimize(
     }
 
     #[cfg(feature = "cse")]
-    let cse_plan_changed =
+    let _cse_plan_changed =
         if comm_subplan_elim && members.has_joins_or_unions && members.has_duplicate_scans() {
             if verbose {
                 eprintln!("found multiple sources; run comm_subplan_elim")
@@ -153,7 +153,7 @@ pub fn optimize(
             false
         };
     #[cfg(not(feature = "cse"))]
-    let cse_plan_changed = false;
+    let _cse_plan_changed = false;
 
     // Should be run before predicate pushdown.
     if projection_pushdown {
@@ -211,7 +211,7 @@ pub fn optimize(
     lp_top = opt.optimize_loop(&mut rules, expr_arena, lp_arena, lp_top)?;
 
     if members.has_joins_or_unions && members.has_cache {
-        cache_states::set_cache_states(lp_top, lp_arena, expr_arena, scratch, cse_plan_changed)?;
+        cache_states::set_cache_states(lp_top, lp_arena, expr_arena, scratch, verbose)?;
     }
 
     // This one should run (nearly) last as this modifies the projections
@@ -227,7 +227,7 @@ pub fn optimize(
     // Make sure that we do that once slice pushdowd and predicate pushdown are done.
     // At that moment the file fingerprints are finished.
     #[cfg(any(feature = "cse", feature = "parquet", feature = "ipc", feature = "csv"))]
-    if agg_scan_projection && !cse_plan_changed {
+    if agg_scan_projection && !_cse_plan_changed {
         // We do this so that expressions, created by the pushdown optimizations, are simplified .
         // We must clean up the predicates, because the agg_scan_projection
         // uses them in the hashtable to determine duplicates.
@@ -240,7 +240,7 @@ pub fn optimize(
     }
 
     #[cfg(feature = "cse")]
-    if cse_plan_changed {
+    if _cse_plan_changed {
         // this must run after cse
         cse::decrement_file_counters_by_cache_hits(lp_top, lp_arena, expr_arena, 0, scratch);
     }
