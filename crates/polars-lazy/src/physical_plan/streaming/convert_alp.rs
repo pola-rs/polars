0587ed83d67ec97832911c4cebc3edbc317b0c08
ritchie <ritchie46@gmail.com>
2024-04-07 10:35:43 +0200

    fix: block decimal in streaming

--- crates/polars-lazy/src/physical_plan/streaming/convert_alp.rs
+++ crates/polars-lazy/src/physical_plan/streaming/convert_alp.rs
@@ -409,6 +409,8 @@ pub(crate) fn insert_streaming_nodes(
                             .all(|fld| allowed_dtype(fld.data_type(), string_cache)),
                         // We need to be able to sink to disk or produce the aggregate return dtype.
                         DataType::Unknown => false,
+                        #[cfg(feature = "dtype-decimal")]
+                        DataType::Decimal(_, _) => false,
                         _ => true,
                     }
                 }
