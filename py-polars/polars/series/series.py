a20e742709bbb2708266a9f6c18da95eaebd17ee
Lava <34743145+CanglongCl@users.noreply.github.com>
2024-04-07 23:19:38 -0700

    revert return type

--- py-polars/polars/series/series.py
+++ py-polars/polars/series/series.py
@@ -4953,7 +4953,7 @@ class Series:
         ]
         """
 
-    def dot(self, other: Series | ArrayLike) -> int | float:
+    def dot(self, other: Series | ArrayLike) -> int | float | None:
         """
         Compute the dot/inner product between two Series.
 
