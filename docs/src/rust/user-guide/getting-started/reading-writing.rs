45620efc5b7d49a6cf2f6f4081d4aa60c3ba3bb2
Priyansh Shah <105874877+Priyansh4444@users.noreply.github.com>
2024-04-15 03:11:40 -0700

    docs(rust): Include prelude import in the example (#15633)

--- docs/src/rust/user-guide/getting-started/reading-writing.rs
+++ docs/src/rust/user-guide/getting-started/reading-writing.rs
@@ -1,10 +1,9 @@
-use polars::prelude::*;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // --8<-- [start:dataframe]
     use std::fs::File;
 
     use chrono::prelude::*;
+    use polars::prelude::*;
 
     let mut df: DataFrame = df!(
         "integer" => &[1, 2, 3],
