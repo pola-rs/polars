25c266824be4db6ec6e455407a02aeafec2827e3
ritchie <ritchie46@gmail.com>
2024-04-19 09:57:43 +0200

    feat(rust!, python): Add context trace to LazyFrame conversion errors

--- py-polars/polars/lazyframe/frame.py
+++ py-polars/polars/lazyframe/frame.py
@@ -526,15 +526,7 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
 
     def __repr__(self) -> str:
         # don't expose internal/private classpath
-        width = self.width
-        cols_str = "{} col{}".format(width, "" if width == 1 else "s")
-        schema_max_2 = (
-            item for i, item in enumerate(self.schema.items()) if i in (0, width - 1)
-        )
-        schema_str = (", " if width == 2 else " … ").join(
-            (f'"{k}": {v}' for k, v in schema_max_2)
-        )
-        return f"<{self.__class__.__name__} [{cols_str}, {{{schema_str}}}] at 0x{id(self):X}>"
+        return f"<{self.__class__.__name__} at 0x{id(self):X}>"
 
     def _repr_html_(self) -> str:
         try:
