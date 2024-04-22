57ed0a65a605c4c5821653ca0c5349424f2492d6
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-04-22 22:33:36 +0200

    Fix owned readers py-polars

--- crates/polars-io/src/csv/read/mod.rs
+++ crates/polars-io/src/csv/read/mod.rs
@@ -8,6 +8,8 @@ mod utils;
 
 pub use options::{CommentPrefix, CsvEncoding, CsvParserOptions, NullValues};
 pub use parser::count_rows;
-pub use read_impl::{BatchedCsvReaderMmap, BatchedCsvReaderRead};
+pub use read_impl::{
+    BatchedCsvReaderMmap, BatchedCsvReaderRead, OwnedBatchedCsvReader, OwnedBatchedCsvReaderMmap,
+};
 pub use reader::CsvReader;
 pub use utils::{infer_file_schema, is_compressed};
--- crates/polars-io/src/csv/read/read_impl/mod.rs
+++ crates/polars-io/src/csv/read/read_impl/mod.rs
@@ -2,8 +2,8 @@ mod batched_mmap;
 mod batched_read;
 mod core_reader;
 
-pub use batched_mmap::BatchedCsvReaderMmap;
-pub(super) use batched_mmap::{to_batched_owned_mmap, OwnedBatchedCsvReaderMmap};
-pub use batched_read::BatchedCsvReaderRead;
-pub(super) use batched_read::{to_batched_owned_read, OwnedBatchedCsvReader};
+pub(super) use batched_mmap::to_batched_owned_mmap;
+pub use batched_mmap::{BatchedCsvReaderMmap, OwnedBatchedCsvReaderMmap};
+pub(super) use batched_read::to_batched_owned_read;
+pub use batched_read::{BatchedCsvReaderRead, OwnedBatchedCsvReader};
 pub(super) use core_reader::CoreReader;
