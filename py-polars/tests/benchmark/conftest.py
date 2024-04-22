d40b7474e6c5f7832192ec3489bd14f11ebcca6b
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-04-22 10:46:23 +0200

    Bigger data

--- py-polars/tests/benchmark/conftest.py
+++ py-polars/tests/benchmark/conftest.py
@@ -6,4 +6,4 @@ from tests.benchmark.datagen_groupby import generate_group_by_data
 
 @pytest.fixture(scope="module")
 def groupby_data() -> pl.DataFrame:
-    return generate_group_by_data(1000, 10, null_ratio=0.1)
+    return generate_group_by_data(10_000, 100, null_ratio=0.05)
