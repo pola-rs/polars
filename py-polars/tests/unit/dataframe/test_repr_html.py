42643f40d1b580389ffb63105163ca8329f81e9e
lisphilar <7270139+lisphilar@users.noreply.github.com>
2024-04-23 02:36:57 +0900

    refactor: fix errors suggested by lint

--- py-polars/tests/unit/dataframe/test_repr_html.py
+++ py-polars/tests/unit/dataframe/test_repr_html.py
@@ -67,8 +67,11 @@ def test_df_repl_html_table_cell_alignment() -> None:
 
     # default: overall=RIGHT, numeric=RIGHT
     assert "text-align: right;" in df._repr_html()
-    assert '<thead><tr><th>a</th><th">b</th></tr>'\
-        '<tr><td>str</td><td>i64</td></tr></thead>' in df._repr_html_()
+    header = (
+        '<thead><tr><th>a</th><th">b</th></tr>'
+        '<tr><td>str</td><td>i64</td></tr></thead>'
+    )
+    assert header in df._repr_html_()
 
     # overall=RIGHT, numeric=LEFT
     for overall_alignment in ["LEFT", "CENTER", "RIGHT"]:
@@ -83,8 +86,10 @@ def test_df_repl_html_table_cell_alignment() -> None:
                 assert f"text-align: {_overall};" in _repr_html
                 if _overall == _numeric:
                     # header
-                    header = '<thead><tr><th>a</th><th">b</th></tr>'\
+                    header = (
+                        '<thead><tr><th>a</th><th">b</th></tr>'
                         '<tr><td>str</td><td">i64</td></tr></thead>'
+                    )
                     assert header in _repr_html
                     # body
                     body = '<tr><td>str</td><td">i64</td></tr>'
@@ -92,15 +97,15 @@ def test_df_repl_html_table_cell_alignment() -> None:
                 else:
                     # header
                     header = (
-                        '<thead><tr><th>a</th>'
+                        "<thead><tr><th>a</th>"
                         f'<th align="{_numeric}">b</th></tr>'
-                        '<tr><td>str</td>'
+                        "<tr><td>str</td>"
                         f'<td align="{_numeric}">i64</td></tr></thead>'
                     )
                     assert header in _repr_html
                     # body
                     body = (
-                        '<tr><td>str</td>'
+                        "<tr><td>str</td>"
                         f'<td align="{_numeric}">i64</td></tr>'
                     )
                     assert body in _repr_html
