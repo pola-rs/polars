f8f988eea03e940c6458be16609ecc0371ba89b5
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-04-23 09:10:31 +0200

    Update docs

--- py-polars/tests/benchmark/__init__.py
+++ py-polars/tests/benchmark/__init__.py
@@ -1,7 +1,8 @@
 """
 Benchmark tests.
 
-These tests are skipped by default as relatively large data must be generated first.
+These tests are skipped by default as a relatively large dataset must be generated
+first.
 
 See the documentation on how to run these tests:
 https://docs.pola.rs/development/contributing/test/#benchmark-tests
