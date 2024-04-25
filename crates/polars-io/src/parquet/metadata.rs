195efa37bd6d8117ecbf10b13a8291d11a01ff55
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-04-25 12:01:43 +0200

    Add back rexport

--- crates/polars-io/src/parquet/metadata.rs
+++ crates/polars-io/src/parquet/metadata.rs
@@ -3,4 +3,6 @@
 use std::sync::Arc;
 
 pub use polars_parquet::parquet::metadata::FileMetaData;
+pub use polars_parquet::read::statistics::{deserialize, Statistics as ParquetStatistics};
+
 pub type FileMetaDataRef = Arc<FileMetaData>;
