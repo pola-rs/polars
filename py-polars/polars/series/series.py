fix(python): converting from numpy datetime64 and overriding dtype with a different resolution was returning incorrect results

py-polars/polars/series/series.py
@@ -326,10 +326,14 @@ class Series:
             )
             if values.dtype.type in [np.datetime64, np.timedelta64]:
                 # cast to appropriate dtype, handling NaT values
+                input_dtype = _resolve_temporal_dtype(None, values.dtype)
+                # numpy dtype has already been validated in `numpy_to_pyseries`
+                assert input_dtype is not None
                 dtype = _resolve_temporal_dtype(dtype, values.dtype)
                 if dtype is not None:
                     self._s = (
-                        self.cast(dtype)
+                        self.cast(input_dtype)
+                        .cast(dtype)
                         .scatter(np.argwhere(np.isnat(values)).flatten(), None)
                         ._s
                     )