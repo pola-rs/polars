679940ec799a6df3d2d6980fa39d94699df21d17
Piriyakorn P <piriyakorn.piriyatamwong@inf.ethz.ch>
2024-05-02 21:07:28 +0200

    fix: raise ValueError when convert from pandas when index name duplicates column name (#15938) - add unit tests

---
+++ py-polars/tests/unit/dataframe/test_from_pandas.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import pytest
+
+import pandas as pd
+import polars as pl
+
+
+def test_from_pandas_exclude_index() -> None:
+    data = pd.DataFrame({"a": [1, 2], "b": [3, 4]}, index=pd.Index([5, 6], name="c"))
+    df = pl.from_pandas(data, include_index=False)
+    assert df.columns == ["a", "b"]
+    assert df.rows() == [(1, 3), (2, 4)]
+
+
+def test_from_pandas_include_index() -> None:
+    data = pd.DataFrame({"a": [1, 2], "b": [3, 4]}, index=pd.Index([5, 6], name="c"))
+    df = pl.from_pandas(data, include_index=True)
+    assert df.columns == ["c", "a", "b"]
+    assert df.rows() == [(5, 1, 3), (6, 2, 4)]
+
+
+def test_from_pandas_exclude_dup_index() -> None:
+    data = pd.DataFrame({"a": [1, 2], "b": [3, 4]}, index=pd.Index([5, 6], name="a"))
+    df = pl.from_pandas(data, include_index=False)
+    assert df.columns == ["a", "b"]
+    assert df.rows() == [(1, 3), (2, 4)]
+
+
+def test_from_pandas_include_dup_index() -> None:
+    data = pd.DataFrame({"a": [1, 2], "b": [3, 4]}, index=pd.Index([5, 6], name="a"))
+
+    with pytest.raises(ValueError):
+        pl.from_pandas(data, include_index=True)
