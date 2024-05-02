9a8425c5dc15eb657e60674f959d60e357a9d690
Zhengbo Wang <77875500+luke396@users.noreply.github.com>
2024-05-02 21:32:45 +0800

    Pass strict before

--- py-polars/polars/series/series.py
+++ py-polars/polars/series/series.py
@@ -350,7 +350,7 @@ class Series:
         elif _check_for_pandas(values) and isinstance(
             values, (pd.Series, pd.Index, pd.DatetimeIndex)
         ):
-            self._s = pandas_to_pyseries(name, values, dtype=dtype)
+            self._s = pandas_to_pyseries(name, values, dtype=dtype, strict=strict)
 
         elif _is_generator(values):
             self._s = iterable_to_pyseries(name, values, dtype=dtype, strict=strict)
