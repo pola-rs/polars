83f0f9f76a9d5b667c33cc329bd8432d4f1038fb
Carbonhell <frafonia@gmail.com>
2024-05-09 11:12:15 +0200

    fix: formatting

--- crates/polars-parquet/build.rs
+++ crates/polars-parquet/build.rs
@@ -2,10 +2,10 @@ use std::process::Command;
 
 fn main() {
     let output = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
+        .args(["rev-parse", "HEAD"])
         .output()
         .expect("`git` should be available");
-    let git_hash = String::from_utf8(output.stdout)
-        .expect("couldn't parse `git rev-parse HEAD` output");
+    let git_hash =
+        String::from_utf8(output.stdout).expect("couldn't parse `git rev-parse HEAD` output");
     println!("cargo:rustc-env=POLARS_GIT_HASH={}", git_hash);
 }
