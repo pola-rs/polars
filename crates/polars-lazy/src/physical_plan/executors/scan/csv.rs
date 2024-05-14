d2cfeac197eee7a28febc58573d39e63b2e99003
Simon Lin <simonlin.rqmmw@slmail.me>
2024-05-14 20:12:12 +1000

    fix skip rows and use accumulate vertical

--- crates/polars-lazy/src/physical_plan/executors/scan/csv.rs
+++ crates/polars-lazy/src/physical_plan/executors/scan/csv.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 use std::sync::Arc;
 
 use polars_core::config::verbose;
-use polars_core::utils::concat_df;
+use polars_core::utils::accumulate_dataframes_vertical;
 
 use super::*;
 
@@ -25,19 +25,16 @@ impl CsvExec {
 
         let n_rows = _set_n_rows_for_scan(self.file_options.n_rows);
         let predicate = self.predicate.clone().map(phys_expr_to_io_expr);
+        let options_base = self.options.clone();
         let options_base = self
             .options
             .clone()
-            .with_skip_rows_after_header(
-                // If we don't set it to 0 here, it will skip double the amount of rows.
-                // But if we set it to 0, it will still skip the requested amount of rows.
-                // The reason I currently cannot fathom.
-                // TODO: Find out why. Maybe has something to do with schema inference.
-                0,
+            .with_schema(
+                options_base
+                    .schema
+                    .is_some()
+                    .then(|| self.file_info.reader_schema.clone().unwrap().unwrap_right()),
             )
-            .with_schema(Some(
-                self.file_info.reader_schema.clone().unwrap().unwrap_right(),
-            ))
             .with_columns(with_columns)
             .with_rechunk(
                 // We rechunk at the end to avoid rechunking multiple times in the
@@ -49,121 +46,123 @@ impl CsvExec {
 
         let verbose = verbose();
 
-        let mut df =
-            if n_rows.is_some() || (predicate.is_some() && self.file_options.row_index.is_some()) {
-                // Basic sequential read
-                // predicate must be done after n_rows and row_index, so we must read sequentially
-                if verbose {
-                    eprintln!("read per-file to apply n_rows or (predicate + row_index)");
-                }
+        let mut df = if n_rows.is_some()
+            || (predicate.is_some() && self.file_options.row_index.is_some())
+        {
+            // Basic sequential read
+            // predicate must be done after n_rows and row_index, so we must read sequentially
+            if verbose {
+                eprintln!("read per-file to apply n_rows or (predicate + row_index)");
+            }
 
-                let mut n_rows_read = 0usize;
-                let mut out = Vec::with_capacity(self.paths.len());
-                // If we have n_rows or row_index then we need to count how many rows we read, so we need
-                // to delay applying the predicate.
-                let predicate_during_read = predicate
+            let mut n_rows_read = 0usize;
+            let mut out = Vec::with_capacity(self.paths.len());
+            // If we have n_rows or row_index then we need to count how many rows we read, so we need
+            // to delay applying the predicate.
+            let predicate_during_read = predicate
+                .clone()
+                .filter(|_| n_rows.is_none() && self.file_options.row_index.is_none());
+
+            for i in 0..self.paths.len() {
+                let path = &self.paths[i];
+
+                let df = options_base
                     .clone()
-                    .filter(|_| n_rows.is_none() && self.file_options.row_index.is_none());
+                    .with_row_index(self.file_options.row_index.clone().map(|mut ri| {
+                        ri.offset += n_rows_read as IdxSize;
+                        ri
+                    }))
+                    .with_n_rows(n_rows.map(|n| n - n_rows_read))
+                    .try_into_reader_with_file_path(Some(path.clone()))
+                    .unwrap()
+                    ._with_predicate(predicate_during_read.clone())
+                    .finish()?;
 
-                for i in 0..self.paths.len() {
-                    let path = &self.paths[i];
+                n_rows_read = n_rows_read.saturating_add(df.height());
 
-                    let df = options_base
-                        .clone()
-                        .with_row_index(self.file_options.row_index.clone().map(|mut ri| {
-                            ri.offset += n_rows_read as IdxSize;
-                            ri
-                        }))
-                        .with_n_rows(n_rows.map(|n| n - n_rows_read))
-                        .try_into_reader_with_file_path(Some(path.clone()))
-                        .unwrap()
-                        ._with_predicate(predicate_during_read.clone())
-                        .finish()?;
+                let df = if predicate.is_some() && predicate_during_read.is_none() {
+                    let predicate = predicate.clone().unwrap();
 
-                    n_rows_read = n_rows_read.saturating_add(df.height());
-
-                    let df = if predicate.is_some() && predicate_during_read.is_none() {
-                        let predicate = predicate.clone().unwrap();
-
-                        // We should have a chunked df since we read with rechunk false,
-                        // so we parallelize over row-wise batches.
-                        concat_df(
-                            POOL.install(|| {
-                                df.split_chunks()
-                                    .collect::<Vec<_>>()
-                                    .into_par_iter()
-                                    .map(|df| {
-                                        let s = predicate.evaluate_io(&df)?;
-                                        let mask = s
-                                            .bool()
-                                            .expect("filter predicates was not of type boolean");
-                                        df._filter_seq(mask)
-                                    })
-                                    .collect::<PolarsResult<Vec<_>>>()
-                            })?
-                            .iter(),
-                        )?
-                    } else {
-                        df
-                    };
-
-                    out.push(df);
-
-                    if n_rows.is_some() && n_rows_read == n_rows.unwrap() {
-                        if verbose {
-                            eprintln!(
-                                "reached n_rows = {} at file {} / {}",
-                                n_rows.unwrap(),
-                                1 + i,
-                                self.paths.len()
-                            )
-                        }
-                        break;
-                    }
-                }
-
-                concat_df(out.iter())?
-            } else {
-                // Basic parallel read
-                assert!(
-                    n_rows.is_none()
-                        && !(
-                            // We can do either but not both because we are doing them
-                            // out-of-order for parallel.
-                            predicate.is_some() && self.file_options.row_index.is_some()
-                        )
-                );
-                if verbose {
-                    eprintln!("read files in parallel")
-                }
-
-                let dfs = POOL.install(|| {
-                    self.paths
-                        .chunks(std::cmp::min(POOL.current_num_threads(), 128))
-                        .map(|paths| {
-                            paths
+                    // We should have a chunked df since we read with rechunk false,
+                    // so we parallelize over row-wise batches.
+                    accumulate_dataframes_vertical(
+                        POOL.install(|| {
+                            df.split_chunks()
+                                .collect::<Vec<_>>()
                                 .into_par_iter()
-                                .map(|path| {
-                                    options_base
-                                        .clone()
-                                        .try_into_reader_with_file_path(Some(path.clone()))
-                                        .unwrap()
-                                        ._with_predicate(predicate.clone())
-                                        .finish()
+                                .map(|df| {
+                                    let s = predicate.evaluate_io(&df)?;
+                                    let mask = s
+                                        .bool()
+                                        .expect("filter predicates was not of type boolean");
+                                    df.filter(mask)
                                 })
                                 .collect::<PolarsResult<Vec<_>>>()
-                        })
-                        .collect::<PolarsResult<Vec<_>>>()
-                })?;
+                        })?
+                        .into_iter(),
+                    )?
+                } else {
+                    df
+                };
 
-                let mut df = concat_df(dfs.iter().flat_map(|dfs| dfs.iter()))?;
+                out.push(df);
 
-                if let Some(row_index) = self.file_options.row_index.clone() {
-                    df.with_row_index_mut(row_index.name.as_ref(), Some(row_index.offset));
+                if n_rows.is_some() && n_rows_read == n_rows.unwrap() {
+                    if verbose {
+                        eprintln!(
+                            "reached n_rows = {} at file {} / {}",
+                            n_rows.unwrap(),
+                            1 + i,
+                            self.paths.len()
+                        )
+                    }
+                    break;
                 }
+            }
 
-                df
-            };
+            accumulate_dataframes_vertical(out.into_iter())?
+        } else {
+            // Basic parallel read
+            assert!(
+                n_rows.is_none()
+                    && !(
+                        // We can do either but not both because we are doing them
+                        // out-of-order for parallel.
+                        predicate.is_some() && self.file_options.row_index.is_some()
+                    )
+            );
+            if verbose {
+                eprintln!("read files in parallel")
+            }
+
+            let dfs = POOL.install(|| {
+                self.paths
+                    .chunks(std::cmp::min(POOL.current_num_threads(), 128))
+                    .map(|paths| {
+                        paths
+                            .into_par_iter()
+                            .map(|path| {
+                                options_base
+                                    .clone()
+                                    .try_into_reader_with_file_path(Some(path.clone()))
+                                    .unwrap()
+                                    ._with_predicate(predicate.clone())
+                                    .finish()
+                            })
+                            .collect::<PolarsResult<Vec<_>>>()
+                    })
+                    .collect::<PolarsResult<Vec<_>>>()
+            })?;
+
+            let mut df =
+                accumulate_dataframes_vertical(dfs.into_iter().flat_map(|dfs| dfs.into_iter()))?;
+
+            if let Some(row_index) = self.file_options.row_index.clone() {
+                df.with_row_index_mut(row_index.name.as_ref(), Some(row_index.offset));
+            }
+
+            df
+        };
 
         if self.file_options.rechunk {
             df.as_single_chunk_par();
