58e35d05718eb89ffa7417fbbe56dbd1eea82bc5
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-05-26 07:55:09 +0200

    Update some tests

--- py-polars/tests/unit/series/test_getitem.py
+++ py-polars/tests/unit/series/test_getitem.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
+from typing import Any
+
 import hypothesis.strategies as st
+import numpy as np
+import pytest
 from hypothesis import given
 
 import polars as pl
@@ -28,3 +32,21 @@ def test_series_getitem(
 
     assert sliced_py_data == sliced_pl_data, f"slice [{start}:{stop}:{step}] failed"
     assert_series_equal(srs, srs, check_exact=True)
+
+
+@pytest.mark.parametrize(
+    "mask",
+    [
+        [True, False, True],
+        pl.Series([True, False, True]),
+        np.array([True, False, True]),
+    ],
+)
+def test_series_getitem_boolean_mask(mask: Any) -> None:
+    s = pl.Series([1, 2, 3])
+    print(mask)
+    with pytest.raises(
+        TypeError,
+        match="selecting rows by passing a boolean mask to `__getitem__` is not supported",
+    ):
+        s[mask]
