dc72116c7e41f62ed4a428fdc4968becef13104e
ion-elgreco <15728914+ion-elgreco@users.noreply.github.com>
2024-05-26 16:43:06 +0200

    fix datetime, and other  predicates

--- crates/polars-plan/src/dsl/expr.rs
+++ crates/polars-plan/src/dsl/expr.rs
@@ -378,7 +378,9 @@ impl Operator {
                 | Self::Gt
                 | Self::GtEq
                 | Self::And
+                | Self::LogicalAnd
                 | Self::Or
+                | Self::LogicalOr
                 | Self::Xor
                 | Self::EqValidity
                 | Self::NotEqValidity
