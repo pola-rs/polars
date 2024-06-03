ac3ca3f777d6a401eb11fe8fcd7ac82e08d0b75a
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-06-04 00:03:09 +0200

    chore: Fix incorrect debug assertion in `ChunkedArray::from_chunks_and_dtype`

--- crates/polars-core/src/chunked_array/from.rs
+++ crates/polars-core/src/chunked_array/from.rs
@@ -207,7 +207,10 @@ where
         #[cfg(debug_assertions)]
         {
             if !chunks.is_empty() && dtype.is_primitive() {
-                assert_eq!(chunks[0].data_type(), &dtype.to_physical().to_arrow(true))
+                assert_eq!(
+                    chunks[0].data_type().underlying_physical_type(),
+                    dtype.to_arrow(true)
+                )
             }
         }
         let field = Arc::new(Field::new(name, dtype));
