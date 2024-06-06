e0b402cb9a6046d5d96d97c38c58bc307d549f99
Henrik Irgens Gravdal <47174810+henrikig@users.noreply.github.com>
2024-06-06 16:01:55 +0200

    fix(rust): boolean array distinct count

--- crates/polars-compute/src/distinct_count.rs
+++ crates/polars-compute/src/distinct_count.rs
@@ -8,17 +8,39 @@ pub trait DistinctCountKernel {
 
 impl DistinctCountKernel for BooleanArray {
     fn distinct_count(&self) -> usize {
-        if self.len() - self.null_count() == 0 {
+        let null_count = self.null_count();
+        if self.len() - null_count == 0 {
             return 0;
         }
 
-        if self.null_count() == 0 {
+        if null_count == 0 {
             let unset_bits = self.values().unset_bits();
             2 - usize::from(unset_bits == 0 || unset_bits == self.values().len())
         } else {
-            let values = self.values() & self.validity().unwrap();
-            let unset_bits = self.values().unset_bits();
-            3 - usize::from(unset_bits == 0 || unset_bits == values.len())
+            let set_bits = (self.values() & self.validity().unwrap()).set_bits();
+            2 - usize::from(set_bits == 0 || set_bits == self.values().len() - null_count)
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_distinct_count() {
+        let test_cases = vec![
+            (BooleanArray::from(vec![None, None]), 0),
+            (BooleanArray::from(vec![Some(true), Some(true)]), 1),
+            (BooleanArray::from(vec![Some(false), Some(false)]), 1),
+            (BooleanArray::from(vec![Some(true), Some(false)]), 2),
+            (BooleanArray::from(vec![Some(true), None]), 1),
+            (BooleanArray::from(vec![Some(false), None]), 1),
+            (BooleanArray::from(vec![Some(true), Some(false), None]), 2),
+        ];
+
+        for (input, expected) in test_cases {
+            assert_eq!(input.distinct_count(), expected);
         }
     }
 }
