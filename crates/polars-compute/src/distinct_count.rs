a0f8b7c5fbdc1e25ed0c115c5a1ca8133543878e
coastalwhite <me@gburghoorn.com>
2024-06-06 18:10:36 +0200

    refactor(rust): add distinct_non_null_count method

--- crates/polars-compute/src/distinct_count.rs
+++ crates/polars-compute/src/distinct_count.rs
@@ -1,13 +1,14 @@
 use arrow::array::{Array, BooleanArray};
 
 /// Kernel to calculate the number of unique elements
-///
-/// A null is also considered a unique value
 pub trait DistinctCountKernel {
     /// Calculate the number of unique elements in [`Self`]
     ///
     /// A null is also considered a unique value
     fn distinct_count(&self) -> usize;
+
+    /// Calculate the number of unique non-null elements in [`Self`]
+    fn distinct_non_null_count(&self) -> usize;
 }
 
 impl DistinctCountKernel for BooleanArray {
@@ -34,6 +35,11 @@ impl DistinctCountKernel for BooleanArray {
         let set_bits = values.num_intersections_with(validity);
         let is_uniform = set_bits == 0 || set_bits == validity.set_bits();
         2 + usize::from(!is_uniform)
+    }
+
+    #[inline]
+    fn distinct_non_null_count(&self) -> usize {
+        self.distinct_count() - usize::from(self.null_count() > 0)
     }
 }
 
