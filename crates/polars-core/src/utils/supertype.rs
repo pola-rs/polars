fee68e3e3e72553ef2395fb3ced4223212b17b59
nameexhaustion <simonlin.rqmmw@slmail.me>
2024-06-07 16:09:41 +1000

    fix: Fix comparison of UInt64 with zero (#16799)

--- crates/polars-core/src/utils/supertype.rs
+++ crates/polars-core/src/utils/supertype.rs
@@ -288,7 +288,7 @@ pub fn get_supertype(l: &DataType, r: &DataType) -> Option<DataType> {
                         }
                         // dyn int vs number
                         else {
-                            let smallest_fitting_dtype = if dt.is_unsigned_integer() && v.is_positive() {
+                            let smallest_fitting_dtype = if dt.is_unsigned_integer() && !v.is_negative() {
                                 materialize_dyn_int_pos(*v).dtype()
                             } else {
                                 materialize_smallest_dyn_int(*v).dtype()
