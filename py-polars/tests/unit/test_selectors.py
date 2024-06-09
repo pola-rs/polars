0c48793eb33694a39e6a7e95509d1c8bc3a75059
alexander-beedie <alexander.m.beedie@icloud.com>
2024-06-09 15:56:27 +0400

    feat: Native `selector` XOR set operation, and guarantee consistent selector column-order

--- py-polars/tests/unit/test_selectors.py
+++ py-polars/tests/unit/test_selectors.py
@@ -1,4 +1,5 @@
 import sys
+from collections import OrderedDict
 from datetime import datetime
 from typing import Any
 
@@ -10,6 +11,7 @@ from polars.dependencies import _ZONEINFO_AVAILABLE
 from polars.exceptions import ColumnNotFoundError
 from polars.selectors import expand_selector, is_selector
 from polars.testing import assert_frame_equal
+from polars.type_aliases import SelectorType
 
 if sys.version_info >= (3, 9):
     from zoneinfo import ZoneInfo
@@ -97,18 +99,24 @@ def test_selector_alpha() -> None:
 
 
 def test_selector_by_dtype(df: pl.DataFrame) -> None:
-    assert df.select(cs.by_dtype(pl.UInt16) | cs.boolean()).schema == {
-        "abc": pl.UInt16,
-        "eee": pl.Boolean,
-        "fgg": pl.Boolean,
-    }
-    assert df.select(~cs.by_dtype(pl.INTEGER_DTYPES, pl.TEMPORAL_DTYPES)).schema == {
-        "cde": pl.Float64,
-        "def": pl.Float32,
-        "eee": pl.Boolean,
-        "fgg": pl.Boolean,
-        "qqR": pl.String,
-    }
+    assert df.select(cs.boolean() | cs.by_dtype(pl.UInt16)).schema == OrderedDict(
+        {
+            "abc": pl.UInt16,
+            "eee": pl.Boolean,
+            "fgg": pl.Boolean,
+        }
+    )
+    assert df.select(
+        ~cs.by_dtype(pl.INTEGER_DTYPES, pl.TEMPORAL_DTYPES)
+    ).schema == OrderedDict(
+        {
+            "cde": pl.Float64,
+            "def": pl.Float32,
+            "eee": pl.Boolean,
+            "fgg": pl.Boolean,
+            "qqR": pl.String,
+        }
+    )
     assert df.select(cs.by_dtype()).schema == {}
     assert df.select(cs.by_dtype([])).schema == {}
 
@@ -196,20 +204,20 @@ def test_selector_by_name(df: pl.DataFrame) -> None:
 
 def test_selector_contains(df: pl.DataFrame) -> None:
     assert df.select(cs.contains("b")).columns == ["abc", "bbb"]
-    assert df.select(cs.contains(("e", "g"))).columns == [
+    assert df.select(cs.contains(("e", "g"))).columns == [  # type: ignore[arg-type]
         "cde",
         "def",
         "eee",
         "fgg",
         "ghi",
     ]
-    assert df.select(~cs.contains(("b", "e", "g"))).columns == [
+    assert df.select(~cs.contains("b", "e", "g")).columns == [
         "JJK",
         "Lmn",
         "opp",
         "qqR",
     ]
-    assert df.select(cs.contains(("ee", "x"))).columns == ["eee"]
+    assert df.select(cs.contains("ee", "x")).columns == ["eee"]
 
     # expected errors
     with pytest.raises(TypeError):
@@ -597,32 +605,44 @@ def test_selector_repr() -> None:
 
 def test_selector_sets(df: pl.DataFrame) -> None:
     # or
-    assert df.select(cs.temporal() | cs.string() | cs.starts_with("e")).schema == {
-        "eee": pl.Boolean,
-        "ghi": pl.Time,
-        "JJK": pl.Date,
-        "Lmn": pl.Duration,
-        "opp": pl.Datetime("ms"),
-        "qqR": pl.String,
-    }
+    assert df.select(
+        cs.temporal() | cs.string() | cs.starts_with("e")
+    ).schema == OrderedDict(
+        {
+            "eee": pl.Boolean,
+            "ghi": pl.Time,
+            "JJK": pl.Date,
+            "Lmn": pl.Duration,
+            "opp": pl.Datetime("ms"),
+            "qqR": pl.String,
+        }
+    )
 
     # and
-    assert df.select(cs.temporal() & cs.matches("opp|JJK")).schema == {
-        "JJK": pl.Date,
-        "opp": pl.Datetime("ms"),
-    }
+    assert df.select(cs.temporal() & cs.matches("opp|JJK")).schema == OrderedDict(
+        {
+            "JJK": pl.Date,
+            "opp": pl.Datetime("ms"),
+        }
+    )
 
     # SET A - SET B
-    assert df.select(cs.temporal() - cs.matches("opp|JJK")).schema == {
-        "ghi": pl.Time,
-        "Lmn": pl.Duration,
-    }
+    assert df.select(cs.temporal() - cs.matches("opp|JJK")).schema == OrderedDict(
+        {
+            "ghi": pl.Time,
+            "Lmn": pl.Duration,
+        }
+    )
 
     # equivalent (though more verbose) to the above, using `exclude`
-    assert df.select(cs.exclude(~cs.temporal() | cs.matches("opp|JJK"))).schema == {
-        "ghi": pl.Time,
-        "Lmn": pl.Duration,
-    }
+    assert df.select(
+        cs.exclude(~cs.temporal() | cs.matches("opp|JJK"))
+    ).schema == OrderedDict(
+        {
+            "ghi": pl.Time,
+            "Lmn": pl.Duration,
+        }
+    )
 
     frame = pl.DataFrame({"colx": [0, 1, 2], "coly": [3, 4, 5], "colz": [6, 7, 8]})
     sub_expr = cs.matches("[yz]$") - pl.col("colx")  # << shouldn't behave as set
@@ -631,7 +651,7 @@ def test_selector_sets(df: pl.DataFrame) -> None:
     with pytest.raises(TypeError, match=r"unsupported .* \('Expr' - 'Selector'\)"):
         df.select(pl.col("colx") - cs.matches("[yz]$"))
 
-    # COMPLEMENT SET
+    # complement
     assert df.select(~cs.by_dtype([pl.Duration, pl.Time])).schema == {
         "abc": pl.UInt16,
         "bbb": pl.UInt32,
@@ -643,6 +663,17 @@ def test_selector_sets(df: pl.DataFrame) -> None:
         "opp": pl.Datetime("ms"),
         "qqR": pl.String,
     }
+
+    # exclusive or
+    assert df.select((cs.matches("e|g")) ^ cs.numeric()).schema == OrderedDict(
+        {
+            "abc": pl.UInt16,
+            "bbb": pl.UInt32,
+            "eee": pl.Boolean,
+            "fgg": pl.Boolean,
+            "ghi": pl.Time,
+        }
+    )
 
 
 def test_selector_dispatch_default_operator() -> None:
@@ -753,6 +784,30 @@ def test_selector_or() -> None:
     result = df.select(cs.by_name("idx") | ~cs.numeric())
 
     expected = pl.DataFrame(
-        {"idx": [0, 1, 2], "str": ["x", "y", "z"]}, schema_overrides={"idx": pl.UInt32}
+        {"idx": [0, 1, 2], "str": ["x", "y", "z"]},
+        schema_overrides={"idx": pl.UInt32},
     )
     assert_frame_equal(result, expected)
+
+
+@pytest.mark.parametrize(
+    "selector",
+    [
+        (cs.string() | cs.numeric()),
+        (cs.numeric() | cs.string()),
+        ~(~cs.numeric() & ~cs.string()),
+        ~(~cs.string() & ~cs.numeric()),
+        (cs.signed_integer() ^ cs.contains("b", "e", "q")) - cs.starts_with("e"),
+    ],
+)
+def test_selector_result_order(df: pl.DataFrame, selector: SelectorType) -> None:
+    # ensure that selector results always match schema column-order
+    assert df.select(selector).schema == OrderedDict(
+        {
+            "abc": pl.UInt16,
+            "bbb": pl.UInt32,
+            "cde": pl.Float64,
+            "def": pl.Float32,
+            "qqR": pl.String,
+        }
+    )
