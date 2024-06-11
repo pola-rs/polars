5280d094ad9ed6b214d0efec82dafb7247f23da0
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-06-12 00:54:31 +0200

    Extend test

--- py-polars/tests/unit/test_schema.py
+++ py-polars/tests/unit/test_schema.py
@@ -2,29 +2,33 @@ import polars as pl
 
 
 def test_schema() -> None:
-    s = pl.Schema({"a": pl.Int8(), "b": pl.String()})
+    s = pl.Schema({"foo": pl.Int8(), "bar": pl.String()})
 
-    assert s["a"] == pl.Int8()
-    assert s["b"] == pl.String()
+    assert s["foo"] == pl.Int8()
+    assert s["bar"] == pl.String()
     assert s.len() == 2
-    assert s.names() == ["a", "b"]
+    assert s.names() == ["foo", "bar"]
     assert s.dtypes() == [pl.Int8(), pl.String()]
 
 
 def test_schema_parse_nonpolars_dtypes() -> None:
     # Currently, no parsing is being done.
-    s = pl.Schema({"a": pl.List, "b": int})  # type: ignore[arg-type]
+    s = pl.Schema({"foo": pl.List, "bar": int})  # type: ignore[arg-type]
 
-    assert s["a"] == pl.List
-    assert s["b"] == int
+    assert s["foo"] == pl.List
+    assert s["bar"] == int
     assert s.len() == 2
-    assert s.names() == ["a", "b"]
+    assert s.names() == ["foo", "bar"]
     assert s.dtypes() == [pl.List, int]
 
 
 def test_schema_equality() -> None:
-    s1 = pl.Schema({"a": pl.Int8(), "b": pl.Float64()})
-    s2 = pl.Schema({"b": pl.Float64(), "a": pl.Int8()})
+    s1 = pl.Schema({"foo": pl.Int8(), "bar": pl.Float64()})
+    s2 = pl.Schema({"foo": pl.Int8(), "bar": pl.String()})
+    s3 = pl.Schema({"bar": pl.Float64(), "foo": pl.Int8()})
     assert s1 == s1
     assert s2 == s2
+    assert s3 == s3
     assert s1 != s2
+    assert s1 != s3
+    assert s2 != s3
