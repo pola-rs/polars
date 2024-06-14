c4977823bae2e5680173c1884cdbbcb1c3dbc5a7
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-06-14 16:49:18 +0200

    Add PerformanceWarning

--- py-polars/tests/unit/test_exceptions.py
+++ py-polars/tests/unit/test_exceptions.py
@@ -1,10 +1,34 @@
 import pytest
 
-import polars as pl
+from polars.exceptions import (
+    CategoricalRemappingWarning,
+    ComputeError,
+    CustomUFuncWarning,
+    MapWithoutReturnDtypeWarning,
+    OutOfBoundsError,
+    PerformanceWarning,
+    PolarsError,
+    PolarsInefficientMapWarning,
+    PolarsWarning,
+)
 
 
-def test_base_class() -> None:
-    assert isinstance(pl.ComputeError("msg"), pl.PolarsError)
+def test_polars_error_base_class() -> None:
     msg = "msg"
-    with pytest.raises(pl.PolarsError, match=msg):
-        raise pl.OutOfBoundsError(msg)
+    assert isinstance(ComputeError(msg), PolarsError)
+    with pytest.raises(PolarsError, match=msg):
+        raise OutOfBoundsError(msg)
+
+
+def test_polars_warning_base_class() -> None:
+    msg = "msg"
+    assert isinstance(MapWithoutReturnDtypeWarning(msg), PolarsWarning)
+    with pytest.raises(PolarsWarning, match=msg):
+        raise CustomUFuncWarning(msg)
+
+
+def test_performance_warning_base_class() -> None:
+    msg = "msg"
+    assert isinstance(PolarsInefficientMapWarning(msg), PerformanceWarning)
+    with pytest.raises(PerformanceWarning, match=msg):
+        raise CategoricalRemappingWarning(msg)
