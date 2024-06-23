64982d7b1c56b9f22b6556edb0939a8f8e44f081
alexander-beedie <alexander.m.beedie@icloud.com>
2024-06-24 00:37:02 +0400

    fix test

--- py-polars/tests/unit/test_errors.py
+++ py-polars/tests/unit/test_errors.py
@@ -474,7 +474,7 @@ def test_with_column_duplicates() -> None:
     df = pl.DataFrame({"a": [0, None, 2, 3, None], "b": [None, 1, 2, 3, None]})
     with pytest.raises(
         ComputeError,
-        match=r"the name: 'same' passed to `LazyFrame.with_columns` is duplicate.*",
+        match=r"the name 'same' passed to `LazyFrame.with_columns` is duplicate.*",
     ):
         assert df.with_columns([pl.all().alias("same")]).columns == ["a", "b", "same"]
 
