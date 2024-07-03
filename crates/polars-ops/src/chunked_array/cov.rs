9445509337512335529eeadb865491797c269a86
Kemeth, Felix <Felix.Kemeth@adidas.com>
2024-07-03 11:08:52 +0200

    Changed return from NaN to None if min_periods>n.

--- crates/polars-ops/src/chunked_array/cov.rs
+++ crates/polars-ops/src/chunked_array/cov.rs
@@ -95,7 +95,7 @@ where
     }
 
     if min_periods > n as usize {
-        f64::NAN
+        None
     } else {
         cxy / (n - ddof as f64)
     }
@@ -198,8 +198,10 @@ where
     let denom = sample_std_x * sample_std_y;
     let result = sample_cov / denom;
 
-    if denom.is_zero() || min_periods > n as usize {
+    if denom.is_zero() {
         f64::NAN
+    } else if min_periods > n as usize {
+        None
     } else {
         result
     }
