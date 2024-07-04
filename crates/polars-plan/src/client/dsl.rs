b423cef2c9eaa49267facf3284bb70a4498fa655
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-07-05 00:43:07 +0200

    Finish inputs/get_expr

--- crates/polars-plan/src/client/dsl.rs
+++ crates/polars-plan/src/client/dsl.rs
@@ -3,8 +3,20 @@ use crate::prelude::DslPlan;
 
 impl DslPlan {
     fn inputs<'a>(&'a self, scratch: &mut Vec<&'a DslPlan>) {
+        use DslPlan::*;
         match self {
-            DslPlan::Join {
+            Select { input, .. }
+            | GroupBy { input, .. }
+            | Filter { input, .. }
+            | Distinct { input, .. }
+            | Sort { input, .. }
+            | Slice { input, .. }
+            | HStack { input, .. }
+            | MapFunction { input, .. }
+            | Sink { input, .. }
+            | Cache { input, .. } => scratch.push(input),
+            Union { inputs, .. } | HConcat { inputs, .. } => scratch.extend(inputs),
+            Join {
                 input_left,
                 input_right,
                 ..
@@ -12,14 +24,53 @@ impl DslPlan {
                 scratch.push(input_left);
                 scratch.push(input_right);
             },
-            DslPlan::Union { inputs, .. } => scratch.extend(inputs),
-            DslPlan::Filter { input, .. } => scratch.push(input),
-            _ => todo!(),
+            ExtContext { input, contexts } => {
+                scratch.push(input);
+                scratch.extend(contexts);
+            },
+            IR { dsl, .. } => scratch.push(dsl),
+            Scan { .. } | DataFrameScan { .. } | PythonScan { .. } => (),
         }
     }
 
-    pub(super) fn get_expr(&self, scratch: &mut Vec<&Expr>) {
-        todo!()
+    pub(super) fn get_expr<'a>(&'a self, scratch: &mut Vec<&'a Expr>) {
+        use DslPlan::*;
+        match self {
+            Filter { predicate, .. } => scratch.push(predicate),
+            Scan { predicate, .. } => {
+                if let Some(expr) = predicate {
+                    scratch.push(expr)
+                }
+            },
+            DataFrameScan { filter, .. } => {
+                if let Some(expr) = filter {
+                    scratch.push(expr)
+                }
+            },
+            Select { expr, .. } => scratch.extend(expr),
+            HStack { exprs, .. } => scratch.extend(exprs),
+            Sort { by_column, .. } => scratch.extend(by_column),
+            GroupBy { keys, aggs, .. } => {
+                scratch.extend(keys);
+                scratch.extend(aggs);
+            },
+            Join {
+                left_on, right_on, ..
+            } => {
+                scratch.extend(left_on);
+                scratch.extend(right_on);
+            },
+            PythonScan { .. }
+            | Cache { .. }
+            | Distinct { .. }
+            | Slice { .. }
+            | MapFunction { .. }
+            | Union { .. }
+            | HConcat { .. }
+            | ExtContext { .. }
+            | Sink { .. }
+            | IR { .. } => (),
+        }
     }
 }
 
