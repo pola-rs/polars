9a8b18841dd614169a5349193d924bd91f4fd652
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-07-05 02:13:23 +0200

    Add some initial tests

---
+++ py-polars/tests/unit/cloud/test_assert_cloud_eligible.py
@@ -0,0 +1,27 @@
+import pytest
+
+import polars as pl
+from polars._utils.cloud import assert_cloud_eligible
+
+
+@pytest.mark.parametrize(
+    "lf",
+    [
+        pl.scan_parquet("data.parquet").select("a"),
+    ],
+)
+def test_assert_cloud_eligible_pass(lf: pl.LazyFrame) -> None:
+    assert_cloud_eligible(lf)
+
+
+@pytest.mark.parametrize(
+    "lf",
+    [
+        pl.scan_parquet("data.parquet").select(
+            pl.col("a").map_elements(lambda x: sum(x))
+        ),
+    ],
+)
+def test_assert_cloud_eligible_fail(lf: pl.LazyFrame) -> None:
+    with pytest.raises(AssertionError):
+        assert_cloud_eligible(lf)
