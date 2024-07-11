2ff07f867de696749b92671032af4710ed783463
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-07-11 09:21:24 +0200

    Update test

--- py-polars/tests/unit/io/test_scan.py
+++ py-polars/tests/unit/io/test_scan.py
@@ -589,6 +589,7 @@ def test_scan_nonexistent_path(format: str) -> None:
         result.collect()
 
 
+@pytest.mark.slow()
 @pytest.mark.parametrize("format", ["parquet", "csv", "ndjson", "ipc"])
 def test_scan_nonexistent_cloud_path_17444(format: str) -> None:
     # https://github.com/pola-rs/polars/issues/17444
@@ -597,10 +598,13 @@ def test_scan_nonexistent_cloud_path_17444(format: str) -> None:
     scan_function = getattr(pl, f"scan_{format}")
 
     # Just calling the scan function should not raise any errors
-    result = scan_function(path_str)
+    if format == "ndjson":
+        # NDJSON does not have a `retries` parameter yet - so use the default
+        result = scan_function(path_str)
+    else:
+        result = scan_function(path_str, retries=0)
     assert isinstance(result, pl.LazyFrame)
 
     # Upon collection, it should fail
-    # TODO: Make sure this is fast (can timeout when no internet connection)
     with pytest.raises(ComputeError):
         result.collect()
