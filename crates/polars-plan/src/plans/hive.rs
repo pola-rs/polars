39854f9279b817cb5e9d20a6c24b3eed1fbe9e33
Simon Lin <simonlin.rqmmw@slmail.me>
2024-07-15 20:27:00 +1000

    c

--- crates/polars-plan/src/plans/hive.rs
+++ crates/polars-plan/src/plans/hive.rs
@@ -230,7 +230,7 @@ pub fn hive_partitions_from_paths(
 /// Determine the path separator for identifying Hive partitions.
 #[cfg(target_os = "windows")]
 fn separator(url: &Path) -> char {
-    if polars_io::utils::is_cloud_url(url) {
+    if polars_io::path_utils::is_cloud_url(url) {
         '/'
     } else {
         '\\'
