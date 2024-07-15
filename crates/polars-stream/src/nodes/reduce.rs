3f4ff6c847603a35525c2c4ce9f735f97e2e8dd5
ritchie <ritchie46@gmail.com>
2024-07-15 17:27:43 +0200

    more [skip ci]

--- crates/polars-stream/src/nodes/reduce.rs
+++ crates/polars-stream/src/nodes/reduce.rs
@@ -1,12 +1,11 @@
-use std::sync::Arc;
-
-use polars_core::prelude::Schema;
+use std::sync::{Arc};
+use parking_lot::Mutex;
 use polars_core::schema::SchemaRef;
 use polars_error::PolarsResult;
 use polars_expr::prelude::{ExecutionState, PhysicalExpr};
 use polars_expr::reduce::Reduction;
 
-use crate::async_executor::{JoinHandle, TaskScope};
+use crate::async_executor::{JoinHandle, TaskPriority, TaskScope};
 use crate::async_primitives::pipe::{Receiver, Sender};
 use crate::graph::PortState;
 use crate::morsel::Morsel;
@@ -14,7 +13,7 @@ use crate::nodes::ComputeNode;
 
 pub struct ReduceNode {
     inputs: Vec<Arc<dyn PhysicalExpr>>,
-    reductions: Vec<Box<dyn Reduction>>,
+    reductions: Mutex<Vec<Box<dyn Reduction>>>,
     output_schema: SchemaRef,
 }
 
@@ -38,7 +37,8 @@ impl ComputeNode for ReduceNode {
     }
 
     fn update_state(&mut self, recv: &mut [PortState], send: &mut [PortState]) {
-        todo!()
+        assert!(recv.len() == 1 && send.len() == 1);
+        recv.swap_with_slice(send);
     }
 
     fn spawn<'env, 's>(
@@ -49,6 +49,26 @@ impl ComputeNode for ReduceNode {
         send: &mut [Option<Sender<Morsel>>],
         state: &'s ExecutionState,
     ) -> JoinHandle<PolarsResult<()>> {
-        todo!()
+        assert!(recv.len() == 1 && send.len() == 1);
+        let mut recv = recv[0].take().unwrap();
+        let mut send = send[0].take().unwrap();
+
+        scope.spawn_task(TaskPriority::High, async move {
+
+            self.reductions.clone()
+
+            while let Ok(morsel) = recv.recv().await {
+                let df = morsel.into_df();
+
+                for (i, input) in self.inputs.iter().map(|s| s.evaluate(&df, state)).enumerate() {
+                    let input = input?;
+                    self.reductions[i].update(&input)?;
+                }
+            }
+
+            let columns = self.reductions.into_iter().zip(self.output_schema.iter_names()).map(|(mut reduction, name)| reduction.finalize().map(|s| s.into_series(name))).collect::<PolarsResult<Vec<_>>>()?;
+
+            Ok(())
+        })
     }
 }
