e7d83784abce3040ee718a8f901d8ecd27b633e4
Orson Peters <orsonpeters@gmail.com>
2024-07-18 13:04:50 +0200

    allow nodes to specify serial or parallel ports and automatically connect them

--- crates/polars-stream/src/nodes/ordered_union.rs
+++ crates/polars-stream/src/nodes/ordered_union.rs
@@ -1,12 +1,10 @@
-use parking_lot::Mutex;
-
 use super::compute_node_prelude::*;
 
 /// A node that first passes through all data from the first input, then the
 /// second input, etc.
 pub struct OrderedUnionNode {
     cur_input_idx: usize,
-    max_morsel_seq_sent: Mutex<MorselSeq>,
+    max_morsel_seq_sent: MorselSeq,
     morsel_offset: MorselSeq,
 }
 
@@ -14,7 +12,7 @@ impl OrderedUnionNode {
     pub fn new() -> Self {
         Self {
             cur_input_idx: 0,
-            max_morsel_seq_sent: Mutex::new(MorselSeq::new(0)),
+            max_morsel_seq_sent: MorselSeq::new(0),
             morsel_offset: MorselSeq::new(0),
         }
     }
@@ -47,38 +45,47 @@ impl ComputeNode for OrderedUnionNode {
         }
 
         // Set the morsel offset one higher than any sent so far.
-        self.morsel_offset = self.max_morsel_seq_sent.lock().successor();
+        self.morsel_offset = self.max_morsel_seq_sent.successor();
     }
 
     fn spawn<'env, 's>(
-        &'env self,
+        &'env mut self,
         scope: &'s TaskScope<'s, 'env>,
-        _pipeline: usize,
-        recv: &mut [Option<Receiver<Morsel>>],
-        send: &mut [Option<Sender<Morsel>>],
+        recv: &mut [Option<RecvPort<'_>>],
+        send: &mut [Option<SendPort<'_>>],
         _state: &'s ExecutionState,
-    ) -> JoinHandle<PolarsResult<()>> {
+        join_handles: &mut Vec<JoinHandle<PolarsResult<()>>>,
+    ) {
         let ready_count = recv.iter().filter(|r| r.is_some()).count();
         assert!(ready_count == 1 && send.len() == 1);
-        let mut recv = recv[self.cur_input_idx].take().unwrap();
-        let mut send = send[0].take().unwrap();
+        let receivers = recv[self.cur_input_idx].take().unwrap().parallel();
+        let senders = send[0].take().unwrap().parallel();
 
-        scope.spawn_task(TaskPriority::High, async move {
-            let mut max_seq = MorselSeq::new(0);
-            while let Ok(mut morsel) = recv.recv().await {
-                // Ensure the morsel sequence id stream is monotonic.
-                let seq = morsel.seq().offset_by(self.morsel_offset);
-                max_seq = max_seq.max(seq);
-                morsel.set_seq(seq);
-                if send.send(morsel).await.is_err() {
-                    break;
+        let mut inner_handles = Vec::new();
+        for (mut recv, mut send) in receivers.into_iter().zip(senders) {
+            let morsel_offset = self.morsel_offset;
+            inner_handles.push(scope.spawn_task(TaskPriority::High, async move {
+                let mut max_seq = MorselSeq::new(0);
+                while let Ok(mut morsel) = recv.recv().await {
+                    // Ensure the morsel sequence id stream is monotonic.
+                    let seq = morsel.seq().offset_by(morsel_offset);
+                    max_seq = max_seq.max(seq);
+
+                    morsel.set_seq(seq);
+                    if send.send(morsel).await.is_err() {
+                        break;
+                    }
                 }
-            }
+                max_seq
+            }));
+        }
 
+        join_handles.push(scope.spawn_task(TaskPriority::High, async move {
             // Update our global maximum.
-            let mut max_morsel_seq_sent = self.max_morsel_seq_sent.lock();
-            *max_morsel_seq_sent = max_morsel_seq_sent.max(max_seq.successor());
+            for handle in inner_handles {
+                self.max_morsel_seq_sent = self.max_morsel_seq_sent.max(handle.await);
+            }
             Ok(())
-        })
+        }));
     }
 }
