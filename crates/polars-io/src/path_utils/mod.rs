c4738d26dc81b3b84b3e1d9f022acd08fe8c3b15
nameexhaustion <simonlin.rqmmw@slmail.me>
2024-07-19 16:11:53 +1000

    feat: Support API token for scanning `hf://` (#17682)

--- crates/polars-io/src/path_utils/mod.rs
+++ crates/polars-io/src/path_utils/mod.rs
@@ -151,7 +151,7 @@ pub fn expand_paths_hive(
             if first_path.starts_with("hf://") {
                 let (expand_start_idx, paths) =
                     crate::pl_async::get_runtime().block_on_potential_spawn(
-                        hugging_face::expand_paths_hf(paths, check_directory_level),
+                        hugging_face::expand_paths_hf(paths, check_directory_level, cloud_options),
                     )?;
 
                 return Ok((Arc::from(paths), expand_start_idx));
