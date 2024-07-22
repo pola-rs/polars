26346cfc1d17ca3319b17c30e0654a24dd1cef35
Sandro Casagrande <22665904+SandroCasagrande@users.noreply.github.com>
2024-07-22 11:39:53 +0200

    fix(python): Simultaneous usage of `named_expr` and `schema` in `pl.struct` (#17768)
    Co-authored-by: Casagrande, Sandro <sandro.casagrande@techem.de>

--- py-polars/tests/unit/datatypes/test_struct.py
+++ py-polars/tests/unit/datatypes/test_struct.py
@@ -970,3 +970,11 @@ def test_struct_out_nullability_from_arrow() -> None:
 def test_empty_struct_raise() -> None:
     with pytest.raises(ValueError):
         pl.struct()
+
+
+def test_named_exprs() -> None:
+    df = pl.DataFrame({"a": 1})
+    schema = {"b": pl.Int64}
+    res = df.select(pl.struct(schema=schema, b=pl.col("a")))
+    assert res.to_dict(as_series=False) == {"b": [{"b": 1}]}
+    assert res.schema["b"] == pl.Struct(schema)
