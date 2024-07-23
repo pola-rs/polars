ad0b5c38101e58d69037ae9e3e8a895273099340
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-07-23 08:40:46 +0200

    Fix existing tests

--- py-polars/tests/unit/cloud/test_prepare_cloud_plan.py
+++ py-polars/tests/unit/cloud/test_prepare_cloud_plan.py
@@ -1,25 +1,29 @@
+from io import BytesIO
 from pathlib import Path
 
 import pyarrow.dataset as ds
 import pytest
 
 import polars as pl
-from polars._utils.cloud import assert_cloud_eligible
+from polars._utils.cloud import prepare_cloud_plan
 
-CLOUD_PATH = "s3://my-nonexistent-bucket/dataset"
+CLOUD_SOURCE = "s3://my-nonexistent-bucket/dataset"
+CLOUD_SINK = "s3://my-nonexistent-bucket/result"
 
 
 @pytest.mark.parametrize(
     "lf",
     [
-        pl.scan_parquet(CLOUD_PATH).select("c", pl.lit(2)).with_row_index(),
+        pl.scan_parquet(CLOUD_SOURCE).select("c", pl.lit(2)).with_row_index(),
         pl.LazyFrame({"a": [1, 2], "b": [3, 4]})
         .select("a", "b")
         .filter(pl.col("a") == pl.lit(1)),
     ],
 )
-def test_assert_cloud_eligible(lf: pl.LazyFrame) -> None:
-    assert_cloud_eligible(lf)
+def test_prepare_cloud_plan(lf: pl.LazyFrame) -> None:
+    result = prepare_cloud_plan(lf, CLOUD_SINK)
+    assert isinstance(result, bytes)
+    assert isinstance(pl.LazyFrame.deserialize(BytesIO(result)), pl.LazyFrame)
 
 
 @pytest.mark.parametrize(
@@ -44,16 +48,16 @@ def test_assert_cloud_eligible(lf: pl.LazyFrame) -> None:
         pl.LazyFrame({"a": [1, 2], "b": [3, 4]})
         .group_by("a")
         .agg(pl.col("b").map_batches(lambda x: sum(x))),
-        pl.scan_parquet(CLOUD_PATH).filter(
+        pl.scan_parquet(CLOUD_SOURCE).filter(
             pl.col("a") < pl.lit(1).map_elements(lambda x: x + 1)
         ),
     ],
 )
-def test_assert_cloud_eligible_fail_on_udf(lf: pl.LazyFrame) -> None:
+def test_prepare_cloud_plan_fail_on_udf(lf: pl.LazyFrame) -> None:
     with pytest.raises(
-        AssertionError, match="logical plan ineligible for execution on Polars Cloud"
+        ValueError, match="logical plan ineligible for execution on Polars Cloud"
     ):
-        assert_cloud_eligible(lf)
+        prepare_cloud_plan(lf, CLOUD_SINK)
 
 
 @pytest.mark.parametrize(
@@ -65,15 +69,15 @@ def test_assert_cloud_eligible_fail_on_udf(lf: pl.LazyFrame) -> None:
         pl.scan_ipc(["data-1.feather", "data-2.feather"]),
     ],
 )
-def test_assert_cloud_eligible_fail_on_local_data_source(lf: pl.LazyFrame) -> None:
+def test_prepare_cloud_plan_fail_on_local_data_source(lf: pl.LazyFrame) -> None:
     with pytest.raises(
-        AssertionError, match="logical plan ineligible for execution on Polars Cloud"
+        ValueError, match="logical plan ineligible for execution on Polars Cloud"
     ):
-        assert_cloud_eligible(lf)
+        prepare_cloud_plan(lf, CLOUD_SINK)
 
 
 @pytest.mark.write_disk()
-def test_assert_cloud_eligible_fail_on_python_scan(tmp_path: Path) -> None:
+def test_prepare_cloud_plan_fail_on_python_scan(tmp_path: Path) -> None:
     tmp_path.mkdir(exist_ok=True)
     data_path = tmp_path / "data.parquet"
     pl.DataFrame({"a": [1, 2]}).write_parquet(data_path)
@@ -81,6 +85,6 @@ def test_assert_cloud_eligible_fail_on_python_scan(tmp_path: Path) -> None:
 
     lf = pl.scan_pyarrow_dataset(dataset)
     with pytest.raises(
-        AssertionError, match="logical plan ineligible for execution on Polars Cloud"
+        ValueError, match="logical plan ineligible for execution on Polars Cloud"
     ):
-        assert_cloud_eligible(lf)
+        prepare_cloud_plan(lf, CLOUD_SINK)
