cab0b51e6a482755762bba566f870e9d11d1d189
Alexander Beedie <alexander-beedie@users.noreply.github.com>
2024-07-29 15:40:30 +0400

    collect directly into buffer

--- crates/polars-arrow/src/legacy/kernels/binary.rs
+++ crates/polars-arrow/src/legacy/kernels/binary.rs
@@ -3,8 +3,7 @@ use crate::buffer::Buffer;
 use crate::datatypes::ArrowDataType;
 
 pub fn binary_size_bytes(array: &BinaryViewArray) -> ArrayRef {
-    let values = array.len_iter().collect::<Vec<_>>();
-    let values: Buffer<_> = values.into();
+    let values: Buffer<_> = array.len_iter().collect();
     let array = UInt32Array::new(ArrowDataType::UInt32, values, array.validity().cloned());
     Box::new(array)
 }
