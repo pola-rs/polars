33e3711265ea6588d3a86434166a918b8e58c34b
Alexander Beedie <alexander-beedie@users.noreply.github.com>
2024-07-29 15:14:27 +0400

    fix feature-gating (map)

--- crates/polars-plan/src/dsl/function_expr/binary.rs
+++ crates/polars-plan/src/dsl/function_expr/binary.rs
@@ -2,9 +2,7 @@
 use serde::{Deserialize, Serialize};
 
 use super::*;
-#[cfg(feature = "binary_encoding")]
-use crate::map;
-use crate::map_as_slice;
+use crate::{map, map_as_slice};
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Clone, PartialEq, Debug, Eq, Hash)]
