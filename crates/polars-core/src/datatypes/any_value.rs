a04d36851330cccb471283e4d04efcdb7838c5d0
Eric Tulowetzke <emtulobusi@gmail.com>
2024-08-08 18:34:35 -0500

    make it so float don't get rounded to ints

--- crates/polars-core/src/datatypes/any_value.rs
+++ crates/polars-core/src/datatypes/any_value.rs
@@ -537,7 +537,13 @@ impl<'a> AnyValue<'a> {
 
             // to string
             (av, DataType::String) => {
-                AnyValue::StringOwned(format_smartstring!("{}", av.extract::<i64>()?))
+                if av.is_integer() {
+                    AnyValue::StringOwned(format_smartstring!("{}", av.extract::<i64>()?))
+                } else if av.is_float() {
+                    AnyValue::StringOwned(format_smartstring!("{}", av.extract::<f64>()?))
+                } else {
+                    AnyValue::StringOwned(format_smartstring!("{}", av))
+                }
             },
 
             // to binary
