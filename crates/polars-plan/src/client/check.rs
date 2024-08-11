fded134602da8a6b07c00e01fbc4a821039f2436
ritchie <ritchie46@gmail.com>
2024-08-11 10:00:15 +0200

    fmt

--- crates/polars-plan/src/client/check.rs
+++ crates/polars-plan/src/client/check.rs
@@ -10,11 +10,10 @@ pub(super) fn assert_cloud_eligible(dsl: &DslPlan) -> PolarsResult<()> {
     let mut expr_stack = vec![];
     for plan_node in dsl.into_iter() {
         match plan_node {
-            DslPlan::MapFunction {
-                function,
-                ..
-            } => match function {
-                DslFunction::FunctionIR(FunctionIR::Opaque {..})=> return ineligible_error("contains opaque function"),
+            DslPlan::MapFunction { function, .. } => match function {
+                DslFunction::FunctionIR(FunctionIR::Opaque { .. }) => {
+                    return ineligible_error("contains opaque function")
+                },
                 #[cfg(feature = "python")]
                 DslFunction::OpaquePython { .. } => {
                     return ineligible_error("contains Python function")
