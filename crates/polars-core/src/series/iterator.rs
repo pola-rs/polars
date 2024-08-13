cb27ccc84f38fda78a2381576387598925318e2f
Vincent HAYS <vincenthays95@gmail.com>
2024-08-13 15:34:08 +0200

    Add FromIterator<Option<&'a str>> for Series and FromIterator<Option<String>> for Series

--- crates/polars-core/src/series/iterator.rs
+++ crates/polars-core/src/series/iterator.rs
@@ -43,8 +43,22 @@ from_iterator!(f32, Float32Type);
 from_iterator!(f64, Float64Type);
 from_iterator!(bool, BooleanType);
 
+impl<'a> FromIterator<Option<&'a str>> for Series {
+    fn from_iter<I: IntoIterator<Item=Option<&'a str>>>(iter: I) -> Self {
+        let ca: StringChunked = iter.into_iter().collect();
+        ca.into_series()
+    }
+}
+
 impl<'a> FromIterator<&'a str> for Series {
     fn from_iter<I: IntoIterator<Item = &'a str>>(iter: I) -> Self {
+        let ca: StringChunked = iter.into_iter().collect();
+        ca.into_series()
+    }
+}
+
+impl FromIterator<Option<String>> for Series {
+    fn from_iter<T: IntoIterator<Item=Option<String>>>(iter: T) -> Self {
         let ca: StringChunked = iter.into_iter().collect();
         ca.into_series()
     }
