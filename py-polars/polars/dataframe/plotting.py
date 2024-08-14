9266adb9d9d696c0a549e2970a0dced802d50341
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-08-14 10:14:55 +0100

    lint

--- py-polars/polars/dataframe/plotting.py
+++ py-polars/polars/dataframe/plotting.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Callable, Dict, Mapping, Union
+from typing import TYPE_CHECKING, Any, Callable, Dict, Union
 
 if TYPE_CHECKING:
     import sys
