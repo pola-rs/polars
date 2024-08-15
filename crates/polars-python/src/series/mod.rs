2155520fe12d9a3e3350a2e88caab44857e0f2c3
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-08-15 16:04:47 +0200

    Feature flag Series

--- crates/polars-python/src/series/mod.rs
+++ crates/polars-python/src/series/mod.rs
@@ -1,13 +1,24 @@
+#[cfg(feature = "pymethods")]
 mod aggregation;
+#[cfg(feature = "pymethods")]
 mod arithmetic;
+#[cfg(feature = "pymethods")]
 mod buffers;
+#[cfg(feature = "pymethods")]
 mod c_interface;
+#[cfg(feature = "pymethods")]
 mod comparison;
+#[cfg(feature = "pymethods")]
 mod construction;
+#[cfg(feature = "pymethods")]
 mod export;
+#[cfg(feature = "pymethods")]
 mod general;
+#[cfg(feature = "pymethods")]
 mod import;
+#[cfg(feature = "pymethods")]
 mod numpy_ufunc;
+#[cfg(feature = "pymethods")]
 mod scatter;
 
 use polars::prelude::Series;
