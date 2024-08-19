d55331f98650800e44667912ac136933d12bbcfa
Dean MacGregor <powertrading121@gmail.com>
2024-08-19 06:58:42 -0400

    raise for utf16

--- crates/polars-io/src/json/mod.rs
+++ crates/polars-io/src/json/mod.rs
@@ -228,7 +228,7 @@ pub fn remove_bom(bytes: &[u8]) -> &[u8] {
         &bytes[3..]
     } else if bytes.starts_with(&[0xFE, 0xFF]) || bytes.starts_with(&[0xFF, 0xFE]) {
         // UTF-16 BOM
-        &bytes[2..]
+        polars_err!(ComputeError: "utf-16 not supported")
     } else {
         bytes
     }
