325dfd1daedae3e4a099b61b3321eb470ac2298d
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-09-06 09:00:27 +0200

    fix: Fix output type for `list.eval` in certain cases (#18570)

--- crates/polars-lazy/src/dsl/list.rs
+++ crates/polars-lazy/src/dsl/list.rs
@@ -136,7 +136,7 @@ fn run_on_group_by_engine(
     let state = ExecutionState::new();
     let mut ac = phys_expr.evaluate_on_groups(&df_context, &groups, &state)?;
     let out = match ac.agg_state() {
-        AggState::AggregatedScalar(_) | AggState::Literal(_) => {
+        AggState::AggregatedScalar(_) => {
             let out = ac.aggregated();
             out.as_list().into_series()
         },
