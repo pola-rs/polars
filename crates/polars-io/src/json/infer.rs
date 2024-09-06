16d8c83938fe0f2a20c583ee0ef49c77853a3720
Dean MacGregor <powertrading121@gmail.com>
2024-09-06 13:09:12 -0400

    make_read_json_accept_empty_list

--- crates/polars-io/src/json/infer.rs
+++ crates/polars-io/src/json/infer.rs
@@ -2,7 +2,7 @@ use std::num::NonZeroUsize;
 
 use polars_core::prelude::DataType;
 use polars_core::utils::try_get_supertype;
-use polars_error::{polars_bail, PolarsResult};
+use polars_error::{polars_bail, PolarsError, PolarsResult};
 use simd_json::BorrowedValue;
 
 pub(crate) fn json_values_to_supertype(
@@ -10,7 +10,7 @@ pub(crate) fn json_values_to_supertype(
     infer_schema_len: NonZeroUsize,
 ) -> PolarsResult<DataType> {
     // struct types may have missing fields so find supertype
-    values
+    let out_opt: Option<Result<DataType, PolarsError>> = values
         .iter()
         .take(infer_schema_len.into())
         .map(|value| polars_json::json::infer(value).map(|dt| DataType::from(&dt)))
@@ -18,8 +18,21 @@ pub(crate) fn json_values_to_supertype(
             let l = l?;
             let r = r?;
             try_get_supertype(&l, &r)
-        })
-        .unwrap_or_else(|| polars_bail!(ComputeError: "could not infer data-type"))
+        });
+    match (out_opt, values.len()==0) {
+        (Some(out), true) => {
+            match out {
+                Ok(out)=>Ok(out),
+                _=>Err(PolarsError::NoData("no data".into()))
+            }
+        },
+        (Some(out), false) => {
+            out
+        },
+        (None, true) => Err(PolarsError::NoData("no data".into())),
+        (None, false) => polars_bail!(ComputeError: "could not infer data-type")
+
+    }
 }
 
 pub(crate) fn dtypes_to_supertype<I: Iterator<Item = DataType>>(
