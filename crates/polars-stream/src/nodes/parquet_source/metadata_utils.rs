5d81d4acd6c5021777f0872f8e200eeca7f2c2f1
nameexhaustion <simonlin.rqmmw@slmail.me>
2024-09-17 15:34:34 +0200

    refactor: Remove extra hashmap construction in new-streaming parquet (#18792)

--- crates/polars-stream/src/nodes/parquet_source/metadata_utils.rs
+++ crates/polars-stream/src/nodes/parquet_source/metadata_utils.rs
@@ -1,9 +1,8 @@
-use polars_core::prelude::{ArrowSchema, DataType, PlHashMap};
+use polars_core::prelude::{ArrowSchema, DataType};
 use polars_error::{polars_bail, PolarsResult};
 use polars_io::prelude::FileMetadata;
 use polars_io::utils::byte_source::{ByteSource, DynByteSource};
 use polars_utils::mmap::MemSlice;
-use polars_utils::pl_str::PlSmallStr;
 
 /// Read the metadata bytes of a parquet file, does not decode the bytes. If during metadata fetch
 /// the bytes of the entire file are loaded, it is returned in the second return value.
@@ -124,26 +123,20 @@ pub(super) async fn read_parquet_metadata_bytes(
 /// Ensures that a parquet file has all the necessary columns for a projection with the correct
 /// dtype. There are no ordering requirements and extra columns are permitted.
 pub(super) fn ensure_metadata_has_projected_fields(
-    projected_fields: &ArrowSchema,
     metadata: &FileMetadata,
+    projected_fields: &ArrowSchema,
 ) -> PolarsResult<()> {
     let schema = polars_parquet::arrow::read::infer_schema(metadata)?;
 
-    // Note: We convert to Polars-native dtypes for timezone normalization.
-    let mut schema = schema
-        .into_iter_values()
-        .map(|x| {
-            let dtype = DataType::from_arrow(&x.dtype, true);
-            (x.name, dtype)
-        })
-        .collect::<PlHashMap<PlSmallStr, DataType>>();
-
     for field in projected_fields.iter_values() {
-        let Some(dtype) = schema.remove(&field.name) else {
-            polars_bail!(SchemaMismatch: "did not find column: {}", field.name)
-        };
-
+        // Note: We convert to Polars-native dtypes for timezone normalization.
         let expected_dtype = DataType::from_arrow(&field.dtype, true);
+        let dtype = {
+            let Some(field) = schema.get(&field.name) else {
+                polars_bail!(SchemaMismatch: "did not find column: {}", field.name)
+            };
+            DataType::from_arrow(&field.dtype, true)
+        };
 
         if dtype != expected_dtype {
             polars_bail!(SchemaMismatch: "data type mismatch for column {}: found: {}, expected: {}",
