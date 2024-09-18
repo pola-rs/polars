938494534f66914afe2815f1d718e0024bdf224b
nameexhaustion <simonlin.rqmmw@slmail.me>
2024-09-18 14:17:07 +0200

    refactor: Add panic to unchecked DataFrame constructors in debug mode (#18807)

--- crates/polars-ops/src/series/ops/horizontal.rs
+++ crates/polars-ops/src/series/ops/horizontal.rs
@@ -2,28 +2,28 @@ use polars_core::frame::NullStrategy;
 use polars_core::prelude::*;
 
 pub fn max_horizontal(s: &[Column]) -> PolarsResult<Option<Column>> {
-    let df = unsafe { DataFrame::new_no_checks(Vec::from(s)) };
+    let df = unsafe { DataFrame::_new_no_checks_impl(Vec::from(s)) };
     df.max_horizontal()
         .map(|s| s.map(Column::from))
         .map(|opt_s| opt_s.map(|res| res.with_name(s[0].name().clone())))
 }
 
 pub fn min_horizontal(s: &[Column]) -> PolarsResult<Option<Column>> {
-    let df = unsafe { DataFrame::new_no_checks(Vec::from(s)) };
+    let df = unsafe { DataFrame::_new_no_checks_impl(Vec::from(s)) };
     df.min_horizontal()
         .map(|s| s.map(Column::from))
         .map(|opt_s| opt_s.map(|res| res.with_name(s[0].name().clone())))
 }
 
 pub fn sum_horizontal(s: &[Column]) -> PolarsResult<Option<Column>> {
-    let df = unsafe { DataFrame::new_no_checks(Vec::from(s)) };
+    let df = unsafe { DataFrame::_new_no_checks_impl(Vec::from(s)) };
     df.sum_horizontal(NullStrategy::Ignore)
         .map(|s| s.map(Column::from))
         .map(|opt_s| opt_s.map(|res| res.with_name(s[0].name().clone())))
 }
 
 pub fn mean_horizontal(s: &[Column]) -> PolarsResult<Option<Column>> {
-    let df = unsafe { DataFrame::new_no_checks(Vec::from(s)) };
+    let df = unsafe { DataFrame::_new_no_checks_impl(Vec::from(s)) };
     df.mean_horizontal(NullStrategy::Ignore)
         .map(|s| s.map(Column::from))
         .map(|opt_s| opt_s.map(|res| res.with_name(s[0].name().clone())))
