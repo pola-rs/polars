d883b1b3eab379c04a330257353af90f08c445c6
ritchie <ritchie46@gmail.com>
2024-09-24 18:48:51 +0200

    add a way to allow

--- crates/polars-mem-engine/src/executors/stack.rs
+++ crates/polars-mem-engine/src/executors/stack.rs
@@ -79,7 +79,10 @@ impl StackExec {
                 for (i, c) in res.iter().enumerate() {
                     let len = c.len();
                     if verify_scalar && len != df_height && len == 1 && df_width > 0 {
-                        if !self.exprs[i].is_scalar() {
+                        #[allow(clippy::collapsible_if)]
+                        if !self.exprs[i].is_scalar()
+                            && std::env::var("POLARS_ALLOW_NON_SCALAR_EXP").as_deref() != Ok("1")
+                        {
                             let identifier = match self.exprs[i].as_expression() {
                                 Some(e) => format!("expression: {}", e),
                                 None => "this Series".to_string(),
