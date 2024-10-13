1e157abb9e3d634404d5d413a06b3f6e8e691e9b
barak1412 <barak1412@gmail.com>
2024-10-13 10:10:18 +0300

    working version

--- crates/polars-plan/src/plans/conversion/functions.rs
+++ crates/polars-plan/src/plans/conversion/functions.rs
@@ -1,6 +1,6 @@
 use arrow::legacy::error::PolarsResult;
 use polars_utils::arena::{Arena, Node};
-use polars_utils::format_pl_smallstr;
+use polars_utils::{arena, format_pl_smallstr};
 
 use super::*;
 use crate::dsl::{Expr, FunctionExpr};
@@ -45,8 +45,7 @@ pub(super) fn convert_functions(
             }
         },
         FunctionExpr::ArrayExpr(ArrayFunction::Explode) => {
-            //let input_aexpr = to_
-            //AExpr::Explode(input)
+            return to_aexpr_impl(Expr::Explode(Arc::new(input[0].clone())), arena, state);
         }
         _ => {},
     }
