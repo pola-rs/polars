3947452bc1061030e34cc59a81de830213a162d1
Pavel Zwerschke <pavelzw@gmail.com>
2024-10-14 15:09:43 +0200

    update test

--- py-polars/tests/unit/test_selectors.py
+++ py-polars/tests/unit/test_selectors.py
@@ -818,7 +818,7 @@ def test_selector_list_of_lists_18499() -> None:
 
 def test_invalid_selector() -> None:
     df = pl.DataFrame(data={"x": [1, 2], "z": ["a", "b"]})
+    sel = pl.selectors.all() + pl.col("x")
+    assert isinstance(sel, pl.selectors._selector_proxy_)
     with pytest.raises(InvalidOperationError, match="invalid selector expression"):
-        df.drop(pl.col("x", "z") + 2)  # type: ignore[arg-type]
-    with pytest.raises(InvalidOperationError, match="invalid selector expression"):
-        df.drop(pl.col("x") + 2)  # type: ignore[arg-type]
+        df.drop(sel)
