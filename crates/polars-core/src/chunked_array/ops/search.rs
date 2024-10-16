955c9326c27808e9e217db8e7b34ce9575f43675
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-10-16 15:31:46 -0400

    Proof-of-concept NaN handling.

--- crates/polars-core/src/chunked_array/ops/search.rs
+++ crates/polars-core/src/chunked_array/ops/search.rs
@@ -1,3 +1,5 @@
+use polars_utils::float::IsFloat;
+
 use crate::prelude::*;
 
 impl<'a, T> ChunkSearch<'a, T::Native> for ChunkedArray<T>
@@ -5,11 +7,19 @@ where
     T: PolarsNumericType,
 {
     fn index_of(&'a self, value: T::Native) -> Option<usize> {
-        // TODO handle NaN
+        if value.is_nan() {
+            let mut index = 0;
+            for opt_val in self.iter() {
+                if opt_val.map(|v| v.is_nan()) == Some(true) {
+                    return Some(index);
+                };
+                index += 1;
+            }
+            return None;
+        }
         let mut index = 0;
-        for opt_val in self.downcast_iter().map(|arr| arr.into_iter()).flatten() {
-            println!("{value} {opt_val:?}, {index}");
-            if Some(&value) == opt_val {
+        for opt_val in self.iter() {//self.downcast_iter().map(|arr| arr.into_iter()).flatten() {
+            if Some(value) == opt_val {
                 return Some(index);
             };
             index += 1;
