95682e8bef010f96772fc5ea6ccdbe56892fc2c8
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-10-16 16:07:44 -0400

    Document the motivation.

--- crates/polars-core/src/chunked_array/ops/search.rs
+++ crates/polars-core/src/chunked_array/ops/search.rs
@@ -7,6 +7,8 @@ where
     T: PolarsNumericType,
 {
     fn index_of(&'a self, value: Option<T::Native>) -> Option<usize> {
+        // A NaN is never equal to anything, including itself. But we still want
+        // to be able to search for NaNs, so we handle them specially.
         if value.map(|v| v.is_nan()) == Some(true) {
             return self
                 .iter()
