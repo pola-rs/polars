a6c23512524934ea5df5316a26440abc358bb013
Liam Connors <connorsl@tcd.ie>
2024-10-19 23:23:54 -0400

    add missing deprecation notice for str.concat

--- py-polars/polars/expr/string.py
+++ py-polars/polars/expr/string.py
@@ -2724,6 +2724,11 @@ class ExprStringNameSpace:
         """
         return wrap_expr(self._pyexpr.str_join(delimiter, ignore_nulls=ignore_nulls))
 
+    @deprecate_function(
+        "Use `str.join` instead. Note that the default `delimiter` for `str.join`"
+        " is an empty string instead of a hyphen.",
+        version="1.0.0",
+    )
     def concat(
         self, delimiter: str | None = None, *, ignore_nulls: bool = True
     ) -> Expr:
