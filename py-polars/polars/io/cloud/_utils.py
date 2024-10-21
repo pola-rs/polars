4cf1039cbac14fd6d77bc93e751e5f45ea33bc1c
Simon Lin <simonlin.rqmmw@slmail.me>
2024-10-21 20:08:02 +1100

    c

--- py-polars/polars/io/cloud/_utils.py
+++ py-polars/polars/io/cloud/_utils.py
@@ -20,39 +20,6 @@ def _first_scan_path(
     return None
 
 
-def _infer_cloud_type(
-    source: ScanSource,
-) -> Literal["aws", "azure", "gcp", "file", "http", "hf"] | None:
-    if (path := _first_scan_path(source)) is None:
-        return None
-
-    scheme = _get_path_scheme(path)
-
-    # Fast path - local file
-    if not scheme:
-        return None
-
-    if scheme == "file":
-        return "file"
-
-    if _is_aws_cloud(scheme):
-        return "aws"
-
-    if _is_azure_cloud(scheme):
-        return "azure"
-
-    if _is_gcp_cloud(scheme):
-        return "gcp"
-
-    if _is_http_cloud(scheme):
-        return "http"
-
-    if _is_hf_cloud(scheme):
-        return "hf"
-
-    return None
-
-
 def _get_path_scheme(path: str | Path) -> str | None:
     splitted = str(path).split("://", maxsplit=1)
 
@@ -63,17 +30,5 @@ def _is_aws_cloud(scheme: str) -> bool:
     return any(scheme == x for x in ["s3", "s3a"])
 
 
-def _is_azure_cloud(scheme: str) -> bool:
-    return any(scheme == x for x in ["az", "azure", "adl", "abfs", "abfss"])
-
-
 def _is_gcp_cloud(scheme: str) -> bool:
     return any(scheme == x for x in ["gs", "gcp", "gcs"])
-
-
-def _is_http_cloud(scheme: str) -> bool:
-    return any(scheme == x for x in ["http", "https"])
-
-
-def _is_hf_cloud(scheme: str) -> bool:
-    return scheme == "hf"
