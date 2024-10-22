e97cebd8356f7e63c759ee462f969cd9e9d135a7
Corwin Joy <corwin.joy@gmail.com>
2024-10-21 18:21:20 -0700

    Add initial fix to prevent caputure expansion when literal=True

--- crates/polars-plan/src/dsl/function_expr/strings.rs
+++ crates/polars-plan/src/dsl/function_expr/strings.rs
@@ -1,5 +1,4 @@
 use std::borrow::Cow;
-
 use arrow::legacy::utils::CustomIterTools;
 #[cfg(feature = "timezones")]
 use once_cell::sync::Lazy;
@@ -9,7 +8,7 @@ use polars_core::utils::handle_casting_failures;
 #[cfg(feature = "dtype-struct")]
 use polars_utils::format_pl_smallstr;
 #[cfg(feature = "regex")]
-use regex::{escape, Regex};
+use regex::{escape, Regex, NoExpand};
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
@@ -888,15 +887,25 @@ fn replace_all<'a>(
                 "replacement value length ({}) does not match string column length ({})",
                 len_val, ca.len(),
             );
-            let literal = literal || is_literal_pat(&pat);
 
-            if literal {
+            let literal_pat = literal || is_literal_pat(&pat);
+
+            if literal_pat {
                 pat = escape(&pat)
             }
 
             let reg = Regex::new(&pat)?;
 
-            let f = |s: &'a str, val: &'a str| reg.replace_all(s, val);
+            let f = |s: &'a str, val: &'a str| {
+                // According to the docs for replace_all
+                // when literal = True then capture groups are ignored.
+                if literal {
+                    return reg.replace_all(s, NoExpand(val))
+                } else {
+                    return reg.replace_all(s, val)
+                }
+            };
+
             Ok(iter_and_replace(ca, val, f))
         },
         _ => polars_bail!(
