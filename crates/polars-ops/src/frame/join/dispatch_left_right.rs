bcab4b9b89be8e4f6d39d011297bc201577e1256
Simon Lin <simonlin.rqmmw@slmail.me>
2024-10-24 22:13:34 +1100

    c

--- crates/polars-ops/src/frame/join/dispatch_left_right.rs
+++ crates/polars-ops/src/frame/join/dispatch_left_right.rs
@@ -90,14 +90,14 @@ fn materialize_left_join(
             if let Some((offset, len)) = args.slice {
                 left_idx = slice_slice(left_idx, offset, len);
             }
-            left._create_left_df_from_slice(left_idx, true, true)
+            left._create_left_df_from_slice(left_idx, true, args.slice.is_some(), true)
         },
         ChunkJoinIds::Right(left_idx) => unsafe {
             let mut left_idx = &*left_idx;
             if let Some((offset, len)) = args.slice {
                 left_idx = slice_slice(left_idx, offset, len);
             }
-            left.create_left_df_chunked(left_idx, true)
+            left.create_left_df_chunked(left_idx, true, args.slice.is_some())
         },
     };
 
