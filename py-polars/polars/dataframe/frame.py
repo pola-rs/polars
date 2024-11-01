c3566bf14f5c54b7608dbe0fd7f10a3b751eeb3e
Guilhem de Viry <gdeviry@mytraffic.fr>
2024-11-01 12:20:27 +0100

    feat(python): Add show methods to DataFrame and LazyFrame

--- py-polars/polars/dataframe/frame.py
+++ py-polars/polars/dataframe/frame.py
@@ -54,6 +54,7 @@ from polars._utils.parse import parse_into_expression
 from polars._utils.serde import serialize_polars_object
 from polars._utils.unstable import issue_unstable_warning, unstable
 from polars._utils.various import (
+    _in_notebook,
     is_bool_sequence,
     no_default,
     normalize_filepath,
@@ -11231,6 +11232,62 @@ class DataFrame:
             variable_name=variable_name,
             value_name=value_name,
         )
+
+    def show(self, n: int = 5) -> None:
+        """
+        Show the first `n` rows.
+
+        Parameters
+        ----------
+        n
+            Numbers of rows to show. If a negative value is passed, return all rows
+            except the last `abs(n)`.
+
+        See Also
+        --------
+        head
+
+        Examples
+        --------
+        >>> df = pl.DataFrame(
+        ...     {
+        ...         "foo": [1, 2, 3, 4, 5],
+        ...         "bar": [6, 7, 8, 9, 10],
+        ...         "ham": ["a", "b", "c", "d", "e"],
+        ...     }
+        ... )
+        >>> df.show(3)
+        shape: (3, 3)
+        ┌─────┬─────┬─────┐
+        │ foo ┆ bar ┆ ham │
+        │ --- ┆ --- ┆ --- │
+        │ i64 ┆ i64 ┆ str │
+        ╞═════╪═════╪═════╡
+        │ 1   ┆ 6   ┆ a   │
+        │ 2   ┆ 7   ┆ b   │
+        │ 3   ┆ 8   ┆ c   │
+        └─────┴─────┴─────┘
+
+        Pass a negative value to get all rows `except` the last `abs(n)`.
+
+        >>> df.show(-3)
+        shape: (2, 3)
+        ┌─────┬─────┬─────┐
+        │ foo ┆ bar ┆ ham │
+        │ --- ┆ --- ┆ --- │
+        │ i64 ┆ i64 ┆ str │
+        ╞═════╪═════╪═════╡
+        │ 1   ┆ 6   ┆ a   │
+        │ 2   ┆ 7   ┆ b   │
+        └─────┴─────┴─────┘
+        """
+        show_result = self.head(n)
+        if _in_notebook():
+            from IPython.display import display_html
+
+            display_html(show_result)
+        else:
+            print(show_result)
 
     def _to_metadata(
         self,
