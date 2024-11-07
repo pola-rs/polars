db06f15ec757d3957c3f7d8a994cfc93b178cf1b
Guilhem de Viry <gdeviry@mytraffic.fr>
2024-11-07 10:57:46 +0100

    fix lint

--- py-polars/src/polars/dataframe/frame.py
+++ py-polars/src/polars/dataframe/frame.py
@@ -49,7 +49,6 @@ from polars._dependencies import pandas as pd
 from polars._dependencies import pyarrow as pa
 from polars._typing import (
     DbWriteMode,
-    FloatFmt,
     JaxExportType,
     TorchExportType,
 )
@@ -156,6 +155,7 @@ if TYPE_CHECKING:
         DbWriteEngine,
         EngineType,
         FillNullStrategy,
+        FloatFmt,
         FrameInitTypes,
         IndexOrder,
         IntoExpr,
