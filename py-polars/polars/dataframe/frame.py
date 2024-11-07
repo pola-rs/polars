6d9bfe441ee93e1a5778c987a05ccb7c96af78ff
Guilhem de Viry <gdeviry@mytraffic.fr>
2024-11-07 10:57:46 +0100

    fix lint

--- py-polars/polars/dataframe/frame.py
+++ py-polars/polars/dataframe/frame.py
@@ -30,7 +30,6 @@ import polars._reexport as pl
 from polars import functions as F
 from polars._typing import (
     DbWriteMode,
-    FloatFmt,
     JaxExportType,
     TorchExportType,
 )
@@ -149,6 +148,7 @@ if TYPE_CHECKING:
         CsvQuoteStyle,
         DbWriteEngine,
         FillNullStrategy,
+        FloatFmt,
         FrameInitTypes,
         IndexOrder,
         IntoExpr,
