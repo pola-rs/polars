4c90458812665189775ae308ac8b8da61b330fee
Ritchie Vink <ritchie46@gmail.com>
2024-11-11 17:56:00 +0100

    perf: Improve `DataFrame.sort().limit/top_k` performance (#19731)

--- crates/polars-core/src/frame/mod.rs
+++ crates/polars-core/src/frame/mod.rs
@@ -1989,6 +1989,12 @@ impl DataFrame {
             return Ok(out);
         }
         if let Some((0, k)) = slice {
+            let desc = if sort_options.descending.len() == 1 {
+                sort_options.descending[0]
+            } else {
+                false
+            };
+            sort_options.limit = Some((k as IdxSize, desc));
             return self.bottom_k_impl(k, by_column, sort_options);
         }
 
@@ -2012,6 +2018,7 @@ impl DataFrame {
                     nulls_last: sort_options.nulls_last[0],
                     multithreaded: sort_options.multithreaded,
                     maintain_order: sort_options.maintain_order,
+                    limit: sort_options.limit,
                 };
                 // fast path for a frame with a single series
                 // no need to compute the sort indices and then take by these indices
