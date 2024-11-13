534f9dbb618b988df8757ccbfad4eda141c33ef5
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-11-13 14:35:37 -0500

    search_sorted() can give (from our perspective) false positives.

--- crates/polars-plan/src/dsl/function_expr/index_of.rs
+++ crates/polars-plan/src/dsl/function_expr/index_of.rs
@@ -11,23 +11,32 @@ pub(super) fn index_of(s: &mut [Column]) -> PolarsResult<Option<Column>> {
     };
     let is_sorted_flag = series.is_sorted_flag();
     let result = match is_sorted_flag {
-        IsSorted::Not => index_of_op(series, value)?,
         // If the Series is sorted, we can use an optimized binary search to
         // find the value.
-        IsSorted::Ascending | IsSorted::Descending => {
-            let Ok(value) = s[1].as_materialized_series().strict_cast(series.dtype()) else {
+        IsSorted::Ascending | IsSorted::Descending if !value.is_null() => {
+            let Ok(value_as_series) = s[1].as_materialized_series().strict_cast(series.dtype()) else {
                 // If we can't cast, means we couldn't find the value.
                 return Ok(None);
             };
             search_sorted(
                 series,
-                &value,
-                SearchSortedSide::Any, // TODO should this be someting different?
+                &value_as_series,
+                SearchSortedSide::Left,
                 IsSorted::Descending == is_sorted_flag,
             )?
             .get(0)
-            .map(|v| v as usize)
+            .map(|idx| {
+                // search_sorted() gives an index even if it's not an exact
+                // match! So we want to make sure it actually found the value.
+                let value = value.strict_cast(series.dtype())?;
+                if series.get(idx as usize).ok()? == value {
+                    Some(idx as usize)
+                } else {
+                    None
+                }
+            }).flatten()
         },
+        _ => index_of_op(series, value)?,
     };
     Ok(result.map(|r| Column::new(series.name().clone(), [r as IdxSize])))
 }
