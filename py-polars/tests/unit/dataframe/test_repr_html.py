e775f89ce20ec64a7064ffd61f3433ac78445daa
Thijs Nieuwdorp <thijsnieuwdorp@hotmail.com>
2024-11-14 13:43:06 +0100

    Replace spaces with &nbsp; in HTML repr

--- py-polars/tests/unit/dataframe/test_repr_html.py
+++ py-polars/tests/unit/dataframe/test_repr_html.py
@@ -77,3 +77,22 @@ def test_series_repr_html_max_rows_default() -> None:
 
     expected_rows = 10
     assert html.count("<td>") - 2 == expected_rows
+
+
+def test_html_representation_multiple_spaces() -> None:
+    df = pl.DataFrame(
+        {"string_col": ["multiple   spaces", "  trailing and leading   "]}
+    )
+    html_repr = df._repr_html_()
+
+    assert (
+        html_repr
+        == """<div><style>
+.dataframe > thead > tr,
+.dataframe > tbody > tr {
+  text-align: right;
+  white-space: pre-wrap;
+}
+</style>
+<small>shape: (2, 1)</small><table border="1" class="dataframe"><thead><tr><th>string_col</th></tr><tr><td>str</td></tr></thead><tbody><tr><td>&quot;multiple&nbsp;&nbsp;&nbsp;spaces&quot;</td></tr><tr><td>&quot;&nbsp;&nbsp;trailing&nbsp;and&nbsp;leading&nbsp;&nbsp;&nbsp;&quot;</td></tr></tbody></table></div>"""
+    )
