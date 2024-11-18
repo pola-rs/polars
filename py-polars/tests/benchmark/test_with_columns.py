d1e5ed446c66c706082d2b7b9feae56434c88287
Simon Lin <simonlin.rqmmw@slmail.me>
2024-11-18 21:58:16 +1100

    c

--- py-polars/tests/benchmark/test_with_columns.py
+++ py-polars/tests/benchmark/test_with_columns.py
@@ -17,25 +17,26 @@ def test_with_columns_quadratic_19503() -> None:
     data2 = {f"feature_{i}": [0] for i in range(num_columns)}
     df2 = pl.DataFrame(data2)
 
-    df3 = df2.select(cs.by_index(range(num_columns // 1000)))
-
     times = []  # [slow, fast]
 
     class _:
+        rhs = df2
         t = perf_counter()
-        df1.with_columns(df2)
+        df1.with_columns(rhs)
         times.append(perf_counter() - t)
 
     class _:  # type: ignore[no-redef]
+        rhs = df2.select(cs.by_index(range(num_columns // 1_000)))
         t = perf_counter()
-        df1.with_columns(df3)
+        df1.with_columns(rhs)
         times.append(perf_counter() - t)
-
-    # Assert the relative rather than exact runtime to avoid flakiness in CI
-    # From local testing, the fixed version was roughly 20x, while the quadratic
-    # version was roughly 200x.
 
     factor = times[0] / times[1]
 
-    if factor > 30:
+    # Assert the relative rather than exact runtime to avoid flakiness in CI
+    #                    1.12.0 | 1.14.0
+    #   M3 Pro 11-core |   200x |    20x
+    #  EC2 47i.4xlarge |   150x |    13x
+    # GitHub CI runner |        |    50x
+    if factor > 80:
         raise AssertionError(factor)
