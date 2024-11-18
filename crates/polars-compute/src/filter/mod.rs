39705f1fd2b3df7c088ed904b088b909341d0764
Gijs Burghoorn <me@gburghoorn.com>
2024-11-18 16:55:51 +0100

    perf: Initially trim leading and trailing filtered rows (#19850)

--- crates/polars-compute/src/filter/mod.rs
+++ crates/polars-compute/src/filter/mod.rs
@@ -26,6 +26,20 @@ pub fn filter(array: &dyn Array, mask: &BooleanArray) -> Box<dyn Array> {
 }
 
 pub fn filter_with_bitmap(array: &dyn Array, mask: &Bitmap) -> Box<dyn Array> {
+    // Many filters involve filtering values in a subsection of the array. When we trim the leading
+    // and trailing filtered items, we can close in on those items and not have to perform and
+    // thinking about those. The overhead for when there are no leading or trailing filtered values
+    // is very minimal: only a clone of the mask and the array.
+    //
+    // This also allows dispatching to the fast paths way, way, way more often.
+    let mut mask = mask.clone();
+    let leading_zeros = mask.take_leading_zeros();
+    mask.take_trailing_zeros();
+    let array = array.sliced(leading_zeros, mask.len());
+
+    let mask = &mask;
+    let array = array.as_ref();
+
     // Fast-path: completely empty or completely full mask.
     let false_count = mask.unset_bits();
     if false_count == mask.len() {
