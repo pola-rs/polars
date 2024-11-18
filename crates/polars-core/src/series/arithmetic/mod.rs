191b94fd0136fdccb87bcab3b7d82ddcc2831d21
Simon Lin <simonlin.rqmmw@slmail.me>
2024-11-18 14:51:01 +1100

    c

--- crates/polars-core/src/series/arithmetic/mod.rs
+++ crates/polars-core/src/series/arithmetic/mod.rs
@@ -7,6 +7,7 @@ use std::borrow::Cow;
 use std::ops::{Add, Div, Mul, Rem, Sub};
 
 pub use borrowed::*;
+#[cfg(feature = "dtype-array")]
 pub use fixed_size_list::NumericFixedSizeListOp;
 pub use list_borrowed::NumericListOp;
 use num_traits::{Num, NumCast};
