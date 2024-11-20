3aeada334f5be10cfe90139735846a4af89b5c06
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-11-20 13:11:54 -0500

    Remove more usage of iter().

--- crates/polars-ops/src/series/ops/index_of.rs
+++ crates/polars-ops/src/series/ops/index_of.rs
@@ -1,3 +1,4 @@
+use arrow::array::PrimitiveArray;
 use polars_core::downcast_as_macro_arg_physical;
 use polars_core::prelude::*;
 use polars_utils::float::IsFloat;
@@ -12,10 +13,34 @@ trait ChunkSearch<'a, T> {
 fn index_of_predicate<P, T>(ca: &ChunkedArray<T>, predicate: P) -> Option<usize>
 where
     T: PolarsNumericType,
-    P: Fn(T::Native) -> bool,
+    P: Fn(&T::Native) -> bool,
 {
-    let predicate = &predicate;
-    ca.iter().position(|v| v.map(predicate) == Some(true))
+    let mut index = 0;
+    for chunk in ca.chunks() {
+        let chunk = chunk
+            .as_any()
+            .downcast_ref::<PrimitiveArray<T::Native>>()
+            .unwrap();
+        if let Some(_) = chunk.validity() {
+            for maybe_value in chunk.iter() {
+                if maybe_value.map(&predicate) == Some(true) {
+                    return Some(index);
+                } else {
+                    index += 1;
+                }
+            }
+        } else {
+            // No nulls, so we can simplify:
+            for value in chunk.values_iter() {
+                if predicate(value) {
+                    return Some(index);
+                } else {
+                    index += 1;
+                }
+            }
+        }
+    }
+    None
 }
 
 impl<'a, T> ChunkSearch<'a, T::Native> for ChunkedArray<T>
@@ -31,7 +56,7 @@ where
 
         // Searching for an actual value:
         if let Some(value) = value {
-            return index_of_predicate(self, |v| v == value);
+            return index_of_predicate(self, |v| *v == value);
         }
 
         // Searching for null:
