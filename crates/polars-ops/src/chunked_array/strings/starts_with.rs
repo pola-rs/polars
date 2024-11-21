ad22190f8a00302ddd80f3aafc3a23e783408f5f
Stijn Herfst <stijn.herfst@polars.tech>
2024-11-21 14:24:10 +0100

    Specializations

--- crates/polars-ops/src/chunked_array/strings/starts_with.rs
+++ crates/polars-ops/src/chunked_array/strings/starts_with.rs
@@ -4,7 +4,7 @@ use polars_utils::slice::SliceAble;
 
 /// Checks if the string starts with the prefix
 /// When the prefix is smaller than View::MAX_INLINE_SIZE then this will be very fast
-pub fn starts_with(view: View, prefix: &str, buffers: &[Buffer<u8>]) -> bool {
+pub(crate) fn starts_with_str(view: View, prefix: &str, buffers: &[Buffer<u8>]) -> bool {
     unsafe {
         if view.length <= View::MAX_INLINE_SIZE {
             view.get_inlined_slice_unchecked()
@@ -21,5 +21,27 @@ pub fn starts_with(view: View, prefix: &str, buffers: &[Buffer<u8>]) -> bool {
             }
             false
         }
+    }
+}
+
+/// Checks if the string starts with the prefix
+/// If you call this in a loop and the prefix doesn't change then prefer starts_with_str()
+pub(crate) fn starts_with_view(
+    view: View,
+    prefix: View,
+    left_buffers: &[Buffer<u8>],
+    right_buffers: &[Buffer<u8>],
+) -> bool {
+    unsafe {
+        if !view.prefix.to_le_bytes()[0..view.length.min(4) as usize]
+            .starts_with(&prefix.prefix.to_le_bytes()[..view.length.min(4) as usize])
+        {
+            return false;
+        }
+
+        let left_buffer = view.get_slice_unchecked(left_buffers);
+        let right_buffer = prefix.get_slice_unchecked(right_buffers);
+
+        left_buffer.starts_with(right_buffer)
     }
 }
