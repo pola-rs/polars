82bf33316c22bfd3a87291344258b89e6d24bb24
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-12-02 13:29:04 -0500

    Fix searching for nulls (and speed it up!) by moving it earlier in the processing.

--- crates/polars-ops/src/series/ops/index_of.rs
+++ crates/polars-ops/src/series/ops/index_of.rs
@@ -5,8 +5,8 @@ use polars_core::prelude::*;
 use polars_utils::total_ord::TotalEq;
 use row_encode::encode_rows_unordered;
 
-/// Annoyingly, iter() and values_iter() are not part of a trait, so we have to
-/// create our own.
+/// Annoyingly, iter() and values_iter() on Arrow arrays are not part of the
+/// Array trait, so we have to create our own.
 trait IterableArray<'a>: Array {
     type Value: TotalEq;
 
@@ -43,61 +43,43 @@ impl<'a> IterableArray<'a> for BinaryArray<i64> {
 }
 
 /// Find the index of the value, or ``None`` if it can't be found.
-fn index_of_value<'a, DT, AR>(ca: &'a ChunkedArray<DT>, value: Option<AR::Value>) -> Option<usize>
+fn index_of_value<'a, DT, AR>(ca: &'a ChunkedArray<DT>, value: AR::Value) -> Option<usize>
 where
     DT: PolarsDataType,
     AR: IterableArray<'a>,
 {
-    // Searching for an actual value:
-    if let Some(value) = value {
-        let req_value = &value;
-        let mut index = 0;
-        for chunk in ca.chunks() {
-            let chunk = chunk.as_any().downcast_ref::<AR>().unwrap();
-            if chunk.validity().is_some() {
-                for maybe_value in chunk.wrapped_iter() {
-                    if maybe_value.map(|v| v.tot_eq(req_value)) == Some(true) {
-                        return Some(index);
-                    } else {
-                        index += 1;
-                    }
-                }
-            } else {
-                // A lack of a validity bitmap means there are no nulls, so we
-                // can simplify our logic and use a faster code path:
-                for value in chunk.wrapped_values_iter() {
-                    if value.tot_eq(req_value) {
-                        return Some(index);
-                    } else {
-                        index += 1;
-                    }
-                }
-            }
-        }
-        return None;
-    }
-
-    // Searching for null:
+    let req_value = &value;
     let mut index = 0;
     for chunk in ca.chunks() {
-        let length = chunk.len();
-        if let Some(bitmap) = chunk.validity() {
-            let leading_ones = bitmap.leading_ones();
-            if leading_ones < length {
-                return Some(index + leading_ones);
+        let chunk = chunk.as_any().downcast_ref::<AR>().unwrap();
+        if chunk.validity().is_some() {
+            for maybe_value in chunk.wrapped_iter() {
+                if maybe_value.map(|v| v.tot_eq(req_value)) == Some(true) {
+                    return Some(index);
+                } else {
+                    index += 1;
+                }
             }
         } else {
-            index += length;
+            // A lack of a validity bitmap means there are no nulls, so we
+            // can simplify our logic and use a faster code path:
+            for value in chunk.wrapped_values_iter() {
+                if value.tot_eq(req_value) {
+                    return Some(index);
+                } else {
+                    index += 1;
+                }
+            }
         }
     }
-    None
+    return None;
 }
 
-fn index_of_numeric_value<T>(ca: &ChunkedArray<T>, value: Option<T::Native>) -> Option<usize>
+fn index_of_numeric_value<T>(ca: &ChunkedArray<T>, value: T::Native) -> Option<usize>
 where
     T: PolarsNumericType,
 {
-    index_of_value::<_, PrimitiveArray<T::Native>>(ca, value.as_ref())
+    index_of_value::<_, PrimitiveArray<T::Native>>(ca, &value)
 }
 
 /// Try casting the value to the correct type, then call index_of().
@@ -105,28 +87,43 @@ macro_rules! try_index_of_numeric_ca {
     ($ca:expr, $value:expr) => {{
         let ca = $ca;
         let value = $value;
-        // extract() returns None if casting failed, but we're using None to
-        // imply a null. So handle nulls first, and then consider an extract()
+        // extract() returns None if casting failed, and at this point we
+        // already have dealt with the nulls case, so consider an extract()
         // failure as not finding the value.
-        if *value == AnyValue::Null {
-            return Ok(index_of_numeric_value(ca, None));
-        }
         let Some(value) = value.extract() else {
             return Ok(None);
         };
-        index_of_numeric_value(ca, Some(value))
+        index_of_numeric_value(ca, value)
     }};
 }
 
 /// Find the index of a given value (the first and only entry in `value_series`)
 /// within the series.
 pub fn index_of(series: &Series, value: &AnyValue<'_>) -> PolarsResult<Option<usize>> {
-    if *series.dtype() == DataType::Null {
+    // Series is null:
+    if series.dtype().is_null() {
         if value.is_null() {
             return Ok((series.len() > 0).then_some(0));
         } else {
             return Ok(None);
         }
+    }
+
+    // Series is not null, and the value is null:
+    if value.dtype().is_null() {
+        let mut index = 0;
+        for chunk in series.chunks() {
+            let length = chunk.len();
+            if let Some(bitmap) = chunk.validity() {
+                let leading_ones = bitmap.leading_ones();
+                if leading_ones < length {
+                    return Ok(Some(index + leading_ones));
+                }
+            } else {
+                index += length;
+            }
+        }
+        return Ok(None);
     }
 
     if series.dtype().is_numeric() {
@@ -142,7 +139,9 @@ pub fn index_of(series: &Series, value: &AnyValue<'_>) -> PolarsResult<Option<us
     // bytes.
     let value_as_series =
         encode_rows_unordered(&[Series::from_any_values("".into(), &[value.clone()], false)?])?;
-    let value = value_as_series.first();
+    let value = value_as_series
+        .first()
+        .expect("We already handled nulls earlier?!");
     let ca = encode_rows_unordered(&[series.clone()])?;
     Ok(index_of_value::<_, BinaryArray<i64>>(&ca, value))
 }
