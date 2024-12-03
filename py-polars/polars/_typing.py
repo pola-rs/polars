a0fae1e69190e461dd11d5102cc11304f31d35fa
Stijn Herfst <stijn.herfst@polars.tech>
2024-12-03 13:14:39 +0100

    Formatting

--- py-polars/polars/_typing.py
+++ py-polars/polars/_typing.py
@@ -108,7 +108,9 @@ IndexOrder: TypeAlias = Literal["c", "fortran"]
 IpcCompression: TypeAlias = Literal["uncompressed", "lz4", "zstd"]
 JoinValidation: TypeAlias = Literal["m:m", "m:1", "1:m", "1:1"]
 Label: TypeAlias = Literal["left", "right", "datapoint"]
-MaintainOrderJoin: TypeAlias = Literal["none", "left", "right", "left_right", "right_left"]
+MaintainOrderJoin: TypeAlias = Literal[
+    "none", "left", "right", "left_right", "right_left"
+]
 NonExistent: TypeAlias = Literal["raise", "null"]
 NullBehavior: TypeAlias = Literal["ignore", "drop"]
 ParallelStrategy: TypeAlias = Literal[
