5f3e8a66afc5288022791eb16dc3be40c1e33f32
Nimit <62750523+nimit@users.noreply.github.com>
2024-12-04 01:26:10 +0000

    pass additional parameter to Zip from IR::HConcat for strict concat

--- crates/polars-stream/src/nodes/zip.rs
+++ crates/polars-stream/src/nodes/zip.rs
@@ -275,7 +275,8 @@ impl ComputeNode for ZipNode {
                 for input_head in &mut self.input_heads {
                     out.push(input_head.take(common_size));
                 }
-                let out_df = concat_df_horizontal(&out, false, false)?;
+                let strict_concat = !self.null_extend;
+                let out_df = concat_df_horizontal(&out, false, strict_concat)?;
                 out.clear();
 
                 let morsel = Morsel::new(out_df, self.out_seq, source_token.clone());
@@ -320,7 +321,8 @@ impl ComputeNode for ZipNode {
                 for input_head in &mut self.input_heads {
                     out.push(input_head.consume_broadcast());
                 }
-                let out_df = concat_df_horizontal(&out, false, false)?;
+                let strict_concat = !self.null_extend;
+                let out_df = concat_df_horizontal(&out, false, strict_concat)?;
                 out.clear();
 
                 let morsel = Morsel::new(out_df, self.out_seq, source_token.clone());
--- crates/polars-stream/src/physical_plan/lower_ir.rs
+++ crates/polars-stream/src/physical_plan/lower_ir.rs
@@ -299,8 +299,9 @@ pub fn lower_ir(
         IR::HConcat {
             inputs,
             schema: _,
-            options: _,
+            options,
         } => {
+            let null_extend = !options.strict;
             let inputs = inputs
                 .clone() // Needed to borrow ir_arena mutably.
                 .into_iter()
@@ -308,7 +309,7 @@ pub fn lower_ir(
                 .collect::<Result<_, _>>()?;
             PhysNodeKind::Zip {
                 inputs,
-                null_extend: true,
+                null_extend,
             }
         },
 
