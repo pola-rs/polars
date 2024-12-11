66d873d269dc10f6a100e419e4eb80340fea9f70
Simon Lin <simonlin.rqmmw@slmail.me>
2024-12-11 23:16:21 +1100

    docs(python): Update example for writing to cloud storage

--- docs/source/src/python/user-guide/io/cloud-storage.py
+++ docs/source/src/python/user-guide/io/cloud-storage.py
@@ -77,20 +77,18 @@ dset = ds.dataset("s3://my-partitioned-folder/", format="parquet")
 # --8<-- [end:scan_pyarrow_dataset]
 
 # --8<-- [start:write_parquet]
-
 import polars as pl
-import s3fs
 
-df = pl.DataFrame({
-    "foo": ["a", "b", "c", "d", "d"],
-    "bar": [1, 2, 3, 4, 5],
-})
+df = pl.DataFrame(
+    {
+        "foo": ["a", "b", "c", "d", "d"],
+        "bar": [1, 2, 3, 4, 5],
+    }
+)
 
-fs = s3fs.S3FileSystem()
 destination = "s3://bucket/my_file.parquet"
 
-# write parquet
-with fs.open(destination, mode='wb') as f:
-    df.write_parquet(f)
+df.write_parquet(destination)
+
 # --8<-- [end:write_parquet]
 """
