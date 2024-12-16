aad95441466fec2df9c53b244f7e864aefd20ae3
Itamar Turner-Trauring <itamar@pythonspeed.com>
2024-12-16 12:06:04 -0500

    Error out instead of giving the wrong result

--- crates/polars-ops/src/series/ops/search_sorted.rs
+++ crates/polars-ops/src/series/ops/search_sorted.rs
@@ -8,6 +8,10 @@ pub fn search_sorted(
     side: SearchSortedSide,
     descending: bool,
 ) -> PolarsResult<IdxCa> {
+    if s.dtype().is_categorical()
+    {
+        unimplemented!("Categorical search_sorted() can give incorrect result until https://github.com/pola-rs/polars/issues/20318 is fixed");
+    }
     let original_dtype = s.dtype();
     let s = s.to_physical_repr();
     let phys_dtype = s.dtype();
