4b9a2f6ee4a9d9c67d5a113b963ec8da4eee535f
Stijn Herfst <stijn.herfst@polars.tech>
2024-12-19 09:45:49 +0100

    Fix formatting

--- py-polars/tests/unit/operations/test_join.py
+++ py-polars/tests/unit/operations/test_join.py
@@ -1247,13 +1247,7 @@ def test_join_preserve_order_left() -> None:
         right, on="a", how="right", maintain_order="left"
     ).collect()
     print(right_left)
-    assert right_left.get_column("a").cast(pl.UInt32).to_list() == [
-        2,
-        1,
-        1,
-        None,
-        6
-    ]
+    assert right_left.get_column("a").cast(pl.UInt32).to_list() == [2, 1, 1, None, 6]
 
     right_right = left.join(
         right, on="a", how="right", maintain_order="right"
