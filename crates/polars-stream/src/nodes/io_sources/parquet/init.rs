119e29c76bde6775bbebb8520529a6a8967534b1
Orson Peters <orsonpeters@gmail.com>
2024-12-20 15:22:31 +0100

    clippy

--- crates/polars-stream/src/nodes/io_sources/parquet/init.rs
+++ crates/polars-stream/src/nodes/io_sources/parquet/init.rs
@@ -150,7 +150,7 @@ impl ParquetSourceNode {
                     })
                 }
 
-                if decoded_dfs.len() == 0 {
+                if decoded_dfs.is_empty() {
                     let Some(prefetch) = prefetches.pop_front() else {
                         break;
                     };
