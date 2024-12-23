4f7bc98b911f728b73560e78e31802f6e941abb8
coastalwhite <me@gburghoorn.com>
2024-12-23 13:53:31 +0100

    fmt

--- crates/polars-core/src/chunked_array/struct_/mod.rs
+++ crates/polars-core/src/chunked_array/struct_/mod.rs
@@ -161,7 +161,6 @@ impl StructChunked {
                 .map(|s| s.to_physical_repr().into_owned()),
         );
 
-
         let mut ca = constructor(self.name().clone(), self.length, physicals.iter());
         if self.null_count() > 0 {
             ca.zip_outer_validity(self);
