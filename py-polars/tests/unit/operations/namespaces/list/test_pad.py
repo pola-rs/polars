b36433ac3fba7c53803212a22d86552dbc526895
etiennebacher <etienne.bacher@protonmail.com>
2025-01-15 12:03:34 +0100

    more tests [skip ci]

--- py-polars/tests/unit/operations/namespaces/list/test_pad.py
+++ py-polars/tests/unit/operations/namespaces/list/test_pad.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
-from datetime import timedelta
+from datetime import date, datetime, timedelta
+from typing import Any
 
 import pytest
 
 import polars as pl
 from polars.exceptions import (
     InvalidOperationError,
+    SchemaError,
 )
 from polars.testing import assert_frame_equal
 
@@ -48,6 +50,11 @@ def test_list_pad_start_casts_to_supertype() -> None:
     expected = pl.DataFrame({"a": [["1", "a"], ["a", "b"]]})
     assert_frame_equal(result, expected)
 
+    with pytest.raises(SchemaError, match="failed to determine supertype"):
+        pl.DataFrame({"a": [[]]}, schema={"a": pl.List(pl.Categorical)}).select(
+            pl.col("a").list.pad_start(True)
+        )
+
 
 def test_list_pad_start_errors() -> None:
     df = pl.DataFrame({"a": [["a"], ["a", "b"]]})
@@ -56,3 +63,18 @@ def test_list_pad_start_errors() -> None:
         df.select(pl.col("a").list.pad_start())
     with pytest.raises(InvalidOperationError, match="to String not supported"):
         df.select(pl.col("a").list.pad_start(timedelta(days=1)))
+
+
+@pytest.mark.parametrize(
+    ("fill_value", "type"),
+    [
+        (True, pl.Boolean),
+        (timedelta(days=1), pl.Duration,),
+        (date(2022, 1, 1), pl.Date),
+        (datetime(2022, 1, 1, 23), pl.Datetime),
+    ],
+)
+def test_list_pad_start_unsupported_type(fill_value: Any, type: Any) -> None:
+    df = pl.DataFrame({"a": [[], []]}, schema={"a": pl.List(type)})
+    with pytest.raises(InvalidOperationError, match="doesn't work on data type"):
+        df.select(pl.col("a").list.pad_start(fill_value))
