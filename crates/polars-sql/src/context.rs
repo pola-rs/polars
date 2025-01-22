8e58e2398d470f8467d52d5ae79785221365ee5e
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2025-01-22 16:56:10 -0500

    fix: simplify unsupported SQL query handling in `process_query`

--- crates/polars-sql/src/context.rs
+++ crates/polars-sql/src/context.rs
@@ -347,10 +347,6 @@ impl SQLContext {
                 }
             },
             op => {
-                let op = match op {
-                    SetExpr::SetOperation { op, .. } => op,
-                    _ => polars_bail!(SQLInterface: "'{}' is currently unsupported", op),
-                };
                 polars_bail!(SQLInterface: "'{}' operation is currently unsupported", op)
             },
         }
