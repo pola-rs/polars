69328e379d58f3a20b838bc3a6a30dc4e0b6d433
etiennebacher <etienne.bacher@protonmail.com>
2025-01-23 10:33:31 +0100

    init

--- py-polars/tests/unit/datatypes/test_categorical.py
+++ py-polars/tests/unit/datatypes/test_categorical.py
@@ -848,8 +848,6 @@ def test_cat_preserve_lexical_ordering_on_concat() -> None:
     assert df2["x"].dtype == dtype
 
 
-# TODO: Bug see: https://github.com/pola-rs/polars/issues/20440
-@pytest.mark.may_fail_auto_streaming
 @pytest.mark.usefixtures("test_global_and_local")
 def test_cat_append_lexical_sorted_flag() -> None:
     df = pl.DataFrame({"x": [0, 1, 1], "y": ["B", "B", "A"]}).with_columns(
