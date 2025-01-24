2722454671599eb36e5b56efcd9c3f2e1a0e0f34
banflam <rishiagrawal2002@gmail.com>
2025-01-24 14:32:49 -0500

    Added error checking for max Excel rows, and raised exception when the max rows are exceeded for Excel

--- py-polars/polars/dataframe/frame.py
+++ py-polars/polars/dataframe/frame.py
@@ -97,6 +97,7 @@ from polars.dependencies import numpy as np
 from polars.dependencies import pandas as pd
 from polars.dependencies import pyarrow as pa
 from polars.exceptions import (
+    InvalidOperationError,
     ColumnNotFoundError,
     ModuleUpgradeRequiredError,
     NoRowsReturnedError,
@@ -3505,6 +3506,11 @@ class DataFrame:
             + int(bool(column_totals)),
             table_start[1] + len(df.columns) - 1,
         )
+        
+        excel_max_valid_rows = 1048575
+        if len(self.rows) > excel_max_valid_rows:
+            msg = "Dataframe too large to be compatible with Excel. Exceeded Excel limit of 1048575 rows of data."
+            raise InvalidOperationError(msg)
 
         # write table structure and formats into the target sheet
         if not is_empty or include_header:
