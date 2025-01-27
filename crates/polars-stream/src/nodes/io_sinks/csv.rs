ec0107e966b527e097fc25054305053ef59540a1
coastalwhite <me@gburghoorn.com>
2025-01-27 15:09:02 +0100

    fix: Allow for respawning of new streaming sinks

--- crates/polars-stream/src/nodes/io_sinks/csv.rs
+++ crates/polars-stream/src/nodes/io_sinks/csv.rs
@@ -1,5 +1,4 @@
 use std::cmp::Reverse;
-use std::io::Write;
 use std::path::{Path, PathBuf};
 
 use polars_core::frame::DataFrame;
@@ -10,16 +9,21 @@ use polars_io::prelude::{CsvWriter, CsvWriterOptions};
 use polars_io::SerWriter;
 use polars_utils::priority::Priority;
 
-use crate::async_primitives::linearizer::Linearizer;
+use crate::async_primitives::linearizer::{Inserter, Linearizer};
 use crate::nodes::{ComputeNode, JoinHandle, MorselSeq, PortState, TaskPriority, TaskScope};
 use crate::pipe::{RecvPort, SendPort};
 use crate::DEFAULT_LINEARIZER_BUFFER_SIZE;
 
+type Linearized = Priority<Reverse<MorselSeq>, Vec<u8>>;
 pub struct CsvSinkNode {
     path: PathBuf,
     schema: SchemaRef,
 
     write_options: CsvWriterOptions,
+
+    // IO task related:
+    senders: Vec<Inserter<Linearized>>,
+    io_task: Option<tokio::task::JoinHandle<PolarsResult<()>>>,
 }
 
 impl CsvSinkNode {
@@ -33,6 +37,9 @@ impl CsvSinkNode {
             schema,
 
             write_options: write_options.clone(),
+
+            senders: Vec::new(),
+            io_task: None,
         })
     }
 }
@@ -40,6 +47,55 @@ impl CsvSinkNode {
 impl ComputeNode for CsvSinkNode {
     fn name(&self) -> &str {
         "csv_sink"
+    }
+
+    fn initialize(&mut self, num_pipelines: usize) {
+        // Encode tasks -> IO task
+        let (mut linearizer, senders) =
+            Linearizer::<Linearized>::new(num_pipelines, DEFAULT_LINEARIZER_BUFFER_SIZE);
+
+        self.senders = senders;
+
+        // IO task.
+        //
+        // Task that will actually do write to the target file.
+        let io_runtime = polars_io::pl_async::get_runtime();
+
+        let path = self.path.clone();
+        let schema = self.schema.clone();
+        let include_header = self.write_options.include_header;
+        let include_bom = self.write_options.include_bom;
+
+        self.io_task = Some(io_runtime.spawn(async move {
+            use tokio::fs::OpenOptions;
+            use tokio::io::AsyncWriteExt;
+
+            let mut file = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(path.as_path())
+                .await
+                .map_err(|err| polars_utils::_limit_path_len_io_err(path.as_path(), err))?;
+
+            // Write the header
+            if include_header || include_bom {
+                let mut std_file = file.into_std().await;
+                let mut writer = CsvWriter::new(&mut std_file)
+                    .include_bom(include_bom)
+                    .include_header(include_header)
+                    .n_threads(1) // Disable rayon parallelism
+                    .batched(&schema)?;
+                writer.write_batch(&DataFrame::empty_with_schema(&schema))?;
+                file = tokio::fs::File::from_std(std_file);
+            }
+
+            while let Some(Priority(_, buffer)) = linearizer.get().await {
+                file.write_all(&buffer).await?;
+            }
+
+            PolarsResult::Ok(())
+        }));
     }
 
     fn update_state(&mut self, recv: &mut [PortState], send: &mut [PortState]) -> PolarsResult<()> {
@@ -50,6 +106,13 @@ impl ComputeNode for CsvSinkNode {
         // also done.
         if recv[0] != PortState::Done {
             recv[0] = PortState::Ready;
+        } else if let Some(io_task) = self.io_task.take() {
+            // Stop the IO task from waiting for more morsels.
+            self.senders.clear();
+
+            polars_io::pl_async::get_runtime()
+                .block_on(io_task)
+                .unwrap_or_else(|e| Err(std::io::Error::from(e).into()))?;
         }
 
         Ok(())
@@ -68,13 +131,9 @@ impl ComputeNode for CsvSinkNode {
 
         // .. -> Encode task
         let receivers = recv_ports[0].take().unwrap().parallel();
-        // Encode tasks -> IO task
-        let (mut linearizer, senders) = Linearizer::<Priority<Reverse<MorselSeq>, Vec<u8>>>::new(
-            receivers.len(),
-            DEFAULT_LINEARIZER_BUFFER_SIZE,
-        );
 
-        let slf = &*self;
+        let schema = &self.schema;
+        let options = &self.write_options;
 
         // 16MB
         const DEFAULT_ALLOCATION_SIZE: usize = 1 << 24;
@@ -82,12 +141,12 @@ impl ComputeNode for CsvSinkNode {
         // Encode task.
         //
         // Task encodes the columns into their corresponding CSV encoding.
-        for (mut receiver, mut sender) in receivers.into_iter().zip(senders) {
+        for (mut receiver, sender) in receivers.into_iter().zip(self.senders.iter_mut()) {
             join_handles.push(scope.spawn_task(TaskPriority::High, async move {
                 // Amortize the allocations over time. If we see that we need to do way larger
                 // allocations, we adjust to that over time.
                 let mut allocation_size = DEFAULT_ALLOCATION_SIZE;
-                let options = slf.write_options.clone();
+                let options = options.clone();
 
                 while let Ok(morsel) = receiver.recv().await {
                     let (df, seq, _, _) = morsel.into_inner();
@@ -107,7 +166,7 @@ impl ComputeNode for CsvSinkNode {
                         .with_null_value(options.serialize_options.null.clone())
                         .with_quote_style(options.serialize_options.quote_style)
                         .n_threads(1) // Disable rayon parallelism
-                        .batched(&slf.schema)?;
+                        .batched(schema)?;
 
                     writer.write_batch(&df)?;
 
@@ -118,47 +177,5 @@ impl ComputeNode for CsvSinkNode {
                 PolarsResult::Ok(())
             }));
         }
-
-        // IO task.
-        //
-        // Task that will actually do write to the target file.
-        let io_runtime = polars_io::pl_async::get_runtime();
-
-        let path = slf.path.clone();
-        let schema = slf.schema.clone();
-        let include_header = slf.write_options.include_header;
-        let include_bom = slf.write_options.include_bom;
-
-        let io_task = io_runtime.spawn(async move {
-            use tokio::fs::OpenOptions;
-
-            let file = OpenOptions::new()
-                .write(true)
-                .create(true)
-                .truncate(true)
-                .open(path.as_path())
-                .await
-                .map_err(|err| polars_utils::_limit_path_len_io_err(path.as_path(), err))?;
-
-            let mut file = file.into_std().await;
-
-            // Write the header
-            if include_header || include_bom {
-                let mut writer = CsvWriter::new(&mut file)
-                    .include_bom(include_bom)
-                    .include_header(include_header)
-                    .n_threads(1) // Disable rayon parallelism
-                    .batched(&schema)?;
-                writer.write_batch(&DataFrame::empty_with_schema(&schema))?;
-            }
-
-            while let Some(Priority(_, buffer)) = linearizer.get().await {
-                file.write_all(&buffer)?;
-            }
-
-            PolarsResult::Ok(())
-        });
-        join_handles
-            .push(scope.spawn_task(TaskPriority::Low, async move { io_task.await.unwrap() }));
     }
 }
