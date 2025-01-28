32db4e8c2da9367fbfa0b3b4825b98fe9d946d71
Itamar Turner-Trauring <itamar@pythonspeed.com>
2025-01-28 13:43:53 -0500

    More accurate.

--- py-polars/tests/benchmark/test_list_op.py
+++ py-polars/tests/benchmark/test_list_op.py
@@ -10,7 +10,7 @@ pytestmark = pytest.mark.benchmark()
 @pytest.fixture(scope="module")
 def lists_and_values() -> pl.DataFrame:
     return pl.DataFrame(
-        {"lists": [[0, 2, 1, 3, 5]] * 1_000, "values": [3, 1, 4, 5, 0] * 200}
+        {"lists": [[0, 2, 1, 3, 5]] * 10_000, "values": [3, 1, 4, 5, 0] * 2000}
     )
 
 
@@ -18,7 +18,6 @@ def test_list_contains(
     benchmark: Callable[[Callable[[], None]], object], lists_and_values: pl.DataFrame
 ) -> None:
     def go() -> None:
-        for _ in range(10):
-            lists_and_values.select(pl.col("lists").list.contains(pl.col("values")))
+        lists_and_values.select(pl.col("lists").list.contains(pl.col("values")))
 
     benchmark(go)
