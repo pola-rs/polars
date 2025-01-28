dadad0d216707839c6f58c3d65e4c73aa4a346fb
Arnab Animesh Das <arnabanimesh@gmail.com>
2025-01-28 16:03:36 +0530

    chore: Update copyright year (#20764)

--- py-polars/docs/source/conf.py
+++ py-polars/docs/source/conf.py
@@ -26,7 +26,7 @@ sys.path.insert(0, str(Path("../..").resolve()))
 
 project = "Polars"
 author = "Ritchie Vink"
-copyright = f"2020, {author}"
+copyright = f"2025, {author}"
 
 
 # -- General configuration ---------------------------------------------------
