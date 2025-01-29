5b5630713d0b3152bbc880f97e7f9bddacfa4efd
coastalwhite <me@gburghoorn.com>
2025-01-29 11:01:32 +0100

    two state system

--- crates/polars-stream/src/nodes/merge_sorted.rs
+++ crates/polars-stream/src/nodes/merge_sorted.rs
@@ -5,8 +5,7 @@ use polars_core::schema::Schema;
 use polars_ops::frame::_merge_sorted_dfs;
 use polars_utils::pl_str::PlSmallStr;
 
-use crate::async_primitives::connector::connector;
-use crate::morsel::{get_ideal_morsel_size, SourceToken};
+use crate::morsel::SourceToken;
 use crate::nodes::compute_node_prelude::*;
 
 #[derive(Clone, Copy)]
@@ -15,15 +14,26 @@ enum Side {
     Right,
 }
 
+#[derive(Debug)]
+enum State {
+    /// Merging values from buffered or ports.
+    Merging,
+    /// Passing values along from one of the ports.
+    Passing,
+}
+
 pub struct MergeSortedNode {
     key_column_idx: usize,
+    state: State,
 
     seq: MorselSeq,
 
+    /// Already merged buffer.
     merged: DataFrame,
-    mergable: (DataFrame, DataFrame),
 
+    /// Not yet merged buffer.
     unmerged: DataFrame,
+    /// The port-side the unmerged buffer belongs to.
     unmerged_side: Side,
 }
 
@@ -34,14 +44,11 @@ impl MergeSortedNode {
 
         Self {
             key_column_idx,
+            state: State::Merging,
+
             seq: MorselSeq::default(),
 
             merged: DataFrame::empty_with_schema(&schema),
-            mergable: (
-                DataFrame::empty_with_schema(&schema),
-                DataFrame::empty_with_schema(&schema),
-            ),
-
             unmerged: DataFrame::empty_with_schema(&schema),
             unmerged_side: Side::Left,
         }
@@ -54,28 +61,28 @@ impl ComputeNode for MergeSortedNode {
     }
 
     fn update_state(&mut self, recv: &mut [PortState], send: &mut [PortState]) -> PolarsResult<()> {
-        assert!(recv.len() == 2 && send.len() == 1);
+        assert_eq!(send.len(), 1);
+        assert_eq!(recv.len(), 2);
 
         // If the output doesn't want any more data, transition to being done.
-        if dbg!(send[0]) == PortState::Done {
+        if send[0] == PortState::Done {
             recv[0] = PortState::Done;
             recv[1] = PortState::Done;
         }
 
-        dbg!(&self.unmerged);
-        dbg!(&self.mergable.0);
-        dbg!(&self.mergable.1);
-        dbg!(&self.merged);
+        self.state = match (recv[0], recv[1]) {
+            _ if !self.merged.is_empty() || !self.unmerged.is_empty() => State::Merging,
 
-        if dbg!(matches!(recv[0], PortState::Done))
-            && dbg!(matches!(recv[1], PortState::Done))
-            && self.unmerged.is_empty()
-            && self.mergable.0.is_empty()
-            && self.mergable.1.is_empty()
-            && self.merged.is_empty()
-        {
-            send[0] = PortState::Done;
-        }
+            // If one of the ports is closed and the buffers are empty, we can just start passing
+            // the morsels along the port.
+            (PortState::Done, PortState::Done) => {
+                send[0] = PortState::Done;
+                State::Passing
+            },
+            (PortState::Done, _) | (_, PortState::Done) => State::Passing,
+
+            _ => State::Merging,
+        };
 
         Ok(())
     }
@@ -88,96 +95,98 @@ impl ComputeNode for MergeSortedNode {
         _state: &'s ExecutionState,
         join_handles: &mut Vec<JoinHandle<PolarsResult<()>>>,
     ) {
-        assert!(recv_ports.len() == 2);
-        assert!(send_ports.len() == 1);
-
-        let mut send = send_ports[0].take().unwrap().serial();
-
-        let left = recv_ports[0].take().map(|p| p.serial());
-        let right = recv_ports[1].take().map(|p| p.serial());
-
-        let (mut emit_send, mut emit_recv) = connector();
-
-        let ideal_morsel_size = get_ideal_morsel_size();
+        assert_eq!(recv_ports.len(), 2);
+        assert_eq!(send_ports.len(), 1);
 
         let seq = &mut self.seq;
         let key_column_idx = self.key_column_idx;
         let merged = &mut self.merged;
-        let mergable = &mut self.mergable;
         let unmerged = &mut self.unmerged;
         let unmerged_side = &mut self.unmerged_side;
 
-        join_handles.push(scope.spawn_task(TaskPriority::Low, async move {
-            if !mergable.0.is_empty() || !mergable.1.is_empty() {
-                let l;
-                let r;
-                (mergable.0, l) = mergable.0.split_at(0);
-                (mergable.1, r) = mergable.1.split_at(0);
-                if let Err(m) = emit_send.send((l, r)).await {
-                    *mergable = m;
-                    return Ok(());
-                }
-            }
+        match self.state {
+            State::Passing => {
+                let mut send = send_ports[0].take().unwrap().serial();
 
-            match (left, right) {
-                (None, None) => {
-                    if !unmerged.is_empty() {
-                        let um;
-                        (*unmerged, um) = unmerged.split_at(0);
-                        if let Err((m, _)) = emit_send.send((um, DataFrame::empty())).await {
-                            *unmerged = m;
+                match (recv_ports[0].take(), recv_ports[1].take()) {
+                    (None, None) => {},
+                    (Some(port), None) | (None, Some(port)) => {
+                        // @TODO: Turn into parallel passing.
+                        let mut recv = port.serial();
+                        join_handles.push(scope.spawn_task(TaskPriority::High, async move {
+                            while let Ok(morsel) = recv.recv().await {
+                                match send.send(morsel).await {
+                                    Ok(_) => *seq = seq.successor(),
+                                    Err(m) => {
+                                        *merged = m.into_df();
+                                        return Ok(());
+                                    },
+                                }
+                            }
+
+                            Ok(())
+                        }));
+                    },
+                    (Some(_), Some(_)) => unreachable!(),
+                }
+            },
+            State::Merging => {
+                let source_token = SourceToken::new();
+                let mut send = send_ports[0].take().unwrap().serial();
+
+                let mut left = recv_ports[0].take().map(|p| p.serial());
+                let mut right = recv_ports[1].take().map(|p| p.serial());
+
+                join_handles.push(scope.spawn_task(TaskPriority::Low, async move {
+                    loop {
+                        if source_token.stop_requested() {
                             return Ok(());
                         }
-                    }
-                },
-                (mut left, mut right) => {
-                    loop {
-                        if left.is_none() && matches!(*unmerged_side, Side::Right) {
-                            loop {
-                                if !unmerged.is_empty() {
-                                    let um;
-                                    (*unmerged, um) = unmerged.split_at(0);
-                                    if let Err((m, _)) = emit_send.send((um, DataFrame::empty())).await {
-                                        *unmerged = m;
-                                        return Ok(());
-                                    }
-                                }
 
-                                let Ok(um) = right.as_mut().unwrap().recv().await else {
-                                    break;
-                                };
-                                *unmerged = dbg!(um.into_df());
+                        if !merged.is_empty() {
+                            // @TODO: Break unmerged up in smaller chunks as it might be very
+                            // big.
+                            let morsel = Morsel::new(merged.clone(), *seq, source_token.clone());
+                            if send.send(morsel).await.is_err() {
+                                return Ok(());
                             }
 
-                            return Ok(())
-                        } else if right.is_none() && matches!(*unmerged_side, Side::Left) {
-                            loop {
-                                if !unmerged.is_empty() {
-                                    let um;
-                                    (*unmerged, um) = unmerged.split_at(0);
-                                    if let Err((m, _)) = emit_send.send((um, DataFrame::empty())).await {
-                                        *unmerged = m;
-                                        return Ok(());
-                                    }
-                                }
-
-                                let Ok(um) = left.as_mut().unwrap().recv().await else {
-                                    break;
-                                };
-                                *unmerged = dbg!(um.into_df());
-                            }
-
-                            return Ok(())
+                            *merged = merged.clear();
+                            *seq = seq.successor();
                         }
 
-                        let other_unmerged = match *unmerged_side {
-                            Side::Left => right.as_mut().unwrap().recv().await,
-                            Side::Right => left.as_mut().unwrap().recv().await,
+                        let opposite_port = match *unmerged_side {
+                            Side::Left => right.as_mut(),
+                            Side::Right => left.as_mut(),
                         };
+
+                        let other_unmerged = match opposite_port {
+                            None => {
+                                if unmerged.is_empty() {
+                                    return Ok(());
+                                }
+
+                                // @TODO: Break unmerged up in smaller chunks as it might be very
+                                // big.
+                                if send
+                                    .send(Morsel::new(unmerged.clone(), *seq, source_token.clone()))
+                                    .await
+                                    .is_err()
+                                {
+                                    return Ok(());
+                                };
+
+                                *unmerged = unmerged.clear();
+                                *seq = seq.successor();
+                                return Ok(());
+                            },
+                            Some(port) => port.recv().await,
+                        };
+
                         let Ok(other_unmerged) = other_unmerged else {
                             return Ok(());
                         };
-                        let other_unmerged = dbg!(other_unmerged.into_df());
+                        let other_unmerged = other_unmerged.into_df();
                         let taken_unmerged = std::mem::take(unmerged);
                         let (left_unmerged, right_unmerged) = match *unmerged_side {
                             Side::Left => (taken_unmerged, other_unmerged),
@@ -194,9 +203,6 @@ impl ComputeNode for MergeSortedNode {
                             *unmerged_side = Side::Left;
                             continue;
                         }
-
-                        dbg!(&left_unmerged);
-                        dbg!(&right_unmerged);
 
                         let left_key = &left_unmerged.get_columns()[key_column_idx];
                         let right_key = &right_unmerged.get_columns()[key_column_idx];
@@ -238,65 +244,21 @@ impl ComputeNode for MergeSortedNode {
                             (left_mergable, right_unmerged)
                         };
 
-                        if let Err(m) = emit_send.send((left_mergable, right_mergable)).await {
-                            *mergable = m;
-                            return Ok(());
-                        }
+                        let left_s =
+                            left_mergable.get_columns()[key_column_idx].as_materialized_series();
+                        let right_s =
+                            right_mergable.get_columns()[key_column_idx].as_materialized_series();
+
+                        *merged = _merge_sorted_dfs(
+                            &left_mergable,
+                            &right_mergable,
+                            left_s,
+                            right_s,
+                            false,
+                        )?;
                     }
-                },
-            }
-
-            Ok(())
-        }));
-
-        join_handles.push(scope.spawn_task(TaskPriority::High, async move {
-            let source_token = SourceToken::new();
-
-            loop {
-                if !merged.is_empty() {
-                    let num_morsels = merged.height().div_ceil(ideal_morsel_size);
-                    let max_morsel_size = merged.height() / num_morsels;
-
-                    for _ in 0..num_morsels {
-                        let df = merged.head(Some(max_morsel_size));
-                        dbg!(&df);
-                        let morsel_height = df.height();
-                        let morsel = Morsel::new(df, *seq, source_token.clone());
-
-                        if send.send(morsel).await.is_err() {
-                            return Ok(());
-                        }
-
-                        *merged =
-                            merged.slice(morsel_height as i64, merged.height() - morsel_height);
-                        *seq = seq.successor();
-
-                        if source_token.stop_requested() {
-                            return Ok(());
-                        }
-                    }
-                }
-
-                let Ok((left_mergable, right_mergable)) = emit_recv.recv().await else {
-                    break;
-                };
-
-                if left_mergable.is_empty() {
-                    *merged = right_mergable;
-                } else if right_mergable.is_empty() {
-                    *merged = left_mergable;
-                } else {
-                    let left_s =
-                        left_mergable.get_columns()[key_column_idx].as_materialized_series();
-                    let right_s =
-                        right_mergable.get_columns()[key_column_idx].as_materialized_series();
-
-                    *merged =
-                        _merge_sorted_dfs(&left_mergable, &right_mergable, left_s, right_s, false)?;
-                }
-            }
-
-            Ok(())
-        }));
+                }));
+            },
+        }
     }
 }
