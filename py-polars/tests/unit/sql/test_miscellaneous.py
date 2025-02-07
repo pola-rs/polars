ae18074a82840820fea622067a646313f20aa829
Simon Lin <simonlin.rqmmw@slmail.me>
2025-02-07 14:36:28 +1100

    tests

--- py-polars/tests/unit/sql/test_miscellaneous.py
+++ py-polars/tests/unit/sql/test_miscellaneous.py
@@ -390,7 +390,7 @@ def test_invalid_cols(query: str) -> None:
 @pytest.mark.parametrize("filter_expr", ["", "WHERE 1 = 1", "WHERE a == 1 OR a != 1"])
 @pytest.mark.parametrize("order_expr", ["", "ORDER BY 1", "ORDER BY a"])
 def test_select_output_heights_20058_21084(filter_expr: str, order_expr: str) -> None:
-    df = pl.DataFrame({"a": [1, 2, 3], "list": [[1, 1], [2], [3]]})
+    df = pl.DataFrame({"a": [1, 2, 3]})
 
     # Queries that maintain original height
 
@@ -458,7 +458,8 @@ def test_select_output_heights_20058_21084(filter_expr: str, order_expr: str) ->
     )
 
 
-def test_select_unnest_height_filter_order_by() -> None:
+def test_select_explode_height_filter_order_by() -> None:
+    # Note: `unnest()` from SQL equates to `Expr.explode()`
     df = pl.DataFrame(
         {
             "list_long": [[1, 2, 3], [4, 5, 6]],
@@ -487,6 +488,16 @@ def test_select_unnest_height_filter_order_by() -> None:
         """
         ),
         pl.Series("list", [3, 4, 5, 6, 2, 1]).to_frame(),
+    )
+
+    # Literals are broadcasted to output height of UNNEST:
+    assert_frame_equal(
+        df.sql(
+            """\
+    SELECT UNNEST(list_long) as list, 1 as x FROM self ORDER BY sort_key
+        """
+        ),
+        pl.select(pl.Series("list", [2, 1, 3, 4, 5, 6]), x=1),
     )
 
     # Note: Filter applies before projections in SQL
