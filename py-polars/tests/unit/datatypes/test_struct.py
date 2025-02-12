dedc6d5e6ddf1ef744a3edb52ad07391357557ef
etiennebacher <etienne.bacher@protonmail.com>
2025-02-12 12:36:43 +0100

    fmt

--- py-polars/tests/unit/datatypes/test_struct.py
+++ py-polars/tests/unit/datatypes/test_struct.py
@@ -1223,16 +1223,17 @@ def test_leaf_list_eq_19613(data: Any) -> None:
 def test_nested_object_raises_15237() -> None:
     obj = object()
     df = pl.DataFrame({"a": [obj]})
-    with pytest.raises(
-        InvalidOperationError, match="nested objects are not allowed"
-    ):
+    with pytest.raises(InvalidOperationError, match="nested objects are not allowed"):
         df.select(pl.struct("a"))
 
 
 def test_cast_to_struct_needs_field_14083() -> None:
-    with pytest.raises(InvalidOperationError, match="must specify one field in the struct"):
+    with pytest.raises(
+        InvalidOperationError, match="must specify one field in the struct"
+    ):
         pl.Series([1], dtype=pl.Int32).cast(pl.Struct)
 
-    with pytest.raises(InvalidOperationError, match="must specify one field in the struct"):
+    with pytest.raises(
+        InvalidOperationError, match="must specify one field in the struct"
+    ):
         pl.Series([1], dtype=pl.Int32).cast(pl.Struct({"a": pl.UInt8, "b": pl.UInt8}))
-
