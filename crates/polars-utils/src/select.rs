de0b1f702cc0d680f1634110a2a858fcaff332a4
Orson Peters <orsonpeters@gmail.com>
2025-02-12 10:51:38 +0100

    fmt

--- crates/polars-utils/src/select.rs
+++ crates/polars-utils/src/select.rs
@@ -5,5 +5,9 @@ pub fn select_unpredictable<T>(cond: bool, true_val: T, false_val: T) -> T {
 
 #[cfg(not(feature = "nightly"))]
 pub fn select_unpredictable<T>(cond: bool, true_val: T, false_val: T) -> T {
-    if cond { true_val } else { false_val }
+    if cond {
+        true_val
+    } else {
+        false_val
+    }
 }
