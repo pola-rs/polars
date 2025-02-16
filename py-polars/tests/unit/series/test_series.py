87ca5abba1da6f1ca8309ab921d0c1029ed4b3a5
Jakub Valtar <jakub.valtar@gmail.com>
2025-02-16 19:28:14 +0100

    Fix incompatible type assignment in test

--- py-polars/tests/unit/series/test_series.py
+++ py-polars/tests/unit/series/test_series.py
@@ -1676,18 +1676,18 @@ def test_to_physical_rechunked_21285() -> None:
     # A series with multiple chunks, dtype is array or list of structs with a
     # null field (causes rechunking) and a field with a different physical and
     # logical repr (causes the full body of `to_physical_repr` to run).
-    dtype = pl.Array(pl.Struct({"f0": pl.Time, "f1": pl.Null}), shape=(1,))
-    s = pl.Series("a", [None], dtype)  # content doesn't matter
+    arr_dtype = pl.Array(pl.Struct({"f0": pl.Time, "f1": pl.Null}), shape=(1,))
+    s = pl.Series("a", [None], arr_dtype)  # content doesn't matter
     s = s.append(s)
-    expected_dtype = pl.Array(pl.Struct({"f0": Int64, "f1": pl.Null}), shape=(1,))
-    expected = pl.Series("a", [None, None], expected_dtype)
+    expected_arr_dtype = pl.Array(pl.Struct({"f0": Int64, "f1": pl.Null}), shape=(1,))
+    expected = pl.Series("a", [None, None], expected_arr_dtype)
     assert_series_equal(s.to_physical(), expected)
 
-    dtype = pl.List(pl.Struct({"f0": pl.Time, "f1": pl.Null}))
-    s = pl.Series("a", [None], dtype)  # content doesn't matter
+    list_dtype = pl.List(pl.Struct({"f0": pl.Time, "f1": pl.Null}))
+    s = pl.Series("a", [None], list_dtype)  # content doesn't matter
     s = s.append(s)
-    expected_dtype = pl.List(pl.Struct({"f0": Int64, "f1": pl.Null}))
-    expected = pl.Series("a", [None, None], expected_dtype)
+    expected_list_dtype = pl.List(pl.Struct({"f0": Int64, "f1": pl.Null}))
+    expected = pl.Series("a", [None, None], expected_list_dtype)
     assert_series_equal(s.to_physical(), expected)
 
 
