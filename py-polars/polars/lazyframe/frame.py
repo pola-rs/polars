309457e28469f4939a887784302a14c7f23ecef3
ritchie <ritchie46@gmail.com>
2025-02-21 15:03:38 +0100

    fmt

--- py-polars/polars/lazyframe/frame.py
+++ py-polars/polars/lazyframe/frame.py
@@ -994,7 +994,7 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
         # reshape wide result
         n_metrics = len(metrics)
         column_metrics = [
-            df_metrics.row(0)[(n * n_metrics): (n + 1) * n_metrics]
+            df_metrics.row(0)[(n * n_metrics) : (n + 1) * n_metrics]
             for n in range(schema.len())
         ]
         summary = dict(zip(schema, column_metrics))
