0c7e9ea686d2cb07a0b33ea6d84e5fa2a1a2ba5e
ritchie <ritchie46@gmail.com>
2025-02-21 09:45:59 +0100

    feat: Make user facing binary formats mostly self describing

--- crates/polars-plan/src/client/mod.rs
+++ crates/polars-plan/src/client/mod.rs
@@ -12,7 +12,8 @@ pub fn prepare_cloud_plan(dsl: DslPlan) -> PolarsResult<Vec<u8>> {
 
     // Serialize the plan.
     let mut writer = Vec::new();
-    pl_serialize::SerializeOptions::default().serialize_into_writer(&mut writer, &dsl)?;
+    pl_serialize::SerializeOptions::default()
+        .serialize_into_writer::<_, _, true>(&mut writer, &dsl)?;
 
     Ok(writer)
 }
