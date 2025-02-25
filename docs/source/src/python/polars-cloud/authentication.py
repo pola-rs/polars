08a5bdeece606fd623f3a633a5efcfdc74f93105
r-brink <auc2yz7ab@mozmail.com>
2025-02-25 15:50:10 +0100

    formatting

--- docs/source/src/python/polars-cloud/authentication.py
+++ docs/source/src/python/polars-cloud/authentication.py
@@ -1,4 +1,5 @@
 # --8<-- [start:login]
 import polars_cloud as pc
+
 workspace = pc.login()
 # --8<-- [end:login]
--- docs/source/src/python/polars-cloud/quickstart.py
+++ docs/source/src/python/polars-cloud/quickstart.py
@@ -1,13 +1,10 @@
-# ruff: noqa
 # --8<-- [start:general]
 import polars_cloud as pc
 import polars as pl
 
 ctx = pc.ComputeContext(memory=8, cpus=2, cluster_size=1)
-df = pl.DataFrame({
-    "a": [1, 2, 3],
-    "b": [4, 4, 5]
-})
-lf = df.lazy().with_columns(pl.col("a").max().over("b").alias("c"))
-lf.remote(context = ctx).sink_parquet(uri="s3://my-bucket/result.parquet")
+lf = pl.LazyFrame({"a": [1, 2, 3], "b": [4, 4, 5]}).with_columns(
+    pl.col("a").max().over("b").alias("c")
+)
+lf.remote(context=ctx).sink_parquet(uri="s3://my-bucket/result.parquet")
 # --8<-- [end:general]
