823bfc9a394a264500492da34649cd4ab2a2903a
Orson Peters <orsonpeters@gmail.com>
2025-02-27 14:51:57 +0100

    refactor(rust): Add ArrayBuilders (#21370)

--- crates/polars-arrow/src/array/struct_/mod.rs
+++ crates/polars-arrow/src/array/struct_/mod.rs
@@ -2,6 +2,8 @@ use super::{new_empty_array, new_null_array, Array, Splitable};
 use crate::bitmap::Bitmap;
 use crate::datatypes::{ArrowDataType, Field};
 
+mod builder;
+pub use builder::*;
 mod ffi;
 pub(super) mod fmt;
 mod iterator;
