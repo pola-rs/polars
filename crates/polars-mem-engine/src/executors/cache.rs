6b33c924e5f737556b942fdd8b0e25eebc8f8e8c
ritchie <ritchie46@gmail.com>
2025-03-07 14:05:15 +0100

    rewrite cache node

--- crates/polars-mem-engine/src/executors/cache.rs
+++ crates/polars-mem-engine/src/executors/cache.rs
@@ -10,33 +10,29 @@ pub struct CacheExec {
 
 impl Executor for CacheExec {
     fn execute(&mut self, state: &mut ExecutionState) -> PolarsResult<DataFrame> {
-        let cache = state.get_df_cache(self.id, self.count);
-        let mut cache_hit = true;
-        let previous = cache.0.fetch_sub(1, Ordering::Relaxed);
-        debug_assert!(previous >= 0);
+        match &mut self.input {
+            // Cache node
+            None => {
+                if state.verbose() {
+                    eprintln!("CACHE HIT: cache id: {:x}", self.id);
+                }
+                let cache = state.get_df_cache(self.id, self.count);
+                let previous = cache.0.fetch_sub(1, Ordering::Relaxed);
+                debug_assert!(previous >= 0);
 
-        let df = cache.1.get_or_try_init(|| {
-            cache_hit = false;
-            self.input
-                .as_mut()
-                .expect("should be set on pre-fill cache")
-                .execute(state)
-        })?;
-
-        // Decrement count on cache hits.
-        if cache_hit && previous == 0 {
-            state.remove_df_cache(self.id);
+                Ok(cache.1.get().unwrap().clone())
+            },
+            // Cache Prefill node
+            Some(input) => {
+                if state.verbose() {
+                    eprintln!("CACHE SET: cache id: {:x}", self.id);
+                }
+                let df = input.execute(state)?;
+                let cache = state.get_df_cache(self.id, self.count);
+                cache.1.set(df).expect("should be empty");
+                Ok(DataFrame::empty())
+            },
         }
-
-        if state.verbose() {
-            if cache_hit {
-                eprintln!("CACHE HIT: cache id: {:x}", self.id);
-            } else {
-                eprintln!("CACHE SET: cache id: {:x}", self.id);
-            }
-        }
-
-        Ok(df.clone())
     }
 }
 
