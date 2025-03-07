b1158ec90aa9c63f1ef3c9fd4a0a1236eadb1939
Matt711 <matthewmurray711@gmail.com>
2025-03-07 18:09:09 +0000

    clean up

--- crates/polars-python/src/functions/utils.rs
+++ crates/polars-python/src/functions/utils.rs
@@ -7,7 +7,7 @@ pub fn check_length(check: bool) {
     unsafe { _set_check_length(check) }
 }
 
-#[pyfunction(name="get_engine_affinity")]
+#[pyfunction(name = "get_engine_affinity")]
 pub fn py_get_engine_affinity() -> PyResult<String> {
     Ok(get_engine_affinity())
 }
