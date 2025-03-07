65efd874e1bf4b1730f761ebae87b598b0a0fcc9
Simon Lin <simonlin.rqmmw@slmail.me>
2025-03-08 01:23:29 +1100

    ci

--- py-polars/polars/_utils/nest_asyncio.py
+++ py-polars/polars/_utils/nest_asyncio.py
@@ -29,7 +29,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# Ignore all, file is currently directly copied with no changes.
+# Ignore all lints, file is currently copied.
 # ruff: noqa
 # type: ignore
 
