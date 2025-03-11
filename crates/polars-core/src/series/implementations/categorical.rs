3d978c279b89cdb5e0df04d1d3bc1fb1b7ad3c38
Orson Peters <orsonpeters@gmail.com>
2025-03-11 19:47:14 +0100

    refactor: Add as_phys_any to PrivateSeries for downcasting (#21696)

--- crates/polars-core/src/series/implementations/categorical.rs
+++ crates/polars-core/src/series/implementations/categorical.rs
@@ -312,6 +312,10 @@ impl SeriesTrait for SeriesWrap<CategoricalChunked> {
         &mut self.0
     }
 
+    fn as_phys_any(&self) -> &dyn Any {
+        self.0.physical()
+    }
+
     fn as_arc_any(self: Arc<Self>) -> Arc<dyn Any + Send + Sync> {
         self as _
     }
