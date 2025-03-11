5d84fb365fe7da84bc7200d941b4c2964a817583
Orson Peters <orsonpeters@gmail.com>
2025-03-11 17:48:47 +0100

    refactor: Add as_phys_any to PrivateSeries for downcasting

--- crates/polars-core/src/series/implementations/boolean.rs
+++ crates/polars-core/src/series/implementations/boolean.rs
@@ -363,6 +363,10 @@ impl SeriesTrait for SeriesWrap<BooleanChunked> {
         &mut self.0
     }
 
+    fn as_phys_any(&self) -> &dyn Any {
+        &self.0
+    }
+
     fn as_arc_any(self: Arc<Self>) -> Arc<dyn Any + Send + Sync> {
         self as _
     }
