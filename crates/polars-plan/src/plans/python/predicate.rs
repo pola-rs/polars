0b8138c9d3065a2359db76cd08c04dba9b22fc55
Ritchie Vink <ritchie46@gmail.com>
2025-03-12 10:48:27 +0100

    feat: Support all elementwise functions in IO plugin predicates (#21705)

--- crates/polars-plan/src/plans/python/predicate.rs
+++ crates/polars-plan/src/plans/python/predicate.rs
@@ -1,8 +1,10 @@
 use polars_core::prelude::PolarsResult;
 use polars_utils::pl_serialize;
+use recursive::recursive;
 
 use crate::prelude::*;
 
+#[recursive]
 fn accept_as_io_predicate(e: &Expr) -> bool {
     const LIMIT: usize = 1 << 16;
     match e {
@@ -29,29 +31,8 @@ fn accept_as_io_predicate(e: &Expr) -> bool {
                 && accept_as_io_predicate(predicate)
         },
         Expr::Alias(_, _) => true,
-        Expr::Function {
-            function, input, ..
-        } => {
-            match function {
-                // we already checked if streaming, so we can all functions
-                FunctionExpr::Boolean(_) | FunctionExpr::BinaryExpr(_) | FunctionExpr::Coalesce => {
-                },
-                #[cfg(feature = "log")]
-                FunctionExpr::Entropy { .. }
-                | FunctionExpr::Log { .. }
-                | FunctionExpr::Log1p
-                | FunctionExpr::Exp => {},
-                #[cfg(feature = "abs")]
-                FunctionExpr::Abs => {},
-                #[cfg(feature = "trigonometry")]
-                FunctionExpr::Atan2 => {},
-                #[cfg(feature = "round_series")]
-                FunctionExpr::Clip { .. } => {},
-                #[cfg(feature = "fused")]
-                FunctionExpr::Fused(_) => {},
-                _ => return false,
-            }
-            input.iter().all(accept_as_io_predicate)
+        Expr::AnonymousFunction { input, options, .. } | Expr::Function { input, options, .. } => {
+            options.is_elementwise() && input.iter().all(accept_as_io_predicate)
         },
         _ => false,
     }
