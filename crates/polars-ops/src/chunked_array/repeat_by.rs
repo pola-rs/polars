be73acfcab843c3965c09cee6e3225a5bca17195
Oliver Borchert <oliver.borchert@quantco.com>
2025-03-16 01:35:25 +0100

    Simplify via array builders

--- crates/polars-ops/src/chunked_array/repeat_by.rs
+++ crates/polars-ops/src/chunked_array/repeat_by.rs
@@ -1,4 +1,8 @@
-use arrow::array::ListArray;
+use arrow::array::builder::{ArrayBuilder, ShareStrategy, make_builder};
+use arrow::array::{Array, ListArray};
+use arrow::bitmap::BitmapBuilder;
+use arrow::offset::Offsets;
+use arrow::pushable::Pushable;
 use polars_core::prelude::*;
 use polars_core::with_match_physical_numeric_polars_type;
 
@@ -111,6 +115,73 @@ fn repeat_by_binary(ca: &BinaryChunked, by: &IdxCa) -> PolarsResult<ListChunked>
     }
 }
 
+fn repeat_by_list(ca: &ListChunked, by: &IdxCa) -> PolarsResult<ListChunked> {
+    check_lengths(ca.len(), by.len())?;
+
+    match (ca.len(), by.len()) {
+        (left_len, right_len) if left_len == right_len => Ok(repeat_by_generic_inner(ca, by)),
+        (_, 1) => {
+            let by = new_by(by, ca.len());
+            repeat_by_list(ca, &by)
+        },
+        (1, _) => {
+            let new_array = ca.new_from_index(0, by.len());
+            repeat_by_list(&new_array, by)
+        },
+        // we have already checked the length
+        _ => unreachable!(),
+    }
+}
+
+fn repeat_by_struct(ca: &StructChunked, by: &IdxCa) -> PolarsResult<ListChunked> {
+    check_lengths(ca.len(), by.len())?;
+
+    match (ca.len(), by.len()) {
+        (left_len, right_len) if left_len == right_len => Ok(repeat_by_generic_inner(ca, by)),
+        (_, 1) => {
+            let by = new_by(by, ca.len());
+            repeat_by_struct(ca, &by)
+        },
+        (1, _) => {
+            let new_array = ca.new_from_index(0, by.len());
+            repeat_by_struct(&new_array, by)
+        },
+        // we have already checked the length
+        _ => unreachable!(),
+    }
+}
+
+fn repeat_by_generic_inner<T: PolarsDataType>(ca: &ChunkedArray<T>, by: &IdxCa) -> ListChunked {
+    arity::binary(ca, by, |arr, by| {
+        let output_length = by.iter().filter_map(|x| x).map(|x| *x as usize).sum();
+        let mut builder = make_builder(arr.dtype());
+        let mut validity = BitmapBuilder::with_capacity(output_length);
+        let mut offsets = Offsets::<i64>::with_capacity(output_length);
+        for (idx, n_repeat) in by.iter().enumerate() {
+            validity.push(n_repeat.is_some());
+            if let Some(repeats) = n_repeat {
+                offsets.push(*repeats as usize);
+                builder.subslice_extend_repeated(
+                    arr,
+                    idx,
+                    1,
+                    *repeats as usize,
+                    ShareStrategy::Always,
+                );
+            } else {
+                offsets.push_null();
+            }
+        }
+        let repeated_values = builder.freeze();
+        LargeListArray::new(
+            ListArray::<i64>::default_datatype(arr.dtype().clone()),
+            offsets.into(),
+            repeated_values,
+            validity.into_opt_validity(),
+        )
+    })
+}
+
 pub fn repeat_by(s: &Series, by: &IdxCa) -> PolarsResult<ListChunked> {
     let s_phys = s.to_physical_repr();
     use DataType::*;
@@ -128,6 +199,8 @@ pub fn repeat_by(s: &Series, by: &IdxCa) -> PolarsResult<ListChunked> {
                 repeat_by_primitive(ca, by)
             })
         },
+        List(_) => repeat_by_list(s_phys.list().unwrap(), by),
+        Struct(_) => repeat_by_struct(s_phys.struct_().unwrap(), by),
         _ => polars_bail!(opq = repeat_by, s.dtype()),
     };
     out.and_then(|ca| {
