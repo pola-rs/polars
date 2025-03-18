80b81266896c850d44a25e579f8bcbeb47dca710
Oliver Borchert <oliver.borchert@quantco.com>
2025-03-18 02:51:24 +0100

    Remove accidental changes

--- py-polars/polars/__init__.py
+++ py-polars/polars/__init__.py
@@ -31,7 +31,7 @@ from polars import api, exceptions, plugins, selectors
 from polars._utils.polars_version import get_polars_version as _get_polars_version
 
 # TODO: remove need for importing wrap utils at top level
-from polars._utils.wrap import wrap_df, wrap_s
+from polars._utils.wrap import wrap_df, wrap_s  # noqa: F401
 from polars.catalog.unity import Catalog
 from polars.config import Config
 from polars.convert import (
