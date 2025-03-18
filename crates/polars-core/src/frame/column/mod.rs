b5e1c9a3253662ec5d0d13ec2ace61a1f94a3d92
Simon Lin <simonlin.rqmmw@slmail.me>
2025-03-18 19:52:37 +1100

    oops

--- crates/polars-core/src/frame/column/mod.rs
+++ crates/polars-core/src/frame/column/mod.rs
@@ -85,7 +85,7 @@ impl Column {
     pub fn new_row_index(name: PlSmallStr, offset: IdxSize, length: usize) -> PolarsResult<Column> {
         let length = IdxSize::try_from(length).unwrap_or(IdxSize::MAX);
 
-        if offset.checked_add(IdxSize::MAX).is_none() {
+        if length.checked_add(offset).is_none() {
             polars_bail!(
                 ComputeError:
                 "row index with offset {} overflows on dataframe with height {}",
