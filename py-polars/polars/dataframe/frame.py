4ac6e6b8c365879af1abf6d4450916d28aea1e8e
Oliver Borchert <oliver.borchert@quantco.com>
2025-03-21 01:45:08 +0100

    Fix mypy (again)

--- py-polars/polars/dataframe/frame.py
+++ py-polars/polars/dataframe/frame.py
@@ -4039,7 +4039,7 @@ class DataFrame:
                 # Handle empty dict input
                 metadata = None
         elif callable(metadata):
-            metadata = wrap_parquet_metadata_callback(metadata)  # type: ignore
+            metadata = wrap_parquet_metadata_callback(metadata)  # type: ignore[assignment,arg-type]
 
         if isinstance(statistics, bool) and statistics:
             statistics = {
