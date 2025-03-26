99317641061e3728aff7ee55b7a7e7a72330ec1e
Matthijs Kok <matthijs.kok2@gmail.com>
2025-03-26 10:39:56 +0100

    chore(python): Allow `os.PathLike` parameters in `normalize_filepath` and add tests

--- py-polars/tests/unit/utils/test_various.py
+++ py-polars/tests/unit/utils/test_various.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+
 import pytest
 
-from polars._utils.various import issue_warning
+from polars._utils.various import issue_warning, normalize_filepath
 from polars.exceptions import PerformanceWarning
 
 
@@ -8,3 +10,65 @@ def test_issue_warning() -> None:
     msg = "hello"
     with pytest.warns(PerformanceWarning, match=msg):
         issue_warning(msg, PerformanceWarning)
+
+
+class TestNormalizeFilepath:
+    """Test coverage for `polars/_utils/various.py::normalize_filepath()`."""
+
+    def test_normalize_filepath_str(self) -> None:
+        path = "/dummy/file.py"
+        normalized = normalize_filepath(path)
+        assert type(normalized) is str
+        assert normalized == path
+
+    def test_normalize_filepath_str_normalizes(self) -> None:
+        path = "~/dummy/file.py"
+        normalized = normalize_filepath(path)
+        assert type(normalized) is str
+        assert normalized != path
+
+    def test_normalize_filepath_bytes(self) -> None:
+        path = b"/dummy/file.py"
+        normalized = normalize_filepath(path)
+        assert type(normalized) is bytes
+        assert normalized == path
+
+    def test_normalize_filepath_bytes_normalizes(self) -> None:
+        path = b"~/dummy/file.py"
+        normalized = normalize_filepath(path)
+        assert type(normalized) is bytes
+        assert normalized != path
+
+    def test_normalize_filepath_pathlib(self) -> None:
+        path = Path("/") / "dummy" / "file.py"
+        normalized = normalize_filepath(path)
+        assert type(normalized) is str
+        assert normalized == str(path)
+
+    def test_normalize_filepath_pathlib_normalizes(self) -> None:
+        path = Path("~") / "dummy" / "file.py"
+        normalized = normalize_filepath(path)
+        assert type(normalized) is str
+        assert normalized != str(path)
+        assert normalized == str(Path.home() / "dummy" / "file.py")
+
+    def test_normalize_filepath_custom_pathlike(self) -> None:
+        class CustomPathLike:
+            def __fspath__(self) -> bytes:
+                return b"/dummy/file.py"
+
+        path = CustomPathLike()
+        normalized = normalize_filepath(path)
+        assert type(normalized) is bytes
+        assert normalized == b"/dummy/file.py"
+
+    def test_normalize_filepath_custom_pathlike_normalizes(self) -> None:
+        class CustomPathLike:
+            def __fspath__(self) -> bytes:
+                return b"~/dummy/file.py"
+
+        path = CustomPathLike()
+        normalized = normalize_filepath(path)
+        assert type(normalized) is bytes
+        assert normalized != b"~/dummy/file.py"
+        assert normalized == bytes(Path.home() / "dummy" / "file.py")
