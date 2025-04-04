50fbef2bd15e244ef828cd0539bc6bfebf7e919a
Marshall Crumiller <mcrumiller@gmail.com>
2025-04-04 18:32:14 -0400

    Update rust tests with expr

--- crates/polars-lazy/src/tests/queries.rs
+++ crates/polars-lazy/src/tests/queries.rs
@@ -1721,7 +1721,7 @@ fn empty_df() -> PolarsResult<()> {
             col("A").shift_and_fill(lit(-1), lit(1)).alias("3"),
             col("A").fill_null(lit(1)).alias("4"),
             col("A").cum_count(false).alias("5"),
-            col("A").diff(1, NullBehavior::Ignore).alias("6"),
+            col("A").diff(lit(1), NullBehavior::Ignore).alias("6"),
             col("A").cum_max(false).alias("7"),
             col("A").cum_min(false).alias("8"),
         ])
