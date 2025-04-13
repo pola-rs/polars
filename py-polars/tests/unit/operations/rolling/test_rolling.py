7ac74031ccee5a8a901e54a7721fa050c62e15dc
ritchie <ritchie46@gmail.com>
2025-04-13 11:39:35 +0200

    print

--- py-polars/tests/unit/operations/rolling/test_rolling.py
+++ py-polars/tests/unit/operations/rolling/test_rolling.py
@@ -474,6 +474,7 @@ def test_overlapping_groups_4628() -> None:
 
 def test_rolling_skew_lagging_null_5179() -> None:
     s = pl.Series([None, 3, 4, 1, None, None, None, None, 3, None, 5, 4, 7, 2, 1, None])
+    print(s)
     assert s.rolling_skew(3).fill_nan(-1.0).to_list() == [
         None,
         None,
