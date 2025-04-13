aef60d6829f47a85437a4c7251367a51c1ee2e7e
koki watanabe <address8128@gmail.com>
2025-04-13 15:41:06 +0900

    add comment

--- crates/polars-ops/src/chunked_array/strings/namespace.rs
+++ crates/polars-ops/src/chunked_array/strings/namespace.rs
@@ -96,6 +96,7 @@ pub trait StringNameSpaceImpl: AsString {
                 ca.clone()
             } else {
                 let all_failures = ca.filter(&failure_mask)?;
+                // `.unique()` does not necessarily preserve the original order.
                 let unique_failures_args = all_failures.arg_unique()?;
                 all_failures.take(&unique_failures_args.slice(0, 10))?
             };
