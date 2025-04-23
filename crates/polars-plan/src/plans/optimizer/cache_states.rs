a3187b31449c99d5a7722948120d09176b96adde
nameexhaustion <simonlin.rqmmw@slmail.me>
2025-04-23 20:05:32 +1000

    fix: Fix `select(len())` performance regression (#22363)

--- crates/polars-plan/src/plans/optimizer/cache_states.rs
+++ crates/polars-plan/src/plans/optimizer/cache_states.rs
@@ -253,7 +253,7 @@ pub(super) fn set_cache_states(
     // and finally remove that last projection and stitch the subplan
     // back to the cache node again
     if !cache_schema_and_children.is_empty() {
-        let mut proj_pd = ProjectionPushDown::new(new_streaming);
+        let mut proj_pd = ProjectionPushDown::new();
         let mut pred_pd = PredicatePushDown::new(expr_eval, new_streaming).block_at_cache(false);
         for (_cache_id, v) in cache_schema_and_children {
             // # CHECK IF WE NEED TO REMOVE CACHES
