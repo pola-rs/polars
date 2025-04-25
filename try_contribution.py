b6f0f1cae49e96fd4f7f714f916edbe7e17726d8
Yusuf Yudhistira <yusuf.yudhistirazhy@gmail.com>
2025-04-25 16:49:23 +0930

    Handle meta data files

--- try_contribution.py
+++ try_contribution.py
@@ -6,7 +6,7 @@ def read_csv_from_zip(path):
 
     with zipfile.ZipFile(path, 'r') as zip_ref:
         for file_name in zip_ref.namelist():
-            if file_name.endswith(".csv"):
+            if file_name.endswith(".csv") and not file_name.startswith("__") and not file_name.startswith("."):
                 with zip_ref.open(file_name) as csv_file:
                     df = pl.read_csv(csv_file)
                     dataframes[file_name] = df
