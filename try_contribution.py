56ea6698bbd9c4cff86f642c163b2cacccf00269
Yusuf Yudhistira <yusuf.yudhistirazhy@gmail.com>
2025-04-25 16:45:42 +0930

    Implement basic functionalities

--- try_contribution.py
+++ try_contribution.py
@@ -2,14 +2,17 @@ import polars as pl
 import zipfile
 
 def read_csv_from_zip(path):
-    dataframes = []
+    dataframes = {}
 
     with zipfile.ZipFile(path, 'r') as zip_ref:
         for file_name in zip_ref.namelist():
-            print(file_name)
+            if file_name.endswith(".csv"):
+                with zip_ref.open(file_name) as csv_file:
+                    df = pl.read_csv(csv_file)
+                    dataframes[file_name] = df
 
+    return dataframes
 
-
-
-read_csv_from_zip("docs/assets/data/test_csv.zip")
+dataframes = read_csv_from_zip("docs/assets/data/test_csv.zip")
+print(dataframes)
 
