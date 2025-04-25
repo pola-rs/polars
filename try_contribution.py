0306e7b62ec93015e64d04c52a8990f3d0578cc9
Yusuf Yudhistira <yusuf.yudhistirazhy@gmail.com>
2025-04-25 17:09:33 +0930

    add parameter to Read only single file from zip

--- try_contribution.py
+++ try_contribution.py
@@ -1,18 +1,27 @@
 import polars as pl
 import zipfile
 
-def read_csv_from_zip(path):
-    dataframes = {}
-
+# TODO: Read only single file from zip
+def read_csv_from_zip(path, target_file=None):
     with zipfile.ZipFile(path, 'r') as zip_ref:
-        for file_name in zip_ref.namelist():
-            if file_name.endswith(".csv") and not file_name.startswith("__") and not file_name.startswith("."):
-                with zip_ref.open(file_name) as csv_file:
-                    df = pl.read_csv(csv_file)
-                    dataframes[file_name] = df
 
-    return dataframes
+        if target_file is not None:
+            dataframe = None
+            with zip_ref.open(target_file) as csv_file:
+                df = pl.read_csv(csv_file)
+                dataframe = df
+                return dataframe
 
-dataframes = read_csv_from_zip("docs/assets/data/test_csv.zip")
+        else:
+            dataframes = {}
+            for file_name in zip_ref.namelist():
+                if file_name.endswith(".csv") and not file_name.startswith("__") and not file_name.startswith("."):
+                    with zip_ref.open(file_name) as csv_file:
+                        df = pl.read_csv(csv_file)
+                        dataframes[file_name] = df
+            return dataframes
+
+dataframes = read_csv_from_zip("docs/assets/data/test_csv.zip", "apple_stock.csv")
+
 print(dataframes)
 
