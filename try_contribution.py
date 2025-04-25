802806e59bf5bc40b692ffe3d037d9746e71f434
Yusuf Yudhistira <yusuf.yudhistirazhy@gmail.com>
2025-04-25 20:51:05 +0930

    the target_file param now can read multiple files

--- try_contribution.py
+++ try_contribution.py
@@ -1,27 +1,21 @@
 import polars as pl
 import zipfile
 
-# TODO: Read only single file from zip
-def read_csv_from_zip(path, target_file=None):
+def read_csv_from_zip(path, target_files=None):
+    dataframes = {}
+
     with zipfile.ZipFile(path, 'r') as zip_ref:
-
-        if target_file is not None:
-            dataframe = None
-            with zip_ref.open(target_file) as csv_file:
-                df = pl.read_csv(csv_file)
-                dataframe = df
-                return dataframe
-
-        else:
-            dataframes = {}
             for file_name in zip_ref.namelist():
                 if file_name.endswith(".csv") and not file_name.startswith("__") and not file_name.startswith("."):
+                    if target_files is not None and file_name not in target_files:
+                        continue
                     with zip_ref.open(file_name) as csv_file:
                         df = pl.read_csv(csv_file)
                         dataframes[file_name] = df
+
             return dataframes
 
-dataframes = read_csv_from_zip("docs/assets/data/test_csv.zip", "apple_stock.csv")
+dataframes = read_csv_from_zip("docs/assets/data/test_csv.zip", ["apple_stock.csv", "iris.csv"])
 
 print(dataframes)
 
