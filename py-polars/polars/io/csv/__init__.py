b0a6ac0290fff958b87dbd246807b15bb048872b
Yusuf Yudhistira <yusuf.yudhistirazhy@gmail.com>
2025-04-27 21:19:19 +0930

    test

--- py-polars/polars/io/csv/__init__.py
+++ py-polars/polars/io/csv/__init__.py
@@ -1,6 +1,6 @@
 from polars.io.csv.batched_reader import BatchedCsvReader
 from polars.io.csv.functions import read_csv, read_csv_batched, scan_csv, read_csv_from_zip
-
+# ehc
 __all__ = [
     "BatchedCsvReader",
     "read_csv",
