7d2e7f64ba0cff93ef634eb48907b09734b296d9
Gijs Burghoorn <me@gburghoorn.com>
2025-05-23 12:52:08 +0200

    fix(python): Add {top, bottom}_k_by to Series (#22902)
    Co-authored-by: TareqKomboz <Tareq.Komboz@hotmail.com>

--- py-polars/polars/series/series.py
+++ py-polars/polars/series/series.py
@@ -3381,12 +3381,64 @@ class Series:
 
         See Also
         --------
+        top_k_by
         bottom_k
+        bottom_k_by
 
         Examples
         --------
         >>> s = pl.Series("a", [2, 5, 1, 4, 3])
         >>> s.top_k(3)
+        shape: (3,)
+        Series: 'a' [i64]
+        [
+            5
+            4
+            3
+        ]
+        """
+
+    def top_k_by(
+        self,
+        by: IntoExpr | Iterable[IntoExpr],
+        k: int = 5,
+        *,
+        reverse: bool | Sequence[bool] = False,
+    ) -> Series:
+        r"""
+        Return the `k` largest elements of the `by` column.
+
+        Non-null elements are always preferred over null elements, regardless of
+        the value of `reverse`. The output is not guaranteed to be in any
+        particular order, call :func:`sort` after this function if you wish the
+        output to be sorted.
+
+        This has time complexity:
+
+        .. math:: O(n \log{n})
+
+        Parameters
+        ----------
+        by
+            Column used to determine the largest elements.
+            Accepts expression input. Strings are parsed as column names.
+        k
+            Number of elements to return.
+        reverse
+            Consider the `k` smallest elements of the `by` column (instead of the `k`
+            largest). This can be specified per column by passing a sequence of
+            booleans.
+
+        See Also
+        --------
+        top_k
+        bottom_k
+        bottom_k_by
+
+        Examples
+        --------
+        >>> s = pl.Series("a", [2, 5, 1, 4, 3])
+        >>> s.top_k_by("a", 3)
         shape: (3,)
         Series: 'a' [i64]
         [
@@ -3416,11 +3468,63 @@ class Series:
         See Also
         --------
         top_k
+        top_k_by
+        bottom_k_by
 
         Examples
         --------
         >>> s = pl.Series("a", [2, 5, 1, 4, 3])
         >>> s.bottom_k(3)
+        shape: (3,)
+        Series: 'a' [i64]
+        [
+            1
+            2
+            3
+        ]
+        """
+
+    def bottom_k_by(
+        self,
+        by: IntoExpr | Iterable[IntoExpr],
+        k: int = 5,
+        *,
+        reverse: bool | Sequence[bool] = False,
+    ) -> Series:
+        r"""
+        Return the `k` smallest elements of the `by` column.
+
+        Non-null elements are always preferred over null elements, regardless of
+        the value of `reverse`. The output is not guaranteed to be in any
+        particular order, call :func:`sort` after this function if you wish the
+        output to be sorted.
+
+        This has time complexity:
+
+        .. math:: O(n \log{n})
+
+        Parameters
+        ----------
+        by
+            Column used to determine the smallest elements.
+            Accepts expression input. Strings are parsed as column names.
+        k
+            Number of elements to return.
+        reverse
+            Consider the `k` largest elements of the `by` column( (instead of the `k`
+            smallest). This can be specified per column by passing a sequence of
+            booleans.
+
+        See Also
+        --------
+        top_k
+        top_k_by
+        bottom_k
+
+        Examples
+        --------
+        >>> s = pl.Series("a", [2, 5, 1, 4, 3])
+        >>> s.bottom_k_by("a", 3)
         shape: (3,)
         Series: 'a' [i64]
         [
