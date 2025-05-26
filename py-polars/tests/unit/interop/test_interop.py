0b057e6e57ad701df286da0daeabf850ba24a946
eitsupi <ts1s1andn@gmail.com>
2025-05-26 03:15:22 +0000

    test: fix values

--- py-polars/tests/unit/interop/test_interop.py
+++ py-polars/tests/unit/interop/test_interop.py
@@ -862,12 +862,12 @@ def test_compat_level(monkeypatch: pytest.MonkeyPatch) -> None:
         df.to_arrow(compat_level=newest)["bin_col"][0], pa.BinaryViewScalar
     )
 
-    assert len(df.write_ipc(None).getbuffer()) == 786
-    assert len(df.write_ipc(None, compat_level=oldest).getbuffer()) == 914
-    assert len(df.write_ipc(None, compat_level=newest).getbuffer()) == 786
-    assert len(df.write_ipc_stream(None).getbuffer()) == 544
-    assert len(df.write_ipc_stream(None, compat_level=oldest).getbuffer()) == 672
-    assert len(df.write_ipc_stream(None, compat_level=newest).getbuffer()) == 544
+    assert len(df.write_ipc(None).getbuffer()) == 738
+    assert len(df.write_ipc(None, compat_level=oldest).getbuffer()) == 866
+    assert len(df.write_ipc(None, compat_level=newest).getbuffer()) == 738
+    assert len(df.write_ipc_stream(None).getbuffer()) == 520
+    assert len(df.write_ipc_stream(None, compat_level=oldest).getbuffer()) == 648
+    assert len(df.write_ipc_stream(None, compat_level=newest).getbuffer()) == 520
 
 
 def test_df_pycapsule_interface() -> None:
