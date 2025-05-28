8f5d2d68a240aaa9b08d89180c7e53064eed49ef
Simon Lin <simonlin.rqmmw@slmail.me>
2025-05-28 17:08:06 +1000

    c

--- crates/polars-utils/src/unique_id.rs
+++ crates/polars-utils/src/unique_id.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 /// Unique ref-counted identifier.
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+#[derive(Debug, Clone)]
 pub struct UniqueId(
     // We use an Arc to reserve a memory address rather than a static atomic counter, as the latter
     // may cause duplicate IDs on wrap-around.
@@ -11,6 +11,7 @@ pub struct UniqueId(
 );
 
 impl UniqueId {
+    #[inline]
     pub fn to_usize(&self) -> usize {
         Arc::as_ptr(&self.0) as usize
     }
@@ -19,5 +20,30 @@ impl UniqueId {
 impl Default for UniqueId {
     fn default() -> Self {
         Self(Arc::new(()))
+    }
+}
+
+impl PartialEq for UniqueId {
+    fn eq(&self, other: &Self) -> bool {
+        self.to_usize() == other.to_usize()
+    }
+}
+
+impl std::hash::Hash for UniqueId {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.to_usize().hash(state)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::UniqueId;
+
+    #[test]
+    fn test_unique_id() {
+        let a = UniqueId::default();
+        let b = UniqueId::default();
+
+        assert_ne!(a, b);
     }
 }
