3ac22ab560ce565804567307328dbe5d3a8a830f
Simon Lin <simonlin.rqmmw@slmail.me>
2025-05-28 17:13:24 +1000

    c

--- crates/polars-utils/src/unique_id.rs
+++ crates/polars-utils/src/unique_id.rs
@@ -1,14 +1,23 @@
 use std::sync::Arc;
 
+/// Intentionally a custom inner type to prevent deriving eq / hash, as that would not be correct.
+struct Inner;
+
 /// Unique ref-counted identifier.
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct UniqueId(
     // We use an Arc to reserve a memory address rather than a static atomic counter, as the latter
     // may cause duplicate IDs on wrap-around.
     //
     // Note, this inner repr is a private implementation detail.
-    Arc<()>,
+    Arc<Inner>,
 );
+
+impl std::fmt::Debug for UniqueId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "UniqueId({})", self.to_usize())
+    }
+}
 
 impl UniqueId {
     #[inline]
@@ -19,7 +28,7 @@ impl UniqueId {
 
 impl Default for UniqueId {
     fn default() -> Self {
-        Self(Arc::new(()))
+        Self(Arc::new(Inner {}))
     }
 }
 
