4ced645a9fa36e6faf905a7843c5af111f3346b8
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-05-30 15:34:33 -0700

    Implement StructFunction expressions in into_py

--- py-polars/src/lib.rs
+++ py-polars/src/lib.rs
@@ -79,6 +79,7 @@ fn _expr_nodes(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     m.add_class::<PyStringFunction>().unwrap();
     m.add_class::<PyBooleanFunction>().unwrap();
     m.add_class::<PyTemporalFunction>().unwrap();
+    m.add_class::<PyStructFunction>().unwrap();
     // Options
     m.add_class::<PyWindowMapping>().unwrap();
     m.add_class::<PyRollingGroupOptions>().unwrap();
