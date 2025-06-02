bae02a79b508025451d93edd7166844f57cd58d0
coastalwhite <me@gburghoorn.com>
2025-06-02 14:00:16 +0200

    don't mkdir for file://

--- crates/polars-io/src/utils/mkdir.rs
+++ crates/polars-io/src/utils/mkdir.rs
@@ -2,6 +2,10 @@ use std::io;
 use std::path::Path;
 
 pub fn mkdir_recursive(path: &Path) -> io::Result<()> {
+    if path.starts_with("file:") {
+        return Ok(());
+    }
+
     std::fs::DirBuilder::new().recursive(true).create(
         path.parent()
             .ok_or(io::Error::other("path is not a file"))?,
@@ -10,6 +14,10 @@ pub fn mkdir_recursive(path: &Path) -> io::Result<()> {
 
 #[cfg(feature = "tokio")]
 pub async fn tokio_mkdir_recursive(path: &Path) -> io::Result<()> {
+    if path.starts_with("file:") {
+        return Ok(());
+    }
+
     tokio::fs::DirBuilder::new()
         .recursive(true)
         .create(
