4f2734fed95203d4f3d4718dd035071bbf4eb6e9
Simon Lin <simonlin.rqmmw@slmail.me>
2025-06-05 21:43:19 +1000

    c

--- crates/polars-stream/src/nodes/io_sources/multi_file_reader/row_counter.rs
+++ crates/polars-stream/src/nodes/io_sources/multi_file_reader/row_counter.rs
@@ -150,6 +150,7 @@ impl RowCounter {
     }
 
     #[inline]
+    #[expect(unused)]
     pub fn num_physical_rows_idxsize_saturating(&self) -> IdxSize {
         IdxSize::try_from(self.physical_rows).unwrap_or(IdxSize::MAX)
     }
