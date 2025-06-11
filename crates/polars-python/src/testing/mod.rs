97a0433d178b9d9a7b872633bae84aeb9d174d6e
Kevin Patyk <74557243+Kevin-Patyk@users.noreply.github.com>
2025-06-11 12:52:48 +0200

    refactor: Connect Python `assert_series_equal()` to Rust back-end (#23141)

---
+++ crates/polars-python/src/testing/mod.rs
@@ -0,0 +1,3 @@
+mod series;
+
+pub use series::*;
--- py-polars/tests/unit/testing/test_assert_series_equal.py
+++ py-polars/tests/unit/testing/test_assert_series_equal.py
@@ -10,6 +10,7 @@ import pytest
 from hypothesis import given
 
 import polars as pl
+from polars.exceptions import InvalidOperationError
 from polars.testing import assert_series_equal, assert_series_not_equal
 from polars.testing.parametric import dtypes, series
 
@@ -71,8 +72,8 @@ def test_assert_series_equal_check_order() -> None:
 def test_assert_series_equal_check_order_unsortable_type() -> None:
     s = pl.Series([object(), object()])
     with pytest.raises(
-        TypeError,
-        match="cannot set `check_order=False` on Series with unsortable data type",
+        InvalidOperationError,
+        match="`sort_with` operation not supported for dtype `object`",
     ):
         assert_series_equal(s, s, check_order=False)
 
@@ -550,12 +551,9 @@ def test_assert_series_equal_incompatible_data_types() -> None:
 def test_assert_series_equal_full_series() -> None:
     s1 = pl.Series([1, 2, 3])
     s2 = pl.Series([1, 2, 4])
-    msg = (
-        r"Series are different \(exact value mismatch\)\n"
-        r"\[left\]:  \[1, 2, 3\]\n"
-        r"\[right\]: \[1, 2, 4\]"
-    )
-    with pytest.raises(AssertionError, match=msg):
+    with pytest.raises(
+        AssertionError, match=r"Series are different \(exact value mismatch\)"
+    ):
         assert_series_equal(s1, s2)
 
 
