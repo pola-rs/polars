2126cf3bef48a1515ffd14a11517dd641538c270
coastalwhite <me@gburghoorn.com>
2025-06-20 11:58:59 +0200

    add some testing

--- py-polars/tests/unit/test_datatype_exprs.py
+++ py-polars/tests/unit/test_datatype_exprs.py
@@ -1,6 +1,8 @@
+from __future__ import annotations
 import pytest
 
 import polars as pl
+from polars._typing import PolarsDataType
 from polars.testing import assert_frame_equal
 
 c = pl.col
@@ -35,3 +37,81 @@ def test_expressions(e: pl.Expr, equiv: pl.Expr) -> None:
         df.select(e),
         df.select(equiv),
     )
+
+
+DTYPES = [
+    (pl.Null(), "null"),
+    (pl.Boolean(), "boolean"),
+    (pl.String(), "string"),
+    (pl.Binary(), "binary"),
+    (pl.Int8(), "int"),
+    (pl.Int16(), "int"),
+    (pl.Int32(), "int"),
+    (pl.Int64(), "int"),
+    (pl.Int128(), "int"),
+    (pl.UInt8(), "uint"),
+    (pl.UInt16(), "uint"),
+    (pl.UInt32(), "uint"),
+    (pl.UInt64(), "uint"),
+    (pl.Float32(), "float"),
+    (pl.Float64(), "float"),
+    (pl.Decimal(scale=4), "decimal"),
+    (pl.Decimal(scale=12), "decimal"),
+    (pl.Categorical(), "categorical"),
+    (pl.Enum([]), "enum"),
+    (pl.Enum(["a", "b"]), "enum"),
+    (pl.List(pl.Null()), "list"),
+    (pl.List(pl.Array(pl.Int8, 5)), "list"),
+    (pl.List(pl.Array(pl.Struct({"x": pl.Int8, "y": pl.String}), 5)), "list"),
+    (pl.Array(pl.Null(), 2), "array"),
+    (pl.Array(pl.List(pl.Int8), 2), "array"),
+    (
+        pl.Array(pl.List(pl.Struct({"x": pl.Int8, "y": pl.String})), 2),
+        "array",
+    ),
+    (pl.Struct({"x": pl.Null()}), "struct"),
+    (pl.Struct({"x": pl.List(pl.Int8)}), "struct"),
+]
+
+
+@pytest.mark.parametrize(
+    ("fn", "fn_tags"),
+    [
+        ("is_numeric", ["uint", "int", "float", "decimal"]),
+        ("is_integer", ["uint", "int"]),
+        ("is_float", ["float"]),
+        ("is_decimal", ["decimal"]),
+        ("is_categorical", ["categorical"]),
+        ("is_enum", ["enum"]),
+        ("is_nested", ["list", "array", "struct"]),
+        ("is_list", ["list"]),
+        ("is_array", ["array"]),
+        ("is_struct", ["struct"]),
+        ("is_temporal", ["date", "datetime", "duration", "time"]),
+        ("is_datetime", ["datetime"]),
+        ("is_duration", ["duration"]),
+        ("is_object", ["object"]),
+    ],
+)
+def test_classification(fn: str, fn_tags: list[str]) -> None:
+    f = getattr(pl.DataTypeExpr, fn)
+    for dtype, dtype_tag in DTYPES:
+        dtype_expr = dtype.to_dtype_expr()
+        expr = f(dtype_expr)
+        expected = dtype_tag in fn_tags
+        assert pl.select(expr).to_series().item() == expected
+
+
+@pytest.mark.parametrize(
+    ("fn", "fn_tag"),
+    [
+        ("is_signed", "int"),
+        ("is_unsigned", "uint"),
+    ],
+)
+def test_int_signed_classification(fn: str, fn_tag: str) -> None:
+    for dtype, dtype_tag in DTYPES:
+        dtype_expr = dtype.to_dtype_expr()
+        expr = getattr(dtype_expr.int, fn)()
+        expected = dtype_tag == fn_tag
+        assert pl.select(expr).to_series().item() == expected
