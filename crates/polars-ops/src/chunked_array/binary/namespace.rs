9a70d0b3eec18327d2487c2f4c2fbdfe16b2e860
Itamar Turner-Trauring <itamar@itamarst.org>
2025-06-25 04:36:00 -0400

    refactor: Casting from binview to primitives code moved from polars-ops to polars-compute (#23234)
    Co-authored-by: Itamar Turner-Trauring <itamar@pythonspeed.com>
    Co-authored-by: nameexhaustion <simonlin.rqmmw@slmail.me>

--- crates/polars-ops/src/chunked_array/binary/namespace.rs
+++ crates/polars-ops/src/chunked_array/binary/namespace.rs
@@ -7,10 +7,10 @@ use base64::Engine as _;
 #[cfg(feature = "binary_encoding")]
 use base64::engine::general_purpose;
 use memchr::memmem::find;
+use polars_compute::cast::binview_to_primitive_dyn;
 use polars_compute::size::binary_size_bytes;
 use polars_core::prelude::arity::{broadcast_binary_elementwise_values, unary_elementwise_values};
 
-use super::cast_binary_to_numerical::cast_binview_to_primitive_dyn;
 use super::*;
 
 pub trait BinaryNameSpaceImpl: AsBinary {
@@ -167,7 +167,7 @@ pub trait BinaryNameSpaceImpl: AsBinary {
                         Ok(Series::from_chunks_and_dtype_unchecked(
                             ca.name().clone(),
                             ca.chunks().iter().map(|chunk| {
-                                cast_binview_to_primitive_dyn::<$T>(
+                                binview_to_primitive_dyn::<$T>(
                                     &**chunk,
                                     &arrow_type,
                                     is_little_endian,
