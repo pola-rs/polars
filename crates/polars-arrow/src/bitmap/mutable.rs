36a7051c09d65b0d511ae4c29a29b5865d37826d
Koen Denecker <koen@polars.tech>
2025-06-29 15:22:15 +0200

    code style

--- crates/polars-arrow/src/bitmap/mutable.rs
+++ crates/polars-arrow/src/bitmap/mutable.rs
@@ -185,7 +185,7 @@ impl MutableBitmap {
     #[inline]
     pub unsafe fn and_pos_unchecked(&mut self, index: usize, value: bool) {
         *self.buffer.get_unchecked_mut(index / 8) &=
-            (0xfe | (value as u8)).rotate_left(index as u32);
+            (0xFE | u8::from(value)).rotate_left(index as u32);
     }
 
     /// constructs a new iterator over the bits of [`MutableBitmap`].
