48b7cfdd5c04d4bda64afaef41aaa507eca2da76
Gian Violi <96206151+gfvioli@users.noreply.github.com>
2025-07-07 20:09:03 +0200

    fixing linting error

--- py-polars/polars/io/delta.py
+++ py-polars/polars/io/delta.py
@@ -305,7 +305,7 @@ def scan_delta(
     )
 
     if isinstance(source, DeltaTable) and storage_options is None:
-        storage_options = source._storage_options  # type: ignore
+        storage_options = source._storage_options
     if not isinstance(source, DeltaTable):
         credential_provider_builder = _init_credential_provider_builder(
             credential_provider, source, storage_options, "scan_delta"
