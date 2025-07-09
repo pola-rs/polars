e6bf210b1a89ce4b760bd1aa7cf41fa5453e12d3
Orson Peters <orsonpeters@gmail.com>
2025-07-09 11:43:40 +0200

    fix: Properly handle non-finite floats in rolling_sum/mean (#23482)

--- crates/polars-utils/src/float.rs
+++ crates/polars-utils/src/float.rs
@@ -17,6 +17,14 @@ pub unsafe trait IsFloat: private::Sealed + Sized {
         unimplemented!()
     }
 
+    fn pos_inf_value() -> Self {
+        unimplemented!()
+    }
+
+    fn neg_inf_value() -> Self {
+        unimplemented!()
+    }
+
     #[allow(clippy::wrong_self_convention)]
     fn is_nan(&self) -> bool
     where
@@ -84,6 +92,14 @@ macro_rules! impl_is_float {
 
             fn nan_value() -> Self {
                 Self::NAN
+            }
+
+            fn pos_inf_value() -> Self {
+                Self::INFINITY
+            }
+
+            fn neg_inf_value() -> Self {
+                Self::NEG_INFINITY
             }
 
             #[inline]
