6ace8838956c7208adb5b0da253f819d61d54190
Koen Denecker <koen@polars.tech>
2025-07-21 17:04:31 +0200

    fix dtype method

--- crates/polars-plan/src/plans/aexpr/function_expr/strings.rs
+++ crates/polars-plan/src/plans/aexpr/function_expr/strings.rs
@@ -155,7 +155,7 @@ impl IRStringFunction {
             #[cfg(feature = "extract_groups")]
             ExtractGroups { dtype, .. } => mapper.with_dtype(dtype.clone()),
             #[cfg(feature = "string_to_integer")]
-            ToInteger { dtype, .. } => mapper.with_opt_dtype(dtype.clone()),
+            ToInteger { dtype, .. } => mapper.with_dtype(dtype.clone().unwrap_or(DataType::Int64)),
             #[cfg(feature = "regex")]
             Find { .. } => mapper.with_dtype(DataType::UInt32),
             #[cfg(feature = "extract_jsonpath")]
