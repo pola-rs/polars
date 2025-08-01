1b5ba2e95d5a22f145a209c6987f1dca920081cf
Itamar Turner-Trauring <itamar@itamarst.org>
2025-08-01 08:02:38 -0400

    feat: Reinterpret binary data to fixed size numerical array (#22840)
    Co-authored-by: Jakub Filipek <jfilipek@proton.me>
    Co-authored-by: Itamar Turner-Trauring <itamar@pythonspeed.com>
    Co-authored-by: nameexhaustion <simonlin.rqmmw@slmail.me>

--- crates/polars-compute/src/cast/binview_to.rs
+++ crates/polars-compute/src/cast/binview_to.rs
@@ -1,12 +1,16 @@
+use std::ptr::copy_nonoverlapping;
+
 use arrow::array::*;
+use arrow::bitmap::MutableBitmap;
 #[cfg(feature = "dtype-decimal")]
 use arrow::compute::decimal::deserialize_decimal;
-use arrow::datatypes::{ArrowDataType, TimeUnit};
+use arrow::datatypes::{ArrowDataType, Field, TimeUnit};
 use arrow::offset::Offset;
 use arrow::types::NativeType;
+use bytemuck::cast_slice_mut;
 use chrono::Datelike;
 use num_traits::FromBytes;
-use polars_error::PolarsResult;
+use polars_error::{PolarsResult, polars_err};
 
 use super::CastOptionsImpl;
 use super::binary_to::Parse;
@@ -190,4 +194,132 @@ where
         to,
         is_little_endian,
     )))
+}
+
+/// Casts a [`BinaryViewArray`] to a [`FixedSizeListArray`], making any un-castable value a Null.
+///
+/// # Arguments
+///
+/// * `from`: The array to reinterpret.
+/// * `array_width`: The number of items in each `Array`.
+pub(super) fn try_binview_to_fixed_size_list<T, const IS_LITTLE_ENDIAN: bool>(
+    from: &BinaryViewArray,
+    array_width: usize,
+) -> PolarsResult<FixedSizeListArray>
+where
+    T: FromBytes + NativeType,
+    for<'a> &'a <T as FromBytes>::Bytes: TryFrom<&'a [u8]>,
+{
+    let element_size = std::mem::size_of::<T>();
+    // The maximum number of primitives in the result:
+    let primitive_length = from.len().checked_mul(array_width).ok_or_else(|| {
+        polars_err!(
+            InvalidOperation:
+            "array chunk length * number of items ({} * {}) is too large",
+            from.len(),
+            array_width
+        )
+    })?;
+    // The size of each array, in bytes:
+    let row_size_bytes = element_size.checked_mul(array_width).ok_or_else(|| {
+        polars_err!(
+            InvalidOperation:
+            "array size in bytes ({} * {}) is too large",
+            element_size,
+            array_width
+        )
+    })?;
+
+    let mut out: Vec<T> = vec![T::zeroed(); primitive_length];
+    let (out_u8_ptr, out_len_bytes) = {
+        let out_u8_slice = cast_slice_mut::<_, u8>(out.as_mut());
+        (out_u8_slice.as_mut_ptr(), out_u8_slice.len())
+    };
+    assert_eq!(out_len_bytes, row_size_bytes * from.len());
+    let mut validity = MutableBitmap::from_len_set(from.len());
+
+    for (index, value) in from.iter().enumerate() {
+        if let Some(value) = value
+            && value.len() == row_size_bytes
+        {
+            if cfg!(target_endian = "little") && IS_LITTLE_ENDIAN {
+                // Fast path, we can just copy the data with no need to
+                // reinterpret.
+                let write_index = index * row_size_bytes;
+                debug_assert!(value.is_empty() || write_index < out_len_bytes);
+                debug_assert!(value.is_empty() || (write_index + value.len() - 1 < out_len_bytes));
+                // # Safety
+                // - The start index is smaller than `out`'s capacity.
+                // - The end index is smaller than `out`'s capacity.
+                unsafe {
+                    copy_nonoverlapping(value.as_ptr(), out_u8_ptr.add(write_index), value.len());
+                }
+            } else {
+                // Slow path, reinterpret items one by one.
+                for j in 0..array_width {
+                    let jth_range = (j * element_size)..((j + 1) * element_size);
+                    debug_assert!(value.get(jth_range.clone()).is_some());
+                    // # Safety
+                    // We made sure the range is smaller than `value` length.
+                    let jth_bytes = unsafe { value.get_unchecked(jth_range) };
+                    // # Safety
+                    // We just made sure that the slice has length `element_size`
+                    let byte_array = unsafe { jth_bytes.try_into().unwrap_unchecked() };
+                    let jth_value = if IS_LITTLE_ENDIAN {
+                        <T as FromBytes>::from_le_bytes(byte_array)
+                    } else {
+                        <T as FromBytes>::from_be_bytes(byte_array)
+                    };
+
+                    let write_index = array_width * index + j;
+                    debug_assert!(write_index < out.len());
+                    // # Safety
+                    // - The target index is smaller than the vector's pre-allocated capacity.
+                    unsafe {
+                        *out.get_unchecked_mut(write_index) = jth_value;
+                    }
+                }
+            }
+        } else {
+            validity.set(index, false);
+        };
+    }
+
+    FixedSizeListArray::try_new(
+        ArrowDataType::FixedSizeList(
+            Box::new(Field::new("".into(), T::PRIMITIVE.into(), true)),
+            array_width,
+        ),
+        from.len(),
+        Box::new(PrimitiveArray::<T>::from_vec(out)),
+        validity.into(),
+    )
+}
+
+/// Casts a `dyn` [`Array`] to a [`FixedSizeListArray`], making any un-castable value a Null.
+///
+/// # Arguments
+///
+/// * `from`: The array to reinterpret.
+/// * `array_width`: The number of items in each `Array`.
+///
+/// # Panics
+///    Panics if `from` is not `BinaryViewArray`.
+pub fn binview_to_fixed_size_list_dyn<T>(
+    from: &dyn Array,
+    array_width: usize,
+    is_little_endian: bool,
+) -> PolarsResult<Box<dyn Array>>
+where
+    T: FromBytes + NativeType,
+    for<'a> &'a <T as FromBytes>::Bytes: TryFrom<&'a [u8]>,
+{
+    let from = from.as_any().downcast_ref().unwrap();
+
+    let result = if is_little_endian {
+        try_binview_to_fixed_size_list::<T, true>(from, array_width)
+    } else {
+        try_binview_to_fixed_size_list::<T, false>(from, array_width)
+    }?;
+    Ok(Box::new(result))
 }
