1962c7ec265bc1047776124e3b82e314cf37f27a
Orson Peters <orsonpeters@gmail.com>
2025-08-05 18:49:37 +0200

    chore: Fix pyo3-polars proc-macro re-exports (#23918)

--- pyo3-polars/pyo3-polars-derive/src/lib.rs
+++ pyo3-polars/pyo3-polars-derive/src/lib.rs
@@ -29,7 +29,7 @@ fn quote_get_kwargs() -> proc_macro2::TokenStream {
     let kwargs = match pyo3_polars::derive::_parse_kwargs(kwargs)  {
         Ok(value) => value,
         Err(err) => {
-            let err = polars_err!(InvalidOperation: "could not parse kwargs: '{}'\n\nCheck: registration of kwargs in the plugin.", err);
+            let err = polars_error::polars_err!(InvalidOperation: "could not parse kwargs: '{}'\n\nCheck: registration of kwargs in the plugin.", err);
             pyo3_polars::derive::_update_last_error(err);
             return;
         }
@@ -48,7 +48,7 @@ fn quote_call_kwargs(ast: &syn::ItemFn, fn_name: &syn::Ident) -> proc_macro2::To
             #ast
 
             // call the function
-        let result: PolarsResult<polars_core::prelude::Series> = #fn_name(&inputs, kwargs);
+        let result: polars_error::PolarsResult<polars_core::prelude::Series> = #fn_name(&inputs, kwargs);
 
     )
 }
@@ -61,7 +61,7 @@ fn quote_call_context(ast: &syn::ItemFn, fn_name: &syn::Ident) -> proc_macro2::T
             #ast
 
             // call the function
-        let result: PolarsResult<polars_core::prelude::Series> = #fn_name(&inputs, context);
+        let result: polars_error::PolarsResult<polars_core::prelude::Series> = #fn_name(&inputs, context);
     )
 }
 
@@ -83,7 +83,7 @@ fn quote_call_context_kwargs(ast: &syn::ItemFn, fn_name: &syn::Ident) -> proc_ma
             #ast
 
             // call the function
-        let result: PolarsResult<polars_core::prelude::Series> = #fn_name(&inputs, context, kwargs);
+        let result: polars_error::PolarsResult<polars_core::prelude::Series> = #fn_name(&inputs, context, kwargs);
     )
 }
 
@@ -92,7 +92,7 @@ fn quote_call_no_kwargs(ast: &syn::ItemFn, fn_name: &syn::Ident) -> proc_macro2:
             // define the function
             #ast
             // call the function
-            let result: PolarsResult<polars_core::prelude::Series> = #fn_name(&inputs);
+            let result: polars_error::PolarsResult<polars_core::prelude::Series> = #fn_name(&inputs);
     )
 }
 
@@ -195,7 +195,7 @@ fn quote_get_inputs() -> proc_macro2::TokenStream {
     quote!(
              let inputs = std::slice::from_raw_parts(field, len);
              let inputs = inputs.iter().map(|field| {
-                 let field = arrow::ffi::import_field_from_c(field).unwrap();
+                 let field = polars_arrow::ffi::import_field_from_c(field).unwrap();
                  let out = polars_core::prelude::Field::from(&field);
                  out
              }).collect::<Vec<_>>();
@@ -225,9 +225,9 @@ fn create_field_function(
     quote! (
         #[no_mangle]
         pub unsafe extern "C" fn #map_field_name(
-            field: *mut arrow::ffi::ArrowSchema,
+            field: *mut polars_arrow::ffi::ArrowSchema,
             len: usize,
-            return_value: *mut arrow::ffi::ArrowSchema,
+            return_value: *mut polars_arrow::ffi::ArrowSchema,
             kwargs_ptr: *const u8,
             kwargs_len: usize,
         ) {
@@ -238,7 +238,7 @@ fn create_field_function(
 
                 match result {
                     Ok(out) => {
-                        let out = arrow::ffi::export_field_to_c(&out.to_arrow(CompatLevel::newest()));
+                        let out = polars_arrow::ffi::export_field_to_c(&out.to_arrow(polars_core::datatypes::CompatLevel::newest()));
                         *return_value = out;
                     },
                     Err(err) => {
@@ -266,16 +266,16 @@ fn create_field_function_from_with_dtype(
     quote! (
         #[no_mangle]
         pub unsafe extern "C" fn #map_field_name(
-            field: *mut arrow::ffi::ArrowSchema,
+            field: *mut polars_arrow::ffi::ArrowSchema,
             len: usize,
-            return_value: *mut arrow::ffi::ArrowSchema
+            return_value: *mut polars_arrow::ffi::ArrowSchema
         ) {
             #inputs
 
             let mapper = polars_plan::prelude::FieldsMapper::new(&inputs);
             let dtype = polars_core::datatypes::DataType::#dtype;
             let out = mapper.with_dtype(dtype).unwrap();
-            let out = arrow::ffi::export_field_to_c(&out.to_arrow(CompatLevel::newest()));
+            let out = polars_arrow::ffi::export_field_to_c(&out.to_arrow(polars_core::datatypes::CompatLevel::newest()));
             *return_value = out;
         }
     )
