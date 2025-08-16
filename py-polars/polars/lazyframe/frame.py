8598d77eac639df5d1fc0e0c5d26cde567c13b91
coastalwhite <me@gburghoorn.com>
2025-08-16 13:03:30 +0200

    mypy again

--- py-polars/polars/lazyframe/frame.py
+++ py-polars/polars/lazyframe/frame.py
@@ -3654,8 +3654,8 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
 
         if not lazy:
             ldf = ldf.with_optimizations(optimizations._pyoptflags)
-            ldf = LazyFrame._from_pyldf(ldf)
-            ldf.collect(engine=engine)
+            lf = LazyFrame._from_pyldf(ldf)
+            lf.collect(engine=engine)
             return None
         return LazyFrame._from_pyldf(ldf)
 
@@ -3712,7 +3712,7 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
             import threading
             from queue import Queue
 
-            q = Queue(maxsize=1)
+            q: Queue = Queue(maxsize=1)
 
             def task() -> None:
                 def _wrap(df: DataFrame) -> bool | None:
