c8b887131404509462aa53d72aed2dc4494d6786
Henry Harbeck <henry.harbeck@canstar.com.au>
2025-08-24 22:37:54 +1000

    skip ADBC tests on windows

--- py-polars/tests/unit/io/database/test_read.py
+++ py-polars/tests/unit/io/database/test_read.py
@@ -876,9 +876,27 @@ def test_read_database_uri_parameterised_multiple(
         ("turbodbc", None, False, "fetchallarrow"),
         ("turbodbc", 50_000, False, "fetchallarrow"),
         ("turbodbc", 50_000, True, "fetcharrowbatches"),
-        ("adbc_driver_postgresql", None, False, "fetch_arrow"),
-        ("adbc_driver_postgresql", 75_000, False, "fetch_arrow"),
-        ("adbc_driver_postgresql", 75_000, True, "fetch_record_batch"),
+        pytest.param(
+            ("adbc_driver_postgresql", None, False, "fetch_arrow"),
+            marks=pytest.mark.skipif(
+                sys.platform == "win32",
+                reason="adbc_driver_postgresql not available on Windows",
+            ),
+        ),
+        pytest.param(
+            ("adbc_driver_postgresql", 75_000, False, "fetch_arrow"),
+            marks=pytest.mark.skipif(
+                sys.platform == "win32",
+                reason="adbc_driver_postgresql not available on Windows",
+            ),
+        ),
+        pytest.param(
+            ("adbc_driver_postgresql", 75_000, True, "fetch_record_batch"),
+            marks=pytest.mark.skipif(
+                sys.platform == "win32",
+                reason="adbc_driver_postgresql not available on Windows",
+            ),
+        ),
     ],
 )
 def test_read_database_mocked(
