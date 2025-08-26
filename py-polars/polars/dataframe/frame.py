9eb16a7695468f5273bb950ea38ee29a7982626c
Orson Peters <orsonpeters@gmail.com>
2025-08-26 18:18:29 +0200

    Remove unnecessary Python mutable borrowed checks

--- py-polars/polars/dataframe/frame.py
+++ py-polars/polars/dataframe/frame.py
@@ -8333,16 +8333,8 @@ class DataFrame:
         """
         require_same_type(self, other)
         if in_place:
-            try:
-                self._df.vstack_mut(other._df)
-            except RuntimeError as exc:
-                if str(exc) == "Already mutably borrowed":
-                    self._df.vstack_mut(other._df.clone())
-                    return self
-                else:
-                    raise
-            else:
-                return self
+            self._df.vstack_mut(other._df)
+            return self
 
         return self._from_pydf(self._df.vstack(other._df))
 
@@ -8400,13 +8392,7 @@ class DataFrame:
         └─────┴─────┘
         """
         require_same_type(self, other)
-        try:
-            self._df.extend(other._df)
-        except RuntimeError as exc:
-            if str(exc) == "Already mutably borrowed":
-                self._df.extend(other._df.clone())
-            else:
-                raise
+        self._df.extend(other._df)
         return self
 
     def drop(
