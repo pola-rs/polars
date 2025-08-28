93a279f22757189c611972fcf22d479944d773c9
Orson Peters <orsonpeters@gmail.com>
2025-08-29 00:46:10 +0200

    Add test

--- py-polars/tests/unit/operations/unique/test_unique.py
+++ py-polars/tests/unit/operations/unique/test_unique.py
@@ -302,3 +302,9 @@ def test_unique_i128_24231() -> None:
         [-(1 << 127), -(1 << 126), 1 << 125, 1 << 126], dtype=pl.Int128
     ).to_frame("a")
     assert_frame_equal(df, df.unique(), check_row_order=False)
+
+
+def test_unique_keep_none_24260() -> None:
+    data = pl.DataFrame({"a": [1, 3, 2], "b": [4, 4, 6]})
+    out = data.lazy().unique(subset="b", keep="none").collect()
+    assert_frame_equal(out, pl.DataFrame({"a": [2], "b": [6]}))
