08c9499224fb79b0c8a28c9bdf638ba7d3b13f55
Ritchie Vink <ritchie46@gmail.com>
2025-08-29 09:37:12 +0200

    fix: Sanitize and warn about eval usage

--- py-polars/polars/io/pyarrow_dataset/anonymous_scan.py
+++ py-polars/polars/io/pyarrow_dataset/anonymous_scan.py
@@ -62,6 +62,11 @@ def _scan_pyarrow_dataset_impl(
     batch_size
         The maximum row count for scanned pyarrow record batches.
 
+    Warnings
+    --------
+    Don't use this if you accept user inputs. Predicates will be evaluated with
+    python 'eval'. There is sanitation in place, but it is a possible attack vector.
+
     Returns
     -------
     DataFrame
