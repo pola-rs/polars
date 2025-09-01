665722ac3f3664c589c4827208d173cc16f0ec68
Orson Peters <orsonpeters@gmail.com>
2025-09-01 15:55:35 +0200

    fix: Invalid conversion from non-bit numpy bools (#24312)

--- crates/polars-python/src/series/construction.rs
+++ crates/polars-python/src/series/construction.rs
@@ -3,7 +3,7 @@ use std::borrow::Cow;
 use arrow::array::Array;
 use arrow::bitmap::BitmapBuilder;
 use arrow::types::NativeType;
-use numpy::{Element, PyArray1, PyArrayMethods};
+use numpy::{Element, PyArray1, PyArrayMethods, PyUntypedArrayMethods};
 use polars_core::prelude::*;
 use polars_core::utils::CustomIterTools;
 use pyo3::exceptions::{PyTypeError, PyValueError};
@@ -58,8 +58,13 @@ impl PySeries {
         _strict: bool,
     ) -> PyResult<Self> {
         let array = array.readonly();
-        let vals = array.as_slice().unwrap();
-        py.enter_polars_series(|| Ok(Series::new(name.into(), vals)))
+
+        // We use raw ptr methods to read this as a u8 slice to work around PyO3/rust-numpy#509.
+        assert!(array.is_contiguous());
+        let data_ptr = array.data().cast::<u8>();
+        let data_len = array.len();
+        let vals = unsafe { core::slice::from_raw_parts(data_ptr, data_len) };
+        py.enter_polars_series(|| Series::new(name.into(), vals).cast(&DataType::Boolean))
     }
 
     #[staticmethod]
