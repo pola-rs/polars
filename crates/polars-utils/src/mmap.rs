a6b8c22c9cfa14d4c516d78f9eaae0ca8cdaa759
Nilsson <victor.b.nilsson@if.se>
2025-09-03 16:02:02 +0200

    Fallback for unix only

--- crates/polars-utils/src/mmap.rs
+++ crates/polars-utils/src/mmap.rs
@@ -346,13 +346,14 @@ impl MMapSemaphore {
     ) -> PolarsResult<MMapSemaphore> {
         let mmap = match unsafe { options.map(file) } {
             Ok(m) => m,
-        
+
             // Mmap can fail with ENODEV on filesystems which don't support
             // MAP_SHARED, try MAP_PRIVATE instead, see #24343.
-            Err(e) if e.raw_os_error() == Some(libc::ENODEV) => {
-                unsafe { options.map_copy_read_only(file)? }
-            }
-            
+            #[cfg(target_family = "unix")]
+            Err(e) if e.raw_os_error() == Some(libc::ENODEV) => unsafe {
+                options.map_copy_read_only(file)?
+            },
+
             Err(e) => return Err(e.into()),
         };
 
