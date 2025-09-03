2e47352988d97e470afe74b642b0357d03c02b18
Simon Lin <simonlin.rqmmw@slmail.me>
2025-09-03 22:44:07 +1000

    style

--- crates/polars-python/src/lazyframe/general.rs
+++ crates/polars-python/src/lazyframe/general.rs
@@ -679,7 +679,7 @@ impl PyLazyFrame {
         py.enter_polars_ok(|| {
             let ldf = self.ldf.read().clone();
 
-            let func = move || {
+            polars_io::utils::spawn_blocking(move || {
                 let result = ldf
                     .collect_with_engine(engine.0)
                     .map(PyDataFrame::new)
@@ -696,9 +696,7 @@ impl PyLazyFrame {
                             .ok();
                     },
                 });
-            };
-
-            polars_io::utils::spawn_blocking(func);
+            });
         })
     }
 
