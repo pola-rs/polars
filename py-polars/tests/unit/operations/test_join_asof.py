c4a7846bd03b0fcc7aae77da2fc05c0de024eeaa
Marshall Crumiller <mcrumiller@gmail.com>
2025-09-05 22:24:28 -0400

    Add u8 and u16 implementations

--- py-polars/tests/unit/operations/test_join_asof.py
+++ py-polars/tests/unit/operations/test_join_asof.py
@@ -8,6 +8,7 @@ import numpy as np
 import pytest
 
 import polars as pl
+from polars.datatypes.group import INTEGER_DTYPES
 from polars.exceptions import DuplicateError, InvalidOperationError
 from polars.testing import assert_frame_equal
 
@@ -1431,3 +1432,37 @@ def test_join_asof_planner_schema_24000() -> None:
     q = a.lazy().join_asof(b.lazy(), left_on="index", right_on="index_right")
 
     assert q.collect().schema == q.collect_schema()
+
+
+@pytest.mark.parametrize("dtype", INTEGER_DTYPES)
+def test_join_asof_int_dtypes_24383(dtype: PolarsIntegerType) -> None:
+    lf1 = pl.LazyFrame(
+        {
+            "id": pl.Series([1], dtype=dtype),
+            "date": pl.Series([date(2025, 12, 31)], dtype=pl.Date),
+        }
+    )
+
+    lf2 = pl.LazyFrame(
+        {
+            "id": pl.Series([1], dtype=dtype),
+            "date": pl.Series([date(2025, 12, 31)], dtype=pl.Date),
+            "value": pl.Series([2.5], dtype=pl.Float32),
+        }
+    )
+
+    result = lf1.join_asof(
+        other=lf2,
+        on="date",
+        by="id",
+        check_sortedness=False,
+    )
+    expected = pl.DataFrame(
+        {
+            "id": pl.Series([1], dtype=dtype),
+            "date": pl.Series([date(2025, 12, 31)], dtype=pl.Date),
+            "value": pl.Series([2.5], dtype=pl.Float32),
+        }
+    )
+    assert result.collect_schema() == expected.schema
+    assert_frame_equal(result.collect(), expected)
