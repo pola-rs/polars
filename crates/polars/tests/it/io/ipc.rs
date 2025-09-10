50362694e38bff182eb7c91ddb6e2dbe54ec8a2a
Ritchie Vink <ritchie46@gmail.com>
2025-09-10 12:45:36 +0200

    feat: Add Zstd Level to IPC compression DSL

--- crates/polars/tests/it/io/ipc.rs
+++ crates/polars/tests/it/io/ipc.rs
@@ -120,7 +120,11 @@ fn test_read_ipc_with_columns() {
 fn test_write_with_compression() {
     let mut df = create_df();
 
-    let compressions = vec![None, Some(IpcCompression::LZ4), Some(IpcCompression::ZSTD)];
+    let compressions = vec![
+        None,
+        Some(IpcCompression::LZ4),
+        Some(IpcCompression::ZSTD(Default::default())),
+    ];
 
     for compression in compressions.into_iter() {
         let mut buf: Cursor<Vec<u8>> = Cursor::new(Vec::new());
