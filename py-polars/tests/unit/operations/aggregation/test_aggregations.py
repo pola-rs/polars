899aae3d5cf743f9f93dcc954431f5168178c214
Amber Sprenkels <amber@electricdusk.com>
2025-09-15 14:31:45 +0200

    Revert old code that should not have been committed

--- py-polars/tests/unit/operations/aggregation/test_aggregations.py
+++ py-polars/tests/unit/operations/aggregation/test_aggregations.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from datetime import date, datetime, timedelta
-from typing import TYPE_CHECKING, Any, Callable, cast
+from typing import TYPE_CHECKING, Any, cast
 
 import numpy as np
 import pytest
