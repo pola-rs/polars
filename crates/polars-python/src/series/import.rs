d39f35be23ab014cd53b1f013827de577c14b2a6
Simon Lin <simonlin.rqmmw@slmail.me>
2025-09-15 23:00:23 +1000

    c

--- crates/polars-python/src/series/import.rs
+++ crates/polars-python/src/series/import.rs
@@ -200,7 +200,7 @@ impl PySeries {
                 if value > max_decimal_value {
                     return Err(PyValueError::new_err(format!(
                         "iceberg decoded value for decimal exceeded precision: \
-                    value: {}, precision: {}",
+                        value: {}, precision: {}",
                         value, precision
                     )));
                 }
