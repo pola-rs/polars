1ae1e7155bd6747aca961c9b3be8f2b0d59eed35
r-brink <auc2yz7ab@mozmail.com>
2025-09-19 22:18:37 +0200

    chore: Fix quickstart example in Polars Cloud user guide to remove ambiguity

--- docs/source/src/python/polars-cloud/quickstart.py
+++ docs/source/src/python/polars-cloud/quickstart.py
@@ -18,15 +18,13 @@ lf = pl.LazyFrame(
 )
 
 # At this point, the query has not been executed yet.
-# We need to call `.remote()` to signal that we want to run on Polars Cloud and then `.sink_parquet()` to send
-# the query and execute it.
+# We need to call `.remote()` to signal that we want to run
+# on Polars Cloud and then `.show()` to execute.
 
 (
-    lf.remote(context=ctx)
-    .sink_parquet(uri="s3://my-bucket/result.parquet")
+    lf.remote(context=ctx).show()
 )
 
-# We can then wait for the result with `result = lf.await_result()`.
 # This will only include a few rows of the output as the result might be very large.
 # The query and compute used will also show up in the portal https://cloud.pola.rs/portal/
 
