cf237fad6252c2e1e02cbab1c37dee812cf41bf5
coastalwhite <me@gburghoorn.com>
2025-09-23 10:52:14 +0200

    again

--- py-polars/tests/unit/test_init.py
+++ py-polars/tests/unit/test_init.py
@@ -48,11 +48,8 @@ def test_import_all() -> None:
 
 def test_version() -> None:
     # This has already gone wrong once (#23940), preventing future problems.
-    # Match something like 1.3.0 and ignore b01 part.
-    # This is needed because beta release names are changed by Python.
-    pattern = r"\d+\.\d+\.\d+"
-    lhs = re.search(pattern, pl.__version__).group()  # type: ignore[union-attr]
-    rhs = re.search(pattern, importlib.metadata.version("polars")).group()  # type: ignore[union-attr]
+    lhs = pl.__version__.replace("-beta.", "b")
+    rhs = importlib.metadata.version("polars")
 
     assert lhs == rhs, (
         f"`static PYPOLARS_VERSION` ({lhs}) at `crates/polars-python/src/c_api/mod.rs` "
