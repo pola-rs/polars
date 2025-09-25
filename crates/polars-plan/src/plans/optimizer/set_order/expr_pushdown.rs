ebd491ff6d130cc7b1743e1507c246be2818ca68
Simon Lin <simonlin.rqmmw@slmail.me>
2025-09-25 20:01:04 +1000

    c

--- crates/polars-plan/src/plans/optimizer/set_order/expr_pushdown.rs
+++ crates/polars-plan/src/plans/optimizer/set_order/expr_pushdown.rs
@@ -92,240 +92,266 @@ pub fn adjust_for_with_columns_context(
 ///
 /// This answers the question:
 /// > Given that my output is (un)ordered, can my input be unordered?
-#[recursive::recursive]
 pub fn get_frame_observing(
     aexpr: &AExpr,
     expr_arena: &Arena<AExpr>,
 ) -> Result<ExprOutputOrder, FrameOrderObserved> {
-    macro_rules! rec {
-        ($expr:expr) => {{ get_frame_observing(expr_arena.get($expr), expr_arena)? }};
+    ObservableOrderingsResolver::new(ExprOutputOrder::Frame)
+        .resolve_observable_orderings(aexpr, expr_arena)
+}
+
+pub(super) struct ObservableOrderingsResolver {
+    column_ordering: ExprOutputOrder,
+}
+
+impl ObservableOrderingsResolver {
+    pub(super) fn new(column_ordering: ExprOutputOrder) -> Self {
+        use ExprOutputOrder as O;
+
+        match &column_ordering {
+            O::Frame | O::Independent | O::None => {},
+            O::Both => panic!(),
+        }
+
+        Self { column_ordering }
     }
 
-    macro_rules! zip {
-        ($($expr:expr),*) => {{ zip([$(Ok(rec!($expr))),*])? }};
-    }
+    #[recursive::recursive]
+    pub(super) fn resolve_observable_orderings(
+        &self,
+        aexpr: &AExpr,
+        expr_arena: &Arena<AExpr>,
+    ) -> Result<ExprOutputOrder, FrameOrderObserved> {
+        macro_rules! rec {
+            ($expr:expr) => {{ self.resolve_observable_orderings(expr_arena.get($expr), expr_arena)? }};
+        }
 
-    use ExprOutputOrder as O;
-    Ok(match aexpr {
-        // Explode creates local orders.
-        //
-        // The following observes order:
-        //
-        // a: [[1, 2], [3]]
-        // b: [[3], [4, 5]]
-        //
-        // col(a).explode() * col(b).explode()
-        AExpr::Explode { expr, .. } => rec!(*expr) | O::Independent,
+        macro_rules! zip {
+            ($($expr:expr),*) => {{ zip([$(Ok(rec!($expr))),*])? }};
+        }
 
-        AExpr::Column(_) => O::Frame,
-        AExpr::Literal(lv) if lv.is_scalar() => O::None,
-        AExpr::Literal(_) => O::Independent,
+        use ExprOutputOrder as O;
+        Ok(match aexpr {
+            // Explode creates local orders.
+            //
+            // The following observes order:
+            //
+            // a: [[1, 2], [3]]
+            // b: [[3], [4, 5]]
+            //
+            // col(a).explode() * col(b).explode()
+            AExpr::Explode { expr, .. } => rec!(*expr) | O::Independent,
 
-        AExpr::Cast { expr, .. } => rec!(*expr),
+            AExpr::Column(_) => self.column_ordering,
+            AExpr::Literal(lv) if lv.is_scalar() => O::None,
+            AExpr::Literal(_) => O::Independent,
 
-        // Elementwise can be seen as a `zip + op`.
-        AExpr::BinaryExpr { left, op: _, right } => zip!(*left, *right),
-        AExpr::Filter { input, by } => zip!(*input, *by),
-        AExpr::Ternary {
-            predicate,
-            truthy,
-            falsy,
-        } => zip!(*predicate, *truthy, *falsy),
+            AExpr::Cast { expr, .. } => rec!(*expr),
 
-        AExpr::Sort { expr, options } => {
-            if options.maintain_order {
-                rec!(*expr) | O::Independent
-            } else {
-                _ = rec!(*expr);
-                O::Independent
-            }
-        },
-        AExpr::SortBy {
-            expr,
-            by,
-            sort_options,
-        } => {
-            let mut zipped = rec!(*expr);
-            for e in by {
-                zipped = zipped.zip_with(rec!(*e))?;
-            }
+            // Elementwise can be seen as a `zip + op`.
+            AExpr::BinaryExpr { left, op: _, right } => zip!(*left, *right),
+            AExpr::Filter { input, by } => zip!(*input, *by),
+            AExpr::Ternary {
+                predicate,
+                truthy,
+                falsy,
+            } => zip!(*predicate, *truthy, *falsy),
 
-            if sort_options.maintain_order {
-                zipped | O::Independent
-            } else {
-                O::Independent
-            }
-        },
-
-        AExpr::Agg(agg) => match agg {
-            // Input order agnostic aggregations.
-            IRAggExpr::Min { input: node, .. }
-            | IRAggExpr::Max { input: node, .. }
-            | IRAggExpr::Median(node)
-            | IRAggExpr::NUnique(node)
-            | IRAggExpr::Mean(node)
-            | IRAggExpr::Sum(node)
-            | IRAggExpr::Count { input: node, .. }
-            | IRAggExpr::Std(node, _)
-            | IRAggExpr::Var(node, _) => {
-                // Input order is deregarded, but must not observe order.
-                _ = rec!(*node);
-                O::None
-            },
-            IRAggExpr::Quantile { expr, quantile, .. } => {
-                // Input and quantile order is deregarded, but must not observe order.
-                _ = rec!(*expr);
-                _ = rec!(*quantile);
-                O::None
-            },
-
-            // Input order observing aggregations.
-            IRAggExpr::Implode(node) | IRAggExpr::First(node) | IRAggExpr::Last(node) => {
-                if rec!(*node).has_frame_ordering() {
-                    return Err(FrameOrderObserved);
+            AExpr::Sort { expr, options } => {
+                if options.maintain_order {
+                    rec!(*expr) | O::Independent
+                } else {
+                    _ = rec!(*expr);
+                    O::Independent
                 }
-                O::None
             },
-
-            // @NOTE: This aggregation makes very little sense. We do the most pessimistic thing
-            // possible here.
-            IRAggExpr::AggGroups(_) => return Err(FrameOrderObserved),
-        },
-
-        AExpr::Gather {
-            expr,
-            idx,
-            returns_scalar,
-        } => {
-            let expr = rec!(*expr);
-            let idx = rec!(*idx);
-
-            // We need to ensure that the values come in frame order. The order of the idxes is
-            // propagated.
-            if expr.has_frame_ordering() {
-                return Err(FrameOrderObserved);
-            }
-
-            if *returns_scalar { O::None } else { idx }
-        },
-        AExpr::AnonymousFunction { input, options, .. }
-        | AExpr::Function { input, options, .. } => {
-            // By definition, does not observe any input so cannot observe any ordering.
-            if input.is_empty() {
-                return Ok(
-                    if options.flags.returns_scalar() || options.flags.is_output_unordered() {
-                        O::None
-                    } else {
-                        O::Independent
-                    },
-                );
-            }
-
-            // Elementwise are regarded as a `zip + function`.
-            if options.flags.is_elementwise() {
-                return zip(input.iter().map(|e| Ok(rec!(e.node()))));
-            }
-
-            if options.flags.propagates_order() {
-                // Propagate the order of the singular input, this is for expressions like
-                // `drop_nulls` and `rechunk`.
-                assert_eq!(input.len(), 1);
-                match rec!(input[0].node()) {
-                    v if !(options.flags.is_row_separable()
-                        || options.flags.is_input_order_agnostic())
-                        && v.has_frame_ordering() =>
-                    {
-                        return Err(FrameOrderObserved);
-                    },
-                    v => v,
-                }
-            } else if options.flags.is_input_order_agnostic() {
-                // There are also expressions that are entirely input order agnostic like
-                // `null_count` and `unique(maintain_order=False)`
-                for e in input {
-                    _ = rec!(e.node());
+            AExpr::SortBy {
+                expr,
+                by,
+                sort_options,
+            } => {
+                let mut zipped = rec!(*expr);
+                for e in by {
+                    zipped = zipped.zip_with(rec!(*e))?;
                 }
 
-                if options.returns_scalar() || options.flags.is_output_unordered() {
-                    O::None
+                if sort_options.maintain_order {
+                    zipped | O::Independent
                 } else {
                     O::Independent
                 }
-            } else {
-                // For other expressions, we are observing frame order i.f.f. any of the inputs are
-                // observing frame order.
-                for e in input {
-                    if rec!(e.node()).has_frame_ordering() {
+            },
+
+            AExpr::Agg(agg) => match agg {
+                // Input order agnostic aggregations.
+                IRAggExpr::Min { input: node, .. }
+                | IRAggExpr::Max { input: node, .. }
+                | IRAggExpr::Median(node)
+                | IRAggExpr::NUnique(node)
+                | IRAggExpr::Mean(node)
+                | IRAggExpr::Sum(node)
+                | IRAggExpr::Count { input: node, .. }
+                | IRAggExpr::Std(node, _)
+                | IRAggExpr::Var(node, _) => {
+                    // Input order is deregarded, but must not observe order.
+                    _ = rec!(*node);
+                    O::None
+                },
+                IRAggExpr::Quantile { expr, quantile, .. } => {
+                    // Input and quantile order is deregarded, but must not observe order.
+                    _ = rec!(*expr);
+                    _ = rec!(*quantile);
+                    O::None
+                },
+
+                // Input order observing aggregations.
+                IRAggExpr::Implode(node) | IRAggExpr::First(node) | IRAggExpr::Last(node) => {
+                    if rec!(*node).has_frame_ordering() {
                         return Err(FrameOrderObserved);
                     }
-                }
-
-                if options.returns_scalar() || options.flags.is_output_unordered() {
                     O::None
-                } else {
-                    O::Independent
-                }
-            }
-        },
+                },
 
-        AExpr::Eval {
-            expr,
-            evaluation: _,
-            variant,
-        } => match variant {
-            EvalVariant::List => rec!(*expr),
-            EvalVariant::Cumulative { min_samples: _ } => {
+                // @NOTE: This aggregation makes very little sense. We do the most pessimistic thing
+                // possible here.
+                IRAggExpr::AggGroups(_) => return Err(FrameOrderObserved),
+            },
+
+            AExpr::Gather {
+                expr,
+                idx,
+                returns_scalar,
+            } => {
                 let expr = rec!(*expr);
+                let idx = rec!(*idx);
+
+                // We need to ensure that the values come in frame order. The order of the idxes is
+                // propagated.
                 if expr.has_frame_ordering() {
                     return Err(FrameOrderObserved);
                 }
-                expr
+
+                if *returns_scalar { O::None } else { idx }
             },
-        },
+            AExpr::AnonymousFunction { input, options, .. }
+            | AExpr::Function { input, options, .. } => {
+                // By definition, does not observe any input so cannot observe any ordering.
+                if input.is_empty() {
+                    return Ok(
+                        if options.flags.returns_scalar() || options.flags.is_output_unordered() {
+                            O::None
+                        } else {
+                            O::Independent
+                        },
+                    );
+                }
 
-        AExpr::Window {
-            function,
-            partition_by,
-            order_by,
-            options: _,
-        } => {
-            let input = rec!(*function);
+                // Elementwise are regarded as a `zip + function`.
+                if options.flags.is_elementwise() {
+                    return zip(input.iter().map(|e| Ok(rec!(e.node()))));
+                }
 
-            // @Performance.
-            // All of the code below might be a bit pessimistic, several window function variants
-            // are length preserving and/or propagate order in specific ways.
-            if input.has_frame_ordering() {
-                return Err(FrameOrderObserved);
-            }
-            for e in partition_by {
-                if rec!(*e).has_frame_ordering() {
+                if options.flags.propagates_order() {
+                    // Propagate the order of the singular input, this is for expressions like
+                    // `drop_nulls` and `rechunk`.
+                    assert_eq!(input.len(), 1);
+                    match rec!(input[0].node()) {
+                        v if !(options.flags.is_row_separable()
+                            || options.flags.is_input_order_agnostic())
+                            && v.has_frame_ordering() =>
+                        {
+                            return Err(FrameOrderObserved);
+                        },
+                        v => v,
+                    }
+                } else if options.flags.is_input_order_agnostic() {
+                    // There are also expressions that are entirely input order agnostic like
+                    // `null_count` and `unique(maintain_order=False)`
+                    for e in input {
+                        _ = rec!(e.node());
+                    }
+
+                    if options.returns_scalar() || options.flags.is_output_unordered() {
+                        O::None
+                    } else {
+                        O::Independent
+                    }
+                } else {
+                    // For other expressions, we are observing frame order i.f.f. any of the inputs are
+                    // observing frame order.
+                    for e in input {
+                        if rec!(e.node()).has_frame_ordering() {
+                            return Err(FrameOrderObserved);
+                        }
+                    }
+
+                    if options.returns_scalar() || options.flags.is_output_unordered() {
+                        O::None
+                    } else {
+                        O::Independent
+                    }
+                }
+            },
+
+            AExpr::Eval {
+                expr,
+                evaluation: _,
+                variant,
+            } => match variant {
+                EvalVariant::List => rec!(*expr),
+                EvalVariant::Cumulative { min_samples: _ } => {
+                    let expr = rec!(*expr);
+                    if expr.has_frame_ordering() {
+                        return Err(FrameOrderObserved);
+                    }
+                    expr
+                },
+            },
+
+            AExpr::Window {
+                function,
+                partition_by,
+                order_by,
+                options: _,
+            } => {
+                let input = rec!(*function);
+
+                // @Performance.
+                // All of the code below might be a bit pessimistic, several window function variants
+                // are length preserving and/or propagate order in specific ways.
+                if input.has_frame_ordering() {
                     return Err(FrameOrderObserved);
                 }
-            }
-            if let Some((e, _)) = &order_by
-                && rec!(*e).has_frame_ordering()
-            {
-                return Err(FrameOrderObserved);
-            }
-            O::Independent
-        },
-        AExpr::Slice {
-            input,
-            offset,
-            length,
-        } => {
-            // @NOTE
-            // `offset` and `length` are supposed to be scalars, they have to resolved as they
-            // might be order observing, but are not important for the output order.
-            _ = rec!(*offset);
-            _ = rec!(*length);
+                for e in partition_by {
+                    if rec!(*e).has_frame_ordering() {
+                        return Err(FrameOrderObserved);
+                    }
+                }
+                if let Some((e, _)) = &order_by
+                    && rec!(*e).has_frame_ordering()
+                {
+                    return Err(FrameOrderObserved);
+                }
+                O::Independent
+            },
+            AExpr::Slice {
+                input,
+                offset,
+                length,
+            } => {
+                // @NOTE
+                // `offset` and `length` are supposed to be scalars, they have to resolved as they
+                // might be order observing, but are not important for the output order.
+                _ = rec!(*offset);
+                _ = rec!(*length);
 
-            let input = rec!(*input);
-            if input.has_frame_ordering() {
-                return Err(FrameOrderObserved);
-            }
-            input
-        },
-        AExpr::Len => O::None,
-    })
+                let input = rec!(*input);
+                if input.has_frame_ordering() {
+                    return Err(FrameOrderObserved);
+                }
+                input
+            },
+            AExpr::Len => O::None,
+        })
+    }
 }
