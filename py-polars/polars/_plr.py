f38ab4d1d57152d6e85f978a4a7a8162cd1c6724
Ritchie Vink <ritchie46@gmail.com>
2025-09-27 12:34:13 +0200

    build: Include source in main package

--- py-polars/polars/_plr.py
+++ py-polars/polars/_plr.py
@@ -7,7 +7,7 @@ import sys
 
 from polars._cpu_check import check_cpu_flags
 
-PKG_VERSION = "1.34.0-beta.2"
+PKG_VERSION = "1.34.0-beta.3"
 
 # Replaced during the build process with our list of required feature flags
 # enabled at compile time.
