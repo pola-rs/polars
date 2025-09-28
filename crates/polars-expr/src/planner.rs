5add0501d6a13081f4d1e1e4fc97171eb9e34246
Koen Denecker <koen@polars.tech>
2025-09-28 20:27:58 +0200

    fix: Fix dtype in `EvalExpr`

--- crates/polars-expr/src/planner.rs
+++ crates/polars-expr/src/planner.rs
@@ -516,18 +516,7 @@ fn create_physical_expr_inner(
             let mut pd_group = ExprPushdownGroup::Pushable;
             pd_group.update_with_expr_rec(expr_arena.get(*evaluation), expr_arena, None);
 
-            let non_aggregated_output_field = expr_arena
-                .get(expression)
-                .to_field(&ToFieldContext::new(expr_arena, schema))?;
-            let output_field_with_ctx =
-                if matches!(ctxt, Context::Aggregation) && !is_scalar_ae(expression, expr_arena) {
-                    let mut f = non_aggregated_output_field.clone();
-                    f.coerce(non_aggregated_output_field.dtype().clone().implode());
-                    f
-                } else {
-                    non_aggregated_output_field.clone()
-                };
-            let non_aggregated_output_field = expr_arena
+            let output_field = expr_arena
                 .get(expression)
                 .to_field(&ToFieldContext::new(expr_arena, schema))?;
             let input_field = expr_arena
@@ -562,8 +551,7 @@ fn create_physical_expr_inner(
                 *variant,
                 node_to_expr(expression, expr_arena),
                 state.allow_threading,
-                output_field_with_ctx,
-                non_aggregated_output_field.dtype,
+                output_field,
                 is_scalar,
                 pd_group,
                 evaluation_is_scalar,
