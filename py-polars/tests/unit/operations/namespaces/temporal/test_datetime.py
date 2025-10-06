18fa96281101bc2de49dddb2a1758d83f63b46fa
Alexander Beedie <alexander.m.beedie@icloud.com>
2025-10-06 09:38:58 +0400

    perf: Duration string parsing optimisation

--- py-polars/tests/unit/operations/namespaces/temporal/test_datetime.py
+++ py-polars/tests/unit/operations/namespaces/temporal/test_datetime.py
@@ -225,36 +225,35 @@ def test_offset_by_invalid_duration(offset: str) -> None:
 
 @pytest.mark.parametrize("offset", ["++1d", "+1d+1m+1s", "--1d", "-1d-1m-1s"])
 def test_offset_by_invalid_duration_unary_ops(offset: str) -> None:
-    op_name = "plus" if "+" in offset else "minus"
+    op = "+" if "+" in offset else "-"
     with pytest.raises(
         InvalidOperationError,
-        match=f"duration string can only have a single {op_name} sign",
+        match=rf"duration string can only have a single '\{op}' sign",
     ):
         pl.Series([datetime(2025, 10, 3, 11, 42)]).dt.offset_by(offset)
 
 
-def test_offset_by_missing_unit() -> None:
+@pytest.mark.parametrize("offset", ["1", "1mo23d4", "-2d1"])
+def test_offset_by_invalid_missing_unit(offset: str) -> None:
     with pytest.raises(
         InvalidOperationError,
-        match="expected a unit to follow integer in the duration string '1'",
+        match=f"expected a valid unit to follow integer in the duration string '{offset}'",
     ):
-        pl.Series([datetime(2022, 3, 20, 5, 7)]).dt.offset_by("1")
+        pl.Series([datetime(2025, 10, 6, 13, 45)]).dt.offset_by(offset)
 
+
+def test_offset_by_invalid_units() -> None:
     with pytest.raises(
         InvalidOperationError,
-        match="expected a unit to follow integer in the duration string '1mo23d4'",
+        match="expected a valid unit to follow integer in the duration string '12時30分45秒'",
     ):
-        pl.Series([datetime(2022, 3, 20, 5, 7)]).dt.offset_by("1mo23d4")
+        pl.Series([datetime(2025, 10, 6, 13, 45)]).dt.offset_by("12時30分45秒")
 
+
+def test_offset_by_missing_unit_in_expr() -> None:
     with pytest.raises(
         InvalidOperationError,
-        match="expected a unit to follow integer in the duration string '-2d1'",
-    ):
-        pl.Series([datetime(2022, 3, 20, 5, 7)]).dt.offset_by("-2d1")
-
-    with pytest.raises(
-        InvalidOperationError,
-        match="expected a unit to follow integer in the duration string '1d2'",
+        match="expected a valid unit to follow integer in the duration string '1d2'",
     ):
         pl.DataFrame(
             {"a": [datetime(2022, 3, 20, 5, 7)] * 2, "b": ["1d", "1d2"]}
