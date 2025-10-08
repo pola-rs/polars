37eae3706a015a8e04c9a9da96e7820215758b0d
Alexander Beedie <alexander.m.beedie@icloud.com>
2025-10-08 16:38:38 +0400

    feat(python): Allow `glimpse` to return a `DataFrame`

--- py-polars/tests/unit/dataframe/test_glimpse.py
+++ py-polars/tests/unit/dataframe/test_glimpse.py
@@ -4,52 +4,51 @@ from typing import Any
 
 import polars as pl
 
+TEST_DF = pl.DataFrame(
+    {
+        "a": [1.0, 2.8, 3.0],
+        "b": [4, 5, None],
+        "c": [True, False, True],
+        "d": [None, "b", "c"],
+        "e": ["usd", "eur", None],
+        "f": pl.datetime_range(
+            datetime(2023, 1, 1),
+            datetime(2023, 1, 3),
+            "1d",
+            time_unit="us",
+            eager=True,
+        ),
+        "g": pl.datetime_range(
+            datetime(2023, 1, 1),
+            datetime(2023, 1, 3),
+            "1d",
+            time_unit="ms",
+            eager=True,
+        ),
+        "h": pl.datetime_range(
+            datetime(2023, 1, 1),
+            datetime(2023, 1, 3),
+            "1d",
+            time_unit="ns",
+            eager=True,
+        ),
+        "i": [[5, 6], [3, 4], [9, 8]],
+        "j": [[5.0, 6.0], [3.0, 4.0], [9.0, 8.0]],
+        "k": [["A", "a"], ["B", "b"], ["C", "c"]],
+    }
+)
+
 
 def test_glimpse(capsys: Any) -> None:
-    df = pl.DataFrame(
-        {
-            "a": [1.0, 2.8, 3.0],
-            "b": [4, 5, None],
-            "c": [True, False, True],
-            "d": [None, "b", "c"],
-            "e": ["usd", "eur", None],
-            "f": pl.datetime_range(
-                datetime(2023, 1, 1),
-                datetime(2023, 1, 3),
-                "1d",
-                time_unit="us",
-                eager=True,
-            ),
-            "g": pl.datetime_range(
-                datetime(2023, 1, 1),
-                datetime(2023, 1, 3),
-                "1d",
-                time_unit="ms",
-                eager=True,
-            ),
-            "h": pl.datetime_range(
-                datetime(2023, 1, 1),
-                datetime(2023, 1, 3),
-                "1d",
-                time_unit="ns",
-                eager=True,
-            ),
-            "i": [[5, 6], [3, 4], [9, 8]],
-            "j": [[5.0, 6.0], [3.0, 4.0], [9.0, 8.0]],
-            "k": [["A", "a"], ["B", "b"], ["C", "c"]],
-        }
-    )
-    result = df.glimpse(return_as_string=True)
-
     expected = textwrap.dedent(
         """\
         Rows: 3
         Columns: 11
         $ a          <f64> 1.0, 2.8, 3.0
-        $ b          <i64> 4, 5, None
+        $ b          <i64> 4, 5, null
         $ c         <bool> True, False, True
-        $ d          <str> None, 'b', 'c'
-        $ e          <str> 'usd', 'eur', None
+        $ d          <str> null, 'b', 'c'
+        $ e          <str> 'usd', 'eur', null
         $ f <datetime[μs]> 2023-01-01 00:00:00, 2023-01-02 00:00:00, 2023-01-03 00:00:00
         $ g <datetime[ms]> 2023-01-01 00:00:00, 2023-01-02 00:00:00, 2023-01-03 00:00:00
         $ h <datetime[ns]> 2023-01-01 00:00:00, 2023-01-02 00:00:00, 2023-01-03 00:00:00
@@ -58,17 +57,22 @@ def test_glimpse(capsys: Any) -> None:
         $ k    <list[str]> ['A', 'a'], ['B', 'b'], ['C', 'c']
         """
     )
-    assert result == expected
+    for result in (
+        # check deprecated parameter still works
+        TEST_DF.glimpse(return_as_string=True),  # type: ignore[call-overload]
+        TEST_DF.glimpse(return_type="string"),
+    ):
+        assert result == expected
 
     # the default is to print to the console
-    df.glimpse()
+    TEST_DF.glimpse()
     # remove the last newline on the capsys
     assert capsys.readouterr().out[:-1] == expected
 
-    colc = "a" * 96
-    df = pl.DataFrame({colc: [11, 22, 33, 44, 55, 66]})
+    col_aaaaaaa = "a" * 96
+    df = pl.DataFrame({col_aaaaaaa: [11, 22, 33, 44, 55, 66]})
     result = df.glimpse(
-        return_as_string=True, max_colname_length=20, max_items_per_column=4
+        return_type="string", max_colname_length=20, max_items_per_column=4
     )
     expected = textwrap.dedent(
         """\
@@ -80,9 +84,63 @@ def test_glimpse(capsys: Any) -> None:
     assert result == expected
 
 
+def test_glimpse_as_frame() -> None:
+    result = TEST_DF.glimpse(return_type="frame")
+
+    assert isinstance(result, pl.DataFrame)
+    assert result.schema == pl.Schema(
+        {
+            "column": pl.String(),
+            "dtype": pl.String(),
+            "values": pl.List(pl.String),
+        }
+    )
+    assert result.to_dict(as_series=False) == {
+        "column": [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+        ],
+        "dtype": [
+            "f64",
+            "i64",
+            "bool",
+            "str",
+            "str",
+            "datetime[μs]",
+            "datetime[ms]",
+            "datetime[ns]",
+            "list[i64]",
+            "list[f64]",
+            "list[str]",
+        ],
+        "values": [
+            ["1.0", "2.8", "3.0"],
+            ["4", "5", None],
+            ["True", "False", "True"],
+            [None, "'b'", "'c'"],
+            ["'usd'", "'eur'", None],
+            ["2023-01-01 00:00:00", "2023-01-02 00:00:00", "2023-01-03 00:00:00"],
+            ["2023-01-01 00:00:00", "2023-01-02 00:00:00", "2023-01-03 00:00:00"],
+            ["2023-01-01 00:00:00", "2023-01-02 00:00:00", "2023-01-03 00:00:00"],
+            ["[5, 6]", "[3, 4]", "[9, 8]"],
+            ["[5.0, 6.0]", "[3.0, 4.0]", "[9.0, 8.0]"],
+            ["['A', 'a']", "['B', 'b']", "['C', 'c']"],
+        ],
+    }
+
+
 def test_glimpse_colname_length() -> None:
     df = pl.DataFrame({"a" * 100: [1, 2, 3]})
-    result = df.glimpse(max_colname_length=96, return_as_string=True)
+    result = df.glimpse(max_colname_length=96, return_type="string")
 
     expected = f"$ {'a' * 95}… <i64> 1, 2, 3"
     assert result.strip().split("\n")[-1] == expected
