2918d170acc5e260b701b04e24d1147e851f4ce4
koki watanabe <address8128@gmail.com>
2025-10-09 22:52:35 +0900

    fix: test the value of list.sum

--- py-polars/tests/unit/datatypes/test_list.py
+++ py-polars/tests/unit/datatypes/test_list.py
@@ -331,7 +331,7 @@ def test_list_sum_and_dtypes() -> None:
         df = pl.DataFrame(
             {
                 "a": [[1], [1, 2, 3], [1, 2, 3, 4], [1, 2, 3, 4, 5]],
-                "b": [[1], [1, 2, None], [1, 2, 3, None], [1, 2, 3, 4, None]],
+                "b": [[None], [1, 2, None], [1, 2, 3, None], [1, 2, 3, 4, None]],
             },
             schema={
                 "a": pl.List(dt_in),
@@ -343,12 +343,18 @@ def test_list_sum_and_dtypes() -> None:
         assert summed_a.dtypes == [dt_out]
         assert summed_a.item() == 32
         assert df.select(pl.col("a").list.sum()).dtypes == [dt_out]
+        assert df.select(pl.col("a").list.sum()).to_dict(as_series=False) == {
+            "a": [1, 6, 10, 15]
+        }
 
         # include nulls in the list
         summed_b = df.select("b").explode("b").sum()
         assert summed_b.dtypes == [dt_out]
-        assert summed_b.item() == 20
+        assert summed_b.item() == 19
         assert df.select(pl.col("b").list.sum()).dtypes == [dt_out]
+        assert df.select(pl.col("b").list.sum()).to_dict(as_series=False) == {
+            "b": [0, 3, 6, 10]
+        }
 
     assert df.select(pl.col("a").list.sum()).dtypes == [dt_out]
 
