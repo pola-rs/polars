578b979c33ed630297fc754a0103cf9fc6c8d62e
coastalwhite <me@gburghoorn.com>
2025-11-03 16:19:59 +0100

    add example

---
+++ pyo3-polars/example/plugin-v2/plugin_v2/plugin_v2/dist.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import polars as pl
+from polars.plugins import register_plugin_function, register_plugin_v2_function
+
+from plugin_v2._utils import LIB
+
+if TYPE_CHECKING:
+    from expression_lib._typing import IntoExprColumn
+
+
+def new_expr(expr: IntoExprColumn, by: IntoExprColumn) -> pl.Expr:
+    from plugin_v2 import plugin_v2
+    return register_plugin_v2_function(
+        plugin_path=LIB,
+        args=[expr, by],
+        info=plugin_v2.min_by(),
+    )
+
+
+def hamming_distance(expr: IntoExprColumn, other: IntoExprColumn) -> pl.Expr:
+    return register_plugin_function(
+        plugin_path=LIB,
+        args=[expr, other],
+        function_name="hamming_distance",
+        is_elementwise=True,
+    )
+
+
+def jaccard_similarity(expr: IntoExprColumn, other: IntoExprColumn) -> pl.Expr:
+    return register_plugin_function(
+        plugin_path=LIB,
+        args=[expr, other],
+        function_name="jaccard_similarity",
+        is_elementwise=True,
+    )
+
+
+def haversine(
+    start_lat: IntoExprColumn,
+    start_long: IntoExprColumn,
+    end_lat: IntoExprColumn,
+    end_long: IntoExprColumn,
+) -> pl.Expr:
+    return register_plugin_function(
+        plugin_path=LIB,
+        args=[start_lat, start_long, end_lat, end_long],
+        function_name="haversine",
+        is_elementwise=True,
+        cast_to_supertype=True,
+    )
