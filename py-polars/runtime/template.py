e36d9fc977672b07af73cb94c3fa10a43db6da13
Orson Peters <orsonpeters@gmail.com>
2025-11-12 14:42:28 +0100

    refactor: Use dedicated runtime packages from template

---
+++ py-polars/runtime/template.py
@@ -0,0 +1,34 @@
+import shutil
+from pathlib import Path
+
+this_dir = Path(__file__).parent
+
+
+def template(s: str, rt: str) -> str:
+    """Apply the runtime template substitutions."""
+    polars_features = '"bigidx"' if rt != "32" else ""
+    polars_python_features = f'"rt{rt}"'
+    s = s.replace("{{%RT_SUFFIX%}}", rt)
+    s = s.replace('"POLARS_FEATURES"', polars_features)
+    s = s.replace('"POLARS_PYTHON_FEATURES"', polars_python_features)
+    return s
+
+
+if __name__ == "__main__":
+    for rt in ["32", "64", "compat"]:
+        basedir = this_dir / Path("polars-runtime-" + rt)
+        shutil.rmtree(basedir, ignore_errors=True)
+        shutil.copytree(this_dir / "template", basedir)
+        shutil.move(
+            basedir / "_polars_runtime_mod", basedir / ("_polars_runtime_" + rt)
+        )
+
+        with (basedir / "Cargo.toml").open() as f:
+            cargo_toml = f.read()
+        with (basedir / "pyproject.toml").open() as f:
+            pyproject_toml = f.read()
+
+        with (basedir / "Cargo.toml").open("w") as f:
+            f.write(template(cargo_toml, rt))
+        with (basedir / "pyproject.toml").open("w") as f:
+            f.write(template(pyproject_toml, rt))
