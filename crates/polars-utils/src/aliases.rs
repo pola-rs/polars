cece8be694ef0b4c19e8f7377cd58e910eb31030
Itamar Turner-Trauring <itamar@pythonspeed.com>
2025-11-14 13:49:38 -0500

    Cleanups.

--- crates/polars-utils/src/aliases.rs
+++ crates/polars-utils/src/aliases.rs
@@ -1,24 +1,38 @@
 use std::hash::BuildHasher;
 use std::sync::atomic::{AtomicU64, Ordering};
-use std::u64;
 
 use foldhash::SharedSeed;
 use foldhash::quality::SeedableRandomState as FHSeedableState;
 
+/// A value of ``u64::MAX`` indicates using a random hash seed, the default
+/// behavior. Other values indicate a specific seed has been set.
+static HASH_SEED: AtomicU64 = AtomicU64::new(u64::MAX);
+
+/// Set a hash seed instead of using a random one. Ideally should only be called
+/// once, at startup.
+pub fn set_hash_seed(mut seed: u64) {
+    // This is a marker value so it can't be used.
+    if seed == u64::MAX {
+        seed = u64::MAX - 1;
+    }
+    // Relaxed ordering is why this should only be called once, ideally at
+    // startup via e.g. an environment variable, before any additional threads
+    // are started.
+    HASH_SEED.store(seed, Ordering::Relaxed);
+}
+
+/// A RandomState that uses the seed set with ``set_hash_seed()``, if any,
+/// otherwise using a random seed.
 #[derive(Clone, Debug)]
 pub struct PlSeedableRandomStateQuality(FHSeedableState);
 
 impl PlSeedableRandomStateQuality {
+    #[inline]
     fn with_seed(seed: u64, shared_seed: &'static SharedSeed) -> Self {
         Self(FHSeedableState::with_seed(seed, shared_seed))
     }
 
-    /// Ideally this shouldn't be used, in order to ensure (optional)
-    /// determinism.
-    pub fn random() -> Self {
-        Self(FHSeedableState::random())
-    }
-
+    #[inline]
     pub fn fixed() -> Self {
         Self(FHSeedableState::fixed())
     }
@@ -27,6 +41,7 @@ impl PlSeedableRandomStateQuality {
 impl BuildHasher for PlSeedableRandomStateQuality {
     type Hasher = <FHSeedableState as BuildHasher>::Hasher;
 
+    #[inline]
     fn build_hasher(&self) -> Self::Hasher {
         self.0.build_hasher()
     }
@@ -34,6 +49,7 @@ impl BuildHasher for PlSeedableRandomStateQuality {
 
 impl Default for PlSeedableRandomStateQuality {
     /// If a seed is set use it, otherwise use a random seed as usual.
+    #[inline]
     fn default() -> Self {
         let seed = HASH_SEED.load(Ordering::Relaxed);
         if seed == u64::MAX {
@@ -54,22 +70,6 @@ pub type PlHashMap<K, V> = hashbrown::HashMap<K, V, PlSeedableRandomStateQuality
 pub type PlHashSet<V> = hashbrown::HashSet<V, PlSeedableRandomStateQuality>;
 pub type PlIndexMap<K, V> = indexmap::IndexMap<K, V, PlSeedableRandomStateQuality>;
 pub type PlIndexSet<K> = indexmap::IndexSet<K, PlSeedableRandomStateQuality>;
-
-/// A value of ``u64::MAX`` indicates using a random hash seed, the normal
-/// default behavior. Other values indicate a specific seed.
-static HASH_SEED: AtomicU64 = AtomicU64::new(u64::MAX);
-
-/// Set a hash seed instead of using a random one. Ideally should only be called
-/// once, at startup.
-pub fn set_hash_seed(mut seed: u64) {
-    if seed == u64::MAX {
-        seed = u64::MAX - 1;
-    }
-    // Relaxed ordering is why this should only be called once, ideally at
-    // startup via e.g. an environment variable, before any additional threads
-    // are started.
-    HASH_SEED.store(seed, Ordering::Relaxed);
-}
 
 pub trait SeedableFromU64SeedExt {
     fn seed_from_u64(seed: u64) -> Self;
