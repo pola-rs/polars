60fc7b34a9e7a72ab76ed7fc47b8ead9b0bae99e
Simon Lin <simonlin.rqmmw@slmail.me>
2025-12-04 21:28:18 +1100

    c

--- crates/polars-stream/src/nodes/io_sinks2/writers/mod.rs
+++ crates/polars-stream/src/nodes/io_sinks2/writers/mod.rs
@@ -8,13 +8,37 @@ use polars_plan::dsl::FileType;
 use crate::nodes::io_sinks2::writers::interface::FileWriterStarter;
 
 pub mod interface;
+#[cfg(feature = "parquet")]
+pub mod parquet;
 
-#[expect(unused)]
 pub fn create_file_writer_starter(
     file_format: &Arc<FileType>,
     file_schema: &SchemaRef,
     pipeline_depth: usize,
     sync_on_close: SyncOnCloseType,
 ) -> PolarsResult<Arc<dyn FileWriterStarter>> {
-    unimplemented!()
+    Ok(match file_format.as_ref() {
+        #[cfg(feature = "parquet")]
+        FileType::Parquet(options) => {
+            use polars_core::prelude::CompatLevel;
+            use polars_io::schema_to_arrow_checked;
+
+            use crate::nodes::io_sinks2::writers::parquet::ParquetWriterStarter;
+
+            let arrow_schema = Arc::new(schema_to_arrow_checked(
+                file_schema.as_ref(),
+                CompatLevel::newest(),
+                "",
+            )?);
+
+            Arc::new(ParquetWriterStarter {
+                options: options.clone(),
+                arrow_schema,
+                initialized_state: Default::default(),
+                pipeline_depth,
+                sync_on_close,
+            }) as _
+        },
+        _ => unimplemented!(),
+    })
 }
