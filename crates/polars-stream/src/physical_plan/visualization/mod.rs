ee328c669ed2c480d63c90949d1208ae27d6a3b6
Simon Lin <simonlin.rqmmw@slmail.me>
2025-12-09 22:34:14 +1100

    clippy

--- crates/polars-stream/src/physical_plan/visualization/mod.rs
+++ crates/polars-stream/src/physical_plan/visualization/mod.rs
@@ -572,7 +572,7 @@ impl PhysicalPlanVisualizationDataGenerator<'_> {
                     predicate: predicate
                         .as_ref()
                         .map(|e| format_pl_smallstr!("{}", e.display(self.expr_arena))),
-                    predicate_file_skip_applied: predicate_file_skip_applied.clone(),
+                    predicate_file_skip_applied: *predicate_file_skip_applied,
                     has_table_statistics: table_statistics.is_some(),
                     include_file_paths: include_file_paths.clone(),
                     deletion_files_type: deletion_files
