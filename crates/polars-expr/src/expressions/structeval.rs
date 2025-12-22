ba53de3ad7a3b5b74eebe80fa1ee20fce090b521
Koen Denecker <koen@polars.tech>
2025-12-22 09:08:01 +0100

    evaluate evaluations in parallel

--- crates/polars-expr/src/expressions/structeval.rs
+++ crates/polars-expr/src/expressions/structeval.rs
@@ -1,10 +1,12 @@
 use std::sync::Arc;
 
+use polars_core::POOL;
 use polars_core::error::{PolarsResult, polars_ensure};
 use polars_core::frame::DataFrame;
 use polars_core::prelude::*;
 use polars_core::schema::Schema;
 use polars_plan::dsl::Expr;
+use rayon::prelude::*;
 
 use super::PhysicalExpr;
 #[cfg(feature = "dtype-struct")]
@@ -18,6 +20,7 @@ pub struct StructEvalExpr {
     evaluation: Vec<Arc<dyn PhysicalExpr>>,
     expr: Expr,
     output_field: Field,
+    allow_threading: bool,
 }
 
 impl StructEvalExpr {
@@ -26,12 +29,14 @@ impl StructEvalExpr {
         evaluation: Vec<Arc<dyn PhysicalExpr>>,
         expr: Expr,
         output_field: Field,
+        allow_threading: bool,
     ) -> Self {
         Self {
             input,
             evaluation,
             expr,
             output_field,
+            allow_threading,
         }
     }
 }
@@ -51,14 +56,32 @@ impl PhysicalExpr for StructEvalExpr {
 
         state.with_fields = Arc::new(Some(input.struct_()?.clone()));
 
-        // Collect evaluation fields.
+        // Collect evaluation fields; input goes first.
         eval.push(input);
-        for e in self.evaluation.iter() {
+
+        let f = |e: &Arc<dyn PhysicalExpr>| {
             let result = e.evaluate(df, &state)?;
-            polars_ensure!(result.len() == input_len || result.len() == 1,
+            polars_ensure!(
+                result.len() == input_len || result.len() == 1,
                 ShapeMismatch: "struct.with_fields expressions must have matching or unit length"
             );
-            eval.push(result);
+            Ok(result)
+        };
+        let cols = if self.allow_threading {
+            POOL.install(|| {
+                self.evaluation
+                    .par_iter()
+                    .map(f)
+                    .collect::<PolarsResult<Vec<_>>>()
+            })
+        } else {
+            self.evaluation
+                .iter()
+                .map(f)
+                .collect::<PolarsResult<Vec<_>>>()
+        };
+        for col in cols? {
+            eval.push(col);
         }
 
         // Apply with_fields.
@@ -82,13 +105,27 @@ impl PhysicalExpr for StructEvalExpr {
         // Collect evaluation fields.
         let mut acs = Vec::with_capacity(self.evaluation.len() + 1);
         acs.push(ac);
-        for e in self.evaluation.iter() {
-            let ac = e.evaluate_on_groups(df, groups, &state)?;
-            acs.push(ac);
+
+        let f = |e: &Arc<dyn PhysicalExpr>| e.evaluate_on_groups(df, groups, &state);
+        let acs_eval = if self.allow_threading {
+            POOL.install(|| {
+                self.evaluation
+                    .par_iter()
+                    .map(f)
+                    .collect::<PolarsResult<Vec<_>>>()
+            })
+        } else {
+            self.evaluation
+                .iter()
+                .map(f)
+                .collect::<PolarsResult<Vec<_>>>()
+        };
+        for ac in acs_eval? {
+            acs.push(ac)
         }
 
         // Apply with_fields. We default to group_aware as groups may have diverged.
-        // @TODO. Performance can be optimized: elementwise, re-use PlIndexMap, etc.
+        // @TODO. Performance can be optimized: elementwise, re-use PlIndexMap, parallel evaluation, etc.
         let mut iters = acs
             .iter_mut()
             .map(|ac| ac.iter_groups(true))
