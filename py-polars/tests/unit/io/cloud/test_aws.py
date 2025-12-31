caccd2900fefce479934ef9052d151e366a58a76
Paul <53956863+hutch3232@users.noreply.github.com>
2025-12-31 10:48:03 -0500

    Add CSV scanning to AWS test parameters

--- py-polars/tests/unit/io/cloud/test_aws.py
+++ py-polars/tests/unit/io/cloud/test_aws.py
@@ -84,7 +84,11 @@ def test_read_s3(s3: str, function: Callable[..., Any], extension: str) -> None:
 
 @pytest.mark.parametrize(
     ("function", "extension"),
-    [(pl.scan_ipc, "ipc"), (pl.scan_parquet, "parquet")],
+    [
+        (pl.scan_csv, "csv"),
+        (pl.scan_ipc, "ipc"),
+        (pl.scan_parquet, "parquet"),
+    ],
 )
 def test_scan_s3(s3: str, function: Callable[..., Any], extension: str) -> None:
     lf = function(
