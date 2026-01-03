6dd0ef165116cd1285ec6fce2d2e780aa1449697
Paul <53956863+hutch3232@users.noreply.github.com>
2026-01-02 21:00:51 -0500

    Update S3 fixture to include endpoint URL

--- py-polars/tests/unit/io/cloud/test_aws.py
+++ py-polars/tests/unit/io/cloud/test_aws.py
@@ -28,16 +28,18 @@ def monkeypatch_module() -> Any:
 
 @pytest.fixture(scope="module")
 def s3_base(monkeypatch_module: Any) -> Iterator[str]:
+    host = "127.0.0.1"
+    port = 5000
+    endpoint_uri = f"http://{host}:{port}"
     monkeypatch_module.setenv("AWS_ACCESS_KEY_ID", "accesskey")
     monkeypatch_module.setenv("AWS_SECRET_ACCESS_KEY", "secretkey")
     monkeypatch_module.setenv("AWS_DEFAULT_REGION", "us-east-1")
-
-    host = "127.0.0.1"
-    port = 5000
+    monkeypatch_module.setenv("AWS_ENDPOINT_URL", endpoint_uri)
+    
     moto_server = ThreadedMotoServer(host, port)
     moto_server.start()
     print("server up")
-    yield f"http://{host}:{port}"
+    yield endpoint_uri
     print("moto done")
     moto_server.stop()
 
@@ -65,6 +67,7 @@ def s3(s3_base: str, io_files_path: Path) -> str:
     [
         (pl.read_csv, "csv"),
         (pl.read_ipc, "ipc"),
+        (pl.read_parquet, "parquet"),
     ],
 )
 def test_read_s3(s3: str, function: Callable[..., Any], extension: str) -> None:
