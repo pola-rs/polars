454d34ac523f10e2400b61f71bc0989fd9d85ce9
Amber Sprenkels <amber@electricdusk.com>
2026-01-08 16:44:12 +0100

    Fix dealing with blocked channels

--- crates/polars-stream/src/nodes/joins/merge_join.rs
+++ crates/polars-stream/src/nodes/joins/merge_join.rs
@@ -167,9 +167,6 @@ impl ComputeNode for MergeJoinNode {
         assert!(send.len() == 1);
 
         let prev_state = self.state;
-        let any_input_channel_blocked =
-            recv[0] == PortState::Blocked || recv[1] == PortState::Blocked;
-        let output_channel_blocked = send[0] == PortState::Blocked;
         let input_channels_done = recv[0] == PortState::Done && recv[1] == PortState::Done;
         let output_channel_done = send[0] == PortState::Done;
         let input_buffers_empty = self.left_unmerged.is_empty() && self.right_unmerged.is_empty();
@@ -192,17 +189,20 @@ impl ComputeNode for MergeJoinNode {
 
         match self.state {
             MergeJoinState::Running => {
-                if output_channel_blocked {
+                recv[0] = PortState::Ready;
+                recv[1] = PortState::Ready;
+                send[0] = PortState::Ready;
+                if recv[0] == PortState::Blocked {
+                    recv[1] = PortState::Blocked;
+                    send[0] = PortState::Blocked;
+                }
+                if recv[1] == PortState::Blocked {
+                    recv[0] = PortState::Blocked;
+                    send[0] = PortState::Blocked;
+                }
+                if send[0] == PortState::Blocked {
                     recv[0] = PortState::Blocked;
                     recv[1] = PortState::Blocked;
-                } else {
-                    recv[0] = PortState::Ready;
-                    recv[1] = PortState::Ready;
-                }
-                if any_input_channel_blocked {
-                    send[0] = PortState::Blocked;
-                } else {
-                    send[0] = PortState::Ready;
                 }
             },
             MergeJoinState::FlushInputBuffers | MergeJoinState::EmitUnmatched => {
