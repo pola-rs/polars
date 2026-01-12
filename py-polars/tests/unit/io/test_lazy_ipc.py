707796731ab7cf51f296a9b56a0231a5537e4259
Koen Denecker <koen@polars.tech>
2026-01-12 15:00:50 +0100

    fix: Optimize slicing support on compressed IPC (#26071)

--- py-polars/tests/unit/io/test_lazy_ipc.py
+++ py-polars/tests/unit/io/test_lazy_ipc.py
@@ -329,12 +329,21 @@ def test_sink_ipc_record_batch_size(record_batch_size: int, n_chunks: int) -> No
         )
 
 
+@pytest.mark.parametrize("record_batch_size", [None, 3])
+@pytest.mark.parametrize("slice", [(0, 0), (0, 1), (0, 5), (4, 7), (-1, 1), (-5, 4)])
 @pytest.mark.parametrize("compression", COMPRESSIONS)
-def test_scan_ipc_compression_with_slice_26063(compression: IpcCompression) -> None:
-    df = pl.DataFrame({"a": [1, 2, 3]})
+def test_scan_ipc_compression_with_slice_26063(
+    record_batch_size: int, slice: tuple[int, int], compression: IpcCompression
+) -> None:
+    n_rows = 15
+    df = pl.DataFrame({"a": range(n_rows)}).with_columns(
+        pl.col.a.pow(3).cast(pl.String).alias("b")
+    )
     buf = io.BytesIO()
 
-    df.lazy().sink_ipc(buf, compression=compression)
-    out = pl.scan_ipc(buf).slice(0, 1).collect()
-    expected = df.slice(0, 1)
+    df.lazy().sink_ipc(
+        buf, compression=compression, record_batch_size=record_batch_size
+    )
+    out = pl.scan_ipc(buf).slice(slice[0], slice[1]).collect()
+    expected = df.slice(slice[0], slice[1])
     assert_frame_equal(out, expected)
