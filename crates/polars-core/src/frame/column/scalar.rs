ba6eb317053189b5b5002d29d6fe5d00cece6f58
Amber Sprenkels <amber@electricdusk.com>
2026-01-15 15:29:36 +0100

    snapshot

--- crates/polars-core/src/frame/column/scalar.rs
+++ crates/polars-core/src/frame/column/scalar.rs
@@ -25,6 +25,13 @@ pub struct ScalarColumn {
     materialized: OnceLock<Series>,
 }
 
+impl IntoColumn for &ScalarColumn {
+    #[inline(always)]
+    fn into_column(self) -> Column {
+        self.clone().into()
+    }
+}
+
 impl ScalarColumn {
     #[inline]
     pub fn new(name: PlSmallStr, scalar: Scalar, length: usize) -> Self {
