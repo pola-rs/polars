c4578aebd789314baf6fde27761abb95f9218863
azimafroozeh <afroozeh3@gmail.com>
2026-01-16 17:00:22 +0100

    fix

--- crates/polars-core/src/frame/group_by/aggregations/mod.rs
+++ crates/polars-core/src/frame/group_by/aggregations/mod.rs
@@ -437,11 +437,18 @@ where
 {
     pub(crate) unsafe fn agg_min(&self, groups: &GroupsType) -> Series {
         // faster paths
-        match self.is_sorted_flag() {
-            IsSorted::Ascending => return self.clone().into_series().agg_first_non_null(groups),
-            IsSorted::Descending => return self.clone().into_series().agg_last_non_null(groups),
-            _ => {},
+        if groups.is_sorted_flag() {
+            match self.is_sorted_flag() {
+                IsSorted::Ascending => {
+                    return self.clone().into_series().agg_first_non_null(groups);
+                },
+                IsSorted::Descending => {
+                    return self.clone().into_series().agg_last_non_null(groups);
+                },
+                _ => {},
+            }
         }
+
         match groups {
             GroupsType::Idx(groups) => {
                 let ca = self.rechunk();
@@ -505,14 +512,16 @@ where
 
     pub(crate) unsafe fn agg_max(&self, groups: &GroupsType) -> Series {
         // faster paths
-        match (self.is_sorted_flag(), self.null_count()) {
-            (IsSorted::Ascending, 0) => {
-                return self.clone().into_series().agg_last(groups);
-            },
-            (IsSorted::Descending, 0) => {
-                return self.clone().into_series().agg_first(groups);
-            },
-            _ => {},
+        if groups.is_sorted_flag() {
+            match (self.is_sorted_flag(), self.null_count()) {
+                (IsSorted::Ascending, 0) => {
+                    return self.clone().into_series().agg_last(groups);
+                },
+                (IsSorted::Descending, 0) => {
+                    return self.clone().into_series().agg_first(groups);
+                },
+                _ => {},
+            }
         }
 
         match groups {
