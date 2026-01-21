bdca4c5c6ab54812f2a2f21b280a0828e1c03816
Kevin-Patyk <kvn.ptk@gmail.com>
2026-01-21 14:15:16 +0100

    chore: Organize operations tests

--- py-polars/tests/unit/operations/hashing/test_hash.py
+++ py-polars/tests/unit/operations/hashing/test_hash.py
@@ -1,6 +1,16 @@
 import polars as pl
 
 
+def test_hash_struct() -> None:
+    df = pl.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+    df = df.select(pl.struct(pl.all()))
+    assert df.select(pl.col("a").hash())["a"].to_list() == [
+        5535262844797696299,
+        15139341575481673729,
+        12593759486533989774,
+    ]
+
+
 def test_0_width_df() -> None:
     df = pl.DataFrame(height=5)
 
