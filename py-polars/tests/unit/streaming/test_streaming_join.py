2afa0e4d9b741de48c69922fc489bbb8c8f2525a
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-01-28 02:58:04 +0530

    Drop a lot of the old changes

--- py-polars/tests/unit/streaming/test_streaming_join.py
+++ py-polars/tests/unit/streaming/test_streaming_join.py
@@ -17,7 +17,7 @@ if TYPE_CHECKING:
 
     from polars._typing import JoinStrategy, MaintainOrderJoin
 
-# pytestmark = pytest.mark.xdist_group("streaming")
+pytestmark = pytest.mark.xdist_group("streaming")
 
 
 def test_streaming_full_outer_joins() -> None:
