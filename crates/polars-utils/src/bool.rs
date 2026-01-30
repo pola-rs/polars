f4922f85bf067f3cb7e10c3bf685dc64d422cad6
ritchie46 <ritchie46@gmail.com>
2026-01-30 16:20:13 +0100

    features

--- crates/polars-utils/src/bool.rs
+++ crates/polars-utils/src/bool.rs
@@ -1,8 +1,6 @@
-use serde::{Deserialize, Serialize};
-
-// a boolean that can only be set to `true` safely
+// a boolean flag that can only be set to `true` safely
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "dsl-schema", derive(schemars::JsonSchema))]
 pub struct UnsafeBool(pub bool);
 impl Default for UnsafeBool {
