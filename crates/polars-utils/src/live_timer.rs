d4d9df8966bddf308c64563a1758e8c1183cab9a
Simon Lin <simonlin.rqmmw@slmail.me>
2026-02-01 16:30:05 +1100

    c

--- crates/polars-utils/src/live_timer.rs
+++ crates/polars-utils/src/live_timer.rs
@@ -125,7 +125,7 @@ fn main() {
 
     let mut recorded = [0u64; 16];
 
-    'outer: while !h1.is_finished() {
+    'outer: while timer.base_instant.elapsed().as_millis() < 4000 {
         let mut prev = timer.total_time_live_ns();
         for i in recorded.iter_mut() {
             let new = timer.total_time_live_ns();
@@ -140,10 +140,9 @@ fn main() {
     }
 
     dbg!(&recorded);
+    dbg!(recorded.is_sorted());
 
     dbg!(h1.join().unwrap());
-
-    dbg!(&recorded);
 
     dbg!(timer.total_time_live_ns());
 }
