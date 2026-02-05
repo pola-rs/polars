120cedf3d8f9a3e20fc7c63e2594256fa1c93077
Kevin-Patyk <kvn.ptk@gmail.com>
2026-02-05 16:30:44 +0100

    fix: Error with strict replacement

--- crates/polars-ops/src/series/ops/replace.rs
+++ crates/polars-ops/src/series/ops/replace.rs
@@ -166,7 +166,16 @@ pub fn replace_strict(
     }
     validate_old(&old)?;
 
+    if !matches!(old.dtype().can_cast_to(s.dtype()), Some(true)) {
+        polars_bail!(
+            InvalidOperation: "cannot use values of type `{}` to replace values in a column of type `{}`",
+            old.dtype(),
+            s.dtype()
+        )
+    }
+
     let old = old.strict_cast(s.dtype())?;
+
     let new = match return_dtype {
         Some(dtype) => new.strict_cast(&dtype)?,
         None => new,
