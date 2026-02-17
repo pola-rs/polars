f526726ab80496d675382fe330e38099afee8206
Kevin-Patyk <kvn.ptk@gmail.com>
2026-02-17 11:15:53 +0100

    update: Fix clippy issue

--- crates/polars-testing/src/asserts/utils.rs
+++ crates/polars-testing/src/asserts/utils.rs
@@ -767,19 +767,17 @@ fn assert_dataframe_schema_equal(
                     format!("{:?}", right_dtypes_ordered)
                 ));
             }
-        } else {
-            if left_schema.len() != right_schema.len()
-                || left_schema
-                    .iter()
-                    .any(|(name, dtype)| right_schema.get(name) != Some(dtype))
-            {
-                return Err(polars_err!(
-                    assertion_error = "DataFrames",
-                    "dtypes do not match",
-                    format!("{:?}", left_schema),
-                    format!("{:?}", right_schema)
-                ));
-            }
+        } else if left_schema.len() != right_schema.len()
+            || left_schema
+                .iter()
+                .any(|(name, dtype)| right_schema.get(name) != Some(dtype))
+        {
+            return Err(polars_err!(
+                assertion_error = "DataFrames",
+                "dtypes do not match",
+                format!("{:?}", left_schema),
+                format!("{:?}", right_schema)
+            ));
         }
     }
 
