fdbee73286ce7242ae4d73393caef32d154fb577
Kevin-Patyk <kvn.ptk@gmail.com>
2026-02-20 19:10:40 +0100

    chore: Readd df proptest strat

--- crates/polars-core/src/frame/proptest.rs
+++ crates/polars-core/src/frame/proptest.rs
@@ -1,6 +1,10 @@
 use std::ops::RangeInclusive;
+use std::rc::Rc;
 
-use crate::series::proptest::SeriesArbitraryOptions;
+use proptest::prelude::*;
+
+use crate::prelude::{Column, DataFrame};
+use crate::series::proptest::{SeriesArbitraryOptions, series_strategy};
 
 pub struct DataFrameArbitraryOptions {
     pub series_options: SeriesArbitraryOptions,
@@ -14,4 +18,28 @@ impl Default for DataFrameArbitraryOptions {
             num_columns: 0..=5,
         }
     }
+}
+
+pub fn dataframe_strategy(
+    options: Rc<DataFrameArbitraryOptions>,
+    nesting_level: usize,
+) -> impl Strategy<Value = DataFrame> {
+    options
+        .series_options
+        .series_length_range
+        .clone()
+        .prop_flat_map(move |series_length| {
+            let mut opts = options.series_options.clone();
+            opts.series_length_range = series_length..=series_length;
+
+            prop::collection::vec(
+                series_strategy(Rc::new(opts), nesting_level),
+                options.num_columns.clone(),
+            )
+        })
+        .prop_map(|series| {
+            let columns: Vec<Column> = series.into_iter().map(Column::from).collect();
+            let height = columns.first().map_or(0, |c| c.len());
+            DataFrame::new(height, columns).unwrap()
+        })
 }
