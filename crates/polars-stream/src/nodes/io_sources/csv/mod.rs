6ba6d1c6b6e028b5aaf96de856c4cf9ace4c21c1
Koen Denecker <koen@polars.tech>
2026-02-20 10:13:03 +0100

    reorg files

--- crates/polars-stream/src/nodes/io_sources/csv/mod.rs
+++ crates/polars-stream/src/nodes/io_sources/csv/mod.rs
@@ -1,31 +1,27 @@
 pub mod builder;
+mod chunk_reader;
+mod line_batch_source;
 
 use std::iter::Iterator;
 use std::num::NonZeroUsize;
-use std::ops::Range;
 use std::sync::Arc;
 
 use async_trait::async_trait;
-use polars_buffer::Buffer;
-use polars_core::prelude::Field;
-use polars_core::schema::{SchemaExt, SchemaRef};
-use polars_error::{PolarsResult, polars_bail, polars_err, polars_warn};
+use chunk_reader::ChunkReader;
+use line_batch_source::{LineBatch, LineBatchSource};
+use polars_error::{PolarsResult, polars_err};
 use polars_io::cloud::CloudOptions;
 use polars_io::metrics::OptIOMetrics;
 use polars_io::pl_async;
-use polars_io::prelude::_csv_read_internal::{
-    CountLines, NullValuesCompiled, cast_columns, prepare_csv_schema, read_chunk,
-};
-use polars_io::prelude::builder::validate_utf8;
+use polars_io::prelude::_csv_read_internal::CountLines;
+use polars_io::prelude::CsvReadOptions;
 use polars_io::prelude::streaming::read_until_start_and_infer_schema;
-use polars_io::prelude::{CsvEncoding, CsvParseOptions, CsvReadOptions};
 use polars_io::utils::byte_source::{ByteSource, DynByteSource, DynByteSourceBuilder};
 use polars_io::utils::compression::{ByteSourceReader, SupportedCompression};
-use polars_io::utils::slice::SplitSlicePosition;
 use polars_io::utils::stream_buf_reader::{ReaderSource, StreamBufReader};
 use polars_plan::dsl::ScanSource;
 use polars_utils::IdxSize;
-use polars_utils::mem::prefetch::{get_memory_prefetch_func, prefetch_l2};
+use polars_utils::mem::prefetch::get_memory_prefetch_func;
 use polars_utils::slice_enum::Slice;
 
 use super::multi_scan::reader_interface::output::FileReaderOutputRecv;
@@ -33,13 +29,13 @@ use super::multi_scan::reader_interface::{BeginReadArgs, FileReader, FileReaderC
 use super::ndjson::chunk_data_fetch::ChunkDataFetcher;
 use crate::DEFAULT_DISTRIBUTOR_BUFFER_SIZE;
 use crate::async_executor::{AbortOnDropHandle, spawn};
-use crate::async_primitives::distributor_channel::{self, distributor_channel};
+use crate::async_primitives::distributor_channel::distributor_channel;
 use crate::async_primitives::wait_group::{WaitGroup, WaitToken};
 use crate::morsel::SourceToken;
+use crate::nodes::TaskPriority;
 use crate::nodes::compute_node_prelude::*;
 use crate::nodes::io_sources::multi_scan::reader_interface::Projection;
 use crate::nodes::io_sources::multi_scan::reader_interface::output::FileReaderOutputSend;
-use crate::nodes::{MorselSeq, TaskPriority};
 use crate::utils::tokio_handle_ext;
 
 /// Read all rows in the chunk
@@ -47,16 +43,6 @@ const NO_SLICE: (usize, usize) = (0, usize::MAX);
 /// This is used if we finish the slice but still need a row count. It signals to the workers to
 /// go into line-counting mode where they can skip parsing the chunks.
 const SLICE_ENDED: (usize, usize) = (usize::MAX, 0);
-
-struct LineBatch {
-    // Safety: All receivers (LineBatchProcessors) hold a Buffer ref to this.
-    mem_slice: Buffer<u8>,
-    n_lines: usize,
-    slice: (usize, usize),
-    /// Position of this chunk relative to the start of the file according to CountLines.
-    row_offset: usize,
-    morsel_seq: MorselSeq,
-}
 
 struct CsvFileReader {
     scan_source: ScanSource,
@@ -294,14 +280,13 @@ impl FileReader for CsvFileReader {
             ByteSourceReader::from_memory(memslice, compression)?
         };
 
-        let (inferred_schema, base_leftover) =
-            read_until_start_and_infer_schema(
-                &self.options,
-                Some(projected_schema.clone()),
-                decompressed_file_size_hint,
-                None,
-                &mut reader,
-            )?;
+        let (inferred_schema, base_leftover) = read_until_start_and_infer_schema(
+            &self.options,
+            Some(projected_schema.clone()),
+            decompressed_file_size_hint,
+            None,
+            &mut reader,
+        )?;
 
         let used_schema = Arc::new(inferred_schema);
 
@@ -465,236 +450,5 @@ impl FileReader for CsvFileReader {
                 Ok(())
             }),
         ))
-    }
-}
-
-struct LineBatchSource {
-    base_leftover: Buffer<u8>,
-    reader: ByteSourceReader<ReaderSource>,
-    line_counter: CountLines,
-    line_batch_tx: distributor_channel::Sender<LineBatch>,
-    pre_slice: Option<Slice>,
-    needs_full_row_count: bool,
-    verbose: bool,
-}
-
-impl LineBatchSource {
-    /// Returns the number of rows skipped from the start of the file according to CountLines.
-    async fn run(self) -> PolarsResult<usize> {
-        dbg!("start LineBatchSource::run"); //kdn
-        let LineBatchSource {
-            base_leftover,
-            mut reader,
-            line_counter,
-            mut line_batch_tx,
-            pre_slice,
-            needs_full_row_count,
-            verbose,
-        } = self;
-
-        let global_slice = if let Some(pre_slice) = pre_slice {
-            match pre_slice {
-                Slice::Positive { .. } => Some(Range::<usize>::from(pre_slice)),
-                // IR lowering puts negative slice in separate node.
-                // TODO: Native line buffering for negative slice
-                Slice::Negative { .. } => unreachable!(),
-            }
-        } else {
-            None
-        };
-
-        if verbose {
-            eprintln!("[CsvSource]: Start line splitting",);
-        }
-
-        let mut prev_leftover = base_leftover;
-        let mut row_offset = 0usize;
-        let mut morsel_seq = MorselSeq::default();
-        let mut n_rows_skipped: usize = 0;
-        let mut read_size = ByteSourceReader::<ReaderSource>::initial_read_size();
-
-        loop {
-            let (mem_slice, bytes_read) =
-                reader.read_next_slice(&prev_leftover, read_size, Some(read_size))?;
-            if mem_slice.is_empty() {
-                break;
-            }
-
-            prefetch_l2(&mem_slice);
-
-            let is_eof = bytes_read == 0;
-            let (n_lines, unconsumed_offset) = line_counter.count_rows(&mem_slice, is_eof);
-
-            let batch_slice = mem_slice.clone().sliced(0..unconsumed_offset);
-            prev_leftover = mem_slice.sliced(unconsumed_offset..);
-
-            if batch_slice.is_empty() && !is_eof {
-                // This allows the slice to grow until at least a single row is included. To avoid a quadratic run-time for large row sizes, we double the read size.
-                read_size = read_size.saturating_mul(2);
-                continue;
-            }
-
-            // Has to happen here before slicing, since there are slice operations that skip morsel
-            // sending.
-            let prev_row_offset = row_offset;
-            row_offset += n_lines;
-
-            let slice = if let Some(global_slice) = &global_slice {
-                match SplitSlicePosition::split_slice_at_file(
-                    prev_row_offset,
-                    n_lines,
-                    global_slice.clone(),
-                ) {
-                    // Note that we don't check that the skipped line batches actually contain this many
-                    // lines.
-                    SplitSlicePosition::Before => {
-                        n_rows_skipped = n_rows_skipped.saturating_add(n_lines);
-                        continue;
-                    },
-                    SplitSlicePosition::Overlapping(offset, len) => (offset, len),
-                    SplitSlicePosition::After => {
-                        if needs_full_row_count {
-                            // If we need to know the unrestricted row count, we need
-                            // to go until the end.
-                            SLICE_ENDED
-                        } else {
-                            break;
-                        }
-                    },
-                }
-            } else {
-                NO_SLICE
-            };
-
-            morsel_seq = morsel_seq.successor();
-
-            let batch = LineBatch {
-                mem_slice: batch_slice,
-                n_lines,
-                slice,
-                row_offset,
-                morsel_seq,
-            };
-
-            if line_batch_tx.send(batch).await.is_err() {
-                break;
-            }
-
-            if is_eof {
-                break;
-            }
-
-            if read_size < ByteSourceReader::<ReaderSource>::ideal_read_size() {
-                read_size *= 4;
-            }
-        }
-
-        Ok(n_rows_skipped)
-    }
-}
-
-#[derive(Default)]
-struct ChunkReader {
-    reader_schema: SchemaRef,
-    parse_options: Arc<CsvParseOptions>,
-    fields_to_cast: Vec<Field>,
-    ignore_errors: bool,
-    projection: Vec<usize>,
-    null_values: Option<NullValuesCompiled>,
-    validate_utf8: bool,
-}
-
-impl ChunkReader {
-    fn try_new(
-        options: Arc<CsvReadOptions>,
-        mut reader_schema: SchemaRef,
-        projection: Vec<usize>,
-    ) -> PolarsResult<Self> {
-        let mut fields_to_cast: Vec<Field> = options.fields_to_cast.clone();
-        prepare_csv_schema(&mut reader_schema, &mut fields_to_cast)?;
-
-        let parse_options = options.parse_options.clone();
-
-        // Logic from `CoreReader::new()`
-
-        let null_values = parse_options
-            .null_values
-            .clone()
-            .map(|nv| nv.compile(&reader_schema))
-            .transpose()?;
-
-        let validate_utf8 = matches!(parse_options.encoding, CsvEncoding::Utf8)
-            && reader_schema.iter_fields().any(|f| f.dtype().is_string());
-
-        Ok(Self {
-            reader_schema,
-            parse_options,
-            fields_to_cast,
-            ignore_errors: options.ignore_errors,
-            projection,
-            null_values,
-            validate_utf8,
-        })
-    }
-
-    /// The 2nd return value indicates how many rows exist in the chunk.
-    fn read_chunk(
-        &self,
-        chunk: &[u8],
-        // Number of lines according to CountLines
-        n_lines: usize,
-        slice: (usize, usize),
-        chunk_row_offset: usize,
-    ) -> PolarsResult<(DataFrame, usize)> {
-        if self.validate_utf8 && !validate_utf8(chunk) {
-            polars_bail!(ComputeError: "invalid utf-8 sequence")
-        }
-
-        // If projection is empty create a DataFrame with the correct height by counting the lines.
-        let mut df = if self.projection.is_empty() {
-            DataFrame::empty_with_height(n_lines)
-        } else {
-            read_chunk(
-                chunk,
-                &self.parse_options,
-                &self.reader_schema,
-                self.ignore_errors,
-                &self.projection,
-                0,       // bytes_offset_thread
-                n_lines, // capacity
-                self.null_values.as_ref(),
-                usize::MAX,  // chunk_size
-                chunk.len(), // stop_at_nbytes
-                Some(0),     // starting_point_offset
-            )?
-        };
-
-        let height = df.height();
-
-        if height != n_lines {
-            // Note: in case data is malformed, height is more likely to be correct than n_lines.
-            let msg = format!(
-                "CSV malformed: expected {} rows, actual {} rows, in chunk starting at row_offset {}, length {}",
-                n_lines,
-                height,
-                chunk_row_offset,
-                chunk.len()
-            );
-            if self.ignore_errors {
-                polars_warn!("{msg}");
-            } else {
-                polars_bail!(ComputeError: msg)
-            }
-        }
-
-        if slice != NO_SLICE {
-            assert!(slice != SLICE_ENDED);
-
-            df = df.slice(i64::try_from(slice.0).unwrap(), slice.1);
-        }
-
-        cast_columns(&mut df, &self.fields_to_cast, false, self.ignore_errors)?;
-
-        Ok((df, height))
     }
 }
