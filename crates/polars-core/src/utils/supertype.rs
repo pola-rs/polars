4463128e1afd586a666b432d60f34142e1ee7de6
Kevin-Patyk <kvn.ptk@gmail.com>
2026-03-10 16:06:50 +0100

    update: Add rust code

--- crates/polars-core/src/utils/supertype.rs
+++ crates/polars-core/src/utils/supertype.rs
@@ -486,6 +486,10 @@ pub fn get_supertype_with_options(
                             }
                         }
                     }
+                    UnknownKind::Int(v) if dt.is_bool() => {
+                        let smallest_fitting_dtype = materialize_smallest_dyn_int(*v).dtype();
+                        get_supertype(dt, &smallest_fitting_dtype)
+                    },
                     #[cfg(feature = "dtype-decimal")]
                     UnknownKind::Int(_) if dt.is_decimal() => {
                         let DataType::Decimal(_prec, scale) = dt else { unreachable!() };
