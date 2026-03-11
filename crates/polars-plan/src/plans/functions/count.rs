e9d3fa16585d356dfef97a91b67410bd120c7fa8
moktamd <109174491+moktamd@users.noreply.github.com>
2026-03-11 18:58:38 +0900

    fix: Fix panic on CSV count_rows with FORCE_ASYNC (#26883)
    Co-authored-by: moktamd <moktamd@users.noreply.github.com>

--- crates/polars-plan/src/plans/functions/count.rs
+++ crates/polars-plan/src/plans/functions/count.rs
@@ -1,14 +1,17 @@
+use polars_io::cloud::CloudOptions;
+
 use super::*;
 
 pub fn count_rows(
     sources: &ScanSources,
     scan_type: &FileScanIR,
     alias: Option<PlSmallStr>,
+    cloud_options: Option<&CloudOptions>,
 ) -> PolarsResult<DataFrame> {
     feature_gated!("csv", {
         let count: PolarsResult<usize> = match scan_type {
             #[cfg(feature = "csv")]
-            FileScanIR::Csv { options } => count_all_rows_csv(sources, options),
+            FileScanIR::Csv { options } => count_all_rows_csv(sources, options, cloud_options),
             _ => unreachable!(),
         };
         let count = count?;
@@ -25,7 +28,27 @@ pub fn count_rows(
 fn count_all_rows_csv(
     sources: &ScanSources,
     options: &polars_io::prelude::CsvReadOptions,
+    cloud_options: Option<&CloudOptions>,
 ) -> PolarsResult<usize> {
+    let run_async =
+        sources.is_cloud_url() || (sources.is_paths() && polars_config::config().force_async());
+
+    if run_async {
+        if sources.as_paths().is_some() {
+            let sources_clone = sources.clone();
+            feature_gated!("cloud", {
+                polars_io::pl_async::get_runtime().block_in_place_on(
+                    polars_io::file_cache::init_entries_from_uri_list(
+                        (0..sources_clone.len()).map(move |i| {
+                            sources_clone.as_paths().unwrap().get(i).unwrap().clone()
+                        }),
+                        cloud_options,
+                    ),
+                )?;
+            })
+        }
+    }
+
     let parse_options = options.get_parse_options();
 
     sources
