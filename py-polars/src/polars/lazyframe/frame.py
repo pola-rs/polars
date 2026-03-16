e2a3c15bef8ca0416a47a29d46bad9abf7312cae
Simon Lin <simonlin.rqmmw@slmail.me>
2026-03-17 00:10:22 +1100

    c

--- py-polars/src/polars/lazyframe/frame.py
+++ py-polars/src/polars/lazyframe/frame.py
@@ -4226,7 +4226,7 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
         chunk_size: int | None = None,
         maintain_order: bool = True,
         lazy: bool = False,
-        engine: EngineType = "streaming",
+        engine: EngineType = "auto",
         optimizations: QueryOptFlags = DEFAULT_QUERY_OPT_FLAGS,
     ) -> Iterator[DataFrame]:
         """
@@ -4290,7 +4290,7 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
 
         ldf = self._ldf.with_optimizations(optimizations._pyoptflags)
         inner = ldf.collect_batches(
-            engine=engine,
+            engine="streaming" if engine == "auto" else engine,
             maintain_order=maintain_order,
             chunk_size=chunk_size,
             lazy=lazy,
