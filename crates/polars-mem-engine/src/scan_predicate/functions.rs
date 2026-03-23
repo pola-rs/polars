d8002251f6f820dda9046e0854792fe00c936d32
Simon Lin <simonlin.rqmmw@slmail.me>
2026-03-24 00:06:54 +1100

    fix

--- crates/polars-mem-engine/src/scan_predicate/functions.rs
+++ crates/polars-mem-engine/src/scan_predicate/functions.rs
@@ -41,9 +41,9 @@ pub fn create_scan_predicate(
     let mut hive_predicate = None;
     let mut hive_predicate_is_full_predicate = false;
 
-    'set_hive_schema: {
+    'set_scan_predicate: {
         let Some(hive_schema) = hive_schema else {
-            break 'set_hive_schema;
+            break 'set_scan_predicate;
         };
 
         let mut hive_predicate_parts = vec![];
@@ -60,12 +60,12 @@ pub fn create_scan_predicate(
         }
 
         if hive_predicate_parts.is_empty() {
-            break 'set_hive_schema;
+            break 'set_scan_predicate;
         }
 
         if non_hive_predicate_parts.is_empty() {
             hive_predicate_is_full_predicate = true;
-            break 'set_hive_schema;
+            break 'set_scan_predicate;
         }
 
         {
@@ -211,9 +211,9 @@ pub fn initialize_scan_predicate<'a>(
     table_statistics: Option<&TableStatistics>,
     verbose: bool,
 ) -> PolarsResult<(Option<SkipFilesMask>, Option<&'a ScanIOPredicate>)> {
-    'set_hive_schema: {
+    'create_skip_files_mask: {
         let Some(predicate) = predicate else {
-            break 'set_hive_schema;
+            break 'create_skip_files_mask;
         };
 
         let expected_mask_len: usize;
@@ -259,7 +259,7 @@ pub fn initialize_scan_predicate<'a>(
 
             (SkipFilesMask::Exclusion(exclusion_mask), true)
         } else {
-            break 'set_hive_schema;
+            break 'create_skip_files_mask;
         };
 
         if skip_files_mask.len() != expected_mask_len {
