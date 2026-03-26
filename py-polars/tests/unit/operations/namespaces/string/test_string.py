fb0970adad72934cb4e803b95989b7d36f64f5eb
Nathan Hu <nahu725@gmail.com>
2026-03-25 21:45:38 -0400

    format test

--- py-polars/tests/unit/operations/namespaces/string/test_string.py
+++ py-polars/tests/unit/operations/namespaces/string/test_string.py
@@ -1562,18 +1562,16 @@ def test_starts_ends_with() -> None:
     assert df.select(
         pl.col("a").str.ends_with("pop").alias("ends_pop"),
         pl.col("a").str.ends_with("pop", "ts").alias("ends_pop_or_ts"),
-        pl.col("a").str.ends_with(pl.col("sub"), "pop").alias(
-            "ends_sub_or_pop"
-        ),
+        pl.col("a").str.ends_with(pl.col("sub"), "pop").alias("ends_sub_or_pop"),
         pl.col("a").str.ends_with(pl.lit(None)).alias("ends_None"),
         pl.col("a").str.ends_with(pl.col("sub")).alias("ends_sub"),
         pl.col("a").str.starts_with("ham").alias("starts_ham"),
         pl.col("a").str.starts_with(pl.lit(None)).alias("starts_None"),
         pl.col("a").str.starts_with(pl.col("sub")).alias("starts_sub"),
         pl.col("a").str.starts_with("ham", "nut").alias("starts_ham_or_nut"),
-        pl.col("a").str.starts_with(pl.col("sub"), "lolly").alias(
-            "starts_sub_or_lolly"
-        ),
+        pl.col("a")
+        .str.starts_with(pl.col("sub"), "lolly")
+        .alias("starts_sub_or_lolly"),
     ).to_dict(as_series=False) == {
         "ends_pop": [False, False, True, None],
         "ends_pop_or_ts": [False, True, True, None],
