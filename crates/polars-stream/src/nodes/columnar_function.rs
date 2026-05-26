5068aae6b7d8f2ab85099de853cc25e6ceee0f72
Simon Lin <simonlin.rqmmw@slmail.me>
2026-05-27 03:49:17 +1000

    c

--- crates/polars-stream/src/nodes/columnar_function.rs
+++ crates/polars-stream/src/nodes/columnar_function.rs
@@ -108,11 +108,11 @@ impl ComputeNode for ColumnarFunctionNode {
                 send[0] = PortState::Blocked;
             },
             Self::Source(source_node) => {
-                recv[0] = PortState::Done;
+                recv.iter_mut().for_each(|v| *v = PortState::Done);
                 source_node.update_state(&mut [], send, state)?;
             },
             Self::Done => {
-                recv[0] = PortState::Done;
+                recv.iter_mut().for_each(|v| *v = PortState::Done);
                 send[0] = PortState::Done;
             },
         }
