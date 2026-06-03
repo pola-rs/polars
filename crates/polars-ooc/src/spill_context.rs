6a1eb1f86a5a476cdc15290a7ba23b6509957c08
Orson Peters <orsonpeters@gmail.com>
2026-06-03 18:01:53 +0200

    refactor(rust): Add statistics to spill contexts

--- crates/polars-ooc/src/spill_context.rs
+++ crates/polars-ooc/src/spill_context.rs
@@ -1,7 +1,9 @@
 use std::collections::VecDeque;
 use std::fmt::Debug;
-use std::sync::{Arc, RwLock, Weak};
+use std::sync::{Arc, Mutex, RwLock, Weak};
+use std::time::Instant;
 
+use polars_utils::relaxed_cell::RelaxedCell;
 use thread_local::ThreadLocal;
 
 use crate::{DynSpillToken, SpillToken, SpillTokenInner, Spillable, memory_manager};
@@ -68,17 +70,21 @@ pub trait ParameterFreeSpillContext {
     where
         T: AsRef<SpillToken<S>>,
         S: Spillable;
+
+    fn stats(&self) -> &Arc<SpillContextStatistics>;
 }
 
 /// A context that spills the most-recently registered spillable when asked.
 pub struct MostRecentSpillContext {
     local: ThreadLocal<RwLock<LocalSpillQueue>>,
+    stats: Arc<SpillContextStatistics>,
 }
 
 impl MostRecentSpillContext {
     pub fn new() -> Arc<Self> {
         let slf = Arc::new(Self {
             local: ThreadLocal::default(),
+            stats: Arc::default(),
         });
         memory_manager().register_ctx(&slf);
         slf
@@ -97,6 +103,10 @@ impl ParameterFreeSpillContext for MostRecentSpillContext {
         let mut local = self.local.get_or_default().write().unwrap();
         local.push_back(&inner, inner.new_registration_id());
     }
+
+    fn stats(&self) -> &Arc<SpillContextStatistics> {
+        &self.stats
+    }
 }
 
 impl SpillContext for MostRecentSpillContext {}
@@ -110,12 +120,14 @@ impl Debug for MostRecentSpillContext {
 /// A context that spills the least-recently registered spillable when asked.
 pub struct LeastRecentSpillContext {
     local: ThreadLocal<RwLock<LocalSpillQueue>>,
+    stats: Arc<SpillContextStatistics>,
 }
 
 impl LeastRecentSpillContext {
     pub fn new() -> Arc<Self> {
         let slf = Arc::new(Self {
             local: ThreadLocal::default(),
+            stats: Arc::default(),
         });
         memory_manager().register_ctx(&slf);
         slf
@@ -134,6 +146,10 @@ impl ParameterFreeSpillContext for LeastRecentSpillContext {
         let mut local = self.local.get_or_default().write().unwrap();
         local.push_back(&inner, inner.new_registration_id());
     }
+
+    fn stats(&self) -> &Arc<SpillContextStatistics> {
+        &self.stats
+    }
 }
 
 impl SpillContext for LeastRecentSpillContext {}
@@ -147,12 +163,14 @@ impl Debug for LeastRecentSpillContext {
 /// A context that spills a random registered spillable when asked.
 pub struct RandomSpillContext {
     local: ThreadLocal<RwLock<LocalSpillQueue>>,
+    stats: Arc<SpillContextStatistics>,
 }
 
 impl RandomSpillContext {
     pub fn new() -> Arc<Self> {
         let slf = Arc::new(Self {
             local: ThreadLocal::default(),
+            stats: Arc::default(),
         });
         memory_manager().register_ctx(&slf);
         slf
@@ -171,6 +189,10 @@ impl ParameterFreeSpillContext for RandomSpillContext {
         let mut local = self.local.get_or_default().write().unwrap();
         local.push_back(&inner, inner.new_registration_id());
     }
+
+    fn stats(&self) -> &Arc<SpillContextStatistics> {
+        &self.stats
+    }
 }
 
 impl SpillContext for RandomSpillContext {}
@@ -178,5 +200,87 @@ impl SpillContext for RandomSpillContext {}
 impl Debug for RandomSpillContext {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("RandomSpillContext").finish()
+    }
+}
+
+// Used to normalize divisor to avoid absurdly high scores. Set to 0.1ms.
+const BASE_IO_TIME: f64 = 1.0 / 10_000.0;
+
+#[derive(Default)]
+pub struct SpillContextStatistics {
+    score: RelaxedCell<u64>,
+    stats: Mutex<Statistics>,
+}
+
+impl SpillContextStatistics {
+    pub fn score(&self) -> f64 {
+        // Try to re-compute, if lock is taken just take cached value.
+        if let Ok(mut stats) = self.stats.try_lock() {
+            stats.update(Instant::now());
+            let io_time = stats.spill_time + stats.unspill_time;
+            let score = stats.spilled_byte_seconds / (BASE_IO_TIME + io_time);
+            self.score.store(score.to_bits());
+            score
+        } else {
+            f64::from_bits(self.score.load())
+        }
+    }
+
+    pub fn add_failed_spill(&self, spill_start: Instant) {
+        let spill_time_sec = spill_start.elapsed().as_secs_f64();
+        let mut stats = self.stats.lock().unwrap();
+        stats.spill_time += spill_time_sec;
+        stats.failed_spills += 1;
+    }
+
+    pub fn add_successful_spill(&self, n_bytes: usize, spill_start: Instant) {
+        let mut stats = self.stats.lock().unwrap();
+        let now = Instant::now();
+        let spill_time_sec = (now - spill_start).as_secs_f64();
+        stats.update(now); // Update before mutating bytes_currently_spilled.
+        stats.spill_time += spill_time_sec;
+        stats.bytes_currently_spilled += n_bytes as u64;
+        stats.successful_spills += 1;
+    }
+
+    pub fn add_unspill(&self, n_bytes: usize, unspill_start: Instant) {
+        let mut stats = self.stats.lock().unwrap();
+        let now = Instant::now();
+        let spill_time_sec = (now - unspill_start).as_secs_f64();
+        stats.update(now); // Update before mutating bytes_currently_spilled.
+        stats.unspill_time += spill_time_sec;
+        stats.bytes_currently_spilled -= n_bytes as u64;
+    }
+}
+
+struct Statistics {
+    spilled_byte_seconds: f64,
+    bytes_currently_spilled: u64,
+    last_update: Instant,
+    spill_time: f64,
+    unspill_time: f64,
+    successful_spills: u64,
+    failed_spills: u64,
+}
+
+impl Statistics {
+    fn update(&mut self, now: Instant) {
+        let delta = now - self.last_update;
+        self.spilled_byte_seconds += self.bytes_currently_spilled as f64 * delta.as_secs_f64();
+        self.last_update = now;
+    }
+}
+
+impl Default for Statistics {
+    fn default() -> Self {
+        Self {
+            spilled_byte_seconds: 0.0,
+            bytes_currently_spilled: 0,
+            last_update: Instant::now(),
+            spill_time: 0.0,
+            unspill_time: 0.0,
+            successful_spills: 0,
+            failed_spills: 0,
+        }
     }
 }
