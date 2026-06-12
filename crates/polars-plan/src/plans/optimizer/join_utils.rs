67ae2624dec53c6facda2c29dd3a9a8a809be20e
ritchie46 <ritchie46@gmail.com>
2026-06-12 17:07:43 +0200

    lint

--- crates/polars-plan/src/plans/optimizer/join_utils.rs
+++ crates/polars-plan/src/plans/optimizer/join_utils.rs
@@ -83,9 +83,15 @@ impl ExprOrigin {
             } else {
                 let suggestion = polars_utils::levenshtein::did_you_mean(
                     column_name,
-                    left_schema.iter_names().chain(right_schema.iter_names()).map(|s| s.as_str()),
+                    left_schema
+                        .iter_names()
+                        .chain(right_schema.iter_names())
+                        .map(|s| s.as_str()),
                 );
-                let available: Vec<_> = left_schema.iter_names().chain(right_schema.iter_names()).collect();
+                let available: Vec<_> = left_schema
+                    .iter_names()
+                    .chain(right_schema.iter_names())
+                    .collect();
                 return Err(if let Some(s) = suggestion {
                     polars_err!(ColumnNotFound: "unable to find column {:?}; valid columns: {:?}\n\nDid you mean {:?}?", column_name, available, s)
                 } else {
