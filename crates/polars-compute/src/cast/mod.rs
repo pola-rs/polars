a2236d12f344dfd88af8a106c8a66d43f54cd9b1
Amber Sprenkels <amber@electricdusk.com>
2026-06-23 15:03:03 +0200

    Remove casts from string to temporal types

--- crates/polars-compute/src/cast/mod.rs
+++ crates/polars-compute/src/cast/mod.rs
@@ -23,17 +23,13 @@ use arrow::array::*;
 use arrow::datatypes::*;
 use arrow::match_integer_type;
 use arrow::offset::{Offset, Offsets};
-use binview_to::{
-    binview_to_dictionary, utf8view_to_date32_dyn, utf8view_to_dictionary,
-    utf8view_to_naive_timestamp_dyn, view_to_binary,
-};
+use binview_to::{binview_to_dictionary, utf8view_to_dictionary, view_to_binary};
 pub use binview_to::{binview_to_fixed_size_list_dyn, binview_to_primitive_dyn};
 use dictionary_to::*;
-use polars_error::{PolarsResult, polars_bail, polars_ensure, polars_err, polars_warn};
+use polars_error::{PolarsResult, polars_bail, polars_ensure, polars_err};
 use polars_utils::IdxSize;
 use polars_utils::float16::pf16;
 pub use primitive_to::*;
-use temporal::utf8view_to_timestamp;
 pub use utf8_to::*;
 
 /// options defining how Cast kernels behave
@@ -549,35 +545,26 @@ pub fn cast(
                 Float16 => utf8view_to_primitive_dyn::<pf16>(arr, to_type, options),
                 Float32 => utf8view_to_primitive_dyn::<f32>(arr, to_type, options),
                 Float64 => utf8view_to_primitive_dyn::<f64>(arr, to_type, options),
-                Timestamp(time_unit, None) => {
-                    polars_warn!(
-                        Deprecation,
-                        "Casting from String to DateTime is deprecated and will be removed in Polars 2.0.\n\
-                        Use `str.to_datetime()` instead."
+                Timestamp(_, None) => {
+                    polars_bail!(
+                        InvalidOperation:
+                        "casting from String to DateTime is not supported.\n\
+                        It was removed in Polars 2.0. Use `str.to_datetime()` instead."
                     );
-                    utf8view_to_naive_timestamp_dyn(array, time_unit.to_owned())
                 },
-                Timestamp(time_unit, Some(time_zone)) => {
-                    polars_warn!(
-                        Deprecation,
-                        "Casting from String to DateTime is deprecated and will be removed in Polars 2.0.\n\
-                        Use `str.to_datetime(..., \"time_zone={time_zone}\")` instead."
+                Timestamp(_, Some(time_zone)) => {
+                    polars_bail!(
+                        InvalidOperation:
+                        "casting from String to DateTime is not supported.\n\
+                        It was removed in Polars 2.0. Use `str.to_datetime(..., \"time_zone={time_zone}\")` instead."
                     );
-                    utf8view_to_timestamp(
-                        array.as_any().downcast_ref().unwrap(),
-                        RFC3339,
-                        time_zone.clone(),
-                        time_unit.to_owned(),
-                    )
-                    .map(|arr| arr.boxed())
                 },
                 Date32 => {
-                    polars_warn!(
-                        Deprecation,
-                        "Casting from String to Date is deprecated and will be removed in Polars 2.0.\n\
-                        Use `str.to_date()` instead."
+                    polars_bail!(
+                        InvalidOperation:
+                        "casting from String to Date is not supported.\n\
+                        It was removed in Polars 2.0. Use `str.to_date()` instead."
                     );
-                    utf8view_to_date32_dyn(array)
                 },
                 #[cfg(feature = "dtype-decimal")]
                 Decimal(precision, scale) => {
