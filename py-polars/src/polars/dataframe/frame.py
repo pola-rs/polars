9a4a8e227abb8215d29cf8522cc0800044691699
Amber Sprenkels <amber@electricdusk.com>
2026-06-24 12:01:12 +0200

    Set explode(empty_as_null = False) by default

--- py-polars/src/polars/dataframe/frame.py
+++ py-polars/src/polars/dataframe/frame.py
@@ -71,7 +71,6 @@ from polars._utils.unstable import issue_unstable_warning, unstable
 from polars._utils.various import (
     NO_DEFAULT,
     _in_notebook,
-    _Omitted,
     is_bool_sequence,
     normalize_filepath,
     parse_version,
@@ -9456,7 +9455,7 @@ class DataFrame:
         self,
         columns: ColumnNameOrSelector | Iterable[ColumnNameOrSelector],
         *more_columns: ColumnNameOrSelector,
-        empty_as_null: bool = _Omitted,
+        empty_as_null: bool = False,
         keep_nulls: bool = True,
     ) -> DataFrame:
         """
