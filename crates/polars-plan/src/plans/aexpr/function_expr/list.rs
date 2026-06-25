bea3cf7a69c55c601a0fa49b683b3406e6f4c16e
Amber Sprenkels <amber@polars.tech>
2026-06-25 16:26:27 +0200

    feat!: Remove support for `std()` for `Duration` dtype (#28089)

--- crates/polars-plan/src/plans/aexpr/function_expr/list.rs
+++ crates/polars-plan/src/plans/aexpr/function_expr/list.rs
@@ -91,7 +91,7 @@ impl IRListFunction {
             Max => mapper.ensure_is_list()?.map_to_list_and_array_inner_dtype(),
             Mean => mapper.nested_mean_median_type(),
             Median => mapper.nested_mean_median_type(),
-            Std(_) => mapper.ensure_is_list()?.moment_dtype(), // Need to also have this sometimes marked as float32 or duration..
+            Std(_) => mapper.ensure_is_list()?.var_dtype(), // Need to also have this sometimes marked as float32 or duration..
             Var(_) => mapper.ensure_is_list()?.var_dtype(),
             ArgMin => mapper.ensure_is_list()?.with_dtype(IDX_DTYPE),
             ArgMax => mapper.ensure_is_list()?.with_dtype(IDX_DTYPE),
