152f981e1cf76de1dc29d2f6a9f55ea3e7ac0dc4
Amber Sprenkels <amber@polars.tech>
2026-07-01 10:20:48 +0200

    feat(python!): Remove `Expr.agg_groups()` and `pl.groups()` (#28153)

--- crates/polars-plan/src/plans/aexpr/schema.rs
+++ crates/polars-plan/src/plans/aexpr/schema.rs
@@ -247,11 +247,6 @@ impl AExpr {
                         field.coerce(IDX_DTYPE);
                         Ok(field)
                     },
-                    AggGroups(expr) => {
-                        let mut field = ctx.arena.get(*expr).to_field_impl(ctx)?;
-                        field.coerce(IDX_DTYPE.implode());
-                        Ok(field)
-                    },
                 }
             },
             Cast { expr, dtype, .. } => {
@@ -449,8 +444,7 @@ impl AExpr {
             | Agg(Std(expr, _))
             | Agg(Var(expr, _))
             | Agg(NUnique(expr))
-            | Agg(Count { input: expr, .. })
-            | Agg(AggGroups(expr)) => expr_arena.get(*expr).to_name(expr_arena),
+            | Agg(Count { input: expr, .. }) => expr_arena.get(*expr).to_name(expr_arena),
             AnonymousFunction { input, fmt_str, .. } | AnonymousAgg { input, fmt_str, .. } => {
                 if input.is_empty() {
                     fmt_str.as_ref().clone()
