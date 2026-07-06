a298fc95ac8023957331253aaa956ae2aa89d16a
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2026-07-06 15:42:02 +0100

    feat: Add `list` expression which consistently packs elements together into new List type (#27990)
    Co-authored-by: Orson Peters <orsonpeters@gmail.com>

--- crates/polars-python/src/c_api/mod.rs
+++ crates/polars-python/src/c_api/mod.rs
@@ -206,6 +206,7 @@ pub fn _polars_runtime(py: Python, m: &Bound<PyModule>) -> PyResult<()> {
         .unwrap();
     m.add_wrapped(wrap_pyfunction!(functions::concat_list))
         .unwrap();
+    m.add_wrapped(wrap_pyfunction!(functions::as_list)).unwrap();
     m.add_wrapped(wrap_pyfunction!(functions::concat_str))
         .unwrap();
     m.add_wrapped(wrap_pyfunction!(functions::len)).unwrap();
