98f8e82a016d964d07f5d1c3f3a2eee955aaa6ce
Lawrence Mitchell <lmitchell@nvidia.com>
2026-07-08 17:22:29 +0100

    refactor(rust): Extract binary coercion dtype resolution
    We will use the logic for coercing to a common supertype in IsBetween type
    coercion.

--- crates/polars-plan/src/plans/conversion/type_coercion/binary.rs
+++ crates/polars-plan/src/plans/conversion/type_coercion/binary.rs
@@ -115,6 +115,119 @@ fn err_date_str_compare() -> PolarsResult<()> {
     }
 }
 
+pub(super) fn coerced_binop_dtype(
+    expr_arena: &Arena<AExpr>,
+    node_left: Node,
+    type_left: &DataType,
+    op: Operator,
+    node_right: Node,
+    type_right: &DataType,
+) -> PolarsResult<Option<DataType>> {
+    let left = expr_arena.get(node_left);
+    let right = expr_arena.get(node_right);
+
+    use DataType::*;
+    // don't coerce string with number comparisons. They must error
+    match (&type_left, &type_right, op) {
+        #[cfg(not(feature = "dtype-categorical"))]
+        (DataType::String, dt, op) | (dt, DataType::String, op)
+            if op.is_comparison_or_bitwise() && dt.is_primitive_numeric() =>
+        {
+            return Ok(None);
+        },
+        #[cfg(feature = "dtype-categorical")]
+        (String | Unknown(UnknownKind::Str) | Categorical(_, _), dt, op)
+        | (dt, Unknown(UnknownKind::Str) | String | Categorical(_, _), op)
+            if op.is_comparison_or_bitwise() && dt.is_primitive_numeric() =>
+        {
+            return Ok(None);
+        },
+        #[cfg(feature = "dtype-categorical")]
+        (Unknown(UnknownKind::Str) | String | Enum(_, _), dt, op)
+        | (dt, Unknown(UnknownKind::Str) | String | Enum(_, _), op)
+            if op.is_comparison_or_bitwise() && dt.is_primitive_numeric() =>
+        {
+            return Ok(None);
+        },
+        #[cfg(feature = "dtype-date")]
+        (Date, String | Unknown(UnknownKind::Str), op)
+        | (String | Unknown(UnknownKind::Str), Date, op)
+            if op.is_comparison_or_bitwise() =>
+        {
+            err_date_str_compare()?
+        },
+        #[cfg(feature = "dtype-datetime")]
+        (Datetime(_, _), String | Unknown(UnknownKind::Str), op)
+        | (String | Unknown(UnknownKind::Str), Datetime(_, _), op)
+            if op.is_comparison_or_bitwise() =>
+        {
+            err_date_str_compare()?
+        },
+        #[cfg(feature = "dtype-time")]
+        (Time | Unknown(UnknownKind::Str), String, op) if op.is_comparison_or_bitwise() => {
+            err_date_str_compare()?
+        },
+        // structs can be arbitrarily nested, leave the complexity to the caller for now.
+        #[cfg(feature = "dtype-struct")]
+        (Struct(_), Struct(_), _op) => return Ok(None),
+        _ => {},
+    }
+
+    if op.is_arithmetic() {
+        match (&type_left, &type_right) {
+            (Duration(_), Duration(_)) => return Ok(None),
+            (Duration(_), r) if r.is_primitive_numeric() => return Ok(None),
+            (String, a) | (a, String) if a.is_primitive_numeric() => {
+                let fmt_side = |node: Node, ae: &AExpr| -> std::string::String {
+                    if let AExpr::Column(name) = ae {
+                        format!(" column '{name}'")
+                    } else {
+                        format!(" expression `{}`", node_to_expr(node, expr_arena))
+                    }
+                };
+                let lhs = fmt_side(node_left, left);
+                let rhs = fmt_side(node_right, right);
+                polars_bail!(
+                    InvalidOperation:
+                    "arithmetic on dtypes {type_left} and {type_right} is not allowed \
+                    (lhs:{lhs}, rhs:{rhs}); try an explicit cast first",
+                )
+            },
+            (Datetime(_, _), _)
+            | (_, Datetime(_, _))
+            | (Date, _)
+            | (_, Date)
+            | (Duration(_), _)
+            | (_, Duration(_))
+            | (Time, _)
+            | (_, Time)
+            | (List(_), _)
+            | (_, List(_)) => return Ok(None),
+            #[cfg(feature = "dtype-array")]
+            (Array(..), _) | (_, Array(..)) => return Ok(None),
+            _ => {},
+        }
+    } else if compares_cat_to_string(type_left, type_right, op) {
+        return Ok(None);
+    }
+
+    // Coerce types:
+    let st = unpack!(get_supertype(type_left, type_right));
+    let mut st = modify_supertype(st, left, right, type_left, type_right);
+
+    if is_cat_str_binary(type_left, type_right) {
+        st = String
+    }
+
+    // TODO! raise here?
+    // We should at least never cast to Unknown.
+    if matches!(st, DataType::Unknown(_)) {
+        return Ok(None);
+    }
+
+    Ok(Some(st))
+}
+
 pub(super) fn process_binary(
     expr_arena: &mut Arena<AExpr>,
     input_schema: &Schema,
@@ -122,6 +235,8 @@ pub(super) fn process_binary(
     op: Operator,
     node_right: Node,
 ) -> PolarsResult<Option<AExpr>> {
+    use DataType::*;
+
     let (left, type_left): (&AExpr, DataType) =
         unpack!(get_aexpr_and_type(expr_arena, node_left, input_schema));
     let (right, type_right): (&AExpr, DataType) =
@@ -212,113 +327,27 @@ pub(super) fn process_binary(
 
     unpack!(early_escape(&type_left, &type_right));
 
-    let left = expr_arena.get(node_left);
-    let right = expr_arena.get(node_right);
-
-    use DataType::*;
-    // don't coerce string with number comparisons. They must error
-    match (&type_left, &type_right, op) {
-        #[cfg(not(feature = "dtype-categorical"))]
-        (DataType::String, dt, op) | (dt, DataType::String, op)
-            if op.is_comparison_or_bitwise() && dt.is_primitive_numeric() =>
-        {
-            return Ok(None);
-        },
-        #[cfg(feature = "dtype-categorical")]
-        (String | Unknown(UnknownKind::Str) | Categorical(_, _), dt, op)
-        | (dt, Unknown(UnknownKind::Str) | String | Categorical(_, _), op)
-            if op.is_comparison_or_bitwise() && dt.is_primitive_numeric() =>
-        {
-            return Ok(None);
-        },
-        #[cfg(feature = "dtype-categorical")]
-        (Unknown(UnknownKind::Str) | String | Enum(_, _), dt, op)
-        | (dt, Unknown(UnknownKind::Str) | String | Enum(_, _), op)
-            if op.is_comparison_or_bitwise() && dt.is_primitive_numeric() =>
-        {
-            return Ok(None);
-        },
-        #[cfg(feature = "dtype-date")]
-        (Date, String | Unknown(UnknownKind::Str), op)
-        | (String | Unknown(UnknownKind::Str), Date, op)
-            if op.is_comparison_or_bitwise() =>
-        {
-            err_date_str_compare()?
-        },
-        #[cfg(feature = "dtype-datetime")]
-        (Datetime(_, _), String | Unknown(UnknownKind::Str), op)
-        | (String | Unknown(UnknownKind::Str), Datetime(_, _), op)
-            if op.is_comparison_or_bitwise() =>
-        {
-            err_date_str_compare()?
-        },
-        #[cfg(feature = "dtype-time")]
-        (Time | Unknown(UnknownKind::Str), String, op) if op.is_comparison_or_bitwise() => {
-            err_date_str_compare()?
-        },
-        // structs can be arbitrarily nested, leave the complexity to the caller for now.
-        #[cfg(feature = "dtype-struct")]
-        (Struct(_), Struct(_), _op) => return Ok(None),
-        _ => {},
+    #[cfg(feature = "dtype-struct")]
+    if op.is_arithmetic()
+        && matches!(
+            (&type_left, &type_right),
+            (DataType::Struct(_), a) | (a, DataType::Struct(_))
+                if a.is_primitive_numeric()
+        )
+    {
+        return process_struct_numeric_arithmetic(
+            type_left, type_right, node_left, node_right, op, expr_arena,
+        );
     }
 
-    if op.is_arithmetic() {
-        match (&type_left, &type_right) {
-            (Duration(_), Duration(_)) => return Ok(None),
-            (Duration(_), r) if r.is_primitive_numeric() => return Ok(None),
-            (String, a) | (a, String) if a.is_primitive_numeric() => {
-                let fmt_side = |node: Node, ae: &AExpr| -> std::string::String {
-                    if let AExpr::Column(name) = ae {
-                        format!(" column '{name}'")
-                    } else {
-                        format!(" expression `{}`", node_to_expr(node, expr_arena))
-                    }
-                };
-                let lhs = fmt_side(node_left, left);
-                let rhs = fmt_side(node_right, right);
-                polars_bail!(
-                    InvalidOperation:
-                    "arithmetic on dtypes {type_left} and {type_right} is not allowed \
-                    (lhs:{lhs}, rhs:{rhs}); try an explicit cast first",
-                )
-            },
-            (Datetime(_, _), _)
-            | (_, Datetime(_, _))
-            | (Date, _)
-            | (_, Date)
-            | (Duration(_), _)
-            | (_, Duration(_))
-            | (Time, _)
-            | (_, Time)
-            | (List(_), _)
-            | (_, List(_)) => return Ok(None),
-            #[cfg(feature = "dtype-array")]
-            (Array(..), _) | (_, Array(..)) => return Ok(None),
-            #[cfg(feature = "dtype-struct")]
-            (Struct(_), a) | (a, Struct(_)) if a.is_primitive_numeric() => {
-                return process_struct_numeric_arithmetic(
-                    type_left, type_right, node_left, node_right, op, expr_arena,
-                );
-            },
-            _ => {},
-        }
-    } else if compares_cat_to_string(&type_left, &type_right, op) {
-        return Ok(None);
-    }
-
-    // Coerce types:
-    let st = unpack!(get_supertype(&type_left, &type_right));
-    let mut st = modify_supertype(st, left, right, &type_left, &type_right);
-
-    if is_cat_str_binary(&type_left, &type_right) {
-        st = String
-    }
-
-    // TODO! raise here?
-    // We should at least never cast to Unknown.
-    if matches!(st, DataType::Unknown(_)) {
-        return Ok(None);
-    }
+    let st = unpack!(coerced_binop_dtype(
+        expr_arena,
+        node_left,
+        &type_left,
+        op,
+        node_right,
+        &type_right,
+    )?);
 
     // Only cast if the type is not already the super type.
     // this can prevent an expensive flattening and subsequent aggregation
