2aa533a7f613b3679683e06f4350be9f1f3bdc85
Amber Sprenkels <amber@polars.tech>
2026-07-15 10:10:17 +0200

    build: Set the default `dev` profile to `line-tables-only` (#28358)

--- flake.nix
+++ flake.nix
@@ -157,10 +157,10 @@
                     cmd = buildPy "debug" "maturin develop -m $WORKSPACE_ROOT/py-polars/runtime/polars-runtime-32/Cargo.toml \"$@\" --uv";
                     doc = "Build the python library";
                   };
-                  pybuild-mindebug = {
+                  pybuild-debug = {
                     pwd = "py-polars";
-                    cmd = buildPy "mindebug" "maturin develop --profile mindebug-dev \"$@\" --uv";
-                    doc = "Build the python library with minimal debug information";
+                    cmd = buildPy "debug" "maturin develop --profile debug-dev \"$@\" --uv";
+                    doc = "Build the python library with full debug information";
                   };
                   pybuild-nodebug-release = {
                     pwd = "py-polars";
@@ -271,7 +271,7 @@
                     cmd = ''
                       ${aliasToScript precommit}
                       ${step "Rust Tests" rstest}
-                      ${step "Python Build" pybuild-mindebug}
+                      ${step "Python Build" pybuild}
                       ${step "Python Tests" pytest-all}
                     '';
                     doc = "Run the checks to do before pushing";
