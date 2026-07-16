6478498e3da13969f0d059478beb13c15356bb49
nameexhaustion <simonlin.rqmmw@slmail.me>
2026-07-17 06:00:40 +1000

    fix: Ensure BinaryView offset+len does not exceed i32::MAX where possible (#28048)

--- crates/polars-compute/src/cast/utf8_to.rs
+++ crates/polars-compute/src/cast/utf8_to.rs
@@ -1,10 +1,9 @@
 use arrow::array::*;
 use arrow::datatypes::ArrowDataType;
 use arrow::offset::Offset;
-use arrow::types::NativeType;
 use polars_buffer::Buffer;
 use polars_error::PolarsResult;
-use polars_utils::vec::PushUnchecked;
+use polars_utils::unitvec;
 
 pub(super) const RFC3339: &str = "%Y-%m-%dT%H:%M:%S%.f%:z";
 
@@ -67,100 +66,83 @@ pub fn utf8_to_binary<O: Offset>(from: &Utf8Array<O>, to_dtype: ArrowDataType) -
     )
 }
 
-// Different types to test the overflow path.
-#[cfg(not(test))]
-type OffsetType = u32;
-
-// To trigger overflow
-#[cfg(test)]
-type OffsetType = i8;
-
-// If we don't do this the GC of binview will trigger. As we will split up buffers into multiple
-// chunks so that we don't overflow the offset u32.
-fn truncate_buffer(buf: &Buffer<u8>) -> Buffer<u8> {
-    // * 2, as it must be able to hold u32::MAX offset + u32::MAX len.
-    let len = std::cmp::min(buf.len(), ((OffsetType::MAX as u64) * 2) as usize);
-    buf.clone().sliced(..len)
-}
+const ARROW_MAX_OFFSET: u32 = if cfg!(test) {
+    // Used to test buffer splitting.
+    i8::MAX as u32
+} else {
+    // Limit to i32 rather than u32 to maintain interop compatibility with arrow consumers that
+    // use signed integers.
+    i32::MAX as u32
+};
 
 pub fn binary_to_binview<O: Offset>(arr: &BinaryArray<O>) -> BinaryViewArray {
-    // Ensure we didn't accidentally set wrong type
-    #[cfg(not(debug_assertions))]
-    let _ = std::mem::transmute::<OffsetType, u32>;
-
     let mut views = Vec::with_capacity(arr.len());
-    let mut uses_buffer = false;
 
-    let mut base_buffer = arr.values().clone();
-    // Offset into the buffer
-    let mut base_ptr = base_buffer.as_ptr() as usize;
+    let mut buffers = unitvec![];
+    let mut current_buffer_range: std::ops::Range<usize> = 0..0;
+    let mut total_buffer_len: usize = 0;
 
-    // Offset into the binview buffers
-    let mut buffer_idx = 0_u32;
+    for row_byte_range in arr
+        .offsets()
+        .array_windows::<2>()
+        .map(|[start, end]| start.to_usize()..end.to_usize())
+    {
+        let row_byte_len: usize = row_byte_range.len();
+        assert!(
+            row_byte_len <= BINVIEW_MAX_ROW_BYTE_LEN,
+            "max string/binary length exceeded"
+        );
 
-    // Binview buffers
-    // Note that the buffer may look far further than u32::MAX, but as we don't clone data
-    let mut buffers = vec![truncate_buffer(&base_buffer)];
+        let row_byte_values = unsafe { arr.values().get_unchecked(row_byte_range.clone()) };
 
-    for bytes in arr.values_iter() {
-        let len: u32 = bytes
-            .len()
-            .try_into()
-            .expect("max string/binary length exceeded");
-
-        let mut payload = [0; 16];
-        payload[0..4].copy_from_slice(&len.to_le_bytes());
-
-        if len <= 12 {
-            payload[4..4 + bytes.len()].copy_from_slice(bytes);
+        let view = if row_byte_len <= View::MAX_INLINE_SIZE as usize {
+            unsafe { View::new_inline_unchecked(row_byte_values) }
         } else {
-            uses_buffer = true;
+            if row_byte_range.end > current_buffer_range.end {
+                let new_buffer_end = usize::min(
+                    arr.values().len(),
+                    usize::max(
+                        row_byte_range.end,
+                        row_byte_range
+                            .start
+                            .saturating_add(ARROW_MAX_OFFSET as usize),
+                    ),
+                );
 
-            // Copy the parts we know are correct.
-            unsafe { payload[4..8].copy_from_slice(bytes.get_unchecked(0..4)) };
-            payload[0..4].copy_from_slice(&len.to_le_bytes());
+                let new_buffer_range = row_byte_range.start..new_buffer_end;
 
-            let current_bytes_ptr = bytes.as_ptr() as usize;
-            let offset = current_bytes_ptr - base_ptr;
-
-            // Here we check the overflow of the buffer offset.
-            if let Ok(offset) = OffsetType::try_from(offset) {
-                #[allow(clippy::unnecessary_cast)]
-                let offset = offset as u32;
-                payload[12..16].copy_from_slice(&offset.to_le_bytes());
-                payload[8..12].copy_from_slice(&buffer_idx.to_le_bytes());
-            } else {
-                let len = base_buffer.len() - offset;
-
-                // Set new buffer
-                base_buffer = base_buffer.clone().sliced(offset..offset + len);
-                base_ptr = base_buffer.as_ptr() as usize;
-
-                // And add the (truncated) one to the buffers
-                buffers.push(truncate_buffer(&base_buffer));
-                buffer_idx = buffer_idx.checked_add(1).expect("max buffers exceeded");
-
-                let offset = 0u32;
-                payload[12..16].copy_from_slice(&offset.to_le_bytes());
-                payload[8..12].copy_from_slice(&buffer_idx.to_le_bytes());
+                assert!(
+                    buffers.len() < u32::MAX as usize,
+                    "max string/binary buffers exceeded"
+                );
+                buffers.push(unsafe {
+                    arr.values()
+                        .clone()
+                        .sliced_unchecked(new_buffer_range.clone())
+                });
+                total_buffer_len = total_buffer_len
+                    .checked_add(new_buffer_range.len())
+                    .unwrap();
+                current_buffer_range = new_buffer_range;
             }
-        }
 
-        let value = View::from_le_bytes(payload);
-        unsafe { views.push_unchecked(value) };
+            let offset: usize = row_byte_range.start - current_buffer_range.start;
+            let offset: u32 = offset as u32;
+            unsafe {
+                View::new_noninline_unchecked(row_byte_values, (buffers.len() - 1) as u32, offset)
+            }
+        };
+
+        views.push(view);
     }
-    let buffers = if uses_buffer {
-        Buffer::from(buffers)
-    } else {
-        Buffer::new()
-    };
+
     unsafe {
         BinaryViewArray::new_unchecked_unknown_md(
             ArrowDataType::BinaryView,
             views.into(),
-            buffers,
+            Buffer::from_owner(buffers),
             arr.validity().cloned(),
-            None,
+            Some(total_buffer_len),
         )
     }
 }
@@ -174,19 +156,19 @@ mod test {
     use super::*;
 
     #[test]
-    fn overflowing_utf8_to_binview() {
+    fn test_overflowing_utf8_to_binview() {
         let values = [
-            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdajfkasjdflkasjdf", // 0 (offset)
-            "123",                                                                        // inline
-            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdajfkasjdflkasjdf", // 74
-            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdajfkasjdflkasjdf", // 0 (new buffer)
-            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdajfkasjdflkasjdf", // 74
-            "234",                                                                        // inline
-            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdajfkasjdflkasjdf", // 0 (new buffer)
-            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdajfkasjdflkasjdf", // 74
-            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdajfkasjdflkasjdf", // 0 (new buffer)
-            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdajfkasjdflkasjdf", // 74
-            "324",                                                                        // inline
+            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdaj", // 0 (offset)
+            "123",                                                          // inline
+            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdaj", // 60
+            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdaj", // 0 (new buffer)
+            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdaj", // 60
+            "234",                                                          // inline
+            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdaj", // 0 (new buffer)
+            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdaj", // 60
+            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdaj", // 0 (new buffer)
+            "lksafjdlkakjslkjsafkjdalkjfalkdsalkjfaslkfjlkakdsjfkajfksdaj", // 60
+            "324",                                                          // inline
         ];
         let array = Utf8Array::<i64>::from_slice(values);
 
