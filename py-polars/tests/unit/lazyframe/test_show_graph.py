d9966d7bd050ed4cde3c337c220e926265d19093
Amber Sprenkels <amber@polars.tech>
2026-07-16 10:43:20 +0200

    Issue a deprecation warning for the changing default value of plan_stage argument in show_graph()

--- py-polars/tests/unit/lazyframe/test_show_graph.py
+++ py-polars/tests/unit/lazyframe/test_show_graph.py
@@ -45,3 +45,10 @@ def test_show_graph_phys_not_streaming(query: pl.LazyFrame) -> None:
 def test_show_graph_invalid_stage(query: pl.LazyFrame) -> None:
     with pytest.raises(TypeError, match="invalid plan stage 'invalid-stage'"):
         query.show_graph(raw_output=True, plan_stage="invalid-stage")  # type: ignore[call-overload]
+
+
+def test_show_graph_plan_stage_default_changed(query: pl.LazyFrame) -> None:
+    with pytest.raises(
+        DeprecationWarning, match="The default value of `plan_stage` will change"
+    ):
+        query.show_graph(raw_output=True)
