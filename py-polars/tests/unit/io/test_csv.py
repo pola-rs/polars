c41725c48af7cc4996242178b22e16577c56b9bf
nameexhaustion <simonlin.rqmmw@slmail.me>
2026-07-28 21:49:53 +1000

    feat(python!): Automatically disable `raise_if_empty` when `has_header=False` and schema is specified for `read/scan_csv` (#28439)
    Co-authored-by: Amber Sprenkels <amber@polars.tech>

--- py-polars/tests/unit/io/test_csv.py
+++ py-polars/tests/unit/io/test_csv.py
@@ -8,7 +8,7 @@ import warnings
 import zlib
 from datetime import date, datetime, time, timedelta, timezone
 from decimal import Decimal as D
-from typing import TYPE_CHECKING, TypedDict
+from typing import TYPE_CHECKING, Any, TypedDict
 
 import numpy as np
 import pyarrow as pa
@@ -27,6 +27,7 @@ from polars.testing import assert_frame_equal, assert_series_equal
 from tests.conftest import PlMonkeyPatch
 
 if TYPE_CHECKING:
+    from collections.abc import Callable
     from pathlib import Path
     from typing import Any
 
@@ -3253,3 +3254,37 @@ a,a_duplicated_0,a
 
     with pytest.raises(DuplicateError, match=err_msg):
         q.collect()
+
+
+@pytest.mark.parametrize(
+    "read_csv",
+    [
+        pl.read_csv,
+        lambda *a, **kw: pl.scan_csv(*a, **kw).collect(),
+    ],
+)
+def test_read_csv_default_raise_if_empty(
+    read_csv: Callable[..., pl.DataFrame],
+) -> None:
+    schema = {"a": pl.Int64}
+    assert_frame_equal(
+        read_csv(b"", has_header=False, schema=schema),
+        pl.DataFrame(schema=schema),
+    )
+
+    with pytest.raises(NoDataError):
+        read_csv(b"", has_header=False, schema=schema, raise_if_empty=True)
+
+    with pytest.raises(NoDataError):
+        read_csv(b"", has_header=False)
+
+    for has_header in [True, False]:
+        assert_frame_equal(
+            read_csv(b"", has_header=has_header, raise_if_empty=False),
+            pl.DataFrame(),
+        )
+
+        assert_frame_equal(
+            read_csv(b"", has_header=has_header, schema=schema, raise_if_empty=False),
+            pl.DataFrame(schema=schema),
+        )
