33c833e666f6c4a29421d0709b3cc4e07a0351e7
Amber Sprenkels <amber@polars.tech>
2026-07-30 10:36:18 +0200

    Remove LenType alias from aliases.rs

--- crates/polars-core/src/datatypes/aliases.rs
+++ crates/polars-core/src/datatypes/aliases.rs
@@ -21,8 +21,6 @@ pub type IdxType = UInt32Type;
 #[cfg(feature = "bigidx")]
 pub type IdxType = UInt64Type;
 
-pub type LenType = Int64Type;
-
 pub use polars_utils::pl_str::PlSmallStr;
 
 /// This hashmap uses an IdHasher
