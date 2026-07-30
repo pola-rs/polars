313c783fff48e5d4887b5281c2fa972093297df1
Simon Lin <simonlin.rqmmw@slmail.me>
2026-07-30 14:25:33 +1000

    c

--- py-polars/src/polars/lazyframe/frame.py
+++ py-polars/src/polars/lazyframe/frame.py
@@ -94,6 +94,7 @@ from polars.datatypes import (
     parse_into_datatype_expr,
     parse_into_dtype,
 )
+from polars.datatypes.classes import Struct
 from polars.datatypes.group import DataTypeGroup
 from polars.exceptions import InvalidOperationError, PerformanceWarning
 from polars.interchange.protocol import CompatLevel
@@ -7482,7 +7483,13 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
         │ 4   ┆ 8   │
         └─────┴─────┘
         """
-        return self.select(F.col("*").gather_every(n, offset))
+        return (
+            self.with_columns(
+                F.lit({}, dtype=Struct({})).alias("__POLARS_INTERNAL_HEIGHT_COLUMN")
+            )
+            .select(F.col("*").gather_every(n, offset))
+            .drop("__POLARS_INTERNAL_HEIGHT_COLUMN")
+        )
 
     def fill_null(
         self,
