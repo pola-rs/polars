14b0767b3601fc0f977e5c07f13201987bc481f1
Amber Sprenkels <amber@polars.tech>
2026-07-31 07:49:37 +0200

    feat(python!): Set the `default show_graph()` `plan_stage` to `"physical"` (#28605)

--- py-polars/src/polars/lazyframe/frame.py
+++ py-polars/src/polars/lazyframe/frame.py
@@ -1396,7 +1396,7 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
         raw_output: Literal[True],
         figsize: tuple[float, float] = ...,
         engine: EngineType = ...,
-        plan_stage: PlanStage | None = ...,
+        plan_stage: PlanStage = ...,
         optimizations: QueryOptFlags = ...,
     ) -> str: ...
 
@@ -1449,7 +1449,7 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
         cluster_with_columns: bool = True,  # noqa: ARG002
         collapse_joins: bool = True,  # noqa: ARG002
         engine: EngineType = "auto",
-        plan_stage: PlanStage | None = None,
+        plan_stage: PlanStage = "physical",
         _check_order: bool = True,
         optimizations: QueryOptFlags = DEFAULT_QUERY_OPT_FLAGS,
     ) -> str | None:
@@ -1564,22 +1564,13 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
         ... )
         >>> lf.group_by("a", maintain_order=True).agg(pl.all().sum()).sort(
         ...     "a"
-        ... ).show_graph(plan_stage="ir")  # doctest: +SKIP
+        ... ).show_graph()  # doctest: +SKIP
         """
         engine_ = _select_engine(engine)
 
         optimizations = optimizations.__copy__()
         optimizations._pyoptflags.streaming = engine_.plan_engine == "streaming"
         _ldf = self._ldf.with_optimizations(optimizations._pyoptflags)
-
-        if plan_stage is None:
-            warnings.warn(
-                "The default value of `plan_stage` will change from 'ir' to 'physical' in Polars 2.0. "
-                'Explicitly set `plan_stage="ir"` to suppress this warning.',
-                category=FutureWarning,
-                stacklevel=find_stacklevel(),
-            )
-            plan_stage = "ir"
 
         if plan_stage == "ir":
             dot = _ldf.to_dot(optimized)
