6c746f94ae66250a912ff4005bbb42dfb410c148
Amber Sprenkels <amber@polars.tech>
2026-08-05 10:57:20 +0200

    Don't remove interchange just yet

--- py-polars/src/polars/dataframe/frame.py
+++ py-polars/src/polars/dataframe/frame.py
@@ -60,6 +60,7 @@ from polars._utils.construction import (
 from polars._utils.convert import parse_as_duration_string
 from polars._utils.deprecation import (
     deprecate_renamed_parameter,
+    deprecated,
     issue_deprecation_warning,
 )
 from polars._utils.expired import expired_fallthrough, raise_expired_error
@@ -192,9 +193,15 @@ if TYPE_CHECKING:
     )
     from polars._utils.various import NoDefault
     from polars.config import TableFormatNames
+    from polars.interchange.dataframe import PolarsDataFrame
     from polars.io.cloud import CredentialProviderFunction
     from polars.io.partition import PartitionBy
     from polars.ml.torch import PolarsDataset
+
+    if sys.version_info >= (3, 13):
+        from warnings import deprecated
+    else:
+        from typing_extensions import deprecated
 
     T = TypeVar("T")
     P = ParamSpec("P")
@@ -1024,6 +1031,62 @@ class DataFrame:
             arr = arr.__array__(dtype)
 
         return arr
+
+    @deprecated(
+        "Support for the dataframe interchange protocol is deprecated since version 1.40.0"
+    )
+    def __dataframe__(
+        self,
+        nan_as_null: bool = False,  # noqa: FBT001
+        allow_copy: bool = True,  # noqa: FBT001
+    ) -> PolarsDataFrame:
+        """
+        Convert to a dataframe object implementing the dataframe interchange protocol.
+
+        .. deprecated:: 1.40.0
+            Support for the Dataframe Interchange Protocol is deprecated.
+
+        Parameters
+        ----------
+        nan_as_null
+            Overwrite null values in the data with `NaN`.
+
+            .. warning::
+                This functionality has not been implemented and the parameter will be
+                removed in a future version.
+                Setting this to `True` will raise a `NotImplementedError`.
+        allow_copy
+            Allow memory to be copied to perform the conversion. If set to `False`,
+            causes conversions that are not zero-copy to fail.
+
+        Notes
+        -----
+        Details on the Python dataframe interchange protocol:
+        https://data-apis.org/dataframe-protocol/latest/index.html
+
+        Examples
+        --------
+        Convert a Polars DataFrame to a generic dataframe object and access some
+        properties.
+
+        >>> df = pl.DataFrame({"a": [1, 2], "b": [3.0, 4.0], "c": ["x", "y"]})
+        >>> dfi = df.__dataframe__()  # doctest: +SKIP
+        >>> dfi.num_rows()  # doctest: +SKIP
+        2
+        >>> dfi.get_column(1).dtype  # doctest: +SKIP
+        (<DtypeKind.FLOAT: 2>, 64, 'g', '=')
+        """
+        if nan_as_null:
+            msg = (
+                "functionality for `nan_as_null` has not been implemented and the"
+                " parameter will be removed in a future version"
+                "\n\nUse the default `nan_as_null=False`."
+            )
+            raise NotImplementedError(msg)
+
+        from polars.interchange.dataframe import PolarsDataFrame
+
+        return PolarsDataFrame(self, allow_copy=allow_copy)
 
     def _comp(self, other: Any, op: ComparisonOperator) -> DataFrame:
         """Compare a DataFrame with another object."""
@@ -13264,8 +13327,6 @@ class DataFrame:
             case "melt":
                 hint = "use `DataFrame.unpivot` instead, with `index` instead of `id_vars` and `on` instead of `value_vars`"
                 return raise_expired_error(self, name, hint=hint)
-            case "__dataframe__":
-                return raise_expired_error(self, name)
             case "with_row_count":
                 hint = "use `with_row_index` instead. Note that the default column name has changed from 'row_nr' to 'index'."
                 return raise_expired_error(self, name, hint=hint)
