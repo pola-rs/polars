7cb1174e34d008486aae01873cbcf1d01b9e877c
Amber Sprenkels <amber@polars.tech>
2026-08-05 13:24:08 +0200

    docs: Document that arg_extremum functions return any index on ties (#28691)

--- py-polars/src/polars/series/list.py
+++ py-polars/src/polars/series/list.py
@@ -653,7 +653,11 @@ class ListNameSpace(_NamespaceSuggestMixin):
 
     def arg_min(self) -> Series:
         """
-        Retrieve the index of the minimal value in every sublist.
+        Retrieve an index of a minimal value in every sublist.
+
+        When multiple values are equal to the minimum, this function may arbitrarily
+        return the index of any of the minimum values. In this case, the returned index
+        is not guaranteed to be the same across multiple runs.
 
         Returns
         -------
@@ -675,7 +679,11 @@ class ListNameSpace(_NamespaceSuggestMixin):
 
     def arg_max(self) -> Series:
         """
-        Retrieve the index of the maximum value in every sublist.
+        Retrieve an index of a maximum value in every sublist.
+
+        When multiple values are equal to the maximum, this function may arbitrarily
+        return the index of any of the maximum values. In this case, the returned index
+        is not guaranteed to be the same across multiple runs.
 
         Returns
         -------
