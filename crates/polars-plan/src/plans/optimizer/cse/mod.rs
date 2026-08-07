7050a8990b4e359b49ea591a7c296877b9e5ecb5
Daniel Szilagyi <daniel@polars.tech>
2026-08-07 16:28:06 +0200

    Move canonical_ir to the ir/ folder

--- crates/polars-plan/src/plans/optimizer/cse/mod.rs
+++ crates/polars-plan/src/plans/optimizer/cse/mod.rs
@@ -1,8 +1,7 @@
 mod cache_states;
-mod canonical_ir;
 mod csee;
 pub mod cspe;
 
 pub(crate) use cache_states::set_cache_states;
-pub(super) use canonical_ir::{CanonicalIRId, CanonicalIRMap};
 pub(super) use csee::CommonSubExprOptimizer;
+pub(super) use crate::plans::ir::{CanonicalIRId, CanonicalIRMap};
