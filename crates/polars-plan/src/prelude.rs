ae4386cd9692b583419a3fc578e0739cfbc6b8b8
Orson Peters <orsonpeters@gmail.com>
2026-08-10 14:57:03 +0200

    Fmt

--- crates/polars-plan/src/prelude.rs
+++ crates/polars-plan/src/prelude.rs
@@ -6,23 +6,23 @@ pub(crate) use polars_ops::chunked_array::UnicodeForm;
 #[cfg(feature = "iejoin")]
 pub(crate) use polars_ops::frame::{IEJoinOptions, InequalityOperator};
 pub(crate) use polars_ops::frame::{JoinArgs, JoinCoalesce, JoinType, MaintainOrderJoin};
-#[cfg(feature = "business")]
-pub(crate) use polars_ops::series::Roll;
 #[cfg(feature = "ewma")]
 pub(crate) use polars_ops::series::EWMOptions;
 #[cfg(feature = "interpolate")]
 pub(crate) use polars_ops::series::InterpolationMethod;
-#[cfg(feature = "rank")]
-pub(crate) use polars_ops::series::{RankMethod, RankOptions};
+#[cfg(feature = "business")]
+pub(crate) use polars_ops::series::Roll;
 #[cfg(feature = "round_series")]
 pub(crate) use polars_ops::series::RoundMode;
 pub(crate) use polars_ops::series::{ClosedInterval, SearchSortedSide};
+#[cfg(feature = "rank")]
+pub(crate) use polars_ops::series::{RankMethod, RankOptions};
 #[cfg(any(feature = "rolling_window", feature = "rolling_window_by"))]
 pub(crate) use polars_time::chunkedarray::RollingOptionsDynamicWindow;
-#[cfg(feature = "dynamic_group_by")]
-pub(crate) use polars_time::{DynamicGroupOptions, RollingGroupOptions};
 #[cfg(feature = "temporal")]
 pub(crate) use polars_time::{ClosedWindow, Duration};
+#[cfg(feature = "dynamic_group_by")]
+pub(crate) use polars_time::{DynamicGroupOptions, RollingGroupOptions};
 pub use polars_utils::arena::{Arena, Node};
 
 pub use crate::callback::*;
