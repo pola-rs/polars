d2c652e10174ed70fd2c5210cc6b018f5437d36d
Mikhail Epifanov <7824786+mikhail5555@users.noreply.github.com>
2026-08-10 14:10:05 +0200

    fix: Small typo in docstring (#28763)

--- py-polars/src/polars/expr/list.py
+++ py-polars/src/polars/expr/list.py
@@ -1563,7 +1563,7 @@ class ExprListNameSpace(_NamespaceSuggestMixin):
 
     def set_difference(self, other: IntoExpr | Collection[Any]) -> Expr:
         """
-        Compute the SET DIFFERENCE between the elements in this list and the elements of `other.
+        Compute the SET DIFFERENCE between the elements in this list and the elements of `other`.
 
         .. engine-support:: in-memory, streaming, distributed
 
