f77d583f09d8938c797463219d8a85c3f5a1f079
Henry Sanford <henrytsanford@gmail.com>
2026-08-13 17:36:13 -0400

    swap decoder to MultiGzDecoder

--- py-polars/tests/unit/io/test_parquet.py
+++ py-polars/tests/unit/io/test_parquet.py
@@ -4406,7 +4406,7 @@ def test_read_parquet_legacy_nested_maps_27159(io_files_path: Path) -> None:
 def test_read_parquet_concatenated_gzip_members_28787(io_files_path: Path) -> None:
     path = io_files_path / "concatenated_gzip_members.parquet"
 
-    expected = pl.DataFrame({"long_col": range(1, 514)}, schema={"long_col": pl.Int64})
+    expected = pl.DataFrame({"long_col": range(1, 514)}, schema={"long_col": pl.UInt64})
 
     assert_frame_equal(pl.read_parquet(path), expected)
     assert_frame_equal(pl.scan_parquet(path).collect(), expected)
