b41c1e03d1f760d471055d5915ca4cb3646404cd
Daniel Szilagyi <daniel@polars.tech>
2026-08-13 16:41:20 +0200

    Better `LazyFrame.describe()` collection

--- py-polars/src/polars/lazyframe/frame.py
+++ py-polars/src/polars/lazyframe/frame.py
@@ -1172,18 +1172,21 @@ naive plan: (run LazyFrame.explain(optimized=True) to see the optimized plan)
                 ]
             )
 
-        metrics_query = (
-            # if more than one quantile, sort the relevant columns to make them O(1)
-            # TODO: drop sort once we have efficient retrieval of multiple quantiles
-            self.with_columns(F.col(c).sort() for c in sort_cols) if sort_cols else self
-        ).select(*metric_exprs)
-
         # We want to compute the metrics using the selected engine, and once they have
         # been computed, we collect them eagerly for local consumption
-        engine = _select_engine("auto")
-        opt = DEFAULT_QUERY_OPT_FLAGS
-        lf_metrics = engine.execute(metrics_query, optimizations=opt).lazy()
-        df_metrics = engine._collect_eager(lf_metrics, optimizations=opt)
+        df_metrics = (
+            (
+                # if more than one quantile, sort the relevant columns to make them O(1)
+                # TODO: drop sort once we have efficient retrieval of multiple quantiles
+                self.with_columns(F.col(c).sort() for c in sort_cols)
+                if sort_cols
+                else self
+            )
+            .select(*metric_exprs)
+            .execute()
+            .lazy()
+            ._collect_eager()
+        )
 
         # reshape wide result
         n_metrics = len(metrics)
