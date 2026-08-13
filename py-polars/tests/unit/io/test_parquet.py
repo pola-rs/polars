77cbc149738fc642fc8fd8ac473a9bbce3bd0550
Henry Sanford <henrytsanford@gmail.com>
2026-08-13 17:45:55 -0400

    ruff linting

--- py-polars/tests/unit/io/test_parquet.py
+++ py-polars/tests/unit/io/test_parquet.py
@@ -4403,6 +4403,7 @@ def test_read_parquet_legacy_nested_maps_27159(io_files_path: Path) -> None:
     assert_frame_equal(pl.read_parquet(path), expected)
     assert_frame_equal(pl.scan_parquet(path).collect(), expected)
 
+
 def test_read_parquet_concatenated_gzip_members_28787(io_files_path: Path) -> None:
     path = io_files_path / "concatenated_gzip_members.parquet"
 
@@ -4410,6 +4411,7 @@ def test_read_parquet_concatenated_gzip_members_28787(io_files_path: Path) -> No
 
     assert_frame_equal(pl.read_parquet(path), expected)
     assert_frame_equal(pl.scan_parquet(path).collect(), expected)
+
 
 @pytest.mark.write_disk
 @pytest.mark.parametrize(
