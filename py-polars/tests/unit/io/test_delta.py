957b6cca4028c45cf9ec6db670066fc3c6e18752
nameexhaustion <simonlin.rqmmw@slmail.me>
2026-08-18 22:11:10 +1000

    depr(python): Deprecate `rechunk` parameter for all read/scan functions (#28063)

--- py-polars/tests/unit/io/test_delta.py
+++ py-polars/tests/unit/io/test_delta.py
@@ -43,7 +43,7 @@ def new_pl_delta_dataset(source: str | DeltaTable) -> DeltaDataset:
         delta_table_options=None,
         use_pyarrow=False,
         pyarrow_options=None,
-        rechunk=False,
+        rechunk=None,
     )
 
 
