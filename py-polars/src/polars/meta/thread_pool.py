4f274f5d35376cc377b9fc4dd12a51ac43574117
Amber Sprenkels <amber@polars.tech>
2026-08-19 11:07:22 +0200

    Remove threadpool_size() function

--- py-polars/src/polars/meta/thread_pool.py
+++ py-polars/src/polars/meta/thread_pool.py
@@ -2,20 +2,8 @@ from __future__ import annotations
 
 import contextlib
 
-from polars._utils.deprecation import deprecated
-
 with contextlib.suppress(ImportError):  # Module not available when building docs
     import polars._plr as plr
-
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    import sys
-
-    if sys.version_info >= (3, 13):
-        from warnings import deprecated
-    else:
-        from typing_extensions import deprecated  # noqa: TC004
 
 
 def thread_pool_size() -> int:
@@ -37,14 +25,3 @@ def thread_pool_size() -> int:
     16
     """
     return plr.thread_pool_size()
-
-
-@deprecated("`threadpool_size` was renamed; use `thread_pool_size` instead.")
-def threadpool_size() -> int:
-    """
-    Return the number of threads in the Polars thread pool.
-
-    .. deprecated:: 0.20.7
-        This function has been renamed to :func:`thread_pool_size`.
-    """
-    return thread_pool_size()
