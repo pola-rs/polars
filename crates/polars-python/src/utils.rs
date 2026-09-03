ed1a1a136d43319fc94b0bc5e424936f4bdfe24e
Orson Peters <orsonpeters@gmail.com>
2026-09-03 22:28:30 +0200

    fix(rust): Ensure panics don't trigger POLARS_TIMEOUT_MS

--- crates/polars-python/src/utils.rs
+++ crates/polars-python/src/utils.rs
@@ -12,7 +12,7 @@ use pyo3::{PyErr, PyResult, Python};
 use crate::dataframe::PyDataFrame;
 use crate::error::PyPolarsErr;
 use crate::series::PySeries;
-use crate::timeout::{cancel_polars_timeout, is_timeout_enabled, schedule_polars_timeout};
+use crate::timeout::{is_timeout_enabled, schedule_polars_timeout};
 
 /// Calls method on downcasted ChunkedArray for all possible publicly exposed Polars dtypes.
 #[macro_export]
@@ -138,14 +138,11 @@ impl EnterPolarsExt for Python<'_> {
         T: Ungil + Send,
         E: Ungil + Send + Into<PyPolarsErr>,
     {
-        let timeout = if is_timeout_enabled() {
+        let _timeout_guard = is_timeout_enabled().then(|| {
             std::hint::cold_path();
             schedule_polars_timeout(get_traceback(self).ok())
-        } else {
-            None
-        };
+        });
         let ret = self.detach(|| catch_polars_abort(AssertUnwindSafe(f)));
-        cancel_polars_timeout(timeout);
         match ret {
             Ok(Ok(ret)) => Ok(ret),
             Ok(Err(err)) => Err(PyErr::from(err.into())),
