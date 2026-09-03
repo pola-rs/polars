4e079527fabfe11893acf69d30a4a0c276cc7ed0
Connor Grady <connor.grady@gmail.com>
2026-09-03 02:36:18 -0500

    chore: Use nix-systems for declaring the flake's supported systems

--- flake.nix
+++ flake.nix
@@ -11,17 +11,13 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+    systems.url = "github:nix-systems/triplet";
   };
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+      systems = import inputs.systems;
 
       perSystem =
         {
