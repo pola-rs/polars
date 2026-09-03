9f8c1cdb80ee561a342d59763691bb4119b4ae35
Kevin-Patyk <kvn.ptk@gmail.com>
2026-09-03 13:47:41 +0200

    update: fix test

--- py-polars/tests/unit/datatypes/test_decimal.py
+++ py-polars/tests/unit/datatypes/test_decimal.py
@@ -687,6 +687,10 @@ def test_decimal_truediv_int_schema_29105(int_dtype: PolarsDataType) -> None:
         assert schema[name] == pl.Decimal(38, 4)
 
 
+@pytest.mark.parametrize(
+    "int_dtype",
+    [pl.Int8, pl.Int16, pl.Int32, pl.Int64, pl.UInt8, pl.UInt32],
+)
 def test_decimal_add_int_schema_29104(int_dtype: PolarsDataType) -> None:
     lf = pl.LazyFrame({"i": [1]}, schema={"i": int_dtype}).with_columns(
         d=pl.lit(1).cast(pl.Decimal(18, 4))
