86e89fc4f384ed2f0189adffb922225bea504c13
Ritchie Vink <ritchie46@gmail.com>
2026-09-03 09:42:27 +0200

    Update crates/polars-plan/src/dsl/file_scan/mod.rs
    Co-authored-by: nameexhaustion <simonlin.rqmmw@slmail.me>

--- crates/polars-plan/src/dsl/file_scan/mod.rs
+++ crates/polars-plan/src/dsl/file_scan/mod.rs
@@ -591,7 +591,7 @@ pub struct UnifiedScanArgs {
     ///
     /// Note, intentionally store u64 instead of IdxSize to avoid erroring if it's unused.
     pub row_count: Option<(u64, u64)>,
-    pub source_sizes: Option<Arc<[u64]>>,
+    pub source_sizes: Option<Buffer<u64>>,
 }
 
 #[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
