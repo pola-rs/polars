e1b02aed363bcffe90cd23d157a4f78205cdbfc9
Orson Peters <orsonpeters@gmail.com>
2026-09-07 13:19:44 +0200

    refactor(rust): Remove pyobject_converter

--- crates/polars-python/src/on_startup.rs
+++ crates/polars-python/src/on_startup.rs
@@ -198,10 +198,6 @@ pub unsafe fn register_startup_deps(catch_keyboard_interrupt: bool, warn_functio
             });
             Box::new(object) as Box<dyn Any>
         });
-        let pyobject_converter = Arc::new(|av: AnyValue| {
-            let object = Python::attach(|py| Wrap(av).into_py_any(py).unwrap());
-            Box::new(object) as Box<dyn Any>
-        });
         fn object_array_getter(arr: &dyn Array, idx: usize) -> Option<AnyValue<'_>> {
             let arr = arr
                 .as_any()
@@ -289,7 +285,6 @@ pub unsafe fn register_startup_deps(catch_keyboard_interrupt: bool, warn_functio
         registry::register_object_builder(
             object_builder,
             object_converter,
-            pyobject_converter,
             physical_dtype,
             Arc::new(object_array_getter),
             Arc::new(with_gil),
