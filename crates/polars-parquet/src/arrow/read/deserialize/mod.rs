9e7e2220f71d69d6b2810089b56070a8c594f282
Orson Peters <orsonpeters@gmail.com>
2026-09-07 16:02:55 +0200

    Rm get_page_iterator

--- crates/polars-parquet/src/arrow/read/deserialize/mod.rs
+++ crates/polars-parquet/src/arrow/read/deserialize/mod.rs
@@ -27,23 +27,7 @@ pub use self::utils::filter::{Filter, PredicateFilter};
 use self::utils::freeze_validity;
 use super::*;
 use crate::parquet::error::{ParquetError, ParquetResult};
-use crate::parquet::read::get_page_iterator as _get_page_iterator;
 use crate::parquet::schema::types::PrimitiveType;
-
-/// Creates a new iterator of compressed pages.
-pub fn get_page_iterator(
-    column_metadata: &ColumnChunkMetadata,
-    reader: Cursor<Buffer<u8>>,
-    buffer: Vec<u8>,
-    max_header_size: usize,
-) -> PolarsResult<PageReader> {
-    Ok(_get_page_iterator(
-        column_metadata,
-        reader,
-        buffer,
-        max_header_size,
-    )?)
-}
 
 /// Creates a new [`ListArray`] or [`FixedSizeListArray`].
 pub fn create_list(
