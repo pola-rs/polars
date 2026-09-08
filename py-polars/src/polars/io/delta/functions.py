96c401a96eb00426ef68bc80e844de49320c85d4
Simon Lin <simonlin.rqmmw@slmail.me>
2026-09-08 17:17:38 +1000

    nxs/scan-resolver-delta

--- py-polars/src/polars/io/delta/functions.py
+++ py-polars/src/polars/io/delta/functions.py
@@ -5,9 +5,8 @@ import importlib.util
 from typing import TYPE_CHECKING, Any
 
 from polars._utils.expired import RemovedParameter, removed_parameters
-from polars._utils.wrap import wrap_ldf
 from polars.io.cloud._utils import NoPickleOption
-from polars.io.delta._dataset import DeltaDataset
+from polars.io.delta._scan_resolver import DeltaScanResolver
 
 if TYPE_CHECKING:
     from datetime import datetime
@@ -296,7 +295,6 @@ def scan_delta(
     ...     table_path, delta_table_options=delta_table_options
     ... ).collect()  # doctest: +SKIP
     """
-    from polars._plr import PyLazyFrame
     from polars.io.cloud.credential_provider._builder import (
         _init_credential_provider_builder,
     )
@@ -329,7 +327,7 @@ def scan_delta(
             **(storage_options or {}),
         }
 
-    dataset = DeltaDataset(
+    return DeltaScanResolver(
         table_=NoPickleOption(table),
         table_uri_=str(source) if table is None else None,
         version=version,
@@ -338,6 +336,4 @@ def scan_delta(
         delta_table_options=delta_table_options,
         use_pyarrow=use_pyarrow,
         pyarrow_options=pyarrow_options,
-    )
-
-    return wrap_ldf(PyLazyFrame.new_from_dataset_object(dataset))
+    ).lazy()
