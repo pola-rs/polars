bfa7c417ece39968d6eb564336147cccd7ae4813
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2026-09-11 14:50:35 +0100

    chore(rust): Remove unused `utf8_to_timestamp_scalar`

--- crates/polars-compute/src/cast/temporal.rs
+++ crates/polars-compute/src/cast/temporal.rs
@@ -16,37 +16,6 @@ pub const fn time_unit_multiple(unit: TimeUnit) -> i64 {
     }
 }
 
-/// Parses `value` to `Option<i64>` consistent with the Arrow's definition of timestamp with timezone.
-///
-/// `tz` must be built from `timezone` (either via [`parse_offset`] or `chrono-tz`).
-/// Returns in scale `tz` of `TimeUnit`.
-#[inline]
-pub fn utf8_to_timestamp_scalar<T: chrono::TimeZone>(
-    value: &str,
-    fmt: &str,
-    tz: &T,
-    tu: &TimeUnit,
-) -> Option<i64> {
-    let mut parsed = Parsed::new();
-    let fmt = StrftimeItems::new(fmt);
-    let r = chrono::format::parse(&mut parsed, value, fmt).ok();
-    if r.is_some() {
-        parsed
-            .to_datetime()
-            .map(|x| x.naive_utc())
-            .map(|x| tz.from_utc_datetime(&x))
-            .map(|x| match tu {
-                TimeUnit::Second => x.timestamp(),
-                TimeUnit::Millisecond => x.timestamp_millis(),
-                TimeUnit::Microsecond => x.timestamp_micros(),
-                TimeUnit::Nanosecond => x.timestamp_nanos_opt().unwrap(),
-            })
-            .ok()
-    } else {
-        None
-    }
-}
-
 /// Parses `value` to `Option<i64>` consistent with the Arrow's definition of timestamp without timezone.
 /// Returns in scale `tz` of `TimeUnit`.
 #[inline]
