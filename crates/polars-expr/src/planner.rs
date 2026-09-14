f9c5cb4396487d0ec4458306f1aaba9c101bab83
Orson Peters <orsonpeters@gmail.com>
2026-09-14 14:46:32 +0200

    Remove name dedup

--- crates/polars-expr/src/planner.rs
+++ crates/polars-expr/src/planner.rs
@@ -470,19 +470,13 @@ fn create_physical_expr_inner(
                 && !matches!(expr_arena.get(truthy), AExpr::Column(_) | AExpr::Literal(_));
             let mask_falsy = is_elementwise_rec(falsy, expr_arena)
                 && !matches!(expr_arena.get(falsy), AExpr::Column(_) | AExpr::Literal(_));
-            let leaf_names = |node| {
-                let mut names = aexpr_to_leaf_names(node, expr_arena);
-                names.sort_unstable();
-                names.dedup();
-                names
-            };
             let truthy_mask_columns = if mask_truthy {
-                leaf_names(truthy)
+                aexpr_to_leaf_names(truthy, expr_arena)
             } else {
                 Vec::new()
             };
             let falsy_mask_columns = if mask_falsy {
-                leaf_names(falsy)
+                aexpr_to_leaf_names(falsy, expr_arena)
             } else {
                 Vec::new()
             };
