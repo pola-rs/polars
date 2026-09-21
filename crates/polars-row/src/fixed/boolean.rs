d5be1f6f86df7889d3087a671c68f3199b33c8c3
Ritchie Vink <ritchie46@gmail.com>
2026-09-21 11:29:21 +0200

    perf: Optimize row-encoding (#29405)

--- crates/polars-row/src/fixed/boolean.rs
+++ crates/polars-row/src/fixed/boolean.rs
@@ -17,24 +17,23 @@ use polars_arrow::datatypes::ArrowDataType;
 
 use crate::row::RowEncodingOptions;
 
+#[inline(always)]
+fn sentinel(opt_value: Option<bool>, opt: RowEncodingOptions) -> u8 {
+    match opt_value {
+        None => opt.null_sentinel(),
+        Some(false) => opt.bool_false_sentinel(),
+        Some(true) => opt.bool_true_sentinel(),
+    }
+}
+
 pub(crate) unsafe fn encode_bool<I: Iterator<Item = Option<bool>>>(
     buffer: &mut [MaybeUninit<u8>],
     input: I,
     opt: RowEncodingOptions,
     offsets: &mut [usize],
 ) {
-    let null_sentinel = opt.null_sentinel();
-    let true_sentinel = opt.bool_true_sentinel();
-    let false_sentinel = opt.bool_false_sentinel();
-
     for (offset, opt_value) in offsets.iter_mut().zip(input) {
-        let b = match opt_value {
-            None => null_sentinel,
-            Some(false) => false_sentinel,
-            Some(true) => true_sentinel,
-        };
-
-        *buffer.get_unchecked_mut(*offset) = MaybeUninit::new(b);
+        *buffer.get_unchecked_mut(*offset) = MaybeUninit::new(sentinel(opt_value, opt));
         *offset += 1;
     }
 }
